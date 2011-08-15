6ec4c5442699154d7d8b1eff2eed30692f05031c
Erik Tollerud <erik.tollerud@gmail.com>
2011-08-14 23:07:13 -0700

    updated setup.py and astropy/version.py to properly freeze development version number

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from distribute_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup,find_packages,Extension
+from distutils.command.build_py import build_py as du_build_py
 from glob import glob
 from warnings import warn
 
@@ -29,6 +30,9 @@ astropypkgs = find_packages(exclude=['tests'])
 astropyscripts = glob('scripts/*')
 astropyscripts.remove('scripts/README.rst')
 
+#this dictionary stores the command classes used in setup below
+cmdclassd = {}
+
 #C extensions that are not Cython-based should be added here.
 extensions = []
 
@@ -51,6 +55,9 @@ for  dirpath, dirnames, filenames in os.walk('astropy'):
             pyxfiles.append((extmod,fullfn))
     
 if not release and have_cython:
+    from Cython.Distutils import build_ext as cython_build_ext
+    cmdclassd['build_ext'] = cython_build_ext
+    
     #add .pyx files
     for extmod,pyxfn in pyxfiles:
         extensions.append(Extension(extmod,[pyxfn]))
@@ -71,23 +78,52 @@ else:
 try:
     from sphinx.setup_command import BuildDoc
     
-    class apy_build_sphinx(BuildDoc):
+    class astropy_build_sphinx(BuildDoc):
         def finalize_options(self):
             from os.path import isfile    
             from distutils.cmd import DistutilsOptionError
             
             if self.build_dir is not None:
                 if isfile(self.build_dir):
-                    raise DistutilsOptionError('Attempted to build_sphinx into a file '+self.build_dir)
+                    raise DistutilsOptionError('Attempted to build_sphinx ' + \
+                                               'into a file '+self.build_dir)
                 self.mkpath(self.build_dir)
             return BuildDoc.finalize_options(self)
             
+    cmdclassd['build_sphinx'] = astropy_build_sphinx        
 except ImportError: #sphinx not present
-    apy_build_sphinx = None
+    pass
     
-cmdclassd = {}
-if apy_build_sphinx is not None:
-    cmdclassd['build_sphinx'] = apy_build_sphinx
+    
+#This customize build_py command replaces the source version.py with one that
+#has a fixed version number.  This is necessary to support version numbers
+#pulled from the git repository.
+class astropy_build_py(du_build_py):
+    def run(self):
+        from os import path
+        res = du_build_py.run(self)
+        
+        versfile = path.join(self.build_lib,'astropy','version.py')
+        print 'freezing version number to',versfile
+        with open(versfile,'w') as f: #this overwrites the actual version.py
+            f.write(self.get_version_py())
+        
+        return res
+        
+    def get_version_py(self):
+        import datetime
+        from astropy.version import _frozen_version_py_template
+        from astropy.version import version,major,minor,bugfix,release
+        
+        timestamp = str(datetime.datetime.now())
+        return _frozen_version_py_template.format(timestamp=timestamp,
+                                                  verstr=version,
+                                                  maj=major,
+                                                  minor=minor
+                                                  bigfux=bugfix,
+                                                  rel=release)
+cmdclassd['build_py'] = astropy_build_py
+
     
 setup(name=NAME,
       version=versionstring,
