5c73dfb7ae628f0cdaa3a22c8fd6b203e77c70a2
Erik Tollerud <erik.tollerud@gmail.com>
2011-08-15 12:12:43 -0700

    added line providing __version__ variable to the astropy root package

--- astropy/__init__.py
+++ astropy/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from __future__ import division
+from astropy.version import version as __version__
 
 """_Astropy is a package intended to contain core functionality and some
 common tools needed for performing astronomy and astrophysics research with 
