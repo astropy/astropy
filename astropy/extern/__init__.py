5360fcc4b12d55566e9c191385ae4b3c14c0a06c
Erik Tollerud <erik.tollerud@gmail.com>
2011-09-07 20:16:08 -0700

    Corrected /extern to /cextern in astropy/extern/__init__.py

--- astropy/extern/__init__.py
+++ astropy/extern/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 
 """This packages contains python packages that are bundled with Astropy but
 are external to Astropy, and hence are developed in a separate source tree. 
-Note that this package is distinct from the /extern directory of the source 
+Note that this package is distinct from the /cextern directory of the source 
 code distribution, as that directory only contains C extension code.
 
 """
