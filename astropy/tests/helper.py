1081c57bd8dc450a2120d8596a5aad26a0bee549
Matt Davis <jiffyclub.programatic@gmail.com>
2011-10-14 17:25:35 -0400

    Added runtests function to astropy/tests/helper.py for running specific tests. Will be imported in astropy/__init__.py as test.

--- astropy/tests/helper.py
+++ astropy/tests/helper.py
@@ -1,17 +1,17 @@
-"""
-Implements pytest.main() after importing from a stand-alone module produced with
-py.test --genscript. Method adapted from file created by py.test --genscript.
-
-"""
-
 import sys
 import base64
 import zlib
 import imp
 
-import astropy.extern.pytest as pytest
+try:
+    import pytest
+    HAVE_PYTEST = True
+except ImportError:
+    HAVE_PYTEST = False
+    
+import astropy.extern.pytest
 
-def main(args=None,plugins=None):
+def pytest_main(args=None,plugins=None):
     """
     Implements pytest.main() after importing from a stand-alone module produced
     with py.test --genscript. Method adapted from file created by
@@ -24,20 +24,39 @@ def main(args=None,plugins=None):
     """
     if sys.version_info >= (3, 0):
         exec("def do_exec_def(co, loc): exec(co, loc)\n")
-        pytest.do_exec = do_exec_def
+        astropy.extern.pytest.do_exec = do_exec_def
         
         import pickle
-        unpacked_sources = pytest.sources.encode("ascii") # ensure bytes
+        unpacked_sources = astropy.extern.pytest.sources.encode("ascii") # ensure bytes
         unpacked_sources = pickle.loads(zlib.decompress(base64.decodebytes(unpacked_sources)))
     else:
         exec("def do_exec_def(co, loc): exec co in loc\n")
-        pytest.do_exec = do_exec_def
+        astropy.extern.pytest.do_exec = do_exec_def
 
         import cPickle as pickle
-        unpacked_sources = pickle.loads(zlib.decompress(base64.decodestring(pytest.sources)))
+        unpacked_sources = pickle.loads(zlib.decompress(base64.decodestring(astropy.extern.pytest.sources)))
 
-    importer = pytest.DictImporter(unpacked_sources)
+    importer = astropy.extern.pytest.DictImporter(unpacked_sources)
     sys.meta_path.append(importer)
 
-    py = importer.load_module('pytest')
-    py.main(args=args,plugins=plugins)
+    pytest = importer.load_module('pytest')
+    pytest.main(args=args,plugins=plugins)
+    
+
+def runtests(module=None):
+    import os.path
+    
+    if HAVE_PYTEST:
+        main = pytest.main
+    else:
+        main = pytest_main
+    
+    if module is None:
+        main()
+    else:
+        module = module.replace('.',os.path.sep)
+        
+        if not os.path.isdir(module):
+            raise ValueError('Location not found: {0}'.format(module))
+            
+        main(args=module)
