fa76c964d2a0403fa92e6a0276b99d58bc87b6a1
Matt <jiffyclub.programatic@gmail.com>
2011-10-20 11:09:37 -0400

    Added comments to test_run_tests.py.

--- astropy/tests/tests/test_run_tests.py
+++ astropy/tests/tests/test_run_tests.py
@@ -2,10 +2,12 @@
 
 from astropy.tests import helper
 
+# run_tests should raise ValueError when asked to run on a module it can't find
 def test_module_not_found():
     with helper.pytest.raises(ValueError):
         helper.run_tests('fake.module')
-        
+
+# run_tests should raise ValueError when passed an invalid pastebin= option
 def test_pastebin_keyword():
     with helper.pytest.raises(ValueError):
         helper.run_tests(pastebin='not_an_option')
