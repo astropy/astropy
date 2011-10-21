7cd94ee91bb4cf9ab1ad11130c3b161f128adb58
Matt <jiffyclub.programatic@gmail.com>
2011-10-21 18:38:44 -0400

    Modified test_skip_remote_data so that it fails if the test is somehow not skipped when remote_data=False. Test passes if called from the command line or if remote_data=True.

--- astropy/tests/tests/test_skip_remote_data.py
+++ astropy/tests/tests/test_skip_remote_data.py
@@ -2,7 +2,21 @@
 # by run_tests because it has the remote_data decorator.
 
 from astropy.tests.helper import remote_data
+from astropy.tests.helper import pytest
 
 @remote_data
-def test_skip_me():
-    assert True
+def test_skip_remote_data(pytestconfig):
+    # this test was called from the command line and it should behave as if
+    # astropy.test() has remote_data=True
+    if not hasattr(pytestconfig.option, 'remotedata'):
+        assert True
+    
+    # astropy.test() has remote_data=False but we still got here somehow,
+    # so fail with a helpful message
+    elif not getattr(pytestconfig.option, 'remotedata'):
+        pytest.fail('@remote_data was not skipped with remote_data=False')
+        
+    # astropy.test() has remote_data=True, so pass
+    elif getattr(pytestconfig.option, 'remotedata'):
+        assert True
+    
