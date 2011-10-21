b1968be40398d8ce79c6f5e35f25c7f786f47646
Erik Bray <embray@stsci.edu>
2011-10-21 16:13:32 -0400

    Adds a 'test' command to the setup.py (distribute comes with a test command but it doesn't work with py.test, so this replaces it outright).  This also supports all the existing options to astropy.test() such as module and verbose.

--- astropy/tests/helper.py
+++ astropy/tests/helper.py
@@ -2,8 +2,9 @@ import sys
 import base64
 import zlib
 import functools
-
 import os.path
+
+from distutils.core import Command
 
 from .. import __path__ as astropy_path
 
@@ -31,7 +32,7 @@ except ImportError:
     sys.meta_path.append(importer)
 
     pytest = importer.load_module('pytest')
-    
+
 
 # pytest marker to mark tests which get data from the web
 remote_data = pytest.mark.remote_data
@@ -45,7 +46,7 @@ def pytest_addoption(parser):
 def pytest_runtest_setup(item):
     if 'remote_data' in item.keywords and not item.config.getvalue("remotedata"):
         pytest.skip("need --remotedata option to run")
-        
+
 
 def run_tests(module=None, args=None, plugins=None, verbose=False,
               pastebin=None, remote_data=False):
@@ -57,7 +58,7 @@ def run_tests(module=None, args=None, plugins=None, verbose=False,
     ----------
     module : str, optional
         The name of a specific module to test, e.g. 'io.fits' or 'utils'.
-        If nothing is specified all default Astropy tests are run.
+        If nothing is specified all default AstroPy tests are run.
 
     args : str, optional
         Additional arguments to be passed to `pytest.main` in the `args`
@@ -65,21 +66,21 @@ def run_tests(module=None, args=None, plugins=None, verbose=False,
 
     plugins : list, optional
         Plugins to be passed to `pytest.main` in the `plugins` keyword argument.
-        
+
     verbose : bool, optional
         Convenience option to turn on verbose output from py.test. Passing True
         is the same as specifying `-v` in `args`.
-        
+
     pastebin : {'failed','all',None}, optional
         Convenience option for turning on py.test pastebin output. Set to
         'failed' to upload info for failed tests, or 'all' to upload info for
         all tests.
-        
+
     remote_data : bool, optional
         Controls whether to run tests marked with @remote_data. These
         tests use online data and are not run by default. Set to True to
         run these tests.
-        
+
     See Also
     --------
     pytest.main : py.test function wrapped by `run_tests`.
@@ -96,27 +97,62 @@ def run_tests(module=None, args=None, plugins=None, verbose=False,
     # '-p astropy.tests.helper' tells py.test to use this module as a plugin
     # so that the hooks defined above are actually used.
     all_args = module_path + ' -p astropy.tests.helper'
-    
+
     # add any additional args entered by the user
     if args is not None:
         all_args += ' {0}'.format(args)
-    
+
     # add verbosity flag
     if verbose:
         all_args += ' -v'
-    
+
     # turn on pastebin output
     if pastebin is not None:
         if pastebin in ['failed', 'all']:
             all_args += ' --pastebin={0}'.format(pastebin)
         else:
             raise ValueError("pastebin should be 'failed' or 'all'")
-    
+
     # run @remote_data tests
     if remote_data:
         all_args += ' --remotedata'
-    
-    pytest.main(args=all_args, plugins=plugins)
+
+    return pytest.main(args=all_args, plugins=plugins)
+
+
+class astropy_test(Command):
+    user_options = [
+        ('module=', 'm',
+         "The name of a specific module to test, e.g. 'io.fits' or 'utils'.  "
+         "If nothing is specified all default AstroPy tests are run."),
+        ('verbose-results', 'V',
+         'Turn on verbose output from pytest. Same as specifying `-v` in '
+         '`args`.'),
+        ('plugins=', 'p',
+         'Plugins to enable when running pytest.  Same as specifying `-p` in '
+         '`args`.'),
+        ('pastebin=', 'b',
+         "Enable pytest pastebin output. Either 'all' or 'failed'."),
+        ('args=', 'a', 'Additional arguments to be passed to pytest')
+    ]
+
+    def initialize_options(self):
+        self.module = None
+        self.verbose_results = False
+        self.plugins = None
+        self.pastebin = None
+        self.args = None
+
+    def finalize_options(self):
+        # Normally we would validate the options here, but that's handled in
+        # run_tests
+        pass
+
+    def run(self):
+        errno = run_tests(self.module, self.args, self.plugins,
+                          self.verbose_results, self.pastebin)
+        raise SystemExit(errno)
+>>>>>>> Adds a 'test' command to the setup.py (distribute comes with a test command but it doesn't work with py.test, so this replaces it outright).  This also supports all the existing options to astropy.test() such as module and verbose.
 
 
 class raises:
