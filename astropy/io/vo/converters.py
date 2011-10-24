9448d0e19349bedbe9bb0dd14ec220512262293b
Michael Droettboom <mdboom@gmail.com>
2011-10-24 13:28:12 -0400

    Remove licenses.

--- astropy/io/vo/converters.py
+++ astropy/io/vo/converters.py
@@ -1,32 +1,3 @@
-# Copyright (C) 2008-2010 Association of Universities for Research in Astronomy (AURA)
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-
-#     1. Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-
-#     2. Redistributions in binary form must reproduce the above
-#       copyright notice, this list of conditions and the following
-#       disclaimer in the documentation and/or other materials provided
-#       with the distribution.
-
-#     3. The name of AURA and its representatives may not be used to
-#       endorse or promote products derived from this software without
-#       specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY AURA ``AS IS'' AND ANY EXPRESS OR IMPLIED
-# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL AURA BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
-# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
-# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
-# DAMAGE.
-
 """
 This module handles the conversion of various VOTABLE datatypes
 to/from TABLEDATA_ and BINARY_ formats.
@@ -399,7 +370,7 @@ class ScalarVarArray(VarArray):
     def parse(self, value, config={}, pos=None):
         if value.strip() == '':
             return np.array([]), True
-        
+
         parts = self._splitter(value, config, pos)
 
         parse = self._base.parse
@@ -711,7 +682,7 @@ class ComplexArrayVarArray(VarArray):
     def parse(self, value, config={}, pos=None):
         if value.strip() == '':
             return np.array([]), True
-        
+
         parts = self._splitter(value, config, pos)
         items = self._base._items
         parse_parts = self._base.parse_parts
@@ -736,7 +707,7 @@ class ComplexVarArray(VarArray):
     def parse(self, value, config={}, pos=None):
         if value.strip() == '':
             return np.array([]), True
-        
+
         parts = self._splitter(value, config, pos)
         parse_parts = self._base.parse_parts
         result = []
@@ -861,7 +832,7 @@ class BitArray(NumericArray):
         value = np.asarray(value)
         mapping = {False: '0', True: '1'}
         return ''.join(mapping[x] for x in value.flat)
-    
+
     def binparse(self, read):
         data = read(self._bytes)
         results = []
