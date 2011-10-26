d7fbc0857484d29808b7109b90df796c2d6673a0
Erik Tollerud <erik.tollerud@gmail.com>
2011-10-26 14:24:57 -0700

    labeled data tests as remote_data

--- astropy/config/tests/test_data.py
+++ astropy/config/tests/test_data.py
@@ -1,5 +1,8 @@
+from ...tests.helper import remote_data
+
 TESTURL = 'http://www.google.com/index.html'
 
+@remote_data
 def test_url_cache():
     from ..data import get_data_filename,clear_data_cache
     from os.path import isfile
@@ -8,7 +11,8 @@ def test_url_cache():
     assert isfile(fnout)
     clear_data_cache(fnout)
     assert not isfile(fnout)
-    
+
+@remote_data
 def test_url_cache_custom_fn():
     from ..data import get_data_filename,clear_data_cache
     from os.path import isfile
@@ -17,7 +21,8 @@ def test_url_cache_custom_fn():
     assert fnout[-27:] == 'tester_custom_filename.html'
     clear_data_cache(fnout)
     assert not isfile(fnout)    
-    
+
+@remote_data
 def test_url_nocache():
     from ..data import get_data_fileobj
     
