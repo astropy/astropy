013a811e7e3a635a99b641e0f6126ee7be669de1
Erik Tollerud <erik.tollerud@gmail.com>
2011-10-26 03:28:56 -0700

    added config directory locator

--- astropy/config/configs.py
+++ astropy/config/configs.py
@@ -5,4 +5,70 @@ from __future__ import division
 for astropy and affiliated modules.
 """
 
-__all__ = []
+__all__ = ['get_config_dir']
+
+def get_config_dir(create=True):
+    """
+    Determines the Astropy configuration directory name.
+    
+    Parameters
+    ----------
+    create : bool
+        If True, the directory will be created if it doesn't exist.
+    
+    Returns
+    -------
+    configdir : str
+        The absolute path to the configuration directory.
+    """
+    import os,sys
+    from os import environ as env
+
+    #First find the home directory - this is inspired by the scheme ipython
+    #uses to identify "home"
+    if os.name == 'posix':
+        # Linux, Unix, AIX, OS X
+        if 'HOME' in env:
+            homedir = env['HOME'].decode(sys.getfilesystemencoding())
+        else:
+            raise OSError('Could not find unix home directory to search for astropysics config dir')
+    elif os.name == 'nt': # This is for all modern Windows (NT or after)
+        #Try for a network home first
+        if 'HOMESHARE' in env: 
+            homedir = env['HOMESHARE'].decode(sys.getfilesystemencoding())
+        #See if there's a local home
+        elif 'HOMEDRIVE' in env and 'HOMEPATH' in env: 
+            homedir = os.path.join(env['HOMEDRIVE'],env['HOMEPATH'])
+            homedir = homedir.decode(sys.getfilesystemencoding())
+        #Maybe a user profile?
+        elif 'USERPROFILE' in env:
+            homedir = os.path.join(env['USERPROFILE']).decode(sys.getfilesystemencoding())
+        else:            
+            try:
+                import _winreg as wreg
+                key = wreg.OpenKey(
+                    wreg.HKEY_CURRENT_USER,
+                    "Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders"
+                )
+                homedir = wreg.QueryValueEx(key,'Personal')[0]
+                homedir = homedir.decode(sys.getfilesystemencoding())
+                key.Close()
+            except:
+                #As a final possible resort, see if HOME is present for some reason
+                if 'HOME' in env:
+                    homedir = env['HOME'].decode(sys.getfilesystemencoding())
+                else:
+                    raise OSError('Could not find windows home directory to search for astropysics config dir')
+    else:
+        #for other platforms, try HOME, although it probably isn't there
+        if 'HOME' in env:
+            homedir = env['HOME'].decode(sys.getfilesystemencoding())
+        else:
+            raise OSError('Could not find a home directory to search for astropysics config dir - are you on an unspported platform?')
+        
+    configdir = os.path.realpath(os.path.join(homedir,'.astropy'))
+        
+    if create and not os.path.isdir(configdir):
+        #try to create it
+        os.mkdir(configdir)
+    return configdir
