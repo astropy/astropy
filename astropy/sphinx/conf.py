799d03869721f1a42e203b099cb7bf0faa4fdfb4
Erik Bray <embray@stsci.edu>
2011-11-03 19:41:37 -0400

    HTML documentation now builds without any warnings/errors.

--- astropy/sphinx/conf.py
+++ astropy/sphinx/conf.py
@@ -19,7 +19,10 @@ needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode', 'astropy.sphinx.ext.numpydoc', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
+              'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.viewcode', 'astropy.sphinx.ext.numpydoc',
+              'sphinx.ext.autosummary', 'sphinx.ext.inheritance_diagram']
 
 # Don't show summaries of the members in each class along with the
 # class' docstring
