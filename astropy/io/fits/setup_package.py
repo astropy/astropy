56826027a1c6a1977f7712e8b67794351ecaa33d
Erik Bray <embray@stsci.edu>
2011-11-02 20:19:12 -0400

    Add a setup_package module so that the extension module gets built properly.

---
+++ astropy/io/fits/setup_package.py
@@ -0,0 +1,21 @@
+import os
+
+from distutils.core import Extension
+from glob import glob
+
+from astropy import setup_helpers
+
+def get_extensions():
+    return [
+        Extension('astropy.io.fits.compression',
+                  glob(os.path.join(os.path.dirname(__file__), 'src/*.c')),
+                  include_dirs=[setup_helpers.get_numpy_include_path()],
+                  extra_compile_args=['-Wno-unused-function',
+                                      '-Wno-strict-prototypes'])
+    ]
+
+
+def get_package_data():
+    # Installs the testing data files
+    return {
+        'astropy.io.fits.tests': ['data/*.fits']}
