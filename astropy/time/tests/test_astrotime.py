01c54d8760e4ae7156c65cf235f1f9ee059bc056
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-08 03:46:42 -0800

    updated to this version of astrotime

--- astropy/time/tests/test_astrotime.py
+++ astropy/time/tests/test_astrotime.py
@@ -1,13 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from astropy import astrotime
+from .time import AstroTime
 import datetime
 
 def test_jd2gregorian():
-    test_date = astrotime.AstroTime.from_jd(2449443.5)
-    assert test_date.to_date_gregorian() == datetime.datetime(1994, 4, 1, 0, 0, 0)
+    test_date = AstroTime.from_jd(2449443.5)
+    assert test_date.datetime == datetime.datetime(1994, 4, 1, 0, 0, 0)
 
 def test_gregorian2jd():
-    test_date = astrotime.AstroTime.from_date_gregorian(datetime.datetime(1994, 4, 1, 0, 0, 0))
+    test_date = AstroTime(datetime.datetime(1994, 4, 1, 0, 0, 0))
     #Should be assert float
-    assert test_date.to_jd() == 2449443.5
+    assert test_date.jd == 2449443.5
+    
