7b3336ee9721994b9365e2c0029a5e14fcf9ba69
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-08 03:52:48 -0800

    added addsub testing

--- astropy/time/tests/test_astrotime.py
+++ astropy/time/tests/test_astrotime.py
@@ -12,3 +12,23 @@ def test_gregorian2jd():
     #Should be assert float
     assert test_date.jd == 2449443.5
     
+def test_addsub_astrotime():
+    a = AstroTime(-5)
+    b = AstroTime(10)
+    
+    c = a + b
+    assert c._val == 5
+    
+    c = a - b
+    assert c._val == -15
+    
+    a = AstroTime(1,1e-8)
+    b = AstroTime(1,1e-9)
+    assert (a+b)._val == 11
+    
+    
+    a = AstroTime(0,1,0)
+    b = AstroTime(0,1,1) #jd 1 later
+    #uses the jd0 of the first one
+    assert (a+b)._val == 86400
+    assert (b+a)._val == -86400
