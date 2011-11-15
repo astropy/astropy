6064db3000f2aeec66a775345d22b8a2b421497f
Michael Droettboom <mdboom@gmail.com>
2011-11-14 19:11:08 -0500

    Fix gzip test for Python 2.6

--- astropy/utils/tests/test_gzip.py
+++ astropy/utils/tests/test_gzip.py
@@ -4,6 +4,8 @@ import os
 from ...tests.helper import pytest
 from .. import gzip
 
+pytestmark = pytest.mark.skipif("sys.version_info < (3,0)")
+
 def test_gzip(tmpdir):
     fd = gzip.GzipFile(str(tmpdir.join("test.gz")), 'wb')
     fd = io.TextIOWrapper(fd, encoding='utf8')
