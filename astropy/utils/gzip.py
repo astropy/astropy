fdfb3926f4352463b96aff679a164db07e1445ad
Michael Droettboom <mdboom@gmail.com>
2011-11-14 19:12:55 -0500

    Fix gzip usage on 3.1 (again)

--- astropy/utils/gzip.py
+++ astropy/utils/gzip.py
@@ -6,7 +6,7 @@ in Python 3.1.
 from __future__ import absolute_import
 
 import sys
-if sys.version_info[:3] == (3, 1):
+if sys.version_info[:2] == (3, 1):
     """Functions that read and write gzipped files.
 
     The user of the file doesn't have to worry about the compression,
