bc96ee69f13cf4cc278282b7407b0ef71c30932d
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-19 11:04:29 -0800

    Moved some docstrings before imports so they actually show up as docstrings

--- astropy/utils/__init__.py
+++ astropy/utils/__init__.py
@@ -1,11 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import division
-
-from .odict import OrderedDict
-
 """
 This package contains utilities of general use in the `astropy` package and
 affiliated packages.
 """
 
-
+from .odict import OrderedDict
