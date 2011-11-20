e666663f960a4fcd2299cc6cda0f60838ac3256e
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-19 19:39:39 -0800

    some tests for config

--- astropy/config/tests/test_configs.py
+++ astropy/config/tests/test_configs.py
@@ -4,5 +4,16 @@ def test_config_file():
     from ..configs import get_config
     
     
-    cfg = get_config('config')
-    assert cfg.filename.endswith('config.cfg')
+    cfg1 = get_config('astropy')
+    assert cfg1.filename.endswith('astropy.cfg')
+    
+    
+    cfg2 = get_config('astropy.config')
+    assert cg2.depth==1
+    assert cfg2.name=='config'
+    
+def test_pkg_finder():
+    from ..configs import _find_current_module
+    
+    assert _find_current_module(0).__name__ == 'astropy.config.configs'
+    assert _find_current_module(1).__name__ == 'astropy.config.tests.test_configs'
