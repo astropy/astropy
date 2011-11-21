3ba8a87659877672eaaba062313c13a543b22f83
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-21 00:42:25 -0800

    configs work

--- astropy/config/configs.py
+++ astropy/config/configs.py
@@ -10,12 +10,25 @@ configuration files for Astropy and affiliated packages.
 """
 
 from __future__ import division
-from ..extern.configobj import configobj
+from ..extern.configobj import configobj,validate
 
-__all__ = ['ConfigurationItem','get_config']
+__all__ = ['ConfigurationItem','InvalidConfigurationItemWarning','get_config',
+           'save_config','reload_config']
+
+
+class InvalidConfigurationItemWarning(Warning):
+    """ A Warning that is issued when the configuration value specified in the
+    astropy configuration file does not match the type expected for that 
+    configuration value.
+    """
 
 class ConfigurationItem(object):
-    """ Fill in docstring
+    """ A setting and associated value stored in the astropy configuration 
+    files.
+    
+    These objects are typically defined at the top of astropy subpackages
+    or affiliated packages, and store values or option settings that can be 
+    modified by the user to 
     
     Parameters
     ----------
@@ -23,59 +36,150 @@ class ConfigurationItem(object):
         The (case-sensitive) name of this parameter, as shown in the 
         configuration file.
     defaultvalue : 
-        The default value for this item.
+        The default value for this item. If this is a list of strings, this item
+        will be interpreted as an 'options' value - this item must be one of
+        those values, and the first in the list will be taken as the default
+        value.
     description : str or None
         A description of this item (will be shown as a comment in the 
         configuration file)
-    package : str or None
-        The full package name that this item is associated with.  The first
-        element of the pacakge name (e.g. `astropy`) will be used to determine
-        the name of the configuration file, while the remaining items determine
-        the section.  If None, the package will be inferred from the package 
-        within whiich this object's initializer is called.
+    cfgtype : str or None
+        A type specifier like those used as the *values* of a particular key in
+        a `configspec` file of `configobj`. If None, the type will be inferred
+        from the default value.
+    module : str or None
+        The full module name that this item is associated with. The first
+        element (e.g. 'astropy' if this is 'astropy.config.configs') will be
+        used to determine the name of the configuration file, while the
+        remaining items determine the section. If None, the package will be
+        inferred from the package within whiich this object's initializer is
+        called.
         
     Raises
     ------
     RuntimeError
-        If `package` is None, but the package this item is created from cannot 
+        If `module` is None, but the module this item is created from cannot 
         be determined.
+        
+    Examples
+    --------
+    The following example will create an item 'cfgoption = 42' in the 
+    '[configs]' section of astropy.cfg (located in the directory that
+    `astropy.config.paths.get_config_dir` returns), or if the option is already
+    set, it will take the value from the configuration file::
+        
+        from astropy.config import ConfigurationItem
+        
+        CFG_OPTION = ConfigurationItem('cfgoption',42,module='astropy.configs')
+        
+    If called as ``CFG_OPTION()``, this will return the value ``42``, or some
+    other integer if the ``astropy.cfg`` file specifies a different value.    
+    
+    If this were in the ``astropy/configs/__init__.py`` file, the `module` 
+    option would not be necessary, as it would automatically detect the correct
+    module.
     
     """
     
-    def __init__(self,name,defaultvalue='',description=None,package=None):
+    #this is used to make validation faster so a Validator object doesn't
+    #have to be created every time
+    _validator = validate.Validator()
+    
+    def __init__(self,name,defaultvalue='',description=None,cfgtype=None,
+                      module=None):
         
-        if package is None:
-            package = _find_current_module(2)
-            if package is None:
-                raise RuntimeError('get_config not called from inside a valid package')
+        if module is None:
+            module = _find_current_module(2)
+            if module is None:
+                msg1 = 'Cannot automatically determine get_config module, '
+                msg2 = 'because it is not called from inside a valid module'
+                raise RuntimeError(msg1+msg2)
             else:
-                package = package.__name__
-        self.package = package
+                module = module.__name__
+        self.module = module
         raise NotImplementedError
 
     def set(self,value):
-        """ Sets the current valur of this `ConfigurationItem` 
+        """ Sets the current value of this `ConfigurationItem`.
+        
+        .. note::
+            This does *not* save the value of this `ConfigurationItem` to the
+            configuration file.  To do that, use `ConfigurationItem.save` or
+            `save_config`.
+        
+        Parameter
+        ---------
+        value : 
+            The value this item should be set to. If 
+            
+        Raises
+        ------
+        TypeError
+            If the provided `value` is not valid for this `ConfigurationItem`.
         """
         raise NotImplementedError
     
-    def write(self,value=None):
-        """ Saves this `ConfigurationItem` to the relevant configuration file.
+    def save(self,value=None):
+        """ Writes a value for this `ConfigurationItem` to the relevant
+        configuration file.
+        
+        .. note::
+            This only saves the value of this *particular* `ConfigurationItem`.
+            To save all configuration settings for this package at once, see
+            `save_config`.
         
         Parameters
         ----------
         value : 
-            Save this `ConfigurationItem` with the provided value.  If None, 
-            the current value will be saved to the configuration file.  
+            Save this value to the configuration file. If None, the current
+            value of this `ConfigurationItem` will be saved.
         """
         raise NotImplementedError
     
-def get_config(self,package=None):
-    """
-    Gets the configuration item associated with a particular package.
+    def reload(self):
+        """ Reloads the value of this `ConfigurationItem` from the relevant 
+        configuration file. 
+        
+        Returns
+        -------
+        val :
+            The new value loaded from the configuration file.
+        """
+        raise NotImplementedError
+    
+    def __call__(self):
+        """ Returns the value of this `ConfigurationItem`
+        
+        Returns
+        val : 
+            This item's value, with a type determined by the `cfgtype` 
+            attribute.
+        """
+        return self._validate_val(self._retrieve_val())
+    
+    def _retrieve_val(self):
+        """ Gets and returns the value from the `configobj.ConfigObj` object
+        for this item
+        """
+        raise NotImplementedError
+        
+    def _validate_val(self,val):
+        """ Validates the provided value based on cfgtype and returns the 
+        type-cast value
+        """
+        #note that this will normally use the *class* attribute `_validator`,
+        #but if some arcane reason is needed for making a special one for an
+        #instance or sub-class, it will be used
+        return self._validator.check(self.cfgtype,val)
+        
+ 
+def get_config(packageormod=None,reload=False):
+    """ Gets the configuration object or section associated with a particular
+    package or module.
     
     Parameters
     -----------
-    package : str or None
+    packageormod : str or None
         The package for which to retrieve the configuration object. If a string,
         it must be a valid package name, or if None, the package from which this
         function is called will be used.
@@ -83,7 +187,9 @@ def get_config(self,package=None):
     Returns
     -------
     cfgobj : `configobj.ConfigObj` or `configobj.Section`
-        If the requested package is the base package for 
+        If the requested package is a base package, this will be the
+        `configobj.ConfigObj` for that package, or if it is a subpackage or
+        module, it will return the relevant `configobj.Section` object.
         
     Raises
     ------
@@ -92,16 +198,54 @@ def get_config(self,package=None):
         be determined.
     
     """
-    if package is None:
-        package = _find_current_module(2)
-        if package is None:
-            raise RuntimeError('get_config not called from inside a valid package')
+    if packageormod is None:
+        packageormod = _find_current_module(2)
+        if packageormod is None:
+            msg1 = 'Cannot automatically determine get_config module, '
+            msg2 = 'because it is not called from inside a valid module'
+            raise RuntimeError(msg1 + msg2)
     raise NotImplementedError
+
+def save_config(packageormod=None):
+    """ Saves all configuration settings to the configuration file for the
+    root package of the requested package/module.
+    
+    This overwrites any configuration items that have been changed in
+    `ConfigurationItem` objects that are based on the configuration file
+    determined by the *root* package of `packageormod` (e.g. 'astropy.cfg' for
+    the 'astropy.config.configs' module).
+    
+    .. note::
+        To save only a single item, use the `ConfigurationItem.save` method - 
+        this will save all options in the current session that may have been 
+        changed.
+    
+    Parameters
+    ----------
+    packageormod : str or None
+        The package or module name - see `get_config` for details. 
+    """
+    get_config(packageormod).write()
+    
+def reload_config(packageormod=None):
+    """ Reloads configuration settings from a configuration file for the root
+    package of the requested package/module.
+    
+    This overwrites any changes that may have been made in `ConfigurationItem` 
+    objects.  This applies for any items that are based on this file, which is
+    determined by the *root* package of `packageormod` (e.g. 'astropy.cfg' for 
+    the 'astropy.config.configs' module).
+    
+    Parameters
+    ----------
+    packageormod : str or None
+        The package or module name - see `get_config` for details. 
+    """
+    get_config(packageormod).reload()
     
 def _find_current_module(depth=1):
-    """
-    Determines the module this function is called from.  `depth` specifies how
-    far back to go in the call stack - e.g. 1 indicates the package of this 
+    """ Determines the module this function is called from.  `depth` specifies
+    how far back to go in the call stack - e.g. 1 indicates the package of this 
     function's caller, 2 retreives the caller's caller, etc.
     
     Returns the module object or None if the package cannot be found.  The name
@@ -119,7 +263,7 @@ def _find_current_module(depth=1):
     
 def _generate_all_config_items(package=None):
     """
-    Given a root package, this function looks for all `ConfigurationItems`
+    Given a starting package, this function looks for all `ConfigurationItems`
     in it and it's subpackage/modules, and saves them out to the relevant file
     with default values.  Note that this *will* overwrite any existing values.
     """
