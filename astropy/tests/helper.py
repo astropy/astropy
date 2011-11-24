b5785d8a75774f47c93d565a1771beebee21be6b
Matt Davis <jiffyclub.programatic@gmail.com>
2011-11-23 23:07:54 -0500

    Changed --remotedata option to --remote-data.

--- astropy/tests/helper.py
+++ astropy/tests/helper.py
@@ -45,14 +45,14 @@ remote_data = pytest.mark.remote_data
 # these pytest hooks allow us to mark tests and run the marked tests with
 # specific command line options.
 def pytest_addoption(parser):
-    parser.addoption("--remotedata", action="store_true",
+    parser.addoption("--remote-data", action="store_true",
         help="run tests with online data")
 
 
 def pytest_runtest_setup(item):
     if ('remote_data' in item.keywords and
-        not item.config.getvalue("remotedata")):
-        pytest.skip("need --remotedata option to run")
+        not item.config.getvalue("remote_data")):
+        pytest.skip("need --remote-data option to run")
 
 
 def run_tests(package=None, test_path=None, args=None, plugins=None,
