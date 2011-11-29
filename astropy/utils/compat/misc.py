9650803562da37eb0afe0b5e6f63d0765e8a9890
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-28 16:33:52 -0800

    updated utils with more docstrings and addition of misc module

---
+++ astropy/utils/compat/misc.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Simple utility functions and bug fixes for compatibility with all supported
+versions of Python.  This module should generally not be used directly, as 
+everything in `__all__` will be imported into `astropy.utils.compat` and can
+be accessed from there.
+"""
+
+from __future__ import absolute_import
+
+__all__ = []
