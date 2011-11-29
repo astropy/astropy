e4935a6422d6d52317c784a107d3ab8137c4cbe3
Erik Tollerud <erik.tollerud@gmail.com>
2011-11-28 17:34:50 -0800

    added tools package and associated structure

---
+++ astropy/tools/alg.py
@@ -0,0 +1,5 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This module contains simple algorithmic functionality common in astronomy or
+used in multiple places throughout Astropy.
+"""
