ea46546844c8a3bf46fedeed5c7147ab797f33dd
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-12-04 12:02:26 -0800

    added time and time tests (first commit after fixing branches)

---
+++ astropy/time/__init__.py
@@ -0,0 +1,3 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from .time import Time
+"""_Astrotime is is a module that manages basic astronomical time conversion and keeping (e.g. Julian dates, Modified Julian dates, ...)"""
