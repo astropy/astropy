d67d75c7a1eaab548baf77fa6441deadfbf2eadd
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-12-04 12:04:06 -0800

    first commit for nddata object

---
+++ astropy/nddata/__init__.py
@@ -0,0 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from .nddata import NDData
+"""_NDData is is a module that manages n-dimensional array based data (e.g. CCD images, IFU Data, optical spectra, ...)"""
---
+++ astropy/nddata/nddata.py
@@ -0,0 +1,30 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+class NDData(object):
+    """Class to store array type data.
+    `NDData` provides a superclass for all array based data.
+    
+    Initialize an NDData object with data
+    Parameters
+    ----------
+    data : `numpy.array`
+        n-dimensional array containing the n-dimensional data
+    error: `numpy.array`, optional
+        n-dimensional array containing the error of the data
+    mask: `numpy.array`, optional
+        n-dimensional array masking the data (suggested dtype=`bool`)
+    wcs: `astropy.wcs`, optional
+        WCS-object containing the world coordinate algorithms for the adta
+    meta: `dict`-like object, optional
+        contains meta data for the n-dimensional data
+    units: `astropy.units`, optional
+        describing the units of the data
+        
+    """
+    def __init__(self, data, error=None, mask=None, wcs=None, meta=None, units=None):
+        self.data = data
+        self.error = error
+        self.mask = mask
+        self.wcs = wcs
+        self.meta = meta
+        self.units = units
