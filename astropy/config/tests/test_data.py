c3b2ded3b73d13450e438bb8059154552c04f409
Erik Tollerud <erik.tollerud@gmail.com>
2011-12-06 23:01:02 -0800

    added tests that exercise the get_data_file* functions for local data

--- astropy/config/tests/test_data.py
+++ astropy/config/tests/test_data.py
@@ -1,4 +1,4 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst  
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 from ...tests.helper import remote_data
 
 TESTURL = 'http://www.google.com/index.html'
@@ -7,7 +7,7 @@ TESTURL = 'http://www.google.com/index.html'
 def test_url_cache():
     from ..data import get_data_filename,clear_data_cache
     from os.path import isfile
-    
+
     fnout = get_data_filename(TESTURL)
     assert isfile(fnout)
     clear_data_cache(TESTURL)
@@ -16,42 +16,56 @@ def test_url_cache():
 @remote_data
 def test_url_nocache():
     from ..data import get_data_fileobj
-    
+
     with get_data_fileobj(TESTURL,cache=False) as googlepage:
         assert googlepage.read().decode().find('oogle</title>')>-1
-    
-@remote_data    
+
+@remote_data
 def test_find_by_hash():
     from ..data import get_data_fileobj,get_data_filename,clear_data_cache
     from os.path import isfile
     import hashlib
-    
+
     with get_data_fileobj(TESTURL) as googlepage:
         hash = hashlib.md5(googlepage.read())
-        
+
     hashstr = 'hash/'+hash.hexdigest()
-    
+
     fnout = get_data_filename(hashstr)
     assert isfile(fnout)
     clear_data_cache(hashstr[5:])
     assert not isfile(fnout)
-    
+
+def test_local_data_obj():
+    from ..data import get_data_fileobj
+
+    with get_data_fileobj('data/local.dat') as f:
+        f.readline()
+        assert f.read()=='CONTENT\n'
+
+def test_local_data_name():
+    from ..data import get_data_filename
+    from os.path import isfile
+
+    fnout = get_data_filename('data/local.dat')
+    assert isfile(fnout)
+
 def test_compute_hash():
     import string
     import random
     import tempfile
     import hashlib
     from ..data import compute_hash
-    
+
     #generate a random string of 25 characters
     rands = ''.join(random.choice(string.ascii_letters) for x in range(25))
-    
+
     with tempfile.NamedTemporaryFile('w+') as ntf:
         ntf.write(rands)
         ntf.flush()
-        
+
         chhash = compute_hash(ntf.name)
         # the encode() call is necessary for py3.x compatibility
         shash = hashlib.md5(rands.encode()).hexdigest()
-        
+
         assert chhash==shash
