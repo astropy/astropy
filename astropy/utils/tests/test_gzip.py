45f8b0d62fd5ebec92e3a4d42a22ca6150a499d5
Michael Droettboom <mdboom@gmail.com>
2011-12-07 19:53:08 -0500

    Handle gzip module compatibility in a more sane way.  The versions from both 2.7 and 3.2 are included, and used from 2.6 and 3.1 respectively.

--- astropy/utils/tests/test_gzip.py
+++ astropy/utils/tests/test_gzip.py
@@ -10,3 +10,9 @@ pytestmark = pytest.mark.skipif("sys.version_info < (3,0)")
 def test_gzip(tmpdir):
     fd = gzip.GzipFile(str(tmpdir.join("test.gz")), 'wb')
     fd = io.TextIOWrapper(fd, encoding='utf8')
+
+
+def test_gzip2(tmpdir):
+    with gzip.GzipFile(str(tmpdir.join("test.gz")), 'wb') as fd:
+        pass
+
