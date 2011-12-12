c5e628f1a9469182b26a150bbd70430c2b17ed92
Erik Tollerud <erik.tollerud@gmail.com>
2011-12-12 13:19:46 -0800

    expanded docstrings

--- astropy/nddata/nddata.py
+++ astropy/nddata/nddata.py
@@ -5,24 +5,52 @@ __all__ = ['NDData']
 
 
 class NDData(object):
-    """Class to store array type data.
+    """Superclass for Astropy data.
 
-    `NDData` provides a superclass for all array based data.
+    `NDData` provides a superclass for all array-based data. The key
+    distinction from raw numpy arrays is the presence of additional metadata
+    like error arrays, bad pixel masks, or coordinates.
 
     Parameters
     ----------
     data : `~numpy.ndarray`
-        n-dimensional array containing the n-dimensional data
+        The actual data contained in this `NDData` object.
     error : `~numpy.ndarray`, optional
-        n-dimensional array containing the error of the data
+        Error of the data. This should be interpreted as a standard
+        deviation-type error (e.g. square root of the variance).
+
+         .. warning::
+             The physical interpretation of the `error` array may change in the
+             future, as it has not been intensively discussed. For now assume
+             the above description holds, using an `error` property if
+             necessary, but feel free to use the most convinient internal
+             representation in subclasses
+
     mask : `~numpy.ndarray`, optional
-        n-dimensional array masking the data (suggested dtype=`bool`)
-    wcs : `astropy.wcs`, optional
-        WCS-object containing the world coordinate algorithms for the adta
+        Masking of the data; True where the array is *valid*, False where it is
+        *invalid*.
+    wcs : undefined, optional
+        WCS-object containing the world coordinate system for the data.
+
+        .. warning::
+            This is not yet defind because the discussion of how best to
+            represent this class's WCS system generically is still under
+            consideration. For now just leave it as None
+
     meta : `dict`-like object, optional
-        contains meta data for the n-dimensional data
-    units : `astropy.units`, optional
-        describing the units of the data
+        Metadata for this object.  "Metadata" here means all information that
+        is included with this object but not part of the python representation
+        of this particular object.  e.g., exposure times, instrument or
+        telescope status information, observatory location information, etc.
+        Basically the same kind of stuff you would typcially find in a FITS
+        header.
+    units : undefined, optional
+        Description of the units of the data.
+
+        .. warning::
+            The units scheme is under development. For now, just supply a
+            string when relevant - the units system will likely be compatible
+            with providing strings to initialize itself.
 
     """
     def __init__(self, data, error=None, mask=None, wcs=None, meta=None,
