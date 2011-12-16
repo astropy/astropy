d42651e2dbaafe6cc99c417bab070f70fcb2db36
Erik Tollerud <erik.tollerud@gmail.com>
2011-12-15 18:59:14 -0800

    tiny docstring typo fix in nddata/__init__

--- astropy/nddata/__init__.py
+++ astropy/nddata/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-The `nddata`-package manages n-dimensional array based data (e.g. CCD
+The `nddata` package manages n-dimensional array based data (e.g. CCD
 images, IFU Data, optical spectra, ...)
 """
 
