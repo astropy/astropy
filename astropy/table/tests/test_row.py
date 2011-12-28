5841590444d202e6fb1fe8d7d937807ff9805677
Tom Aldcroft <taldcroft@gmail.com>
2011-12-28 09:48:16 -0500

    Add a (skipped) test for row slice assignment.
    E. Bray requested the ability to assign to a table via a row with
    slice assignment, e.g.
    
    row = table[2]
    row[2:5] = [2, 3, 4]
    row[:] = 3
    
    This does not currently work because np.void (which is what numpy
    returns for structured array row access) does not support slice
    assignment.  Test is left there as a placeholder for now.

--- astropy/table/tests/test_row.py
+++ astropy/table/tests/test_row.py
@@ -8,16 +8,16 @@ class TestRow():
     def setup_method(self, method):
         self.a = Column('a', [1, 2, 3])
         self.b = Column('b', [4, 5, 6])
+        self.t = Table([self.a, self.b])
 
     def test_subclass(self):
         """Row is subclass of ndarray and Row"""
-        table = Table([self.a, self.b])
-        c = Row(table, 2)
+        c = Row(self.t, 2)
         assert isinstance(c, Row)
 
     def test_values(self):
         """Row accurately reflects table values and attributes"""
-        table = Table([self.a, self.b], meta={'x': 1})
+        table = self.t
         row = table[1]
         assert row['a'] == 2
         assert row['b'] == 5
@@ -32,7 +32,21 @@ class TestRow():
 
     def test_ref(self):
         """Row is a reference into original table data"""
-        table = Table([self.a, self.b])
+        table = self.t
         row = table[1]
         row['a'] = 10
         assert table['a'][1] == 10
+
+    def SKIP_test_set_slice(self):
+        """Set row elements with a slice
+
+        This currently fails because the underlying np.void object
+        row.data = table._data[index] does not support slice assignment.
+        """
+        table = self.t
+        row = table[0]
+        row[:] = [-1, -1]
+        row[:1] = np.array([-2])
+        assert np.all(table._data == np.array([[-1, -1],
+                                               [-2, 5],
+                                               [3, 6]]))
