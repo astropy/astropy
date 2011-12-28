c0534276646487a47aefc98c708caf08a29880c7
Erik Tollerud <erik.tollerud@gmail.com>
2011-12-28 11:56:40 -0600

    updated test_misc for number argument <0 case

--- astropy/utils/tests/test_misc.py
+++ astropy/utils/tests/test_misc.py
@@ -8,6 +8,19 @@ def test_pkg_finder():
     assert misc.find_current_module(1).__name__ == 'astropy.utils.tests.test_misc'
     assert misc.find_current_module(0,True).__name__ == 'astropy.utils.tests.test_misc'
 
+def test_fnpickling_simple(tmpdir):
+    fn = str(tmpdir.join('test1.pickle'))
+
+    obj1 = 'astring'
+    misc.fnpickle(obj1, fn)
+    res = misc.fnunpickle(fn)
+    assert obj1 == res
+
+    #try without cPickle and the oldest protocol
+    misc.fnpickle(obj1, fn, usecPickle=False, protocol=0)
+    res = misc.fnunpickle(fn, usecPickle=False)
+    assert obj1 == res
+
 class ToBePickled(object):
     def __init__(self,item):
         self.item = item
@@ -18,33 +31,34 @@ class ToBePickled(object):
         else:
             return False
 
-
-def test_fnpickling(tmpdir):
-    fn1 = str(tmpdir.join('test1.pickle'))
-    fn2 = str(tmpdir.join('test2.pickle'))
-    fn3 = str(tmpdir.join('test3.pickle'))
-
+def test_fnpickling_class(tmpdir):
+    fn = str(tmpdir.join('test2.pickle'))
+    
     obj1 = 'astring'
-    misc.fnpickle(obj1, fn1)
-    res = misc.fnunpickle(fn1)
-    assert obj1 == res
-
-    #try without cPickle and the oldest protocol
-    misc.fnpickle(obj1, fn1, usecPickle=False, protocol=0)
-    res = misc.fnunpickle(fn1, usecPickle=False)
-    assert obj1 == res
-
-    #more complex object
     obj2 = ToBePickled(obj1)
-    misc.fnpickle(obj2, fn2)
-    res = misc.fnunpickle(fn2)
+    misc.fnpickle(obj2, fn)
+    res = misc.fnunpickle(fn)
     assert res == obj2
 
+def test_fnpickling_many(tmpdir):
+    from pytest import raises
+    
+    fn = str(tmpdir.join('test3.pickle'))
+    
     #now try multiples
     obj3 = 328.3432
     obj4 = 'blahblahfoo'
-    misc.fnpickle(obj3, fn3)
-    misc.fnpickle(obj4, fn3, append=True)
-    res = misc.fnunpickle(fn3, number=2)
+    misc.fnpickle(obj3, fn)
+    misc.fnpickle(obj4, fn, append=True)
+    
+    res = misc.fnunpickle(fn, number=-1)
+    assert len(res) == 2
     assert res[0] == obj3
     assert res[1] == obj4
+    
+    misc.fnpickle(obj4, fn, append=True)
+    res = misc.fnunpickle(fn, number=2)
+    assert len(res) == 2
+    
+    with raises(EOFError):
+        misc.fnunpickle(fn, number=5)
