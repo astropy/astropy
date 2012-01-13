c31c17bc9d2a5759b5848f47882177742619b053
Matt Davis <jiffyclub.programatic@gmail.com>
2012-01-13 17:06:15 -0500

    Move the py.test conftest.py file to the astropy directory so it is installed. Changed the import of plugins accordingly. Also updated the .gitignore file so it ignores the Mac OS X .DS_Store file.

--- astropy/conftest.py
+++ astropy/conftest.py
@@ -2,4 +2,4 @@
 # by importing them here in conftest.py they are discoverable by py.test
 # no matter how it is invoked within the astropy tree.
 
-from astropy.tests.pytest_plugins import *
+from .tests.pytest_plugins import *
