dcf2335e8f68965b0a6ef56bb5213e30cd5f0aa7
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-02-08 23:11:18 +0100

    Added convolution tests with NaN value

--- astropy/nddata/convolve/tests/test_convolve.py
+++ astropy/nddata/convolve/tests/test_convolve.py
@@ -52,7 +52,8 @@ class TestConvolve2D(object):
     @pytest.mark.parametrize(('boundary'), BOUNDARY_OPTIONS)
     def test_unity_3x3(self, boundary):
         '''
-        Test that a 3x3 unit kernel returns the same array
+        Test that a 3x3 unit kernel returns the same array (except when
+        boundary is None).
         '''
 
         x = np.array([[1., 2., 3.],
@@ -75,7 +76,8 @@ class TestConvolve2D(object):
     @pytest.mark.parametrize(('boundary'), BOUNDARY_OPTIONS)
     def test_uniform_3x3(self, boundary):
         '''
-        Test that a 3x3 unit kernel returns the same array
+        Test that the different modes are producing the correct results using
+        a 3x3 uniform kernel.
         '''
 
         x = np.array([[0., 0., 27.],
@@ -104,3 +106,67 @@ class TestConvolve2D(object):
             assert np.all(z == np.array([[2., 7., 12.],
                                          [4., 6., 8.],
                                          [6., 5., 4.]], dtype='>f8'))
+
+    @pytest.mark.parametrize(('boundary'), BOUNDARY_OPTIONS)
+    def test_unity_3x3_withnan(self, boundary):
+        '''
+        Test that a 3x3 unit kernel returns the same array (except when
+        boundary is None). This version includes a NaN value in the original
+        array.
+        '''
+
+        x = np.array([[1., 2., 3.],
+                      [4., np.nan, 6.],
+                      [7., 8., 9.]], dtype='>f8')
+
+        y = np.array([[0., 0., 0.],
+                      [0., 1., 0.],
+                      [0., 0., 0.]], dtype='>f8')
+
+        z = convolve(x, y, boundary=boundary)
+
+        assert np.isnan(z[1, 1])
+        x = np.nan_to_num(z)
+        z = np.nan_to_num(z)
+
+        if boundary is None:
+            assert np.all(z == np.array([[0., 0., 0.],
+                                         [0., 0., 0.],
+                                         [0., 0., 0.]], dtype='>f8'))
+        else:
+            assert np.all(z == x)
+
+    @pytest.mark.parametrize(('boundary'), BOUNDARY_OPTIONS)
+    def test_uniform_3x3(self, boundary):
+        '''
+        Test that the different modes are producing the correct results using
+        a 3x3 uniform kernel. This version includes a NaN value in the
+        original array.
+        '''
+
+        x = np.array([[0., 0., 27.],
+                      [9., np.nan, 0.],
+                      [0., 18., 0.]], dtype='>f8')
+
+        y = np.array([[1., 1., 1.],
+                      [1., 1., 1.],
+                      [1., 1., 1.]], dtype='>f8')
+
+        z = convolve(x, y, boundary=boundary)
+
+        if boundary is None:
+            assert np.all(z == np.array([[0.00, 0.00, 0.00],
+                                         [0.00, 0.75, 0.00],
+                                         [0.00, 0.00, 0.00]], dtype='>f8'))
+        elif boundary == 'fill':
+            assert np.all(z == np.array([[1.75, 4.75, 3.75],
+                                         [3.75, 6.75, 5.75],
+                                         [3.75, 3.75, 2.75]], dtype='>f8'))
+        elif boundary == 'wrap':
+            assert np.all(z == np.array([[6.75, 6.75, 6.75],
+                                         [6.75, 6.75, 6.75],
+                                         [6.75, 6.75, 6.75]], dtype='>f8'))
+        else:
+            assert np.all(z == np.array([[2.75, 7.75, 12.75],
+                                         [4.75, 6.75, 8.75],
+                                         [6.75, 5.75, 4.75]], dtype='>f8'))
