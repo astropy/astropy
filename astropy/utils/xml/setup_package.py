9d0736cd2b97148512e2e99ff169dc911b112c04
Erik Bray <embray@stsci.edu>
2012-02-10 15:41:15 -0500

    Treat this as a Linux-only issue.  OSX doesn't seem to have a -Bsymbolic equivalent, and it may not be an usse there.  This does work in mingw32, but certainly on in MSVC, though it's not an issue on Windows either as far as I can tell.

--- astropy/utils/xml/setup_package.py
+++ astropy/utils/xml/setup_package.py
@@ -7,6 +7,14 @@ def get_extensions(build_type='release'):
     EXPAT_DIR = 'cextern/expat/lib'
     XML_DIR = 'astropy/utils/xml/src'
 
+    if sys.platform.startswith('linux'):
+        # This is to ensure that _iterparser uses the libexpat functions it was
+        # compiled with, and not the system libexpat (which can happen if the
+        # latter is loaded first).
+        extra_link_args = ['-Wl,-Bsymbolic']
+    else:
+        extra_link_args = []
+
     defines = [("HAVE_EXPAT_CONFIG_H", 1)]
     if sys.byteorder == 'big':
         defines.append(('BYTEORDER', '4321'))
@@ -24,4 +32,4 @@ def get_extensions(build_type='release'):
          join(EXPAT_DIR, "xmltok_impl.c")],
         define_macros=defines,
         include_dirs=[XML_DIR, EXPAT_DIR],
-        extra_link_args=['-Wl,-Bsymbolic'])]
+        extra_link_args=extra_link_args)]
