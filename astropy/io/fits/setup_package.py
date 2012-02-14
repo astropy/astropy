8945f0d9e2d8ca50f768d67d23dce1a8743f9aea
Erik Bray <embray@stsci.edu>
2012-02-14 15:24:37 -0500

    One more Windows fix--fixes io.fits so that it can be compiled by MSVC.  Need to fix this in PyFITS too, I guess.

--- astropy/io/fits/setup_package.py
+++ astropy/io/fits/setup_package.py
@@ -9,14 +9,18 @@ from ... import setup_helpers
 
 
 def get_extensions():
+    if setup_helpers.get_compiler_option() != 'msvc':
+        extra_compile_args = ['-Wno-unused-function',
+                              '-Wno-strict-prototypes']
+    else:
+        extra_compile_args = []
     return [
         Extension(
             'astropy.io.fits.compression',
             [os.path.relpath(x) for x in
              glob(os.path.join(os.path.dirname(__file__), 'src/*.c'))],
             include_dirs=[setup_helpers.get_numpy_include_path()],
-            extra_compile_args=['-Wno-unused-function',
-                                '-Wno-strict-prototypes'])
+            extra_compile_args=extra_compile_args)
     ]
 
 
