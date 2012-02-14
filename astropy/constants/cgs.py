ae4a04205a9a2698da25b3788e190b2be1fef58a
Neil Crighton <neilcrighton@gmail.com>
2012-02-14 12:14:07 +1100

    final changes

--- astropy/constants/cgs.py
+++ astropy/constants/cgs.py
@@ -1,7 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Astronomical and physics constants in cgs units.
+Astronomical and physics constants in cgs units.  The constants
+available (with approximate values) are:
 """
+# This docstring is extended by __init__.py
 
 from .constant import Constant
 from . import si
--- astropy/constants/si.py
+++ astropy/constants/si.py
@@ -1,7 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-Astronomical and physics constants in SI units.
+Astronomical and physics constants in SI units. The constants
+available (with approximate values) are:
 """
+# This docstring is extended by __init__.py
 
 import numpy as np
 from .constant import Constant
