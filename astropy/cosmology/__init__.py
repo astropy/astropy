bc5f43e005fba9bf05a7665c42d553b24fd9a49b
Roban Hultman Kramer <robanhk@gmail.com>
2012-02-24 12:57:21 +0100

    Created cosmology directory and dummy __init__.py file. 	new file:   cosmology/__init__.py

---
+++ astropy/cosmology/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+astropy.cosmology contains routines for cosmological distance measures
+and other quantities.
+
+Capabilities will include
+-------------------------
+
+  Various cosmological densities.
+
+  Various cosmological distance measures.
+
+  Pre-defined sets of cosmological parameters (e.g. from WMAP).
+
+"""
