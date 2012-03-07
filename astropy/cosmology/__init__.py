75691d2eea39072e21fc1822191e035017fa1ecc
Neil Crighton <neilcrighton@gmail.com>
2012-03-07 20:55:44 +0100

    fixing typos and written style

--- astropy/cosmology/__init__.py
+++ astropy/cosmology/__init__.py
@@ -3,28 +3,19 @@
 astropy.cosmology contains routines for cosmological distance measures
 and other quantities.
 
-Capabilities will include
--------------------------
-
-  Various cosmological densities.
-
-  Various cosmological distance measures.
-
-  Pre-defined sets of cosmological parameters (e.g. from WMAP).
-
-Most of the functionality is enabled by the Cosmology object.
-
-For example, create a new Cosmology object by given the hubble
-parameter, omega matter and omega lambda (all at z=0)::
+Most of the functionality in this package is enabled by the Cosmology
+object. For example, create a new Cosmology object with ketwords
+giving the hubble parameter, omega matter and omega lambda (all at
+z=0)::
 
   >>> from astropy.cosmology import Cosmology
   >>> cosmo = Cosmology(H0=70, Om=0.3, Ol=0.7)
   >>> cosmo
   Cosmology(H0=70, Om=0.3, Ol=0.7, Ok=0)
 
-Methods of the Cosmology object allow you to calculate commonly-used
-quantities using your cosmology. So the comoving distance in Mpc at
-redshift 4 is given by::
+The methods of this object calculate commonly used quantities with
+your cosmology. So the comoving distance in Mpc at redshift 4 is given
+by::
 
   >>> cosmo.comoving_distance(4)
   7170.366414463296
@@ -35,8 +26,9 @@ Age of the universe at z = 0 in Gyr::
   13.46698402784007
 
 See the Cosmology object docstring for all the methods and variables
-available.  When you create a new cosmology, instead of specifying H0,
-Om and Ol you can use standard sets of cosmological parameters::
+available.  When creating new cosmology, instead of specifying H0, Om
+and Ol you can also specify a standard set of cosmological
+parameters::
 
   # Parameters from the 7-year WMAP results
   >>> wmap7 = Cosmology('wmap7')
@@ -50,9 +42,9 @@ Om and Ol you can use standard sets of cosmological parameters::
   >>> wmap5.H(3)
   301.54148311633674
 
-There are also several 'convenience' functions that calculate
-commonly-used quantities without have to create a Cosmology
-object. They use WMAP7 cosmology by default.
+There are also several convenience functions that calculate commonly
+used quantities without needing to create a Cosmology object. They use
+a WMAP7 cosmology by default.
 
   >>> from astropy.cosmology import \
         kpc_proper_per_arcmin, arcsec_per_kpc_comoving
@@ -62,7 +54,7 @@ object. They use WMAP7 cosmology by default.
   0.12687166682195736
 
 By default these use a 7-year WMAP cosmology. The convenience
-functions availableq are::
+functions available are::
 
      kpc_comoving_per_arcmin
      kpc_proper_per_arcmin
@@ -72,9 +64,10 @@ functions availableq are::
      to_xyz
 
 See their docstrings for more information.
+
+References for most of the quantities calculated in this package are
+given by Hogg (astro-ph/9905116).
 """
 from cosmology import \
      Cosmology, kpc_comoving_per_arcmin, kpc_proper_per_arcmin, \
      arcsec_per_kpc_comoving, arcsec_per_kpc_proper, distmod, to_xyz
-
-from parameters import WMAP7, WMAP5
