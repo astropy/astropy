ef742718ee8625ba3924f1d0e01382e13d30bff6
Erik Tollerud <erik.tollerud@gmail.com>
2012-03-23 15:05:21 -0700

    typo fix in nddata docs

--- astropy/nddata/__init__.py
+++ astropy/nddata/__init__.py
@@ -1,11 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-The `nddata` subpackage provides the `~astropy.nddata.NDData` class and
-related tools to manage n-dimensional array-based data (e.g. CCD images,
-IFU Data, grid-based simulation data, ...). This is more than just
-`numpy.ndarray` objects, because it provides metadata that cannot be
-easily provided by a single array.
+The `nddata` subpackage provides the `~astropy.nddata.nddata.NDData`
+class and related tools to manage n-dimensional array-based data (e.g.
+CCD images, IFU Data, grid-based simulation data, ...). This is more than
+just `numpy.ndarray` objects, because it provides metadata that cannot
+be easily provided by a single array.
 """
 
 from astropy import setup_helpers
