462fb5edd2d7b7f6c477b5cd2441a83abb256d6f
Neil Crighton <neilcrighton@gmail.com>
2012-04-14 21:52:59 +0200

    add mechanism for getting and setting a default cosmology, add pre-defined cosmology instances for parameter sets in the parameters module, add tests

--- astropy/cosmology/__init__.py
+++ astropy/cosmology/__init__.py
@@ -1,12 +1,12 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 astropy.cosmology contains routines for cosmological distance measures
-and other quantities.
+and other quantities. References for most of the quantities calculated
+in this package are given by Hogg (astro-ph/9905116).
 
-Most of the functionality in this package is enabled by the Cosmology
-object. For example, create a new Cosmology object with ketwords
-giving the hubble parameter, omega matter and omega lambda (all at
-z=0)::
+Most of the functionality is enabled by the Cosmology object. For
+example, to create a new `Cosmology` object with arguments giving the
+hubble parameter, omega matter and omega lambda (all at z=0)::
 
   >>> from astropy.cosmology import Cosmology
   >>> cosmo = Cosmology(H0=70, Om=0.3, Ol=0.7)
@@ -20,31 +20,36 @@ by::
   >>> cosmo.comoving_distance(4)
   7170.366414463296
 
-Age of the universe at z = 0 in Gyr::
+The age of the universe at z = 0 in Gyr::
 
   >>> cosmo.age(0)
   13.46698402784007
 
-See the Cosmology object docstring for all the methods and variables
-available.  When creating new cosmology, instead of specifying H0, Om
-and Ol you can also specify a standard set of cosmological
-parameters::
+See the `Cosmology` object docstring for all the methods and variables
+available.  There are several standard cosmologies already defined::
 
   # Parameters from the 7-year WMAP results
-  >>> wmap7 = Cosmology('wmap7')
+  >>> from cosmology import WMAP7
   # critical density at z=0 in g/cm^3
-  >>> wmap7.critical_density0
+  >>> WMAP7.critical_density0
   9.31000313202047e-30
 
   # from the 5-year results
-  >>> wmap5 = Cosmology('wmap5')
+  >>> from cosmology import WMAP5
   # the Hubble parameter at z = 3 in km/s/Mpc
-  >>> wmap5.H(3)
+  >>> WMAP5.H(3)
   301.54148311633674
 
-There are also several convenience functions that calculate commonly
-used quantities without needing to create a Cosmology object. They use
-a WMAP7 cosmology by default.
+There is also a 'default' cosmology that will be used by relevant
+astropy functions if no cosmology instance is explicitly passed to
+them. This can be set with `cosmology.set_default()` or a
+configuration file option, and is accessed with
+`cosmology.get_default()`. If you don't set the default explicitly,
+then the first time it is accessed a warning message is printed and
+it's set to the 7 year WMAP cosmology.
+
+There are also several convenience functions that calculate quantities
+without needing to create a Cosmology object.
 
   >>> from astropy.cosmology import \
         kpc_proper_per_arcmin, arcsec_per_kpc_comoving
@@ -53,21 +58,20 @@ a WMAP7 cosmology by default.
   >>> arcsec_per_kpc_proper(3)
   0.12687166682195736
 
-By default these use a 7-year WMAP cosmology. The convenience
-functions available are::
+These use the default cosmology, unless overridden by a `cosmo=`
+keyword argument. The convenience functions available are::
 
      kpc_comoving_per_arcmin
      kpc_proper_per_arcmin
      arcsec_per_kpc_comoving
      arcsec_per_kpc_proper
      distmod
-     to_xyz
+     radec_to_xyz
 
 See their docstrings for more information.
-
-References for most of the quantities calculated in this package are
-given by Hogg (astro-ph/9905116).
 """
 from cosmology import \
      Cosmology, kpc_comoving_per_arcmin, kpc_proper_per_arcmin, \
-     arcsec_per_kpc_comoving, arcsec_per_kpc_proper, distmod, to_xyz
+     arcsec_per_kpc_comoving, arcsec_per_kpc_proper, distmod, radec_to_xyz, \
+     get_default, set_default, WMAP5, WMAP7
+
--- astropy/cosmology/parameters.py
+++ astropy/cosmology/parameters.py
@@ -1,12 +1,8 @@
-""" Cosmological parameters from the 5-year and 7-year WMAP results.
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+""" This module contains dictionaries with sets of parameters for a
+given cosmology.
 
-WMAP 7 year parameters are from Komatsu et al. 2011, ApJS, 192,
-18. Table 1 (WMAP + BAO + H0 ML).
-
-WMAP 5 year parameters are from Komatsu et al. 2009, ApJS, 180,
-330. Table 1 (WMAP + BAO + SN ML).
-
-Each set of parameters is in a dictionary with the following keys:
+Each cosmology has the following parameters defined:
 
     Oc          Omega cold dark matter
     Ob          Omega baryon
@@ -19,37 +15,46 @@ Each set of parameters is in a dictionary with the following keys:
     z_reion     Redshift of hydrogen reionisation
     t0          Age of the universe in Gyr
 
-Both of these cosmologies are flat (omega matter + omega lambda = 1).
+The list of cosmologies available are given by the tuple
+`available`. Current cosmologies available:
+
+WMAP 7 year parameters from Komatsu et al. 2011, ApJS, 192, 18. Table
+1 (WMAP + BAO + H0 ML).
+
+WMAP 5 year parameters from Komatsu et al. 2009, ApJS, 180, 330. Table
+1 (WMAP + BAO + SN ML).
+
+Both these cosmologies are flat (omega matter + omega lambda = 1).
 """
 
 # Komatsu et al. 2011, WMAP + BAO + H0 ML (table 1).
-#
-# This is a flat cosmology (Om + Ol = 1)
 
 WMAP7 = dict(
-    Oc =      0.226,  # Omega cold dark matter
-    Ob =      0.0455, # Omega baryon
-    Om =      0.272,  # Omega matter
-    Ol =      0.728,  # Omega lambda
-    H0 =      70.4,   # Hubble parameter at z=0
-    n =       0.967,  # spectral index
-    sigma8 =  0.810,  # sigma8
-    tau =     0.085,  # 
-    z_reion = 10.3,   # Redshift of hydrogen reionisation
-    t0 =      13.76,  # Age of the universe
+    Oc = 0.226,
+    Ob = 0.0455,
+    Om = 0.272,
+    Ol = 0.728,
+    H0 = 70.4,
+    n = 0.967,
+    sigma8 = 0.810,
+    tau = 0.085,
+    z_reion = 10.3,
+    t0 = 13.76,
     )
 
-# Komatsu et al. 2009 WMAP + BAO + SN ML (table 1). Also flat.
+# Komatsu et al. 2009 WMAP + BAO + SN ML (table 1).
 
 WMAP5 = dict(
-    Oc =      0.231,  # Omega cold dark matter
-    Ob =      0.0459, # Omega baryon
-    Om =      0.277,  # Omega matter
-    Ol =      0.723,  # Omega lambda
-    H0 =      70.2,   # Hubble parameter at z=0
-    n =       0.962,  # spectral index
-    sigma8 =  0.817,  # sigma8
-    tau =     0.088,  # 
-    z_reion = 11.3,   # Redshift of hydrogen reionisation
-    t0 =      13.72,  # Age of the universe
+    Oc = 0.231,
+    Ob = 0.0459,
+    Om = 0.277,
+    Ol = 0.723,
+    H0 = 70.2,
+    n = 0.962,
+    sigma8 = 0.817,
+    tau = 0.088,
+    z_reion = 11.3,
+    t0 = 13.72,
     )
+
+available = tuple(k for k in locals() if not k.startswith('_'))
