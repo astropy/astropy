50c2d9e6d1d086f583799267399b100b587cb6fa
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-04-28 18:12:37 -0400

    added tests for astrotime

--- astropy/time/tests/test_astrotime.py
+++ astropy/time/tests/test_astrotime.py
@@ -2,12 +2,13 @@
 
 from astropy.time import Time
 import datetime
-
-def test_jd2gregorian():
+import decimal
+def test_jd2utc():
     test_date = Time.from_jd(2449443.5)
-    assert test_date.to_calendar_date() == datetime.datetime(1994, 4, 1, 0, 0, 0)
+    assert test_date.to_utc() == datetime.datetime(1994, 4, 1, 0, 0, 0)
 
-def test_gregorian2jd():
-    test_date = Time.from_calendar_date(datetime.datetime(1994, 4, 1, 0, 0, 0))
-    #Should be assert float
-    assert test_date.to_jd() == 2449443.5
+def test_utc2jd():
+    test_date = Time.from_utc(datetime.datetime(1994, 4, 1, 0, 0, 0))
+    #less than 1 ms
+    
+    assert float(test_date.to_jd() - decimal.Decimal(2449443.5)) < 1e-9
