11f48694754b525956d170e685c08fc7d5f6dc3c
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-06 10:11:20 -0400

    changed inheritance to object and all that entails

--- astropy/nddata/masks.py
+++ astropy/nddata/masks.py
@@ -4,25 +4,25 @@ import numpy as np
 
 import pyfits
 
-class NDMask(np.ndarray):
+class NDMask(object):
     #Mask superclass
     pass
 
 
 class BoolMask(NDMask):
-
-    def __new__(cls, input_array):
-        obj = np.asarray(input_array, dtype='bool').view(cls)
-        return obj
-    def _check_operand(self, operand):
+    
+    
+    def __init__(self, mask):
+        self.mask = mask
         
+    def _check_operand(self, operand):
         if not isinstance(operand, BoolMask):
             raise ValueError('unsupported operand type(s) for +: %s and %s' %\
                              (type(self), type(operand)))
             
     def _arithmetic_operation(self, operand):
         self._check_operand(operand)
-        return np.logical_and(self, operand)
+        return np.logical_and(self.mask, operand.mask)
         
     def mask_add(self, operand):
         return self._arithmetic_operation(operand)
