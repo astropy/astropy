4ce4e81bfc15dd034a86980427db08acc63140ad
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-05-06 00:04:27 -0400

    changed the inheritance to be from object rather than from ndarray. In addition i added masks to the arithmetic function

--- astropy/nddata/errors.py
+++ astropy/nddata/errors.py
@@ -1,14 +1,16 @@
 #module for errors in ndata
-class NDError(np.ndarray):
+class NDError(object):
     #super class for errors
     pass
     
     
 class SDError(NDError):
     
-    def __new__(cls, input_array):
-        obj = np.asarray(input_array).view(cls)
-        return obj
+    def __init__(self, error, covariant=None):
+        self.error = error
+        #covariant for later implementation
+        self.covariant = covariant
+        
     
     def interpolate(self, old_lookup_table, new_lookup_table):
         return np.interp(new_lookup_table, old_lookup_table)
@@ -18,28 +20,28 @@ class SDError(NDError):
             raise ValueError('unsupported operand type(s) for +: %s and %s' %\
                              (type(self), type(operand)))
     
-    def error_add(self, self_data, operand, operand_data, result_data):
+    def error_add(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):#should we also do result_mask??
         self._check_operand(operand)
         if operand is None:
             return self
         
         return np.sqrt(self**2 + operand**2)
         
-    def error_sub(self, self_data, operand, operand_data, result_data):
+    def error_sub(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
         self._check_operand(operand)
         if operand is None:
             return self
         
         return np.sqrt(self**2 + operand**2)
 
-    def error_mul(self, self_data, operand, operand_data, result_data):
+    def error_mul(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
         self._check_operand(operand)
         if operand is None:
             return self
         
         return np.sqrt((self / self_data)**2 + (operand / operand_data)**2) * result_data
         
-    def error_div(self, self_data, operand, operand_data, result_data):
+    def error_div(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
         self._check_operand(operand)
         if operand is None:
             return self
