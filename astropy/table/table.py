b4a24691cae9041164b80949d315926d0e2d7945
Tom Aldcroft <taldcroft@gmail.com>
2012-05-10 14:32:38 -0400

    Add column width limit to Table pretty printing

--- astropy/table/table.py
+++ astropy/table/table.py
@@ -1,7 +1,6 @@
 import sys
 from copy import deepcopy
 import collections
-from itertools import izip
 
 import numpy as np
 
@@ -17,20 +16,58 @@ except NameError:
 AUTO_COLNAME = 'col{0}'
 _format_funcs = {None: lambda format_, val: str(val)}
 MAX_LINES = 25
+MAX_WIDTH = 80
 
 
-def _get_screen_lines():
-    try:  # Will likely fail on Windows
-        import termios
-        import fcntl
-        import struct
-        s = struct.pack("HHHH", 0, 0, 0, 0)
-        fd_stdout = sys.stdout.fileno()
-        x = fcntl.ioctl(fd_stdout, termios.TIOCGWINSZ, s)
-        (rows, cols, xpixels, ypixels) = struct.unpack("HHHH", x)
-        return max(rows - 6, 10)
-    except:
-        return MAX_LINES
+def _get_pprint_size(max_lines=None, max_width=None):
+    """Get the output size (number of lines and character width) for Column and
+    Table pformat/pprint methods.
+
+    If no value of ``max_lines`` is supplied then the height of the screen
+    terminal is used to set ``max_lines``.  If the terminal height cannot be
+    determined then a default of ``astropy.table.MAX_LINES`` is used.  If a
+    negative value of ``max_lines`` is supplied then there is no line limit
+    applied.
+
+    The Same applies for max_width except the default is
+    ``astropy.table.MAX_WIDTH``.
+
+    Parameters
+    ----------
+    max_lines : int or None
+        Maximum lines of output (header + data rows)
+
+    max_width : int or None
+        Maximum width (characters) output
+
+    Returns
+    -------
+    max_lines, max_width : int
+
+    """
+    if max_lines is None or max_width is None:
+        try:  # Will likely fail on Windows
+            import termios
+            import fcntl
+            import struct
+            s = struct.pack("HHHH", 0, 0, 0, 0)
+            fd_stdout = sys.stdout.fileno()
+            x = fcntl.ioctl(fd_stdout, termios.TIOCGWINSZ, s)
+            (lines, width, xpixels, ypixels) = struct.unpack("HHHH", x)
+        except:
+            lines, width = MAX_LINES, MAX_WIDTH
+
+    if max_lines is None:
+        max_lines = max(lines - 6, 10)
+    elif max_lines < 0:
+        max_lines = sys.maxint
+
+    if max_width is None:
+        max_width = max(width - 1, 10)
+    elif max_width < 0:
+        max_width = sys.maxint
+
+    return max_lines, max_width
 
 
 def _auto_names(n_cols):
@@ -319,10 +356,16 @@ class Column(np.ndarray):
     def pformat(self, max_lines=None, show_name=True, show_units=False):
         """Return a list of formatted string representation of column values.
 
+        If no value of ``max_lines`` is supplied then the height of the screen
+        terminal is used to set ``max_lines``.  If the terminal height cannot
+        be determined then a default of ``astropy.table.MAX_LINES`` is used.
+        If a negative value of ``max_lines`` is supplied then there is no line
+        limit applied.
+
         Parameters
         ----------
         max_lines : int
-            Maximum number of values in output
+            Maximum lines of output (header + data rows)
 
         show_name : bool
             Include column name (default=True)
@@ -336,10 +379,7 @@ class Column(np.ndarray):
             List of formatted column values
 
         """
-        if max_lines == None:
-            max_lines = _get_screen_lines()
-        elif max_lines == -1:
-            max_lines = sys.maxint
+        max_lines, _ = _get_pprint_size(max_lines, -1)
 
         # "Print" all the values into temporary lists by column for
         # subsequent use and to determine the width
@@ -385,6 +425,12 @@ class Column(np.ndarray):
 
     def pprint(self, max_lines=None, show_name=True, show_units=False):
         """Print a formatted string representation of column values.
+
+        If no value of ``max_lines`` is supplied then the height of the screen
+        terminal is used to set ``max_lines``.  If the terminal height cannot
+        be determined then a default of ``astropy.table.MAX_LINES`` is used.
+        If a negative value of ``max_lines`` is supplied then there is no line
+        limit applied.
 
         Parameters
         ----------
@@ -738,13 +784,26 @@ class Table(object):
     def __str__(self):
         return '\n'.join(self.pformat())
 
-    def pprint(self, max_lines=None, show_name=True, show_units=False):
+    def pprint(self, max_lines=None, max_width=None, show_name=True,
+               show_units=False):
         """Print a nicely formatted string representation of the table.
+
+        If no value of ``max_lines`` is supplied then the height of the screen
+        terminal is used to set ``max_lines``.  If the terminal height cannot
+        be determined then a default of ``astropy.table.MAX_LINES`` is used.
+        If a negative value of ``max_lines`` is supplied then there is no line
+        limit applied.
+
+        The Same applies for max_width except the default is
+        ``astropy.table.MAX_WIDTH``.
 
         Parameters
         ----------
         max_lines : int
             Maximum number of lines in table output
+
+        max_width : int or None
+            Maximum character width of output
 
         show_name : bool
             Include a header row for column names (default=True)
@@ -753,16 +812,30 @@ class Table(object):
             Include a header row for units (default=False)
 
         """
-        print '\n'.join(self.pformat(max_lines, show_name, show_units))
+        print '\n'.join(self.pformat(max_lines, max_width, show_name,
+                                     show_units))
 
-    def pformat(self, max_lines=None, show_name=True, show_units=False):
+    def pformat(self, max_lines=None, max_width=None, show_name=True,
+                show_units=False):
         """Return a list of lines for the formatted string representation of
         the table.
 
+        If no value of ``max_lines`` is supplied then the height of the screen
+        terminal is used to set ``max_lines``.  If the terminal height cannot
+        be determined then a default of ``astropy.table.MAX_LINES`` is used.
+        If a negative value of ``max_lines`` is supplied then there is no line
+        limit applied.
+
+        The Same applies for max_width except the default is
+        ``astropy.table.MAX_WIDTH``.
+
         Parameters
         ----------
-        max_lines : int
+        max_lines : int or None
             Maximum number of rows to output
+
+        max_width : int or None
+            Maximum character width of output
 
         show_name : bool
             Include a header row for column names (default=True)
@@ -776,22 +849,35 @@ class Table(object):
             Formatted table as a single string
 
         """
-        n_rows = len(self)
-        col_strs_list = []
-        if len(self.colnames) == 0:
-            return []
-
         # "Print" all the values into temporary lists by column for subsequent
         # use and to determine the width
-        for col in self.columns.values():
-            col_strs_list.append(col.pformat(max_lines, show_name, show_units))
+        max_lines, max_width = _get_pprint_size(max_lines, max_width)
+        cols = [col.pformat(max_lines, show_name, show_units)
+                for col in self.columns.values()]
+
+        if not cols:
+            return []
+
+        n_rows = len(cols[0])
+        outwidth = lambda cols: sum(len(c[0]) for c in cols) + len(cols) - 1
+        dots_col = ['...'] * n_rows
+        middle = len(cols) // 2
+        while outwidth(cols) > max_width:
+            if len(cols) == 1:
+                break
+            if len(cols) == 2:
+                cols[1] = dots_col
+                break
+            if cols[middle] is dots_col:
+                cols.pop(middle)
+                middle = len(cols) // 2
+            cols[middle] = dots_col
 
         # Now "print" the (already-stringified) column values into a
         # row-oriented list.
-        n_rows = len(col_strs_list[0])
         rows = []
         for i in range(n_rows):
-            row = ' '.join(col_strs[i] for col_strs in col_strs_list)
+            row = ' '.join(col[i] for col in cols)
             rows.append(row)
 
         return rows
