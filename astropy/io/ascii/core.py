d35101c38bd979473fab6d5fab58eb946a0e5d6e
Tom Aldcroft <taldcroft@gmail.com>
2012-06-04 23:47:40 -0400

    Fix mistake in indentation when removing has_numpy logic

--- astropy/io/ascii/core.py
+++ astropy/io/ascii/core.py
@@ -982,7 +982,7 @@ def _get_reader(Reader, Inputter=None, Outputter=None, **kwargs):
 
     if Inputter is not None:
         reader.inputter = Inputter()
-        reader.outputter = TableOutputter()
+    reader.outputter = TableOutputter()
 
     if Outputter is not None:
         reader.outputter = Outputter()
