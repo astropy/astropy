adeeeb3e8a866dbe387449e004d083d925a49906
Erik Tollerud <erik.tollerud@gmail.com>
2012-06-11 17:18:52 -0700

    added more convinience functions in astropy.cosmology

--- astropy/cosmology/core.py
+++ astropy/cosmology/core.py
@@ -49,7 +49,7 @@ class CosmologyError(Exception):
 
 class Cosmology(object):
     """ Placeholder for when a more general Cosmology class is
-    implemented. """ 
+    implemented. """
     pass
 
 class FLRWCosmology(Cosmology):
@@ -684,3 +684,129 @@ def distmod(z, cosmo=None):
     if cosmo is None:
         cosmo = get_current()
     return cosmo.distmod(z)
+
+
+def H(z=0, cosmo=None):
+    """ Hubble parameter (km/s/Mpc) at redshift `z`.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    H : ndarray, or float if input scalar
+      Hubble parameter at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.H(z)
+
+
+def scale_factor(z=0, cosmo=None):
+    """ Scale factor at redshift `z`.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    distmod : ndarray, or float if input scalar
+      Scale factor at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.distmod(z)
+
+
+def critical_density(z=0, cosmo=None):
+    """ Critical density in grams per cubic cm at redshift `z`.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    critdens : ndarray, or float if input scalar
+      Critical density at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.critical_density(z)
+
+
+def lookback_time(z=0, cosmo=None):
+    """ Lookback time in Gyr to redshift `z`.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    t : ndarray, or float if input scalar
+      Lookback time at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.lookback_time(z)
+
+
+def comoving_distance(z=0, cosmo=None):
+    """ Comoving distance in Mpc at redshift `z`.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    codist : ndarray, or float if input scalar
+      Comoving distance at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.comoving_distance(z)
+
+
+def angular_diameter_distance(z=0, cosmo=None):
+    """ Angular diameter distance in Mpc at a given redshift.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    angdist : ndarray, or float if input scalar
+      Angular diameter distance at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.angular_diameter_distance(z)
+
+
+def luminosity_distance(z=0, cosmo=None):
+    """ Angular diameter distance in Mpc at a given redshift.
+
+    Parameters
+    ----------
+    z : array_like
+      Input redshifts.
+
+    Returns
+    -------
+    lumdist : ndarray, or float if input scalar
+      Angular diameter distance at each input redshift.
+    """
+    if cosmo is None:
+        cosmo = get_current()
+    return cosmo.luminosity_distance(z)
