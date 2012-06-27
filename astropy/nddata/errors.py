bef55efbf558c6bcf842a6ab8dfa6cbf81271f58
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2012-06-27 15:35:21 -0400

    added example methods to the base class and updated the example of SDError.

--- astropy/nddata/errors.py
+++ astropy/nddata/errors.py
@@ -1,50 +1,91 @@
 #module for errors in ndata
 class NDError(object):
     #super class for errors
-    pass
+    def __init__(self, error)
+        self.error = error
     
-    
+    def interpolate(self, old_wcs, new_wcs):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+    def convolve(self, kernel):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+    def error_add(self,
+                  self_data,
+                  self_mask,
+                  self_flag,
+                  operand,
+                  operand_data,
+                  operand_mask,
+                  operand_flag,
+                  result_data):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+    def error_sub(self,
+                  self_data,
+                  self_mask,
+                  self_flag,
+                  operand,
+                  operand_data,
+                  operand_mask,
+                  operand_flag,
+                  result_data):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+    def error_mul(self,
+                  self_data,
+                  self_mask,
+                  self_flag,
+                  operand,
+                  operand_data,
+                  operand_mask,
+                  operand_flag,
+                  result_data):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+    def error_div(self,
+                  self_data,
+                  self_mask,
+                  self_flag,
+                  operand,
+                  operand_data,
+                  operand_mask,
+                  operand_flag,
+                  result_data):
+        raise NotImplemented('This class is only for structure and not meant for usage')
+
+
 class SDError(NDError):
     
-    def __init__(self, error, covariant=None):
+    def __init__(self, error):
         self.error = error
-        #covariant for later implementation
-        self.covariant = covariant
-        
+
     
-    def interpolate(self, old_lookup_table, new_lookup_table):
-        return np.interp(new_lookup_table, old_lookup_table)
+    def interpolate(self, old_wcs, new_wcs):
+        raise NotImplemented()
+
+    def convolve(self, kernel):
+        raise NotImplemented()
     
     def _check_operand(self, operand):
         if not isinstance(operand, (SDError, None)):
             raise ValueError('unsupported operand type(s) for +: %s and %s' %\
                              (type(self), type(operand)))
     
-    def error_add(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):#should we also do result_mask??
+    def error_add(self,
+                  self_data,
+                  self_mask,
+                  self_flag,
+                  operand,
+                  operand_data,
+                  operand_mask,
+                  operand_flag,
+                  result_data):#should we also do result_mask, result_flag - chicken / egg problem what comes first??
         self._check_operand(operand)
-        if operand is None:
-            return self
-        
-        return np.sqrt(self**2 + operand**2)
-        
-    def error_sub(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
-        self._check_operand(operand)
-        if operand is None:
-            return self
-        
-        return np.sqrt(self**2 + operand**2)
 
-    def error_mul(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
-        self._check_operand(operand)
         if operand is None:
             return self
         
-        return np.sqrt((self / self_data)**2 + (operand / operand_data)**2) * result_data
+        return np.sqrt(self.error**2 + operand**2)
         
-    def error_div(self, self_data, self_mask, operand, operand_data, operand_mask, result_data):
-        self._check_operand(operand)
-        if operand is None:
-            return self
-        
-        return np.sqrt((self / self_data)**2 + (operand / operand_data)**2) * result_data
 
