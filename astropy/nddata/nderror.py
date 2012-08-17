6569b54ae0d839fe9efb1bdaf46b32f17b8a9116
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-08-17 11:51:17 +0200

    Improvements to NDError

--- astropy/nddata/nderror.py
+++ astropy/nddata/nderror.py
@@ -8,19 +8,29 @@ class NDError(object):
     This is the base class for error classes used with NDData. It is
     implemented as an abstract class and should never be directly
     instantiated.
+    
+    Classes inheriting from NDData should implement an __init__ method that
+    takes the parent NDData object as the first argument, and should overload
+    the ``propagate_*`` methods, keeping the call signature the same.
     '''
 
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def add(self, error):
+    def __init__(self, parent):
+        self._parent = parent
+
+    @abc.abstractmethod
+    def propagate_add(self, data, error):
         '''
-        Add current instance errors with `error`.
+        Propagate errors for addition.
 
         Parameters
         ----------
+        data : NDData instance
+            The data for the second operand in a + b
         error : NDError instance
-            The error to add to the current instance
+            The error on the second operand in a + b
 
         Returns
         -------
@@ -30,14 +40,54 @@ class NDError(object):
         pass
 
     @abc.abstractmethod
-    def subtract(self, error):
+    def propagate_subtract(self, data, error):
         '''
-        Subtract `error` from current instance errors.
+        Propagate errors for subtraction.
+        
+        Parameters
+        ----------
+        data : NDData instance
+            The data for the second operand in a + b
+        error : NDError instance
+            The error on the second operand in a - b
+
+        Returns
+        -------
+        result : NDError instance
+            The resulting error
+        '''
+        pass
+
+    @abc.abstractmethod
+    def propagate_multiply(self, data, error):
+        '''
+        Propagate errors for mutliplication.
 
         Parameters
         ----------
+        data : NDData instance
+            The data for the second operand in a + b
         error : NDError instance
-            The error to subtract from the current instance
+            The error on the second operand in a * b
+
+        Returns
+        -------
+        result : NDError instance
+            The resulting error
+        '''
+        pass
+
+    @abc.abstractmethod
+    def propagate_divide(self, data, error):
+        '''
+        Propagate errors for division.
+
+        Parameters
+        ----------
+        data : NDData instance
+            The data for the second operand in a + b
+        error : NDError instance
+            The error on the second operand in a / b
 
         Returns
         -------
