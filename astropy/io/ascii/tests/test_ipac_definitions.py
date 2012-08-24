8584e25dafca4b5b363475f516263971fb291d0d
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-08-25 00:09:48 +0200

    Changed 'strict' to 'between' in tests

--- astropy/io/ascii/tests/test_ipac_definitions.py
+++ astropy/io/ascii/tests/test_ipac_definitions.py
@@ -14,22 +14,19 @@ def test_ipac_default():
     assert table['b'][0] == 'ABBBBBBB'
 
 
-def test_ipac_strict():
-    # default should be strict
-    table = read(DATA, Reader=Ipac, definition='strict')
+def test_ipac_between():
+    table = read(DATA, Reader=Ipac, definition='between')
     assert table['a'][0] == 'BBBBBB'
     assert table['b'][0] == 'BBBBBBB'
     
     
 def test_ipac_left():
-    # default should be strict
     table = read(DATA, Reader=Ipac, definition='left')
     assert table['a'][0] == 'BBBBBBA'
     assert table['b'][0] == 'BBBBBBBA'
     
     
 def test_ipac_right():
-    # default should be strict
     table = read(DATA, Reader=Ipac, definition='right')
     assert table['a'][0] == 'ABBBBBB'
     assert table['b'][0] == 'ABBBBBBB'
