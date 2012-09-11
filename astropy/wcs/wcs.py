ecc45bb14cee856d676e806723e4c1a0ba51c3e1
Michael Droettboom <mdboom@gmail.com>
2012-09-11 15:44:51 -0400

    Write out all DP keywords

--- astropy/wcs/wcs.py
+++ astropy/wcs/wcs.py
@@ -605,6 +605,10 @@ naxis kwarg.
             hdulist[0].header.update('{0}{1:d}.AXIS.{1:d}'.format(d_kw, num),
                                      num)
 
+            for i in range(cpdis.data.ndim):
+                hdulist[0].header.update(
+                    '%s%d.AXIS.%d' % (d_kw, num, i + 1), i + 1)
+
             image = fits.ImageHDU(cpdis.data, name='WCSDVARR')
             header = image.header
 
@@ -614,6 +618,9 @@ naxis kwarg.
             header.update('CRVAL2', cpdis.crval[1])
             header.update('CDELT1', cpdis.cdelt[0])
             header.update('CDELT2', cpdis.cdelt[1])
+
+            header.update('EXTVER',
+                          hdulist[0].header['%s%d.EXTVER' % (d_kw, num)])
 
             hdulist.append(image)
 
