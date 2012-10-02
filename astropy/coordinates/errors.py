eb88e0235477cce80a67b2a0de0e93a661d1767a
Demitri Muna <demitri@me.com>
2012-10-02 11:13:35 -0400

    More work on core, updated several tests.

---
+++ astropy/coordinates/errors.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+''' This module defines custom errors and exceptions used in astropy.coordinates. '''
+
+class RangeError(Exception):
+	pass
