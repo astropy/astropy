afb195b1ca647d776f29fbc1d68a495190caec59
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-10-04 18:48:18 -0400

    Fix remaining include_dirs that imported numpy ('numpy' gets replaced at build-time). This is necessary for egg_info to work.

--- astropy/time/setup_package.py
+++ astropy/time/setup_package.py
@@ -1,5 +1,4 @@
 import os
-import numpy
 from distutils.extension import Extension
 
 TIMEROOT = os.path.relpath(os.path.dirname(__file__))
@@ -9,6 +8,6 @@ def get_extensions():
     time_ext = Extension(
     name="astropy.time.sofa_time",
     sources=[os.path.join(TIMEROOT, "sofa_time.pyx"), "cextern/sofa/sofa.c"],
-    include_dirs=[numpy.get_include(), 'cextern/sofa'],
+    include_dirs=['numpy', 'cextern/sofa'],
     language="c",)
     return [time_ext]
