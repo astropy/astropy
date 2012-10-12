46310be6681cba2d54907367a1b108a8d8f0cd14
Pey Lian Lim <lim@stsci.edu>
2012-10-12 11:25:55 -0400

    got vos_catalog to work

--- astropy/io/vo/query/vos_catalog.py
+++ astropy/io/vo/query/vos_catalog.py
@@ -1,52 +1,37 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
-Common utilities for accessing VO simple services.
-"""
-
-# TODO docstrings
+"""Common utilities for accessing VO simple services."""
 
 from __future__ import print_function, division
+
+# STDLIB
+import io
+import json
+
+# LOCAL CONFIG
+from astropy.config.configuration import ConfigurationItem, get_config_items
+from astropy.config.data import get_data_fileobj
+from astropy import log
+
+BASEURL = ConfigurationItem('conesearch_baseurl',
+                            'http://stsdas.stsci.edu/astrolib/vo_databases/',
+                            'URL where conesearch config file is stored.')
+
+VO_CFG = get_config_items('astropy.io.vo')
+try:
+    VO_PEDANTIC = VO_CFG['PEDANTIC']
+except KeyError as e:
+    # Could use set, but do not want to mess with general vo config
+    log.warn('PEDANTIC not found in config, defaulting to True')
+    VO_PEDANTIC = True
 
 # LOCAL
 from . import webquery
 from .. import table
-from ..voexceptions import vo_warn, W24, W25
-from astropy.config import get_config_dir
-
-BASEURL = 'http://stsdas.stsci.edu/astrolib/vo_databases/'
+from ..exceptions import vo_warn, W24, W25
+from ..util import IS_PY3K
+from astropy.utils.console import color_print
 
 __dbversion__ = 1
-
-
-#  until here - modify to read XML from web query
-#
-# http://docs.astropy.org/en/v0.1/_generated/astropy.config.data.get_data_fileobj.html#astropy.config.data.get_data_fileobj
-# db_cache = cache.Cache(get_config_dir(), BASEURL)
-
-
-# TODO:
-#
-#    *catalog_db* may be one of the following, in order from easiest to
-#    use to most control:
-#
-#      - `None`: A database of conesearch catalogs is downloaded from
-#        STScI.  The first catalog in the database to successfully
-#        return a result is used.
-#
-#      - *catalog name*: A name in the database of conesearch catalogs
-#        at STScI is used.  For a list of acceptable names, see
-#        :func:`list_catalogs`.
-#
-#      - *url*: The prefix of a *url* to a IVOA Cone Search Service.
-#        Must end in either `?` or `&`.
-#
-#      - A `~vos_catalog.VOSCatalog` instance: A specific catalog
-#        manually downloaded and selected from the database using the
-#        APIs in the :mod:`vos_catalog` module.
-#
-#      - Any of the above 3 options combined in a list, in which case
-#        they are tried in order.
-
 
 class VOSCatalog(object):
     """
@@ -73,26 +58,30 @@ class VOSDatabase(object):
 
         if not 'catalogs' in tree:
             raise IOError("Invalid VO service catalog database")
+
         self._catalogs = tree['catalogs']
 
     def get_catalogs(self):
+        """Iterator to get all catalogs."""
         for key, val in self._catalogs.items():
             yield key, VOSCatalog(val)
 
     def get_catalog(self, name):
+        """Get one catalog of given name."""
         if not name in self._catalogs:
             raise ValueError("No catalog '%s' found." % name)
         return VOSCatalog(self._catalogs[name])
 
     def list_catalogs(self):
+        """List catalog names."""
         return self._catalogs.keys()
 
-def get_remote_catalog_db(dbname, cache_instance=db_cache):
+def get_remote_catalog_db(dbname):
     """
     Get a database of VO services (which is a JSON file) from a remote
     location.
     """
-    fd = cache_instance.get_file(dbname + ".json")
+    fd = get_data_fileobj(BASEURL() + dbname + '.json', cache=True)
     if IS_PY3K:
         wrapped_fd = io.TextIOWrapper(fd, 'utf8')
     else:
@@ -134,22 +123,50 @@ def _vo_service_request(url, pedantic, kwargs):
 
     return tab.get_first_table()
 
-def call_vo_service(service_type, catalog_db=None, pedantic=False,
+def call_vo_service(service_type, catalog_db=None, pedantic=None,
                     verbose=True, kwargs={}):
     """
     Makes a generic VO service call.
 
-    *service_type* is the name of the type of service,
-    eg. 'conesearch'.  Used in error messages and to select a catalog
-    database if one is not provided.
+    Parameters
+    ----------
+    service_type : str
+        Name of the type of service, e.g., 'conesearch'.
+        Used in error messages and to select a catalog database
+        if one is not provided.
 
-    %(catalog_db)s
+    catalog_db : may be one of the following, in order from easiest to
+    use to most control:
 
-    %(pedantic)s
+        - `None`: A database of conesearch catalogs is downloaded from
+           STScI.  The first catalog in the database to successfully
+           return a result is used.
 
-    *kwargs* is a dictionary of arguments to pass to the catalog
-    service.  No checking is done that the arguments are accepted by
-    the service etc.
+        - *catalog name*: A name in the database of conesearch catalogs
+          at STScI is used.  For a list of acceptable names, see
+          :func:`list_catalogs`.
+
+        - *url*: The prefix of a *url* to a IVOA Cone Search Service.
+          Must end in either `?` or `&`.
+
+        - A `~vos_catalog.VOSCatalog` instance: A specific catalog
+          manually downloaded and selected from the database using the
+          APIs in the :mod:`vos_catalog` module.
+
+        - Any of the above 3 options combined in a list, in which case
+          they are tried in order.
+
+    pedantic : bool or `None`
+        See  `astropy.io.vo.table.parse`.
+
+    verbose : bool
+        Verbose output.
+
+    kwargs : dictionary
+        Keyword arguments to pass to the catalog service.
+        No checking is done that the arguments are accepted by
+        the service etc.
+
     """
     if catalog_db is None:
         catalog_db = get_remote_catalog_db(service_type)
@@ -165,11 +182,11 @@ def call_vo_service(service_type, catalog_db=None, pedantic=False,
         catalogs = catalog_db.get_catalogs()
     else:
         raise TypeError(
-            "catalog_db must be a catalog database, a list of catalogs, or a catalog")
+            'catalog_db must be a catalog database, a list of catalogs, '
+            'or a catalog')
 
-    # # Perform a random ordering of the catalogs -- this is just a dumb
-    # # heuristic for now.
-    # random.shuffle(catalogs)
+    if pedantic is None:
+        pedantic = VO_PEDANTIC
 
     for name, catalog in catalogs:
         if isinstance(catalog, basestring):
@@ -183,20 +200,27 @@ def call_vo_service(service_type, catalog_db=None, pedantic=False,
             url = catalog.get_url()
 
         if verbose:
-            util.color_print('green', "Trying %s" % url, bold=True)
+            util.color_print('green', 'Trying {}'.format(url), bold=True)
 
         try:
             return _vo_service_request(url, pedantic, kwargs)
         except Exception as e:
             vo_warn(W25, (url, str(e)))
 
-    raise IOError("None of the available catalogs returned valid results.")
-call_vo_service.__doc__ = call_vo_service.__doc__ % _doc_snippets
+    raise IOError('None of the available catalogs returned valid results.')
 
 def list_catalogs(service_type):
     """
     List the catalogs available for the given service type.
-    """
-    catalog_db = get_remote_catalog_db(service_type)
 
-    return catalog_db.list_catalogs()
+    Parameters
+    ----------
+    service_type : {'basic', 'conesearch', 'conesearch_test', 'image', 'ssa'}
+        At this stage of development, these are all incomplete.
+
+    Returns
+    -------
+    value : list of str
+
+    """
+    return get_remote_catalog_db(service_type).list_catalogs()
