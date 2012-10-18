4b18a606c6e5e48645cdd8c68c31c003371f8693
Erik Tollerud <erik.tollerud@gmail.com>
2012-10-18 14:20:25 -0400

    moved NumpyRNGContext from stats tests to utils

--- astropy/stats/tests/test_funcs.py
+++ astropy/stats/tests/test_funcs.py
@@ -1,8 +1,10 @@
 from __future__ import division
 
 import numpy as np
-from .. import funcs
 from pytest import mark
+
+from .. import funcs
+from ...utils.misc import NumpyRNGContext
 
 try:
     from scipy import stats
@@ -10,52 +12,6 @@ except ImportError:
     HAS_SCIPY = False
 else:
     HAS_SCIPY = True
-
-
-class NumpyRNGContext(object):
-    """
-    A context manager (for use with the ``with`` statement) that will seed the
-    numpy random number generator (RNG) to a specific value, and then restore
-    the RNG state back to whatever it was before.
-
-    This is primarily intended for use in the astropy testing suit, but it
-    may be useful in ensuring reproducability of monte carlo simulations in a
-    science context.
-
-    Parameters
-    ----------
-    seed : int
-        The value to use to seed the numpy RNG
-
-    Examples
-    --------
-    A tyoical use case might be::
-
-        with NumpyRNGContext(<some seed value you pick>):
-            from numpy import random
-
-            randarr = random.randn(100)
-            ... run your test using `randarr` ...
-
-        #Any code using numpy.random at this indent level will act just as it
-        #would have if it had been before the with statement - e.g. whatever
-        #the default seed is.
-
-
-    """
-    def __init__(self, seed):
-        self.seed = seed
-
-    def __enter__(self):
-        from numpy import random
-
-        self.startstate = random.get_state()
-        random.seed(self.seed)
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        from numpy import random
-
-        random.set_state(self.startstate)
 
 
 def test_sigma_clip():
