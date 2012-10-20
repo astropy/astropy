c507bf27b95bb724764a66ef85d03e609d3b2e5c
Tom Aldcroft <taldcroft@gmail.com>
2012-10-20 09:03:42 -0400

    Update table test_item_access for masking (not yet passing)

--- astropy/table/tests/test_item_access.py
+++ astropy/table/tests/test_item_access.py
@@ -1,35 +1,59 @@
 """ Verify item access API in:
 https://github.com/astropy/astropy/wiki/Table-item-access-definition
 """
-from .. import Table, Column
+from ... import table
 import numpy as np
 import pytest
 from ... import units as u
 from ...tests.helper import raises
 
 
-COLS = [Column('a', [1, 2, 3], description='da',
-               format='fa', meta={'ma': 1}, units='ua'),
-        Column('b', [4, 5, 6], description='db',
-               format='fb', meta={'mb': 1}, units='uF'),
-        Column('c', [7, 8, 9], description='dc',
-               format='fc', meta={'mc': 1},
-               units=u.Unit('UnrecognizedUnit', parse_strict='silent'))]
-DATA = Table(COLS)
+# Dummy init of Table, DATA for pyflakes and to be sure test fixture is working
+Table = None
+Column = None
+DATA = None
+COLS = None
 
 
+class MaskedTable(table.Table):
+    def __init__(self, *args, **kwargs):
+        kwargs['masked'] = True
+        table.Table.__init__(self, *args, **kwargs)
+
+
+# Fixture to run all the Column tests for both an unmasked (ndarray)
+# and masked (MaskedArray) column.
+@pytest.fixture(params=[False, True])
+def set_global_Table_DATA(request):
+    global Table, Column, DATA, COLS
+
+    Table = MaskedTable if request.param else table.Table
+    Column = table.MaskedColumn if request.param else table.Column
+    COLS = [Column('a', [1, 2, 3], description='da',
+                   format='fa', meta={'ma': 1}, units='ua'),
+            Column('b', [4, 5, 6], description='db',
+                   format='fb', meta={'mb': 1}, units='ub'),
+            Column('c', [7, 8, 9], description='dc',
+                   format='fc', meta={'mc': 1}, units='ub')]
+    DATA = Table(COLS)
+
+
+@pytest.mark.usefixtures('set_global_Table_DATA')
 class BaseTestItems():
 
     def setup_method(self, method):
-        self.t = Table(COLS)
-        self.tc = self.t.columns
+        pass
 
 
+@pytest.mark.usefixtures('set_global_Table_DATA')
 class TestTableColumnsItems(BaseTestItems):
 
     def test_by_name(self):
         """Access TableColumns by name and show that item access returns
         a Column that refers to underlying table data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         assert self.tc['a'].name == 'a'
         assert self.tc['a'][1] == 2
         assert self.tc['a'].description == 'da'
@@ -45,6 +69,9 @@ class TestTableColumnsItems(BaseTestItems):
     def test_by_position(self):
         """Access TableColumns by position and show that item access returns
         a Column that refers to underlying table data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         assert self.tc[1].name == 'b'
         assert np.all(self.tc[1].data == COLS[1].data)
         assert self.tc[1].description == 'db'
@@ -62,6 +89,9 @@ class TestTableColumnsItems(BaseTestItems):
     def test_mult_columns(self):
         """Access TableColumns with "fancy indexing" and showed that returned
         TableColumns object still references original data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         tc2 = self.tc['b', 'c']
         assert tc2[1].name == 'c'
         assert tc2[1][1] == 8
@@ -75,6 +105,9 @@ class TestTableColumnsItems(BaseTestItems):
     def test_column_slice(self):
         """Access TableColumns with slice and showed that returned
         TableColumns object still references original data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         tc2 = self.tc[1:3]
         assert tc2[1].name == 'c'
         assert tc2[1][1] == 8
@@ -86,10 +119,14 @@ class TestTableColumnsItems(BaseTestItems):
         assert self.t['c'][1] == 0
 
 
+@pytest.mark.usefixtures('set_global_Table_DATA')
 class TestTableItems(BaseTestItems):
 
     def test_column(self):
         """Column access returns REFERENCE to data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         a = self.t['a']
         assert a[1] == 2
         a[1] = 0
@@ -97,6 +134,9 @@ class TestTableItems(BaseTestItems):
 
     def test_row(self):
         """Row  access returns REFERENCE to data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         row = self.t[1]
         assert row['a'] == 2
         assert row[1] == 5
@@ -109,6 +149,9 @@ class TestTableItems(BaseTestItems):
 
     def test_table_slice(self):
         """Table slice returns REFERENCE to data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         t2 = self.t[1:3]
         assert np.all(t2['a'] == DATA['a'][1:3])
         assert t2['a'].attrs_equal(COLS[0])
@@ -119,6 +162,9 @@ class TestTableItems(BaseTestItems):
 
     def test_fancy_index_slice(self):
         """Table fancy slice returns COPY of data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         slice = np.array([0, 2])
         t2 = self.t[slice]
         assert np.all(t2['a'] == DATA['a'][slice])
@@ -131,6 +177,9 @@ class TestTableItems(BaseTestItems):
 
     def test_list_index_slice(self):
         """Table list index slice returns COPY of data"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         slice = [0, 2]
         t2 = self.t[slice]
         assert np.all(t2['a'] == DATA['a'][slice])
@@ -144,6 +193,9 @@ class TestTableItems(BaseTestItems):
     def test_select_columns(self):
         """Select columns returns COPY of data and all column
         attributes"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         t2 = self.t['a', 'c']
         assert np.all(t2['a'] == DATA['a'])
         assert np.all(t2['c'] == DATA['c'])
@@ -155,6 +207,9 @@ class TestTableItems(BaseTestItems):
 
     def test_select_bad_column(self):
         """Select column name that does not exist"""
+        self.t = Table(COLS)
+        self.tc = self.t.columns
+
         with pytest.raises(ValueError):
             self.t['a', 1]
 
