b709a0d87af3940ff1972969fbd17981dc57a743
Erik Tollerud <erik.tollerud@gmail.com>
2012-10-23 17:36:32 -0400

    added _repr_latex_ to Quantity for ipython notebook

--- astropy/units/quantity.py
+++ astropy/units/quantity.py
@@ -2,7 +2,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 This module defines the `Quantity` object, which represents a number with some associated
-units. `Quantity` objects support operations like ordinary numbers, but will deal with 
+units. `Quantity` objects support operations like ordinary numbers, but will deal with
 unit conversions internally.
 """
 
@@ -20,25 +20,25 @@ __all__ = ["Quantity", "IncompatibleUnitsError"]
 def _validate_units(unit):
     """ Make sure that the input is a either a string parseable by the `units` package, or a
         `Unit` object.
-        
+
     Parameters
     ----------
     unit : `~astropy.units.UnitBase` instance, str
         Must be an `~astropy.units.UnitBase` object or a string parseable by the `units` package.
     """
-    
+
     if isinstance(unit, (str, unicode)):
         unit_obj = Unit(unit)
     elif isinstance(unit, UnitBase):
         unit_obj = unit
     else:
         raise ValueError("The unit must be a Python string that is parseable by the Units package, or a Unit object.")
-    
+
     return unit_obj
 
 class Quantity(object):
-    """ A `Quantity` represents a number wth some associated unit. 
-        
+    """ A `Quantity` represents a number wth some associated unit.
+
     Parameters
     ----------
     value : number
@@ -46,25 +46,25 @@ class Quantity(object):
     unit : `~astropy.units.UnitBase` instance, str
         An object that represents the unit associated with the input value. Must be an `~astropy.units.UnitBase`
         object or a string parseable by the `units` package.
-    
+
     Raises
     ------
     ValueError
         If the unit provided is not either a `Unit` object or a parseable string unit.
     """
-    
+
     def __init__(self, value, unit):
         # The user must pass in a Python numeric type
         if isinstance(value, numbers.Number):
             self.value = value
         else:
             raise ValueError("The value must be a valid Python numeric type.")
-        
+
         self._unit = _validate_units(unit)
-        
+
     def to(self, unit):
         """ Returns a new `Quantity` object with the specified units.
-        
+
         Parameters
         ----------
         unit : `~astropy.units.UnitBase` instance, str
@@ -74,64 +74,64 @@ class Quantity(object):
         new_quantity = self.copy()
         new_quantity.unit = unit
         return new_quantity
-    
+
     @property
     def unit(self):
         return self._unit
-    
+
     @unit.setter
     def unit(self, unit):
         """ Setter for the unit attribute. We allow the user to change units by setting this attribute,
         so this will validate the unit and internally change the value to the new unit.
-        
+
         Parameters
         ----------
         unit : `~astropy.units.UnitBase` instance, str
             An object that represents the unit to internally convert to. Must be an `~astropy.units.UnitBase`
             object or a string parseable by the `units` package.
-        
+
         Raises
         ------
         IncompatibleUnitsError
             If the unit to convert to is not 'equivalent' (see `Unit` documentation) to the original unit.
         """
         new_unit = _validate_units(unit)
-        
+
         if not self.unit.is_equivalent(new_unit):
             raise IncompatibleUnitsError("This object has units of '{0:s}' and can not be converted to '{1:s}'.".format(self.unit, new_unit))
-        
+
         self.value *= self.unit.to(new_unit)
         self._unit = new_unit
-    
+
     def copy(self):
         """ Return a copy of this `Quantity` instance """
         return Quantity(self.value, unit=self.unit)
-    
+
     # Arithmetic operations
     def __add__(self, other):
         """ Addition between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
         """
         return Quantity(self.value + other.to(self.unit).value, unit=self.unit)
-    
+
     def __radd__(self, other):
         """ Addition between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
         """
         return Quantity(self.to(other.unit).value + other.value, unit=other.unit)
-    
+
     def __sub__(self, other):
         """ Subtraction between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
         """
         return Quantity(self.value - other.to(self.unit).value, unit=self.unit)
-    
+
     def __rsub__(self, other):
         """ Subtraction between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
         """
         return Quantity(self.to(other.unit).value - other.value, unit=other.unit)
-        
+
     def __mul__(self, other):
         """ Multiplication between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
@@ -140,7 +140,7 @@ class Quantity(object):
             return Quantity(self.value * other.to(self.unit).value, unit=self.unit*self.unit)
         else:
             return Quantity(self.value * other.value, unit=self.unit*other.unit)
-    
+
     def __rmul__(self, other):
         """ Multiplication between `Quantity` objects. All operations return a new `Quantity` object
         with the units of the **left** object.
@@ -149,7 +149,7 @@ class Quantity(object):
             return Quantity(other.value * self.to(other.unit).value, unit=other.unit*other.unit)
         else:
             return Quantity(self.value * other.value, unit=other.unit * self.unit)
-    
+
     # TODO: We should make the distinction between the __future__ division and old-style division
     def __div__(self, other):
         """ Division between `Quantity` objects. This operation returns a dimensionless object. """
@@ -157,57 +157,72 @@ class Quantity(object):
             return Quantity(self.value / other.to(self.unit).value, unit=Unit(""))
         else:
             return Quantity(self.value / other.value, unit=self.unit/other.unit)
-    
+
     def __rdiv__(self, other):
         """ Division between `Quantity` objects. This operation returns a dimensionless object. """
         if self.unit.is_equivalent(other.unit):
             return Quantity(other.value / self.to(other.unit).value, unit=Unit(""))
         else:
             return Quantity(other.value / self.value, unit=other.unit/self.unit)
-    
+
     def __truediv__(self, other):
         """ Division between `Quantity` objects. This operation returns a dimensionless object. """
         if self.unit.is_equivalent(other.unit):
             return Quantity(self.value / other.to(self.unit).value, unit=Unit(""))
         else:
             return Quantity(self.value / other.value, unit=self.unit/other.unit)
-    
+
     def __rtruediv__(self, other):
         """ Division between `Quantity` objects. This operation returns a dimensionless object. """
         if self.unit.is_equivalent(other.unit):
             return Quantity(other.value / self.to(other.unit).value, unit=Unit(""))
         else:
             return Quantity(other.value / self.value, unit=other.unit/self.unit)
-    
+
     def __pow__(self, p):
         """ Raise quantity object to a power. """
         return Quantity(self.value**2, unit=(self.unit*self.unit).simplify())
-    
+
     # Comparison operations
     def __eq__(self, other):
         return self.value == other.to(self.unit).value
 
     def __ne__(self, other):
         return self.value != other.to(self.unit).value
-        
+
     def __lt__(self, other):
         return self.value < other.to(self.unit).value
-    
+
     def __le__(self, other):
         return self.value <= other.to(self.unit).value
-    
+
     def __gt__(self, other):
         return self.value > other.to(self.unit).value
-    
+
     def __ge__(self, other):
         return self.value >= other.to(self.unit).value
-    
+
     # Display
     def __str__(self):
         return "{0:g} {1:s}".format(self.value, self.unit.to_string())
-    
+
     def __repr__(self):
         return "<Quantity value: {0:g} unit: {1:s}>".format(self.value, self.unit.to_string())
-        
+
+    def _repr_latex_(self):
+        """
+        Generate latex representation of unit name.  This is used by
+        the IPython notebook to show it all latexified.
+
+        Returns
+        -------
+        lstr
+            LaTeX string
+        """
+        # [1:-1] strips the '$' on either side needed for math mode
+        unitlstr = self.unit._repr_latex_()[1:-1]  # note this is unicode
+        return u'${0:g} \; {1}$'.format(self.value, unitlstr)
+
+
 class IncompatibleUnitsError(Exception):
     pass
