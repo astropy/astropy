8a49826cfbd7e93d615713c2a979bcf38e6cc838
Erik Tollerud <erik.tollerud@gmail.com>
2012-10-23 04:22:26 -0400

    added spherical<-> cartesian tests

---
+++ astropy/coordinates/tests/test_transforms.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function
+
+import numpy as np
+from numpy import testing as npytest
+
+
+def test_sphere_cart():
+    """
+    Tests the spherical <-> cartesian transform functions
+    """
+    from ..transformations import spherical_to_cartesian, cartesian_to_spherical
+
+    x, y ,z = spherical_to_cartesian(1, 0, 0)
+    npytest.assert_almost_equal(x, 1)
+    npytest.assert_almost_equal(y, 0)
+    npytest.assert_almost_equal(z, 0)
+
+
+    x, y ,z = spherical_to_cartesian(0, 1, 1)
+    npytest.assert_almost_equal(x, 0)
+    npytest.assert_almost_equal(y, 0)
+    npytest.assert_almost_equal(z, 0)
+
+
+    r, lat, lng = cartesian_to_spherical(0, 1, 0)
+    npytest.assert_almost_equal(r, 1)
+    npytest.assert_almost_equal(lat, 0)
+    npytest.assert_almost_equal(lng, np.pi / 2)
+
+
+    #test round-tripping
+    #TODO: add NumpRNG context after merging w/master
+    x, y, z = np.random.randn(3, 5)
+
+    r, lat, lng = cartesian_to_spherical(x, y, z)
+    x2, y2, z2 = spherical_to_cartesian(r, lat, lng)
+
+    npytest.assert_allclose(x, x2)
+    npytest.assert_allclose(y, y2)
+    npytest.assert_allclose(z, z2)
