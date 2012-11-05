f499362671d90048d302c3ae9ed15fc9b517b189
Erik Tollerud <erik.tollerud@gmail.com>
2012-11-05 02:07:35 -0500

    moved spherical/cartesian transforms

--- astropy/coordinates/coordsystems.py
+++ astropy/coordinates/coordsystems.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This module contains the base classes and framework for coordinate objects.
+This module contains the base classes and frameworks for coordinate objects.
 """
 
 from abc import ABCMeta, abstractproperty, abstractmethod
@@ -12,7 +12,7 @@ from .. import units as u
 from .. import cosmology
 
 __all__ = ['SphericalCoordinatesBase', 'Coordinates', 'Distance',
-           'CartesianPoint'
+           'CartesianPoint', 'cartesian_to_spherical', 'spherical_to_cartesian'
           ]
 
 
@@ -108,8 +108,6 @@ class SphericalCoordinatesBase(object):
 
     def _make_cart(self, override=False):
         if override or self._cartpoint is None:
-            from .transformations import spherical_to_cartesian
-
             if self._distanceq is None:
                 r = 1
                 runit = None
@@ -379,8 +377,6 @@ class CartesianPoint(object):
             The longitude in radians
 
         """
-        from .transformations import cartesian_to_spherical
-
         return cartesian_to_spherical(self.x, self.y, self.z)
 
 
@@ -513,3 +509,104 @@ class Coordinates(object):
                         # we have two values - the goal is to end up with two Angle
                         # objects.
                         pass
+
+
+#<------------transformation-related utility functions----------------->
+
+def cartesian_to_spherical(x, y, z):
+    """
+    Converts 3D rectangular cartesian coordinates to spherical polar
+    coordinates.
+
+    Note that the resulting angles are latitude/longitude or
+    elevation/azimuthal form.  I.e., the origin is along the equator
+    rather than at the north pole.
+
+    .. note::
+        This is a low-level function used internally in
+        `astropy.coordinates`.  It is provided for users if they really
+        want to use it, but it is recommended that you use the
+        `astropy.coordinates` coordinate systems.
+
+    Parameters
+    ----------
+    x : scalar or array-like
+        The first cartesian coordinate.
+    y : scalar or array-like
+        The second cartesian coordinate.
+    z : scalar or array-like
+        The third cartesian coordinate.
+
+    Returns
+    -------
+    r : float or array
+        The radial coordinate (in the same units as the inputs).
+    lat : float or array
+        The latitude in radians
+    lng : float or array
+        The longitude in radians
+    """
+
+    xsq = x ** 2
+    ysq = y ** 2
+    zsq = z ** 2
+
+    r = (xsq + ysq + zsq) ** 0.5
+    s = (xsq + ysq) ** 0.5
+
+    if np.isscalar(x) and np.isscalar(y) and np.isscalar(z):
+        lng = math.atan2(y, x)
+        lat = math.atan2(z, s)
+    else:
+        lng = np.arctan2(y, x)
+        lat = np.arctan2(z, s)
+
+    return r, lat, lng
+
+
+def spherical_to_cartesian(r, lat, lng):
+    """
+    Converts spherical polar coordinates to rectangular cartesian
+    coordinates.
+
+    Note that the input angles should be in latitude/longitude or
+    elevation/azimuthal form.  I.e., the origin is along the equator
+    rather than at the north pole.
+
+    .. note::
+        This is a low-level function used internally in
+        `astropy.coordinates`.  It is provided for users if they really
+        want to use it, but it is recommended that you use the
+        `astropy.coordinates` coordinate systems.
+
+    Parameters
+    ----------
+    r : scalar or array-like
+        The radial coordinate (in the same units as the inputs).
+    lat : scalar or array-like
+        The latitude in radians
+    lng : scalar or array-like
+        The longitude in radians
+
+    Returns
+    -------
+    x : float or array
+        The first cartesian coordinate.
+    y : float or array
+        The second cartesian coordinate.
+    z : float or array
+        The third cartesian coordinate.
+
+
+    """
+
+    if np.isscalar(r) and np.isscalar(lat) and np.isscalar(lng):
+        x = r * math.cos(lat) * math.cos(lng)
+        y = r * math.cos(lat) * math.sin(lng)
+        z = r * math.sin(lat)
+    else:
+        x = r * np.cos(lat) * np.cos(lng)
+        y = r * np.cos(lat) * np.sin(lng)
+        z = r * np.sin(lat)
+
+    return x, y, z
--- astropy/coordinates/transformations.py
+++ astropy/coordinates/transformations.py
@@ -1,9 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This module contains the functionality for transforming points from
-one coordinate system to another (e.g. equatorial to galactic). The public
-interface for this functionality is in the file core.py.
+This module contains the framework for transforming points from
+one coordinate system to another (e.g. equatorial to galactic). The
+implementation is actually in individual coordinates in the
+`builtin_systems` module, while this module provides the framework and
+related utilities.
 """
 
 import math
@@ -12,103 +14,4 @@ import numpy as np
 
 pi = math.pi
 
-__all__ = ['cartesian_to_spherical', 'spherical_to_cartesian']
-
-
-def cartesian_to_spherical(x, y, z):
-    """
-    Converts 3D rectangular cartesian coordinates to spherical polar
-    coordinates.
-
-    Note that the resulting angles are latitude/longitude or
-    elevation/azimuthal form.  I.e., the origin is along the equator
-    rather than at the north pole.
-
-    .. note::
-        This is a low-level function used internally in
-        `astropy.coordinates`.  It is provided for users if they really
-        want to use it, but it is recommended that you use the
-        `astropy.coordinates` coordinate systems.
-
-    Parameters
-    ----------
-    x : scalar or array-like
-        The first cartesian coordinate.
-    y : scalar or array-like
-        The second cartesian coordinate.
-    z : scalar or array-like
-        The third cartesian coordinate.
-
-    Returns
-    -------
-    r : float or array
-        The radial coordinate (in the same units as the inputs).
-    lat : float or array
-        The latitude in radians
-    lng : float or array
-        The longitude in radians
-    """
-
-    xsq = x ** 2
-    ysq = y ** 2
-    zsq = z ** 2
-
-    r = (xsq + ysq + zsq) ** 0.5
-    s = (xsq + ysq) ** 0.5
-
-    if np.isscalar(x) and np.isscalar(y) and np.isscalar(z):
-        lng = math.atan2(y, x)
-        lat = math.atan2(z, s)
-    else:
-        lng = np.arctan2(y, x)
-        lat = np.arctan2(z, s)
-
-    return r, lat, lng
-
-
-def spherical_to_cartesian(r, lat, lng):
-    """
-    Converts spherical polar coordinates to rectangular cartesian
-    coordinates.
-
-    Note that the input angles should be in latitude/longitude or
-    elevation/azimuthal form.  I.e., the origin is along the equator
-    rather than at the north pole.
-
-    .. note::
-        This is a low-level function used internally in
-        `astropy.coordinates`.  It is provided for users if they really
-        want to use it, but it is recommended that you use the
-        `astropy.coordinates` coordinate systems.
-
-    Parameters
-    ----------
-    r : scalar or array-like
-        The radial coordinate (in the same units as the inputs).
-    lat : scalar or array-like
-        The latitude in radians
-    lng : scalar or array-like
-        The longitude in radians
-
-    Returns
-    -------
-    x : float or array
-        The first cartesian coordinate.
-    y : float or array
-        The second cartesian coordinate.
-    z : float or array
-        The third cartesian coordinate.
-
-
-    """
-
-    if np.isscalar(r) and np.isscalar(lat) and np.isscalar(lng):
-        x = r * math.cos(lat) * math.cos(lng)
-        y = r * math.cos(lat) * math.sin(lng)
-        z = r * math.sin(lat)
-    else:
-        x = r * np.cos(lat) * np.cos(lng)
-        y = r * np.cos(lat) * np.sin(lng)
-        z = r * np.sin(lat)
-
-    return x, y, z
+__all__ = []
