Xfail masking tests that don't work on numpy 1.4, fix test imports

astropy/table/tests/test_pprint.py
@@ -1,8 +1,8 @@
-import pytest
 import numpy as np
 
-from astropy import table
-from astropy.table import pprint
+from ...tests.helper import pytest
+from ... import table
+from ...table import pprint
 
 BIG_WIDE_ARR = np.arange(2000, dtype=np.float).reshape(100, 20)
 SMALL_ARR = np.arange(12, dtype=np.int).reshape(4, 3)

astropy/table/tests/test_row.py
@@ -1,8 +1,11 @@
-import pytest
+from distutils import version
 import numpy as np
-from astropy import table
-from astropy.table import Row
 
+from ...tests.helper import pytest
+from ... import table
+from ...table import Row
+
+numpy_lt_1p5 = version.LooseVersion(np.__version__) < version.LooseVersion('1.5')
 
 # Dummy init of Table, DATA for pyflakes and to be sure test fixture is working
 Table = None
@@ -40,11 +43,13 @@ class TestRow():
             self._t = Table([a, b])
         return self._t
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_subclass(self):
         """Row is subclass of ndarray and Row"""
         c = Row(self.t, 2)
         assert isinstance(c, Row)
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_values(self):
         """Row accurately reflects table values and attributes"""
         table = self.t
@@ -60,6 +65,7 @@ class TestRow():
             row[2]
         assert str(row.dtype) == "[('a', '<i8'), ('b', '<i8')]"
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_ref(self):
         """Row is a reference into original table data"""
         table = self.t
@@ -68,6 +74,7 @@ class TestRow():
         if Table is not MaskedTable:
             assert table['a'][1] == 10
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_left_equal(self):
         """Compare a table row to the corresponding structured array row"""
         np_t = self.t._data.copy()
@@ -78,6 +85,7 @@ class TestRow():
             for row, np_row in zip(self.t, np_t):
                 assert np.all(row == np_row)
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_left_not_equal(self):
         """Compare a table row to the corresponding structured array row"""
         np_t = self.t._data.copy()
@@ -89,6 +97,7 @@ class TestRow():
             for row, np_row in zip(self.t, np_t):
                 assert np.all(row != np_row)
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_right_equal(self):
         """Test right equal"""
         np_t = self.t._data.copy()
@@ -99,6 +108,7 @@ class TestRow():
             for row, np_row in zip(self.t, np_t):
                 assert np.all(np_row == row)
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_convert_numpy_array(self):
         d = self.t[1]
 
@@ -117,6 +127,7 @@ class TestRow():
         with pytest.raises(ValueError):
             np_data = np.array(d, dtype=[('c', 'i8'), ('d', 'i8')])
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_format_row(self):
         """Test formatting row"""
         table = self.t

astropy/table/tests/test_table.py
@@ -1,8 +1,10 @@
-import pytest
+from distutils import version
 import numpy as np
 
-from astropy import table
+from ...tests.helper import pytest
+from ... import table
 
+numpy_lt_1p5 = version.LooseVersion(np.__version__) < version.LooseVersion('1.5')
 
 # Dummy init of Table, DATA for pyflakes and to be sure test fixture is working
 Table = None
@@ -560,6 +562,7 @@ class TestSort():
 @pytest.mark.usefixtures('set_global_Table')
 class TestIterator():
 
+    @pytest.mark.xfail('numpy_lt_1p5')
     def test_iterator(self):
         d = np.array([(2, 1),
                       (3, 6),