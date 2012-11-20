19e7b95b199b300a69a8f22e31a023984b46039c
Nadia Dencheva <nadia.dencheva@gmail.com>
2012-11-20 16:37:50 -0500

    use relative imports

--- astropy/models/__init__.py
+++ astropy/models/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import models
-import fitting
-import parameters
-import projections
-import rotations
+from . import models
+from . import fitting
+from . import parameters
+from . import projections
+from . import rotations
 
