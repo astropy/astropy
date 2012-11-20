afb43271023c8bd7a3aae31d3ae7963f03feee5c
Erik M. Bray <embray@stsci.edu>
2012-11-20 11:06:41 -0500

    Fixed up the astropy.fits.io package setup to support linking the compression module with a system version of CFITSIO.

--- astropy/io/fits/setup_package.py
+++ astropy/io/fits/setup_package.py
@@ -9,29 +9,39 @@ from astropy import setup_helpers
 
 
 def get_extensions():
-    if setup_helpers.get_compiler_option() != 'msvc':
-        # All of these switches are to silence warnings from compiling CFITSIO
-        extra_compile_args = ['-Wno-unused-variable', '-Wno-parentheses',
-                              '-Wno-uninitialized', '-Wno-format',
-                              '-Wno-strict-prototypes', '-Wno-unused',
-                              '-Wno-comments', '-Wno-switch']
-    else:
-        extra_compile_args = []
+    # 'numpy' will be replaced with the proper path to the numpy includes
+    include_dirs = ['numpy']
+    library_dirs = []
+    libraries = []
+    source_files = [os.path.relpath(fname) for fname in
+                    glob(os.path.join(os.path.dirname(__file__),
+                                      'src', '*.c'))]
+    extra_compile_args = []
 
-    cfitsio_path = os.path.join('cextern', 'cfitsio')
-    cfitsio_files = glob(os.path.join('cextern', 'cfitsio', '*.c'))
-    compression_module_files = [os.path.relpath(fname) for fname in
-                                glob(os.path.join(os.path.dirname(__file__),
-                                                  'src', '*.c'))]
+    if not setup_helpers.use_system_library('cfitsio'):
+        if setup_helpers.get_compiler_option() != 'msvc':
+            # All of these switches are to silence warnings from compiling CFITSIO
+            extra_compile_args = ['-Wno-unused-variable', '-Wno-parentheses',
+                                  '-Wno-uninitialized', '-Wno-format',
+                                  '-Wno-strict-prototypes', '-Wno-unused',
+                                  '-Wno-comments', '-Wno-switch']
+
+        cfitsio_path = os.path.join('cextern', 'cfitsio')
+        cfitsio_files = glob(os.path.join(cfitsio_path, '*.c'))
+        include_dirs.append(cfitsio_path)
+        source_files.extend(cfitsio_files)
+    else:
+        setup_helpers.pkg_config(['cfitsio'], ['cfitsio'], include_dirs,
+                                 library_dirs, libraries)
 
     return [
         Extension(
             'astropy.io.fits.compression',
-            cfitsio_files + compression_module_files,
-            # 'numpy' will be replaced with the proper path to the numpy
-            # includes
-            include_dirs=[cfitsio_path, 'numpy'],
-            extra_compile_args=extra_compile_args)
+            source_files,
+            include_dirs=include_dirs,
+            extra_compile_args=extra_compile_args,
+            libraries=libraries,
+            library_dirs=library_dirs)
     ]
 
 
@@ -39,6 +49,10 @@ def get_package_data():
     # Installs the testing data files
     return {
         'astropy.io.fits.tests': [os.path.join('data', '*.fits')]}
+
+
+def get_external_libraries():
+    return ['cfitsio']
 
 
 def get_legacy_alias():
