6004ec945f866151bb2c8ccd78fbc3a39c4a3068
Pey Lian Lim <lim@stsci.edu>
2013-01-16 14:10:44 -0500

    added new timer module

---
+++ astropy/utils/timer.py
@@ -0,0 +1,23 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""General purpose timer related functions."""
+
+from __future__ import absolute_import
+
+# STDLIB
+import time
+
+
+__all__ = ['func_timer']
+
+
+def func_timer(*args, **kwargs):
+    """Time a function.
+
+    Parameters
+    ----------
+    args
+
+    kwargs
+
+    """
+    pass
