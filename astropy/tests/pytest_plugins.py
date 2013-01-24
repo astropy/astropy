afdecb46fd9d50bafab5d2510ce8d44d4ef255f0
Michael Droettboom <mdboom@gmail.com>
2013-01-24 14:20:04 -0500

    Update to alternate implementation that uses lsof

--- astropy/tests/pytest_plugins.py
+++ astropy/tests/pytest_plugins.py
@@ -7,7 +7,6 @@ into conftest.py in the root directory.
 import io
 import os
 import sys
-import traceback
 
 from .helper import pytest
 
@@ -18,97 +17,90 @@ PY3K = sys.version_info[0] >= 3
 def pytest_addoption(parser):
     parser.addoption("--remote-data", action="store_true",
         help="run tests with online data")
+    parser.addoption("--open-files", action="store_true",
+        help="fail if any test leaves files open")
+
 
 # Open file detection.
 #
-# This works by replacing the built-in "open" with one that keeps
-# track of all of the currently open files.  When each test is started,
-# this list is copied.  At the end of the test, that list is compared
-# with the set of currently open files.  If the latter is larger, the
-# newly opened files that were not closed are displayed in an error
-# message and the test fails.
+# This works by calling out to lsof to get the list of open files held
+# by the process both before and after the test.  If something is
+# still open after the test that wasn't open before the test, an
+# AssertionError is raised.
 #
 # This is not thread-safe.  We're not currently running our tests
 # multi-threaded, but that is worth noting.
-#
-# Note that this doesn't work with Python 3.  Since Python 3 has a
-# hierarchy of classes for I/O written in C, it doesn't seem possible
-# to monkey-patch the base file class with our own in order to keep
-# track of opening and closing like we do here.  Further investigation
-# and perhaps a wholly different approach is needed.  In the meantime,
-# this functionality is just turned off for Python 3.
-if not PY3K:
-    import __builtin__
-    _open_files = dict()
-    _old_file = __builtin__.file
 
-    class _new_file(_old_file):
-        def __init__(self, *args, **kwargs):
-            self.x = args[0]
-            _old_file.__init__(self, *args, **kwargs)
-            __builtin__.open = _old_open
-            _open_files[self] = traceback.format_stack()
-            __builtin__.open = _new_open
+SUPPORTS_OPEN_FILE_DETECTION = (
+    sys.platform in ('linux2', 'darwin'))
 
-        def close(self):
-            _old_file.close(self)
-            try:
-                del _open_files[self]
-            except KeyError:
-                pass
 
-    _old_open = __builtin__.open
-    def _new_open(*args, **kwargs):
-        return _new_file(*args, **kwargs)
-    __builtin__.open = _new_open
-    __builtin__.file = _new_file
+def _get_open_file_list():
+    import subprocess
+    output = subprocess.check_output(
+        ['lsof -F0 -n -p {0}'.format(os.getpid())],
+        shell=True)
+    files = []
+    for line in output.split('\n'):
+        columns = line.split('\0')
+        mapping = {}
+        for column in columns:
+            if len(column) >= 2:
+                mapping[column[0]] = column[1:]
+        if (mapping.get('f') and
+            mapping.get('a', ' ') != ' ' and
+            mapping.get('t') == 'REG'):
+            files.append(mapping['n'])
+
+    return set(files)
 
 
 def pytest_runtest_setup(item):
     # Store a list of the currently opened files so we can compare
     # against them when the test is done.
-    if not PY3K:
-        item.open_files = dict(_open_files)
+    if SUPPORTS_OPEN_FILE_DETECTION and item.config.getvalue('open_files'):
+        item.open_files = _get_open_file_list()
 
     if ('remote_data' in item.keywords and
         not item.config.getvalue("remote_data")):
         pytest.skip("need --remote-data option to run")
 
 
-if not PY3K:
+if SUPPORTS_OPEN_FILE_DETECTION:
     def pytest_runtest_teardown(item, nextitem):
         # a "skipped" test will not have been called with
         # pytest_runtest_setup, so therefore won't have an
         # "open_files" member
-        if not hasattr(item, 'open_files'):
+        if (not item.config.getvalue('open_files') or
+            not hasattr(item, 'open_files')):
             return
 
         start_open_files = item.open_files
         del item.open_files
 
+        open_files = _get_open_file_list()
+
         # This works in tandem with the test_open_file_detection test to
         # ensure that it creates one extra open file.
         if item.name == 'test_open_file_detection':
-            assert len(start_open_files) + 1 == len(_open_files)
+            assert len(start_open_files) + 1 == len(open_files)
             return
 
-        if len(start_open_files) != len(_open_files):
-            not_closed = {}
-            for fd, stack in _open_files.items():
-                # astropy.log files are allowed to continue to exist
-                # between test runs
-                if os.path.basename(fd.name) == 'astropy.log':
-                    continue
+        not_closed = set()
+        for filename in open_files:
+            # astropy.log files are allowed to continue to exist
+            # between test runs
+            if os.path.basename(filename) == 'astropy.log':
+                continue
 
-                if fd not in start_open_files:
-                    not_closed[fd.name] = stack
+            if filename not in start_open_files:
+                not_closed.add(filename)
 
-            if len(not_closed):
-                msg = ['File(s) not closed:']
-                for name, stack in not_closed.items():
-                    msg.append('  {0} opened from:'.format(name))
-                    msg.extend('    {0}'.format(x) for x in stack)
-                raise AssertionError('\n'.join(msg))
+        if len(not_closed):
+            msg = ['File(s) not closed:']
+            for name in not_closed:
+                msg.append('  {0}'.format(name))
+            raise AssertionError('\n'.join(msg))
 
 
 def pytest_report_header(config):
--- astropy/tests/tests/test_open_file_detection.py
+++ astropy/tests/tests/test_open_file_detection.py
@@ -5,7 +5,6 @@ import pytest
 from ...utils.data import get_pkg_data_filename
 
 fd = None
-@pytest.mark.skipif("sys.version_info >= (3, 0)")
 def test_open_file_detection():
     global fd
     fd = open(get_pkg_data_filename('data/open_file_detection.txt'))
