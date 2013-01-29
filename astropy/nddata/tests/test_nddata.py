e118633e480529ca29006d07dff9685f2f07d3a5
Erik Tollerud <erik.tollerud@gmail.com>
2013-01-28 17:26:27 -0800

    Merge pull request #678 from eteq/nddata/nddata_arithmetic-2
    Miscellaneous tweaks/fixes for NDData

--- astropy/nddata/tests/test_nddata.py
+++ astropy/nddata/tests/test_nddata.py
@@ -1,13 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
 from ..nddata import NDData
 from ..nduncertainty import StdDevUncertainty, IncompatibleUncertaintiesException, NDUncertainty
 from ...tests.helper import pytest, raises
 from ...io import fits
-
-np.random.seed(12345)
+from ...utils import NumpyRNGContext
 
 
 class FakeUncertainty(NDUncertainty):
@@ -30,20 +30,23 @@ def test_nddata_empty():
         NDData()  # empty initializer should fail
 
 def test_nddata_simple():
-    nd = NDData(np.random.random((10, 10)))
+    with NumpyRNGContext(123):
+        nd = NDData(np.random.random((10, 10)))
     assert nd.shape == (10, 10)
     assert nd.size == 100
     assert nd.dtype == np.dtype(float)
 
 
 def test_nddata_mask_valid():
-    NDData(np.random.random((10, 10)), mask=np.random.random((10, 10)) > 0.5)
+    with NumpyRNGContext(456):
+        NDData(np.random.random((10, 10)), mask=np.random.random((10, 10)) > 0.5)
 
 
 @pytest.mark.parametrize(('shape'), [(10,), (5, 5), (3, 10, 10)])
 def test_nddata_mask_invalid_shape(shape):
     with pytest.raises(ValueError) as exc:
-        NDData(np.random.random((10, 10)), mask=np.random.random(shape) > 0.5)
+        with NumpyRNGContext(789):
+            NDData(np.random.random((10, 10)), mask=np.random.random(shape) > 0.5)
     assert exc.value.args[0] == 'dimensions of mask do not match data'
 
 
@@ -74,13 +77,14 @@ def test_nddata_uncertainty_invalid_type(uncertainty):
     assert exc.value.args[0] == 'Uncertainty must be an instance of a NDUncertainty object'
 
 
-def test_nddata_copy():
+def test_nddata_copy_ref():
+    """
+    Tests to ensure that creating a new NDData object copies by *reference*.
+    """
     a = np.ones((10, 10))
-    nd_copy = NDData(a, copy=True)
-    nd_ref = NDData(a, copy=False)
+    nd_ref = NDData(a)
     a[0, 0] = 0
     assert nd_ref.data[0, 0] == 0
-    assert nd_copy.data[0, 0] == 1
 
 
 def test_nddata_conversion():
@@ -105,8 +109,8 @@ def test_nddata_add_mismatch_wcs():
 
 
 def test_nddata_add_mismatch_units():
-    d1 = NDData(np.ones((5, 5)), units='Jy')
-    d2 = NDData(np.ones((5, 5)), units='erg/s')
+    d1 = NDData(np.ones((5, 5)), unit='Jy')
+    d2 = NDData(np.ones((5, 5)), unit='erg/s')
     with pytest.raises(ValueError) as exc:
         d1.add(d2)
     assert exc.value.args[0] == "operand units do not match"
@@ -127,7 +131,7 @@ def test_nddata_add_uncertainties():
     d2 = NDData(np.ones((5, 5)), uncertainty=u2)
     d3 = d1.add(d2)
     assert np.all(d3.data == 2.)
-    assert np.all(d3.uncertainty.array == np.sqrt(10.))
+    assert_array_equal(d3.uncertainty.array, np.sqrt(10.))
 
 
 def test_nddata_add_uncertainties_mismatch():
@@ -157,8 +161,8 @@ def test_nddata_subtract_mismatch_wcs():
 
 
 def test_nddata_subtract_mismatch_units():
-    d1 = NDData(np.ones((5, 5)), units='Jy')
-    d2 = NDData(np.ones((5, 5)) * 2., units='erg/s')
+    d1 = NDData(np.ones((5, 5)), unit='Jy')
+    d2 = NDData(np.ones((5, 5)) * 2., unit='erg/s')
     with pytest.raises(ValueError) as exc:
         d1.subtract(d2)
     assert exc.value.args[0] == "operand units do not match"
@@ -179,7 +183,7 @@ def test_nddata_subtract_uncertainties():
     d2 = NDData(np.ones((5, 5)) * 2., uncertainty=u2)
     d3 = d1.subtract(d2)
     assert np.all(d3.data == -1.)
-    assert np.all(d3.uncertainty.array == np.sqrt(10.))
+    assert_array_equal(d3.uncertainty.array, np.sqrt(10.))
 
 
 def test_nddata_subtract_uncertainties_mismatch():
@@ -193,16 +197,16 @@ def test_nddata_subtract_uncertainties_mismatch():
     assert exc.value.args[0] == 'Cannot propagate uncertainties of type StdDevUncertainty with uncertainties of type FakeUncertainty for subtraction'
 
 
-def test_convert_units_to():
+def test_convert_unit_to():
     d = NDData(np.ones((5, 5)))
-    d.units = 'km'
-    d1 = d.convert_units_to('m')
+    d.unit = 'km'
+    d1 = d.convert_unit_to('m')
     assert np.all(d1 == np.array(1000.0))
 
 
 @raises(ValueError)
 def test_invalid_unit():
-    d = NDData(np.ones((5, 5)), units="NotAValidUnit")
+    d = NDData(np.ones((5, 5)), unit="NotAValidUnit")
 
 def test_simple_slicing():
     u1 = StdDevUncertainty(array=np.ones((5, 5)) * 3)
@@ -222,7 +226,7 @@ def test_slicing_reference():
 
 def test_initializing_from_nddata():
     d1 = NDData(np.ones((5, 5)))
-    d2 = NDData(d1, copy=False)
+    d2 = NDData(d1)
 
     assert d1.data is d2.data
 
@@ -245,3 +249,22 @@ def test_meta2ordered_dict():
 def test_meta2ordered_dict_fail():
     hdr = 'this is not a valid header'
     d1 = NDData(np.ones((5, 5)), meta=hdr)
+
+def test_masked_array_input():
+
+    with NumpyRNGContext(12345):
+        a = np.random.randn(100)
+        marr = np.ma.masked_where(a > 0, a)
+
+    nd = NDData(marr)
+
+    #check that masks and data match
+    assert_array_equal(nd.mask, marr.mask)
+    assert_array_equal(nd.data, marr.data)
+
+    #check that they are both by reference
+    marr.mask[10] = ~marr.mask[10]
+    marr.data[11] = 123456789
+
+    assert_array_equal(nd.mask, marr.mask)
+    assert_array_equal(nd.data, marr.data)
