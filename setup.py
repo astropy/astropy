41d44092025648484a85c52242a25155f5ba6524
Erik M. Bray <embray@stsci.edu>
2013-01-29 20:10:31 -0500

    Back to development: 0.2.dev

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from astropy.version_helpers import get_git_devstr, generate_version_py
 NAME = 'astropy'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.2b2'
+VERSION = '0.2.dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
