cb4cfd861aa6a7430aa5cc3e050989ad0a90675b
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-01-31 08:18:46 +0100

    Use a better test for dimensionless of units in numeric conversions

--- astropy/units/quantity.py
+++ astropy/units/quantity.py
@@ -323,8 +323,7 @@ class Quantity(object):
         # In the following, we check that unit is not the dimensionless unit
         # (note that this is different from ``is_dimensionless``, which checks
         # if the decomposed - not current - units are dimensionless.
-        from . import dimensionless
-        if self.unit is not dimensionless and WARN_IMPLICIT_NUMERIC_CONVERSION():
+        if (self.unit.bases or [] and self.unit.scale != 1.) and WARN_IMPLICIT_NUMERIC_CONVERSION():
             log.warn("Converting Quantity object in units '{0}' to a Python scalar".format(self.unit))
         return float(self.value)
 
@@ -334,8 +333,7 @@ class Quantity(object):
         # In the following, we check that unit is not the dimensionless unit
         # (note that this is different from ``is_dimensionless``, which checks
         # if the decomposed - not current - units are dimensionless.
-        from . import dimensionless
-        if self.unit is not dimensionless and WARN_IMPLICIT_NUMERIC_CONVERSION():
+        if (self.unit.bases or [] and self.unit.scale != 1.) and WARN_IMPLICIT_NUMERIC_CONVERSION():
             log.warn("Converting Quantity object in units '{0}' to a Python scalar".format(self.unit))
         return int(self.value)
 
@@ -345,8 +343,7 @@ class Quantity(object):
         # In the following, we check that unit is not the dimensionless unit
         # (note that this is different from ``is_dimensionless``, which checks
         # if the decomposed - not current - units are dimensionless.
-        from . import dimensionless
-        if self.unit is not dimensionless and WARN_IMPLICIT_NUMERIC_CONVERSION():
+        if (self.unit.bases or [] and self.unit.scale != 1.) and WARN_IMPLICIT_NUMERIC_CONVERSION():
             log.warn("Converting Quantity object in units '{0}' to a Python scalar".format(self.unit))
         return long(self.value)
 
@@ -355,8 +352,7 @@ class Quantity(object):
         # In the following, we check that unit is not the dimensionless unit
         # (note that this is different from ``is_dimensionless``, which checks
         # if the decomposed - not current - units are dimensionless.
-        from . import dimensionless
-        if self.unit is not dimensionless and WARN_IMPLICIT_NUMERIC_CONVERSION():
+        if (self.unit.bases or [] and self.unit.scale != 1.) and WARN_IMPLICIT_NUMERIC_CONVERSION():
             log.warn("Converting Quantity object in units '{0}' to a Numpy array".format(self.unit))
         return np.array(self.value)
 
