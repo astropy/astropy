ac983a18100379f03b769f6908495f2598d67dc8
Erik Bray <erik.m.bray@gmail.com>
2013-02-14 13:53:24 -0800

    Merge pull request #782 from eteq/fix-failing-open-file-test
    Fix failing open file test

--- astropy/tests/pytest_plugins.py
+++ astropy/tests/pytest_plugins.py
@@ -40,10 +40,10 @@ def _get_open_file_list():
     import subprocess
     fsencoding = sys.getfilesystemencoding()
 
-    proc = subprocess.Popen(
+    sproc = subprocess.Popen(
         ['lsof -F0 -n -p {0}'.format(os.getpid())],
         shell=True, stdout=subprocess.PIPE)
-    output = process.communicate()[0].strip()
+    output = sproc.communicate()[0].strip()
     files = []
     for line in output.split(b'\n'):
         columns = line.split(b'\0')
