07132b788dc4fe94ddea961b667ec6d7ea83dc92
Erik Tollerud <erik.tollerud@gmail.com>
2013-02-18 20:11:07 -0500

    Add test of setting distance a second time on a Coordinate

---
+++ astropy/coordinates/tests/test_distance.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+from __future__ import print_function
+
+from numpy import testing as npt
+
+from ... import units as u
+
+
+"""
+This includes tests for distances/cartesian points that are *not* in the API
+tests.  Right now that's just regression tests.
+"""
+
+
+def test_distance_change():
+    from .. import RA, Dec, ICRSCoordinates, Distance
+
+    ra = RA("4:08:15.162342", unit=u.hour)
+    dec = Dec("-41:08:15.162342", unit=u.degree)
+    c = ICRSCoordinates(ra, dec)
+
+    c.distance = Distance(1, unit=u.kpc)
+
+    oldx = c.x
+    assert (oldx - 0.35284083171901953) < 1e-10
+
+    #now x should increase when the distance increases
+    c.distance = Distance(2, unit=u.kpc)
+
+    assert c.x == oldx * 2
