c416453c5984d04b03d5f8e4dfd13de966968850
Erik M. Bray <embray@stsci.edu>
2013-02-19 17:26:48 -0500

    Preparing release 0.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from astropy.version_helpers import get_git_devstr, generate_version_py
 NAME = 'astropy'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.2.dev'
+VERSION = '0.2'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
