a045deada69aaea68ee8dffec80432b34d0ced7f
Michael Droettboom <mdboom@gmail.com>
2013-03-29 14:15:03 -0700

    Merge pull request #912 from mdboom/setup/generate-config-file-error-message
    Fix a bug when generating the error message in `generate_config_file`

--- astropy/setup_helpers.py
+++ astropy/setup_helpers.py
@@ -575,7 +575,7 @@ def generate_default_config(build_lib, package):
     else:
         msg = ('Generation of default configuration item failed! Stdout '
                'and stderr are shown below.\n'
-               'Stdout:\n{stdout}\nStderr:\n{stderr}')
+               'Stdout:\n{stdout}\nStderr:\n{stderr}').decode('UTF-8')
         log.error(msg.format(stdout=stdout.decode('UTF-8'),
                              stderr=stderr.decode('UTF-8')))
 
