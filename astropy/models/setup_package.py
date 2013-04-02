78557ed864410eacd382ed8eb51cae0b2098b042
Nadia Dencheva <nadia.dencheva@gmail.com>
2013-04-02 08:52:45 -0400

    Added setup_package.py

---
+++ astropy/models/setup_package.py
@@ -0,0 +1,5 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+def get_package_data():
+    return {
+                    'astropy.models.tests' : ['data/*.fits', 'data/*.hdr']
+                    }
