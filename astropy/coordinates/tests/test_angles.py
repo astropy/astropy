fbb109d73614a8a2f420819ae2558fd113999352
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-04-02 21:59:20 +0200

    Fixed typo

--- astropy/coordinates/tests/test_angles.py
+++ astropy/coordinates/tests/test_angles.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Test initalization of angles not already covered by the API
+# Test initalization of angles not already covered by the API tests
 
 from ..angles import Angle
 from ...tests.compat import assert_allclose
