5fb6a681502304370e976c0ea67c83453e2058a3
Nadia Dencheva <nadia.dencheva@gmail.com>
2013-04-02 08:56:38 -0400

    fix formatting

--- astropy/models/setup_package.py
+++ astropy/models/setup_package.py
@@ -1,5 +1,3 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 def get_package_data():
-    return {
-                    'astropy.models.tests' : ['data/*.fits', 'data/*.hdr']
-                    }
+    return {'astropy.models.tests': ['data/*.fits']}
