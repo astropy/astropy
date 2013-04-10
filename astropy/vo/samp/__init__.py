74515fa9dc87ae6887ae78ab1de7f93414bc7714
Luigi Paioro <luigi@lambrate.inaf.it>
2013-04-10 10:13:16 +0200

    `SAMPy` code added within `astropy.vo.samp` package, and `sampy` script added with `scripts`

---
+++ astropy/vo/samp/__init__.py
@@ -0,0 +1,36 @@
+from core import SAMPHubServer
+from core import SAMPHubProxy
+from core import SAMPClient
+from core import SAMPHubError
+from core import SAMPClientError
+from core import SAMPProxyError
+from core import SAMPLog
+from core import SAMPIntegratedClient
+from core import SAMPIntegratedClient
+from core import SAMPMsgReplierWrapper
+from core import SAMP_STATUS_OK
+from core import SAMP_STATUS_WARNING
+from core import SAMP_STATUS_ERROR
+from core import SAMP_HUB_SINGLE_INSTANCE
+from core import SAMP_HUB_MULTIPLE_INSTANCE
+from core import SAMP_RESTRICT_GROUP
+from core import SAMP_RESTRICT_OWNER
+
+
+__all__ = ["SAMPHubServer",
+           "SAMPHubProxy",
+           "SAMPClient",
+           "SAMPHubError",
+           "SAMPClientError",
+           "SAMPProxyError",
+           "SAMPLog",
+           "SAMPIntegratedClient",
+           "SAMPMsgReplierWrapper",
+           "SAMP_STATUS_OK",
+           "SAMP_STATUS_WARNING",
+           "SAMP_STATUS_ERROR",
+           "SAMP_HUB_SINGLE_INSTANCE",
+           "SAMP_HUB_MULTIPLE_INSTANCE",
+           "SAMP_RESTRICT_GROUP",
+           "SAMP_RESTRICT_OWNER"]
+
