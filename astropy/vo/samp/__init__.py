7f718be0ae8c45fd2b6b10d47b2fe74aa873c5b3
Christoph Deil <Deil.Christoph@gmail.com>
2013-04-12 19:07:36 +0200

    Add docstring to `vo` and `vo.samp` packages

--- astropy/vo/samp/__init__.py
+++ astropy/vo/samp/__init__.py
@@ -1,4 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+The `vo.samp` subpackage provides classes to communicate with other
+applications via the Simple Application Messaging Protocal (SAMP)
+www.ivoa.net/samp
+"""
 from core import SAMPHubServer
 from core import SAMPHubProxy
 from core import SAMPClient
