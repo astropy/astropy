7bd73b23397b28209c2300159090c231d9044bd5
Christoph Deil <Deil.Christoph@gmail.com>
2013-04-12 19:02:49 +0200

    Add astropy license line to sampy files

--- astropy/vo/samp/__init__.py
+++ astropy/vo/samp/__init__.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 from core import SAMPHubServer
 from core import SAMPHubProxy
 from core import SAMPClient
--- astropy/vo/samp/core.py
+++ astropy/vo/samp/core.py
@@ -1,3 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
 #!/usr/bin/env python
 
 ###################################################################
