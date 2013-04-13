546e894857c2fc858f5e8e00e2efac2025949a9f
Christoph Deil <Deil.Christoph@gmail.com>
2013-04-13 10:27:57 +0200

    Add first unit test for vo.samp

---
+++ astropy/vo/samp/tests/test_core.py
@@ -0,0 +1,19 @@
+from ....tests.helper import pytest
+from ... import samp
+
+def test_SAMPHubServer():
+    """Test that samp classes can be instantiated"""
+    samp.SAMPHubServer()
+
+
+# TODO: Add tests for all classes and use cases
+"""
+    samp.SAMPHubProxy()
+    samp.SAMPClient()
+    samp.SAMPHubError()
+    samp.SAMPClientError()
+    samp.SAMPProxyError()
+    samp.SAMPLog()
+    samp.SAMPIntegratedClient()
+    samp.SAMPMsgReplierWrapper()
+"""
