91adcb872ac889271e76f1725980a3a321440e28
Tom Aldcroft <taldcroft@gmail.com>
2013-04-24 07:54:17 -0400

    Fix mistake in import name in table __init__

--- astropy/table/__init__.py
+++ astropy/table/__init__.py
@@ -1,6 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from .table import Column, Table, TableColumns, Row, MaskedColumn
-from .np_operations import TableMergeError
+from .np_utils import TableMergeError
 from .operations import join, hstack, vstack
 
 # Import routines that connect readers/writers to astropy.table
