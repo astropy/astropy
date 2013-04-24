7467881cc6aab857c9371cc24bd89b235de56c06
Steven Crawford <crawfordsm@gmail.com>
2013-04-24 22:16:51 +0200

    update of funcs to try to fix documentation issues

--- astropy/stats/funcs.py
+++ astropy/stats/funcs.py
@@ -11,7 +11,9 @@ from __future__ import division
 
 import numpy as np
 
-__all__ = ['sigma_clip', 'binom_conf_interval', 'binned_binom_proportion', 'median_absolute_deviation', 'biweight_location', 'biweight_midvariance']
+__all__ = ['sigma_clip', 'binom_conf_interval', 'binned_binom_proportion',
+           'median_absolute_deviation', 'biweight_location',
+           'biweight_midvariance']
 
 
 def sigma_clip(data, sig=3, iters=1, cenfunc=np.median, varfunc=np.var,
@@ -297,13 +299,13 @@ def binom_conf_interval(k, n, conf=0.68269, interval='wilson'):
 
         lowerbound = betaincinv(k + 0.5, n - k + 0.5, alpha / 2.)
         upperbound = betaincinv(k + 0.5, n - k + 0.5, 1. - alpha / 2.)
-    
-        # Set lower or upper bound to k/n when k/n = 0 or 1. 
+
+        # Set lower or upper bound to k/n when k/n = 0 or 1.
         lowerbound[k == 0] = 0.
         upperbound[k == n] = 1.
 
         return np.array([lowerbound, upperbound])
-    
+
     else:
         raise ValueError('Unrecognized interval: {0:s}'.format(interval))
 
@@ -486,11 +488,12 @@ def binned_binom_proportion(x, success, bins=10, range=None, conf=0.68269,
 
     return bin_ctr, bin_halfwidth, p, perr
 
+
 def median_absolute_deviation(a, axis=None):
     """Compute the median absolute deviation
 
     Returns the median absolute deviation  of the array elements.  The MAD is
-    defined as median(|a-median(a)|).
+    defined as median( |a - median (a)| ).
 
     Parameters
     ----------
@@ -578,14 +581,15 @@ def biweight_location(a, c=6.0, M=None):
     This will generate random variates from a Gaussian distribution and return
     the median absolute deviation for that distribution::
 
-        >>> from astropy.tools.alg import biweight_location
-        >>> from numpy.random import randn
-        >>> randvar = randn(10000)
-        >>> cbl = biweight_location(randvar)
+    >>> from astropy.tools.alg import biweight_location
+    >>> from numpy.random import randn
+    >>> randvar = randn(10000)
+    >>> cbl = biweight_location(randvar)
 
     See Also
     --------
     median absolute deviation, biweight_midvariance
+
     """
 
     a = np.array(a, copy=False)
@@ -619,10 +623,10 @@ def biweight_midvariance(a, c=9.0, M=None):
         C_{bl}= n^{1/2} \frac{[\Sigma_{|u_i|<1} (x_i-M)**2(1-u_i^2)^4]^{0.5}}
         {|\Sigma_{|u_i|<1} (1-u_i^2)(1-5u_i^2)|}
     where  u_i is given by::
-    ..math::a
+    ..math::
         u_{i} = \frac{(x_i-M)}{cMAD}
-    where MAD is the median absolute deviation.  For the midvariance  parameter, c is
-    typically uses a value of 9.0.
+    where MAD is the median absolute deviation.  For the midvariance
+    parameter, c is typically uses a value of 9.0.
 
     For more details, see Beers, Flynn, and Gebhardt, 1990, AJ, 100, 32B
 
@@ -646,14 +650,15 @@ def biweight_midvariance(a, c=9.0, M=None):
     This will generate random variates from a Gaussian distribution and return
     the median absolute deviation for that distribution::
 
-        >>> from astropy.tools.alg import biweight_midvariance
-        >>> from numpy.random import randn
-        >>> randvar = randn(10000)
-        >>> scl = biweight_midvariance(randvar)
+    >>> from astropy.tools.alg import biweight_midvariance
+    >>> from numpy.random import randn
+    >>> randvar = randn(10000)
+    >>> scl = biweight_midvariance(randvar)
 
     See Also
     --------
     median absolute deviation, biweight_location
+
     """
 
     a = np.array(a, copy=False)
@@ -672,5 +677,5 @@ def biweight_midvariance(a, c=9.0, M=None):
     #now remove the outlier points
     mask = np.abs(u) < 1
 
-    return n**0.5 * (d[mask] * d[mask] * (1 - u[mask])**4).sum()**0.5 \
-           / np.abs(((1 - u[mask]) * (1 - 5 * u[mask])).sum())
+    return n**0.5 * (d[mask] * d[mask] * (1 - u[mask])**4).sum()**0.5\
+        / np.abs(((1 - u[mask]) * (1 - 5 * u[mask])).sum())
