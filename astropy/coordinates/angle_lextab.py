ef839a124772a83ef15b9a16e96e20f3b8706e96
Erik M. Bray <embray@stsci.edu>
2013-05-19 16:21:12 -0400

    As a temporary solution for #1112, add the lex/parsetab modules to the repository so we don't have to worry about whether or not they get installed.  Take code for generating them out of the setup_package modules.  Later we can worry about details of how to generate these, (or at least make it easy to regenerate if any changes are made to the parsers).

---
+++ astropy/coordinates/angle_lextab.py
@@ -0,0 +1,9 @@
+# angle_lextab.py. This file automatically created by PLY (version 3.4). Don't edit!
+_tabversion   = '3.4'
+_lextokens    = {u'DEGREE': 1, u'HOUR': 1, u'SIMPLE_UNIT': 1, u'SIGN': 1, u'SECOND': 1, u'COLON': 1, u'UINT': 1, u'MINUTE': 1, u'UFLOAT': 1}
+_lexreflags   = 0
+_lexliterals  = ''
+_lexstateinfo = {'INITIAL': 'inclusive'}
+_lexstatere   = {'INITIAL': [(u'(?P<t_UFLOAT>((\\d+\\.\\d*)|(\\.\\d+))([eE][+-]?\\d+)?)|(?P<t_UINT>\\d+)|(?P<t_SIGN>[+-])|(?P<t_SIMPLE_UNIT>(arcminute)|(rad)|(arcsec)|(uas)|(arcsecond)|(mas)|(arcmin)|(radian))|(?P<t_MINUTE>m(in(ute(s)?)?)?|\u2032|\\\')|(?P<t_SECOND>s(ec(ond(s)?)?)?|\u2033|\\")|(?P<t_DEGREE>d(eg(ree(s)?)?)?|\xb0)|(?P<t_HOUR>hour(s)?|h(r)?|\u02b0)|(?P<t_COLON>:)', [None, (u't_UFLOAT', 'UFLOAT'), None, None, None, None, (u't_UINT', 'UINT'), (u't_SIGN', 'SIGN'), (None, 'SIMPLE_UNIT'), None, None, None, None, None, None, None, None, (None, 'MINUTE'), None, None, None, (None, 'SECOND'), None, None, None, (None, 'DEGREE'), None, None, None, (None, 'HOUR'), None, None, (None, 'COLON')])]}
+_lexstateignore = {'INITIAL': u' '}
+_lexstateerrorf = {'INITIAL': 't_error'}
