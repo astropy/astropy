df3bf4411cf3da4fddf976dddc2cd5a0b318169e
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-05-30 14:03:03 +0200

    PEP8 fixes in astropy/nddata/convolution/tests

--- astropy/nddata/convolution/tests/test_make_kernel.py
+++ astropy/nddata/convolution/tests/test_make_kernel.py
@@ -12,6 +12,7 @@ try:
 except ImportError:
     HAS_SCIPY = False
 
+
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_airy():
     """
@@ -19,7 +20,7 @@ def test_airy():
     Checks https://github.com/astropy/astropy/pull/939
     """
     k1 = make_kernel([3, 3], kernelwidth=0.5, kerneltype='airy')
-    ref = np.array([[ 0.06375119,  0.12992753,  0.06375119],
-                    [ 0.12992753,  0.22528514,  0.12992753],
-                    [ 0.06375119,  0.12992753,  0.06375119]])
+    ref = np.array([[0.06375119, 0.12992753, 0.06375119],
+                    [0.12992753, 0.22528514, 0.12992753],
+                    [0.06375119, 0.12992753, 0.06375119]])
     assert_allclose(k1, ref, rtol=0, atol=1e-7)
