25b417d166d985afda87a8598f9cabdf31a7b590
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2013-05-30 22:57:44 -0400

    PEP8 changes

--- astropy/time/tests/test_basic.py
+++ astropy/time/tests/test_basic.py
@@ -72,12 +72,12 @@ class TestBasic():
         """Test that Time objects holding arrays are properly subscriptable,
         set is_scalar as appropriate, and also subscript delta_ut1_utc, etc."""
 
-        t = Time(np.arange(50000,50010), format='mjd', scale='utc')
+        t = Time(np.arange(50000, 50010), format='mjd', scale='utc')
         t1 = t[3]
-        assert t1.is_scalar == True
+        assert t1.is_scalar is True
         assert np.all(t1._time.jd1 == np.array([t._time.jd1[3]]))
         t2 = t[4:6]
-        assert t2.is_scalar == False
+        assert t2.is_scalar is False
         assert np.all(t2._time.jd1 == t._time.jd1[4:6])
         t.delta_tdb_tt = np.arange(len(t))  # Explicitly set (not testing .tdb)
         t3 = t[4:6]
