af0fdf3a37296fe73075a297dbaaa6c341142f35
hamogu <moritz.guenther@gmx.de>
2013-06-03 17:54:16 -0400

    revert changes to avoid conflict with other PR

--- astropy/io/ascii/connect.py
+++ astropy/io/ascii/connect.py
@@ -39,13 +39,6 @@ def read_ipac(filename, **kwargs):
 
 io_registry.register_reader('ipac', Table, read_ipac)
 
-def write_ipac(table, filename, **kwargs):
-    from .ipac import Ipac
-    from .ui import write
-    write(table, filename, Writer=Ipac, **kwargs)
-
-io_registry.register_writer('ipac', Table, write_ipac)
-
 
 # CDS
 # ===
@@ -137,5 +130,3 @@ def is_rdb(origin, args, kwargs):
     return isinstance(args[0], basestring) and args[0].endswith('.rdb')
 
 io_registry.register_identifier('rdb', Table, is_rdb)
-
-
