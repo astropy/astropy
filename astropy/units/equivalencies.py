b52027329a5b74d9d029e72f02c59d05b0cebe7d
Adam Ginsburg <keflavich@gmail.com>
2013-06-20 12:24:54 -0500

    spectral equivalencies from multiple conventions (work in progress)

--- astropy/units/equivalencies.py
+++ astropy/units/equivalencies.py
@@ -79,3 +79,88 @@ def spectral_density(sunit, sfactor):
         (fnu, nufnu, converter_fnu_nufnu, iconverter_fnu_nufnu),
         (fla, lafla, converter_fla_lafla, iconverter_fla_lafla),
     ]
+
+def radio_velocity(rest):
+    """
+    Return the equivalency pairs for the radio convention for velocity:
+
+    http://www.gb.nrao.edu/~fghigo/gbtdoc/doppler.html
+    Radio   :math:`V = c \frac{f_0 - f}{f_0}  ;  f(V) = f_0 ( 1 - V/c )`
+
+    Parameters
+    ----------
+    rest : Quantity
+        Any quantity supported by the standard spectral equivalencies
+        (wavelength, energy, frequency)
+
+    Example
+    -------
+    >>> CO_restfreq = 115.27120*u.GHz  # rest frequency of 12 CO 1-0 in GHz
+    >>> radio_CO_equiv = radio_velocity(CO_restfreq)
+    >>> measured_freq = 115.2832*u.GHz
+    >>> radio_velocity = measured_freq.to(u.km/u.s, equivalencies=radio_CO_equiv)
+    >>> print radio_velocity
+    -31.2090920889 km / s
+    """
+    restfreq = rest.to(si.Hz, spectral())
+
+    return [(si.Hz, si.km/si.s,
+            lambda x: (restfreq.value-x) / restfreq.value * _si.c.to('km/s').value,
+            lambda x: (1-x/_si.c.to('km/s').value) * restfreq)]
+
+def optical_velocity(rest):
+    """
+    Return the equivalency pairs for the optical convention for velocity:
+
+    http://www.gb.nrao.edu/~fghigo/gbtdoc/doppler.html
+    Optical       :math:`V = c \frac{f_0 - f}{f  }  ;  f(V) = f_0 ( 1 + V/c )^{-1}`
+
+    Parameters
+    ----------
+    rest : Quantity
+        Any quantity supported by the standard spectral equivalencies
+        (wavelength, energy, frequency)
+
+    Example
+    -------
+    >>> CO_restfreq = 115.27120*u.GHz  # rest frequency of 12 CO 1-0 in GHz
+    >>> optical_CO_equiv = optical_velocity(CO_restfreq)
+    >>> measured_freq = 115.2832*u.GHz
+    >>> optical_velocity = measured_freq.to(u.km/u.s, equivalencies=optical_CO_equiv)
+    >>> print optical_velocity
+    -31.205843488 km / s
+    """
+    restfreq = rest.to(si.Hz, spectral())
+
+    return [(si.Hz, si.km/si.s,
+            lambda x: (restfreq.value-x) / x * _si.c.to('km/s').value,
+            lambda x: (1+x/_si.c.to('km/s').value)**(-1) * restfreq)]
+
+def relativistic_velocity(rest):
+    """
+    Return the equivalency pairs for the relativistic convention for velocity:
+
+    http://www.gb.nrao.edu/~fghigo/gbtdoc/doppler.html
+    Relativistic  :math:`V = c \frac{f_0^2 - f^2}{f_0^2 + f^2} ;  f(V) = f_0 \frac{\left(1 - (V/c)^2\right)^{1/2}}{(1+V/c)}`
+
+    Parameters
+    ----------
+    rest : Quantity
+        Any quantity supported by the standard spectral equivalencies
+        (wavelength, energy, frequency)
+
+    Example
+    -------
+    >>> CO_restfreq = 115.27120*u.GHz  # rest frequency of 12 CO 1-0 in GHz
+    >>> optical_CO_equiv = optical_velocity(CO_restfreq)
+    >>> measured_freq = 115.2832*u.GHz
+    >>> optical_velocity = measured_freq.to(u.km/u.s, equivalencies=optical_CO_equiv)
+    >>> print optical_velocity
+    -31.205843488 km / s
+    """
+    restfreq = rest.to(si.Hz, spectral())
+
+    # NOT DONE
+    return [(si.Hz, si.km/si.s,
+            lambda x: (restfreq.value-x) / x * _si.c.to('km/s').value,
+            lambda x: (1+x/_si.c.to('km/s').value)**(-1) * restfreq)]
