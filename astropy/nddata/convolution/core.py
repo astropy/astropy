2fd2eb01e0ea2b841ecf8766670b0cd3565576d9
Axel Donath <donath@stud.uni-heidelberg.de>
2013-07-04 15:46:09 +0200

    WIP

--- astropy/nddata/convolution/core.py
+++ astropy/nddata/convolution/core.py
@@ -1,8 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 This module contains the convolution and filter functionalities of astropy.
-It is in preliminary state. It tries to combine the concepts of astropy models
-and PSF classes.
+It is in preliminary state.
 
 A few conceptual notes:
 A filter kernel is mainly characterized by its response function. In the 1D
@@ -11,71 +10,54 @@ case we speak of "impulse response function", in the 2D case we call it
 by an astropy ParametricModel, which is evaluated on a grid to obtain a filter
 mask, which can then be applied to binned data.
 
-The model is centered on the mask and has an amplitude such that the mask
-integrate to ones per default.
+The model is centered on the mask and should have an amplitude such that the mask
+integrates to one per default.
 """
 
 import abc
-import numpy as np
 
-from .utils import NormalizationError
-from ...modeling.core import Parametric1DModel, Parametric2DModel
+import numpy as np
+import copy
 
 
 class Kernel(object):
     """
     Abstract convolution kernel class
     """
-
     __metaclass__ = abc.ABCMeta
+    _odd = True
+    _separable = False
+    _weighted = False
+    _model = None
 
-    def __init__(self):
-        self._mask
-        self._normalized
-        self._normalization
-        self._separable
-        self._model
-        self._maskfft
-        self.truncation = 1E-5  # Lower limit at which the mask is truncated
-        self.pixsize
-        # This attributes and functions could be useful
-        # self.transfer_function
-        # self.symmetric
-        # self.antisymmetric
+    def __init__(self, mask):
+        self._mask = mask
+        self._normalization = 1. / self._mask.sum()
+        self._truncation = 1. - 1. / self._normalization
 
     @property
     def truncation(self):
         """
-        Lower limit at which the mask is truncated
+        Difference of sum(mask) to 1.
         """
         return self._truncation
-
-    @truncation.setter
-    def truncation(self, value):
-        """
-        Lower limit at which the mask is truncated
-        """
-        self._truncation = value
-        # Note: This is preliminary because it can create "holes" in the mask,
-        # if the mask contains smaller values than truncation, not only at the edges.
-        # A correct way would be to measure the containment fraction and assure
-        # that it is > 1 - truncation.
-        self._greater_truncation = np.abs(self._mask) > self._truncation
 
     @property
     def weighted(self):
         """
-        Indicates if kernel is weighted
+        Indicates if kernel is weighted.
+
+        If the kernel is not weighted the multiplication in the convolution will
+        be omitted, to increase the performance.
         """
-        if getattr(self, "_weighted", False):
-            return self._weighted
-        else:
-            # Check whether mask contains only ones and zeros
-            ones = self.mask == 1
-            zeros = self.mask == 0
-            weighted = not np.all(np.logical_or(ones, zeros))
-            setattr(self, "_weighted", weighted)
-            return weighted
+        return self._weighted
+
+    @property
+    def model(self):
+        """
+        Kernel response model.
+        """
+        return self._model
 
     @property
     def dimension(self):
@@ -89,57 +71,28 @@ class Kernel(object):
         """
         Check if kernel size is odd in all axes.
         """
-        odd = [True for axes_size in self.mask.shape if axes_size % 2 != 0]
-        return np.all(odd)
+        return self._odd
 
     @property
     def center(self):
         """
         Index of the kernels center.
         """
-        return [axes_size / 2 for axes_size in self.mask.shape]
+        return [axes_size / 2 for axes_size in self._mask.shape]
 
     @property
     def normalization(self):
         """
         Kernel normalization factor
         """
-        self._normalization = self._mask.sum()
         return self._normalization
-
-    @property
-    def normalized(self):
-        """
-        Check if the kernel mask is normalized.
-        """
-        if self._normalization > (1 - self._truncation):
-            self._normalized = True
-        else:
-            self._normalized = False
-        return self._normalized
-
-    @normalized.setter
-    def normalized(self, value):
-        """
-        Set kernel normalization.
-        """
-        if abs(self._normalization) < self._truncation:
-            raise NormalizationError("Can't normalize {0} filter kernel"
-                                     .format(__class__.__name__))
-        if value and not self._normalized:
-            self.mask = self.mask / self._normalization
-            self._normalized = value
-        elif value and self._normalized:
-            raise NormalizationError("Kernel is already normalized")
-        elif not value and self._normalized:
-            raise NormalizationError("Can't undo normalization")
 
     @property
     def shape(self):
         """
         Shape of the kernel mask.
         """
-        return self.mask.shape
+        return self._mask.shape
 
     @property
     def separable(self):
@@ -161,64 +114,29 @@ class Kernel(object):
         """
         Filter kernel mask.
         """
-        return self._mask[self._greater_truncat]
-
-    @property
-    def model(self):
-        """
-        Kernel response model.
-        """
-        return self._model
-
-    @abc.abstractmethod
-    @model.setter
-    def model(self):
-        """
-        Evaluate kernel response model.
-
-        I a kernel has no model it should be None.
-        """
-        return NotImplementedError("Subclasses should implement this")
-
-    @mask.setter
-    def mask(self, array):
-        """
-        Filter kernel mask.
-        """
-        if isinstance(array, np.array):
-            self._mask = array
-        elif isinstance(array, list):
-            self._mask = np.array(list)
-        else:
-            raise TypeError("Must be list or array.")
-
-    def _init_mask(self):
-        """
-        Initialize mask from response function.
-        """
-        pass
+        return self._mask
 
     def __add__(self, kernel):
         """
         Add two filter kernels and do a renormalization.
         """
         # As convolution is linear we can add two kernels
-        if self.mask.shape == kernel.mask.shape:
-            self.mask = self.mask + kernel.mask
-        else:
-            pass  # new_shape = np.maximum(self.mask.shape, kernel.mask.shape)
-        self.normalized = True
+        # This will fail, if the kernel shapes don't match
+        add_kernel = Kernel1D(self._mask + kernel._mask)
+        add_kernel._separable = self._separable and kernel._separable
+        add_kernel._weighted = self._weighted or kernel._weighted
+        return add_kernel
 
     def __sub__(self,  kernel):
         """
         Subtract two filter kernels and do a renormalization.
         """
         # As convolution is linear we can subtract two kernels
-        if self.mask.shape == kernel.mask.shape:
-            self.mask = self.mask - kernel.mask
-        else:
-            pass
-        self.normalized = True
+        # This will fail, if the kernel shapes don't match
+        sub_kernel = Kernel1D(self._mask - kernel._mask)
+        sub_kernel._separable = self._separable and kernel._separable
+        sub_kernel._weighted = self._weighted or kernel._weighted
+        return sub_kernel
 
     def __mul__(self, value):
         """
@@ -226,7 +144,25 @@ class Kernel(object):
         """
         # As convolution is linear we can multiply with a scalar
         if isinstance(value, (int, float)):
-            self._mask = self._normalisation * value
+            kernel = copy.copy(self)
+            kernel._normalization *= value
+            return kernel
+        elif isinstance(value, Kernel):
+            #Convolve the two kernels with each other
+            pass
+        else:
+            raise TypeError("Unsupported operand type(s) for *: '{}' and '{}'"
+                            .format(self.__class__, type(value)))
+
+    def __rmul__(self, value):
+        """
+        Multiply kernel with number or convolve two kernels.
+        """
+        # As convolution is linear we can multiply with a scalar
+        if isinstance(value, (int, float)):
+            kernel = copy.copy(self)
+            kernel._normalization *= value
+            return kernel
         elif isinstance(value, Kernel):
             #Convolve the two kernels with each other
             pass
@@ -237,19 +173,11 @@ class Kernel(object):
 
 class Kernel1D(Kernel):
     """
-    Abstract base class for 1D filter kernels
+    Base class for 1D filter kernels
     """
-    def __init__(self):
+    def __init__(self, mask):
         self.axes = 0
-
-    @model.setter
-    def model(self, model):
-        """
-        """
-        if isinstance(model, Parametric1DModel):
-            self._model = model
-        else:
-            TypeError("Must be Parametric1DModel")
+        super(Kernel1D, self).__init__(mask)
 
 
 class Kernel2D(Kernel):
@@ -257,11 +185,5 @@ class Kernel2D(Kernel):
     Abstract base class for 1D filter kernels
     """
     def __init__(self):
-        pass
+        super(Kernel2D, self).__init__()
 
-    @model.setter
-    def model(self, model):
-        if isinstance(model, Parametric2DModel):
-            self._model = model
-        else:
-            TypeError("Must be Parametric2DModel")
--- astropy/nddata/convolution/kernels.py
+++ astropy/nddata/convolution/kernels.py
@@ -1,21 +1,13 @@
-from .core import Kernel1D, Kernel2D
-from .utils import KernelSizeError
+from .core import Kernel1D, Kernel2D, Kernel
+from .utils import KernelSizeError, NormalizationError
 
-from ...modeling.models import Gaussian1DModel, Box1DModel
-from ...modeling.models import Disk2DModel, Box2DModel
+from ...modeling.models import Gaussian1DModel
+from ...modeling.core import ParametricModel  # Box1DModel
+#from ...modeling.models import Disk2DModel, Box2DModel
 
 import numpy as np
 
-
-class Laplace1DKernel(Kernel1D):
-    """
-    Laplace filter kernel.
-
-    The Laplace filter is a derivative filter. It is useful for e.g. detection
-    of fast changing structures in the data e.g edges.
-    """
-    def __init__(self):
-        self._separable = False
+__all__ = ['GaussianKernel', 'CustomKernel']
 
 
 class GaussianKernel(Kernel1D):
@@ -25,37 +17,16 @@ class GaussianKernel(Kernel1D):
     The Gaussian filter is a filter with great smoothing properties. It is
     isotropic and does not produce artifact.
     """
+    _separable = True
+    _weighted = True
+
     def __init__(self, width):
-        self._separable = True
-        self._normalized = True
-        self._weighted = True
         amplitude = 1. / (np.sqrt(2 * np.pi) * width)
-        self.model = Gaussian1DModel(amplitude=amplitude, mean=0.,
+        self._model = Gaussian1DModel(amplitude=amplitude, mean=0.,
                                                  stddev=width)
-
-
-class Binomial1DKernel(Kernel1D):
-    """
-    Binomial filter kernel
-    """
-    def __init__(self, size):
-        self._separable = True
-        self._normalized = True
-        self._weighted = True
-        self.model = None
-        self.mask = np.array([1, 2, 1])
-
-    def _init_mask(self, size):
-        """
-        Init binomial filter mask
-        """
-        if size % 2 == 0:
-            from scipy.special import binom
-            mask = np.empty(size)
-            for i in range(size):
-                mask[i] = binom(size, i)
-        else:
-            raise KernelSizeError("Size must be odd.")
+        width = np.ceil(width)
+        x = np.arange(-4 * width, 4 * width + 1)
+        super(GaussianKernel, self).__init__(self.model(x))
 
 
 class BoxKernel(Kernel1D):
@@ -66,13 +37,12 @@ class BoxKernel(Kernel1D):
     and can produce artifact, when applied repeatedly to the same data. It is
     faster than a Gaussian smoothing filter.
     """
+    _separable = True
+
     def __init__(self, width):
-        self._separable = True
-        self._normalized = True
-        self._weighted = False
         amplitude = 1. / width
-        self.model = Box1DModel(amplitude=amplitude, mean=0.,
-                                                 stddev=width)
+        #self._model = Box1DModel(amplitude=amplitude, mean=0.,
+        #                                       stddev=width)
 
 
 class Tophat2DKernel(Kernel2D):
@@ -82,12 +52,10 @@ class Tophat2DKernel(Kernel2D):
     The Tophat filter is an isotropic smoothing filter. It can produce artifact,
     when applied repeatedly on the same data.
     """
+
     def __init__(self, radius):
-        self._separable = False
-        self._normalized = True
-        self._weighted = False
         amplitude = 1. / (np.pi * radius ** 2)
-        self.model = Disk2DModel(amplitude=amplitude, mean=0., radius=radius)
+        #self.model = Disk2DModel(amplitude=amplitude, mean=0., radius=radius)
 
 
 class Ring2DKernel(Kernel2D):
@@ -98,21 +66,20 @@ class Ring2DKernel(Kernel2D):
     different width. It is useful for e.g background estimation.
     """
     def __init__(self, radius_in, radius_out):
-        self._separable = False
-        self._normalized = True
-        self._weighted = False
-        self.model = (Disk2DModel(amplitude=1., mean=0., radius=radius_out)
-                                 - Disk2DModel(amplitude=1., mean=0., radius=radius_in))
+        pass
+        #self.model = (Disk2DModel(amplitude=1., mean=0., radius=radius_out)
+        #                     - Disk2DModel(amplitude=1., mean=0., radius=radius_in))
 
 
 class Trapezoid1DKernel(Kernel1D):
     """
     1D trapezoid kernel.
     """
+    _weighted = True
+
     def __init__(self):
-        self._separable = False
-        self._weighted = True
-        self.model = Trapezoid1DModel()
+        pass
+        #self.model = Trapezoid1DModel()
 
 
 class MexicanHat1DKernel(Kernel1D):
@@ -123,11 +90,11 @@ class MexicanHat1DKernel(Kernel1D):
     filter. It does not conserve the mean. It is useful for peak or multi-scale
     detection.
     """
+    _weighted = True
+
     def __init__(self):
-        self._separable = False
-        self._normalized = False
-        self._weighted = True
-        self.model = MexicanHat1DModel()
+        pass
+        #self.model = MexicanHat1DModel()
 
 
 class MexicanHat2DKernel(Kernel2D):
@@ -139,40 +106,89 @@ class MexicanHat2DKernel(Kernel2D):
     detection.
     """
     def __init__(self):
-        self._separable = False
-        self._normalized = False
-        self.model = MexicanHat2DModel()
+        pass
+        #self.model = MexicanHat2DModel()
 
 
 class Airy2DKernel(Kernel2D):
     def __init__(self):
         self._separable = False
-        self.model = Airy2DModel()
+        #self.model = Airy2DModel()
 
 
 class Model1DKernel(Kernel1D):
     """
-    Initialize kernel from astropy ParametricModel
+    Initialize kernel from astropy Parametric1DModel
     """
-    def __init__(self):
+    def __init__(self, model):
         self._separable = False
+        if isinstance(model, ParametricModel):
+            self._model = model
+        else:
+            raise TypeError("Must be Parametric1DModel")
+
+
+class Model2DKernel(Kernel2D):
+    """
+    Initialize kernel from astropy Parametric2DModel
+    """
+    def __init__(self, model):
+        self._separable = False
+        if isinstance(model, ParametricModel):
+            self._model = model
+        else:
+            raise TypeError("Must be Parametric2DModel")
 
 
 class PSFKernel(Kernel2D):
     """
-    Initialize filter kernel from astropy PSF model.
+    Initialize filter kernel from astropy PSF instance.
     """
+    _separable = False
+
     def __init__(self):
-        self._separable = False
+        raise NotImplementedError('Not yet implemented')
 
 
-class Custom1DKernel(Kernel1D):
+class CustomKernel(Kernel):
     """
-    Initialize filter kernel from mask.
+    Initialize filter custom kernel from mask.
     """
+    
     def __init__(self, mask):
-        self._separable = False
-        self.model = None
-        self._mask = np.array(mask)
+        # Pass 'None' because mask is overridden in the next line
+        super(GaussianKernel, self).__init__(None)
+        self.mask = mask
+
+    @property
+    def mask(self):
+        """
+        Filter kernel mask.
+        """
+        return self._mask
+
+    @mask.setter
+    def mask(self, mask):
+        """
+        Filter kernel mask setter
+        """
+        if isinstance(mask, np.ndarray):
+            self._mask = mask
+        elif isinstance(mask, list):
+            self._mask = np.array(mask)
+        else:
+            raise TypeError("Must be list or array.")
+
+        #Check if mask is odd in all axis
+        self._odd = np.all([axes_size % 2 != 0 for axes_size in self.shape])
+
         if not self.odd:
-            raise KernelSizeError("Mask must be of odd size.")
+            raise KernelSizeError("Kernel size must be odd.")
+
+        # Check if mask is weighted
+        ones = self._mask == 1.
+        zeros = self._mask == 0
+        self._weighted = not np.all(np.logical_or(ones, zeros))
+
+        # Set normalization
+        self._normalization = 1. / self._mask.sum()
