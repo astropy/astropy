4f3e5b4dd9f09f96ee2fcadd96edd7d09607ae59
Tom Aldcroft <taldcroft@gmail.com>
2013-07-07 13:09:30 -0400

    Add FunctionColumn mixin

--- astropy/table/mixins.py
+++ astropy/table/mixins.py
@@ -1,3 +1,88 @@
+from .pprint import _pformat_col
+
+
+class FunctionColumn(object):
+    """
+    description : str or None
+        Full description of column
+    units : str or None
+        Physical units
+    format : str or None or function
+        Format string for outputting column values.  This can be an
+        "old-style" (``format % value``) or "new-style" (`str.format`)
+        format specification string or a function that accepts a single
+        value and returns a string.
+    meta : dict-like or None
+        Meta-data associated with the column
+    """
+    def __init__(self, func, col_names, name=None, description=None, units=None, format=None):
+        self._col_dependencies = col_names
+        self.func = func
+        self.col_names = col_names
+        self.name = name
+        self.__print_format__ = format
+        self.description = description
+        self.units = units
+
+    @property
+    def shape(self):
+        return (len(self.parent_table),)  # FIX ME
+
+    @property
+    def data(self):
+        col_datas = [self.parent_table[name].data for name in self.col_names]
+        return self.func(*col_datas)
+
+    def __len__(self):
+        return len(self.parent_table)
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def copy(self, data=None, copy_data=False):
+        print 'Calling copy on id={0}'.format(id(self))
+        # FIX ME
+        self_copy = FunctionColumn(self.func, self.col_names, name=self.name,
+                                   description=self.description, units=self.units)
+        return self_copy
+
+    def __str__(self):
+        lines, n_header = _pformat_col(self)
+        return '\n'.join(lines)
+
+    @property
+    def format(self):
+        return self.__print_format__
+
+    @format.setter
+    def format(self, value):
+        self.__print_format__ = value
+
+    def __table_replicate__(self, table):
+        """
+        Replicate the current column but using a new ``table`` (Table object).
+        """
+        new_col = self.copy()
+        new_col.parent_table = table
+        return new_col
+
+    def __table_add_column__(self, table, index):
+        from .table import TableColumns
+        print 'Here in table add column'
+
+        self.parent_table = table
+        columns = TableColumns()
+
+        for i_column, column in enumerate(table.columns.values()):
+            if i_column == index:
+                columns[self.name] = self
+            columns[column.name] = column
+        else:
+            columns[self.name] = self
+
+        table.columns = columns
+
+
 class ViewColumn(object):
     def __init__(self, col, data=None, name=None):
         from .table import BaseColumn
