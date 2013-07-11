702dbafbaf75f5ec8de8b5f6044896908d116558
Alex Rudy <alex.rudy@gmail.com>
2013-07-11 13:37:54 -0700

    Fixing console progressbar without multiprocessing return value

--- astropy/utils/console.py
+++ astropy/utils/console.py
@@ -546,7 +546,7 @@ class ProgressBar(object):
             steps = max(int(float(len(items)) / step_size), 1)
             if not multiprocess:
                 for i, item in enumerate(items):
-                    function(item)
+                    results.append(function(item))
                     if (i % steps) == 0:
                         bar.update(i)
             else:
