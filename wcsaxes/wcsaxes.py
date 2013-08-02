ff4fdeb93dd779d8c5ee4f7ccec2e88331341f02
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-08-02 18:22:37 +0200

    Define SkyCoordinate and SkyCoordinates

--- wcsaxes/wcsaxes.py
+++ wcsaxes/wcsaxes.py
@@ -29,38 +29,94 @@ class AngleFormatter(Formatter):
             return []
 
 
+class SkyCoordinate(object):
+
+    def __init__(self):
+        self.grid_locator = angle_helper.LocatorDMS(12)
+        self.tick_formatter = AngleFormatter(precision=1)
+
+    def set_major_formatter(self, format):
+        raise NotImplementedError()
+
+    def set_ticks():
+        raise NotImplementedError()
+
+
+class ScalarCoordinate(object):
+
+    def __init__(self):
+        pass
+
+    def set_major_formatter(self, format):
+        raise NotImplementedError()
+
+    def set_ticks():
+        raise NotImplementedError()
+
+
+class SkyCoordinates(object):
+
+    def __init__(self, wcs):
+
+        self._wcs = wcs
+
+        # The following code was taken from the demo_floating_axis example in
+        # Matplotlib. We make use of helpers in the mpl_toolkits package.
+
+        self._extreme_finder = angle_helper.ExtremeFinderCycle(20, 20,
+                                                               lon_cycle = 360,
+                                                               lat_cycle = None,
+                                                               lon_minmax = (-180., 180),
+                                                               lat_minmax = (-90., 90.)
+                                                               )
+
+        # Set up coordinates
+        self._coords = {}
+        self._coords[0] = SkyCoordinate()
+        self._coords[1] = SkyCoordinate()
+
+        # Set up aliases for coordinates
+        name_1 = self._wcs.wcs.ctype[0][:4]
+        self._coords[name_1] = self._coords[0]
+        name_2 = self._wcs.wcs.ctype[1][:4]
+        self._coords[name_2] = self._coords[1]
+
+        # Set up transform
+        self._transform = WCSWorld2PixelTransform(self._wcs)
+
+    @property
+    def grid_helper(self):
+        return GridHelperCurveLinear(self._transform,
+                                     extreme_finder=self._extreme_finder,
+                                     grid_locator1=self[0].grid_locator,
+                                     grid_locator2=self[1].grid_locator,
+                                     tick_formatter1=self[0].tick_formatter,
+                                     tick_formatter2=self[1].tick_formatter
+                                     )
+
+    def __getitem__(self, item):
+        return self._coords[item]
+
+
+class ScalarCoordinates(object):
+
+    def __init__(self, ):
+        raise NotImplementedError()
+
+    def __getitem__(self, item):
+        raise NotImplementedError()
+
+
 class WCSAxes(Axes):
 
     def __init__(self, fig, rect, wcs=None, adjustable='box'):
 
         self.wcs = wcs
 
-        # The following code was taken from the demo_floating_axis example in
-        # Matplotlib. We make use of helpers in the mpl_toolkits package.
+        # For now, assume WCS is Sky WCS
+        self.coords = SkyCoordinates(self.wcs)
 
-        extreme_finder = angle_helper.ExtremeFinderCycle(20, 20,
-                                                 lon_cycle = 360,
-                                                 lat_cycle = None,
-                                                 lon_minmax = (-180., 180),
-                                                 lat_minmax = (-90., 90.)
-                                                 )
-
-        grid_locator1 = angle_helper.LocatorDMS(12)
-        grid_locator2 = angle_helper.LocatorDMS(12)
-
-        tick_formatter1 = AngleFormatter(precision=1)
-        tick_formatter2 = AngleFormatter(precision=1)
-
-        transform = WCSWorld2PixelTransform(self.wcs)
-
-        grid_helper = GridHelperCurveLinear(transform,
-                                            extreme_finder=extreme_finder,
-                                            grid_locator1=grid_locator1,
-                                            tick_formatter1=tick_formatter1,
-                                            tick_formatter2=tick_formatter2
-                                            )
-
-        Axes.__init__(self, fig, rect, adjustable=adjustable, grid_helper=grid_helper)
+        Axes.__init__(self, fig, rect, adjustable=adjustable, grid_helper=self.coords.grid_helper)
 
     def get_transform(self, frame, equinox=None, obstime=None):
 
