c75079b73b882d0d23066616d5b477a2c4cf583b
Thomas Robitaille <thomas.robitaille@gmail.com>
2013-08-04 19:41:31 +0200

    Implement basic formatter, but requires changes to astropy.coordinates to function properly for all cases.

--- wcsaxes/coordinate_helpers.py
+++ wcsaxes/coordinate_helpers.py
@@ -1,5 +1,6 @@
 import abc
 
+from matplotlib.ticker import Formatter
 from mpl_toolkits.axisartist import angle_helper
 
 from .formatters import AngleFormatter
@@ -19,7 +20,7 @@ class BaseCoordinateHelper(object):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def set_ticklabel_position(self):
+    def set_ticklabel_position(self, position):
         """
         Set the axes on which the ticklabels for this coordinate should
         appear. Should be a string containing zero or more of ``b``, ``t``,
@@ -28,7 +29,7 @@ class BaseCoordinateHelper(object):
         raise NotImplementedError()
 
     @abc.abstractmethod
-    def set_major_formatter(self):
+    def set_major_formatter(self, formatter):
         """
         Set the major formatter for the ticks - should be either a
         ``Formatter`` instance for world coordinates, or a string such as
@@ -62,17 +63,19 @@ class SkyCoordinateHelper(BaseCoordinateHelper):
         self.grid_locator = angle_helper.LocatorDMS(4)
         self.tick_formatter = AngleFormatter(precision=1)
 
-    def set_formatter_precision(self, precision):
-        self.tick_formatter.precision = precision
-
     def set_ticks_position(self):
         raise NotImplementedError()
 
     def set_ticklabel_position(self):
         raise NotImplementedError()
 
-    def set_major_formatter(self):
-        raise NotImplementedError()
+    def set_major_formatter(self, formatter):
+        if isinstance(formatter, Formatter):
+            raise NotImplementedError()  # figure out how to swap out formatter
+        elif isinstance(formatter, basestring):
+            self.tick_formatter.format = formatter
+        else:
+            raise TypeError("formatter should be a string for Formatter instance")
 
     def set_ticks(self, spacing=None, number=None):
         raise NotImplementedError()
