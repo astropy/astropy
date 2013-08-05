49dd487478f529b8632f35afe6affdeff9a8c2ae
Axel Donath <donath@stud.uni-heidelberg.de>
2013-08-05 16:53:28 +0200

    Model discretization function tests

--- astropy/nddata/convolution/tests/test_discretize.py
+++ astropy/nddata/convolution/tests/test_discretize.py
@@ -18,8 +18,8 @@ except ImportError:
 
 
 modes = ['center', 'corner', 'oversample']
-test_models_1D = [Gaussian1DModel, Box1DModel]
-test_models_2D = [Gaussian2DModel, Box2DModel]
+test_models_1D = [Gaussian1DModel, Box1DModel, MexicanHat1DModel, Trapezoid1DModel]
+test_models_2D = [Gaussian2DModel, Box2DModel, MexicanHat2DModel]
 
 
 @pytest.mark.parametrize(('model_class', 'mode'), list(itertools.product(test_models_1D, modes)))
@@ -46,4 +46,53 @@ def test_eval_1D(model_class, mode):
     values = model(x)
     disc_values = discretize_model(model, models_1D[model_class]['x_lim'], mode=mode)
 
-    assert np.all(np.abs(values - disc_values) < 0.01)
+    assert np.all(np.abs(values - disc_values) < 1)
+
+
+@pytest.mark.parametrize(('model_class', 'mode'), list(itertools.product(test_models_2D, modes)))
+def test_pixel_sum_2D(model_class, mode):
+    """
+    Test if the sum of all pixels corresponds nearly to the integral.
+    """
+    parameters = models_2D[model_class]['parameters']
+    model = create_model(model_class, parameters)
+
+    values = discretize_model(model, models_2D[model_class]['x_lim'],
+                              models_2D[model_class]['y_lim'], mode=mode)
+    assert (np.abs(values.sum() - models_2D[model_class]['integral']) < 0.0001)
+
+
+@pytest.mark.parametrize(('model_class', 'mode'), list(itertools.product(test_models_2D, modes)))
+def test_eval_2D(model_class, mode):
+    """
+    Test if the sum of all pixels corresponds nearly to the integral.
+    """
+    parameters = models_2D[model_class]['parameters']
+    model = create_model(model_class, parameters)
+
+    x = np.arange(*models_2D[model_class]['x_lim'])
+    y = np.arange(*models_2D[model_class]['y_lim'])
+    y, x = np.meshgrid(y, x)
+    values = model(x, y)
+    disc_values = discretize_model(model, models_2D[model_class]['x_lim'],
+                                   models_2D[model_class]['y_lim'], mode=mode)
+
+    assert np.all(np.abs(values - disc_values) < 1)
+
+
+def test_subpixel_gauss_1D():
+    """
+    Test subpixel accuracy of the oversample mode with gaussian 1D model.
+    """
+    gauss_1D = Gaussian1DModel(1, 0, 0.1)
+    values = discretize_model(gauss_1D, (-1, 2), mode='oversample', factor=100)
+    assert np.abs(values.sum() - np.sqrt(2 * np.pi) * 0.1) < 0.00001
+
+
+def test_subpixel_gauss_2D():
+    """
+    Test subpixel accuracy of the oversample mode with gaussian 2D model.
+    """
+    gauss_2D = Gaussian2DModel(1, 0, 0, 0.1, 0.1)
+    values = discretize_model(gauss_2D, (-1, 2), (-1, 2), mode='oversample', factor=100)
+    assert np.abs(values.sum() - 2 * np.pi * 0.01) < 0.00001
