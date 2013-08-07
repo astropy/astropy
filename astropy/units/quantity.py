097b07f305f7999b014c4521cd15b4266eeb206f
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2013-08-07 15:30:03 -0400

    un-autopep8ed another URL as to @astrofrog's request

--- astropy/units/quantity.py
+++ astropy/units/quantity.py
@@ -806,8 +806,7 @@ class Quantity(np.ndarray):
 
     # These functions need to be overridden to take into account the units
     # Array conversion
-    # http://docs.scipy.org/doc/numpy/reference/arrays.ndarray.html#array-
-    # conversion
+    # http://docs.scipy.org/doc/numpy/reference/arrays.ndarray.html#array-conversion
 
     def item(self, *args):
         return self.__quantity_instance__(self.view(np.ndarray).item(*args), self.unit)
