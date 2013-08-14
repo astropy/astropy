e552e355c3d2f9c518d86d3053000edacfee99fc
Christoph Deil <Deil.Christoph@gmail.com>
2013-08-14 13:05:06 +0200

    Move comments from samp core.py to __init__.py

--- astropy/vo/samp/__init__.py
+++ astropy/vo/samp/__init__.py
@@ -2,6 +2,25 @@
 """
 The `vo.samp` subpackage provides classes to communicate with other
 applications via the Simple Application Messaging Protocal (SAMP)
+
 www.ivoa.net/samp
 """
+##   This module contains classes to create a SAMP Hub and interface an
+##   application to a running SAMP Hub (Standard Profile). This package
+##   was formerly known as SAMPy (https://pypi.python.org/pypi/sampy/)
+##
+##
+##   Copyright (C) 2013  INAF-IASF Milano
+##
+##
+##   Authors:
+##
+##   Luigi Paioro
+##   INAF - Istituto Nazionale di Astrofisica
+##
+##   IASF Milano
+##   Via Bassini 15, I-20133 Milano, Italy
+##   E-mail: luigi at iasf-milano.inaf.it
+##   Site  : http://www.iasf-milano.inaf.it/
+
 from .core import *
