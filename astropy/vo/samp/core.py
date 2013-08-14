ef9ff9cbfd0ac7c001237c7aa4602dabbb4f582a
Christoph Deil <Deil.Christoph@gmail.com>
2013-08-14 13:05:06 +0200

    Move comments from samp core.py to __init__.py

--- astropy/vo/samp/core.py
+++ astropy/vo/samp/core.py
@@ -1,31 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-#!/usr/bin/env python
-
 from __future__ import print_function, division
-
-################################################################################
-##
-##   astropy.vo.samp.core.py
-##
-##   This module contains classes to create a SAMP Hub and interface an
-##   application to a running SAMP Hub (Standard Profile). This package
-##   was formerly known as SAMPy (https://pypi.python.org/pypi/sampy/)
-##
-##
-##   Copyright (C) 2013  INAF-IASF Milano
-##
-##
-##   Authors:
-##
-##   Luigi Paioro
-##   INAF - Istituto Nazionale di Astrofisica
-##
-##   IASF Milano
-##   Via Bassini 15, I-20133 Milano, Italy
-##   E-mail: luigi at iasf-milano.inaf.it
-##   Site  : http://www.iasf-milano.inaf.it/
-##
-################################################################################
 
 
 __author__   = "Luigi Paioro"
