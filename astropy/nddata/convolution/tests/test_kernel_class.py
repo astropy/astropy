ac1ad30428e4f4460c5d4342f6bae7252ccb568e
Axel Donath <donath@stud.uni-heidelberg.de>
2013-08-16 12:28:57 +0200

    Test kernels against scipy.ndimage.filters

--- astropy/nddata/convolution/tests/test_kernel_class.py
+++ astropy/nddata/convolution/tests/test_kernel_class.py
@@ -9,6 +9,11 @@ from numpy.testing import assert_almost_equal
 
 import itertools
 
+try:
+    from scipy.ndimage import filters
+    HAS_SCIPY = True
+except ImportError:
+    HAS_SCIPY = False
 
 widths = [3, 5, 7, 9]
 kernel_types = [Gaussian1DKernel, Gaussian2DKernel,
@@ -16,11 +21,61 @@ kernel_types = [Gaussian1DKernel, Gaussian2DKernel,
                 Trapezoid1DKernel, TrapezoidDisk2DKernel,
                 MexicanHat1DKernel, Tophat2DKernel]
 
+# Test data
+delta_pulse_1D = np.zeros(81)
+delta_pulse_1D[40] = 1
+
+delta_pulse_2D = np.zeros((81, 81))
+delta_pulse_2D[40, 40] = 1
+
+random_data_1D = np.random.rand(61)
+random_data_2D = np.random.rand(61, 61)
+
 
 class TestKernels(object):
     """
-    NOTE: in current version, convolve_fft comes before convolve always because convolve changes kernel
+    Test class for the built-in convolution kernels.
     """
+
+    @pytest.mark.skipif('not HAS_SCIPY')
+    @pytest.mark.parametrize(('width'), widths)
+    def test_scipy_filter_gaussian(self, width):
+        """
+        Test GaussianKernel against SciPy ndimage gaussian filter.
+        """
+        gauss_kernel_1D = Gaussian1DKernel(width)
+        gauss_kernel_1D.normalize()
+        gauss_kernel_2D = Gaussian2DKernel(width)
+        gauss_kernel_2D.normalize()
+
+        astropy_1D = convolve(delta_pulse_1D, gauss_kernel_1D, boundary='fill')
+        astropy_2D = convolve(delta_pulse_2D, gauss_kernel_2D, boundary='fill')
+
+        scipy_1D = filters.gaussian_filter(delta_pulse_1D, width)
+        scipy_2D = filters.gaussian_filter(delta_pulse_2D, width)
+
+        assert_almost_equal(astropy_1D, scipy_1D, decimal=12)
+        assert_almost_equal(astropy_2D, scipy_2D, decimal=12)
+
+    @pytest.mark.skipif('not HAS_SCIPY')
+    @pytest.mark.parametrize(('width'), widths)
+    def test_scipy_filter_gaussian_laplace(self, width):
+        """
+        Test MexicanHat kernels against SciPy ndimage gaussian laplace filters.
+        """
+        mexican_kernel_1D = MexicanHat1DKernel(width)
+        mexican_kernel_2D = MexicanHat2DKernel(width)
+
+        astropy_1D = convolve(delta_pulse_1D, mexican_kernel_1D, boundary='fill')
+        astropy_2D = convolve(delta_pulse_2D, mexican_kernel_2D, boundary='fill')
+
+        scipy_1D = filters.gaussian_laplace(delta_pulse_1D, width)
+        scipy_2D = filters.gaussian_laplace(delta_pulse_2D, width)
+
+        # There is a slight deviation in the normalization. They differ by a
+        # factor of ~1.0000284132604045. The reason is not known.
+        assert_almost_equal(astropy_1D, scipy_1D, decimal=5)
+        assert_almost_equal(astropy_2D, scipy_2D, decimal=5)
 
     @pytest.mark.parametrize(('kernel_type', 'width'), list(itertools.product(kernel_types, widths)))
     def test_delta_data(self, kernel_type,  width):
@@ -29,14 +84,14 @@ class TestKernels(object):
         """
         kernel = kernel_type(width)
 
-        x = np.zeros(kernel.shape)
-        xslice = [slice(sh // 2, sh // 2) for sh in kernel.shape]
-        x[xslice] = 1.0
-
-        c2 = convolve_fft(x, kernel, boundary='fill')
-        c1 = convolve(x, kernel, boundary='fill')
-
-        assert_almost_equal(c1, c2, decimal=12)
+        if kernel.dimension == 1:
+            c1 = convolve_fft(delta_pulse_1D, kernel, boundary='fill')
+            c2 = convolve(delta_pulse_1D, kernel, boundary='fill')
+            assert_almost_equal(c1, c2, decimal=12)
+        else:
+            c1 = convolve_fft(delta_pulse_2D, kernel, boundary='fill')
+            c2 = convolve(delta_pulse_2D, kernel, boundary='fill')
+            assert_almost_equal(c1, c2, decimal=12)
 
     @pytest.mark.parametrize(('kernel_type', 'width'), list(itertools.product(kernel_types, widths)))
     def test_random_data(self, kernel_type, width):
@@ -45,17 +100,14 @@ class TestKernels(object):
         """
         kernel = kernel_type(width)
 
-        x = np.zeros(kernel.shape)
-        xslice = [slice(sh // 2, sh // 2) for sh in kernel.shape]
-        x[xslice] = 1.0
-
-        x = np.random.randn(*kernel.shape)
-
-        c2 = convolve_fft(x, kernel, boundary='fill')
-        c1 = convolve(x, kernel, boundary='fill')
-
-        # not clear why, but these differ by a couple ulps...
-        assert_almost_equal(c1, c2, decimal=12)
+        if kernel.dimension == 1:
+            c1 = convolve_fft(random_data_1D, kernel, boundary='fill')
+            c2 = convolve(random_data_1D, kernel, boundary='fill')
+            assert_almost_equal(c1, c2, decimal=12)
+        else:
+            c1 = convolve_fft(random_data_2D, kernel, boundary='fill')
+            c2 = convolve(random_data_2D, kernel, boundary='fill')
+            assert_almost_equal(c1, c2, decimal=12)
 
     @pytest.mark.parametrize(('width'), widths)
     def test_uniform_smallkernel(self, width):
@@ -64,19 +116,10 @@ class TestKernels(object):
 
         Instead of using kernel class, uses a simple, small kernel
         """
-        if width % 2 == 0:
-            # convolve does not accept odd-shape kernels
-            return
-
         kernel = np.ones([width, width])
 
-        x = np.zeros([width, width])
-        xslice = [slice(sh // 2, sh // 2) for sh in [width, width]]
-        x[xslice] = 1.0
-
-        c2 = convolve_fft(x, kernel, boundary='fill')
-        c1 = convolve(x, kernel, boundary='fill')
-
+        c2 = convolve_fft(delta_pulse_2D, kernel, boundary='fill')
+        c1 = convolve(delta_pulse_2D, kernel, boundary='fill')
         assert_almost_equal(c1, c2, decimal=12)
 
     @pytest.mark.parametrize(('width'), widths)
@@ -89,10 +132,7 @@ class TestKernels(object):
         kernel1 = np.ones([width, width]) / width ** 2
         kernel2 = Box2DKernel(width)
 
-        x = np.zeros(kernel2.shape)
-        x[kernel2.center] = 1.0
-
-        c2 = convolve_fft(x, kernel2.array, boundary='fill')
-        c1 = convolve_fft(x, kernel1, boundary='fill')
+        c2 = convolve_fft(delta_pulse_2D, kernel2, boundary='fill')
+        c1 = convolve_fft(delta_pulse_2D, kernel1, boundary='fill')
 
         assert_almost_equal(c1, c2, decimal=12)
