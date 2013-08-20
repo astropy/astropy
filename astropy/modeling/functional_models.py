36977363f47e92f61b55a6a785d33c5a7f41a804
Nadia Dencheva <nadia.dencheva@gmail.com>
2013-08-20 18:04:01 -0400

    Split the SIPModel class into forward and inverse model. Corrected inverse of ShiftModel and ScaleModel. Fixed name in parameter lookup.

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -210,7 +210,9 @@ class ShiftModel(Model):
     param_names = ['offsets']
 
     def __init__(self, offsets, param_dim=1):
-        if isinstance(offsets, collections.Sequence):
+        if not isinstance(offsets, collections.Sequence):
+            param_dim = 1
+        else:
             param_dim = len(offsets)
         self._offsets = parameters.Parameter('offsets', offsets, self, param_dim)
         super(ShiftModel, self).__init__(self.param_names, n_inputs=1, n_outputs=1,
@@ -251,7 +253,9 @@ class ScaleModel(Model):
     param_names = ['factors']
 
     def __init__(self, factors, param_dim=1):
-        if isinstance(factors, collections.Sequence):
+        if not isinstance(factors, collections.Sequence):
+            param_dim = 1
+        else:
             param_dim = len(factors)
         self._factors = parameters.Parameter('factors', factors, self, param_dim)
         super(ScaleModel, self).__init__(self.param_names, n_inputs=1, n_outputs=1,
@@ -691,7 +695,7 @@ class Box1DModel(Parametric1DModel):
         """
         Model function Box1D
         """
-        return np.select([np.logical_and(x > x_0 - width / 2., x < x_0 + width / 2.), 
+        return np.select([np.logical_and(x > x_0 - width / 2., x < x_0 + width / 2.),
                           np.logical_or(x == x_0 - width / 2., x == x_0 + width / 2.)],
                          [amplitude, amplitude / 2.])
 
