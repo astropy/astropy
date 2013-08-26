c91ae3035bbb0d6b5a2f85af4447ebf6cefff3ae
Alex Hagen <mr.alex.hagen@gmail.com>
2013-08-26 14:46:12 -0400

    fixed silly mistake in tests

--- astropy/stats/tests/test_funcs.py
+++ astropy/stats/tests/test_funcs.py
@@ -214,7 +214,7 @@ def test_bootstrap():
     #test general bootstrapping
     answer = np.array([[7,4,8,5,7,0,3,7,8,5],[4,8,8,3,6,5,2,8,6,2]])
     with NumpyRNGContext(42):
-        assert answer = funcs.bootstrap(bootarr,2)
+        assert answer == funcs.bootstrap(bootarr,2)
     
     #test with a bootfunction
     with NumpyRNGContext(42):
