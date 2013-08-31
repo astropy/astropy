e6fce2d60018b6470a2ab165bc62d431a12919ca
Erik Bray <erik.m.bray@gmail.com>
2013-08-31 13:27:46 -0700

    Merge pull request #1197 from embray/ez_setup
    Simplify logic at the top of setup.py
    Conflicts:
    
    	setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-import sys
-import imp
-try:
-    # This incantation forces distribute to be used (over setuptools) if it is
-    # available on the path; otherwise distribute will be downloaded.
-    import pkg_resources
-    distribute = pkg_resources.get_distribution('distribute')
-    if pkg_resources.get_distribution('setuptools') != distribute:
-        sys.path.insert(1, distribute.location)
-        distribute.activate()
-        imp.reload(pkg_resources)
-except:  # There are several types of exceptions that can occur here
-    from distribute_setup import use_setuptools
-    use_setuptools()
-
 import glob
 import os
+import sys
+
+import setuptools_bootstrap
 from setuptools import setup, find_packages
 
 #A dirty hack to get around some early import/configurations ambiguities
