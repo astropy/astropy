558bfb7ec3e46845ac0d7756f218f931b948f5ab
Michael Droettboom <mdboom@gmail.com>
2013-09-16 16:06:24 -0400

    Fix minor problem with install due to moving astropy.wcs to six

--- astropy/setup_helpers.py
+++ astropy/setup_helpers.py
@@ -1566,7 +1566,7 @@ def add_external_library(library):
     """
 
     for command in ['build', 'build_ext', 'install']:
-        add_command_option(command, 'use-system-' + library,
+        add_command_option(command, str('use-system-' + library),
                            'Use the system {0} library'.format(library),
                            is_bool=True)
 
