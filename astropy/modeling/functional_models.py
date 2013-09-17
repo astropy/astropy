3e08370693f8829856d617eadf55d30673c40173
Sarah Kendrew <sarahaskendrew@gmail.com>
2013-09-17 19:27:05 -0400

    added planck function to functional_models

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -1143,3 +1143,35 @@ class Custom1DModel(Parametric1DModel):
         Model function Custom1D
         """
         return self._func(x, *params)
+
+def planck(w, temp):
+	"""
+	Function to calculate blackbody flux given wavelength or frequency and temperature	
+	
+	Parameters
+	----------
+	w 	 : 	astropy.units.Quantity
+			single value or list, in frequency or wavelength units
+	temp : 	astropy.units.Quantity
+			temperature quantity
+	Returns
+	-------
+	flux	: blackbody flux, in units W/m2/um
+
+	Note: This function should be turned into a model once model parameters can be quantities
+	
+	"""
+	from astropy import units as u
+	from astropy import constants as const
+
+
+	if (w.unit.physical_type == 'frequency'):
+		w = w.to(u.m, equivalencies=u.spectral())
+	elif (w.unit.physical_type == 'length'):
+		w = w.to(u.m)
+
+	flux = 2 * const.h * const.c**2 / w**5 / (np.exp(const.h*const.c/(w * const.k_B * temp)) - 1)
+	flux = flux.to(u.Watt / (u.m * u.m * u.micrometer))	
+	return flux
+
+
