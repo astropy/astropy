e6cbe9be2faab93c538a03b874481bfa966258a0
Michael Droettboom <mdboom@gmail.com>
2013-09-23 11:59:20 -0400

    Only include easy conversion members on subclasses of Quantity that opt-in.

--- astropy/units/tests/test_quantity.py
+++ astropy/units/tests/test_quantity.py
@@ -621,18 +621,24 @@ def test_quantity_invalid_unit_string():
 
 def test_implicit_conversion():
     q = u.Quantity(1.0, u.meter)
+    # Manually turn this on to simulate what might happen in a subclass
+    q._include_easy_conversion_members = True
     assert_allclose(q.inch, 39.370078740157474)
     assert_allclose(q.centimeter, 100)
+    assert_allclose(q.cm, 100)
     assert_allclose(q.parsec, 3.240779289469756e-17)
 
 
 def test_implicit_conversion_autocomplete():
     q = u.Quantity(1.0, u.meter)
+    # Manually turn this on to simulate what might happen in a subclass
+    q._include_easy_conversion_members = True
     q.foo = 42
 
     attrs = dir(q)
     assert 'inch' in attrs
     assert 'centimeter' in attrs
+    assert 'cm' in attrs
     assert 'parsec' in attrs
     assert 'foo' in attrs
     assert 'to' in attrs
