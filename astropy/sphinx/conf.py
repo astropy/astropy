astropy/astropy

commit 503e3ebc8ebed2c1ecbe0dbc3300ddc859bd54ad

Be smart about when we need a bleeding-edge Sphinx.

astropy/sphinx/conf.py
@@ -11,13 +11,41 @@
 
 import warnings
 from os import path
+from distutils.version import LooseVersion
+import re
+import subprocess
 
 
 # -- General configuration ----------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-# Some of the docs require the autodoc special-members option, in 1.1
-needs_sphinx = '1.2'
+# Some of the docs require the autodoc special-members option, in 1.1.
+# If using graphviz 2.30 or later, Sphinx < 1.2b2 will not work with
+# it.  Unfortunately, there are other problems with Sphinx 1.2b2, so
+# we need to use "dev" until a release is made post 1.2b2.  If
+# affiliated packages don't want this automatic determination, they
+# may simply override needs_sphinx in their local conf.py.
+
+def get_graphviz_version():
+    try:
+        output = subprocess.check_output(
+            ['dot', '-V'], stdin=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True)
+    except subprocess.CalledProcessError:
+        return '0'
+    tokens = output.split()
+    for token in tokens:
+        if re.match(b'[0-9.]*', token):
+            return token.decode('ascii')
+    return '0'
+
+graphviz_found = LooseVersion(get_graphviz_version())
+graphviz_broken = LooseVersion('0.30')
+
+if graphviz_found >= graphviz_broken:
+    needs_sphinx = '1.2b2'
+else:
+    needs_sphinx = '1.1'
 
 # Configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {

commit 6c9bd3ffd6cae83dbb02825c190a1c9de84fcc3f

503e3ebc inadvertently introduced code that is not compatible with Python 2.6 (subprocess.check_output).  This remedies that.

astropy/sphinx/conf.py
@@ -13,7 +13,8 @@ import warnings
 from os import path
 from distutils.version import LooseVersion
 import re
-import subprocess
+
+from ..utils.compat import subprocess
 
 
 # -- General configuration ----------------------------------------------------

astropy/utils/compat/subprocess.py
@@ -1,0 +1,56 @@
+"""
+A replacement wrapper around the subprocess module that adds
+check_output (which was only added to Python in 2.7.
+
+Instead of importing subprocess, other modules should use this as follows::
+
+    from astropy.utils.compat import subprocess
+
+This module is safe to import from anywhere within astropy.
+"""
+from __future__ import absolute_import, print_function
+
+import subprocess
+
+
+def _check_output(*popenargs, **kwargs):
+    r"""Run command with arguments and return its output as a byte
+    string.
+
+    If the exit code was non-zero it raises a CalledProcessError.  The
+    CalledProcessError object will have the return code in the
+    returncode
+    attribute and output in the output attribute.
+
+    The arguments are the same as for the Popen constructor.  Example::
+
+    >>> check_output(["ls", "-l", "/dev/null"])
+    'crw-rw-rw- 1 root root 1, 3 Oct 18  2007 /dev/null\n'
+
+    The stdout argument is not allowed as it is used internally.
+    To capture standard error in the result, use stderr=STDOUT.::
+
+    >>> check_output(["/bin/sh", "-c",
+    ...               "ls -l non_existent_file ; exit 0"],
+    ...              stderr=STDOUT)
+    'ls: non_existent_file: No such file or directory\n'
+    """
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = Popen(stdout=PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise subprocess.CalledProcessError(retcode, cmd, output=output)
+    return output
+
+
+# python2.7 and later provide a check_output method
+if not hasattr(subprocess, 'check_output'):
+    check_output = _check_output
+
+
+from subprocess import *