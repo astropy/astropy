f5d620e1ae937d8b0407baf49ec226faacde7fe8
Erik Tollerud <erik.tollerud@gmail.com>
2013-10-04 02:35:18 -0400

    Initial shot at coordinate matching function

---
+++ astropy/coordinates/matching.py
@@ -0,0 +1,105 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This module contains functions for matching coordinate catalogs.
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+
+from .. import units as u
+
+__all__ = ['match_coordinates']
+
+def match_coordinates(matchcoord, catalogcoord, nthneighbor=1, storekdtree=None):
+    """
+    Matches a coordinate or coordinates to a set of catalog coordinates 
+    
+    Parameters
+    ----------
+    matchcoord : `~astropy.coordinates.SphericalCoordinatesBase`
+        The coordinates to match to the catalog.
+    catalogcoord : `~astropy.coordinates.SphericalCoordinatesBase`
+        The base catalog in which to search for matches.
+    nthneighbor : int, optional
+        Which closest neighbor to search for.  Typically ``1`` is desired here,
+        as that is correct for matching one set of coordinates to another.
+        The next likely use case is ``2``, for matching a coordinate catalog
+        against *itself* (``1`` is inappropriate because each point will find 
+        itself as the closest match).
+    storekdtree : bool or str, optional
+        If True or a string, will store the KD-Tree used for the computation
+        in the `catalogcoord`.  This dramatically speeds up subsequent calls
+        with the same catalog.
+
+    Returns
+    -------
+    idx : integer array
+        Indecies into `catalogcoord` to get the matched points for each 
+        `matchcoord`. Shape matches `matchcoord`.
+    dist3d : `~astropy.units.quantity.Quantity` 
+        The 3D distance between the closest match for each `matchcoord` and 
+        the `matchcoord`. Shape matches `matchcoord`.
+
+    Notes
+    -----
+    This function requires `scipy` to be installed or it will fail.
+    """
+    from warnings import warn
+
+    #without scipy this will immediately fail
+    from scipy import spatial 
+    try:
+        KDTree = spatial.cKDTree
+    except:
+        warn('C-base KD tree not found, falling back on (much slower) '
+             'python implementation')
+        KDTree = spatial.KDTree
+
+    if storekdtree is True:
+        storekdtree = '_kdtree'
+    
+    # figure out where any cached KDTree might be
+    if isinstance(storekdtree, basestring):
+        kdt = getattr(catalogcoord, storekdtree, None)
+        if kdt is not None and not isinstance(kdt, KDTree):
+            raise ValueError('Invalid `storekdtree` string:' + storekdtree)
+    elif isinstance(storekdtree, KDTree):
+        kdt = storekdtree 
+        storekdtree = None
+    elif not storekdtree:
+        kdt = None
+    else:
+        raise ValueError('Invalid `storekdtree` argument:' + 
+                          str(storekdtree))
+
+    if kdt is None:
+        #need to build the cartesian KD-tree for the catalog
+        cart = catalogcoord.cartesian
+        flatxyz = cart.reshape((3, np.prod(cart.shape) // 3))
+        kdt = KDTree(flatxyz.value.T)
+
+    #make sure coordinate systems match
+    matchcoord = matchcoord.transform_to(catalogcoord.__class__)
+
+    #make sure units match
+    catunit = catalogcoord.cartesian.unit
+    cart = matchcoord.cartesian.to(catunit) 
+
+    flatxyz = cart.reshape((3, np.prod(cart.shape) // 3))
+    dist, idx = kdt.query(flatxyz, nthneighbor)
+
+    if nthneighbor > 1:  # query gives 1D arrays if k=1, 2D arrays otherwise
+        dist = dist[-1]
+        idx = idx[-1]
+
+    if storekdtree:
+        #cache the kdtree
+        setattr(catalogcoord, storekdtree, kdt)
+
+    #TODO: compute 2D separation, too?
+    return idx.reshape(cart.shape[1:]), dist.reshape(cart.shape[1:]) * catunit
+
+
+
