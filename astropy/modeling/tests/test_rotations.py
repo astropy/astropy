aa26e9b597e0a17de18d0d89aca04bfd07e9a0f5
Christoph Deil <Deil.Christoph@gmail.com>
2013-10-17 16:31:02 +0200

    Add more modeling Parameter unit tests

--- astropy/modeling/tests/test_rotations.py
+++ astropy/modeling/tests/test_rotations.py
@@ -1,5 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+from __future__ import division
+
 from .. import models
 from numpy.testing import utils
 from ...tests.helper import pytest
