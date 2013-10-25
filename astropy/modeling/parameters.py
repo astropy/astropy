f1d3f471fd0bfdf89426921d6343160f1450f237
Erik M. Bray <embray@stsci.edu>
2013-10-25 13:54:46 -0400

    Initial implementation of descriptions for parameters, which also override their doc strings--in the long term this will be nice for both interactive help and generated API docs.

--- astropy/modeling/parameters.py
+++ astropy/modeling/parameters.py
@@ -104,10 +104,12 @@ class Parameter(object):
     # See the _nextid classmethod
     _nextid = 1
 
-    def __init__(self, name, default=None, getter=None, setter=None,
-                 fixed=False, tied=False, min=None, max=None, model=None):
+    def __init__(self, name, description='', default=None, getter=None,
+                 setter=None, fixed=False, tied=False, min=None, max=None,
+                 model=None):
         super(Parameter, self).__init__()
         self._name = name
+        self.__doc__ = description.strip()
         self._default = default
         self._attr = '_' + name
 
