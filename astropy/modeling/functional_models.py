c511117cf26e200bdedf1b61a49f8f195b2e0b1b
Adrian Price-Whelan <adrian.prw@gmail.com>
2013-10-26 23:19:18 -0700

    add func_deriv to custom_1d_model

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -1189,7 +1189,7 @@ class Beta2D(Parametric2DModel):
         return [d_A, d_x_0, d_y_0, d_gamma, d_alpha]
 
 
-def custom_model_1d(func):
+def custom_model_1d(func, func_deriv=None):
     """
     Create one dimensional model from a user defined function.
 
@@ -1203,7 +1203,7 @@ def custom_model_1d(func):
     ----------
     func : function
         Function which defines the model
-    func_deriv : function
+    func_deriv : function (optional)
         Function which defines the model derivatives default = None
 
     Examples
@@ -1228,11 +1228,19 @@ def custom_model_1d(func):
     if not callable(func):
         raise ModelDefinitionError("Not callable. Must be function")
 
+    if func_deriv is not None and not callable(func_deriv):
+        raise ModelDefinitionError("func_deriv not callable. Must be function")
+
     model_name = func.__name__
     param_values = func.func_defaults
 
     # Check if all parameters are keyword arguments
     nparams = len(param_values)
+
+    if func_deriv is not None and len(func_deriv.func_defaults) != nparams:
+        raise ModelDefinitionError("derivative function should accept"
+                                   " same number of parameters as func.")
+
     if func.func_code.co_argcount == nparams + 1:
         param_names = func.func_code.co_varnames[1:nparams + 1]
     else:
@@ -1256,6 +1264,7 @@ def custom_model_1d(func):
         modname = '__main__'
 
     members = {'eval': staticmethod(func)}
+
     eval_globals = {}
 
     init_code_string = dedent("""
@@ -1264,6 +1273,9 @@ def custom_model_1d(func):
     """).format(arg_signature_1, arg_signature_2)
 
     eval(compile(init_code_string, filename, 'single'), eval_globals)
+
+    if func_deriv is not None:
+        members['deriv'] = staticmethod(func_deriv)
 
     members['__init__'] = eval_globals['__init__']
     members.update(params)
