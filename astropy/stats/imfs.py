e7419e805754b8213a40c4af55f8066eed20cf9d
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-06 10:59:43 +0100

    fixed _cdf in salpeter

--- astropy/stats/imfs.py
+++ astropy/stats/imfs.py
@@ -15,19 +15,22 @@ import numpy as np
 class SalpeterGen(rv_continuous):
     """
     Power law distribution:
-    p(x) = C * x**(-p-1)
+    p(x) = C * x**(-p)
     where
-    C = x_0**(-p)
+    C = (p-1) / x_0**(1-p)
 
     The default parameters are the Salpeter minimum mass of a=0.03 Msun and
     b=inf.  To change the limits of the mass function, do:
     imf_limited = SalpeterGen(a=0.5,b=120)
     """
-    def _pdf(self, x, p=1.35):
-        return x**(-p-1) / self.a**p
+    def _pdf(self, x, p=2.35):
+        return x**(-p) * (p-1) / self.a**(1-p)
 
-    def __init__(self, a=0.03, **kwargs):
-        super(SalpeterGen,self).__init__(a=a,**kwargs)
+    def _cdf(self, x, p=2.35):
+        return 1-(x/self.a)**(1-p) 
+
+    def __init__(self, a=0.03, name='Salpeter', **kwargs):
+        super(SalpeterGen,self).__init__(a=a, name=name, **kwargs)
 
 Salpeter = SalpeterGen()
 
@@ -56,7 +59,7 @@ class KroupaGen(rv_continuous):
                 d*(m**(-(p3))) * (m>=break2))
         return zeta
 
-    def __init__(self, a=0.03, **kwargs):
-        super(KroupaGen,self).__init__(a=a,**kwargs)
+    def __init__(self, a=0.03, name='Kroupa', **kwargs):
+        super(KroupaGen,self).__init__(a=a, name=name, **kwargs)
 
 Kroupa = KroupaGen()
