7c8480a91e6019d863f665d5f56835374bc6a286
Erik M. Bray <embray@stsci.edu>
2014-01-16 14:59:34 -0500

    This change to itersubclasses restores some determinism to it on Python 3.4.  See https://github.com/astropy/astropy/pull/1945#issuecomment-32522217
    Conflicts:
    
    	astropy/io/fits/util.py

--- astropy/io/fits/util.py
+++ astropy/io/fits/util.py
@@ -39,7 +39,7 @@ def itersubclasses(cls, _seen=None):
     C
     >>> # get ALL (new-style) classes currently defined
     >>> [cls.__name__ for cls in itersubclasses(object)]
-    ['type', ...'tuple', ...]
+    [...'tuple', ...'type', ...]
 
     From http://code.activestate.com/recipes/576949/
     """
@@ -53,7 +53,7 @@ def itersubclasses(cls, _seen=None):
         subs = cls.__subclasses__()
     except TypeError:  # fails only when cls is type
         subs = cls.__subclasses__(cls)
-    for sub in subs:
+    for sub in sorted(subs, key=lambda s: s.__name__):
         if sub not in _seen:
             _seen.add(sub)
             yield sub
