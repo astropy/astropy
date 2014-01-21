3a7d2374fdf641ba2e5acd0e4c8313bba54c3d48
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2014-01-21 23:57:16 +0100

    Fixed missing member in units.core

--- astropy/units/core.py
+++ astropy/units/core.py
@@ -31,7 +31,7 @@ __all__ = [
     'CompositeUnit', 'PrefixUnit', 'UnrecognizedUnit',
     'get_current_unit_registry', 'set_enabled_units',
     'add_enabled_units', 'set_enabled_equivalencies',
-    'add_enabled_equivalencies', 'dimensionless_unscaled']
+    'add_enabled_equivalencies', 'dimensionless_unscaled', 'one']
 
 
 def _flatten_units_collection(items):
