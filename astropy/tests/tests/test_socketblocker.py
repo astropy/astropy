4e81396e914610d84ce3590363a34129e355594f
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-05 22:09:16 +0100

    setup an http server so tests can be run...
    tiny formatting change

--- astropy/tests/tests/test_socketblocker.py
+++ astropy/tests/tests/test_socketblocker.py
@@ -1,5 +1,9 @@
 import urllib
 from ..helper import pytest
+import BaseHTTPServer
+import SimpleHTTPServer
+from threading import Thread
+import time
 
 
 def test_outgoing_fails():
@@ -7,11 +11,29 @@ def test_outgoing_fails():
         urllib.urlopen('http://www.astropy.org')
 
 
+def run_while_true(server_class=BaseHTTPServer.HTTPServer,
+                   handler_class=SimpleHTTPServer.SimpleHTTPRequestHandler):
+    """
+    This assumes that keep_running() is a function of no arguments which
+    is tested initially and after each request.  If its return value
+    is true, the server continues.
+    """
+    server_address = ('localhost', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+ 
+server = Thread(target=run_while_true)
+server.setDaemon(True)
+
+
 def test_localconnect_succeeds():
     """
     Ensure that connections to localhost are allowed, since these are genuinely
     not remotedata.
     """
-    urllib.urlopen('http://localhost').close()
-    urllib.urlopen('http://127.0.0.1').close()
 
+    server.start()
+    time.sleep(0.1)
+
+    urllib.urlopen('http://localhost:8000').close()
+    urllib.urlopen('http://127.0.0.1:8000').close()
