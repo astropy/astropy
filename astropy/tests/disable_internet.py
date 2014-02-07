d541643a6ed86c6278b2361fe606b79ed1fb4762
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-02-08 00:45:38 +0100

    in principle, this should create and destroy an http server...
    py3 compat
    
    __future__ has to be at the top of the file
    
    future expansion

--- astropy/tests/disable_internet.py
+++ astropy/tests/disable_internet.py
@@ -1,4 +1,5 @@
-from __future__ import print_function
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 import socket
 
 # save original socket method for restoration
--- astropy/tests/tests/test_socketblocker.py
+++ astropy/tests/tests/test_socketblocker.py
@@ -1,3 +1,5 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 from ..helper import pytest
 from astropy.extern.six.moves import BaseHTTPServer, SimpleHTTPServer
 from astropy.extern.six.moves.urllib.request import urlopen
@@ -10,6 +12,22 @@ def test_outgoing_fails():
         urlopen('http://www.astropy.org')
 
 
+class StoppableHTTPServer(BaseHTTPServer.HTTPServer):
+    def __init__(self, *args):
+        super(StoppableHTTPServer, self).__init__(*args)
+        self.stop = False
+
+    def handle_request(self):
+        self.stop = True
+        super(StoppableHTTPServer, self).handle_request()
+     
+    def serve_forever(self):
+        """
+        Serve until stop set, which will happen if any request is handled
+        """
+        while not self.stop:
+            self.handle_request()
+
 def run_while_true(server_class=BaseHTTPServer.HTTPServer,
                    handler_class=SimpleHTTPServer.SimpleHTTPRequestHandler):
     """
@@ -19,20 +37,22 @@ def run_while_true(server_class=BaseHTTPServer.HTTPServer,
     """
     server_address = ('localhost', 8000)
     httpd = server_class(server_address, handler_class)
+
+    # "serve forever" really means "serve until first request"
     httpd.serve_forever()
- 
-server = Thread(target=run_while_true)
-server.setDaemon(True)
 
 
-def test_localconnect_succeeds():
+@pytest.mark.parametrize(('localhost'),('localhost','127.0.0.1'))
+def test_localconnect_succeeds(localhost):
     """
     Ensure that connections to localhost are allowed, since these are genuinely
     not remotedata.
     """
 
+    server = Thread(target=run_while_true)
+    server.setDaemon(True)
+
     server.start()
     time.sleep(0.1)
 
-    urlopen('http://localhost:8000').close()
-    urlopen('http://127.0.0.1:8000').close()
+    urlopen('http://{}:8000'.format(localhost)).close()
