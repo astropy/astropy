95160fe253c7a8e8d58b0f04ba3c9107b919963f
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-02-07 18:16:38 +0100

    Refactored FITS table reader/writer using new BaseIO class

--- astropy/io/fits/connect.py
+++ astropy/io/fits/connect.py
@@ -8,7 +8,7 @@ import warnings
 
 import numpy as np
 
-from .. import registry as io_registry
+from ..registry import BaseIO
 from ... import log
 from ... import units as u
 from ...extern.six import string_types
@@ -16,7 +16,7 @@ from ...table import Table
 from ...utils import OrderedDict
 from ...utils.exceptions import AstropyUserWarning
 
-from . import HDUList, TableHDU, BinTableHDU, GroupsHDU
+from . import HDUList, TableHDU, BinTableHDU, GroupsHDU, Header
 from .hdu.hdulist import fitsopen as fits_open
 from .util import first
 
@@ -50,134 +50,24 @@ def is_column_keyword(keyword):
     return False
 
 
-def is_fits(origin, filepath, fileobj, *args, **kwargs):
-    """
-    Determine whether `origin` is a FITS file.
+def header_to_meta(header):
 
-    Parameters
-    ----------
-    origin : str or readable file-like object
-        Path or file object containing a potential FITS file.
+    meta = OrderedDict()
 
-    Returns
-    -------
-    is_fits : bool
-        Returns `True` if the given file is a FITS file.
-    """
-    if fileobj is not None:
-        pos = fileobj.tell()
-        sig = fileobj.read(30)
-        fileobj.seek(pos)
-        return sig == FITS_SIGNATURE
-    elif filepath is not None:
-        if filepath.lower().endswith(('.fits', '.fits.gz', '.fit', '.fit.gz')):
-            return True
-    elif isinstance(args[0], (HDUList, TableHDU, BinTableHDU, GroupsHDU)):
-        return True
-    else:
-        return False
-
-
-def read_table_fits(input, hdu=None):
-    """
-    Read a Table object from an FITS file
-
-    Parameters
-    ----------
-    input : str or file-like object or compatible `astropy.io.fits` HDU object
-        If a string, the filename to read the table from. If a file object, or
-        a compatible HDU object, the object to extract the table from. The
-        following `astropy.io.fits` HDU objects can be used as input:
-        - :class:`~astropy.io.fits.hdu.table.TableHDU`
-        - :class:`~astropy.io.fits.hdu.table.BinTableHDU`
-        - :class:`~astropy.io.fits.hdu.table.GroupsHDU`
-        - :class:`~astropy.io.fits.hdu.hdulist.HDUList`
-    hdu : int or str, optional
-        The HDU to read the table from.
-    """
-
-    if isinstance(input, HDUList):
-
-        # Parse all table objects
-        tables = OrderedDict()
-        for ihdu, hdu_item in enumerate(input):
-            if isinstance(hdu_item, (TableHDU, BinTableHDU, GroupsHDU)):
-                tables[ihdu] = hdu_item
-
-        if len(tables) > 1:
-            if hdu is None:
-                warnings.warn("hdu= was not specified but multiple tables"
-                              " are present, reading in first available"
-                              " table (hdu={0})".format(first(tables)),
-                              AstropyUserWarning)
-                hdu = first(tables)
-
-            # hdu might not be an integer, so we first need to convert it
-            # to the correct HDU index
-            hdu = input.index_of(hdu)
-
-            if hdu in tables:
-                table = tables[hdu]
-            else:
-                raise ValueError("No table found in hdu={0}".format(hdu))
-
-        elif len(tables) == 1:
-            table = tables[first(tables)]
-        else:
-            raise ValueError("No table found")
-
-    elif isinstance(input, (TableHDU, BinTableHDU, GroupsHDU)):
-
-        table = input
-
-    else:
-
-        hdulist = fits_open(input)
-
-        try:
-            return read_table_fits(hdulist, hdu=hdu)
-        finally:
-            hdulist.close()
-
-    # Check if table is masked
-    masked = False
-    for col in table.columns:
-        if col.null is not None:
-            masked = True
-            break
-
-    # Convert to an astropy.table.Table object
-    t = Table(table.data, masked=masked)
-
-    # Copy over null values if needed
-    if masked:
-        for col in table.columns:
-            if col.null is not None:
-                t[col.name].set_fill_value(col.null)
-                t[col.name].mask[t[col.name] == col.null] = True
-
-    # Copy over units
-    for col in table.columns:
-        if col.unit is not None:
-            t[col.name].unit = u.Unit(
-                col.unit, format='fits', parse_strict='warn')
-
-    # TODO: deal properly with unsigned integers
-
-    for key, value, comment in table.header.cards:
+    for key, value, comment in header.cards:
 
         if key in ['COMMENT', 'HISTORY']:
-            if key in t.meta:
-                t.meta[key].append(value)
+            if key in meta:
+                meta[key].append(value)
             else:
-                t.meta[key] = [value]
+                meta[key] = [value]
 
-        elif key in t.meta:  # key is duplicate
+        elif key in meta:  # key is duplicate
 
-            if isinstance(t.meta[key], list):
-                t.meta[key].append(value)
+            if isinstance(meta[key], list):
+                meta[key].append(value)
             else:
-                t.meta[key] = [t.meta[key], value]
+                meta[key] = [meta[key], value]
 
         elif (is_column_keyword(key.upper()) or
               key.upper() in REMOVE_KEYWORDS):
@@ -186,61 +76,16 @@ def read_table_fits(input, hdu=None):
 
         else:
 
-            t.meta[key] = value
+            meta[key] = value
 
-    # TODO: implement masking
-
-    return t
+    return meta
 
 
-def write_table_fits(input, output, overwrite=False):
-    """
-    Write a Table object to a FITS file
+def meta_to_header(meta):
 
-    Parameters
-    ----------
-    input : Table
-        The table to write out.
-    output : str
-        The filename to write the table to.
-    overwrite : bool
-        Whether to overwrite any existing file without warning.
-    """
+    header = Header()
 
-    # Check if output file already exists
-    if isinstance(output, string_types) and os.path.exists(output):
-        if overwrite:
-            os.remove(output)
-        else:
-            raise IOError("File exists: {0}".format(output))
-
-    # Create a new HDU object
-    if input.masked:
-        table_hdu = BinTableHDU(np.array(input.filled()))
-        for col in table_hdu.columns:
-            # Binary FITS tables support TNULL *only* for integer data columns
-            # TODO: Determine a schema for handling non-integer masked columns
-            # in FITS (if at all possible)
-            int_formats = ('B', 'I', 'J', 'K')
-            if not (col.format in int_formats or
-                    col.format.p_format in int_formats):
-                continue
-
-            # The astype is necessary because if the string column is less
-            # than one character, the fill value will be N/A by default which
-            # is too long, and so no values will get masked.
-            fill_value = input[col.name].get_fill_value()
-
-            col.null = fill_value.astype(input[col.name].dtype)
-    else:
-        table_hdu = BinTableHDU(np.array(input))
-
-    # Set units for output HDU
-    for col in table_hdu.columns:
-        if input[col.name].unit is not None:
-            col.unit = input[col.name].unit.to_string(format='fits')
-
-    for key, value in input.meta.items():
+    for key, value in meta.items():
 
         if is_column_keyword(key.upper()) or key.upper() in REMOVE_KEYWORDS:
 
@@ -251,7 +96,7 @@ def write_table_fits(input, output, overwrite=False):
         if isinstance(value, list):
             for item in value:
                 try:
-                    table_hdu.header.append((key, item))
+                    header.append((key, item))
                 except ValueError:
                     warnings.warn(
                         "Attribute `{0}` of type {1} cannot be written to "
@@ -259,16 +104,187 @@ def write_table_fits(input, output, overwrite=False):
                         AstropyUserWarning)
         else:
             try:
-                table_hdu.header[key] = value
+                header[key] = value
             except ValueError:
                 warnings.warn(
                     "Attribute `{0}` of type {1} cannot be written to FITS "
                     "files - skipping".format(key, type(value)),
                     AstropyUserWarning)
 
-    # Write out file
-    table_hdu.writeto(output)
+    return header
 
-io_registry.register_reader('fits', Table, read_table_fits)
-io_registry.register_writer('fits', Table, write_table_fits)
-io_registry.register_identifier('fits', Table, is_fits)
+
+class FITSTableIO(BaseIO):
+
+    _format_name = 'fits'
+    _supported_class = Table
+
+    def identify(self, origin, filepath, fileobj, *args, **kwargs):
+        """
+        Determine whether `origin` is a FITS file.
+
+        Parameters
+        ----------
+        origin : str or readable file-like object
+            Path or file object containing a potential FITS file.
+
+        Returns
+        -------
+        is_fits : bool
+            Returns `True` if the given file is a FITS file.
+        """
+        print("IDENTIFY")
+        if fileobj is not None:
+            pos = fileobj.tell()
+            sig = fileobj.read(30)
+            fileobj.seek(pos)
+            return sig == FITS_SIGNATURE
+        elif filepath is not None:
+            if filepath.lower().endswith(('.fits', '.fits.gz', '.fit', '.fit.gz')):
+                return True
+        elif isinstance(args[0], (HDUList, TableHDU, BinTableHDU, GroupsHDU)):
+            return True
+        else:
+            return False
+
+    def read(self, input, hdu=None):
+        """
+        Read a Table object from an FITS file
+
+        Parameters
+        ----------
+        input : str or file-like object or compatible `astropy.io.fits` HDU object
+            If a string, the filename to read the table from. If a file object, or
+            a compatible HDU object, the object to extract the table from. The
+            following `astropy.io.fits` HDU objects can be used as input:
+            - :class:`~astropy.io.fits.hdu.table.TableHDU`
+            - :class:`~astropy.io.fits.hdu.table.BinTableHDU`
+            - :class:`~astropy.io.fits.hdu.table.GroupsHDU`
+            - :class:`~astropy.io.fits.hdu.hdulist.HDUList`
+        hdu : int or str, optional
+            The HDU to read the table from.
+        """
+
+        if isinstance(input, HDUList):
+
+            # Parse all table objects
+            tables = OrderedDict()
+            for ihdu, hdu_item in enumerate(input):
+                if isinstance(hdu_item, (TableHDU, BinTableHDU, GroupsHDU)):
+                    tables[ihdu] = hdu_item
+
+            if len(tables) > 1:
+                if hdu is None:
+                    warnings.warn("hdu= was not specified but multiple tables"
+                                  " are present, reading in first available"
+                                  " table (hdu={0})".format(first(tables)),
+                                  AstropyUserWarning)
+                    hdu = first(tables)
+
+                # hdu might not be an integer, so we first need to convert it
+                # to the correct HDU index
+                hdu = input.index_of(hdu)
+
+                if hdu in tables:
+                    table = tables[hdu]
+                else:
+                    raise ValueError("No table found in hdu={0}".format(hdu))
+
+            elif len(tables) == 1:
+                table = tables[first(tables)]
+            else:
+                raise ValueError("No table found")
+
+        elif isinstance(input, (TableHDU, BinTableHDU, GroupsHDU)):
+
+            table = input
+
+        else:
+
+            hdulist = fits_open(input)
+
+            try:
+                return Table.read(hdulist, hdu=hdu, format='fits')
+            finally:
+                hdulist.close()
+
+        # Check if table is masked
+        masked = False
+        for col in table.columns:
+            if col.null is not None:
+                masked = True
+                break
+
+        # Convert to an astropy.table.Table object
+        t = Table(table.data, masked=masked)
+
+        # Copy over null values if needed
+        if masked:
+            for col in table.columns:
+                if col.null is not None:
+                    t[col.name].set_fill_value(col.null)
+                    t[col.name].mask[t[col.name] == col.null] = True
+
+        # Copy over units
+        for col in table.columns:
+            if col.unit is not None:
+                t[col.name].unit = u.Unit(
+                    col.unit, format='fits', parse_strict='warn')
+
+        # TODO: deal properly with unsigned integers
+
+        t.meta.update(header_to_meta(table.header))
+
+        return t
+
+    def write(self, input, output, overwrite=False):
+        """
+        Write a Table object to a FITS file
+
+        Parameters
+        ----------
+        input : Table
+            The table to write out.
+        output : str
+            The filename to write the table to.
+        overwrite : bool
+            Whether to overwrite any existing file without warning.
+        """
+
+        # Check if output file already exists
+        if isinstance(output, string_types) and os.path.exists(output):
+            if overwrite:
+                os.remove(output)
+            else:
+                raise IOError("File exists: {0}".format(output))
+
+        # Create a new HDU object
+        if input.masked:
+            table_hdu = BinTableHDU(np.array(input.filled()))
+            for col in table_hdu.columns:
+                # Binary FITS tables support TNULL *only* for integer data columns
+                # TODO: Determine a schema for handling non-integer masked columns
+                # in FITS (if at all possible)
+                int_formats = ('B', 'I', 'J', 'K')
+                if not (col.format in int_formats or
+                        col.format.p_format in int_formats):
+                    continue
+
+                # The astype is necessary because if the string column is less
+                # than one character, the fill value will be N/A by default which
+                # is too long, and so no values will get masked.
+                fill_value = input[col.name].get_fill_value()
+
+                col.null = fill_value.astype(input[col.name].dtype)
+        else:
+            table_hdu = BinTableHDU(np.array(input))
+
+        # Set units for output HDU
+        for col in table_hdu.columns:
+            if input[col.name].unit is not None:
+                col.unit = input[col.name].unit.to_string(format='fits')
+
+        table_hdu.header.update(meta_to_header(input.meta))
+
+        # Write out file
+        table_hdu.writeto(output)
