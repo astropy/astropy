ad4565fb8072b51f745dc4ec38a3e363a6dec25f
Larry Bradley <larry.bradley@gmail.com>
2014-02-20 11:52:35 -0500

    Added new Gaussian2D model test

--- astropy/modeling/tests/test_functional_models.py
+++ astropy/modeling/tests/test_functional_models.py
@@ -7,11 +7,13 @@ import numpy as np
 from numpy.testing import assert_allclose
 from .. import models
 
+
 try:
     from scipy import optimize
     HAS_SCIPY = True
 except ImportError:
     HAS_SCIPY = False
+
 
 def test_Trapezoid1D():
     """Regression test for
@@ -22,3 +24,20 @@ def test_Trapezoid1D():
     yy = model(xx)
     yy_ref = [0., 1.41428571, 3.12857143, 4.2, 4.2, 3.12857143, 1.41428571, 0.]
     assert_allclose(yy, yy_ref, rtol=0, atol=1e-6)
+
+
+def test_Gaussian2D():
+    """
+    Test rotated elliptical Gaussian2D model.
+    https://github.com/astropy/astropy/pull/2038
+    """
+    model = models.Gaussian2D(100, 1.7, 3.1, x_stddev=3.3, y_stddev=5.0,
+                              theta=np.pi/6.)
+    x, y = np.mgrid[0:5, 0:5]
+    g = model(x, y)
+    g_ref = [[77.86787722, 86.01743889, 90.11888627, 89.54601432, 84.38744554],
+             [79.8450774, 90.20335997, 96.6492347, 98.21442091, 94.65710923],
+             [75.66323816, 87.419011, 95.79172412, 99.55228375, 98.12408063],
+             [66.26262987, 78.29536082, 87.74139348, 93.25543692, 94.00369635],
+             [53.6289606, 64.80568981, 74.27249818, 80.73169335, 83.22642276]]
+    assert_allclose(g, g_ref, rtol=0, atol=1e-6)
