15812b42012784f07f3d090ced646fc08182032b
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-02-23 11:48:27 +0100

    Avoid repeated labels

--- wcsaxes/ticklabels.py
+++ wcsaxes/ticklabels.py
@@ -3,6 +3,9 @@ import numpy as np
 from matplotlib.text import Text
 
 
+def sort_using(X, Y):
+    return [x for (y,x) in sorted(zip(Y,X))]
+
 class TickLabels(Text):
 
     def __init__(self, *args, **kwargs):
@@ -18,18 +21,56 @@ class TickLabels(Text):
         self.pixel = {}
         self.angle = {}
         self.text = {}
+        self.disp = {}
 
-    def add(self, axis, world, pixel, angle, text):
+    def add(self, axis, world, pixel, angle, text, axis_displacement):
         if axis not in self.world:
             self.world[axis] = [world]
             self.pixel[axis] = [pixel]
             self.angle[axis] = [angle]
             self.text[axis] = [text]
+            self.disp[axis] = [axis_displacement]
         else:
             self.world[axis].append(world)
             self.pixel[axis].append(pixel)
             self.angle[axis].append(angle)
             self.text[axis].append(text)
+            self.disp[axis].append(axis_displacement)
+
+    def sort(self):
+        """
+        Sort by axis displacement, which allows us to figure out which parts
+        of labels to not repeat.
+        """
+        for axis in self.world:
+            self.world[axis] = sort_using(self.world[axis], self.disp[axis])
+            self.pixel[axis] = sort_using(self.pixel[axis], self.disp[axis])
+            self.angle[axis] = sort_using(self.angle[axis], self.disp[axis])
+            self.text[axis] = sort_using(self.text[axis], self.disp[axis])
+            self.disp[axis] = sort_using(self.disp[axis], self.disp[axis])
+
+    def simplify_labels(self):
+        """
+        Figure out which parts of labels can be dropped to avoid repetition.
+        """
+        self.sort()
+        for axis in self.world:
+            t1 = self.text[axis][0]
+            for i in range(1, len(self.world[axis])):
+                t2 = self.text[axis][i]
+                if len(t1) != len(t2):
+                    t1 = self.text[axis][i]
+                    continue
+                start = 0
+                for j in range(len(t1)):
+                    if t1[j] != t2[j]:
+                        break
+                    if t1[j] not in '0123456789.':
+                        start = j + 1
+                if start == 0:
+                    t1 = self.text[axis][i]
+                else:
+                    self.text[axis][i] = self.text[axis][i][start:]
 
     def set_pixel_to_data_scaling(self, xscale, yscale):
         self._x_pixel_to_data = xscale
@@ -45,6 +86,8 @@ class TickLabels(Text):
             return self._visible_axes
 
     def draw(self, renderer):
+
+        self.simplify_labels()
 
         text_size = renderer.points_to_pixels(self.get_size())
 
