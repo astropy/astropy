b93d2eefd3a7121142e8f2bdd43455b37063563b
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-02-24 12:53:40 -0500

    now with optional kwarg allow_negative

--- astropy/coordinates/distances.py
+++ astropy/coordinates/distances.py
@@ -62,7 +62,7 @@ class Distance(u.Quantity):
         If ``z`` is provided with a ``unit`` or ``cosmology`` is provided when ``z`` is
         *not* given, or ``value`` is given as well as ``z``
     ValueError
-        If value specified is less than 0.
+        If value specified is less than 0 and `allow_negative=False`.
 
     Examples
     --------
@@ -81,7 +81,7 @@ class Distance(u.Quantity):
     _include_easy_conversion_members = True
 
     def __new__(cls, value=None, unit=None, z=None, cosmology=None,
-                distmod=None, dtype=None, copy=True):
+                distmod=None, dtype=None, copy=True, allow_negative=False):
         from ..cosmology import get_current
 
         if isinstance(value, u.Quantity):
@@ -151,7 +151,7 @@ class Distance(u.Quantity):
         if value.dtype.kind not in 'iuf':
             raise TypeError("Unsupported dtype '{0}'".format(value.dtype))
 
-        if np.any(value < 0):
+        if np.any(value < 0) and not allow_negative:
             raise ValueError("Distance must be >= 0.")
 
         return super(Distance, cls).__new__(cls, value, unit, dtype=dtype,
