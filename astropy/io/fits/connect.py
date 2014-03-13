5b226bee1455b7bef6e68cfe42c419eafd5d3ff8
Shailesh Ahuja <shailesh.ahuja03@gmail.com>
2014-03-14 02:51:20 +0800

    Changing default value for float columns from 1e20 to Nan in fits table write function

--- astropy/io/fits/connect.py
+++ astropy/io/fits/connect.py
@@ -218,6 +218,7 @@ def write_table_fits(input, output, overwrite=False):
 
     # Create a new HDU object
     if input.masked:
+        #float column's default mask value needs to be Nan 
         for column in six.itervalues(input.columns):
             if column.dtype.kind == 'f' and column.get_fill_value() == 1e20:
                 column.set_fill_value(np.nan)
