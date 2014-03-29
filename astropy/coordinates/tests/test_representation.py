f684ae3efe82610fee2d9b5650f6fcfe012708b4
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-03-29 19:14:20 +0100

    Added tests for SphericalRepresentation

--- astropy/coordinates/tests/test_representation.py
+++ astropy/coordinates/tests/test_representation.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy.testing import assert_allclose
 
 from ... import units as u
@@ -22,15 +23,25 @@ class TestSphericalRepresentation(object):
         assert s1.lat == 5. * u.deg
         assert s1.distance is None
 
+        assert isinstance(s1.lon, Longitude)
+        assert isinstance(s1.lat, Latitude)
+
         s2 = SphericalRepresentation(lon=8 * u.hourangle, lat=5 * u.deg)
         assert s2.lon == 8. * u.hourangle
         assert s2.lat == 5. * u.deg
         assert s2.distance is None
 
+        assert isinstance(s2.lon, Longitude)
+        assert isinstance(s2.lat, Latitude)
+
         s3 = SphericalRepresentation(lon=8 * u.hourangle, lat=5 * u.deg, distance=10 * u.kpc)
         assert s3.lon == 8. * u.hourangle
         assert s3.lat == 5. * u.deg
         assert s3.distance == 10 * u.kpc
+
+        assert isinstance(s3.lon, Longitude)
+        assert isinstance(s3.lat, Latitude)
+        assert isinstance(s3.distance, Distance)
 
     def test_init_lonlat(self):
 
@@ -41,13 +52,20 @@ class TestSphericalRepresentation(object):
         assert s1.lat == 5. * u.deg
         assert s1.distance is None
 
+        assert isinstance(s1.lon, Longitude)
+        assert isinstance(s1.lat, Latitude)
+
         s2 = SphericalRepresentation(Longitude(8, u.hour),
                                      Latitude(5, u.deg),
                                      Distance(10, u.kpc))
 
-        assert s1.lon == 8. * u.hourangle
-        assert s1.lat == 5. * u.deg
-        assert s1.distance == 10. * u.kpc
+        assert s2.lon == 8. * u.hourangle
+        assert s2.lat == 5. * u.deg
+        assert s2.distance == 10. * u.kpc
+
+        assert isinstance(s2.lon, Longitude)
+        assert isinstance(s2.lat, Latitude)
+        assert isinstance(s2.distance, Distance)
 
     def test_init_array(self):
 
@@ -56,6 +74,9 @@ class TestSphericalRepresentation(object):
 
         assert_allclose(s1.lon.degree, [120, 135])
         assert_allclose(s1.lat.degree, [5, 6])
+
+        assert isinstance(s1.lon, Longitude)
+        assert isinstance(s1.lat, Latitude)
 
     def test_init_array_nocopy(self):
 
@@ -73,3 +94,51 @@ class TestSphericalRepresentation(object):
         s1 = SphericalRepresentation(lon='2h6m3.3s', lat='0.1rad')
         assert_allclose(s1.lon.degree, 31.513749999999995)
         assert_allclose(s1.lat.degree, 5.729577951308233)
+
+    def test_reprobj(self):
+
+        s1 = SphericalRepresentation(lon=8 * u.hourangle, lat=5 * u.deg, distance=10 * u.kpc)
+
+        s2 = SphericalRepresentation(representation=s1)
+
+        assert s2.lon == 8. * u.hourangle
+        assert s2.lat == 5. * u.deg
+        assert s2.distance == 10 * u.kpc
+
+    def test_reprobj_invalid(self):
+
+        s1 = SphericalRepresentation(lon=8 * u.hourangle, lat=5 * u.deg, distance=10 * u.kpc)
+
+        with pytest.raises(ValueError) as exc:
+            s2 = SphericalRepresentation(lon=8 * u.hourangle, lat=5 * u.deg, representation=s1)
+        assert exc.value.args[0] == "If representation is passed, no other arguments can be passed"
+
+    def test_broadcasting(self):
+
+        s1 = SphericalRepresentation(lon=[8, 9]*u.hourangle,
+                                     lat=[5, 6]*u.deg,
+                                     distance=10*u.kpc)
+
+        assert_allclose(s1.lon.degree, [120, 135])
+        assert_allclose(s1.lat.degree, [5, 6])
+        assert_allclose(s1.distance.kpc, [10, 10])
+
+    def test_broadcasting_mismatch(self):
+
+        with pytest.raises(ValueError) as exc:
+            s1 = SphericalRepresentation(lon=[8, 9, 10]*u.hourangle,
+                                         lat=[5, 6]*u.deg)
+        assert exc.value.args[0] == "Input arrays cannot be broadcast"
+
+    def test_mixed_units(self):
+
+        # It's also possible to pass in scalar quantity lists with mixed
+        # units. These are converted to array quantities following the same
+        # rule as `Quantity`: all elements are converted to match the first
+        # element's units.
+
+        s1 = SphericalRepresentation(lon=[8*u.hourangle, 135*u.deg],
+                                     lat=[5*u.deg, (6*np.pi/180)*u.rad])
+        assert s1.lat.unit == u.deg and s1.lon.unit == u.hourangle
+        assert_allclose(s1.lon[1].hourangle, 9)
+        assert_allclose(s1.lat[1].degree, 6)
