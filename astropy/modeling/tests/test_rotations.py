03171234e8079dc57a210d6b2cb3cc36a7f92dd6
Larry Bradley <larry.bradley@gmail.com>
2014-03-31 22:34:12 -0400

    Updated tests for MatrixRotation2D model

--- astropy/modeling/tests/test_rotations.py
+++ astropy/modeling/tests/test_rotations.py
@@ -44,4 +44,4 @@ def test_native_celestial_theta90():
 def test_MatrixRotation2D():
     model = models.MatrixRotation2D(angle=90)
     x, y = model(1, 0)
-    utils.assert_allclose([x, y], [0, -1], atol=1e-10)
+    utils.assert_allclose([x, y], [0, 1], atol=1e-10)
