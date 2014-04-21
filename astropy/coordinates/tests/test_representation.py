3042f9b00376cee81b5101021ce2eed4bbefcecc
Erik Tollerud <erik.tollerud@gmail.com>
2014-04-21 02:11:40 -0400

    Added std header to test_representation

--- astropy/coordinates/tests/test_representation.py
+++ astropy/coordinates/tests/test_representation.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 import numpy as np
 from numpy.testing import assert_allclose
 
