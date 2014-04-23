5bacff7c80296856f6f467d405438d524b79d5af
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-04-23 12:10:31 -0400

    passing icrs to fk5

--- astropy/coordinates/tests/accuracy/test_icrs_fk5.py
+++ astropy/coordinates/tests/accuracy/test_icrs_fk5.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 
 from .... import units as u
-from ... import ICRS, FK5
+from ...builtin_frames import ICRS, FK5
 from ....time import Time
 from ....table import Table
 from ...angle_utilities import angular_separation
@@ -26,27 +26,25 @@ def test_icrs_no_e_fk5():
         # Extract row
         r = t[i]
 
-        # FK4 to FK5
-        c1 = ICRS(r['ra_in'], r['dec_in'],
-                             unit=(u.degree, u.degree),
-                             obstime=Time(r['obstime'], scale='utc'))
-        c2 = c1.transform_to(FK5).precess_to(Time(r['equinox_fk5'], scale='utc'))
+        # ICRS to FK5
+        c1 = ICRS(ra=r['ra_in']*u.deg, dec=r['dec_in']*u.deg)
+        c2 = c1.transform_to(FK5(equinox=Time(r['equinox_fk5'], scale='utc')))
 
         # Find difference
         diff = angular_separation(c2.ra.radian, c2.dec.radian,
-                                    np.radians(r['ra_fk5']), np.radians(r['dec_fk5']))
+                                  np.radians(r['ra_fk5']),
+                                  np.radians(r['dec_fk5']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
 
-        # FK5 to FK4
-        c1 = FK5(r['ra_in'], r['dec_in'],
-                            unit=(u.degree, u.degree),
-                            obstime=Time(r['obstime'], scale='utc'),
-                            equinox=Time(r['equinox_fk5'], scale='utc'))
+        # FK5 to ICRS
+        c1 = FK5(ra=r['ra_in']*u.deg, dec=r['dec_in']*u.deg,
+                 equinox=Time(r['equinox_fk5'], scale='utc'))
         c2 = c1.transform_to(ICRS)
 
         # Find difference
         diff = angular_separation(c2.ra.radian, c2.dec.radian,
-                                    np.radians(r['ra_icrs']), np.radians(r['dec_icrs']))
+                                  np.radians(r['ra_icrs']),
+                                  np.radians(r['dec_icrs']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
