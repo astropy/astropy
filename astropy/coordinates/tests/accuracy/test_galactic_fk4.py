81f80c1dfaa21dfea604d549a17a021ef69c9fdb
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-04-23 11:59:44 -0400

    passing galactic to fk4 tests

--- astropy/coordinates/tests/accuracy/test_galactic_fk4.py
+++ astropy/coordinates/tests/accuracy/test_galactic_fk4.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 
 from .... import units as u
-from ... import Galactic, FK4
+from ...builtin_frames import Galactic, FK4
 from ....time import Time
 from ....table import Table
 from ...angle_utilities import angular_separation
@@ -27,27 +27,26 @@ def test_galactic_fk4():
         # Extract row
         r = t[i]
 
-        # FK4 to FK5
-        c1 = Galactic(r['lon_in'], r['lat_in'],
-                                 unit=(u.degree, u.degree),
-                                 obstime=Time(r['obstime'], scale='utc'))
-        c2 = c1.transform_to(FK4).precess_to(Time(r['equinox_fk4'], scale='utc'))
+        # Galactic to FK4
+        c1 = Galactic(l=r['lon_in']*u.deg, b=r['lat_in']*u.deg)
+        c2 = c1.transform_to(FK4(equinox=Time(r['equinox_fk4'], scale='utc')))
 
         # Find difference
         diff = angular_separation(c2.ra.radian, c2.dec.radian,
-                                    np.radians(r['ra_fk4']), np.radians(r['dec_fk4']))
+                                  np.radians(r['ra_fk4']),
+                                  np.radians(r['dec_fk4']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
 
-        # FK5 to FK4
-        c1 = FK4(r['lon_in'], r['lat_in'],
-                            unit=(u.degree, u.degree),
-                            obstime=Time(r['obstime'], scale='utc'),
-                            equinox=Time(r['equinox_fk4'], scale='utc'))
+        # FK4 to Galactic
+        c1 = FK4(ra=r['lon_in']*u.deg, dec=r['lat_in']*u.deg,
+                 obstime=Time(r['obstime'], scale='utc'),
+                 equinox=Time(r['equinox_fk4'], scale='utc'))
         c2 = c1.transform_to(Galactic)
 
         # Find difference
         diff = angular_separation(c2.l.radian, c2.b.radian,
-                                    np.radians(r['lon_gal']), np.radians(r['lat_gal']))
+                                  np.radians(r['lon_gal']),
+                                  np.radians(r['lat_gal']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
