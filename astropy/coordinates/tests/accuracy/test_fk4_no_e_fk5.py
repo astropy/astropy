e386256d4fd52500d71656dfada2bf00d28cf8ee
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-04-23 12:06:14 -0400

    updated code but accuracy test failing

--- astropy/coordinates/tests/accuracy/test_fk4_no_e_fk5.py
+++ astropy/coordinates/tests/accuracy/test_fk4_no_e_fk5.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 
 from .... import units as u
-from ... import FK4NoETerms, FK5
+from ...builtin_frames import FK4NoETerms, FK5
 from ....time import Time
 from ....table import Table
 from ...angle_utilities import angular_separation
@@ -24,28 +24,28 @@ def test_fk4_no_e_fk5():
         # Extract row
         r = t[i]
 
-        # FK4 to FK5
-        c1 = FK4NoETerms(r['ra_in'], r['dec_in'],
-                                   unit=(u.degree, u.degree),
-                                   obstime=Time(r['obstime'], scale='utc'),
-                                   equinox=Time(r['equinox_fk4'], scale='utc'))
-        c2 = c1.transform_to(FK5).precess_to(Time(r['equinox_fk5'], scale='utc'))
+        # FK4NoETerms to FK5
+        c1 = FK4NoETerms(ra=r['ra_in']*u.deg, dec=r['dec_in']*u.deg,
+                         obstime=Time(r['obstime'], scale='utc'),
+                         equinox=Time(r['equinox_fk4'], scale='utc'))
+        c2 = c1.transform_to(FK5(equinox=Time(r['equinox_fk5'], scale='utc')))
 
         # Find difference
         diff = angular_separation(c2.ra.radian, c2.dec.radian,
-                                    np.radians(r['ra_fk5']), np.radians(r['dec_fk5']))
+                                  np.radians(r['ra_fk5']),
+                                  np.radians(r['dec_fk5']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
 
-        # FK5 to FK4
-        c1 = FK5(r['ra_in'], r['dec_in'],
-                            unit=(u.degree, u.degree),
-                            obstime=Time(r['obstime'], scale='utc'),
-                            equinox=Time(r['equinox_fk5'], scale='utc'))
-        c2 = c1.transform_to(FK4NoETerms).precess_to(Time(r['equinox_fk4'], scale='utc'))
+        # FK5 to FK4NoETerms
+        c1 = FK5(ra=r['ra_in']*u.deg, dec=r['dec_in']*u.deg,
+                 obstime=Time(r['obstime'], scale='utc'),
+                 equinox=Time(r['equinox_fk5'], scale='utc'))
+        c2 = c1.transform_to(FK4NoETerms(equinox=Time(r['equinox_fk4'], scale='utc')))
 
         # Find difference
         diff = angular_separation(c2.ra.radian, c2.dec.radian,
-                                    np.radians(r['ra_fk4']), np.radians(r['dec_fk4']))
+                                  np.radians(r['ra_fk4']),
+                                  np.radians(r['dec_fk4']))
 
         assert np.degrees(diff) * 3600. < TOLERANCE
