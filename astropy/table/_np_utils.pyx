a8cd306f02b7c3221037cf6aaf1b08f369e1c4be
cgohlke <cgohlke@uci.edu>
2014-05-14 18:35:47 -0700

    Use np.intp_t for indices

--- astropy/table/_np_utils.pyx
+++ astropy/table/_np_utils.pyx
@@ -12,7 +12,7 @@ from numpy.lib.recfunctions import drop_fields
 cimport cython
 cimport numpy as np
 DTYPE = np.int
-ctypedef np.int_t DTYPE_t
+ctypedef np.intp_t DTYPE_t
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
