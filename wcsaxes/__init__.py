819d8d0cf86724350280b0afe0ba53257c231221
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-06-12 12:01:50 +0200

    Make use of astropy affiliated package infrastructure

--- wcsaxes/__init__.py
+++ wcsaxes/__init__.py
@@ -1,2 +1,15 @@
-from .wcsaxes import *
-from .coordinate_helpers import CoordinateHelper
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+WCSAxes implementation
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+if not _ASTROPY_SETUP_:
+    from .wcsaxes import *
+    from .coordinate_helpers import CoordinateHelper
