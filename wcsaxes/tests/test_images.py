14e25a3468c0db7a81f56857d781a1df13bb4b79
Asra Nizami <anizami@macalester.edu>
2014-06-25 19:39:16 +0500

    removed commented out code etc

--- wcsaxes/tests/test_images.py
+++ wcsaxes/tests/test_images.py
@@ -11,11 +11,10 @@ from astropy.io import fits
 from wcsaxes import WCSAxes
 from matplotlib import cbook
 from astropy.tests.helper import pytest
-from astropy.utils.data import download_file
 from astropy.tests.helper import remote_data
 from wcsaxes import datasets
 
-@remote_data
+
 class TestImages(object):
 
     @classmethod
@@ -51,7 +50,7 @@ class TestImages(object):
             assert msg is None
 
     # Test for plotting image and also setting values of ticks
-    # @remote_data
+    @remote_data
     def test_image_plot(self, generate):
         hdu = self._image1_hdu
         fig = plt.figure(figsize=(6, 6))
@@ -59,13 +58,12 @@ class TestImages(object):
         fig.add_axes(ax)
         ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
         ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
-#        ax.imshow(hdu.data, vmin=-1e-5, vmax=1e-4, origin='lower')
         ax.coords[0].set_ticks([-0.30, 0., 0.20] * u.degree, size=5, width=1)
 
         self.generate_or_test(generate, fig, 'image_plot.png')
 
     # Test for overlaying contours on images
-    # @remote_data
+    @remote_data
     def test_contour_overlay(self, generate):
         hdu = self._image3_hdu
 
@@ -77,7 +75,6 @@ class TestImages(object):
         fig.add_axes(ax)
         ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
         ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
-        #ax.imshow(hdu.data, vmin=-100, vmax=3000, origin='lower')
         # Overplot contour
         ax.contour(hdu_msx.data, transform=ax.get_transform(wcs_msx), colors='orange', levels=[2.5e-5, 5e-5, 1.e-4])
         ax.coords[0].set_ticks(size=5, width=1)
@@ -88,7 +85,7 @@ class TestImages(object):
         self.generate_or_test(generate, fig, 'contour_overlay.png')
 
     # Test for overlaying grid, changing format of ticks, setting spacing and number of ticks
-    # @remote_data
+    @remote_data
     def test_overlay_features_image(self, generate):
         hdu = self._image1_hdu
         fig = plt.figure(figsize=(6, 6))
@@ -112,7 +109,7 @@ class TestImages(object):
         self.generate_or_test(generate, fig, 'overlay_features_image.png')
 
     # Overlay curvilinear grid and patches on image
-    # @remote_data
+    @remote_data
     def test_curvilinear_grid_patches_image(self, generate):
         hdu = self._image2_hdu
         fig = plt.figure(figsize=(8, 8))
@@ -120,7 +117,6 @@ class TestImages(object):
         fig.add_axes(ax)
         ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
         ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
-        #ax.imshow(hdu.data, vmax=1000, origin='lower')
         ax.grid(color='black', alpha=1.0, lw=1, linestyle='dashed')
         p = Circle((300, 100), radius=40, ec='yellow', fc='none')
         ax.add_patch(p)
@@ -134,7 +130,7 @@ class TestImages(object):
         self.generate_or_test(generate, fig, 'curvlinear_grid_patches_image.png')
 
     # Test for cube slicing
-    # @remote_data
+    @remote_data
     def test_cube_slice_image(self, generate):
         image = self._cube_hdu.data
         w = WCS(self._cube_hdu.header)
@@ -143,7 +139,6 @@ class TestImages(object):
         fig.add_axes(ax)
         ax.set_xlim(-0.5, image.shape[0] - 0.5)
         ax.set_ylim(-0.5, image.shape[1] - 0.5)
-        #ax.imshow(image[:, :, 100].transpose(), cmap=plt.cm.gist_heat, origin='lower')
         ax.coords[2].set_axislabel('Velocity m/s')
         ax.coords[1].set_ticks(width=1)
         ax.coords[2].set_ticks(width=1)
@@ -151,7 +146,7 @@ class TestImages(object):
         self.generate_or_test(generate, fig, 'cube_slice_image.png')
 
     # Test to see if changing the units of axis works
-    # @remote_data
+    @remote_data
     def test_changed_axis_units(self, generate):
         image = self._cube_hdu.data
         w = WCS(self._cube_hdu.header)
@@ -160,7 +155,6 @@ class TestImages(object):
         fig.add_axes(ax)
         ax.set_xlim(-0.5, image.shape[0] - 0.5)
         ax.set_ylim(-0.5, image.shape[1] - 0.5)
-        #ax.imshow(image[:, :, 100].transpose(), cmap=plt.cm.gist_heat, origin='lower')
         ax.coords[2].set_major_formatter('x.xx')
         ax.coords[2].set_format_unit(u.km / u.s)
         ax.coords[2].set_axislabel('Velocity km/s')
