5eb060f7d1253a48f2506866730f6694f54f223d
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2014-07-01 15:31:12 +0200

    Added test for reconnecting SAMP clients.

--- astropy/vo/samp/tests/test_client.py
+++ astropy/vo/samp/tests/test_client.py
@@ -1,8 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+import pytest
+
 from ..hub_proxy import SAMPHubProxy
 from ..client import SAMPClient
 from ..integrated_client import SAMPIntegratedClient
+from ..hub import SAMPHubServer
 
 # By default, tests should not use the internet.
 from .. import conf
@@ -10,6 +13,7 @@ from .. import conf
 def setup_module(module):
     conf.use_internet = False
 
+o
 
 def test_SAMPHubProxy():
     """Test that SAMPHubProxy can be instantiated"""
@@ -25,3 +29,28 @@ def test_SAMPClient():
 def test_SAMPIntegratedClient():
     """Test that SAMPIntegratedClient can be instantiated"""
     SAMPIntegratedClient()
+
+
+# Not yet included because it seems that there is a problem with
+# instantiating multiple hubs.
+#def test_SAMPHubServer():
+#    """Test that SAMPHubServer can be instantiated"""
+#    my_hub = SAMPHubServer()
+#    my_hub.start()
+#    my_hub.stop()
+
+
+@pytest.fixture
+def samp_hub(request):
+    my_hub = SAMPHubServer()
+    my_hub.start()
+    request.addfinalizer(my_hub.stop)
+
+
+def test_reconnect(samp_hub):
+    """Test that SAMPIntegratedClient can reconnect."""
+    my_client = SAMPIntegratedClient()
+    my_client.connect()
+    my_client.disconnect()
+    my_client.connect()
+
