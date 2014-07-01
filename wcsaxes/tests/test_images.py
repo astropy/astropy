bd7b138024be5722f8d8820ebfc90df1442dd291
Asra Nizami <anizami@macalester.edu>
2014-07-02 02:17:38 +0500

    uses header files instead of the hdu object

--- wcsaxes/tests/test_images.py
+++ wcsaxes/tests/test_images.py
@@ -15,15 +15,15 @@ from astropy.tests.helper import remote_data
 from wcsaxes import datasets
 
 
-@remote_data
 class BaseImageTests(object):
-
+    # TODO: move header files to data directory, add them to config files so they get copied over
     @classmethod
     def setup_class(cls):
         cls._filedir = os.path.abspath(__file__)
         cls._basedir = os.path.split(cls._filedir)[0]
         cls._baseline_images_dir = os.path.join(cls._basedir, 'baseline_images')
         cls._result_dir = os.path.abspath(os.path.join(cls._basedir, 'test_result_images'))
+        cls._data_dir = os.path.abspath(os.path.join(cls._basedir, 'data'))
 
         if not os.path.exists(cls._result_dir):
             cbook.mkdirs(cls._result_dir)
@@ -31,11 +31,22 @@ class BaseImageTests(object):
         if not os.path.exists(cls._baseline_images_dir):
             cbook.mkdirs(cls._baseline_images_dir)
 
+        if not os.path.exists(cls._data_dir):
+            cbook.mkdirs(cls._data_dir)
+
         cls._tolerance = 1
-        cls._msx_hdu = datasets.msx_hdu()
-        cls._rosat_hdu = datasets.rosat_hdu()
-        cls._twoMASS_k_hdu = datasets.twoMASS_k_hdu()
-        cls._cube_hdu = datasets.l1448_co_hdu()
+
+        msx_header = os.path.join(cls._data_dir, 'msx_header')
+        cls.msx_header = fits.Header.fromtextfile(msx_header)
+
+        rosat_header = os.path.join(cls._data_dir, 'rosat_header')
+        cls.rosat_header = fits.Header.fromtextfile(rosat_header)
+
+        twoMASS_k_header = os.path.join(cls._data_dir, '2MASS_k_header')
+        cls.twoMASS_k_header = fits.Header.fromtextfile(twoMASS_k_header)
+
+        cube_header = os.path.join(cls._data_dir, 'cube_header')
+        cls.cube_header = fits.Header.fromtextfile(cube_header)
 
     # method to create baseline or test images
     def generate_or_test(self, generate, figure, image, test_image=None, baseline_image=None):
@@ -53,14 +64,12 @@ class BaseImageTests(object):
 class TestBasic(BaseImageTests):
 
     # Test for plotting image and also setting values of ticks
-    @remote_data
     def test_image_plot(self, generate):
-        hdu = self._msx_hdu
         fig = plt.figure(figsize=(6, 6))
-        ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], wcs=WCS(hdu.header), aspect='equal')
+        ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], wcs=WCS(self.msx_header), aspect='equal')
         fig.add_axes(ax)
-        ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
-        ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
+        ax.set_xlim(-0.5, 149 - 0.5)
+        ax.set_ylim(-0.5, 149 - 0.5)
         ax.coords[0].set_ticks([-0.30, 0., 0.20] * u.degree, size=5, width=1)
 
         self.generate_or_test(generate, fig, 'image_plot.png')
@@ -68,15 +77,14 @@ class TestBasic(BaseImageTests):
     # Test for overlaying contours on images
     @remote_data
     def test_contour_overlay(self, generate):
-        hdu = self._twoMASS_k_hdu
-        hdu_msx = self._msx_hdu
-        wcs_msx = WCS(hdu_msx.header)
+        hdu_msx = datasets.msx_hdu()
+        wcs_msx = WCS(self.msx_header)
 
         fig = plt.figure(figsize=(6, 6))
-        ax = WCSAxes(fig, [0.15, 0.15, 0.8, 0.8], wcs=WCS(hdu.header), aspect='equal')
+        ax = WCSAxes(fig, [0.15, 0.15, 0.8, 0.8], wcs=WCS(self.twoMASS_k_header), aspect='equal')
         fig.add_axes(ax)
-        ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
-        ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
+        ax.set_xlim(-0.5, 721 - 0.5)
+        ax.set_ylim(-0.5, 720 - 0.5)
         # Overplot contour
         ax.contour(hdu_msx.data, transform=ax.get_transform(wcs_msx), colors='orange', levels=[2.5e-5, 5e-5, 1.e-4])
         ax.coords[0].set_ticks(size=5, width=1)
@@ -87,11 +95,9 @@ class TestBasic(BaseImageTests):
         self.generate_or_test(generate, fig, 'contour_overlay.png')
 
     # Test for overlaying grid, changing format of ticks, setting spacing and number of ticks
-    @remote_data
     def test_overlay_features_image(self, generate):
-        hdu = self._msx_hdu
         fig = plt.figure(figsize=(6, 6))
-        ax = WCSAxes(fig, [0.25, 0.25, 0.65, 0.65], wcs=WCS(hdu.header), aspect='equal')
+        ax = WCSAxes(fig, [0.25, 0.25, 0.65, 0.65], wcs=WCS(self.msx_header), aspect='equal')
         fig.add_axes(ax)
         # Change the format of the ticks
         ax.coords[0].set_major_formatter('dd:mm:ss')
@@ -111,14 +117,12 @@ class TestBasic(BaseImageTests):
         self.generate_or_test(generate, fig, 'overlay_features_image.png')
 
     # Overlay curvilinear grid and patches on image
-    @remote_data
     def test_curvilinear_grid_patches_image(self, generate):
-        hdu = self._rosat_hdu
         fig = plt.figure(figsize=(8, 8))
-        ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], wcs=WCS(hdu.header), aspect='equal')
+        ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], wcs=WCS(self.rosat_header), aspect='equal')
         fig.add_axes(ax)
-        ax.set_xlim(-0.5, hdu.data.shape[1] - 0.5)
-        ax.set_ylim(-0.5, hdu.data.shape[0] - 0.5)
+        ax.set_xlim(-0.5, 480 - 0.5)
+        ax.set_ylim(-0.5, 240 - 0.5)
         ax.grid(color='black', alpha=1.0, lw=1, linestyle='dashed')
         p = Circle((300, 100), radius=40, ec='yellow', fc='none')
         ax.add_patch(p)
@@ -132,15 +136,13 @@ class TestBasic(BaseImageTests):
         self.generate_or_test(generate, fig, 'curvlinear_grid_patches_image.png')
 
     # Test for cube slicing
-    @remote_data
     def test_cube_slice_image(self, generate):
-        image = self._cube_hdu.data
-        w = WCS(self._cube_hdu.header)
+        w = WCS(self.cube_header)
         fig = plt.figure()
         ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], w, slices=(50, 'y', 'x'), aspect='equal')
         fig.add_axes(ax)
-        ax.set_xlim(-0.5, image.shape[0] - 0.5)
-        ax.set_ylim(-0.5, image.shape[1] - 0.5)
+        ax.set_xlim(-0.5, 53 - 0.5)
+        ax.set_ylim(-0.5, 107 - 0.5)
         ax.coords[2].set_axislabel('Velocity m/s')
         ax.coords[1].set_ticks(width=1)
         ax.coords[2].set_ticks(width=1)
@@ -148,15 +150,13 @@ class TestBasic(BaseImageTests):
         self.generate_or_test(generate, fig, 'cube_slice_image.png')
 
     # Test to see if changing the units of axis works
-    @remote_data
     def test_changed_axis_units(self, generate):
-        image = self._cube_hdu.data
-        w = WCS(self._cube_hdu.header)
+        w = WCS(self.cube_header)
         fig = plt.figure()
         ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8], w, slices=(50, 'y', 'x'), aspect='equal')
         fig.add_axes(ax)
-        ax.set_xlim(-0.5, image.shape[0] - 0.5)
-        ax.set_ylim(-0.5, image.shape[1] - 0.5)
+        ax.set_xlim(-0.5, 53 - 0.5)
+        ax.set_ylim(-0.5, 107 - 0.5)
         ax.coords[2].set_major_formatter('x.xx')
         ax.coords[2].set_format_unit(u.km / u.s)
         ax.coords[2].set_axislabel('Velocity km/s')
