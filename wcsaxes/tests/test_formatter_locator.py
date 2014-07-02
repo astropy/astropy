74eb4eeef8f07681e71b7ebe6c80d9ece2f0ec75
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-07-02 18:14:29 +0200

    Added unit test for #58

--- wcsaxes/tests/test_formatter_locator.py
+++ wcsaxes/tests/test_formatter_locator.py
@@ -109,6 +109,13 @@ class TestAngleFormatterLocator(object):
         fl = AngleFormatterLocator(number=5, format=format)
         assert fl.formatter([15.392231] * u.degree, None)[0] == string
 
+    def test_latex_format(self):
+        fl = AngleFormatterLocator(number=5, format="dd:mm:ss")
+        assert fl.formatter([15.392231] * u.degree, None)[0] == six.u('15\xb023\'32"')
+        import matplotlib as mpl
+        with mpl.rc_context(rc={'text.usetex': True}):
+            assert fl.formatter([15.392231] * u.degree, None)[0] == "15$^\circ$23'32\""
+
     @pytest.mark.parametrize(('format'), ['x.xxx', 'dd.ss', 'dd:ss', 'mdd:mm:ss'])
     def test_invalid_formats(self, format):
         fl = AngleFormatterLocator(number=5)
@@ -254,4 +261,3 @@ class TestScalarFormatterLocator(object):
         fl.spacing = 0.032 * u.m
         fl.format = 'x.xx'
         assert_almost_equal(fl.spacing.to(u.m).value, 0.03)
-
