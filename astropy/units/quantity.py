7c2512abbbae5901055a32eb77826870b0f725a9
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2014-07-07 19:33:22 +0200

    Changelog entry that quantity objects can be assigned NaN

--- astropy/units/quantity.py
+++ astropy/units/quantity.py
@@ -1016,7 +1016,8 @@ class Quantity(np.ndarray):
             value_dtype = getattr(value, 'dtype', None)
             if self.dtype != value_dtype:
                 self_dtype_array = np.array(_value, self.dtype)
-                value_dtype_array = np.array(_value, dtype=value_dtype)
+                value_dtype_array = np.array(_value, dtype=value_dtype,
+                                             copy=False)
                 if not np.all(np.logical_or(self_dtype_array ==
                                             value_dtype_array,
                                             np.isnan(value_dtype_array))):
