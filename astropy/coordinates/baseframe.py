fe6be72779c842bf876425b505a112ddbbc8ce41
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-07-11 16:10:38 -0500

    Added initial implementation for existing frames

--- astropy/coordinates/baseframe.py
+++ astropy/coordinates/baseframe.py
@@ -851,24 +851,23 @@ class BaseCoordinateFrame(object):
             raise AttributeError('Cannot set any frame attribute {0}'.format(attr))
         else:
             super(BaseCoordinateFrame, self).__setattr__(attr, value)
-    
+
     @classmethod
-    def frame_wcs(cls, wcs):
+    def from_wcs(cls, wcs):
         """
         Test if this frame represents the world coordinates in a WCS object
-        
+
         Parameters
         ----------
         wcs: ~astropy.wcs.WCS object
             The WCS object to be tested
-        
+
         Returns
         -------
         match: bool
             True if this frame matches
         """
-        # By default it can't match
-        return False
+        raise NotImplementedError("frame_wcs is not defined for this frame")
 
     def separation(self, other):
         """
@@ -997,5 +996,3 @@ class GenericFrame(BaseCoordinateFrame):
             raise AttributeError("can't set frame attribute '{0}'".format(name))
         else:
             super(GenericFrame, self).__setattr__(name, value)
-
-
--- astropy/coordinates/utils.py
+++ astropy/coordinates/utils.py
@@ -3,36 +3,42 @@
 """
 Utilities for coordinates
 """
-from .transfomations import TransformGraph
+from . import frame_transform_graph
+from ..wcs import WCSSUB_CELESTIAL
 
 def find_wcs_frame(wcs):
     """
     Check the transform graph for any registered class that can represent the
     WCS object.
-    
+
     Parameters
     ----------
     wcs: ~astropy.wcs.WCS object
         A WCS object to check for a frame match
-    
+
     Returns
     -------
     frame: ~astropy.coordinates.BaseCoordinateFrame
         A unique frame match
     """
 
-    # Add WCS validation here
-    
+    # For now, we only care about the celestial portion of the WCS
+    wcs = wcs.sub([WCSSUB_CELESTIAL])
+
+    # Check that WCS has two dimensions
+    if wcs.naxis != 2:
+        raise ValueError("WCS should have two celestial dimensions")
+
+    # TODO: don't use transform graph since not all frames are in the graph
+
     # Check the graph for a match
     matches = []
-    for cls in TransformGraph.frame_set:
-        if cls.frame_wcs(wcs):
-            matches.append(cls)
+    for cls in frame_transform_graph.frame_set:
+        try:
+            matches.append(cls.from_wcs(wcs))
+        except NotImplementedError:
+            pass
+        except ValueError:
+            pass
 
-    if not len(matches):
-        raise Exception("No matches found for this WCS object")
-    
-    elif len(matches) > 1:
-        raise Exception("This WCS object is ambiguious.")
-    
-    return matches[0]
+    return matches
