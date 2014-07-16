e2fc5d442136bb967c657abf9c51e609dbb69776
Erik M. Bray <embray@stsci.edu>
2014-07-16 15:44:52 -0400

    Initial drop of support for Python 3.1 and 3.2, including removal of several hacks specifically for supporting those versions (this is probably not exhaustive, but the others can be removed when found).  Drop 3.2 from travis builds and 3.1 and 3.2 from tox.ini.

--- astropy/time/tests/test_basic.py
+++ astropy/time/tests/test_basic.py
@@ -640,11 +640,8 @@ def test_now():
     # times are more like microseconds.  But it seems safer in case some
     # platforms have slow clock calls or something.
 
-    version_info = sys.version_info
-    # py < 2.7 and py3 < 3.2 doesn't have `total_seconds`
-    if ((version_info[0] == 2 and version_info[1] < 7) or
-        (version_info[0] == 3 and version_info[1] < 2) or
-            version_info[0] < 2):
+    # py < 2.7 doesn't have `total_seconds`
+    if sys.version_info[:2] < (2, 7):
         total_secs = lambda td: (td.microseconds + (
             td.seconds + td.days * 24 * 3600) * 10 ** 6) / 10 ** 6.
     else:
