cd7739559d34ed575ddf96164ac02ca748b314ed
Julien Woillez <jwoillez@gmail.com>
2014-07-17 20:09:42 +0200

    Modified test to include d2dtf()

--- tests/test_erfa.py
+++ tests/test_erfa.py
@@ -1,11 +1,19 @@
 import numpy as np
 import erfa
 
-jd = np.linspace(2456855.5, 2456855.5+1.0, 100)
+jd = np.linspace(2456855.5, 2456855.5+1.0/24.0, 3600*2+1)
 
 aob, zob, hob, dob, rob, eo = erfa.atco13(0.0,0.0,0.0,0.0,0.0,0.0,jd,0.0,0.0,0.0,np.pi/4.0,0.0,0.0,0.0,1014.0,0.0,0.0,0.5)
 print(aob.shape)
 
 aob, zob, hob, dob, rob, eo = erfa.atco13(0.0,0.0,0.0,0.0,0.0,0.0,jd[0],0.0,0.0,0.0,np.pi/4.0,0.0,0.0,0.0,1014.0,0.0,0.0,0.5)
 print(aob.shape)
+
+iy, im, id, ihmsf = erfa.d2dtf("UTC", 3, jd, 0.0)
+print(iy.shape, ihmsf.shape, ihmsf.dtype)
+print(ihmsf)
+
+iy, im, id, ihmsf = erfa.d2dtf("UTC", 3, jd[0], 0.0)
+print(iy.shape, ihmsf.shape, ihmsf.dtype)
+print(ihmsf)
 
