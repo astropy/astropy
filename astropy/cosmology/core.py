b1ef646f76a13379caae0ecf18afb4eba036b8f6
Jörg Dietrich <astro@joergdietrich.com>
2014-07-17 14:52:59 +0200

    Add baryonic matter density paramter and its evolution to cosmology classes

--- astropy/cosmology/core.py
+++ astropy/cosmology/core.py
@@ -131,6 +131,9 @@ class FLRW(Cosmology):
         else:
             self._Ob0 = None
             self._Odm0 = self._Om0
+        else:
+            self._Ob0 = None
+
         self._Neff = float(Neff)
         if self._Neff < 0.0:
             raise ValueError("Effective number of neutrinos can "
