9aae431deb6ea54eba09ac6a23045cbc9e343242
Julien Woillez <jwoillez@gmail.com>
2014-07-18 22:59:42 +0200

    Better broadcasting example

--- tests/test_erfa.py
+++ tests/test_erfa.py
@@ -1,12 +1,17 @@
 import numpy as np
 import erfa.cython_numpy as erfa
 
-jd = np.linspace(2456855.5, 2456855.5+1.0/24.0, 3600*2+1)
+jd = np.linspace(2456855.5, 2456855.5+1.0/24.0/60.0, 60*2+1)
+ra  = np.linspace(0.0,np.pi*2.0,5)
+dec = np.linspace(-np.pi/2.0,+np.pi/2.0,4) 
 
 aob, zob, hob, dob, rob, eo = erfa.atco13(0.0,0.0,0.0,0.0,0.0,0.0,jd,0.0,0.0,0.0,np.pi/4.0,0.0,0.0,0.0,1014.0,0.0,0.0,0.5)
 print(aob.shape)
 
 aob, zob, hob, dob, rob, eo = erfa.atco13(0.0,0.0,0.0,0.0,0.0,0.0,jd[0],0.0,0.0,0.0,np.pi/4.0,0.0,0.0,0.0,1014.0,0.0,0.0,0.5)
+print(aob.shape)
+
+aob, zob, hob, dob, rob, eo = erfa.atco13(ra[:,None,None],dec[None,:,None],0.0,0.0,0.0,0.0,jd[None,None,:],0.0,0.0,0.0,np.pi/4.0,0.0,0.0,0.0,1014.0,0.0,0.0,0.5)
 print(aob.shape)
 
 iy, im, id, ihmsf = erfa.d2dtf("UTC", 3, jd, 0.0)
