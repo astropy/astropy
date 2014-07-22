df3cb902175a269d828a1c41ba469f101177b391
Matthew Bourque <matthewkbourque@gmail.com>
2014-07-22 14:06:39 -0400

    Initial commit of plotting.py, which will house set_astropy_plot_style(), a function that will set basic mpl.rcParams.

---
+++ astropy/utils/plotting.py
@@ -0,0 +1,17 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+""" This module contains changes to matplotlib rc parameters in order
+to adopt an astropy plotting style.
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import matplotlib as mpl
+
+def set_astropy_plot_style():
+    """ 
+    matplotlib configuration parameters for astropy plotting style.
+    """
+
+    pass
