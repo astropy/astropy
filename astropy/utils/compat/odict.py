aaa8d89acbc7b9c7952e80f4c062f73fbf6cb721
David Shupe <dave.shupe@gmail.com>
2014-07-24 14:31:12 -0700

    subclassed OrderedDict to override __str__

--- astropy/utils/compat/odict.py
+++ astropy/utils/compat/odict.py
@@ -1,7 +1,14 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from __future__ import absolute_import
+from pprint import pformat
 
 try:
-    from collections import OrderedDict
+    from collections import OrderedDict as NativeOrderedDict
 except ImportError:
-    from ._odict_py2 import *
+    from ._odict_py2 import OrderedDict as NativeOrderedDict
+
+class OrderedDict(NativeOrderedDict):
+    def __init__(self, *args, **kwds):
+        NativeOrderedDict.__init__(self, *args, **kwds)
+    def __str__(self):
+        return('OrderedDict('+ pformat(dict(self),width=1)[1:-1]+')')
