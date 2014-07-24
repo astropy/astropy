cb9943bdc29ad37ffe3ff16f26653debfb9113a1
David Shupe <dave.shupe@gmail.com>
2014-07-24 14:44:28 -0700

    added docstrings to subclassed OrderedDict

--- astropy/utils/compat/odict.py
+++ astropy/utils/compat/odict.py
@@ -8,7 +8,20 @@ except ImportError:
     from ._odict_py2 import OrderedDict as NativeOrderedDict
 
 class OrderedDict(NativeOrderedDict):
+    """
+    Dictionary that remembers insertion order
+    """
     def __init__(self, *args, **kwds):
+        """
+        Initialize an ordered dictionary, The signature is the same as
+        regular dictionaries, but keyword arguments are not recommended
+        because their insertion order is arbitrary.
+        """
         NativeOrderedDict.__init__(self, *args, **kwds)
     def __str__(self):
+        """
+        Pretty-printed string representation.
+        x.__str__() <==> str(x)
+        """
         return('OrderedDict('+ pformat(dict(self),width=1)[1:-1]+')')
+
