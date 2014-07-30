6e0f585a8f8433d4f6800cb1f093f97f8a1d4ff7
Larry Bradley <larry.bradley@gmail.com>
2014-07-30 15:39:11 -0400

    Update imports for new functions

---
+++ imageutils/__init__.py
@@ -0,0 +1,21 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Image processing utilities for Astropy.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    from .scale_img import *
+    from .array_utils import *
+    from .sampling import *
+
+__all__ = ['find_imgcuts', 'img_stats', 'rescale_img', 'scale_linear',
+           'scale_sqrt', 'scale_power', 'scale_log', 'scale_asinh',
+           'downsample', 'upsample', 'extract_array_2d', 'add_array_2d',
+           'subpixel_indices', 'fix_prf_nan']
