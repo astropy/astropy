e305d75f704395de6af9a81676d11b01d2f52a13
Julien Woillez <jwoillez@gmail.com>
2014-07-31 08:44:41 +0200

    Three new jinja filters: prefix, postfix, surround

--- cython_numpy_auto/cython_generator.py
+++ cython_numpy_auto/cython_generator.py
@@ -2,6 +2,17 @@ from jinja2 import Environment, PackageLoader
 from cython_numpy_auto.code_analyzer import Function
 
 env = Environment(loader=PackageLoader('cython_numpy_auto', '.'))
+
+def prefix(a_list, pre):
+    return [pre+'{0}'.format(an_element) for an_element in a_list]
+def postfix(a_list, post):
+    return ['{0}'.format(an_element)+post for an_element in a_list]
+def surround(a_list, pre, post):
+    return [pre+'{0}'.format(an_element)+post for an_element in a_list]
+env.filters['prefix'] = prefix
+env.filters['postfix'] = postfix
+env.filters['surround'] = surround
+
 erfa_pyx_in = env.get_template('erfa.pyx.in')
 
 era_func_names = ["eraAtco13", "eraD2dtf", "eraAper"]
