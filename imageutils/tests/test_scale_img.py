aa298a01ed7ef854479efceaae97cc2f9f92f5f3
Larry Bradley <larry.bradley@gmail.com>
2014-08-06 15:42:07 -0400

    Rename find_imgcuts in tests

--- imageutils/tests/test_scale_img.py
+++ imageutils/tests/test_scale_img.py
@@ -17,41 +17,41 @@ DATASCL = 0.5 * DATA
 
 
 class TestImgCuts(object):
-    def test_find_imgcuts(self):
+    def test_find_cutlevels(self):
         data = np.arange(101)
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_cut=10)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_cut=10)
         assert_equal([mincut, maxcut], [10, 100])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, max_cut=90)
+        mincut, maxcut = scale_img.find_cutlevels(data, max_cut=90)
         assert_equal([mincut, maxcut], [0, 90])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_cut=10, max_cut=90)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_cut=10, max_cut=90)
         assert_equal([mincut, maxcut], [10, 90])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_percent=20)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_percent=20)
         assert_equal([mincut, maxcut], [20, 100])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, max_percent=80)
+        mincut, maxcut = scale_img.find_cutlevels(data, max_percent=80)
         assert_equal([mincut, maxcut], [0, 80])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_percent=20,
-                                                max_percent=80)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_percent=20,
+                                                  max_percent=80)
         assert_equal([mincut, maxcut], [20, 80])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, percent=90)
+        mincut, maxcut = scale_img.find_cutlevels(data, percent=90)
         assert_equal([mincut, maxcut], [5, 95])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_percent=20,
-                                                max_percent=80, percent=90)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_percent=20,
+                                                  max_percent=80, percent=90)
         assert_equal([mincut, maxcut], [20, 80])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, min_cut=10,
-                                                min_percent=20)
+        mincut, maxcut = scale_img.find_cutlevels(data, min_cut=10,
+                                                  min_percent=20)
         assert_equal([mincut, maxcut], [10, 100])
 
-        mincut, maxcut = scale_img.find_imgcuts(data, max_cut=90,
-                                                max_percent=80)
+        mincut, maxcut = scale_img.find_cutlevels(data, max_cut=90,
+                                                  max_percent=80)
         assert_equal([mincut, maxcut], [0, 90])
 
 
