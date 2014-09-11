edf8bb2ef98b7f13ab91134813e0a468009d9015
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-11 21:12:03 +0200

    Added CompositeStretch and ContrastBias stretch

--- imageutils/scaling/stretch.py
+++ imageutils/scaling/stretch.py
@@ -14,7 +14,34 @@ __all__ = ["LinearStretch", "SqrtStretch", "PowerStretch"]
 
 @six.add_metaclass(abc.ABCMeta)
 class BaseStretch(object):
-    pass
+
+    def __add__(self, other):
+        return CompositeStretch(other, self)
+
+
+class CompositeStretch(BaseStretch):
+    """
+    A combiantion of two stretches.
+
+    Parameters
+    ----------
+    stretch_1:
+        The first stretch to apply.
+    stretch_2:
+        The second stretch to apply.
+    """
+
+    def __init__(self, stretch_1, stretch_2):
+        super(CompositeStretch, self).__init__()
+        self.stretch_1 = stretch_1
+        self.stretch_2 = stretch_2
+
+    def __call__(self, values):
+        return self.stretch_2(self.stretch_1(values))
+
+    def inverted(self):
+        return CompositeStretch(self.stretch_2.inverted(),
+                                self.stretch_1.inverted())
 
 
 class LinearStretch(BaseStretch):
@@ -47,6 +74,7 @@ class PowerStretch(BaseStretch):
     """
 
     def __init__(self, a):
+        super(PowerStretch, self).__init__()
         self.power = a
 
     def __call__(self, values):
@@ -54,3 +82,37 @@ class PowerStretch(BaseStretch):
 
     def inverted(self):
         return PowerStretch(1. / self.power)
+
+
+class ContrastBiasStretch(BaseStretch):
+    """
+    A stretch that takes into account contrast and bias: y = clip((x - bias) * contrast + 0.5, 0, 1)
+    """
+
+    def __init__(self, contrast, bias):
+        super(ContrastBiasStretch, self).__init__()
+        self.contrast = contrast
+        self.bias = bias
+
+    def __call__(self, values):
+        return np.clip((values - self.bias) * self.contrast + 0.5, 0., 1.)
+
+    def inverted(self):
+        return InvertedContrastBiasStretch(self.contrast, self.bias)
+
+
+class InvertedContrastBiasStretch(BaseStretch):
+    """
+    A stretch that takes into account contrast and bias: y = clip((x - bias) * contrast + 0.5, 0, 1)
+    """
+
+    def __init__(self, contrast, bias):
+        super(InvertedContrastBiasStretch, self).__init__()
+        self.contrast = contrast
+        self.bias = bias
+
+    def __call__(self, values):
+        return np.clip((values - 0.5) / self.contrast + self.bias, 0., 1.)
+
+    def inverted(self):
+        return ContrastBiasStretch(self.contrast, self.bias)
