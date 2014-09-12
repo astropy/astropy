bfc807190d810d244a36c9657fd23231b3fd3673
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-12 13:49:53 +0200

    Add the option to sub-sample the data for the PercentileInterval

--- imageutils/scaling/interval.py
+++ imageutils/scaling/interval.py
@@ -55,12 +55,22 @@ class PercentileInterval(BaseInterval):
     percentile : float
         The fraction of pixels to keep. The same fraction of pixels is
         eliminated from both ends.
+    n_samples : int, optional
+        Maximum number of values to use. If this is specified, and there are
+        more values in the dataset as this, then values are randomly sampled
+        from the array (with replacement)
     """
 
-    def __init__(self, percentile):
+    def __init__(self, percentile, n_samples=None):
         self.percentile = percentile
+        self.n_samples = n_samples
 
     def get_limits(self, values):
+
+        # If needed, limmit the number of samples. We sample with replacement
+        # since this is much faster.
+        if self.n_samples is not None and values.size > self.n_samples:
+            values = np.random.choice(values, self.n_samples)
 
         pmin = (100. - self.percentile) * 0.5
         pmax = 100. - pmin
