28bc733597e9be397c5412ac56ad3c04ec1c0829
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-09-28 18:58:48 +0200

    Remove references to functions that no longer exist

--- imageutils/__init__.py
+++ imageutils/__init__.py
@@ -15,6 +15,6 @@ if not _ASTROPY_SETUP_:
     from .array_utils import *
     from .sampling import *
 
-__all__ = ['find_cutlevels', 'normalize_image', 'scale_image',
-           'sigmaclip_stats', 'downsample', 'upsample', 'extract_array_2d',
-           'add_array_2d', 'subpixel_indices', 'mask_to_mirrored_num']
+__all__ = ['scale_image', 'sigmaclip_stats', 'downsample', 'upsample',
+           'extract_array_2d', 'add_array_2d', 'subpixel_indices',
+           'mask_to_mirrored_num']
