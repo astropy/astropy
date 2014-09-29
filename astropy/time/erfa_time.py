dcd91551aae31fd3a6637c22da77f5d45eefb35d
Erik Tollerud <erik.tollerud@gmail.com>
2014-09-29 03:33:00 -0400

    switch from erfa_time to erfa

---
+++ astropy/time/erfa_time.py
@@ -0,0 +1,29 @@
+from ..erfa import *
+
+d_tai_utc = dat
+jd_dtf = d2dtf
+
+dtf_jd = dtf2d
+tai_tt = taitt
+tcb_tdb = tcbtdb
+tcg_tt = tcgtt
+tdb_tcb = tdbtcb
+tt_tai = tttai
+tt_tcg = tttcg
+utc_tai = utctai
+tai_utc = taiutc
+tai_ut1 = taiut1
+ut1_tai = ut1tai
+tt_ut1 = ttut1
+ut1_tt = ut1tt
+tdb_tt = tdbtt
+tt_tdb = tttdb
+ut1_utc = ut1utc
+utc_ut1 = utcut1
+d_tdb_tt = dtdb
+era_gd2gc = gd2gc
+era_gc2gd = gc2gd
+jd_julian_epoch = epj
+julian_epoch_jd = epj2jd
+jd_besselian_epoch = epb
+besselian_epoch_jd = epb2jd
