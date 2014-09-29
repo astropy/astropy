d9e5699390be16263c01decd72c750d11018a272
Erik Tollerud <erik.tollerud@gmail.com>
2014-09-29 00:48:22 -0400

    boilerplate updates

---
+++ astropy/erfa/setup_package.py
@@ -0,0 +1,40 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import os
+from distutils.extension import Extension
+
+from astropy_helpers import setup_helpers
+
+ERFAPKGDIR = os.path.relpath(os.path.dirname(__file__))
+
+
+def get_extensions():
+    sources = [os.path.join(ERFAPKGDIR, "erfa_time.pyx")]
+    include_dirs = ['numpy']
+    libraries = []
+
+    if setup_helpers.use_system_library('erfa'):
+        libraries.append('erfa')
+    else:
+        sources.append("cextern/erfa/erfa.c")
+        include_dirs.append('cextern/erfa')
+
+    time_ext = Extension(
+        name="astropy.erfa",
+        sources=sources,
+        include_dirs=include_dirs,
+        libraries=libraries,
+        language="c",)
+
+    return [time_ext]
+
+
+def get_external_libraries():
+    return ['erfa']
+
+
+def requires_2to3():
+    return False
+
+def get_package_data():
+    return {'astropy.erfa': 'erfa.pyx.templ'}
