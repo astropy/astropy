21eb1f9b0988be38bed6ca41280d8640ccc80b09
Erik Tollerud <erik.tollerud@gmail.com>
2014-10-17 13:42:17 -0400

    Typo fix

--- astropy/coordinates/sky_coordinate.py
+++ astropy/coordinates/sky_coordinate.py
@@ -764,7 +764,7 @@ class SkyCoord(object):
         """
         from astropy.wcs.utils import pixel_to_skycoord
 
-        return pixel_to_skycoord(xp, yp, wcs=wc, origin=origin, mode=mode, cls=cls)
+        return pixel_to_skycoord(xp, yp, wcs=wcs, origin=origin, mode=mode, cls=cls)
 
 
 
