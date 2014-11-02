cf13d23272c7472b16ea767cfd68028d3eb30f9c
Tom Aldcroft <taldcroft@gmail.com>
2014-11-02 17:24:03 -0500

    Add test of initing from list of cols by reference

--- astropy/table/tests/test_init_table.py
+++ astropy/table/tests/test_init_table.py
@@ -223,11 +223,11 @@ class TestInitFromColsList(BaseInitFromListLike):
         assert all(t[name].name == name for name in t.colnames)
 
     def test_ref(self, table_type):
-        # CHANGE: allowed to initialize from a list of cols by ref
-        # self._setup(table_type)
-        # with pytest.raises(ValueError):
-        #    table_type(self.data, copy=False)
-        pass
+        """Test that initializing from a list of columns can be done by reference"""
+        self._setup(table_type)
+        t = table_type(self.data, copy=False)
+        t['x'][0] = 100
+        assert self.data[0][0] == 100
 
 @pytest.mark.usefixtures('table_type')
 class TestInitFromNdarrayStruct(BaseInitFromDictLike):
