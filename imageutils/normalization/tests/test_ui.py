083a8d4f301da2ad665a3fefd13a4381417b1205
Larry Bradley <larry.bradley@gmail.com>
2014-11-18 13:36:15 -0500

    Add test for asinh in scale_image

--- imageutils/normalization/tests/test_ui.py
+++ imageutils/normalization/tests/test_ui.py
@@ -8,7 +8,7 @@ DATASCL = 0.5 * DATA
 
 
 class TestImageScaling(object):
-    
+
     def test_linear(self):
         """Test linear scaling."""
         img = scale_image(DATA, scale='linear')
@@ -29,4 +29,11 @@ class TestImageScaling(object):
         """Test log10 scaling."""
         img = scale_image(DATA, scale='log')
         ref = np.log10(1000 * DATASCL + 1.0) / np.log10(1001.0)
+        assert_allclose(img, ref, atol=0, rtol=1.e-5)
+
+    def test_asinh(self):
+        """Test asinh scaling."""
+        a = 0.1
+        img = scale_image(DATA, scale='asinh', asinh_a=a)
+        ref = np.arcsinh(DATASCL / a) / np.arcsinh(1. / a)
         assert_allclose(img, ref, atol=0, rtol=1.e-5)
