9b026de17f4ef36a412cae6c5d7beb092c2e20c8
Larry Bradley <larry.bradley@gmail.com>
2014-11-21 12:38:46 -0500

    Add Ellipse2D example

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -695,6 +695,29 @@ class Ellipse2D(Fittable2DModel):
                       0 & : \\mathrm{otherwise}
                     \\end{array}
                   \\right.
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import numpy as np
+        from astropy.modeling.models import Ellipse2D
+        from astropy.coordinates import Angle
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as mpatches
+        x0, y0 = 25, 25
+        a, b = 20, 10
+        theta = Angle(30, 'deg')
+        e = Ellipse2D(amplitude=100., x_0=x0, y_0=y0, a=a, b=b,
+                      theta=theta.radian)
+        y, x = np.mgrid[0:50, 0:50]
+        fig, ax = plt.subplots(1, 1)
+        ax.imshow(e(x, y), origin='lower', cmap='Greys_r')
+        e2 = mpatches.Ellipse((x0, y0), 2*a, 2*b, theta.degree,
+                              edgecolor='red', facecolor='none')
+        ax.add_patch(e2)
+        plt.show()
     """
 
     amplitude = Parameter()
