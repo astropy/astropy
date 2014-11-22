049465d81f0bf3abc8bd65be1fefd44e2fd5dcb2
Erik Tollerud <erik.tollerud@gmail.com>
2014-11-21 19:24:11 -0500

    Add some additional docs about where to put transform functions

--- astropy/coordinates/builtin_frames/__init__.py
+++ astropy/coordinates/builtin_frames/__init__.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-This module contains the coordinate frames actually implemented by astropy.
+This package contains the coordinate frames actually implemented by astropy.
+
+Users shouldn't use this module directly, but rather import from the
+`astropy.coordinates` module.  While it is likely to exist for the long-term,
+the existence of this package and details of its organization should be
+considered an implementation detail, and is not guaranteed to hold for future
+versions of astropy.
+
+Notes
+-----
+The builtin coordinates classes are all imported automatically into this package
+namespace, so there's no need to access the sub-modules directly.
+
+To implement a new frame in Astropy, a developer should add the frame as a new
+module in this package.  The functions necessary to transform to/from that frame
+should generally also be placed in that frame's module.  In some cases putting both in the same place will
+be impossible because transformation functions need to have references to the
+class objects they transform to and from; this leads to potential circular
+dependency problems.  So when implementing multiple multiple new frames with
+transformations that are intermixed, simply choose one to include both the "to"
+and "from" functions, with the guideline that the transformations should go in
+whichever frame is the more specific (i.e., less likely to be used by other
+future frames.)
 """
 
 from .icrs import ICRS
