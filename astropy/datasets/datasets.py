1d57aace04f50453a65efdb30124b95a889bfa72
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-11-27 10:15:52 +0100

    Change in API

--- astropy/datasets/datasets.py
+++ astropy/datasets/datasets.py
@@ -5,26 +5,22 @@ import json
 from ..io import fits
 from ..utils.data import download_file, get_readable_fileobj
 
-__all__ = ['fetch_hdu', 'list_available_datasets']
+__all__ = ['dataset']
 
 URL = 'http://data.astropy.org/'
 
 
-def fetch_hdu(filename, cache=True):
+DATASETS = {
+    'galcen_2mass_k': 'galactic_center/gc_2mass_k.fits',
+    'galcen_bolocam_gps': 'galactic_center/gc_bolocam_gps.fits',
+    'galcen_msx_e': 'galactic_center/gc_msx_e.fits',
+    'allsky_rosat': 'allsky/allsky_rosat.fits',
+    'l1448_13co': 'l1448/l1448_13co.fits'
+}
+
+
+def dataset(name, cache=True):
     """
-    Download a FITS file to the cache and open HDU 0.
+    Return the path to a local version of dataset ``name``
     """
-    path = download_file(URL + filename, cache=cache)
-    return fits.open(path)[0]
-
-
-def list_available_datasets():
-
-    with get_readable_fileobj(URL + 'file_index.json', cache=False, show_progress=False) as f:
-        datasets = json.load(f)
-
-    datasets.remove('')  # last entry in json file
-
-    datasets = [d for d in datasets if not d.startswith('tutorials')]
-
-    return datasets
+    return download_file(URL + DATASETS[name], cache=cache)
