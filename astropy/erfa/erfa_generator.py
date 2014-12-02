b6e3ace57d029bde6c3642f91332208b3b4a913a
Erik Tollerud <erik.tollerud@gmail.com>
2014-12-01 19:22:05 -0500

    style tweaks in erfa_generator.py

--- astropy/erfa/erfa_generator.py
+++ astropy/erfa/erfa_generator.py
@@ -357,7 +357,7 @@ def main(srcdir, outfn, templateloc, verbose=True):
     env.filters['surround'] = surround
 
     erfa_pyx_in = env.get_template('erfa.pyx.templ')
-    erfa_py_in  = env.get_template('erfa.py.templ')
+    erfa_py_in = env.get_template('erfa.py.templ')
 
     #Extract all the ERFA function names from erfa.h
     if os.path.isdir(srcdir):
@@ -405,18 +405,19 @@ def main(srcdir, outfn, templateloc, verbose=True):
 
     print_("Rendering template")
     erfa_pyx = erfa_pyx_in.render(funcs=funcs)
-    erfa_py  = erfa_py_in.render(funcs=funcs)
+    erfa_py = erfa_py_in.render(funcs=funcs)
 
     if outfn is not None:
-        print_("Saving to", outfn)
+        outfnx = outfn + "x"
+        print_("Saving to", outfn, 'and', outfnx)
         with open(outfn, "w") as f:
             f.write(erfa_py)
-        with open(outfn+"x", "w") as f:
+        with open(outfnx, "w") as f:
             f.write(erfa_pyx)
 
     print_("Done!")
 
-    return erfa_pyx, funcs
+    return erfa_pyx, erfa_py, funcs
 
 DEFAULT_ERFA_LOC = os.path.join(os.path.split(__file__)[0],
                                 '../../cextern/erfa')
