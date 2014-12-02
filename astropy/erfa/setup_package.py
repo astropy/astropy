a7cb7ab26afc2babfe3ed2cf62b0f234afda9924
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-12-02 09:02:04 +0100

    Make sure that ERFA generation is only done if needed, and not done for a release tarball

--- astropy/erfa/setup_package.py
+++ astropy/erfa/setup_package.py
@@ -1,18 +1,48 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
+import glob
+
 from distutils.extension import Extension
 
 from astropy_helpers import setup_helpers
 
 ERFAPKGDIR = os.path.relpath(os.path.dirname(__file__))
 
+ERFA_SRC = os.path.abspath(os.path.join('..','..','cextern','erfa'))
+
+SRC_FILES = glob.glob(os.path.join(ERFA_SRC, '*'))
+SRC_FILES += [os.path.join(ERFAPKGDIR, filename)
+              for filename in ['erfa.py.templ', 'erfa.pyx.templ', 'cython_generator.py']]
+
+GEN_FILES = [os.path.join(ERFAPKGDIR, 'erfa.py'), os.path.join(ERFAPKGDIR, 'erfa.pyx')]
 
 def _generate_erfa_wrapper():
-    import astropy.erfa.cython_generator as cython_generator
-    srcdir = cython_generator.DEFAULT_ERFA_LOC
-    templateloc = cython_generator.DEFAULT_TEMPLATE_LOC
-    cython_generator.main(srcdir, 'astropy/erfa/erfa.py', templateloc)
+
+    # Generating the ERFA wrappers should only be done if needed. This also
+    # ensures that it is not done for any release tarball since those will
+    # include erfa.py and erfa.pyx.
+    if all(os.path.exists(filename) for filename in GEN_FILES):
+
+        # If templates don't exist, we are in a release so can exit
+        if any(not os.path.exists(filename) for filename in SRC_FILES):
+            return
+
+        # Determine modification times
+        erfa_mtime = max(os.path.getmtime(filename) for filename in SRC_FILES)
+        gen_mtime = min(os.path.getmtime(filename) for filename in GEN_FILES)
+
+        # If generated source is recent enough, don't update
+        if gen_mtime > erfa_mtime:
+            return
+
+    import imp
+    gen = imp.load_source('cython_generator',
+                          os.path.join(ERFAPKGDIR, 'cython_generator.py'))
+
+    gen.main(gen.DEFAULT_ERFA_LOC,
+             os.path.join(ERFAPKGDIR, 'erfa.py'),
+             gen.DEFAULT_TEMPLATE_LOC)
 
 
 def get_extensions():
@@ -48,7 +78,3 @@ def get_external_libraries():
 
 def requires_2to3():
     return False
-
-
-def get_package_data():
-    return {'astropy.erfa': ['erfa.pyx.templ', 'erfa.pyx.templ']}
