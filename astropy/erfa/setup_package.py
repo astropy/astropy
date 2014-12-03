1cbc9394679514412666e427dd46f061568b0888
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-12-03 11:26:19 +0100

    Only re-generate if needed

--- astropy/erfa/setup_package.py
+++ astropy/erfa/setup_package.py
@@ -1,17 +1,42 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
+import glob
+
 from distutils.extension import Extension
 
 from astropy_helpers import setup_helpers
 
 ERFAPKGDIR = os.path.relpath(os.path.dirname(__file__))
 
+ERFA_SRC = os.path.abspath(os.path.join('..','..','cextern','erfa'))
+
+SRC_FILES = glob.glob(os.path.join(ERFA_SRC, '*'))
+SRC_FILES += [os.path.join(ERFAPKGDIR, filename)
+              for filename in ['erfa.py.templ', 'erfa.pyx.templ', 'cython_generator.py']]
+
+GEN_FILES = [os.path.join(ERFAPKGDIR, 'erfa.py'), os.path.join(ERFAPKGDIR, 'erfa.pyx')]
+
 
 def preprocess_source():
+
+    # Generating the ERFA wrappers should only be done if needed. This also
+    # ensures that it is not done for any release tarball since those will
+    # include erfa.py and erfa.pyx.
+    if all(os.path.exists(filename) for filename in GEN_FILES):
+
+        # Determine modification times
+        erfa_mtime = max(os.path.getmtime(filename) for filename in SRC_FILES)
+        gen_mtime = min(os.path.getmtime(filename) for filename in GEN_FILES)
+
+        # If generated source is recent enough, don't update
+        if gen_mtime > erfa_mtime:
+            return
+
     import imp
     gen = imp.load_source('cython_generator',
                           os.path.join(ERFAPKGDIR, 'cython_generator.py'))
+
     gen.main(gen.DEFAULT_ERFA_LOC,
              os.path.join(ERFAPKGDIR, 'erfa.py'),
              gen.DEFAULT_TEMPLATE_LOC)
