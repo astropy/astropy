7935fe81c3381c46efbe2adfe5f7451f9bcaf294
Larry Bradley <larry.bradley@gmail.com>
2014-12-03 15:08:34 -0500

    Change parameter names in sigmaclip_stats

--- imageutils/stats.py
+++ imageutils/stats.py
@@ -7,51 +7,51 @@ from astropy.stats import sigma_clip
 __all__ = ['sigmaclip_stats']
 
 
-def sigmaclip_stats(image, image_mask=None, mask_val=None, sigma=3.0,
-                    iters=None):
+def sigmaclip_stats(data, mask=None, mask_val=None, sigma=3.0, iters=None):
     """
-    Calculate sigma-clipped statistics of an image.  For example,
-    sigma-clipped statistics can be used to estimate the background and
-    background noise in an image.
+    Calculate sigma-clipped statistics of an image.
+
+    For example, sigma-clipped statistics can be used to estimate the
+    background and background noise in an image.
 
     Parameters
     ----------
-    image : array_like
+    data : array_like
         The 2D array of the image.
 
-    image_mask : array_like, bool, optional
-        A boolean mask with the same shape as ``image``, where a `True`
-        value indicates the corresponding element of ``image`` is
-        invalid.  Masked pixels are ignored when computing the image
-        statistics.
+    mask : `numpy.ndarray` (bool), optional
+        A boolean mask with the same shape as ``data``, where a `True`
+        value indicates the corresponding element of ``data`` is masked.
+        Masked pixels are excluded when computing the image statistics.
 
     mask_val : float, optional
         An image data value (e.g., ``0.0``) that is ignored when
         computing the image statistics.  ``mask_val`` will be ignored if
-        ``image_mask`` is input.
+        ``mask`` is input.
 
     sigma : float, optional
         The number of standard deviations to use as the clipping limit.
 
-    iters : float, optional
-       The number of clipping iterations to perform, or `None` to clip
-       until convergence is achieved (i.e. continue until the last
-       iteration clips nothing).
+    iters : int, optional
+        The number of iterations to perform sigma clipping, or `None` to
+        clip until convergence is achieved (i.e., continue until the
+        last iteration clips nothing) when calculating the image
+        statistics.
 
     Returns
     -------
-    mean, median, stddev : floats
+    mean, median, stddev : float
         The mean, median, and standard deviation of the sigma-clipped
         image.
     """
 
-    if image_mask is not None:
-        assert image_mask.dtype == np.bool, \
-            'image_mask must be a boolean ndarray'
-        image = image[~image_mask]
-    if mask_val is not None and image_mask is None:
-        idx = (image != mask_val).nonzero()
-        image = image[idx]
-    image_clip = sigma_clip(image, sig=sigma, iters=iters)
-    goodvals = image_clip.data[~image_clip.mask]
+    if mask is not None:
+        if mask.dtype != np.bool:
+            raise TypeError('mask must be a boolean ndarray')
+        data = data[~mask]
+    if mask_val is not None and mask is None:
+        idx = (data != mask_val).nonzero()
+        data = data[idx]
+    data_clip = sigma_clip(data, sig=sigma, iters=iters)
+    goodvals = data_clip.data[~data_clip.mask]
     return np.mean(goodvals), np.median(goodvals), np.std(goodvals)
