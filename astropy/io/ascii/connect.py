e4c14723cbb09b3c2a07c599500cf83dac7d6ffb
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-12-07 22:07:41 +0100

    Implement ASCII I/O classes

--- astropy/io/ascii/connect.py
+++ astropy/io/ascii/connect.py
@@ -7,7 +7,6 @@ import re
 
 from ..registry import BaseIO
 from ...table import Table
-from ...extern.six.moves import zip
 
 __all__ = []
 
@@ -26,23 +25,92 @@ def write_asciitable(table, filename, **kwargs):
     return write(table, filename, **kwargs)
 
 
+# Generic ASCII I/O class
+
 class ASCIITableIO(BaseIO):
 
     _format_name = 'ascii'
     _supported_class = Table
 
     @staticmethod
-    def read(format, filename, **kwargs):
+    def read(filename, **kwargs):
         from .ui import read
-        format = re.sub(r'^ascii\.', '', format)
-        return read(filename, format=format, **kwargs)
+        return read(filename, **kwargs)
 
     @staticmethod
-    def write(format, table, filename, **kwargs):
+    def write(table, filename, **kwargs):
         from .ui import write
-        format = re.sub(r'^ascii\.', '', format)
-        return write(table, filename, format=format, **kwargs)
+        return write(table, filename, **kwargs)
 
     @staticmethod
-    def identify(suffix, origin, filepath, fileobj, *args, **kwargs):
-        return filepath is not None and filepath.endswith(suffix)
+    def identify(origin, filepath, fileobj, *args, **kwargs):
+        # TODO - this is currently incorrect
+        return filepath is not None and filepath.endswith("TODO")
+
+# Format-specific classes
+
+BUILTIN_ASCII_FORMATS = ['ascii.aastex',
+                         'ascii.basic',
+                         'ascii.cds',
+                         'ascii.commented_header',
+                         'ascii.csv',
+                         'ascii.daophot',
+                         'ascii.fast_basic',
+                         'ascii.fast_commented_header',
+                         'ascii.fast_csv',
+                         'ascii.fast_no_header',
+                         'ascii.fast_rdb',
+                         'ascii.fast_tab',
+                         'ascii.fixed_width',
+                         'ascii.fixed_width_no_header',
+                         'ascii.fixed_width_two_line',
+                         'ascii.html',
+                         'ascii.ipac',
+                         'ascii.latex',
+                         'ascii.no_header',
+                         'ascii.rdb',
+                         'ascii.sextractor',
+                         'ascii.tab']
+
+EXTENSIONS = {
+    'ascii.csv': ['csv'],
+    'ascii.rdb': ['rdb'],
+    'ascii.html': ['html'],
+    'ascii.tex': ['tex']
+}
+
+ASCII_CLASSES = {}
+
+for ascii_format in BUILTIN_ASCII_FORMATS:
+
+    _format_name = 'ascii'
+    _supported_class = Table
+
+    def read(self, filename, **kwargs):
+        from .ui import read
+        fmt = re.sub(r'^ascii\.', '', self._format_name)
+        return read(filename, format=fmt, **kwargs)
+
+    def write(self, table, filename, **kwargs):
+        from .ui import write
+        fmt = re.sub(r'^ascii\.', '', self._format_name)
+        return write(table, filename, format=fmt, **kwargs)
+
+    def identify(self, origin, filepath, fileobj, *args, **kwargs):
+        return filepath is not None and filepath.endswith(self._extensions)
+
+    name = "".join([x.capitalize() for x in ascii_format.split(".")]) + "TableIO"
+
+    attributes = {
+        "_format_name": ascii_format,
+        "_supported_class": Table,
+        "read": read,
+        "write": write
+    }
+
+    if ascii_format in EXTENSIONS:
+        attributes["_extensions"] = EXTENSIONS[ascii_format]
+
+    ASCII_CLASSES[ascii_format] = type(name, (BaseIO,), attributes)
+
+del read, write, identify, name
