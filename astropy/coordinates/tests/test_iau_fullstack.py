a60c661cac54e59ec88c5d9da99f78b9b5252278
Erik Tollerud <erik.tollerud@gmail.com>
2014-12-16 16:20:06 -0500

    added test of full-stack conversion

--- astropy/coordinates/tests/test_iau_fullstack.py
+++ astropy/coordinates/tests/test_iau_fullstack.py
@@ -49,20 +49,19 @@ def fullstack_obsconditions(request):
     return request.param
 
 def _erfa_check(ira, idec, astrom):
+    """
+    This function does the same thing the astropy layer is supposed to do, but
+    all in erfa
+    """
     cra, cdec = erfa.atciq(ira, idec, 0, 0, 0, 0, astrom)
     az, zen, ha, odec, ora = erfa.atioq(cra, cdec, astrom)
+    alt = np.pi/2-zen
     cra2, cdec2 = erfa.atoiq('A', az, zen, astrom)
     ira2, idec2 = erfa.aticq(cra2, cdec2, astrom)
 
-    npt.assert_allclose(cra2, cra)
-    npt.assert_allclose(cdec2, cdec)
-
-    npt.assert_allclose(ira2, ira)
-    npt.assert_allclose(idec2, idec)
-
-    return cra, cdec, az, zen, np.pi/2-zen, cra2, cdec2, ira2, idec2
-
-
+    dct = locals()
+    del dct['astrom']
+    return dct
 
 def test_iau_fullstack(fullstack_icrs,  fullstack_fiducial_altaz,
                        fullstack_times, fullstack_locations,
@@ -81,39 +80,48 @@ def test_iau_fullstack(fullstack_icrs,  fullstack_fiducial_altaz,
     aacoo = fullstack_icrs.transform_to(altazframe)
 
     #compare aacoo to the fiducial AltAz - should always be different
-    assert not np.allclose(aacoo.alt.deg, fullstack_fiducial_altaz.alt.deg)
-    assert not np.allclose(aacoo.az.deg, fullstack_fiducial_altaz.az.deg)
+    assert np.all(np.abs(aacoo.alt - fullstack_fiducial_altaz.alt) > 50*u.milliarcsecond)
+    assert np.all(np.abs(aacoo.az - fullstack_fiducial_altaz.az) > 50*u.milliarcsecond)
 
-    #check that we're consistent with the ERFA result
+    #if the refraction correction is included, we *only* do the comparisons
+    # where altitude >5 degrees.  The SOFA guides imply that below 5 is where
+    # where accuracy gets more problematic, and testing reveals that alt<~0
+    # gives garbage round-tripping, and <10 can give ~1 arcsec uncertainty
+    if fullstack_obsconditions[0].value == 0:
+        # but if there is no refraction correction, check everything
+        msk = slice(None)
+        tol = 5*u.microarcsecond
+    else:
+        msk = aacoo.alt > 5*u.deg
+        # most of them aren't this bad, but some of those at low alt are offset
+        # this much.  For alt > 10, this is always better than 100 masec
+        tol = 750*u.milliarcsecond
+
+
+    #now make sure the full stack round-tripping works
+    icrs2 = aacoo.transform_to(ICRS)
+
+    adras = np.abs(fullstack_icrs.ra - icrs2.ra)[msk]
+    addecs = np.abs(fullstack_icrs.dec - icrs2.dec)[msk]
+    assert np.all(adras < tol), 'largest RA change is {0} mas, > {1}'.format(np.max(adras.arcsec*1000), tol)
+    assert np.all(addecs < tol), 'largest Dec change is {0} mas, > {1}'.format(np.max(addecs.arcsec*1000), tol)
+
+    #check that we're consistent with the ERFA alt/az result
     xp, yp = u.Quantity(iers.IERS.open().pm_xy(fullstack_times.jd1, fullstack_times.jd2)).to(u.radian).value
     lon = fullstack_locations.geodetic[0].to(u.radian).value
     lat = fullstack_locations.geodetic[1].to(u.radian).value
     height = fullstack_locations.geodetic[2].to(u.m).value
     astrom, eo = erfa.apco13(fullstack_times.jd1, fullstack_times.jd2,
-                          fullstack_times.delta_ut1_utc,
-                          lon, lat, height,
-                          xp, yp,
-                          fullstack_obsconditions[0].to(u.hPa).value,
-                          fullstack_obsconditions[1].to(u.deg_C).value,
-                          fullstack_obsconditions[2],
-                          fullstack_obsconditions[3].to(u.micron).value)
-    erfares = _erfa_check(fullstack_icrs.ra.rad, fullstack_icrs.dec.rad, astrom)
-
-
-    #now make sure the round-tripping works
-
-    #first check just the CIRS stages
-    cirs1 = fullstack_icrs.transform_to(CIRS(obstime=fullstack_times))
-    cirs2 = cirs1.transform_to(altazframe).transform_to(CIRS(obstime=fullstack_times))
-    npt.assert_allclose(cirs1.ra.deg, cirs2.ra.deg)
-    npt.assert_allclose(cirs1.dec.deg, cirs2.dec.deg)
-
-    #now the full stack
-    icrs2 = aacoo.transform_to(ICRS)
-    icrs2 = cirs2.transform_to(ICRS)
-
-    npt.assert_allclose(fullstack_icrs.ra.deg, icrs2.ra.deg)
-    npt.assert_allclose(fullstack_icrs.dec.deg, icrs2.dec.deg)
+                             fullstack_times.delta_ut1_utc,
+                             lon, lat, height,
+                             xp, yp,
+                             fullstack_obsconditions[0].to(u.hPa).value,
+                             fullstack_obsconditions[1].to(u.deg_C).value,
+                             fullstack_obsconditions[2],
+                             fullstack_obsconditions[3].to(u.micron).value)
+    erfadct = _erfa_check(fullstack_icrs.ra.rad, fullstack_icrs.dec.rad, astrom)
+    npt.assert_allclose(erfadct['alt'], aacoo.alt.radian, atol=1e-7)
+    npt.assert_allclose(erfadct['az'], aacoo.az.radian, atol=1e-7)
 
 def test_fiducial_roudtrip(fullstack_icrs, fullstack_fiducial_altaz):
     """
@@ -125,4 +133,3 @@ def test_fiducial_roudtrip(fullstack_icrs, fullstack_fiducial_altaz):
     icrs2 = aacoo.transform_to(ICRS)
     npt.assert_allclose(fullstack_icrs.ra.deg, icrs2.ra.deg)
     npt.assert_allclose(fullstack_icrs.dec.deg, icrs2.dec.deg)
-
