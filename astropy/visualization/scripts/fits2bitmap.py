cc56d739110515894533276b99fe76eb382f428a
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-12-18 23:38:53 +0000

    Removed whitespace

--- astropy/visualization/scripts/fits2bitmap.py
+++ astropy/visualization/scripts/fits2bitmap.py
@@ -25,51 +25,41 @@ def fits2bitmap(filename, ext=0, out_fn=None, scale='linear',
     ----------
     filename : str
         The filename of the FITS file.
-
     ext : int
         FITS extension number of the image to convert.  The default is 0.
-
     out_fn : str
         The filename of the output bitmap image.  The type of bitmap
         is determined by the filename extension (e.g. '.jpg', '.png').
         The default is a PNG file with the same name as the FITS file.
-
     scale : {{'linear', 'sqrt', 'power', log', 'asinh'}}
         The scaling/stretch function to apply to the image.  The default
         is 'linear'.
-
     power : float, optional
         The power index for ``scale='power'`` image scaling.  The
         default is 1.0.
-
     asinh_a : float, optional
         For ``scale='asinh'`` image scaling, the value where the asinh
         curve transitions from linear to logarithmic behavior, expressed
         as a fraction of the normalized image.  Must be in the range
         between 0 and 1.
-
     min_cut : float, optional
         The pixel value of the minimum cut level.  Data values less than
         ``min_cut`` will set to ``min_cut`` before scaling the image.
         The default is the image minimum.  ``min_cut`` overrides
         ``min_percent``.
-
     max_cut : float, optional
         The pixel value of the maximum cut level.  Data values greater
         than ``min_cut`` will set to ``min_cut`` before scaling the
         image.  The default is the image maximum.  ``max_cut`` overrides
         ``max_percent``.
-
     min_percent : float, optional
         The percentile value used to determine the pixel value of
         minimum cut level.  The default is 0.0.  ``min_percent``
         overrides ``percent``.
-
     max_percent : float, optional
         The percentile value used to determine the pixel value of
         maximum cut level.  The default is 100.0.  ``max_percent``
         overrides ``percent``.
-
     percent : float, optional
         The percentage of the image values used to determine the pixel
         values of the minimum and maximum cut levels.  The lower cut
@@ -77,7 +67,6 @@ def fits2bitmap(filename, ext=0, out_fn=None, scale='linear',
         the upper cut level will be set at the ``(100 + percent) / 2``
         percentile.  The default is 100.0.  ``percent`` is ignored if
         either ``min_percent`` or ``max_percent`` is input.
-
     cmap : str
         The matplotlib color map name.  The default is 'Greys_r'.
     """
