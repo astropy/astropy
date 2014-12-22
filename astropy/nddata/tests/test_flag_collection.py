46ea1ad49e9fc44e88a08cd07e3fd615e093ad0c
Matthew Craig <mattwcraig@gmail.com>
2014-12-21 20:48:52 -0600

    Add missing import

--- astropy/nddata/tests/test_flag_collection.py
+++ astropy/nddata/tests/test_flag_collection.py
@@ -5,6 +5,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
+
 import numpy as np
 
 from ...tests.helper import pytest
