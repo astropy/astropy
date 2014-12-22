e0b920d02f076516fdd15b05475abe1637c5c1a9
Matthew Craig <mattwcraig@gmail.com>
2014-12-21 20:53:15 -0600

    Back to the __future__

--- astropy/nddata/slicing.py
+++ astropy/nddata/slicing.py
@@ -1,6 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # This module implements the Slicing mixin to the NDData class.
 
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 __all__ = ['NDSlicingMixin']
 
 
