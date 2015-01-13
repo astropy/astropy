3bb23a68e5db2e2b6c60dad6b3669caa3fb84530
Geert Barentsen <geert@barentsen.be>
2015-01-13 18:22:49 +0000

    Added test to verify that metadata_conflicts='error' raises an exception during metadata merging

--- astropy/utils/tests/test_metadata.py
+++ astropy/utils/tests/test_metadata.py
@@ -1,6 +1,6 @@
 import abc
 
-from ..metadata import MetaData
+from ..metadata import MetaData, MergeConflictError, merge
 from ..compat.odict import OrderedDict
 from ...tests.helper import pytest
 from ...io import fits
@@ -71,3 +71,14 @@ class ExampleData(object):
 class TestMetaExampleData(MetaBaseTest):
     test_class = ExampleData
     args = ()
+
+
+def test_metadata_conflicts_exception():
+    """Test the behaviour of merging metadata with metadata_conflicts='error'"""
+    from ..metadata import merge, MergeConflictError
+    data1 = ExampleData()
+    data2 = ExampleData()
+    data1.meta['somekey'] = {'x': 1, 'y': 1}
+    data2.meta['somekey'] = {'x': 1, 'y': 999}
+    with pytest.raises(MergeConflictError):
+        merge(data1.meta, data2.meta, metadata_conflicts='error')
