33a1421fa850c3f5ff3bda6e96f2f9e5e1163976
Nadia Dencheva <nadia.dencheva@gmail.com>
2015-01-14 15:13:12 -0500

    Fix Mapping.inverse. Allow models to be initialized with a name argument. Add docstrings.

--- astropy/modeling/mappings.py
+++ astropy/modeling/mappings.py
@@ -10,16 +10,34 @@ __all__ = ['Mapping', 'Identity']
 
 
 class Mapping(Model):
-    def __init__(self, mapping):
+    """
+    Allows inputs to be reordered, duplicated or dropped.
+
+    Parameters
+    ----------
+    mapping : tuple
+        Integers representing indices of the inputs.
+
+    Raises
+    ------
+    TypeError
+        Raised when number of inputs is less that `max(mapping)`.
+
+    Examples
+    --------
+    >>> poly1 = Polynomial2D(1, c0_0=1, c1_0=2, c0_1=3)
+    >>> poly2 = Polynomial2D(1, c0_0=1, c1_0=2.4, c0_1=2.1)
+    >>> model = (Shift(1) & Shift(2)) | Mapping((0, 1, 0, 1)) | (poly1 & poly2)
+    >>> model(1, 2)
+    (17.0, 14.2)
+
+    """
+    def __init__(self, mapping, **kwargs):
         self._inputs = tuple('x' + str(idx)
                              for idx in range(max(mapping) + 1))
         self._outputs = tuple('x' + str(idx) for idx in range(len(mapping)))
         self._mapping = mapping
-        super(Mapping, self).__init__()
-
-    @property
-    def name(self):
-        return 'Mapping({0})'.format(self.mapping)
+        super(Mapping, self).__init__(**kwargs)
 
     @property
     def inputs(self):
@@ -55,17 +73,37 @@ class Mapping(Model):
                 "Mappings such as {0} that drop one or more of their inputs "
                 "are not invertible at this time.".format(self.mapping))
 
-        return self.__class__(mapping)
+        inv = self.__class__(mapping)
+        inv._inputs = self._outputs
+        inv._outputs = self._inputs
+        return inv
 
 
 class Identity(Mapping):
-    def __init__(self, n_inputs):
-        mapping = tuple(range(n_inputs))
-        super(Identity, self).__init__(mapping)
+    """
+    Returns inputs unchanged.
 
-    @property
-    def name(self):
-        return 'Identity({0})'.format(self.n_inputs)
+    This class is useful in compound models when some of the inputs must be
+    passed unchanged to the next model.
+
+    Parameters
+    ----------
+    n_inputs : int
+        Specifies how many of the inputs will be returned.
+
+    Examples
+    --------
+    # Transform (x, y) by a shift in x, followed by scaling the two inputs.
+    >>> model = (Shift(1) & Identity(1)) | Scale(1.2) & Scale(2)
+    >>> model(1,1)
+    (2.4, 2.0)
+    >>> model.inverse(2.4,2)
+    (1.0, 1.0)
+
+    """
+    def __init__(self, n_inputs, **kwargs):
+        mapping = tuple(range(n_inputs))
+        super(Identity, self).__init__(mapping, **kwargs)
 
     @property
     def inverse(self):
