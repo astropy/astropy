b5f50737375cc782216f65a5cd6a2d4aeb9691e6
Erik M. Bray <embray@stsci.edu>
2015-01-28 22:41:08 -0500

    Add minversion and resolve_name utilities.  Right now resolve_name is only used by minversion (and in fact for a feature that we don't really need right now.  But it may be useful in other places.

--- astropy/utils/introspection.py
+++ astropy/utils/introspection.py
@@ -14,10 +14,117 @@ import types
 from ..extern import six
 
 
-__all__ = ['find_current_module', 'isinstancemethod']
+__all__ = ['resolve_name', 'minversion', 'find_current_module',
+           'isinstancemethod']
 
 
 __doctest_skip__ = ['find_current_module']
+
+
+def resolve_name(name):
+    """Resolve a name like ``module.object`` to an object and return it.
+
+    This ends up working like ``from module import object`` but is easier
+    to deal with than the `__import__` builtin and supports digging into
+    submodules.
+
+    Parameters
+    ----------
+
+    name : `str`
+        A dotted path to a Python object--that is, the name of a function,
+        class, or other object in a module with the full path to that module,
+        including parent modules, separated by dots.  Also known as the fully
+        qualified name of the object.
+
+    Examples
+    --------
+
+    >>> resolve_name('astropy.utils.introspection.resolve_name')
+    <function resolve_name at 0x...>
+
+    Raises
+    ------
+    `ImportError`
+        If the module or named object is not found.
+    """
+
+    parts = name.split('.')
+    cursor = len(parts) - 1
+    module_name = parts[:cursor]
+    attr_name = parts[-1]
+
+    while cursor > 0:
+        try:
+            ret = __import__('.'.join(module_name), fromlist=[attr_name])
+            break
+        except ImportError:
+            if cursor == 0:
+                raise
+            cursor -= 1
+            module_name = parts[:cursor]
+            attr_name = parts[cursor]
+            ret = ''
+
+    for part in parts[cursor:]:
+        try:
+            ret = getattr(ret, part)
+        except AttributeError:
+            raise ImportError(name)
+
+    return ret
+
+
+def minversion(module, version, inclusive=True, version_path='__version__'):
+    """
+    Returns `True` if the specified Python module satisfies a minimum version
+    requirement, and `False` if not.
+
+    By default this uses `pkg_resources.parse_version` to do the version
+    comparison if available.  Otherwise it falls back on
+    `distutils.version.LooseVersion`.
+
+    Parameters
+    ----------
+
+    module : module
+        An imported module of which to check the version.
+
+    version : `str`
+        The version as a string that this module must have at a minimum (e.g.
+        ``'0.12'``).
+
+    inclusive : `bool`
+        The specified version meets the requirement inclusively (i.e. ``>=``)
+        as opposed to strictly greater than (default: `True`).
+
+    version_path : `str`
+        A dotted attribute path to follow in the module for the version.
+        Defaults to just ``'__version__'``, which should work for most Python
+        modules.
+
+    Examples
+    --------
+
+    >>> import astropy
+    >>> minversion(astropy, '0.4.4')
+    True
+    """
+
+    if '.' not in version_path:
+        have_version = getattr(module, version_path)
+    else:
+        have_version = resolve_name('.'.join([module.__name__, version_path]))
+
+    try:
+        from pkg_resources import parse_version
+    except ImportError:
+        from distutils.version import LooseVersion as parse_version
+
+    if inclusive:
+        return parse_version(have_version) >= parse_version(version)
+    else:
+        return parse_version(have_version) > parse_version(version)
 
 
 def find_current_module(depth=1, finddiff=False):
