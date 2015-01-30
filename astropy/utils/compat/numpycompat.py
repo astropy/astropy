9f734de7b053b7502a6d7a33b3fa2ecc0be95f84
Erik M. Bray <embray@stsci.edu>
2015-01-30 12:01:11 -0500

    The docstring here was misleading.  This patch is really required for any Numpy < 1.8

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -18,7 +18,6 @@ __all__ = ['NUMPY_LT_1_6_1', 'NUMPY_LT_1_7', 'NUMPY_LT_1_8', 'NUMPY_LT_1_9',
 # features/bugs we're checking for (ex:
 # astropy.table.table._BROKEN_UNICODE_TABLE_SORT)
 NUMPY_LT_1_6_1 = not minversion(np, '1.6.1')
-NUMPY_LE_1_7 = not minversion(np, '1.7.0', inclusive=False)
 NUMPY_LT_1_7 = not minversion(np, '1.7.0')
 NUMPY_LT_1_8 = not minversion(np, '1.8.0')
 NUMPY_LT_1_9 = not minversion(np, '1.9.0')
@@ -27,13 +26,13 @@ NUMPY_LT_1_9_1 = not minversion(np, '1.9.1')
 
 def _monkeypatch_unicode_mask_fill_values():
     """
-    Numpy <= 1.7.0 on Python 2 does not support Unicode fill values, since
+    Numpy < 1.8.0 on Python 2 does not support Unicode fill values, since
     it assumes that all of the string dtypes are ``S`` and ``V`` (not ``U``).
 
     This monkey patches the function that validates and corrects a
     fill value to handle this case.
     """
-    if NUMPY_LE_1_7 and six.PY2:
+    if NUMPY_LT_1_8 and six.PY2:
         from numpy.ma import core as ma_core
         _check_fill_value_original = ma_core._check_fill_value
 
