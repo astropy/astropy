11c278a45d1cf3638d563ae7ca07c7402556dc73
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-02-17 11:05:51 +0000

    Merge pull request #3509 from olebole/Fix_FreeBSD_Test_Failures
    Catch also OSError in multiprocessing.Queue()

--- astropy/io/ascii/cparser.pyx
+++ astropy/io/ascii/cparser.pyx
@@ -391,7 +391,7 @@ cdef class CParser:
         cdef int N = self.parallel
         try:
             queue = multiprocessing.Queue()
-        except (ImportError, NotImplementedError, AttributeError):
+        except (ImportError, NotImplementedError, AttributeError, OSError):
             self.raise_error("shared semaphore implementation required "
                              "but not available")
         cdef int offset = self.tokenizer.source_pos
