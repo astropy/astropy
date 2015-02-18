35501fcba6811705fcd53669742db8346727672d
Erik M. Bray <embray@stsci.edu>
2015-02-18 18:01:03 -0500

    Updated some copyright dates

--- docs/conf.py
+++ docs/conf.py
@@ -87,7 +87,7 @@ rst_epilog += """
 
 project = u'Astropy'
 author = u'The Astropy Developers'
-copyright = u'2011-2014, ' + author
+copyright = u'2011-2015, ' + author
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
