60ab8720ff236338dfd2f74528e7eabc74b926a8
Michael Droettboom <mdboom@gmail.com>
2015-04-08 12:49:24 -0400

    Rename WCSLIB_VERSION to __version__ on the Python side

--- astropy/wcs/tests/test_wcs.py
+++ astropy/wcs/tests/test_wcs.py
@@ -394,7 +394,7 @@ def test_validate():
     with catch_warnings():
         results = wcs.validate(get_pkg_data_filename("data/validate.fits"))
         results_txt = repr(results)
-        if wcs._wcs.WCSLIB_VERSION[0] == '5':
+        if wcs._wcs.__version__[0] == '5':
             filename = 'data/validate.5.0.txt'
         else:
             filename = 'data/validate.txt'
