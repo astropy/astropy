d308f16bb8d50a4246e6432881cf9d1ba71d3f02
Larry Bradley <larry.bradley@gmail.com>
2015-04-09 16:59:35 -0400

    Add block_replicate

--- astropy/nddata/utils.py
+++ astropy/nddata/utils.py
@@ -8,7 +8,7 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 
 __all__ = ['extract_array', 'add_array', 'subpixel_indices',
-           'overlap_slices', 'block_reduce']
+           'overlap_slices', 'block_reduce', 'block_replicate']
 
 
 def overlap_slices(large_array_shape, small_array_shape, position):
@@ -198,7 +198,7 @@ def subpixel_indices(position, subsampling):
 
 def block_reduce(data, block_size, func=np.sum):
     """
-    Downsample data by applying a function to local blocks.
+    Downsample a data array by applying a function to local blocks.
 
     If ``data`` is not perfectly divisible by ``block_size`` along a
     given axis then the data will be trimmed (from the end) along that
@@ -246,3 +246,58 @@ def block_reduce(data, block_size, func=np.sum):
             data = data.swapaxes(0, i)
 
     return block_reduce(data, tuple(block_size), func=func)
+
+
+def block_replicate(data, block_size, conserve_sum=True):
+    """
+    Upsample a data array by block replication.
+
+    Parameters
+    ----------
+    data : array_like (1D, 2D, or 3D)
+        The data to be block replicated.
+
+    block_size : array_like (int)
+        The integer block size (upsampling factor) along each axis.
+
+    conserve_sum : bool
+        If `True` (the default) then the sum of the output
+        block-replicated data will equal the sum of the input ``data``.
+
+    Returns
+    -------
+    output : array_like
+        The block-replicated data.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from imutils import block_replicate
+    >>> data = np.array([[0., 1.], [2., 3.]])
+    >>> block_replicate(data, 2)
+    array([[ 0.  ,  0.  ,  0.25,  0.25],
+           [ 0.  ,  0.  ,  0.25,  0.25],
+           [ 0.5 ,  0.5 ,  0.75,  0.75],
+           [ 0.5 ,  0.5 ,  0.75,  0.75]])
+
+    >>> block_replicate(data, 2, conserve_sum=False)
+    array([[ 0.,  0.,  1.,  1.],
+           [ 0.,  0.,  1.,  1.],
+           [ 2.,  2.,  3.,  3.],
+           [ 2.,  2.,  3.,  3.]])
+    """
+
+    data = np.asanyarray(data)
+
+    block_size = np.atleast_1d(block_size)
+    if len(block_size) != data.ndim:
+        raise ValueError('`block_size` must have the same length as '
+                         '`data.shape`')
+
+    for i in range(data.ndim):
+        data = np.repeat(data, block_size[i], axis=i)
+
+    if conserve_sum:
+        data /= float(np.prod(block_size))
+
+    return data
