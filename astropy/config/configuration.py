astropy/astropy

commit 7186664e777153f5f0d735aee9f09a1bf4c1c5db

Don't need find_current_module to find module of class definition.

astropy/config/configuration.py
@@ -225,13 +225,7 @@ class ConfigItem(object):
         from ..utils import isiterable
 
         if module is None:
-            module = find_current_module(2)
-            if module is None:
-                msg1 = 'Cannot automatically determine get_config module, '
-                msg2 = 'because it is not called from inside a valid module'
-                raise RuntimeError(msg1 + msg2)
-            else:
-                module = module.__name__
+            module = self.__class__.__module__
 
         self.module = module
         self.description = description

commit 71ab26d8e7397cc7758aae64c4b89253accc86f7

Merge pull request #3627 from mdboom/config/revert-broken-old-api
Revert 7186664e, which broke the old config API.

astropy/config/configuration.py
@@ -225,7 +225,13 @@ class ConfigItem(object):
         from ..utils import isiterable
 
         if module is None:
-            module = self.__class__.__module__
+            module = find_current_module(2)
+            if module is None:
+                msg1 = 'Cannot automatically determine get_config module, '
+                msg2 = 'because it is not called from inside a valid module'
+                raise RuntimeError(msg1 + msg2)
+            else:
+                module = module.__name__
 
         self.module = module
         self.description = description