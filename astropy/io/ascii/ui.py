f71ad44395d7932f80c6ca4ac48b594fd3dc45a8
Tom Aldcroft <taldcroft@gmail.com>
2015-04-15 22:48:58 -0400

    Add io.ascii.READ_TRIES list that shows how table got read

--- astropy/io/ascii/ui.py
+++ astropy/io/ascii/ui.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, division, print_function
 import re
 import os
 import sys
+import copy
 
 
 from . import core
@@ -31,6 +32,8 @@ from . import fixedwidth
 from ...table import Table
 from ...utils.data import get_readable_fileobj
 from ...extern import six
+
+READ_TRIES = []
 
 try:
     import yaml
@@ -186,6 +189,7 @@ def read(table, guess=None, **kwargs):
     dat : `~astropy.table.Table`
         Output table
     """
+    del READ_TRIES[:]
 
     if 'fill_values' not in kwargs:
         kwargs['fill_values'] = [('', '0')]
@@ -238,18 +242,27 @@ def read(table, guess=None, **kwargs):
         # Try the fast reader first if applicable
         if fast_reader_param and format is not None and 'fast_{0}'.format(format) \
                                                         in core.FAST_CLASSES:
-            new_kwargs['Reader'] = core.FAST_CLASSES['fast_{0}'.format(format)]
-            fast_reader = get_reader(**new_kwargs)
+            fast_kwargs = copy.copy(new_kwargs)
+            fast_kwargs['Reader'] = core.FAST_CLASSES['fast_{0}'.format(format)]
+            fast_reader = get_reader(**fast_kwargs)
             try:
-                return fast_reader.read(table)
+                dat = fast_reader.read(table)
+                READ_TRIES.append({'kwargs': fast_kwargs,
+                                   'status': 'Success with fast reader (no guessing)'})
             except (core.ParameterError, cparser.CParserError) as e:
                 # special testing value to avoid falling back on the slow reader
                 if fast_reader_param == 'force':
                     raise e
                 # If the fast reader doesn't work, try the slow version
                 dat = reader.read(table)
+                READ_TRIES.append({'kwargs': new_kwargs,
+                                   'status': 'Success with slow reader after failing'
+                                            ' with fast (no guessing)'})
         else:
             dat = reader.read(table)
+            READ_TRIES.append({'kwargs': new_kwargs,
+                               'status': 'Success with slow reader which has no fast '
+                                         'reader analog (no guessing)'})
 
     return dat
 
@@ -339,19 +352,31 @@ def _guess(table, read_kwargs, format, fast_reader):
 
             reader = get_reader(**guess_kwargs)
             reader.guessing = True
-            return reader.read(table)
+            dat = reader.read(table)
+            READ_TRIES.append({'kwargs': guess_kwargs, 'status': 'Success (guessing)'})
+            return dat
 
         except (core.InconsistentTableError, ValueError, TypeError, AttributeError,
-                core.OptionalTableImportError, core.ParameterError, cparser.CParserError):
+                core.OptionalTableImportError, core.ParameterError, cparser.CParserError) as err:
+            READ_TRIES.append({'kwargs': guess_kwargs,
+                               'status': '{0}: {1}'.format(err.__class__.__name__,
+                                                           str(err))})
             failed_kwargs.append(guess_kwargs)
     else:
         # Failed all guesses, try the original read_kwargs without column requirements
         try:
             reader = get_reader(**read_kwargs)
-            return reader.read(table)
+            dat = reader.read(table)
+            READ_TRIES.append({'kwargs': read_kwargs,
+                               'status': 'Success with original kwargs without strict_names '
+                                         '(guessing)'})
+            return dat
 
         except (core.InconsistentTableError, ValueError, ImportError,
-                core.OptionalTableImportError, core.ParameterError, cparser.CParserError):
+                core.OptionalTableImportError, core.ParameterError, cparser.CParserError) as err:
+            READ_TRIES.append({'kwargs': guess_kwargs,
+                               'status': '{0}: {1}'.format(err.__class__.__name__,
+                                                           str(err))})
             failed_kwargs.append(read_kwargs)
             lines = ['\nERROR: Unable to guess table format with the guesses listed below:']
             for kwargs in failed_kwargs:
