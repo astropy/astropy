cae300d7af9772616d44a6f025ba8fe13cb78295
Christoph Deil <Deil.Christoph@gmail.com>
2015-04-28 10:10:34 +0200

    Initial commit of astroplan edited for sites

---
+++ astroplan/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+Observation planning package for astronomers.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+# For egg_info test builds to pass, put package imports here.
+if not _ASTROPY_SETUP_:
+    pass
