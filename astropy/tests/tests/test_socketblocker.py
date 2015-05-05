4fd1241c1caa486170e2ec0dd73d82ff5891f9ae
Erik Bray <erik.m.bray@gmail.com>
2015-05-05 13:13:52 -0400

    Merge pull request #3713 from embray/testing/issue-3713
    astropy test framework's disable_internet is incompatible with python 3.4 multiprocessing forkserver

--- astropy/tests/tests/test_socketblocker.py
+++ astropy/tests/tests/test_socketblocker.py
@@ -1,11 +1,15 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+import sys
+import time
+
+from threading import Thread
+
 from ..helper import pytest
 from ..disable_internet import no_internet
 from astropy.extern.six.moves import BaseHTTPServer, SimpleHTTPServer
 from astropy.extern.six.moves.urllib.request import urlopen
-from threading import Thread
-import time
 
 
 def test_outgoing_fails():
@@ -52,3 +56,31 @@ def test_localconnect_succeeds(localhost):
     time.sleep(0.1)
 
     urlopen('http://{localhost:s}:{port:d}'.format(localhost=localhost,port=port)).close()
+
+
+PY3_4 = sys.version_info[:2] >= (3, 4)
+
+
+# Used for the below test--inline functions aren't pickleable
+# by multiprocessing?
+def _square(x):
+    return x ** 2
+
+
+@pytest.mark.skipif('not PY3_4 or sys.platform == "win32"')
+def test_multiprocessing_forkserver():
+    """
+    Test that using multiprocessing with forkserver works.  Perhaps
+    a simpler more direct test would be to just open some local
+    sockets and pass something through them.
+
+    Regression test for https://github.com/astropy/astropy/pull/3713
+    """
+
+    import multiprocessing
+    ctx = multiprocessing.get_context('forkserver')
+    pool = ctx.Pool(1)
+    result = pool.map(_square, [1, 2, 3, 4, 5])
+    pool.close()
+    pool.join()
+    assert result == [1, 4, 9, 16, 25]
