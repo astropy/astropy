a1ad42ce332d2195db31d8c6bc17d26ad74c1c55
Neil Parley <mail@neilparley.co.uk>
2015-05-06 09:19:31 +0300

    Update sigma_clipping.py

--- astropy/stats/sigma_clipping.py
+++ astropy/stats/sigma_clipping.py
@@ -135,9 +135,10 @@ def sigma_clip(data, sig=3.0, iters=1, cenfunc=np.ma.median, varfunc=np.var,
             standard_deviations = np.sqrt(varfunc(filtered_data, **kwargs)) * sig
             min_value = max_value - standard_deviations
             max_value += standard_deviations
-            if axis > 0:
-                min_value = np.expand_dims(min_value, axis=axis)
-                max_value = np.expand_dims(max_value, axis=axis)
+            if axis is not None:
+                if axis > 0:
+                    min_value = np.expand_dims(min_value, axis=axis)
+                    max_value = np.expand_dims(max_value, axis=axis)
             filtered_data.mask |= filtered_data > max_value
             filtered_data.mask |= filtered_data < min_value
     else:
@@ -146,9 +147,10 @@ def sigma_clip(data, sig=3.0, iters=1, cenfunc=np.ma.median, varfunc=np.var,
             standard_deviations = np.sqrt(varfunc(filtered_data, **kwargs)) * sig
             min_value = max_value - standard_deviations
             max_value += standard_deviations
-            if axis > 0:
-                min_value = np.expand_dims(min_value, axis=axis)
-                max_value = np.expand_dims(max_value, axis=axis)
+            if axis is not None:    
+                if axis > 0:
+                    min_value = np.expand_dims(min_value, axis=axis)
+                    max_value = np.expand_dims(max_value, axis=axis)
             filtered_data.mask |= filtered_data > max_value
             filtered_data.mask |= filtered_data < min_value
 
