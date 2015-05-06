a958cef50f7c052701b058c5976cc8cc8b208539
Neil Parley <mail@neilparley.co.uk>
2015-05-06 15:12:33 +0300

    Re-fix axis is None for python 3

--- astropy/stats/sigma_clipping.py
+++ astropy/stats/sigma_clipping.py
@@ -136,9 +136,10 @@ def sigma_clip(data, sig=3.0, iters=1, cenfunc=np.ma.median, varfunc=np.var,
             standard_deviations *= sig
             min_value = max_value - standard_deviations
             max_value += standard_deviations
-            if axis > 0:
-                min_value = np.expand_dims(min_value, axis=axis)
-                max_value = np.expand_dims(max_value, axis=axis)
+            if axis is not None:
+                if axis > 0:
+                    min_value = np.expand_dims(min_value, axis=axis)
+                    max_value = np.expand_dims(max_value, axis=axis)
             filtered_data.mask |= filtered_data > max_value
             filtered_data.mask |= filtered_data < min_value
     else:
@@ -148,9 +149,10 @@ def sigma_clip(data, sig=3.0, iters=1, cenfunc=np.ma.median, varfunc=np.var,
             standard_deviations *= sig
             min_value = max_value - standard_deviations
             max_value += standard_deviations
-            if axis > 0:
-                min_value = np.expand_dims(min_value, axis=axis)
-                max_value = np.expand_dims(max_value, axis=axis)
+            if axis is not None:
+                if axis > 0:
+                    min_value = np.expand_dims(min_value, axis=axis)
+                    max_value = np.expand_dims(max_value, axis=axis)
             filtered_data.mask |= filtered_data > max_value
             filtered_data.mask |= filtered_data < min_value
 
