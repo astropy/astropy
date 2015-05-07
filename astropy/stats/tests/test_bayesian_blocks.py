371ce691b4e995e7beb77117831d51b3d0468f4a
Jake VanderPlas <jakevdp@gmail.com>
2015-05-07 07:10:46 -0700

    TST: increase coverage in stats.bayesian_blocks

--- astropy/stats/tests/test_bayesian_blocks.py
+++ astropy/stats/tests/test_bayesian_blocks.py
@@ -4,8 +4,10 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import numpy as np
+
+from ...tests.helper import pytest
 from  numpy.testing import assert_allclose
-from .. import bayesian_blocks
+from .. import bayesian_blocks, Events, RegularEvents, PointMeasures
 
 
 def test_single_change_point():
@@ -63,7 +65,51 @@ def test_regular_events():
                             np.unique(np.random.randint(500, 1000, 200))])
     t = dt * steps
 
-    bins = bayesian_blocks(t, fitness='regular_events', dt=dt)
+    # string fitness
+    bins1 = bayesian_blocks(t, fitness='regular_events', dt=dt)
+    assert (len(bins1) == 3)
+    assert_allclose(bins1[1], 5, rtol=0.05)
 
-    assert (len(bins) == 3)
-    assert_allclose(bins[1], 5, rtol=0.05)
+    # class name fitness
+    bins2 = bayesian_blocks(t, fitness=RegularEvents, dt=dt)
+    assert_allclose(bins1, bins2)
+
+    # class instance fitness
+    bins3 = bayesian_blocks(t, fitness=RegularEvents(dt=dt))
+    assert_allclose(bins1, bins3)
+
+
+def test_errors():
+    rng = np.random.RandomState(0)
+    t = rng.rand(100)
+
+    # x must be integer or None for events
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='events', x=t)
+
+    # x must be binary for regular events
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='regular_events', x=10 * t)
+    
+    # x must be specified for measures
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='measures')
+
+    # sigma cannot be specified without x
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='events', sigma=0.5)
+
+    # length of x must match length of t
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='measures', x=t[:-1])
+
+    # repeated values in t fail when x is specified
+    t2 = t.copy()
+    t2[1] = t2[0]
+    with pytest.raises(ValueError):
+        bayesian_blocks(t2, fitness='measures', x=t)
+
+    # sigma must be broadcastable with x
+    with pytest.raises(ValueError):
+        bayesian_blocks(t, fitness='measures',  x=t, sigma=t[:-1])
+    
