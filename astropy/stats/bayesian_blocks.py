5ed0f20ce7435fe2f2bbcd869733ded0be3290bc
Jake VanderPlas <jakevdp@gmail.com>
2015-05-08 10:29:50 -0700

    DOC: improve docs for bayesian_blocks routines & classes

--- astropy/stats/bayesian_blocks.py
+++ astropy/stats/bayesian_blocks.py
@@ -24,7 +24,6 @@ For more fine-tuned control over the fitness functions used, it is possible
 to define custom :class:`FitnessFunc` classes directly and use them with
 the :func:`bayesian_blocks` routine.
 
-
 One common application of the Bayesian Blocks algorithm is the determination
 of optimal adaptive-width histogram bins. This uses the same fitness function
 as for irregularly-spaced time series events. The easiest interface for
@@ -80,11 +79,12 @@ def bayesian_blocks(t, x=None, sigma=None,
             of the prior on the number of bins.  If ``gamma`` is not specified,
             then a simulation-derived prior will be used.
 
-        Alternatively, the fitness parameter can be an instance of FitnessFunc
-        or a subclass thereof.
+        Alternatively, the fitness parameter can be an instance of
+        :class:`FitnessFunc` or a subclass thereof.
 
     **kwargs :
-        any additional keyword arguments will be passed to FitnessFunc
+        any additional keyword arguments will be passed to the specified
+        :class:`FitnessFunc` derived class.
 
     Returns
     -------
@@ -110,7 +110,7 @@ def bayesian_blocks(t, x=None, sigma=None,
     >>> t = dt * np.arange(1000)
     >>> x = np.zeros(len(t))
     >>> x[np.random.randint(0, len(t), len(t) // 10)] = 1
-    >>> edges = bayesian_blocks(t, fitness='regular_events', dt=dt, gamma=0.9)
+    >>> edges = bayesian_blocks(t, x, fitness='regular_events', dt=dt)
 
     Measured point data with errors:
 
@@ -147,15 +147,44 @@ def bayesian_blocks(t, x=None, sigma=None,
 class FitnessFunc(object):
     """Base class for fitness functions
 
-    Each fitness function class has the following:
+    Derived classes should overload the following method:
 
-    fitness(...)
-      compute fitness function.
-      Arguments accepted by fitness must be among [T_k, N_k, a_k, b_k, c_k]
+    ``fitness(self, **kwargs)``:
+      Compute the fitness given a set of named arguments.
+      Arguments accepted by fitness must be among ``[T_k, N_k, a_k, b_k, c_k]``
+      (See Scargle2012_ for details on the meaning of these parameters).
 
-    prior(N, Ntot)
-      compute prior on N given a total number of points Ntot
+    Additionally, other methods may be overloaded as well:
 
+    ``__init__(self, **kwargs)``:
+      Initialize the fitness function with any parameters beyond the normal
+      ``p0`` and ``gamma``.
+
+    ``validate_input(self, t, x, sigma)``:
+      Enable specific checks of the input data (``t``, ``x``, ``sigma``)
+      to be performed prior to the fit.
+
+    ``prior(self, N, Ntot)``:
+      Specify the form of the Bayesian prior on the number of bins ``N`` given
+      the total number of points ``Ntot``. By default it will call
+      the ``gamma_prior()`` method if ``gamma`` is specified, and otherwise
+      call the ``p0_prior()`` method.
+
+    ``p0_prior(self, N, Ntot)``:
+      Specify the form of the prior given the false-alarm probability ``p0``
+      (See Scargle2012_ for details).
+
+    ``gamma_prior(self, N, Ntot)``:
+      Specify the form of the prior given the slope ``gamma`` of the prior
+      on the number of bins (See Scargle2012_ for details).
+
+    For examples of implemented fitness functions, see :class:`Events`,
+    :class:`RegularEvents`, and :class:`PointMeasures`.
+
+    References
+    ----------
+    .. [Scargle2012] Scargle, J et al. (2012)
+       http://adsabs.harvard.edu/abs/2012arXiv1207.5578S
     """
     def __init__(self, p0=0.05, gamma=None):
         self.p0 = p0
@@ -234,17 +263,27 @@ class FitnessFunc(object):
         raise NotImplementedError()
 
     def prior(self, N, Ntot):
+        """
+        Compute the prior on the number of blocks ``N``,
+        given the maximum possible blocks ``Ntot``.
+        """
         if self.gamma is None:
             return self.p0_prior(N, Ntot)
         else:
             return self.gamma_prior(N, Ntot)
 
     def p0_prior(self, N, Ntot):
-        # eq. 21 from Scargle 2012
+        """
+        Empirical prior, parametrized by the false alarm probability ``p0``
+        See  eq. 21 in Scargle (2012)
+        """
         return 4 - np.log(73.53 * self.p0 * (N ** -0.478))
 
     def gamma_prior(self, N, Ntot):
-        """Basic prior, parametrized by gamma (eq. 3 in Scargle 2012)"""
+        """
+        Basic prior, parametrized by slope gamma.
+        See eq. 3 in Scargle (2012)
+        """
         if self.gamma == 1:
             return 0
         else:
@@ -255,7 +294,7 @@ class FitnessFunc(object):
     # the fitness_args property will return the list of arguments accepted by
     # the method fitness().  This allows more efficient computation below.
     @property
-    def args(self):
+    def _fitness_args(self):
         return inspect.getargspec(self.fitness)[0][1:]
 
     def fit(self, t, x=None, sigma=None):
@@ -278,11 +317,11 @@ class FitnessFunc(object):
         t, x, sigma = self.validate_input(t, x, sigma)
 
         # compute values needed for computation, below
-        if 'a_k' in self.args:
+        if 'a_k' in self._fitness_args:
             ak_raw = np.ones_like(x) / sigma ** 2
-        if 'b_k' in self.args:
+        if 'b_k' in self._fitness_args:
             bk_raw = x / sigma ** 2
-        if 'c_k' in self.args:
+        if 'c_k' in self._fitness_args:
             ck_raw = x * x / sigma ** 2
 
         # create length-(N + 1) array of cell edges
@@ -304,23 +343,23 @@ class FitnessFunc(object):
             kwds = {}
 
             # T_k: width/duration of each block
-            if 'T_k' in self.args:
+            if 'T_k' in self._fitness_args:
                 kwds['T_k'] = block_length[:R + 1] - block_length[R + 1]
 
             # N_k: number of elements in each block
-            if 'N_k' in self.args:
+            if 'N_k' in self._fitness_args:
                 kwds['N_k'] = np.cumsum(x[:R + 1][::-1])[::-1]
 
             # a_k: eq. 31
-            if 'a_k' in self.args:
+            if 'a_k' in self._fitness_args:
                 kwds['a_k'] = 0.5 * np.cumsum(ak_raw[:R + 1][::-1])[::-1]
 
             # b_k: eq. 32
-            if 'b_k' in self.args:
+            if 'b_k' in self._fitness_args:
                 kwds['b_k'] = - np.cumsum(bk_raw[:R + 1][::-1])[::-1]
 
             # c_k: eq. 33
-            if 'c_k' in self.args:
+            if 'c_k' in self._fitness_args:
                 kwds['c_k'] = 0.5 * np.cumsum(ck_raw[:R + 1][::-1])[::-1]
 
             # evaluate fitness function
@@ -372,12 +411,9 @@ class Events(FitnessFunc):
             raise ValueError("x must be integer counts for fitness='events'")
         return t, x, sigma
 
-    def prior(self, N, Ntot):
-        if self.gamma is not None:
-            return self.gamma_prior(N, Ntot)
-        else:
-            # eq. 21 from Scargle 2012
-            return 4 - np.log(73.53 * self.p0 * (N ** -0.478))
+    def p0_prior(self, N, Ntot):
+        # eq. 21 from Scargle 2012
+        return 4 - np.log(73.53 * self.p0 * (N ** -0.478))
 
 
 class RegularEvents(FitnessFunc):
