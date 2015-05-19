b4cc6d9239911313585facdbf3a70e50a77bf4b2
Jake VanderPlas <jakevdp@gmail.com>
2015-05-19 09:49:10 -0700

    TST: add tests of output for bayesian block models

--- astropy/stats/tests/test_bayesian_blocks.py
+++ astropy/stats/tests/test_bayesian_blocks.py
@@ -91,7 +91,7 @@ def test_errors():
     # x must be binary for regular events
     with pytest.raises(ValueError):
         bayesian_blocks(t, fitness='regular_events', x=10 * t, dt=1)
-    
+
     # x must be specified for measures
     with pytest.raises(ValueError):
         bayesian_blocks(t, fitness='measures')
@@ -113,4 +113,38 @@ def test_errors():
     # sigma must be broadcastable with x
     with pytest.raises(ValueError):
         bayesian_blocks(t, fitness='measures',  x=t, sigma=t[:-1])
-    
+
+
+def test_fitness_function_results():
+    """Test results for several fitness functions"""
+    rng = np.random.RandomState(42)
+
+    # Event Data
+    t = rng.randn(100)
+    edges = bayesian_blocks(t, fitness='events')
+    assert_allclose(edges, [-2.6197451, -1.97361952, -0.71094865,
+                            0.36866702, 1.85227818])
+
+    # Event data with repeats
+    t[80:] = t[:20]
+    edges = bayesian_blocks(t, fitness='events', p0=0.01)
+    assert_allclose(edges, [-2.6197451, -1.97361952, -0.47432431,
+                            -0.46202823,  1.85227818])
+
+    # Regular event data
+    dt = 0.01
+    t = dt * np.arange(1000)
+    x = np.zeros(len(t))
+    N = len(t) // 10
+    x[rng.randint(0, len(t), N)] = 1
+    x[rng.randint(0, len(t) // 2, N)] = 1
+    edges = bayesian_blocks(t, x, fitness='regular_events', dt=dt)
+    assert_allclose(edges, [0, 5.105, 9.99])
+
+    # Measured point data with errors
+    t = 100 * rng.rand(20)
+    x = np.exp(-0.5 * (t - 50) ** 2)
+    sigma = 0.1
+    x_obs = x + sigma * rng.randn(len(x))
+    edges = bayesian_blocks(t, x_obs, sigma, fitness='measures')
+    assert_allclose(edges, [4.360377, 48.456895, 52.597917, 99.455051])
