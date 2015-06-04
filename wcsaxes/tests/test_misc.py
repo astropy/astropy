a3eaa503d09088d5bb6bfe83d8ada0e442cf78d4
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-06-04 16:23:52 +0200

    Fixed a bug that occurred if format_coord was called before first draw

--- wcsaxes/tests/test_misc.py
+++ wcsaxes/tests/test_misc.py
@@ -8,3 +8,18 @@ def test_grid_regression():
     plt.rc('axes', grid=True)
     fig = plt.figure(figsize=(3, 3))
     WCSAxes(fig, [0.1, 0.1, 0.8, 0.8])
+
+
+def test_format_coord_regression(tmpdir):
+    # Regression test for a bug that meant that if format_coord was called by
+    # Matplotlib before the axes were drawn, an error occurred.
+    fig = plt.figure(figsize=(3, 3))
+    ax = WCSAxes(fig, [0.1, 0.1, 0.8, 0.8])
+    fig.add_axes(ax)
+    assert ax.format_coord(10,10) == ""
+    assert ax.coords[0].format_coord(10) == ""
+    assert ax.coords[1].format_coord(10) == ""
+    fig.savefig(tmpdir.join('nothing').strpath)
+    assert ax.format_coord(10,10) == "11.0 11.0 (world)"
+    assert ax.coords[0].format_coord(10) == "10.0"
+    assert ax.coords[1].format_coord(10) == "10.0"
