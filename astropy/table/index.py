3715645a5ff09c460abf8a7a71f94d043f45f2a6
Michael Mueller <michaeldmueller7@gmail.com>
2015-06-06 23:28:55 -0400

    Wrote a few methods for a BST class

--- astropy/table/index.py
+++ astropy/table/index.py
@@ -1,17 +1,31 @@
-DataStructure = lambda x: None # will change
+from bst import BST
 
 class Index:
+    Implementation = BST
     def __init__(self, data):
-        self.data = DataStructure(data)
+        self.data = self.Implementation(data)
         # nodes of self.data will be (key val, row index)
+
     def insert_row(self, pos, val):
-        pass
+        self.data.add((val, pos))
+
     def remove_rows(self, row_specifier):
-        pass
+        if isinstance(row_specifier, int):
+            self.data.remove(row_specifier)
+        elif isinstance(row_specifier, list): ##TODO: check other iterables
+            for row in row_specifier:
+                self.data.remove(row)
+        else: # must be slice
+            max_row = max([row for val, row in data])
+            for row in row_specifier.indices(max_row):
+                self.data.remove(row)
+
     def find(self, key):
-        pass
+        return self.data.find(key)
+
     def range(self, lower, upper):
-        pass
+        return self.data.range(lower, upper)
+
     def replace(self, pos, val):
         self.remove_rows(pos)
         self.insert_row(pos, val)
