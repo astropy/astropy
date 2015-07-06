8ab36e957aade18d2601f32e4aa066244a1bb5d4
Michael Mueller <michaeldmueller7@gmail.com>
2015-07-05 23:54:15 -0400

    Did more work on SortedArray, using np.searchsorted

---
+++ astropy/table/tests/test_array.py
@@ -0,0 +1,17 @@
+from ..array import SortedArray
+import pytest
+import numpy as np
+
+@pytest.fixture
+def array():
+    # composite index
+    return SortedArray(dict(((x % 2, x), [x]) for x in range(1, 11)))
+
+def test_array_find(array):
+    for i in range(1, 11):
+        print("Searching for {0}".format(i))
+        assert array.find((i % 2, i)) == [i]
+    assert array.find((1, 4)) == []
+
+def test_array_range(array):
+    assert np.all(array.range((0, 8), (1, 3), (True, True)) == [8, 10, 1, 3])
