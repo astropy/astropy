2216a5833ebca08c6fb2d1ab7e98e90bc91f2e8a
Michael Droettboom <mdboom@gmail.com>
2015-07-13 12:40:42 -0400

    Wrap all the projections

--- astropy/modeling/setup_package.py
+++ astropy/modeling/setup_package.py
@@ -24,13 +24,37 @@ GEN_FILES = [join(MODELING_SRC, 'projections.c')]
 # This defines the set of projection functions that we want to wrap.
 # The key is the projection name, and the value is the number of
 # parameters.
+
+# (These are in the order that the appear in the WCS coordinate
+# systems paper).
 projections = {
     'azp': 2,
+    'szp': 3,
     'tan': 0,
     'stg': 0,
+    'sin': 2,
+    'arc': 0,
+    'zpn': 20,
+    'zea': 0,
+    'air': 1,
     'cyp': 2,
     'cea': 1,
-    'mer': 0
+    'mer': 0,
+    'sfl': 0,
+    'par': 0,
+    'mol': 0,
+    'ait': 0,
+    'cop': 2,
+    'coe': 2,
+    'cod': 2,
+    'coo': 2,
+    'bon': 1,
+    'pco': 0,
+    'tsc': 0,
+    'csc': 0,
+    'qsc': 0,
+    'hpx': 2,
+    'xph': 0,
 }
 
 
