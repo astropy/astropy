db20525fa1f49983d7f0e54c6ce29a6dd9ab9571
Erik M. Bray <embray@stsci.edu>
2015-07-21 12:32:47 -0400

    Merge pull request #3882 from mwcraig/fix-appveyor
    Fix appveyor builds
    Conflicts:
    	appveyor.yml

--- astropy/io/misc/tests/test_hdf5.py
+++ astropy/io/misc/tests/test_hdf5.py
@@ -120,7 +120,6 @@ def test_read_write_simple(tmpdir):
 
 
 @pytest.mark.skipif('not HAS_H5PY')
-@pytest.mark.skipif(os.environ.get('APPVEYOR'), reason="fails on AppVeyor")
 def test_read_write_existing_table(tmpdir):
     test_file = str(tmpdir.join('test.hdf5'))
     t1 = Table()
@@ -193,7 +192,6 @@ def test_read_write_existing_append_groups(tmpdir):
 
 
 @pytest.mark.skipif('not HAS_H5PY')
-@pytest.mark.skipif(os.environ.get('APPVEYOR'), reason="fails on AppVeyor")
 def test_read_write_existing_append_overwrite(tmpdir):
     test_file = str(tmpdir.join('test.hdf5'))
     t1 = Table()
