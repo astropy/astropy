c2343270a5f8e8eb3f8b850d518646a848aac4ed
Matej Stuchlik <mstuchli@redhat.com>
2015-08-02 14:21:46 +0200

    [Box1D] Add sample plot

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -1189,6 +1189,28 @@ class Box1D(Fittable1DModel):
                        0 & : \\textnormal{else}
                      \\end{array}
                    \\right.
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        from astropy.modeling.models import Box1D
+
+        plt.figure()
+        s1 = Box1D()
+        r = np.arange(-5, 5, .01)
+
+        for factor in range(1, 4):
+            s1.amplitude = factor
+            s1.width = factor
+            plt.plot(r, s1(r), color=str(0.25 * factor), lw=2)
+
+        plt.axis([-5, 5, -1, 4])
+        plt.show()
     """
 
     amplitude = Parameter(default=1)
