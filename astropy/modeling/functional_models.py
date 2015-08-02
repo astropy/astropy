397f2801e23d9c33cdde54ddf76c34cec18cc005
Matej Stuchlik <mstuchli@redhat.com>
2015-08-02 14:22:01 +0200

    [Trapezoid1D] Add sample plot

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -1307,6 +1307,28 @@ class Trapezoid1D(Fittable1DModel):
     See Also
     --------
     Box1D, Gaussian1D, Moffat1D
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        from astropy.modeling.models import Trapezoid1D
+
+        plt.figure()
+        s1 = Trapezoid1D()
+        r = np.arange(-5, 5, .01)
+
+        for factor in range(1, 4):
+            s1.amplitude = factor
+            s1.width = factor
+            plt.plot(r, s1(r), color=str(0.25 * factor), lw=2)
+
+        plt.axis([-5, 5, -1, 4])
+        plt.show()
     """
 
     amplitude = Parameter(default=1)
