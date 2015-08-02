4dc98bc4ca748eefccdc7a0321f16aaf1789aa71
Matej Stuchlik <mstuchli@redhat.com>
2015-08-02 14:21:31 +0200

    [Const1D] Add sample plot

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -852,6 +852,27 @@ class Const1D(Fittable1DModel):
     Model formula:
 
         .. math:: f(x) = A
+
+    Examples
+    --------
+    .. plot::
+        :include-source:
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        from astropy.modeling.models import Const1D
+
+        plt.figure()
+        s1 = Const1D()
+        r = np.arange(-5, 5, .01)
+
+        for factor in range(1, 4):
+            s1.amplitude = factor
+            plt.plot(r, s1(r), color=str(0.25 * factor), lw=2)
+
+        plt.axis([-5, 5, -1, 4])
+        plt.show()
     """
 
     amplitude = Parameter(default=1)
