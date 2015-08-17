4f93e1569378caf4ca9ad04aa95850178ca5d1a6
Joseph Jon Booker <joe@neoturbine.net>
2015-08-17 17:30:29 -0400

    Change sorting methodology in jsviewer
    Forces masked values < NaNs < finite values

--- astropy/table/tests/test_jsviewer.py
+++ astropy/table/tests/test_jsviewer.py
@@ -33,10 +33,20 @@ table.dataTable {width: auto !important; margin: 0 !important;}
  </head>
  <body>
   <script>
+var astropy_sort_num = function(a, b) {
+    var a_num = parseFloat(a);
+    var b_num = parseFloat(b);
+
+    if (isNaN(a_num) && isNaN(b_num))
+        return ((a < b) ? -1 : ((a > b) ? 1 : 0));
+    else if (!isNaN(a_num) && !isNaN(b_num))
+        return ((a_num < b_num) ? -1 : ((a_num > b_num) ? 1 : 0));
+    else
+        return isNaN(a_num) ? -1 : 1;
+}
 jQuery.extend( jQuery.fn.dataTableExt.oSort, {
-    "optionalnum-pre": function (a) { var b=parseFloat(a); if (isNaN(b)) return Number.NEGATIVE_INFINITY; else return b;},
-    "optionalnum-asc": function (a,b) { return ((a < b) ? -1 : ((a > b) ? 1 : 0)); },
-    "optionalnum-desc": function (a,b) { return ((a < b) ? 1 : ((a > b) ? -1 : 0)); }
+    "optionalnum-asc": astropy_sort_num,
+    "optionalnum-desc": function (a,b) { return -astropy_sort_num(a, b); }
 });
 
 $(document).ready(function() {
