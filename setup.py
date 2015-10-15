78297894393d60797db1f2c2844d200ce743e361
Erik M. Bray <embray@stsci.edu>
2015-10-15 18:04:29 -0400

    Finishing v1.1b1 release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import astropy
 NAME = 'astropy'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '1.1b1'
+VERSION = '1.1.dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
