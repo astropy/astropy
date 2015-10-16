cd0b3712cdc1ad2c3a2aeef756cdc641c126c16c
Michael Seifert <michaelseifert04@yahoo.de>
2015-10-16 17:46:31 +0200

    Changed according to embrays vision

--- astropy/utils/decorators.py
+++ astropy/utils/decorators.py
@@ -19,7 +19,8 @@ from ..extern import six
 
 
 __all__ = ['deprecated', 'deprecated_attribute', 'classproperty',
-           'lazyproperty', 'sharedmethod', 'wraps', 'add_docstring']
+           'lazyproperty', 'sharedmethod', 'wraps',
+           'replace_and_format_docstring']
 
 
 def deprecated(since, message='', name='', alternative='', pending=False,
@@ -757,47 +758,72 @@ def _get_function_args(func, exclude_args=()):
     return all_args
 
 
-def add_docstring(docstring, replace=True):
+def replace_and_format_docstring(docstring, *args, **kwargs):
     """
-    Useable for replacing a docstring on a function or method or appending
-    to it.
+    Replaces the docstring of the decorated object and then formats it.
 
-    This decorator keeps the functions signature and only alters the docstring.
-    The primary use-case would be if multiple functions have the same or only
+    This decorator keeps the objects signature and only alters the saved
+    ``__doc__`` property (so it cannot be used as class decorator in python2).
+    The formatting works like :func:`str.format` and if the decorated object
+    already has a docstring this docstring can be included in the new
+    documentation if you use the ``{original_doc}`` placeholder.
+    It's primary use is if multiple functions have the same or only
     a slightly different *long* docstring and you want to DRY
     (https://de.wikipedia.org/wiki/Don%E2%80%99t_repeat_yourself).
 
     Parameters
     ----------
-    docstring: `str`
-        The docstring that will be appended or set for the function/method
+    docstring: `str` or ``object``
+        The docstring that will replace the docstring of the decorated
+        function/method. If it is an object like a function or class it will
+        take the docstring of this object. If it is a string it will use the
+        string itself. One special case is if the string is ``'self'`` then
+        it will use the decorated functions docstring and format it.
 
-    replace: `bool`
-        If ``True`` the original docstring is replaced and if ``False`` the
-        new docstring is added to the original docstring. Default is ``True``
+    arg:
+        passed to :func:`str.format`.
+
+    kwargs:
+        passed to :func:`str.format`. If the function has a (not empty)
+        docstring the original docstring is added to the kwargs with the
+        keyword ``original_doc``.
+
+    Raises
+    ------
+    ValueError:
+        If the interpreted (if it was ``'self'`` or not a string) ``docstring``
+        is empty.
+
+    IndexError, KeyError:
+        If a placeholder in the interpreted ``docstring`` was not filled. see
+        :meth:`str.format` for more information.
 
     Notes
     -----
-    Using this decorator allows Sphinx and the spyder object inspector load the
-    new docstring.
+    Using this decorator allows, for example Sphinx, to parse the
+    correct docstring.
 
-    For example::
+    There might be problems with certain objects which have a nor-writable
+    ``__doc__`` attribute. Like class objects before python 3.
 
-        >>> from astropy.utils.decorators import add_docstring
-        >>> doc = '''Perform num1 {op} num2'''
-        >>> @add_docstring(doc.format(op='+'))
+    Replacing a current docstring is very easy::
+
+        >>> from astropy.utils.decorators import replace_and_format_docstring
+        >>> doc = '''Perform num1 + num2'''
+        >>> @replace_and_format_docstring(doc)
         ... def add(num1, num2):
         ...     return num1+num2
         ...
         >>> help(add) # doctest: +SKIP
         Help on function add in module __main__:
+        <BLANKLINE>
         add(num1, num2)
             Perform num1 + num2
 
-    sometimes only appending some common docstring is required. In that case
-    setting ``replace`` to ``False`` will only append the docstring.
+    sometimes instead of replacing you only want to add to it::
 
         >>> doc = '''
+        ...       {original_doc}
         ...       Parameters
         ...       ----------
         ...       num1, num2 : Numbers
@@ -805,26 +831,154 @@ def add_docstring(docstring, replace=True):
         ...       -------
         ...       result: Number
         ...       '''
-        >>> @add_docstring(doc, replace=False)
+        >>> @replace_and_format_docstring(doc)
         ... def add(num1, num2):
         ...     '''Perform addition.'''
         ...     return num1+num2
         ...
         >>> help(add) # doctest: +SKIP
         Help on function add in module __main__:
+        <BLANKLINE>
         add(num1, num2)
-            Perform addition
+            Perform addition.
             Parameters
             ----------
             num1, num2 : Numbers
             Returns
             -------
             result: Number
+
+    in case one might want to format it further::
+
+        >>> doc = '''
+        ...       Perform {0}.
+        ...       Parameters
+        ...       ----------
+        ...       num1, num2 : Numbers
+        ...       Returns
+        ...       -------
+        ...       result: Number
+        ...           result of num1 {op} num2
+        ...       {original_doc}
+        ...       '''
+        >>> @replace_and_format_docstring(doc, 'addition', op='+')
+        ... def add(num1, num2):
+        ...     return num1+num2
+        ...
+        >>> @replace_and_format_docstring(doc, 'subtraction', op='-')
+        ... def subtract(num1, num2):
+        ...     '''Notes: This one has additional notes.'''
+        ...     return num1-num2
+        ...
+        >>> help(add) # doctest: +SKIP
+        Help on function add in module __main__:
+        <BLANKLINE>
+        add(num1, num2)
+            Perform addition.
+            Parameters
+            ----------
+            num1, num2 : Numbers
+            Returns
+            -------
+            result: Number
+                result of num1 + num2
+        >>> help(subtract) # doctest: +SKIP
+        Help on function subtract in module __main__:
+        <BLANKLINE>
+        subtract(num1, num2)
+            Perform subtraction.
+            Parameters
+            ----------
+            num1, num2 : Numbers
+            Returns
+            -------
+            result: Number
+                result of num1 - num2
+            Notes: This one has additional notes.
+
+    These methods can be combined an even taking the docstring from another
+    object is possible as docstring attribute. You just have to specify the
+    object::
+
+        >>> @replace_and_format_docstring(add)
+        ... def another_add(num1, num2):
+        ...     return num1 + num2
+        ...
+        >>> help(another_add) # doctest: +SKIP
+        Help on function another_add in module __main__:
+        <BLANKLINE>
+        another_add(num1, num2)
+            Perform addition.
+            Parameters
+            ----------
+            num1, num2 : Numbers
+            Returns
+            -------
+            result: Number
+                result of num1 + num2
+
+    But be aware that this decorator *only* formats the given docstring not
+    the docstring that is formulated inside the function itself::
+
+        >>> @replace_and_format_docstring(doc, 'addition', op='+')
+        ... def yet_another_add(num1, num2):
+        ...    '''This one is good for {0}.'''
+        ...    return num1 + num2
+        ...
+        >>> help(yet_another_add) # doctest: +SKIP
+        Help on function yet_another_add in module __main__:
+        <BLANKLINE>
+        yet_another_add(num1, num2)
+            Perform addition.
+            Parameters
+            ----------
+            num1, num2 : Numbers
+            Returns
+            -------
+            result: Number
+                result of num1 + num2
+            This one is good for {0}.
+
+    To work around it you could specify the docstring to be ``'self'``
+    but then the ``args`` and ``kwargs`` don't get formatted::
+
+        >>> @replace_and_format_docstring('self', 'addition')
+        ... def last_add_i_swear(num1, num2):
+        ...    '''This one is good for {0}.'''
+        ...    return num1 + num2
+        ...
+        >>> help(last_add_i_swear) # doctest: +SKIP
+        Help on function last_add_i_swear in module __main__:
+        <BLANKLINE>
+        last_add_i_swear(num1, num2)
+            This one is good for addition.
+
+    Using it with ``'self'`` as docstring allows to use the decorator twice on
+    an object to first parse the new docstring and then to parse the original
+    docstring. But since this decorator performs string operations using it
+    too often might affect the performance.
     """
     def set_docstring(func):
-        if replace:
-            func.__doc__ = docstring
+        # Not a string so assume we want the saved doc from the object
+        if not isinstance(docstring, str):
+            doc = docstring.__doc__
+        elif docstring != 'self':
+            doc = docstring
         else:
-            func.__doc__ = func.__doc__ + docstring
+            doc = func.__doc__
+            # Delete documentation in this case so we don't end up with
+            # awkwardly self-inserted docs.
+            func.__doc__ = None
+
+        if not doc:
+            # I guess there is nothing which does contain no doc but better be
+            # prepared.
+            raise ValueError('docstring must be a string or containing a'
+                             'docstring that is not empty.')
+
+        # If the original has a not-empty docstring append it to the format
+        # kwargs.
+        kwargs['original_doc'] = func.__doc__ or ''
+        func.__doc__ = doc.format(*args, **kwargs)
         return func
     return set_docstring
--- astropy/utils/tests/test_decorators.py
+++ astropy/utils/tests/test_decorators.py
@@ -7,7 +7,8 @@ import inspect
 import pickle
 
 from ..decorators import (deprecated_attribute, deprecated, wraps,
-                          sharedmethod, classproperty, add_docstring)
+                          sharedmethod, classproperty,
+                          replace_and_format_docstring)
 from ..exceptions import AstropyDeprecationWarning
 from ...extern import six
 from ...tests.helper import pytest, catch_warnings
@@ -367,79 +368,189 @@ def test_classproperty_docstring():
     assert B.__dict__['foo'].__doc__ == "The foo."
 
 
-def test_add_docstring_function():
+def test_replace_and_format_docstring_stringInput_simple():
+    # Simple tests with string input
+
+    docstring_fail = ''
+
+    # Raises an valueerror if input is empty
+    with pytest.raises(ValueError):
+        @replace_and_format_docstring(docstring_fail)
+        def testfunc_fail():
+            pass
+
     docstring = 'test'
 
-    @add_docstring(docstring)
-    def testfunc1():
+    # A first test that replaces an empty docstring
+    @replace_and_format_docstring(docstring)
+    def testfunc_1():
         pass
+    assert inspect.getdoc(testfunc_1) == docstring
 
-    @add_docstring(docstring)
-    def testfunc2():
-        """this is a """
+    # Test that it replaces an existing docstring
+    @replace_and_format_docstring(docstring)
+    def testfunc_2():
+        '''not test'''
         pass
-
-    @add_docstring(docstring, replace=False)
-    def testfunc3():
-        """this is a """
-        pass
-
-    assert inspect.getdoc(testfunc1) == docstring
-    assert inspect.getdoc(testfunc2) == docstring
-    assert inspect.getdoc(testfunc3) == 'this is a test'
+    assert inspect.getdoc(testfunc_2) == docstring
 
 
-def test_add_docstring_method():
-    docstring = 'test'
+def test_replace_and_format_docstring_stringInput_format():
+    # Tests with string input and formatting
 
-    class TestClass(object):
-        @add_docstring(docstring)
+    docstring = 'yes {0} no {opt}'
+
+    # Raises an indexerror if not given the formatted args and kwargs
+    with pytest.raises(IndexError):
+        @replace_and_format_docstring(docstring)
         def testfunc1():
             pass
 
-        @add_docstring(docstring)
-        def testfunc2():
-            """this is a """
+    # Test that the formatting is done right
+    @replace_and_format_docstring(docstring, '/', opt='= life')
+    def testfunc2():
+        pass
+    assert inspect.getdoc(testfunc2) == 'yes / no = life'
+
+    # Test that we can include the original docstring
+
+    docstring2 = 'yes {0} no {original_doc}'
+
+    @replace_and_format_docstring(docstring2, '/')
+    def testfunc3():
+        '''= 2 / 2 * life'''
+        pass
+    assert inspect.getdoc(testfunc3) == 'yes / no = 2 / 2 * life'
+
+
+def test_replace_and_format_docstring_objectInput_simple():
+    # Simple tests with object input
+
+    def docstring_fail():
+        pass
+
+    # Self input while the function has no docstring raises an error
+    with pytest.raises(ValueError):
+        @replace_and_format_docstring(docstring_fail)
+        def testfunc_fail():
             pass
 
-        @add_docstring(docstring, replace=False)
-        def testfunc3():
-            """this is a """
+    def docstring0():
+        '''test'''
+        pass
+
+    # A first test that replaces an empty docstring
+    @replace_and_format_docstring(docstring0)
+    def testfunc_1():
+        pass
+    assert inspect.getdoc(testfunc_1) == inspect.getdoc(docstring0)
+
+    # Test that it replaces an existing docstring
+    @replace_and_format_docstring(docstring0)
+    def testfunc_2():
+        '''not test'''
+        pass
+    assert inspect.getdoc(testfunc_2) == inspect.getdoc(docstring0)
+
+
+def test_replace_and_format_docstring_objectInput_format():
+    # Tests with object input and formatting
+
+    def docstring():
+        '''test {0} test {opt}'''
+        pass
+
+    # Raises an indexerror if not given the formatted args and kwargs
+    with pytest.raises(IndexError):
+        @replace_and_format_docstring(docstring)
+        def testfunc_fail():
             pass
 
-    assert inspect.getdoc(TestClass.testfunc1) == docstring
-    assert inspect.getdoc(TestClass.testfunc2) == docstring
-    assert inspect.getdoc(TestClass.testfunc3) == 'this is a test'
-    instance = TestClass()
-    assert inspect.getdoc(instance.testfunc1) == docstring
-    assert inspect.getdoc(instance.testfunc2) == docstring
-    assert inspect.getdoc(instance.testfunc3) == 'this is a test'
+    # Test that the formatting is done right
+    @replace_and_format_docstring(docstring, '+', opt='= 2 * test')
+    def testfunc2():
+        pass
+    assert inspect.getdoc(testfunc2) == 'test + test = 2 * test'
+
+    # Test that we can include the original docstring
+
+    def docstring2():
+        '''test {0} test {original_doc}'''
+        pass
+
+    @replace_and_format_docstring(docstring2, '+')
+    def testfunc3():
+        '''= 4 / 2 * test'''
+        pass
+    assert inspect.getdoc(testfunc3) == 'test + test = 4 / 2 * test'
+
+
+def test_replace_and_format_docstring_selfInput_simple():
+    # Simple tests with self input
+
+    # Self input while the function has no docstring raises an error
+    with pytest.raises(ValueError):
+        @replace_and_format_docstring('self')
+        def testfunc_fail():
+            pass
+
+    # Test that it keeps an existing docstring
+    @replace_and_format_docstring('self')
+    def testfunc_1():
+        '''not test'''
+        pass
+    assert inspect.getdoc(testfunc_1) == 'not test'
+
+
+def test_replace_and_format_docstring_selfInput_format():
+    # Tests with string input which is 'self' (special case) and formatting
+
+    # Raises an indexerror if not given the formatted args and kwargs
+    with pytest.raises(IndexError):
+        @replace_and_format_docstring('self')
+        def testfunc_fail():
+            '''dum {0} dum {opt}'''
+            pass
+
+    # Test that the formatting is done right
+    @replace_and_format_docstring('self', 'di', opt='da dum')
+    def testfunc1():
+        '''dum {0} dum {opt}'''
+        pass
+    assert inspect.getdoc(testfunc1) == 'dum di dum da dum'
+
+    # Test that we cannot recursivly insert the original documentation
+
+    @replace_and_format_docstring('self', 'di')
+    def testfunc2():
+        '''dum {0} dum {original_doc}'''
+        pass
+    assert inspect.getdoc(testfunc2) == 'dum di dum '
+
+
+def test_replace_and_format_docstring_onMethod():
+    # Check if the decorator works on methods too, to spice it up we try double
+    # decorator
+    docstring = 'what we do {original_doc}'
+
+    class TestClass(object):
+        @replace_and_format_docstring(docstring)
+        @replace_and_format_docstring('self', 'strange.')
+        def test_method(self):
+            '''is {0}'''
+            pass
+
+    assert inspect.getdoc(TestClass.test_method) == 'what we do is strange.'
 
 
 @pytest.mark.skipif('six.PY2')
-def test_add_docstring_class():
-    docstring = 'test'
+def test_replace_and_format_docstring_onClass():
+    # Check if the decorator works on classes too
+    docstring = 'what we do {original_doc} {0}{opt}'
 
-    @add_docstring(docstring)
-    class TestClass1(object):
+    @replace_and_format_docstring(docstring, 'strange', opt='.')
+    class TestClass(object):
+        '''is'''
         pass
 
-    @add_docstring(docstring)
-    class TestClass2(object):
-        """this is a """
-        pass
-
-    @add_docstring(docstring, replace=False)
-    class TestClass3(object):
-        """this is a """
-        pass
-
-    assert inspect.getdoc(TestClass1) == docstring
-    assert inspect.getdoc(TestClass2) == docstring
-    assert inspect.getdoc(TestClass3) == 'this is a test'
-    instance1 = TestClass1()
-    instance2 = TestClass2()
-    instance3 = TestClass3()
-    assert inspect.getdoc(instance1) == docstring
-    assert inspect.getdoc(instance2) == docstring
-    assert inspect.getdoc(instance3) == 'this is a test'
+    assert inspect.getdoc(TestClass) == 'what we do is strange.'
