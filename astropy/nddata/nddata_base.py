718e11dfb09c8f842304f17e6cd46aa6319cf821
Michael Seifert <michaelseifert04@yahoo.de>
2015-10-25 20:29:58 +0100

    Initial refactor of NDData and NDDataBase

--- astropy/nddata/nddata_base.py
+++ astropy/nddata/nddata_base.py
@@ -14,79 +14,66 @@ __all__ = ['NDDataBase']
 @six.add_metaclass(ABCMeta)
 class NDDataBase(object):
     """
-    Base metaclass that defines the interface for NDData
+    Base metaclass that defines the interface for N-dimensional datasets with
+    associated meta informations used in ``astropy``.
 
-    Classes that wish to use this interface without inheriting from
-    `~astropy.nddata.NDData` should subclass ``NDDataBase`` instead.
-
-    All properties and methods except uncertainty must be override by derived
-    classes.
+    NDDataBase makes no attempt of restricting any attribute or defines how
+    these are stored. Therefore all properties and methods have to be
+    overridden in subclasses. See `~astropy.nddata.NDData` for a subclass that
+    defines this interface on `~numpy.ndarray`-like based ``data``.
     """
 
     @abstractmethod
     def __init__(self):
-        self._uncertainty = None
+        pass
 
     @abstractproperty
     def data(self):
         """
-        The data; should be capable of behaving like a numpy array, though it
-        need not actually be a numpy array.
+        The stored dataset.
         """
         pass
 
     @abstractproperty
     def mask(self):
         """
-        Mask for the data, following the numpy convention that ``True`` means
-        the data should not be used.
+        Mask for the dataset.
+
+        Masks should follow the ``numpy`` convention that valid data points are
+        marked by ``False`` and invalid ones with ``True``.
         """
         return None
 
     @abstractproperty
     def unit(self):
         """
-        Unit for the data, if any.
+        Unit for the dataset.
         """
         return None
 
     @abstractproperty
     def wcs(self):
         """
-        WCS for the data, if any.
+        A world coordinate system (WCS) for the dataset.
         """
         return None
 
     @abstractproperty
     def meta(self):
         """
-        Metadata, if any, must be dict-like.
+        Meta information about the dataset.
+
+        Should be `dict`-like.
         """
         return None
 
-    # uncertainty and its setter are implemented as concrete to enforce the
-    # logic in the uncertainty setter. For a long discussion of the problems
-    # with trying to implement them as abstract (particularly the setter but
-    # not the getter), see http://bugs.python.org/issue11610
-    #
-    # In python >= 3.3 it would be easy to decorate one of these (setter or
-    # getter) as abstract but not the other.
-    @property
+    @abstractproperty
     def uncertainty(self):
         """
-        Uncertainty in the data.
+        Uncertainty in the dataset.
 
-        Uncertainty must have an attribute ``uncertainty_type`` that is
-        a string.
+        Should have an attribute ``uncertainty_type`` that defines what kind of
+        uncertainty is stored, such as ``'std'`` for standard deviation or
+        ``'var'`` for variance.
         """
-        return self._uncertainty
-
-    @uncertainty.setter
-    def uncertainty(self, value):
-        if value is not None:
-            if (not hasattr(value, 'uncertainty_type') or
-                    not isinstance(value.uncertainty_type, six.string_types)):
-
-                raise TypeError('Uncertainty must have attribute '
-                                'uncertainty_type whose type is string.')
-        self._uncertainty = value
+        return None
