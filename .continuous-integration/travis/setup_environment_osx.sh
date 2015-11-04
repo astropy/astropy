b91e1ecec660885cba294da063e1f4cf40476295
Matt Craig <mattwcraig@gmail.com>
2015-11-04 13:17:19 -0600

    Merge pull request #4291 from cdeil/conda-travis-fix
    Fix conda install on travis-ci

--- .continuous-integration/travis/setup_environment_osx.sh
+++ .continuous-integration/travis/setup_environment_osx.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Install conda
+# http://conda.pydata.org/docs/travis.html#the-travis-yml-file
 wget http://repo.continuum.io/miniconda/Miniconda-3.7.3-MacOSX-x86_64.sh -O miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b
-export PATH=/Users/travis/miniconda/bin:$PATH
-conda update --yes conda
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
 
 # Install Python dependencies
 source "$( dirname "${BASH_SOURCE[0]}" )"/setup_dependencies_common.sh
