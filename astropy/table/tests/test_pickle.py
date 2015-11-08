5f1aebcb7b58cdacb53145e742009e0433afc1cc
Tom Aldcroft <taldcroft@gmail.com>
2015-11-08 15:21:56 -0500

    Copy mixin cols before pickling to remove parent_table ref

--- astropy/table/tests/test_pickle.py
+++ astropy/table/tests/test_pickle.py
@@ -54,39 +54,32 @@ def test_pickle_table(protocol):
     a = Column(data=[1, 2], name='a', format='%05d', description='col a', unit='cm', meta={'a': 1})
     b = Column(data=[3.0, 4.0], name='b', format='%05d', description='col b', unit='cm',
                meta={'b': 1})
-    t = Table([a, b], meta={'a': 1})
-    ts = pickle.dumps(t)
-    tp = pickle.loads(ts)
 
-    assert np.all(tp['a'] == t['a'])
-    assert np.all(tp['b'] == t['b'])
-    assert tp['a'].attrs_equal(t['a'])
-    assert tp['b'].attrs_equal(t['b'])
-    assert tp.meta == t.meta
+    for table_class in Table, QTable:
+        t = table_class([a, b], meta={'a': 1, 'b': Quantity(10, unit='s')})
+        t['c'] = Quantity([1, 2], unit='m')
+        t['d'] = Time(['2001-01-02T12:34:56', '2001-02-03T00:01:02'])
+        t['e'] = SkyCoord([125.0,180.0]*deg, [-45.0,36.5]*deg)
 
-def test_pickle_qtable(protocol):
-    a = Column(data=[1, 2], name='a', format='%05d', description='col a', unit='cm', meta={'a': 1})
-    b = Column(data=[3.0, 4.0], name='b', format='%05d', description='col b', unit='cm',
-               meta={'b': 1})
-    t = QTable([a, b], meta={'a': 1, 'b':Quantity(10,unit='s')})
-    t['c'] = Quantity([1, 2], unit='m')
-    t['d'] = Time(['2001-01-02T12:34:56', '2001-02-03T00:01:02'])
-    t['e'] = SkyCoord([125.0,180.0]*deg, [-45.0,36.5]*deg)
-    ts = pickle.dumps(t)
-    tp = pickle.loads(ts)
+        ts = pickle.dumps(t)
+        tp = pickle.loads(ts)
 
-    assert np.all(tp['a'] == t['a'])
-    assert np.all(tp['b'] == t['b'])
-    # test mixin columns
-    assert np.all(tp['c'] == t['c'])
-    assert np.all(tp['d'] == t['d'])
-    assert np.all(tp['e'].ra == t['e'].ra)
-    assert np.all(tp['e'].dec == t['e'].dec)
-    assert type(tp['c']) == type(t['c'])
-    assert type(tp['d']) == type(t['d'])
-    assert type(tp['e']) == type(t['e'])
-    assert tp.meta == t.meta
-    assert type(tp) == type(t)
+        assert tp.__class__ is table_class
+        assert np.all(tp['a'] == t['a'])
+        assert np.all(tp['b'] == t['b'])
+
+        # test mixin columns
+        assert np.all(tp['c'] == t['c'])
+        assert np.all(tp['d'] == t['d'])
+        assert np.all(tp['e'].ra == t['e'].ra)
+        assert np.all(tp['e'].dec == t['e'].dec)
+        assert type(tp['c']) == type(t['c'])
+        assert type(tp['d']) == type(t['d'])
+        assert type(tp['e']) == type(t['e'])
+        assert tp.meta == t.meta
+        assert type(tp) == type(t)
+
+        assert isinstance(tp['c'], Quantity if (table_class is QTable) else Column)
 
 def test_pickle_masked_table(protocol):
     a = Column(data=[1, 2], name='a', format='%05d', description='col a', unit='cm', meta={'a': 1})
