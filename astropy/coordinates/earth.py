98c385ac5f5800430f303857ecea743ea26b46ea
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-11-13 18:31:13 +0000

    Merge pull request #4304 from mhvk/earth-pickling-on-python2
    EarthLocation instances can't be pickled on python2

--- astropy/coordinates/earth.py
+++ astropy/coordinates/earth.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
+from __future__ import absolute_import, division, print_function
 
 import numpy as np
 from .. import units as u
