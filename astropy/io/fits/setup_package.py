4f51ef1a113440ae89820f4c2ebf51453e5a3ab1
Joe Hunkeler <jhunk@stsci.edu>
2015-12-14 18:54:31 -0500

    Remove flag no-unused-result

--- astropy/io/fits/setup_package.py
+++ astropy/io/fits/setup_package.py
@@ -39,7 +39,7 @@ def _get_compression_extension():
                     '-Wno-uninitialized', '-Wno-format',
                     '-Wno-strict-prototypes', '-Wno-unused', '-Wno-comments',
                     '-Wno-switch', '-Wno-strict-aliasing', '-Wno-return-type',
-                    '-Wno-address', '-Wno-unused-result'
+                    '-Wno-address'
                 ])
 
         cfitsio_path = os.path.join('cextern', 'cfitsio')
