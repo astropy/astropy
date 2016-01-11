5ea6ab5fefd1d720fbe41d637a53fc96229e02e2
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-11 18:02:57 +0000

    Clean up whitespace

--- astropy/utils/tests/test_odict.py
+++ astropy/utils/tests/test_odict.py
@@ -3,15 +3,14 @@ from ..exceptions import AstropyDeprecationWarning
 
 
 def test_init_warning():
-    
+
     from ..compat.odict import OrderedDict
-    
+
     with catch_warnings() as w:
         OrderedDict()
-    
+
     assert len(w) == 1
     assert w[0].category == AstropyDeprecationWarning
     assert w[0].message.args[0] == ("astropy.utils.compat.odict.OrderedDict is "
                                     "now deprecated - import OrderedDict from "
                                     "the collections module instead")
-    
