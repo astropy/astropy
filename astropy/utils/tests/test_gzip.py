283a1ef5c5bdb55ba2596a3f7fc49b099cdb117e
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-01-11 17:12:17 +0000

    Added test of deprecation warning when importing astropy.utils.compat.gzip

---
+++ astropy/utils/tests/test_gzip.py
@@ -0,0 +1,13 @@
+from ...tests.helper import catch_warnings
+from ..exceptions import AstropyDeprecationWarning
+
+
+def test_import_warning():
+
+    with catch_warnings() as w:
+        from ..compat import gzip
+
+    assert len(w) == 1
+    assert w[0].category == AstropyDeprecationWarning
+    assert w[0].message.args[0] == ("astropy.utils.compat.gzip is now deprecated "
+                                    "- use the gzip module directly instead")
