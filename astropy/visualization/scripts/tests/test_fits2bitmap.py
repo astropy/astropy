620222e9087f67061814e8274336b3314e17d750
Larry Bradley <larry.bradley@gmail.com>
2016-01-11 15:11:53 -0500

    Add fits2bitmap tests for exten name/number

--- astropy/visualization/scripts/tests/test_fits2bitmap.py
+++ astropy/visualization/scripts/tests/test_fits2bitmap.py
@@ -25,3 +25,35 @@ def test_fits2bitmap_script(tmpdir):
     filename = tmpdir.join('test.fits').strpath
     fits.writeto(filename, np.ones((128, 128)))
     main([filename, '-e', '0'])
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_exten_num(tmpdir):
+    filename = tmpdir.join('test.fits').strpath
+    data = np.ones((100, 100))
+    hdu1 = fits.PrimaryHDU()
+    hdu2 = fits.ImageHDU(data)
+    hdulist = fits.HDUList([hdu1, hdu2])
+    hdulist.writeto(filename)
+    main([filename, '-e', '1'])
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+def test_exten_name(tmpdir):
+    filename = tmpdir.join('test.fits').strpath
+    data = np.ones((100, 100))
+    hdu1 = fits.PrimaryHDU()
+    extname = 'SCI'
+    hdu2 = fits.ImageHDU(data)
+    hdu2.header['EXTNAME'] = extname
+    hdulist = fits.HDUList([hdu1, hdu2])
+    hdulist.writeto(filename)
+    main([filename, '-e', extname])
+
+
+@pytest.mark.skipif('not HAS_MATPLOTLIB')
+@pytest.mark.parametrize('file_exten', ['.gz', '.bz2'])
+def test_compressed_fits(tmpdir, file_exten):
+    filename = tmpdir.join('test.fits' + file_exten).strpath
+    fits.writeto(filename, np.ones((128, 128)))
+    main([filename, '-e', '0'])
