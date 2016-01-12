5c9329418a9f399839d5c0813645460aaf1ada6a
Anchit Jain <anchitjain1234@gmail.com>
2016-01-12 13:01:15 +0530

    Tabs and spaces fix

--- astropy/io/ascii/cds.py
+++ astropy/io/ascii/cds.py
@@ -123,13 +123,13 @@ class CdsHeader(core.BaseHeader):
                 if col.unit == '---':
                     col.unit = None  # "---" is the marker for no unit in CDS table
                 if len(line) > match.end():
-		    descr_match = re_descr.match(line[match.end():])
-		    if descr_match:
-			col.description = descr_match.group('descr').strip()
-		    else:
-			col.description = ""
-		else:
-		    col.description = ""
+                    descr_match = re_descr.match(line[match.end():])
+                    if descr_match:
+                        col.description = descr_match.group('descr').strip()
+                    else:
+                        col.description = ""
+                else:
+                    col.description = ""
                 col.raw_type = match.group('format')
                 col.type = self.get_col_type(col)
 
