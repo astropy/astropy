7a929dc812924394bb41d667ef56ec3aabe84480
Erik M. Bray <embray@stsci.edu>
2016-01-28 10:25:35 -0500

    A modified version of the VendorImporter from setuptools, tweaked a bit to include support for a minimum version check as well.  This works just as well when the package is imported from a zip file, or in principle any other bizarro importer.  See https://bitbucket.org/pypa/setuptools/raw/7c902993cb620f7a5dc97738065d0430e25df45d/pkg_resources/extern/__init__.py

--- astropy/extern/__init__.py
+++ astropy/extern/__init__.py
@@ -8,3 +8,94 @@ distribution, as that directory only contains C extension code.
 See the README.rst in this directory of the Astropy source repository for more
 details.
 """
+
+import sys
+from distutils.version import StrictVersion as V
+
+
+_VENDORED = [('six', '1.7.3')]
+_SEARCH_PATH = ['astropy.extern.bundled.', '']
+
+
+class VendorImporter(object):
+    """
+    A PEP 302 meta path importer for finding optionally-vendored
+    or otherwise naturally-installed packages from __name__.
+    """
+
+    @staticmethod
+    def _vendored_match(target):
+        """
+        Check if the given module/package name is in the _VENDORED
+        module list, or is a subpackage of a module in the _VENDORED
+        list.
+
+        Returns the vendored module name and, if given, a required version
+        for that module.
+        """
+
+        for v in _VENDORED:
+            if isinstance(v, tuple):
+                modname = v[0]
+            else:
+                modname = v
+
+            if target == modname or target.startswith(modname + '.'):
+                return v
+
+    def find_module(self, fullname, path=None):
+        root, base, target = fullname.partition(__name__ + '.')
+        if root:
+            return
+
+        if not self._vendored_match(target):
+            return
+
+        return self
+
+    def load_module(self, fullname):
+        root, base, target = fullname.partition(__name__ + '.')
+        vendored = self._vendored_match(target)
+        version = None
+        if isinstance(vendored, tuple):
+            vendored, version = vendored
+
+        for prefix in _SEARCH_PATH:
+            try:
+                __import__(prefix + target)
+            except ImportError:
+                continue
+
+            mod = sys.modules[prefix + target]
+
+            if target == vendored and version:
+                # Only for top-level modules
+                try:
+                    if V(mod.__version__) >= V(version):
+                        break
+                except (AttributeError, ValueError):
+                    # Attribute error if the module isn't what it should be and
+                    # doesn't have a .__version__; ValueError if the version
+                    # string exists but is somehow bogus/unparseable
+                    continue
+            else:
+                break
+        else:
+            at_least = 'of minimum version {0} '.format(version)
+            raise ImportError(
+                "The '{target}' package {at_least}is required; "
+                "normally this is bundled with this package so if you get "
+                "this warning, consult the packager of your "
+                "distribution.".format(**locals())
+            )
+
+        sys.modules[fullname] = mod
+        return mod
+
+    @classmethod
+    def install(cls):
+        if not any(isinstance(imp, cls) for imp in sys.meta_path):
+            sys.meta_path.append(cls())
+
+
+VendorImporter.install()
