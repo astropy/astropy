5b39314794dc8b086702e2b9466072d5ee7d027c
Erik Tollerud <erik.tollerud@gmail.com>
2016-01-28 11:21:05 -0500

    remove str and add reasons

--- astropy/coordinates/tests/test_regression.py
+++ astropy/coordinates/tests/test_regression.py
@@ -113,8 +113,8 @@ def test_regression_4033():
     assert_quantity_allclose(alb_wdistaa.separation(deaa), 22.2862*u.deg, rtol=1e-3)
 
 
-@pytest.mark.skipif(str(not HAS_SCIPY))
-@pytest.mark.skipif(str(OLDER_SCIPY))
+@pytest.mark.skipif(not HAS_SCIPY, reason='No Scipy')
+@pytest.mark.skipif(OLDER_SCIPY, reason='Scipy too old')
 def test_regression_4082():
     """
     Issue: https://github.com/astropy/astropy/issues/4082
