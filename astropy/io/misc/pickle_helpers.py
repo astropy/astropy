e4fb6fc4969aa2bd64d724bdccea9b11f3f42c92
Brigitta Sipocz <bsipocz@gmail.com>
2016-02-02 20:12:23 +0000

    Unlinking unnecessary links

--- astropy/io/misc/pickle_helpers.py
+++ astropy/io/misc/pickle_helpers.py
@@ -19,7 +19,7 @@ def fnunpickle(fileorname, number=0, usecPickle=True):
 
     Parameters
     ----------
-    fileorname : str or `file`-like
+    fileorname : str or file-like
         The file name or file from which to unpickle objects. If a file object,
         it should have been opened in binary mode.
     number : int
@@ -87,7 +87,7 @@ def fnpickle(object, fileorname, usecPickle=True, protocol=None, append=False):
     ----------
     object
         The python object to pickle.
-    fileorname : str or `file`-like
+    fileorname : str or file-like
         The filename or file into which the `object` should be pickled. If a
         file object, it should have been opened in binary mode.
     usecPickle : bool
