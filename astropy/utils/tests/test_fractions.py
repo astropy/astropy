c9f1cd190605e81f2e1ccab43fd247bee342abf1
Erik Tollerud <erik.tollerud@gmail.com>
2016-02-12 17:10:12 -0500

    make on-import warning tests pass even if done twice in the same session

--- astropy/utils/tests/test_fractions.py
+++ astropy/utils/tests/test_fractions.py
@@ -1,11 +1,19 @@
 from ...tests.helper import catch_warnings
 from ..exceptions import AstropyDeprecationWarning
+from ...extern.six.moves import reload_module
 
 
 def test_import_warning():
+    # this import *maybe* raises the warning too, but we force the matter below
+    from ..compat import fractions  # pylint: disable=W0611
 
     with catch_warnings() as w:
-        from ..compat import fractions  # pylint: disable=W0611
+        # this ensures the warning will be re-issues at reload.  Without this,
+        # the warning is missing if you invoke astropy.test twice in the same
+        # python session
+        if hasattr(fractions, '__warningregistry__'):
+            fractions.__warningregistry__.clear()
+        reload_module(fractions)
 
     assert len(w) == 1
     assert w[0].category == AstropyDeprecationWarning
