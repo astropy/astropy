c3653b8e6a17cc4bc5ec69920309b73825baa4ad
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-19 13:45:43 +0000

    Updated comments, added a test for more complex equivalencies that require information about input values, and include an example of fitting a spectrum in Jy to data in K and in erg/cm^2/s/micron.

--- astropy/modeling/tests/test_quantities_fitting.py
+++ astropy/modeling/tests/test_quantities_fitting.py
@@ -10,6 +10,11 @@ from __future__ import (absolute_import, unicode_literals, division,
 from ...utils import NumpyRNGContext
 
 
+# Fitting should be as intuitive as possible to the user. Essentially, models
+# and fitting should work without units, but if one has units, the other should
+# have units too, and the resulting fitted parameters will also have units.
+
+
 @pytest.mark.xfail
 def test_fitting_simple():
 
@@ -28,7 +33,8 @@ def test_fitting_simple():
     fit_g = fitting.LevMarLSQFitter()
     g = fit_g(g_init, x, y)
 
-    # TODO: update actual numerical results, but these should be close
+    # TODO: update actual numerical results once implemented, but these should
+    # be close to the values below.
     assert_quantity_allclose(g.amplitude, 3 * u.Jy)
     assert_quantity_allclose(g.mean, 1.3 * u.m)
     assert_quantity_allclose(g.stddev, 0.8 * u.m)
@@ -59,7 +65,7 @@ def test_fitting_missing_model_units():
     Raise an error if the data has units but the model doesn't.
     """
 
-    # TODO: determine whether this breaks backward-compatibility
+    # TODO: determine whether this breaks backward-compatibility.
 
     g_init = models.Gaussian1D(amplitude=1., mean=3, stddev=2)
     fit_g = fitting.LevMarLSQFitter()
@@ -99,13 +105,32 @@ def test_fitting_incompatible_units():
 @pytest.mark.xfail
 def test_fitting_with_equivalencies():
     """
-    Raise an error if the data and model have incompatible units
+    Check that equivalencies are correctly taken into account in the fitting.
     """
+
+    # A simple test with the spectral equivalency
 
     g_init = models.Gaussian1D(amplitude=1. * u.Jy, mean=3 * u.m, stddev=2 * u.cm)
     g_init.input_equivalencies = u.spectral()
 
     fit_g = fitting.LevMarLSQFitter()
+
     g = fit_g(g_init, [1, 2, 3] * u.Hz, [4, 5, 6] * u.Jy)
 
-    # TODO: check value of result
+    # TODO: numerical test of results
+
+    # A more complex test with an equivalency that depends on x values
+    g_init.output_equivalencies = u.brightness_temperature, (3e-5 * u.sr, 'x')
+
+    # Fit to data in Kelvin
+    g = fit_g(g_init, [1, 2, 3] * u.Hz, [4, 5, 6] * u.K)
+
+    # TODO: numerical test of results
+
+    # And we can also try with the spectral density equivalency
+    g_init.output_equivalencies = u.spectral_density, ('x',)
+
+    # Fit to data in Flambda = erg / cm^2 / s / micron
+    g = fit_g(g_init, [1, 2, 3] * u.Hz, [4, 5, 6] * u.erg / u.cm ** 2 / u.s / u.micron)
+
+    # TODO: numerical test of results
