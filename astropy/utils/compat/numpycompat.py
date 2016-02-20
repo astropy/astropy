9e28b7d50cb13b815cc31312e7799e2aa9c36070
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-02-20 08:46:33 +0000

    Unmark test_dot_func as xfail for Numpy 1.10 and later

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -10,7 +10,7 @@ from ...utils import minversion
 
 
 __all__ = ['NUMPY_LT_1_6_1', 'NUMPY_LT_1_7', 'NUMPY_LT_1_8', 'NUMPY_LT_1_9',
-           'NUMPY_LT_1_9_1']
+           'NUMPY_LT_1_9_1', 'NUMPY_LT_1_10', 'NUMPY_LT_1_11']
 
 # TODO: It might also be nice to have aliases to these named for specific
 # features/bugs we're checking for (ex:
@@ -21,6 +21,7 @@ NUMPY_LT_1_8 = not minversion('numpy', '1.8.0')
 NUMPY_LT_1_9 = not minversion('numpy', '1.9.0')
 NUMPY_LT_1_9_1 = not minversion('numpy', '1.9.1')
 NUMPY_LT_1_10 = not minversion('numpy', '1.10.0')
+NUMPY_LT_1_11 = not minversion('numpy', '1.11.0')
 
 
 def _monkeypatch_unicode_mask_fill_values():
