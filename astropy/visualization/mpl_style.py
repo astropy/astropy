da05ac396961cf116ab443d045a94a97dfadf2c8
stbnps <stbnps@users.noreply.github.com>
2016-03-04 01:01:25 +0100

    less redundant code

--- astropy/visualization/mpl_style.py
+++ astropy/visualization/mpl_style.py
@@ -38,136 +38,81 @@ except:
     HAS_MATPLOTLIB = False
 
 
+astropy_mpl_style_1 = {
+
+    # Lines
+    'lines.linewidth': 1.7,
+    'lines.antialiased': True,
+
+    # Patches
+    'patch.linewidth': 1.0,
+    'patch.facecolor': '#348ABD',
+    'patch.edgecolor': '#CCCCCC',
+    'patch.antialiased': True,
+
+    # images
+    'image.cmap': 'gist_heat',
+    'image.origin': 'upper',
+
+    # Font
+    'font.size': 12.0,
+
+    # Axes
+    'axes.facecolor': '#FFFFFF',
+    'axes.edgecolor': '#AAAAAA',
+    'axes.linewidth': 1.0,
+    'axes.grid': True,
+    'axes.titlesize': 'x-large',
+    'axes.labelsize': 'large',
+    'axes.labelcolor': 'k',
+    'axes.axisbelow': True,
+
+    # Ticks
+    'xtick.major.size': 0,
+    'xtick.minor.size': 0,
+    'xtick.major.pad': 6,
+    'xtick.minor.pad': 6,
+    'xtick.color': '#565656',
+    'xtick.direction': 'in',
+    'ytick.major.size': 0,
+    'ytick.minor.size': 0,
+    'ytick.major.pad': 6,
+    'ytick.minor.pad': 6,
+    'ytick.color': '#565656',
+    'ytick.direction': 'in',
+
+    # Legend
+    'legend.fancybox': True,
+    'legend.loc': 'best',
+
+    # Figure
+    'figure.figsize': [8, 6],
+    'figure.facecolor': '1.0',
+    'figure.edgecolor': '0.50',
+    'figure.subplot.hspace': 0.5,
+
+    # Other
+    'savefig.dpi': 72,
+}
+
 if HAS_MATPLOTLIB and matplotlib.__version__ >= '1.5':
     from cycler import cycler
-
-    astropy_mpl_style_1 = {
-
-        # Lines
-        'lines.linewidth': 1.7,
-        'lines.antialiased': True,
-
-        # Patches
-        'patch.linewidth': 1.0,
-        'patch.facecolor': '#348ABD',
-        'patch.edgecolor': '#CCCCCC',
-        'patch.antialiased': True,
-
-        # images
-        'image.cmap': 'gist_heat',
-        'image.origin': 'upper',
-
-        # Font
-        'font.size': 12.0,
-
-        # Axes
-        'axes.facecolor': '#FFFFFF',
-        'axes.edgecolor': '#AAAAAA',
-        'axes.linewidth': 1.0,
-        'axes.grid': True,
-        'axes.titlesize': 'x-large',
-        'axes.labelsize': 'large',
-        'axes.labelcolor': 'k',
-        'axes.axisbelow': True,
-        'axes.prop_cycle': cycler('color',['#348ABD',   # blue
+    astropy_mpl_style_1['axes.prop_cycle'] = cycler('color',['#348ABD',   # blue
                              '#7A68A6',   # purple
                              '#A60628',   # red
                              '#467821',   # green
                              '#CF4457',   # pink
                              '#188487',   # turquoise
-                             '#E24A33']),  # orange
-
-        # Ticks
-        'xtick.major.size': 0,
-        'xtick.minor.size': 0,
-        'xtick.major.pad': 6,
-        'xtick.minor.pad': 6,
-        'xtick.color': '#565656',
-        'xtick.direction': 'in',
-        'ytick.major.size': 0,
-        'ytick.minor.size': 0,
-        'ytick.major.pad': 6,
-        'ytick.minor.pad': 6,
-        'ytick.color': '#565656',
-        'ytick.direction': 'in',
-
-        # Legend
-        'legend.fancybox': True,
-        'legend.loc': 'best',
-
-        # Figure
-        'figure.figsize': [8, 6],
-        'figure.facecolor': '1.0',
-        'figure.edgecolor': '0.50',
-        'figure.subplot.hspace': 0.5,
-
-        # Other
-        'savefig.dpi': 72,
-    }
+                             '#E24A33'])  # orange
 else:
-    astropy_mpl_style_1 = {
+    astropy_mpl_style_1['axes.color_cycle'] = ['#348ABD',   # blue
+                     '#7A68A6',   # purple
+                     '#A60628',   # red
+                     '#467821',   # green
+                     '#CF4457',   # pink
+                     '#188487',   # turquoise
+                     '#E24A33']  # orange
 
-        # Lines
-        'lines.linewidth': 1.7,
-        'lines.antialiased': True,
-
-        # Patches
-        'patch.linewidth': 1.0,
-        'patch.facecolor': '#348ABD',
-        'patch.edgecolor': '#CCCCCC',
-        'patch.antialiased': True,
-
-        # images
-        'image.cmap': 'gist_heat',
-        'image.origin': 'upper',
-
-        # Font
-        'font.size': 12.0,
-
-        # Axes
-        'axes.facecolor': '#FFFFFF',
-        'axes.edgecolor': '#AAAAAA',
-        'axes.linewidth': 1.0,
-        'axes.grid': True,
-        'axes.titlesize': 'x-large',
-        'axes.labelsize': 'large',
-        'axes.labelcolor': 'k',
-        'axes.axisbelow': True,
-        'axes.color_cycle': ['#348ABD',   # blue
-                         '#7A68A6',   # purple
-                         '#A60628',   # red
-                         '#467821',   # green
-                         '#CF4457',   # pink
-                         '#188487',   # turquoise
-                         '#E24A33'],  # orange
-
-        # Ticks
-        'xtick.major.size': 0,
-        'xtick.minor.size': 0,
-        'xtick.major.pad': 6,
-        'xtick.minor.pad': 6,
-        'xtick.color': '#565656',
-        'xtick.direction': 'in',
-        'ytick.major.size': 0,
-        'ytick.minor.size': 0,
-        'ytick.major.pad': 6,
-        'ytick.minor.pad': 6,
-        'ytick.color': '#565656',
-        'ytick.direction': 'in',
-
-        # Legend
-        'legend.fancybox': True,
-        'legend.loc': 'best',
-
-        # Figure
-        'figure.figsize': [8, 6],
-        'figure.facecolor': '1.0',
-        'figure.edgecolor': '0.50',
-        'figure.subplot.hspace': 0.5,
-
-        # Other
-        'savefig.dpi': 72,
-    }
 
 '''
 Version 1 astropy plotting style for matplotlib.
