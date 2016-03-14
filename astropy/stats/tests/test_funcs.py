10005b43da4fb2ab0b2328926a98d40098f92d36
Leonardo Ferreira <leonardo.ferreira.furg@gmail.com>
2016-03-14 12:29:28 -0300

    Remove tolerance in test_mad_std_with_axis()

--- astropy/stats/tests/test_funcs.py
+++ astropy/stats/tests/test_funcs.py
@@ -347,17 +347,15 @@ def test_mad_std():
         assert_allclose(funcs.mad_std(data), 2.0, rtol=0.05)
 
 def test_mad_std_with_axis():
-    with NumpyRNGContext(12345):
-        data = np.array([[1, 2, 3, 4],
-                         [4, 3, 2, 1]])
-        
-        #results follow data symmetry 
-        result_axis0 = np.array([ 2.22390333,  0.74130111,  0.74130111,  2.22390333])
-        result_axis1 = np.array([ 1.48260222,  1.48260222])
+    data = np.array([[1, 2, 3, 4],
+                     [4, 3, 2, 1]])
+    
+    #results follow data symmetry 
+    result_axis0 = np.array([2.22390333,  0.74130111,  0.74130111,  2.22390333])
+    result_axis1 = np.array([1.48260222,  1.48260222])
 
-        assert_allclose(funcs.mad_std(data, axis=0), result_axis0, rtol=0.05)  
-        assert_allclose(funcs.mad_std(data, axis=1), result_axis1, rtol=0.05)  
-
+    assert_allclose(funcs.mad_std(data, axis=0), result_axis0)  
+    assert_allclose(funcs.mad_std(data, axis=1), result_axis1) 
 
 
 def test_gaussian_fwhm_to_sigma():
