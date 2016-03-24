87b8fa4e943f8d9b095a5b1ca63eb29a0590b1d7
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-24 09:29:35 -0700

    update matplotlib style and whitespace

--- examples/fits-tables.py
+++ examples/fits-tables.py
@@ -4,7 +4,7 @@
 Fits Table example
 ========================
 
-Demonstrates astropy.utils.data to download the file, astropy.io.fits to open 
+Demonstrates astropy.utils.data to download the file, astropy.io.fits to open
 and view the file, matplotlib for making
 
 """
@@ -15,11 +15,9 @@ import numpy as np
 
 ##############################################################################
 # Set up matplotlib and use a nicer set of plot parameters
-%config InlineBackend.rc = {}
-import matplotlib
-matplotlib.rc_file("../../templates/matplotlibrc")
 import matplotlib.pyplot as plt
-%matplotlib inline
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
 
 ##############################################################################
 # The following line is needed to download the example FITS files used here.
@@ -27,7 +25,8 @@ import matplotlib.pyplot as plt
 from astropy.utils.data import download_file
 from astropy.io import fits
 
-image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits', cache=True )
+image_file = download_file('http://data.astropy.org/tutorials/FITS-images/HorseHead.fits',
+                           cache=True)
 
 ##############################################################################
 # Open the FITS file and find out what it contains.
@@ -36,7 +35,7 @@ hdu_list = fits.open(image_file)
 hdu_list.info()
 
 ##############################################################################
-# Generally the image information is located in the PRIMARY block. 
+# Generally the image information is located in the PRIMARY block.
 # The blocks are numbered and can be accessed by indexing hdu_list.
 
 image_data = hdu_list[0].data
@@ -49,14 +48,13 @@ print(type(image_data))
 print(image_data.shape)
 
 ##############################################################################
-# At this point, the FITS file can be closed since everything needed is now 
+# At this point, the FITS file can be closed since everything needed is now
 # stored in variables.
 
 hdu_list.close()
 
-
 ##############################################################################
-# To simply load the image and not examine the header, use fits.getdata to 
+# To simply load the image and not examine the header, use fits.getdata to
 # bypass the previous steps.
 
 image_data = fits.getdata(image_file)
