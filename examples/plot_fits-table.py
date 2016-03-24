ce7a0262571751050e137e50c3d5139adf650001
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-03-24 10:36:43 -0700

    fix FITS acronym

--- examples/plot_fits-table.py
+++ examples/plot_fits-table.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 ========================
-Fits Table example
+FITS Table example
 ========================
 
 Demonstrates `astropy.utils.data` to download the file, `astropy.io.fits` to open
