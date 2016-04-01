cb32a14f84fc15fe2a0e81fded1734ec9efafc64
Pey Lian Lim <lim@stsci.edu>
2016-04-01 13:30:43 -0400

    Added Mars orbital calculations.

---
+++ astropy/analytic_functions/mars_orbital.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Functions related to orbital calculations for Mars."""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+__all__ = ['plug_my_laptop_in']
+
+
+def plug_my_laptop_in():
+    """Do it like The Martian."""
+    return 'Calculations correct!'
