8a38a9beadefd162ee5e39b06aec7bdf8c9e5069
Evert Rol <evert.rol@gmail.com>
2016-04-13 17:32:52 +1000

    Add function to convert a table to a single HDU

--- astropy/io/fits/connect.py
+++ astropy/io/fits/connect.py
@@ -15,12 +15,10 @@ from ...extern import six
 from ...extern.six import string_types
 from ...table import Table
 from ...utils.exceptions import AstropyUserWarning
-from astropy.units.format.fits import UnitScaleError
-
 from . import HDUList, TableHDU, BinTableHDU, GroupsHDU
-from . import FITS_rec
 from .hdu.hdulist import fitsopen as fits_open
 from .util import first
+from .convenience import table_to_hdu
 
 
 # FITS file signature as per RFC 4047
@@ -209,12 +207,7 @@ def write_table_fits(input, output, overwrite=False):
         Whether to overwrite any existing file without warning.
     """
 
-    # Tables with mixin columns are not supported
-    if input.has_mixin_columns:
-        mixin_names = [name for name, col in input.columns.items()
-                       if not isinstance(col, input.ColumnClass)]
-        raise ValueError('cannot write table with mixin column(s) {0} to FITS'
-                         .format(mixin_names))
+    table_hdu = table_to_hdu(input)
 
     # Check if output file already exists
     if isinstance(output, string_types) and os.path.exists(output):
@@ -223,80 +216,6 @@ def write_table_fits(input, output, overwrite=False):
         else:
             raise IOError("File exists: {0}".format(output))
 
-    # Create a new HDU object
-    if input.masked:
-        #float column's default mask value needs to be Nan
-        for column in six.itervalues(input.columns):
-            fill_value = column.get_fill_value()
-            if column.dtype.kind == 'f' and np.allclose(fill_value, 1e20):
-                column.set_fill_value(np.nan)
-
-        fits_rec = FITS_rec.from_columns(np.array(input.filled()))
-        table_hdu = BinTableHDU(fits_rec)
-        for col in table_hdu.columns:
-            # Binary FITS tables support TNULL *only* for integer data columns
-            # TODO: Determine a schema for handling non-integer masked columns
-            # in FITS (if at all possible)
-            int_formats = ('B', 'I', 'J', 'K')
-            if not (col.format in int_formats or
-                    col.format.p_format in int_formats):
-                continue
-
-            # The astype is necessary because if the string column is less
-            # than one character, the fill value will be N/A by default which
-            # is too long, and so no values will get masked.
-            fill_value = input[col.name].get_fill_value()
-
-            col.null = fill_value.astype(input[col.name].dtype)
-    else:
-        fits_rec = FITS_rec.from_columns(np.array(input.filled()))
-        table_hdu = BinTableHDU(fits_rec)
-
-    # Set units for output HDU
-    for col in table_hdu.columns:
-        unit = input[col.name].unit
-        if unit is not None:
-            try:
-                col.unit = unit.to_string(format='fits')
-            except UnitScaleError:
-                scale = unit.scale
-                raise UnitScaleError(
-                    "The column '{0}' could not be stored in FITS format "
-                    "because it has a scale '({1})' that "
-                    "is not recognized by the FITS standard. Either scale "
-                    "the data or change the units.".format(col.name, str(scale)))
-            except ValueError:
-                warnings.warn(
-                    "The unit '{0}' could not be saved to FITS format".format(
-                        unit.to_string()), AstropyUserWarning)
-
-    for key, value in input.meta.items():
-
-        if is_column_keyword(key.upper()) or key.upper() in REMOVE_KEYWORDS:
-
-            warnings.warn(
-                "Meta-data keyword {0} will be ignored since it conflicts "
-                "with a FITS reserved keyword".format(key), AstropyUserWarning)
-
-        if isinstance(value, list):
-            for item in value:
-                try:
-                    table_hdu.header.append((key, item))
-                except ValueError:
-                    warnings.warn(
-                        "Attribute `{0}` of type {1} cannot be written to "
-                        "FITS files - skipping".format(key, type(value)),
-                        AstropyUserWarning)
-        else:
-            try:
-                table_hdu.header[key] = value
-            except ValueError:
-                warnings.warn(
-                    "Attribute `{0}` of type {1} cannot be written to FITS "
-                    "files - skipping".format(key, type(value)),
-                    AstropyUserWarning)
-
-    # Write out file
     table_hdu.writeto(output)
 
 io_registry.register_reader('fits', Table, read_table_fits)
--- astropy/io/fits/convenience.py
+++ astropy/io/fits/convenience.py
@@ -56,6 +56,7 @@ explanation of all the different formats.
 
 
 import os
+import warnings
 
 import numpy as np
 
@@ -66,13 +67,17 @@ from .hdu.image import PrimaryHDU, ImageHDU
 from .hdu.table import BinTableHDU
 from .header import Header
 from .util import fileobj_closed, fileobj_name, fileobj_mode, _is_int
+from .fitsrec import FITS_rec
+from ...units.format.fits import UnitScaleError
+from ...extern import six
 from ...extern.six import string_types
 from ...utils import deprecated
+from ...utils.exceptions import AstropyUserWarning
 
 
 __all__ = ['getheader', 'getdata', 'getval', 'setval', 'delval', 'writeto',
            'append', 'update', 'info', 'tdump', 'tcreate', 'tabledump',
-           'tableload']
+           'tableload', 'table_to_hdu']
 
 
 def getheader(filename, *args, **kwargs):
@@ -407,6 +412,103 @@ def writeto(filename, data, header=None, output_verify='exception',
         hdu = PrimaryHDU(data, header=header)
     hdu.writeto(filename, clobber=clobber, output_verify=output_verify,
                 checksum=checksum)
+
+
+def table_to_hdu(table):
+    """
+    Convert an astropy.table.Table object to a FITS BinTableHDU
+
+    Parameters
+    ----------
+    table : astropy.table.Table
+        The table to convert.
+
+    Returns
+    -------
+    table_hdu : astropy.io.fits.BinTableHDU
+        The FITS binary table HDU
+    """
+    # Avoid circular imports
+    from .connect import is_column_keyword, REMOVE_KEYWORDS
+
+    # Tables with mixin columns are not supported
+    if table.has_mixin_columns:
+        mixin_names = [name for name, col in table.columns.items()
+                       if not isinstance(col, table.ColumnClass)]
+        raise ValueError('cannot write table with mixin column(s) {0} to FITS'
+                         .format(mixin_names))
+
+    # Create a new HDU object
+    if table.masked:
+        #float column's default mask value needs to be Nan
+        for column in six.itervalues(table.columns):
+            fill_value = column.get_fill_value()
+            if column.dtype.kind == 'f' and np.allclose(fill_value, 1e20):
+                column.set_fill_value(np.nan)
+
+        fits_rec = FITS_rec.from_columns(np.array(table.filled()))
+        table_hdu = BinTableHDU(fits_rec)
+        for col in table_hdu.columns:
+            # Binary FITS tables support TNULL *only* for integer data columns
+            # TODO: Determine a schema for handling non-integer masked columns
+            # in FITS (if at all possible)
+            int_formats = ('B', 'I', 'J', 'K')
+            if not (col.format in int_formats or
+                    col.format.p_format in int_formats):
+                continue
+
+            # The astype is necessary because if the string column is less
+            # than one character, the fill value will be N/A by default which
+            # is too long, and so no values will get masked.
+            fill_value = table[col.name].get_fill_value()
+
+            col.null = fill_value.astype(table[col.name].dtype)
+    else:
+        fits_rec = FITS_rec.from_columns(np.array(table.filled()))
+        table_hdu = BinTableHDU(fits_rec)
+
+    # Set units for output HDU
+    for col in table_hdu.columns:
+        unit = table[col.name].unit
+        if unit is not None:
+            try:
+                col.unit = unit.to_string(format='fits')
+            except UnitScaleError:
+                scale = unit.scale
+                raise UnitScaleError(
+                    "The column '{0}' could not be stored in FITS format "
+                    "because it has a scale '({1})' that "
+                    "is not recognized by the FITS standard. Either scale "
+                    "the data or change the units.".format(col.name, str(scale)))
+            except ValueError:
+                warnings.warn(
+                    "The unit '{0}' could not be saved to FITS format".format(
+                        unit.to_string()), AstropyUserWarning)
+
+    for key, value in table.meta.items():
+        if is_column_keyword(key.upper()) or key.upper() in REMOVE_KEYWORDS:
+            warnings.warn(
+                "Meta-data keyword {0} will be ignored since it conflicts "
+                "with a FITS reserved keyword".format(key), AstropyUserWarning)
+
+        if isinstance(value, list):
+            for item in value:
+                try:
+                    table_hdu.header.append((key, item))
+                except ValueError:
+                    warnings.warn(
+                        "Attribute `{0}` of type {1} cannot be written to "
+                        "FITS files - skipping".format(key, type(value)),
+                        AstropyUserWarning)
+        else:
+            try:
+                table_hdu.header[key] = value
+            except ValueError:
+                warnings.warn(
+                    "Attribute `{0}` of type {1} cannot be written to FITS "
+                    "files - skipping".format(key, type(value)),
+                    AstropyUserWarning)
+    return table_hdu
 
 
 def append(filename, data, header=None, checksum=False, verify=True, **kwargs):
