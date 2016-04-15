Fix avoids implicit casting which doesn't work in numpy anymore but leaves existing behavior with sign casting as is.

astropy/io/fits/hdu/image.py
@@ -514,7 +514,8 @@ class _ImageBaseHDU(_ValidHDU):
         # Do the scaling
         if _zero != 0:
             # 0.9.6.3 to avoid out of range error for BZERO = +32768
-            self.data += -_zero
+            #have to explcitly cast _zero to prevent numpy from complaining, consider explicit type assertions for signed vs unsigned
+            np.add(self.data, -_zero, out=self.data, casting='unsafe')
             self._header['BZERO'] = _zero
         else:
             try: