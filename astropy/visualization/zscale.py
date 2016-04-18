216c9d169f51632060305ecb83571ad87dc481f1
Simon Conseil <contact@saimon.org>
2016-04-18 13:06:57 +0200

    Replace global variables with optional parameters.
    + Merge the zsc_fit_line function in the zscale

--- astropy/visualization/zscale.py
+++ astropy/visualization/zscale.py
@@ -15,13 +15,9 @@ import numpy as np
 
 __all__ = ['zscale']
 
-MAX_REJECT = 0.5
-MIN_NPIXELS = 5
-KREJ = 2.5
-MAX_ITERATIONS = 5
 
-
-def zscale(image, nsamples=1000, contrast=0.25):
+def zscale(image, nsamples=1000, contrast=0.25, max_reject=0.5, min_npixels=5,
+           krej=2.5, max_iterations=5):
     """Implement IRAF zscale algorithm.
 
     Parameters
@@ -32,8 +28,19 @@ def zscale(image, nsamples=1000, contrast=0.25):
         Number of points in array to sample for determining scaling factors.
         Default to 1000.
     contrast : float, optional
-        Scaling factor for determining min and max. Larger values increase the
-        difference between min and max values used for display. Default to 0.25
+        Scaling factor (between 0 and 1) for determining min and max. Larger
+        values increase the difference between min and max values used for
+        display. Default to 0.25.
+    max_reject : float, optional
+        If more than ``max_reject * npixels`` pixels are rejected, then the
+        returned values are the min and max of the data. Default to 0.5.
+    min_npixels : int, optional
+        If less than ``min_npixels`` pixels are rejected, then the
+        returned values are the min and max of the data. Default to 5.
+    krej : float, optional
+        Number of sigma used for the rejection. Default to 2.5.
+    max_iterations : int, optional
+        Maximum number of iterations for the rejection. Default to 5.
 
     Returns
     -------
@@ -53,35 +60,20 @@ def zscale(image, nsamples=1000, contrast=0.25):
     zmax = samples[-1]
 
     # Fit a line to the sorted array of samples
-    minpix = max(MIN_NPIXELS, int(npix * MAX_REJECT))
-    ngoodpix, zslope = zsc_fit_line(samples, KREJ, MAX_ITERATIONS)
-
-    if ngoodpix >= minpix:
-        if contrast > 0:
-            zslope = zslope / contrast
-        center_pixel = (npix - 1) // 2
-        median = np.median(samples)
-        zmin = max(zmin, median - (center_pixel - 1) * zslope)
-        zmax = min(zmax, median + (npix - center_pixel) * zslope)
-    return zmin, zmax
-
-
-def zsc_fit_line(samples, krej, maxiter):
-    npix = len(samples)
+    minpix = max(min_npixels, int(npix * max_reject))
     x = np.arange(npix)
-
     ngoodpix = npix
-    minpix = max(MIN_NPIXELS, int(npix * MAX_REJECT))
     last_ngoodpix = npix + 1
 
+    # Bad pixels mask used in k-sigma clipping
+    badpix = np.zeros(npix, dtype=bool)
+
+    # Kernel used to dilate the bad pixels mask
     ngrow = max(1, int(npix * 0.01))
     kernel = np.ones(ngrow, dtype=bool)
 
-    # This is the mask used in k-sigma clipping
-    badpix = np.zeros(npix, dtype=bool)
-
-    for niter in range(maxiter):
-        if (ngoodpix >= last_ngoodpix) or (ngoodpix < minpix):
+    for niter in range(max_iterations):
+        if ngoodpix >= last_ngoodpix or ngoodpix < minpix:
             break
 
         fit = np.polyfit(x, samples, deg=1, w=(~badpix).astype(int))
@@ -103,4 +95,12 @@ def zsc_fit_line(samples, krej, maxiter):
         ngoodpix = np.sum(~badpix)
 
     slope, intercept = fit
-    return ngoodpix, slope
+
+    if ngoodpix >= minpix:
+        if contrast > 0:
+            slope = slope / contrast
+        center_pixel = (npix - 1) // 2
+        median = np.median(samples)
+        zmin = max(zmin, median - (center_pixel - 1) * slope)
+        zmax = min(zmax, median + (npix - center_pixel) * slope)
+    return zmin, zmax
