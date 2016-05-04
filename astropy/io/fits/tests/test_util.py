536b95cd6e5432a3894577430222c94f5a0b979b
Michael Seifert <michaelseifert04@yahoo.de>
2016-05-04 21:27:49 +0200

    inline documentation update

--- astropy/io/fits/tests/test_util.py
+++ astropy/io/fits/tests/test_util.py
@@ -57,32 +57,39 @@ class TestUtilMode(FitsTestCase):
         # Just check that opening a file in ab mode is not considered
         # in append mode. The reason for this really low level function is
         # that (maybe) a file loses it's "a" in the mode, so this function
-        # as worst case option should not fail!
+        # as fallback option should not fail!
         with open(str(self.temp('test_simple.fits')), 'ab') as fileobj:
             assert fits.util._is_append_mode_platform(fileobj)
 
     def test_mode_strings(self):
+        # A string signals that the file should be opened so the function
+        # should return None, because it's simply not opened yet.
         assert util.fileobj_mode('tmp1.fits') is None
 
     @pytest.mark.skipif("not HAS_PIL")
     def test_mode_pil_image(self):
         img = np.random.randint(0, 255, (5, 5, 3)).astype(np.uint8)
-
         result = Image.fromarray(img)
-        result.save(self.temp('test_simple.jpg'))
 
+        result.save(self.temp('test_simple.jpg'))
+        # PIL doesn't support append mode. So it will allways use binary read.
         with Image.open(self.temp('test_simple.jpg')) as fileobj:
             assert util.fileobj_mode(fileobj) == 'rb'
 
     def test_mode_gzip(self):
-        # Open a gzip in every possible (gzip is binary or test only) way
+        # Open a gzip in every possible (gzip is binary or "touch" only) way
         # and check if the mode was correctly identified.
+
+        # The lists consist of tuples: filenumber, given mode, identified mode
+        # The filenumber must be given because read expects the file to exist
+        # and x expects it to NOT exist.
         if (version_info.major < 3 or
                 (version_info.major >= 3 and version_info.minor < 4)):
             num_mode_resmode = [(0, 'a', 'ab'), (0, 'ab', 'ab'),
                                 (1, 'w', 'wb'), (1, 'wb', 'wb'),
                                 (1, 'r', 'rb'), (1, 'rb', 'rb')]
         else:
+            # x mode was added in python 3.4
             num_mode_resmode = [(0, 'a', 'ab'), (0, 'ab', 'ab'),
                                 (0, 'w', 'wb'), (0, 'wb', 'wb'),
                                 (1, 'x', 'xb'),
@@ -94,14 +101,16 @@ class TestUtilMode(FitsTestCase):
                 assert util.fileobj_mode(fileobj) == res
 
     def test_mode_normal_buffering(self):
-        # Open a gzip in every possible (gzip is binary or test only) way
-        # and check if the mode was correctly identified.
+        # Use the python IO with buffering parameter. Binary mode only:
+
+        # see "test_mode_gzip" for explanation of tuple meanings.
         if (version_info.major < 3 or
                 (version_info.major >= 3 and version_info.minor < 3)):
             num_mode_resmode = [(0, 'ab', 'ab'),
                                 (1, 'wb', 'wb'),
                                 (1, 'rb', 'rb')]
         else:
+            # x mode was added in python 3.3
             num_mode_resmode = [(0, 'ab', 'ab'),
                                 (0, 'wb', 'wb'),
                                 (1, 'xb', 'xb'),
@@ -112,14 +121,16 @@ class TestUtilMode(FitsTestCase):
                 assert util.fileobj_mode(fileobj) == res
 
     def test_mode_normal_no_buffering(self):
-        # Open a gzip in every possible (gzip is binary or test only) way
-        # and check if the mode was correctly identified.
+        # Python IO without buffering
+
+        # see "test_mode_gzip" for explanation of tuple meanings.
         if (version_info.major < 3 or
                 (version_info.major >= 3 and version_info.minor < 3)):
             num_mode_resmode = [(0, 'a', 'a'), (0, 'ab', 'ab'),
                                 (1, 'w', 'w'), (2, 'wb', 'wb'),
                                 (1, 'r', 'r'), (2, 'rb', 'rb')]
         else:
+            # x mode was added in python 3.3
             num_mode_resmode = [(0, 'a', 'a'), (0, 'ab', 'ab'),
                                 (0, 'w', 'w'), (0, 'wb', 'wb'),
                                 (1, 'x', 'x'),
@@ -130,8 +141,12 @@ class TestUtilMode(FitsTestCase):
                 assert util.fileobj_mode(fileobj) == res
 
     def test_mode_normalization(self):
-        # Open a gzip in every possible (gzip is binary or test only) way
-        # and check if the mode was correctly identified.
+        # Use the normal python IO in append mode with all possible permutation
+        # of the "mode" letters.
+
+        # Tuple gives a file name suffix, the given mode and the functions
+        # return. The filenumber is only for consistency with the other
+        # test functions. Append can deal with existing and not existing files.
         for num, mode, res in [(0, 'a', 'a'),
                                (0, 'a+', 'a+'),
                                (0, 'ab', 'ab'),
