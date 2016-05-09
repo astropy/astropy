34c78abc2ef8c8b654276eb67d986ae634022487
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-05-09 13:58:34 +0100

    Remove unused imports

--- astropy/modeling/core.py
+++ astropy/modeling/core.py
@@ -34,9 +34,7 @@ from ..utils import indent, isinstancemethod, metadata
 from ..extern import six
 from ..extern.six.moves import copyreg, zip
 from ..table import Table
-from ..units import (Quantity, UnitBase, UnitConversionError,
-                     dimensionless_unscaled)
-from ..units.quantity import _can_have_arbitrary_unit
+from ..units import Quantity
 from ..utils import (deprecated, sharedmethod, find_current_module,
                      InheritDocstrings, OrderedDescriptorContainer,
                      check_broadcast, IncompatibleShapeError)
