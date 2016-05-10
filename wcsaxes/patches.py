83aba490a331fd1d36ac96860c9c131468a29767
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-05-10 14:15:15 +0100

    Added SphericalCircle patch

---
+++ wcsaxes/patches.py
@@ -0,0 +1,80 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from __future__ import print_function, division, absolute_import
+
+import numpy as np
+from matplotlib.patches import Polygon
+
+from astropy import units as u
+from astropy.coordinates.representation import UnitSphericalRepresentation
+from astropy.coordinates.angles import rotation_matrix
+
+__all__ = ['SphericalCircle']
+
+
+class SphericalCircle(Polygon):
+    """
+    Create a patch representing a spherical circle - that is, a circle that is
+    formed of all the points that are within a certain angle of the central
+    coordinates on a sphere. Here we assume that latitude goes from -90 to +90
+    
+    This class is needed in cases where the user wants to add a circular patch
+    to a celestial image, since otherwise the circle will be distorted, because
+    a fixed interval in longitude corresponds to a different angle on the sky
+    depending on the latitude.
+    
+    Parameters
+    ----------
+    center : tuple or `~astropy.units.Quantity`
+        This can be either a tuple of two `~astropy.units.Quantity` objects, or
+        a single `~astropy.units.Quantity` array with two elements.
+    radius : `~astropy.units.Quantity`
+        The radius of the circle
+    resolution : int, optional
+        The number of points that make up the circle - increase this to get a
+        smoother circle.
+    vertex_unit : `~astropy.units.Unit`
+        The units in which the resulting polygon should be defined.
+
+    Notes
+    -----
+    Additional keyword arguments are passed to `~matplotlib.patches.Polygon`
+    """
+
+    def __init__(self, center, radius, resolution=100, vertex_unit=u.degree, **kwargs):
+
+        # Extract longitude/latitude, either from a tuple of two quantities, or
+        # a single 2-element Quantity.
+        longitude, latitude = center
+
+        # Convert all input values to radians
+        longitude = longitude.to(u.radian).value
+        latitude = latitude.to(u.radian).value
+        radius = radius.to(u.radian).value
+
+        # Start off by generating the circle around the North pole
+        lon = np.linspace(0., 2 * np.pi, resolution + 1)[:-1]
+        lat = np.repeat(0.5 * np.pi - radius, resolution)
+
+        # Create a representation object
+        polygon = UnitSphericalRepresentation(lon=lon * u.radian, lat=lat * u.radian)
+
+        # Determine rotation matrix to make it so that the circle is centered
+        # on the correct longitude/latitude.
+        m1 = rotation_matrix(-(0.5 * np.pi - latitude) * u.radian, axis='y')
+        m2 = rotation_matrix(-longitude * u.radian, axis='z')
+        transform_matrix = m2 * m1
+
+        # Apply 3D rotation
+        polygon = polygon.to_cartesian()
+        polygon = polygon.transform(transform_matrix)
+        polygon = UnitSphericalRepresentation.from_cartesian(polygon)
+
+        # Extract new longitude/latitude in degrees
+        lon = polygon.lon.to(vertex_unit).value
+        lat = polygon.lat.to(vertex_unit).value
+
+        # Create polygon vertices
+        vertices = np.array([lon, lat]).transpose()
+
+        super(SphericalCircle, self).__init__(vertices, **kwargs)
