07922500a2716e37d7fc39784a14923f314b3622
Tom Aldcroft <taldcroft@gmail.com>
2016-05-15 17:35:25 -0400

    Move test IERS-A files from data into tests

--- astropy/utils/setup_package.py
+++ astropy/utils/setup_package.py
@@ -30,9 +30,9 @@ def get_package_data():
         'astropy.utils.iers': [
             'data/ReadMe.eopc04_IAU2000',
             'data/ReadMe.finals2000A',
-            'data/finals2000A-2016-04-30-test',
-            'data/finals2000A-2016-02-30-test',
             'data/eopc04_IAU2000.62-now',
+            'tests/finals2000A-2016-04-30-test',
+            'tests/finals2000A-2016-02-30-test',
             'tests/iers_a_excerpt']
     }
 
