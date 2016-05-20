1b37035d7b1148e5507af649b46aac6504d36699
Simon Conseil <contact@saimon.org>
2016-05-20 11:08:14 +0200

    Check if DE locale is available in the test itself

--- astropy/io/ascii/tests/test_read.py
+++ astropy/io/ascii/tests/test_read.py
@@ -35,19 +35,6 @@ except ImportError:
 else:
     HAS_PATHLIB = True
 
-current = locale.setlocale(locale.LC_ALL)
-try:
-    if platform.system() == 'Darwin':
-        locale.setlocale(locale.LC_ALL, str('de_DE'))
-    else:
-        locale.setlocale(locale.LC_ALL, str('de_DE.utf8'))
-except:
-    HAS_DE_LOCALE = False
-else:
-    HAS_DE_LOCALE = True
-finally:
-    locale.setlocale(locale.LC_ALL, current)
-
 
 @pytest.mark.parametrize('fast_reader', [True, False, 'force'])
 def test_convert_overflow(fast_reader):
@@ -1168,17 +1155,21 @@ def test_column_conversion_error():
         ascii.read(['a b', '1 2'], guess=False, format='basic', converters={'a': []})
     assert 'no converters' in str(err.value)
 
-@pytest.mark.skipif('not HAS_DE_LOCALE')
 def test_non_C_locale_with_fast_reader():
     """Test code that forces "C" locale while calling fast reader (#4364)"""
     current = locale.setlocale(locale.LC_ALL)
 
     try:
-        locale.setlocale(locale.LC_ALL, str('de_DE.utf8'))
+        if platform.system() == 'Darwin':
+            locale.setlocale(locale.LC_ALL, str('de_DE'))
+        else:
+            locale.setlocale(locale.LC_ALL, str('de_DE.utf8'))
 
         for fast_reader in (True, False, {'use_fast_converter': False}, {'use_fast_converter': True}):
             t = ascii.read(['a b', '1.5 2'], format='basic', guess=False,
                            fast_reader=fast_reader)
             assert t['a'].dtype.kind == 'f1'
+    except locale.Error as e:
+        pytest.skip('Locale error: {}'.format(e))
     finally:
         locale.setlocale(locale.LC_ALL, current)
