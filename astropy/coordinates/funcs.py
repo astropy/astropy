Merge pull request #5024 from eteq/fix-coordinate-broadcasting
Restore get_sun broadcasting to fix coordinate gallery example

astropy/coordinates/funcs.py
@@ -159,18 +159,14 @@ def get_sun(time):
     # convert barycentric velocity to units of c, but keep as array for passing in to erfa
     earth_v /= c.to(u.au/u.d).value
 
-    dsun = np.sqrt(np.sum(earth_p**2, axis=-1, keepdims=True))
+    dsun = np.sqrt(np.sum(earth_p**2, axis=-1))
     invlorentz = (1-np.sum(earth_v**2, axis=-1))**0.5
-    properdir = erfa.ab(earth_p/dsun, -earth_v, dsun, invlorentz)
+    properdir = erfa.ab(earth_p/dsun.reshape(dsun.shape + (1,)),
+                        -earth_v, dsun, invlorentz)
 
-    x = -dsun*properdir[..., 0] * u.AU
-    y = -dsun*properdir[..., 1] * u.AU
-    z = -dsun*properdir[..., 2] * u.AU
-
-    if time.isscalar:
-        cartrep = CartesianRepresentation(x=x[0], y=y[0], z=z[0])
-    else:
-        cartrep = CartesianRepresentation(x=x, y=y, z=z)
+    cartrep = CartesianRepresentation(x=-dsun*properdir[..., 0] * u.AU,
+                                      y=-dsun*properdir[..., 1] * u.AU,
+                                      z=-dsun*properdir[..., 2] * u.AU)
     return SkyCoord(cartrep, frame=GCRS(obstime=time))
 
 