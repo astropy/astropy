7754e8bcf57c381d2e79ef8e53c0171d200a5c32
Larry Bradley <larry.bradley@gmail.com>
2016-06-09 20:12:10 -0400

    Update non_celestial_pixel_scales docs [skip ci]

--- astropy/wcs/utils.py
+++ astropy/wcs/utils.py
@@ -322,18 +322,18 @@ def _is_cd_orthogonal(cd, maxerr):
 
 def non_celestial_pixel_scales(inwcs):
     """
-    For a non-celestial WCS, e.g. one with mixed spectral and spatial axes, it
-    is still sometimes possible to define a pixel scale.
+    Calculate the pixel scale along each axis of a non-celestial WCS,
+    for example one with mixed spectral and spatial axes.
 
     Parameters
     ----------
     inwcs : `~astropy.wcs.WCS`
-        The world coordinate system object
+        The world coordinate system object.
 
     Returns
     -------
     scale : `numpy.ndarray`
-        The pixel scale along each axis
+        The pixel scale along each axis.
     """
 
     if inwcs.is_celestial:
