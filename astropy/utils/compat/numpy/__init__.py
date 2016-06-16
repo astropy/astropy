60504a613ca8cce730b61ca4a1e3b73e6c08dba7
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2016-06-16 15:04:09 -0400

    Add matmul to utils.compat.numpy.

--- astropy/utils/compat/numpy/__init__.py
+++ astropy/utils/compat/numpy/__init__.py
@@ -8,3 +8,4 @@ in all supported NumPy versions.  See docs/utils/numpy.rst for details.
 from __future__ import division, absolute_import, unicode_literals
 
 from .lib.stride_tricks import broadcast_arrays, broadcast_to
+from .core.multiarray import matmul
