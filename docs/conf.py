acae29d2dbd5352501a01384e3b09fc2d89adad0
Brigitta Sipocz <bsipocz@gmail.com>
2016-07-01 18:08:54 +0100

    Removing rtd theme leftover

--- docs/conf.py
+++ docs/conf.py
@@ -192,16 +192,6 @@ plot_template = """
 
 """
 
-# on_rtd is whether we are on readthedocs.org
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 # Enable nitpicky mode - which ensures that all references in the docs
 # resolve.
