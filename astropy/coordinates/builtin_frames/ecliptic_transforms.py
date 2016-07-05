56e4c0495e9f37a0a9ab91fc185c5f3b0d85931d
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2016-07-05 15:42:17 -0400

    Remove cartrepr_from_matmul altogether, since it hardly does anything any more.

--- astropy/coordinates/builtin_frames/ecliptic_transforms.py
+++ astropy/coordinates/builtin_frames/ecliptic_transforms.py
@@ -16,8 +16,7 @@ from ... import _erfa as erfa
 from .icrs import ICRS
 from .gcrs import GCRS
 from .ecliptic import GeocentricTrueEcliptic, BarycentricTrueEcliptic, HeliocentricTrueEcliptic
-from .utils import (cartrepr_from_matmul, get_jd12,
-                    matrix_product, matrix_transpose)
+from .utils import get_jd12, matrix_product, matrix_transpose
 from ..errors import UnitsError
 
 
@@ -34,14 +33,14 @@ def gcrs_to_geoecliptic(gcrs_coo, to_frame):
     gcrs_coo2 = gcrs_coo.transform_to(GCRS(obstime=to_frame.equinox))
 
     rmat = _ecliptic_rotation_matrix(to_frame.equinox)
-    newrepr = cartrepr_from_matmul(rmat, gcrs_coo2)
+    newrepr = gcrs_coo2.cartesian.transform(rmat)
     return to_frame.realize_frame(newrepr)
 
 
 @frame_transform_graph.transform(FunctionTransform, GeocentricTrueEcliptic, GCRS)
 def geoecliptic_to_gcrs(from_coo, gcrs_frame):
     rmat = _ecliptic_rotation_matrix(from_coo.equinox)
-    newrepr = cartrepr_from_matmul(rmat, from_coo, transpose=True)
+    newrepr = from_coo.cartesian.transform(matrix_transpose(rmat))
     gcrs = GCRS(newrepr, obstime=from_coo.equinox)
 
     # now do any needed offsets (no-op if same obstime and 0 pos/vel)
@@ -78,8 +77,7 @@ def icrs_to_helioecliptic(from_coo, to_frame):
     # now compute the matrix to precess to the right orientation
     rmat = _ecliptic_rotation_matrix(to_frame.equinox)
 
-    # it's not really ICRS because of the offset, but this is digestible by cartrepr_from_matmul
-    newrepr = cartrepr_from_matmul(rmat, ICRS(CartesianRepresentation(heliocart.T)))
+    newrepr = CartesianRepresentation(heliocart.T).transform(rmat)
     return to_frame.realize_frame(newrepr)
 
 
@@ -90,7 +88,7 @@ def helioecliptic_to_icrs(from_coo, to_frame):
 
     # first un-precess from ecliptic to ICRS orientation
     rmat = _ecliptic_rotation_matrix(from_coo.equinox)
-    intermed_repr = cartrepr_from_matmul(rmat, from_coo, transpose=True)
+    intermed_repr = from_coo.cartesian.transform(matrix_transpose(rmat))
 
     # now offset back to barycentric, which is the correct center for ICRS
     pvh, pvb = erfa.epv00(*get_jd12(from_coo.equinox, 'tdb'))
--- astropy/coordinates/solar_system.py
+++ astropy/coordinates/solar_system.py
@@ -22,7 +22,7 @@ from ..constants import c as speed_of_light
 from .representation import CartesianRepresentation
 from .orbital_elements import calc_moon
 from .builtin_frames import GCRS, ICRS
-from .builtin_frames.utils import get_jd12, cartrepr_from_matmul
+from .builtin_frames.utils import get_jd12
 from .. import _erfa
 from ..extern import six
 
@@ -389,4 +389,5 @@ def _apparent_position_in_true_coordinates(skycoord):
     """
     jd1, jd2 = get_jd12(skycoord.obstime, 'tt')
     _, _, _, _, _, _, _, rbpn = _erfa.pn00a(jd1, jd2)
-    return SkyCoord(skycoord.frame.realize_frame(cartrepr_from_matmul(rbpn, skycoord)))
+    return SkyCoord(skycoord.frame.realize_frame(
+        skycoord.cartesian.transform(rbpn)))
