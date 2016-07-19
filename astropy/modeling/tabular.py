abb79b51c2a6a797d3fc9c71f8ad16de297273a8
Nadia Dencheva <nadia.dencheva@gmail.com>
2016-07-19 17:11:32 -0400

    pass the dimension of the table instead of the table to create a class.

--- astropy/modeling/tabular.py
+++ astropy/modeling/tabular.py
@@ -1,6 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-"""Tabular models."""
+"""
+Tabular models.
+
+Tabular models of any dimension can be created using `tabular_model`.
+For convenience `Tabular1D` and `Tabular2D` are provided.
+
+Examples
+--------
+>>> table = np.array([[ 3.,  0.,  0.],
+...                  [ 0.,  2.,  0.],
+...                  [ 0.,  0.,  0.]])
+>>> points = ([1, 2, 3], [1, 2, 3])
+>>> t2 = Tabular2D(points, lookup_table=table, bounds_error=False, fill_value=None, method='nearest')
+
+"""
 
 from __future__ import (absolute_import, unicode_literals, division,
                         print_function)
@@ -16,16 +30,16 @@ try:
 except ImportError:
     has_scipy = False
 
-if has_scipy and not minversion(scipy, "0.14"):
-    has_scipy = False
+has_scipy = has_scipy and minversion(scipy, "0.14")
 
 
-__all__ = ['tabular_model']
-
-__doctest_requires__ = {('Tabular', 'tabular_model'): ['scipy']}
+__all__ = ['tabular_model', 'Tabular1D', 'Tabular2D']
 
 
-class Tabular(Model):
+__doctest_requires__ = {('tabular_model'): ['scipy']}
+
+
+class _Tabular(Model):
     """
     Returns an interpolated lookup table value.
 
@@ -33,7 +47,7 @@ class Tabular(Model):
     ----------
     points : tuple of ndarray of float, with shapes (m1, ), ..., (mn, ), optional
         The points defining the regular grid in n dimensions.
-    lookup_table : array_like, shape (m1, ..., mn, ...)
+    lookup_table : array-like, shape (m1, ..., mn, ...)
         The data on a regular grid in n dimensions.
     method : str, optional
         The method of interpolation to perform. Supported are "linear" and
@@ -42,7 +56,7 @@ class Tabular(Model):
     bounds_error : bool, optional
         If True, when interpolated values are requested outside of the
         domain of the input data, a ValueError is raised.
-        If False, then `fill_value` is used.
+        If False, then ``fill_value`` is used.
     fill_value : float, optional
         If provided, the value to use for points outside of the
         interpolation domain. If None, values outside
@@ -61,7 +75,7 @@ class Tabular(Model):
 
     Notes
     -----
-    Uses ``scipy.interpolate.interpn``
+    Uses `scipy.interpolate.interpn`.
 
     """
 
@@ -80,7 +94,11 @@ class Tabular(Model):
     def __init__(self, points=None, lookup_table=None, method='linear',
                  bounds_error=True, fill_value=np.nan, **kwargs):
 
-        super(Tabular, self).__init__(**kwargs)
+        n_models = kwargs.get('n_models', 1)
+        if n_models > 1:
+            raise NotImplementedError('Only n_models=1 is supported.')
+        super(_Tabular, self).__init__(**kwargs)
+
         if lookup_table is not None:
             lookup_table = np.asarray(lookup_table)
             if self.lookup_table.ndim != lookup_table.ndim:
@@ -106,7 +124,59 @@ class Tabular(Model):
 
     @property
     def points(self):
-        return self._points
+        """
+        An iterable of points defining the values in the lookup table.
+        """
+
+        return np.squeeze(self._points)
+
+    def __repr__(self):
+        fmt = "<{0}(points={1}, lookup_table={2})>".format(self.__class__.__name__,
+                                                           self.points, self.lookup_table)
+        return fmt
+
+    def __str__(self):
+        default_keywords = [
+            ('Model', self.__class__.__name__),
+            ('Name', self.name),
+            ('Inputs', self.inputs),
+            ('Outputs', self.outputs),
+            ('Parameters', ""),
+            ('  points', self.points),
+            ('  lookup_table', self.lookup_table),
+            ('  method', self.method),
+            ('  fill_value', self.fill_value),
+            ('  bounds_error', self.bounds_error)
+        ]
+
+        parts = ['{0}: {1}'.format(keyword, value)
+                 for keyword, value in default_keywords
+                 if value is not None]
+
+        return '\n'.join(parts)
+
+
+    @property
+    def bounding_box(self):
+        """
+        Tuple defining the default ``bounding_box`` limits,
+        ``(points_low, points_high)``.
+
+        Examples
+        --------
+        >>> from astropy.modeling.models import Tabular1D, Tabular2D
+        >>> t1 = Tabular1D(points=[1,2,3], lookup_table=[10, 20, 30])
+        >>> t1.bounding_box
+        (1, 3)
+        >>> t2 = Tabular2D(points=[[1,2,3],[2,3,4]], lookup_table=[[10,20,30],[20,30,40]])
+        >>> t2.bounding_box
+        ((2, 4), (1, 3))
+
+        """
+        bbox = [(min(p), max(p)) for p in self._points][::-1]
+        if len(bbox) == 1:
+            bbox = bbox[0]
+        return tuple(bbox)
 
     def evaluate(self, *inputs):
         """
@@ -121,12 +191,12 @@ class Tabular(Model):
         inputs = np.array(inputs[: self.n_inputs]).T
         if not has_scipy:
             raise ImportError("This model requires scipy >= v0.14")
-        return interpn(self.points, self.lookup_table, inputs,
+        return interpn(self._points, self.lookup_table, inputs,
                        method=self.method, bounds_error=self.bounds_error,
                        fill_value=self.fill_value)
 
 
-def tabular_model(lookup_table, name=None):
+def tabular_model(dim, name=None):
     """
     Make a ``Tabular`` model where ``n_inputs`` is
     based on the dimension of the lookup_table.
@@ -136,8 +206,8 @@ def tabular_model(lookup_table, name=None):
 
     Parameters
     ----------
-    lookup_table : array_like, shape (m1, ..., mn)
-        The data on a regular grid in n dimensions.
+    dim : int
+        Dimensions of the lookup table.
     name : str
         Name for the class.
 
@@ -147,7 +217,7 @@ def tabular_model(lookup_table, name=None):
     ...                  [ 0.,  2.,  0.],
     ...                  [ 0.,  0.,  0.]])
 
-    >>> tab = tabular_model(table, name='Tabular2D')
+    >>> tab = tabular_model(2, name='Tabular2D')
     >>> print(tab)
         <class 'abc.Tabular2D'>
         Name: Tabular2D
@@ -157,19 +227,82 @@ def tabular_model(lookup_table, name=None):
     >>> points = ([1, 2, 3], [1, 2, 3])
 
     Setting fill_value to None, allows extrapolation.
-    >>> m = tab(points, name='my_table', bounds_error=False, fill_value=None, method='nearest')
+    >>> m = tab(points, lookup_table=table, name='my_table', bounds_error=False, fill_value=None, method='nearest')
 
     >>> xinterp = [0, 1, 1.5, 2.72, 3.14]
     >>> m(xinterp, xinterp)
         array([ 3., 3., 3., 0., 0.])
 
     """
-    table = np.asanyarray(lookup_table)
+    table = np.zeros([2] * dim)
     inputs = tuple('x{0}'.format(idx) for idx in range(table.ndim))
     members = {'lookup_table': table, 'inputs': inputs}
     if name is None:
-        model_id = Tabular._id
-        Tabular._id += 1
+        model_id = _Tabular._id
+        _Tabular._id += 1
         name = 'Tabular{0}'.format(model_id)
 
-    return type(str(name), (Tabular,), members)
+    return type(str(name), (_Tabular,), members)
+
+
+Tabular1D = tabular_model(1, name='Tabular1D')
+
+
+Tabular2D = tabular_model(2, name='Tabular2D')
+
+_tab_docs = """
+    method : str, optional
+        The method of interpolation to perform. Supported are "linear" and
+        "nearest", and "splinef2d". "splinef2d" is only supported for
+        2-dimensional data. Default is "linear".
+    bounds_error : bool, optional
+        If True, when interpolated values are requested outside of the
+        domain of the input data, a ValueError is raised.
+        If False, then ``fill_value`` is used.
+    fill_value : float, optional
+        If provided, the value to use for points outside of the
+        interpolation domain. If None, values outside
+        the domain are extrapolated.  Extrapolation is not supported by method
+        "splinef2d".
+
+    Returns
+    -------
+    value : ndarray
+        Interpolated values at input coordinates.
+
+    Raises
+    ------
+    ImportError
+        Scipy is not installed.
+
+    Notes
+    -----
+    Uses `scipy.interpolate.interpn`.
+"""
+
+
+Tabular1D.__doc__  =  """
+    Tabular model in 1D.
+    Returns an interpolated lookup table value.
+
+    Parameters
+    ----------
+    points : array-like of float of ndim=1.
+        The points defining the regular grid in n dimensions.
+    lookup_table : array-like, of ndim=1.
+        The data in one dimensions.
+""" + _tab_docs
+
+
+Tabular2D.__doc__  =  """
+    Tabular model in 2D.
+    Returns an interpolated lookup table value.
+
+    Parameters
+    ----------
+    points : tuple of ndarray of float, with shapes (m1, m2), optional
+        The points defining the regular grid in n dimensions.
+    lookup_table : array-like, shape (m1, m2)
+        The data on a regular grid in 2 dimensions.
+
+""" + _tab_docs
