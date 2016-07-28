ddb1e3ef4dd5765846bfdcdb9537b028fcf246a7
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-07-28 17:03:25 +0100

    import builtin sites module

--- astropy/coordinates/tests/test_regression.py
+++ astropy/coordinates/tests/test_regression.py
@@ -13,7 +13,8 @@ import numpy as np
 
 from ... import units as u
 from .. import (AltAz, EarthLocation, SkyCoord, get_sun, ICRS, CIRS, ITRS,
-                GeocentricTrueEcliptic, Longitude, Latitude, GCRS, get_moon)
+                GeocentricTrueEcliptic, Longitude, Latitude, GCRS, get_moon,
+                get_builtin_sites)
 from ...time import Time
 from ...utils import iers
 
