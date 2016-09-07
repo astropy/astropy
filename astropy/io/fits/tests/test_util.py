d068b1ab564358ba50abac47ae70872250b2d736
Erik M. Bray <erik.bray@lri.fr>
2016-09-07 10:33:15 +0200

    Fix logic in how the new dtype's itemsize is determined, and add regression tests for corner cases.
    The previous logic mistakenly assumed that the field with the highest offset determines the itemsize, but this isn't necessarily true when there are overlapping fields (which there may be, though it's highly unlikely in practice)

--- astropy/io/fits/tests/test_util.py
+++ astropy/io/fits/tests/test_util.py
@@ -18,6 +18,7 @@ except ImportError:
 from ....tests.helper import pytest, catch_warnings
 from .. import util
 from ..util import ignore_sigint
+from .._numpy_hacks import realign_dtype
 
 from . import FitsTestCase
 
@@ -37,6 +38,35 @@ class TestUtils(FitsTestCase):
                     'KeyboardInterrupt ignored until test is complete!')
 
         pytest.raises(KeyboardInterrupt, test)
+
+    def test_realign_dtype(self):
+        """
+        Tests a few corner-cases for the realign_dtype hack.
+
+        These are unlikely to come in practice given how this is currently
+        used in astropy.io.fits, but nonetheless tests for bugs that were
+        present in earlier versions of the function.
+        """
+
+        dt = np.dtype([('a', np.int32), ('b', np.int16)])
+        dt2 = realign_dtype(dt, [0, 0])
+        assert dt2.itemsize == 4
+
+        dt2 = realign_dtype(dt, [0, 1])
+        assert dt2.itemsize == 4
+
+        dt2 = realign_dtype(dt, [1, 0])
+        assert dt2.itemsize == 5
+
+        dt = np.dtype([('a', np.float64), ('b', np.int8), ('c', np.int8)])
+        dt2 = realign_dtype(dt, [0, 0, 0])
+        assert dt2.itemsize == 8
+
+        dt2 = realign_dtype(dt, [0, 0, 1])
+        assert dt2.itemsize == 8
+
+        dt2 = realign_dtype(dt, [0, 0, 27])
+        assert dt2.itemsize == 28
 
 
 class TestUtilMode(FitsTestCase):
