f8b4b5efc51a476ba4983a9cb2066b3944a0bb2b
Michael Seifert <michaelseifert04@yahoo.de>
2016-11-22 16:28:53 +0100

    remove dynamic altering of the docstring in deprecated_renamed_argument

--- astropy/utils/decorators.py
+++ astropy/utils/decorators.py
@@ -7,12 +7,9 @@ from __future__ import print_function
 
 import functools
 import inspect
-import itertools
-import re
 import textwrap
 import types
 import warnings
-from distutils.version import LooseVersion
 
 from .codegen import make_function_with_signature
 from .exceptions import (AstropyDeprecationWarning, AstropyUserWarning,
@@ -24,9 +21,6 @@ from ..extern.six.moves import zip
 __all__ = ['classproperty', 'deprecated', 'deprecated_attribute',
            'deprecated_renamed_argument', 'format_doc',
            'lazyproperty', 'sharedmethod', 'wraps']
-
-
-_BLANKLINE_REGEX = re.compile(r'\n\s*\n')
 
 
 def deprecated(since, message='', name='', alternative='', pending=False,
@@ -314,12 +308,6 @@ def deprecated_renamed_argument(old_name, new_name, since,
         and a Warning is issued.
         Default is ``False``.
 
-    .. warning::
-        If ``old_name`` is a list or tuple the ``new_name`` and ``since`` must
-        also be a list or tuple with the same number of entries. ``relax`` and
-        ``arg_in_kwarg`` can be a single bool (applied to all) or also a
-        list/tuple with the same number of entries like ``new_name``, etc.
-
     Raises
     ------
     TypeError
@@ -334,6 +322,12 @@ def deprecated_renamed_argument(old_name, new_name, since,
     -----
     The decorator should be applied to a function where the **name**
     of an argument was changed but it applies the same logic.
+
+    .. warning::
+        If ``old_name`` is a list or tuple the ``new_name`` and ``since`` must
+        also be a list or tuple with the same number of entries. ``relax`` and
+        ``arg_in_kwarg`` can be a single bool (applied to all) or also a
+        list/tuple with the same number of entries like ``new_name``, etc.
 
     Examples
     --------
@@ -459,36 +453,6 @@ def deprecated_renamed_argument(old_name, new_name, since,
                                 'signature. If it was meant to be part of '
                                 '"**kwargs" then set "arg_in_kwargs" to "True"'
                                 '.'.format(new_name[i]))
-
-        if function.__doc__:
-            # Remove indentation of the original documentation so it will be
-            # correctly rendered.
-            doc = []
-            original_doc = _BLANKLINE_REGEX.split(inspect.getdoc(function),
-                                                  maxsplit=1)
-
-            # Append the deprecation messages ordered by their "since".
-            def item2(x):
-                return LooseVersion(str(x[2]))
-
-            # Add the "versionchanged" directive to the docstring of the
-            # function, but in _sorted_ order and _grouped_ by version.
-            # Note that groupby only groups successive identical elements so
-            # the sorting is required.
-            sorted_zipped = sorted(zip(old_name, new_name, since), key=item2)
-            for k, vals in itertools.groupby(sorted_zipped, key=item2):
-                doc.append("\n.. versionchanged:: {}".format(k))
-                for old, new, _ in vals:
-                    doc.append("   ``{}`` replaces the deprecated "
-                               "``{}`` argument.".format(new, old))
-
-            doc.insert(0, original_doc[0])
-            if len(original_doc) > 1 and original_doc[1].strip():
-                # Also append an empty string so there is an empty line between
-                # directives and the remaining parts of the original docs.
-                doc.extend(('', original_doc[1]))
-
-            function.__doc__ = '\n'.join(doc)
 
         @functools.wraps(function)
         def wrapper(*args, **kwargs):
