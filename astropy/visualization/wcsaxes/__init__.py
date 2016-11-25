43c0b283073edc93d99aff04c9bca699dd39fa83
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-11-25 20:28:14 +0000

    Skip astropy.visualization.wcsaxes in tests if Matplotlib is not installed

--- astropy/visualization/wcsaxes/__init__.py
+++ astropy/visualization/wcsaxes/__init__.py
@@ -1,5 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+# The following few lines skip this module when running tests (and have no
+# impact otherwise)
+from ...tests.helper import pytest
+pytest.importorskip("matplotlib.pyplot")
+
 from .core import *
 from .coordinate_helpers import CoordinateHelper
 from .coordinates_map import CoordinatesMap
