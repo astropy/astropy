eceef3ff747012f861c27e91037ca57ddf8f4ce6
John Parejko <parejkoj@uw.edu>
2016-12-02 15:23:47 -0800

    Get all the future imports

--- astropy/visualization/tests/test_lupton_rgb.py
+++ astropy/visualization/tests/test_lupton_rgb.py
@@ -4,7 +4,7 @@
 Tests for RGB Images
 """
 
-from __future__ import absolute_import, division
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
 import os
