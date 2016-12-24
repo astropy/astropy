ca7d6856e4169eb491f6178d6592a523f853eb52
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2016-12-24 17:06:08 -0500

    Ensure NUMPY_LT_1_12 works for beta prerelease.

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -21,7 +21,7 @@ NUMPY_LT_1_9_1 = not minversion('numpy', '1.9.1')
 NUMPY_LT_1_10 = not minversion('numpy', '1.10.0')
 NUMPY_LT_1_10_4 = not minversion('numpy', '1.10.4')
 NUMPY_LT_1_11 = not minversion('numpy', '1.11.0')
-NUMPY_LT_1_12 = not minversion('numpy', '1.12dev')
+NUMPY_LT_1_12 = not minversion('numpy', '1.12')
 
 
 def _monkeypatch_unicode_mask_fill_values():
