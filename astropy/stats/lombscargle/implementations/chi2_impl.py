BUG: fix psd normalization for LombScargle.
Note: this only affects results for normalization='psd' with unequal errors

astropy/stats/lombscargle/implementations/chi2_impl.py
@@ -75,7 +75,7 @@ def lombscargle_chi2(t, y, dy, frequency, normalization='standard',
     p = np.array([compute_power(f) for f in frequency])
 
     if normalization == 'psd':
-        p *= 0.5 * t.size / (dy ** -2.0).sum()
+        p *= 0.5
     elif normalization == 'model':
         p /= (chi2_ref - p)
     elif normalization == 'log':

astropy/stats/lombscargle/implementations/slow_impl.py
@@ -115,7 +115,7 @@ def lombscargle_slow(t, y, dy, frequency, normalization='standard',
     elif normalization == 'log':
         p = -np.log(1 - p / YY)
     elif normalization == 'psd':
-        p *= 0.5 * t.size
+        p *= 0.5 * (dy ** -2.0).sum()
     else:
         raise ValueError("normalization='{0}' "
                          "not recognized".format(normalization))