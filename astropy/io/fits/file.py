62355a0b590befb49f5db208da1aea434757ea76
Michael Seifert <michaelseifert04@yahoo.de>
2017-02-02 05:26:16 +0100

    changed fits clobber decorators to be pending.

--- astropy/io/fits/file.py
+++ astropy/io/fits/file.py
@@ -79,7 +79,7 @@ class _File(object):
     Represents a FITS file on disk (or in some other file-like object).
     """
 
-    @deprecated_renamed_argument('clobber', 'overwrite', '1.3')
+    @deprecated_renamed_argument('clobber', 'overwrite', '1.3', pending=True)
     def __init__(self, fileobj=None, mode=None, memmap=None, overwrite=False,
                  cache=True):
         self.strict_memmap = bool(memmap)
