fix broadcasting bug in fitters

astropy/modeling/fitting.py
@@ -619,7 +619,10 @@ class LevMarLSQFitter(object):
             if z is None:
                 return [np.ravel(_) for _ in np.ravel(weights) * np.array(model.fit_deriv(x, *params))]
             else:
-                return [np.ravel(_) for _ in (np.ravel(weights) * np.array(model.fit_deriv(x, y, *params)).T).T]
+                if not model.col_fit_deriv:
+                    return [np.ravel(_) for _ in (np.ravel(weights) * np.array(model.fit_deriv(x, y, *params)).T).T]
+                else:
+                    return [np.ravel(_) for _ in (weights * np.array(model.fit_deriv(x, y, *params)))]
 
 
 class SLSQPLSQFitter(Fitter):