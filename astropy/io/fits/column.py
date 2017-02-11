143a0aa3c1492c79cd3f09ffa053e0893cd24fa0
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2017-02-11 16:14:59 -0500

    Merge pull request #5642 from bsipocz/travis_add_python36
    Adding python 3.6 builds to travis

--- astropy/io/fits/column.py
+++ astropy/io/fits/column.py
@@ -338,7 +338,7 @@ class _FormatP(str):
     # As far as I can tell from my reading of the FITS standard, a type code is
     # *required* for P and Q formats; there is no default
     _format_re_template = (r'(?P<repeat>\d+)?{}(?P<dtype>[LXBIJKAEDCM])'
-                            '(?:\((?P<max>\d*)\))?')
+                           r'(?:\((?P<max>\d*)\))?')
     _format_code = 'P'
     _format_re = re.compile(_format_re_template.format(_format_code))
     _descriptor_format = '2i4'
