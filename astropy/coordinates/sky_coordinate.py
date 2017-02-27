e2ff13cc0c743408bc48d039b64bb4ac0486c594
David Palmer <palmer@lanl.gov>
2017-02-27 10:58:28 -0700

    Redoing a loss in a previous merge

--- astropy/coordinates/sky_coordinate.py
+++ astropy/coordinates/sky_coordinate.py
@@ -515,7 +515,9 @@ class SkyCoord(ShapedLikeNDArray):
             if self.frame.name == attr:
                 raise AttributeError("'{0}' is immutable".format(attr))
 
-            if not attr.startswith('_') and hasattr(self._sky_coord_frame, attr):
+            if (attr in frame_transform_graph.frame_attrnames_set or
+                    (not attr.startswith('_') and
+                     hasattr(self._sky_coord_frame, attr))):
                 setattr(self._sky_coord_frame, attr, val)
                 return
 
