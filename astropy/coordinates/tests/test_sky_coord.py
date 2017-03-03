2a67d8dab621e936899bf811a8500b03ee0385da
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2017-03-03 10:49:41 -0500

    Add regression equivalency test for all PA, separation methods.

--- astropy/coordinates/tests/test_sky_coord.py
+++ astropy/coordinates/tests/test_sky_coord.py
@@ -601,23 +601,38 @@ def test_position_angle():
     assert cicrs.position_angle(cfk5) > 90.0 * u.deg
     assert cicrs.position_angle(cfk5) < 91.0 * u.deg
 
-    # regression check for bug in #5702
-    cfk5B1950 = SkyCoord(1*u.deg, 0*u.deg, frame='fk5', equinox='B1950')
-    # test with both default and explicit equinox #5722 and #3106
-    for cfk5J2000 in (SkyCoord(1*u.deg, 0*u.deg, frame='fk5', equinox='J2000'),
-                      SkyCoord(1 * u.deg, 0 * u.deg, frame='fk5')):
-        posang_forward = cfk5.position_angle(cfk5B1950)
-        posang_backward = cfk5B1950.position_angle(cfk5)
-        assert posang_forward.degree != 0 and posang_backward.degree != 0
-        assert 179 < (posang_forward - posang_backward).wrap_at(360*u.deg).degree < 181
-
-
 def test_position_angle_directly():
     """Regression check for #3800: position_angle should accept floats."""
     from ..angle_utilities import position_angle
     result = position_angle(10., 20., 10., 20.)
     assert result.unit is u.radian
     assert result.value == 0.
+
+
+def test_sep_pa_equivalence():
+    """Regression check for bug in #5702.
+
+    PA and separation from object 1 to 2 should be consistent with those
+    from 2 to 1
+    """
+    cfk5 = SkyCoord(1*u.deg, 0*u.deg, frame='fk5')
+    cfk5B1950 = SkyCoord(1*u.deg, 0*u.deg, frame='fk5', equinox='B1950')
+    # test with both default and explicit equinox #5722 and #3106
+    sep_forward = cfk5.separation(cfk5B1950)
+    sep_backward = cfk5B1950.separation(cfk5)
+    assert sep_forward != 0 and sep_backward != 0
+    assert_allclose(sep_forward, sep_backward)
+    posang_forward = cfk5.position_angle(cfk5B1950)
+    posang_backward = cfk5B1950.position_angle(cfk5)
+    assert posang_forward != 0 and posang_backward != 0
+    assert 179 < (posang_forward - posang_backward).wrap_at(360*u.deg).degree < 181
+    dcfk5 = SkyCoord(1*u.deg, 0*u.deg, frame='fk5', distance=1*u.pc)
+    dcfk5B1950 = SkyCoord(1*u.deg, 0*u.deg, frame='fk5', equinox='B1950',
+                          distance=1.*u.pc)
+    sep3d_forward = dcfk5.separation_3d(dcfk5B1950)
+    sep3d_backward = dcfk5B1950.separation_3d(dcfk5)
+    assert sep3d_forward != 0 and sep3d_backward != 0
+    assert_allclose(sep3d_forward, sep3d_backward)
 
 
 def test_table_to_coord():
