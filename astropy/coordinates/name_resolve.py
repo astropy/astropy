a74f4b93222560c85fc550751b88d69fca2c790b
Adrian Price-Whelan <adrianmpw@gmail.com>
2017-03-17 09:43:58 -0400

    keep track of exceptions (#1)

--- astropy/coordinates/name_resolve.py
+++ astropy/coordinates/name_resolve.py
@@ -132,24 +132,30 @@ def get_icrs_coordinates(name):
             fmt_url = fmt_url.format(name=urllib.parse.quote(name), db=db)
             urls.append(fmt_url)
 
+    exceptions = []
     for url in urls:
         try:
             # Retrieve ascii name resolve data from CDS
             resp = urllib.request.urlopen(url, timeout=data.conf.remote_timeout)
             resp_data = resp.read()
             break
-        except urllib.error.URLError:
+        except urllib.error.URLError as e:
+            exceptions.append(e)
             continue
-        except socket.timeout:
+        except socket.timeout as e:
             # There are some cases where urllib2 does not catch socket.timeout
             # especially while receiving response data on an already previously
             # working request
+            exceptions.append(e)
             continue
 
-    # All Sesame URL's timed out...
+    # All Sesame URL's failed...
     else:
-        raise NameResolveError("All Sesame queries timed out. Unable to "
-                               "retrieve coordinates.")
+        messages = ["{url}: {e.reason}".format(url=url, e=e)
+                    for url,e in zip(urls, exceptions)]
+        raise NameResolveError("All Sesame queries failed. Unable to "
+                               "retrieve coordinates. See errors per URL "
+                               "below: \n {}".format("\n".join(messages)))
 
     ra, dec = _parse_response(resp_data)
 
