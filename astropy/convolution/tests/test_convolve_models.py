9c653808ac22790bee3ecc5f713f558c554a3b13
Ze Vinicius <jvmirca@gmail.com>
2017-05-09 16:22:38 +0200

    add test fitting convolve model

--- astropy/convolution/tests/test_convolve_models.py
+++ astropy/convolution/tests/test_convolve_models.py
@@ -2,11 +2,13 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import math
 import numpy as np
 from ..convolve import convolve, convolve_models
-from ...modeling import models
+from ...modeling import models, fitting
 from ...tests.helper import pytest
-from numpy.testing import assert_allclose
+from ...utils.misc import NumpyRNGContext
+from numpy.testing import assert_allclose, assert_almost_equal
 
 try:
     import scipy
@@ -29,10 +31,10 @@ class TestConvolve1DModels(object):
         assert_allclose(ans, model_conv(x), atol=1e-5)
 
     def test_sum_of_gaussians(self):
-        '''
+        """
         Test that convolving N(a, b) with N(c, d) gives N(a + b, c + d),
         where N(.|.) stands for Gaussian probability density function.
-        '''
+        """
 
         g1 = models.Gaussian1D(1, 1, 1)
         g2 = models.Gaussian1D(1, 3, 1)
@@ -42,3 +44,22 @@ class TestConvolve1DModels(object):
         x = np.arange(-5, 6)
         assert_allclose(g3(x) / (2 * np.pi), ans(x) / np.sqrt(2 * np.pi * 2),
                         atol=1e-3)
+
+    def test_fitting_convolve_models(self):
+        """
+        test that a convolve model can be fitted
+        """
+        b1 = models.Box1D()
+        g1 = models.Gaussian1D()
+
+        x = np.linspace(-5, 5, 100)
+        fake_model = models.Gaussian1D(amplitude=10)
+        with NumpyRNGContext(123):
+            fake_data = fake_model(x) + np.random.normal(size=len(x))
+
+        init_model = convolve_models(b1, g1)
+        fitter = fitting.LevMarLSQFitter()
+        fitted_model = fitter(init_model, x, fake_data)
+
+        me = np.mean(fitted_model(x) - fake_data)
+        assert_almost_equal(1e-2, me, decimal=2)
