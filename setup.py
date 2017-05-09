5fb08e5aac472ffbd3553e9ddc3c93c81522921b
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-05-10 00:16:23 +0200

    Add pytest as a runtime dependency

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ entry_points['console_scripts'] = [
 ]
 
 setup_requires = ['numpy>=' + astropy.__minimum_numpy_version__]
-install_requires = ['numpy>=' + astropy.__minimum_numpy_version__]
+install_requires = ['pytest', 'numpy>=' + astropy.__minimum_numpy_version__]
 # Avoid installing setup_requires dependencies if the user just
 # queries for information
 if is_distutils_display_option():
