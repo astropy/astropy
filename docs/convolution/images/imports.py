fd64d3b5d194b22ee56644f18cab3283b8998f0c
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2017-05-12 15:25:19 +0200

    restructure the scripts again, removing the conveniently extracted stuff

--- docs/convolution/images/imports.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from astropy.io import fits
-from astropy.utils.data import get_pkg_data_filename
-from astropy.visualization.wcsaxes.frame import EllipticalFrame
-from astropy.convolution import Gaussian2DKernel
-from scipy.signal import convolve as scipy_convolve
-from astropy.convolution import convolve,convolve_fft
-import matplotlib.pyplot as plt
