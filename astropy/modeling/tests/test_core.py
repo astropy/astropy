c8263fdfb14a8e1e703b9961bd740099b62c1556
Nadia Dencheva <nadia.astropy@gmail.com>
2017-05-15 11:27:06 -0400

    respect the bounding_box when evaluating models

--- astropy/modeling/tests/test_core.py
+++ astropy/modeling/tests/test_core.py
@@ -106,7 +106,8 @@ def test_custom_model_signature():
     sig = signature(model_a.__init__)
     assert list(sig.parameters.keys()) == ['self', 'args', 'kwargs']
     sig = signature(model_a.__call__)
-    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis']
+    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis',
+                                           'with_bounding_box', 'fill_value']
 
     @custom_model
     def model_b(x, a=1, b=2):
@@ -118,8 +119,8 @@ def test_custom_model_signature():
     assert list(sig.parameters.keys()) == ['self', 'a', 'b', 'kwargs']
     assert [x.default for x in sig.parameters.values()] == [sig.empty, 1, 2, sig.empty]
     sig = signature(model_b.__call__)
-    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis']
-
+    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis',
+                                           'with_bounding_box', 'fill_value']
     @custom_model
     def model_c(x, y, a=1, b=2):
         return x + y + a + b
@@ -130,7 +131,8 @@ def test_custom_model_signature():
     assert list(sig.parameters.keys()) == ['self', 'a', 'b', 'kwargs']
     assert [x.default for x in sig.parameters.values()] == [sig.empty, 1, 2, sig.empty]
     sig = signature(model_c.__call__)
-    assert list(sig.parameters.keys()) == ['self', 'x', 'y', 'model_set_axis']
+    assert list(sig.parameters.keys()) == ['self', 'x', 'y', 'model_set_axis',
+                                           'with_bounding_box', 'fill_value']
 
 
 def test_custom_model_subclass():
@@ -154,7 +156,8 @@ def test_custom_model_subclass():
     sig = signature(model_b.__init__)
     assert list(sig.parameters.keys()) == ['self', 'a', 'kwargs']
     sig = signature(model_b.__call__)
-    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis']
+    assert list(sig.parameters.keys()) == ['self', 'x', 'model_set_axis',
+                                           'with_bounding_box', 'fill_value']
 
 
 def test_custom_model_parametrized_decorator():
