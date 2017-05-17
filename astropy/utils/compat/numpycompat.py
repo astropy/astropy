a238b5f0ace7bcd5cb051af7508b23c808fa5a00
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2017-05-17 14:10:45 -0400

    Do not use "style" argument in array2string for numpy>1.13.

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -15,4 +15,4 @@ __all__ = ['NUMPY_LT_1_10_4', 'NUMPY_LT_1_11',
 NUMPY_LT_1_10_4 = not minversion('numpy', '1.10.4')
 NUMPY_LT_1_11 = not minversion('numpy', '1.11.0')
 NUMPY_LT_1_12 = not minversion('numpy', '1.12')
-NUMPY_LT_1_13 = not minversion('numpy', '1.13dev')
+NUMPY_LT_1_13 = not minversion('numpy', '1.13')
