678918afd1f7690266a83d20ecce2c255b229d9b
Brigitta Sipocz <bsipocz@gmail.com>
2017-06-03 18:39:21 +0100

    Removing python2.6 workarounds

--- astropy/time/tests/test_basic.py
+++ astropy/time/tests/test_basic.py
@@ -822,12 +822,8 @@ def test_now():
     # times are more like microseconds.  But it seems safer in case some
     # platforms have slow clock calls or something.
 
-    # py < 2.7 doesn't have `total_seconds`
-    if sys.version_info[:2] < (2, 7):
-        total_secs = lambda td: (td.microseconds + (
-            td.seconds + td.days * 24 * 3600) * 10 ** 6) / 10 ** 6.
-    else:
-        total_secs = lambda td: td.total_seconds()
+    total_secs = lambda td: td.total_seconds()
+
     assert total_secs(dt) < 0.1
 
 
