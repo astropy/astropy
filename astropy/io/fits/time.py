0c8efd3759adab5bb416cf2346d02abbd1f4cb0f
AustereCuriosity <aaryapatil1996@gmail.com>
2017-06-08 15:04:29 +0530

    Time column

--- astropy/io/fits/time.py
+++ astropy/io/fits/time.py
@@ -9,12 +9,12 @@ from ...time import Time
 
 
 # Global time reference frame keywords
-TIME_KEYWORDS = ['TIMESYS', 'MJDREF', 'JDREF', 'DATEREF', 'TREFPOS', 'TIMEUNIT', 'TIMEOFFS']
+TIME_KEYWORDS = ['TIMESYS', 'MJDREF', 'JDREF', 'DATEREF', 'TREFPOS', 'TREFDIR', 'TIMEUNIT', 'TIMEOFFS']
 
 # Column-specific time keywords
-COLUMN_TIME_KEYWORDS = ['TCTYP[0-9]+']
-
-reference_date = dict('cxcsec' = ('1998-01-01 00:00:00','tt'), 'unix' = ('1970-01-01 00:00:00', 'utc'), 'gps' = ('1980-01-06 00:00:19', 'tai'))
+COLUMN_TIME_KEYWORDS = ['TCTYP[0-9]+',
+                        'TRPOS[0-9]+',
+                        'TUNIT[0-9]+']
 
 
 def is_time_keyword(keyword):
@@ -24,29 +24,21 @@ def is_time_keyword(keyword):
     return False
 
 def replace_time_table(table):
-    
-    hdr = Header()
+    '''
+    Replace Time column in a Table with a normal column containing 
+    each element as a vector of two doubles jd1 and jd2.
+    jd = jd1 + jd2 represents time in the Julian Date format with high-precision.
+    '''
+    hdr = Header([Card(keyword = 'TIMESYS', value = 'UTC', comment = 'Default time scale'), Card(keyword = 'JDREF', value = 0.0, comment = 'Time columns are jd = jd1 + jd2')])
     
     time_cols = table.columns.isinstance(Time)
     for col in time_cols:
-        if col.format is in ('cxcsec', 'unix', 'gps'):
-            if ref is not None:
-                curr_scale = col.scale
-                ref = Time(reference_date[col.format][0], scale = reference_date[col.format][1]).curr_scale
-                c = Card(keyword = 'MJDREF', value = ref.mjd)
-                hdr.append(c)
-                continue
-            
-        else:
             table[col.info.name] = Column(np.empty(col.shape + (2,)))
             table[col.info.name][...,0] = col.jd1
             table[col.info.name][...,1] = col.jd2
-            n = col.get_pos()
-            c = Card(keyword = 'TCTYP%d' %n, value = col.scale)
-            if col.format == 'jd':
-                c = Card(keyword = 'JDREF', value = 0.0)
-            elif col.format == 'mjd':
-                c = Card(keyword = 'MJDREF', vaue = 0.0)
-                
+            
+            n = table.get_pos(col.info.name)
+            hdr.append(Card(keyword = 'TCTYP%d' %n, value = col.scale))
+            hdr.append(Card(keyword = 'APyFM%d' %n, value = col.format))                
 
     return table
