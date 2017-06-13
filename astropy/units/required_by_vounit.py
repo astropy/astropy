19e5e245236e9ecaa7bcfeb21963ee5040bf7e30
Erik Tollerud <erik.tollerud@gmail.com>
2017-06-13 17:52:14 -0400

    updates from @mhvk review

--- astropy/units/required_by_vounit.py
+++ astropy/units/required_by_vounit.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
-This package defines SI prefix ed units that are required by the VOUnit standard
-but that are rarely used in practice.  E.g. ``Unit('msolMass')`` will yield
-milli-solar mass, but are in a separate module so they must be accessed as
-``astropy.units.required_by_vounit.msolMass`` instead of ``astropy.units.msolMass``.
-
-These units are in a separate modeule from `astropy.units.deprecated` because
-they need to be enabled by default for `astropy.units` to be able to compliantly
-parse VOUnit strings.
-
+This package defines SI prefixed units that are required by the VOUnit standard
+but that are rarely used in practice and liable to lead to confusion (such as
+``msolMass`` for milli-solar mass). They are in a separate module from
+`astropy.units.deprecated` because they need to be enabled by default for
+`astropy.units` to parse compliant VOUnit strings. As a result, e.g.,
+``Unit('msolMass')`` will just work, but to access the unit directly, use
+``astropy.units.required_by_vounit.msolMass`` instead of the more typical idiom
+possible for the non-prefixed unit, ``astropy.units.solMass``.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
@@ -36,8 +35,8 @@ _initialize_module()
 
 # This generates a docstring for this module that describes all of the
 # standard units defined here.
-from .utils import generate_unit_summary as _generate_unit_summary  # noqa
-from .utils import generate_prefixonly_unit_summary as _generate_prefixonly_unit_summary  # noqa
+from .utils import (generate_unit_summary as _generate_unit_summary,
+                    generate_prefixonly_unit_summary as _generate_prefixonly_unit_summary)
 if __doc__ is not None:
     __doc__ += _generate_unit_summary(globals())
     __doc__ += _generate_prefixonly_unit_summary(globals())
@@ -45,12 +44,10 @@ if __doc__ is not None:
 
 def _enable():
     """
-    Enable deprecated units so they appear in results of
+    Enable the VOUnit-required extra units so they appear in results of
     `~astropy.units.UnitBase.find_equivalent_units` and
-    `~astropy.units.UnitBase.compose`.
-
-    This may be used with the ``with`` statement to enable deprecated
-    units only temporarily.
+    `~astropy.units.UnitBase.compose`, and are recognized in the ``Unit('...')``
+    idiom.
     """
     # Local import to avoid cyclical import
     from .core import add_enabled_units
