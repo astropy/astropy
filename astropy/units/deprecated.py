a69351f51c3b9d60e12a939af4074aa5714a3fc2
Erik Tollerud <erik.tollerud@gmail.com>
2017-06-13 12:41:09 -0400

    make docstrings for prefix-only unit namespaces be useful

--- astropy/units/deprecated.py
+++ astropy/units/deprecated.py
@@ -47,6 +47,7 @@ _initialize_module()
 # This generates a docstring for this module that describes all of the
 # standard units defined here.
 from .utils import generate_unit_summary as _generate_unit_summary  # noqa
+from .utils import generate_prefixonly_unit_summary as _generate_prefixonly_unit_summary  # noqa
 if __doc__ is not None:
     __doc__ += _generate_unit_summary(globals())
 
