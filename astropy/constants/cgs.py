4086b2b541b5d4274d379d56e3a6ba700dab19a6
David Shupe <shupe@ipac.caltech.edu>
2017-06-14 11:19:22 -0700

    revamp cgs module to load constants from versioned modules

--- astropy/constants/cgs.py
+++ astropy/constants/cgs.py
@@ -6,28 +6,14 @@ for a complete listing of constants defined in Astropy.
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-# Only constants that cannot be converted directly from S.I. are defined here.
+import itertools
 
-from .constant import EMConstant
-from . import si
+from .constant import Constant
+from . import CODATA2014, IAU2015
 
-# PHYSICAL CONSTANTS
+for _nm, _c in itertools.chain(sorted(vars(CODATA2014).items()),
+                               sorted(vars(IAU2015).items())):
+    if (isinstance(_c, Constant) and _c.abbrev not in locals()
+         and _c.system in ['esu', 'gauss', 'emu']):
+        locals()[_c.abbrev] = _c
 
-# Electron charge
-
-for cver, cc in [(si.ASTROPYCONST20, si.CODATA2014),
-                    (si.ASTROPYCONST13, si.CODATA2010)]:
-    cver.e_esu = EMConstant(cc.e.abbrev, cc.e.name, cc.e.value * cc.c.value * 10.0,
-                   'statC', cc.e.uncertainty * cc.c.value * 10.0,
-                   cc.e.reference, system='esu')
-    cver.e_emu = EMConstant(cc.e.abbrev, cc.e.name, cc.e.value / 10, 'abC',
-                   cc.e.uncertainty / 10, cc.e.reference, system='emu')
-    cver.e_gauss = EMConstant(cc.e.abbrev, cc.e.name, cc.e.value * cc.c.value * 10.0,
-                     'Fr', cc.e.uncertainty * cc.c.value * 10.0,
-                     cc.e.reference, system='gauss')
-
-e_esu = si.ASTROPYCONST20.e_esu
-
-e_emu = si.ASTROPYCONST20.e_emu
-
-e_gauss = si.ASTROPYCONST20.e_gauss
