Added test of SmoothlyBrokenPowerLaw with units and fix resulting issues.

astropy/modeling/powerlaws.py
@@ -13,7 +13,7 @@ import numpy as np
 
 from .core import Fittable1DModel
 from .parameters import Parameter, InputParameterError
-
+from ..units import Quantity
 
 __all__ = ['PowerLaw1D', 'BrokenPowerLaw1D', 'SmoothlyBrokenPowerLaw1D',
            'ExponentialCutoffPowerLaw1D', 'LogParabola1D']
@@ -271,7 +271,13 @@ class SmoothlyBrokenPowerLaw1D(Fittable1DModel):
         xx = x / x_break
 
         #Initialize the return value
-        f = np.zeros_like(xx)
+        f = np.zeros_like(xx, subok=False)
+
+        if isinstance(amplitude, Quantity):
+            return_unit = amplitude.unit
+            amplitude = amplitude.value
+        else:
+            return_unit = None
 
         #The quantity `t = (x / x_b)^(1 / delta)` can become quite
         #large.  To avoid overflow errors we will start by calculating
@@ -308,7 +314,10 @@ class SmoothlyBrokenPowerLaw1D(Fittable1DModel):
             f[i] = amplitude * xx[i] ** (-alpha_1) \
                    * r ** ((alpha_1 - alpha_2) * delta)
 
-        return f
+        if return_unit:
+            return Quantity(f, unit=return_unit, copy=False)
+        else:
+            return f
 
     @staticmethod
     def fit_deriv(x, amplitude, x_break, alpha_1, alpha_2, delta):
@@ -366,6 +375,17 @@ class SmoothlyBrokenPowerLaw1D(Fittable1DModel):
                          * (np.log(r) - t / (1. + t) / delta * np.log(xx[i]))
 
         return [d_amplitude, d_x_break, d_alpha_1, d_alpha_2, d_delta]
+
+    @property
+    def input_units(self):
+        if self.x_break.unit is None:
+            return None
+        else:
+            return {'x': self.x_break.unit}
+
+    def _parameter_units_for_data_units(self, inputs_unit, outputs_unit):
+        return OrderedDict([('x_break', inputs_unit['x']),
+                            ('amplitude', outputs_unit['y'])])
 
 
 class ExponentialCutoffPowerLaw1D(Fittable1DModel):

astropy/modeling/tests/test_functional_models_quantities.py
@@ -128,7 +128,8 @@ FUNC_MODELS_2D = [
  'bounding_box': False},
 {'class': Sersic2D,
  'parameters': {'amplitude': 3 * u.MJy / u.sr, 'x_0': 1 * u.arcsec,
-                'y_0': 2 * u.arcsec, 'r_eff': 2 * u.arcsec, 'n': 4},
+                'y_0': 2 * u.arcsec, 'r_eff': 2 * u.arcsec, 'n': 4,
+                'ellip': 0, 'theta': 0},
  'evaluation':[(3 * u.arcsec, 2.5 * u.arcsec, 2.829990489 * u.MJy/u.sr)],
  'bounding_box': False},
 ]
@@ -142,10 +143,10 @@ POWERLAW_MODELS = [
  'parameters': {'amplitude': 5 * u.kg, 'x_break': 10 * u.cm, 'alpha_1': 1, 'alpha_2': -1},
  'evaluation':[(1 * u.m, 50 * u.kg), (1 * u.cm, 50 * u.kg)],
  'bounding_box': False},
-# {'class': SmoothlyBrokenPowerLaw1D,
-#  'parameters': {'amplitude': 5 * u.kg, 'log_break': 10 * u.cm, 'alpha_1': 1, 'alpha_2': -1},
-#  'evaluation':[(1 * u.m, 50 * u.kg), (1 * u.cm, 50 * u.kg)],
-#  'bounding_box': False},
+{'class': SmoothlyBrokenPowerLaw1D,
+ 'parameters': {'amplitude': 5 * u.kg, 'x_break': 10 * u.cm, 'alpha_1': 1, 'alpha_2': -1, 'delta':1},
+ 'evaluation':[(1 * u.m, 15.125 * u.kg), (1 * u.cm, 15.125 * u.kg)],
+ 'bounding_box': False},
 {'class': ExponentialCutoffPowerLaw1D,
  'parameters': {'amplitude': 5 * u.kg, 'x_0': 10 * u.cm, 'alpha': 1, 'x_cutoff': 1 * u.m},
  'evaluation':[(1 * u.um, 499999.5 * u.kg), (10 * u.m, 50 * np.exp(-10) * u.g)],
@@ -157,7 +158,7 @@ POWERLAW_MODELS = [
 ]
 
 
-MODELS = MODELS_1D + MODELS_2D + POWERLAW_MODELS
+MODELS = FUNC_MODELS_1D + FUNC_MODELS_2D + POWERLAW_MODELS
 
 SCIPY_MODELS = set([Sersic1D, Sersic2D, AiryDisk2D])
 
@@ -222,7 +223,10 @@ def test_models_evaluate_with_units_param_array(model):
         else:
             params[key] = np.repeat(value, 2)
 
+    params['n_models'] = 2
+
     m = model['class'](**params)
+
     for args in model['evaluation']:
         if len(args) == 2:
             x, y = args