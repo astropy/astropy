240335a22c43d0a610ee3722c913acaf95f65cd5
David Shupe <shupe@ipac.caltech.edu>
2017-06-14 11:59:41 -0700

    add the ASTROPYCONST<version> modules

---
+++ astropy/constants/ASTROPYCONST20.py
@@ -0,0 +1,20 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Astronomical and physics constants for Astropy v2.0.  See :mod:`astropy.constants`
+for a complete listing of constants defined in Astropy.
+"""
+
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import itertools
+
+from .constant import Constant
+from . import CODATA2014, IAU2015
+
+for _nm, _c in itertools.chain(sorted(vars(CODATA2014).items()),
+                               sorted(vars(IAU2015).items())):
+    if (isinstance(_c, Constant) and _c.abbrev not in locals()):
+        locals()[_c.abbrev] = _c
+
