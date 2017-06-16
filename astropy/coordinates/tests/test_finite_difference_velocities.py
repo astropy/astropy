44e50d3b05aae77594c07ec57288205e239abd18
Erik Tollerud <erik.tollerud@gmail.com>
2017-06-16 03:41:44 -0400

    implement more complete set of tests

--- astropy/coordinates/tests/test_finite_difference_velocities.py
+++ astropy/coordinates/tests/test_finite_difference_velocities.py
@@ -9,12 +9,14 @@ import numpy as np
 from ...tests.helper import quantity_allclose
 
 from ... import units as u
+from ... import constants
 from ...time import Time
 from ..builtin_frames import ICRS, AltAz, LSR, GCRS
 from ..baseframe import frame_transform_graph
 from .. import (EarthLocation, TimeFrameAttribute,
                 FunctionTransformWithFiniteDifference, get_sun,
-                CartesianRepresentation, SphericalRepresentation)
+                CartesianRepresentation, SphericalRepresentation,
+                CartesianDifferential)
 
 J2000 = Time('J2000')
 
@@ -95,3 +97,75 @@ def test_gcrs_diffs():
     assert np.all(suni2.data.differentials[0].d_xyz < 1e-5*u.km/u.s)
     qtrisun2 = qtrsung.transform_to(ICRS)
     assert np.all(qtrisun2.data.differentials[0].d_xyz < 1e-5*u.km/u.s)
+
+def test_altaz_diffs():
+    time = Time('J2015') + np.linspace(-1, 1, 1000)*u.day
+    loc = EarthLocation.of_site('greenwich')  #built-in
+    aa = AltAz(obstime=time, location=loc)
+
+    icoo = ICRS(np.zeros_like(time)*u.deg, 10*u.deg, 100*u.au,
+         pm_ra=np.zeros_like(time)*u.marcsec/u.yr, pm_dec=0*u.marcsec/u.yr,
+         radial_velocity=0*u.km/u.s)
+
+    acoo = icoo.transform_to(aa)
+
+    # make sure the change in radial velocity over ~2 days isn't much more than
+    # the rotation speed of the Earth
+    assert np.ptp(acoo.radial_velocity)/2 < (2*np.pi*constants.R_earth/u.day)*1.2
+
+    cdiff = acoo.data.differentials[0].represent_as(CartesianDifferential,
+                                                    acoo.data)
+
+    # The "total" velocity should be > c, because the *tangential* velocity
+    # isn't a True velocity, but rather an induced velocity due to the Earth's
+    # rotation at a distance of 100 AU
+    assert np.all(np.sum(cdiff.d_xyz**2, axis=0)**0.5 > constants.c)
+
+_xfail = pytest.mark.xfail
+@pytest.mark.parametrize('distance', [1000*u.au,
+                                      10*u.pc,
+                                      pytest.param(10*u.kpc, marks=_xfail),
+                                      pytest.param(100*u.kpc, marks=_xfail)])
+def test_numerical_limits(distance):
+    """
+    Tests the numerical stability of the default settings for the finite
+    difference transformation calculation.  This is *known* to fail for at
+    >~1kpc, but this may be improved in future versions.
+    """
+    time = Time('J2017') + np.linspace(-.5, .5, 100)*u.year
+
+    icoo = ICRS(ra=0*u.deg, dec=10*u.deg, distance=distance,
+                pm_ra=0*u.marcsec/u.yr, pm_dec=0*u.marcsec/u.yr,
+                radial_velocity=0*u.km/u.s)
+    gcoo = icoo.transform_to(GCRS(obstime=time))
+    rv =  gcoo.radial_velocity.to('km/s')
+
+    # if its a lot bigger than this, finite-difference noise has ruined things
+    assert np.ptp(rv) < 65*u.km/u.s
+
+def diff_info_plot(frame, times):
+    """
+    Useful for plotting a frame with multiple times. *Not* used in the testing
+    suite per se, but extremely useful for interactive plotting of results from
+    tests in this module.
+    """
+    from matplotlib import pyplot as plt
+
+    fig, ((ax1, ax2), (ax3,ax4)) = plt.subplots( 2, 2, figsize=(20, 12))
+    ax1.plot_date(time.plot_date, frame.data.differentials[0].d_xyz.to(u.km/u.s).T, fmt='-')
+    ax1.legend(['x', 'y', 'z'])
+
+    ax2.plot_date(time.plot_date, np.sum(frame.data.differentials[0].d_xyz.to(u.km/u.s)**2,axis=0)**0.5, fmt='-')
+    ax2.set_title('total')
+
+
+    sd = frame.data.differentials[0].represent_as(SphericalDifferential, frame.data)
+
+    ax3.plot_date(time.plot_date, sd.d_distance.to(u.km/u.s), fmt='-')
+    ax3.set_title('radial')
+
+
+    ax4.plot_date(time.plot_date, sd.d_lat.to(u.marcsec/u.yr), fmt='-', label='lat')
+    ax4.plot_date(time.plot_date, sd.d_lon.to(u.marcsec/u.yr), fmt='-', label='lon')
+
+    return fig
