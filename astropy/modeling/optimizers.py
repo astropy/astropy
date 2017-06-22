f6c6b4174b55c4c43336ddb6c22304c792ee24bd
Pey Lian Lim <lim@stsci.edu>
2017-06-22 00:25:45 -0400

    Another attempt to get rid of R115 warning.

--- astropy/modeling/optimizers.py
+++ astropy/modeling/optimizers.py
@@ -178,14 +178,14 @@ class SLSQP(Optimization):
 
 class Simplex(Optimization):
     """
-    Neald-Mead (downhill simplex) algorithm [1]_.
+    Neald-Mead (downhill simplex) algorithm [2]_.
 
     This algorithm only uses function values, not derivatives.
     Uses `scipy.optimize.fmin`.
 
     References
     ----------
-    .. [1] Nelder, J.A. and Mead, R. (1965), "A simplex method for function
+    .. [2] Nelder, J.A. and Mead, R. (1965), "A simplex method for function
        minimization", The Computer Journal, 7, pp. 308-313
     """
 
