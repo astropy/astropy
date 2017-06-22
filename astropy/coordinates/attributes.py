4d9f50ad6eb1663fecd06b3ec967bdb815144a96
Tom Aldcroft <taldcroft@gmail.com>
2017-06-22 17:58:56 -0400

    Only set TimeFrame not writeable for non-scalar values

--- astropy/coordinates/attributes.py
+++ astropy/coordinates/attributes.py
@@ -186,7 +186,10 @@ class TimeAttribute(Attribute):
                                                                value, err))
             converted = True
 
-        out.writeable = False
+        # Set attribute as read-only for arrays (not allowed by numpy
+        # for array scalars)
+        if out.shape:
+            out.writeable = False
         return out, converted
 
 
