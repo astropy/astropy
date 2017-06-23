dfa0008334254336a53ab0d8ad72355c2afadee9
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2017-06-23 19:18:50 +0200

    Test angle mixins in table operations.

--- astropy/table/tests/conftest.py
+++ astropy/table/tests/conftest.py
@@ -139,6 +139,9 @@ def table_type(request):
 # Stuff for testing mixin columns
 
 MIXIN_COLS = {'quantity': [0, 1, 2, 3] * u.m,
+              'longitude' : coordinates.Longitude([0., 1., 5., 6.]*u.deg,
+                                                  wrap_angle=180.*u.deg),
+              'latitude' : coordinates.Latitude([5., 6., 10., 11.]*u.deg),
               'time': time.Time([2000, 2001, 2002, 2003], format='jyear'),
               'skycoord': coordinates.SkyCoord(ra=[0, 1, 2, 3] * u.deg,
                                                dec=[0, 1, 2, 3] * u.deg),
