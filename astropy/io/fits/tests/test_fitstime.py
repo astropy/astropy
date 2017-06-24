c24ea496a0a4a20faea0f5df3b500e326e3f15c6
AustereCuriosity <aaryapatil1996@gmail.com>
2017-06-24 15:54:36 +0530

    HIERARCH FORMAT [ci skip]

--- astropy/io/fits/tests/test_fitstime.py
+++ astropy/io/fits/tests/test_fitstime.py
@@ -4,30 +4,30 @@ import pytest
 
 from . import FitsTestCase
 
-from ..fits_time import FitsTime
+from ..fitstime import FITS_time
 from ....coordinates import EarthLocation
 from ....table import Table, QTable
 from ....time import Time
 from ....tests.helper import catch_warnings
 
 
-class TestFitsTime(FitsTestCase):
+class Test_FITS_time(FitsTestCase):
 
     def setup_class(self):
         self.data = ['1999-01-01T00:00:00.123456789', '2010-01-01T00:00:00']
 
     def test_is_time_column_keyword(self):
         # Time column keyword without column number
-        assert FitsTime.is_time_column_keyword('TRPOS') == False
+        assert FITS_time.is_time_column_keyword('TRPOS') == False
 
         # Global time column keyword
-        assert FitsTime.is_time_column_keyword('TIMESYS') == False
+        assert FITS_time.is_time_column_keyword('TIMESYS') == False
 
     def test_set_time(self):
         """
         Test that setting illegal time keywords fails.
         """
-        ft = FitsTime()
+        ft = FITS_time()
 
         with pytest.raises(ValueError) as err:
             ft.set_global_time('TRPOS7', 8)
@@ -50,7 +50,7 @@ class TestFitsTime(FitsTestCase):
         t['a'].location = EarthLocation([1,2], [2,3], [3,4,])
 
         with pytest.raises(ValueError) as err:
-            table, hdr = FitsTime.replace_time_table(t)
+            table, hdr = FITS_time.replace_time_table(t)
         assert 'Vectorized Location of Time Column' in str(err.value)
 
         # Check that multiple Time columns with different locations raise an exception
@@ -58,14 +58,14 @@ class TestFitsTime(FitsTestCase):
         t['b'].location = EarthLocation(2, 3, 4)
 
         with pytest.raises(ValueError) as err:
-            table, hdr = FitsTime.replace_time_table(t)
+            table, hdr = FITS_time.replace_time_table(t)
             assert 'Multiple Time Columns with different geocentric' in str(err.value)
 
         # Check that Time column with no location specified will assume global location
         t['b'].location = None
 
         with catch_warnings() as w:
-            table, hdr = FitsTime.replace_time_table(t)
+            table, hdr = FITS_time.replace_time_table(t)
             assert len(w) == 1
             assert str(w[0].message).startswith('Time Column "b" has no specified location,'
                                                 ' but global Time Position is present')
@@ -74,19 +74,19 @@ class TestFitsTime(FitsTestCase):
         t['b'].location = EarthLocation(1, 2, 3)
 
         with catch_warnings() as w:
-            table, hdr = FitsTime.replace_time_table(t)
+            table, hdr = FITS_time.replace_time_table(t)
             assert len(w) == 0
 
         # Check compatibility of Time Scales and Reference Positions
         t['b'].location = EarthLocation(2, 3, 4)
 
-        time_ref = FitsTime.TIME_SCALE_REF
+        time_ref = FITS_time.TIME_SCALE_REF
         uncomp_scales = [scale for scale in time_ref if time_ref[scale]!='TOPOCENTER']
 
         for scale in uncomp_scales:
             t.replace_column('a', getattr(t['a'], scale))
             with catch_warnings() as w:
-                table, hdr = FitsTime.replace_time_table(t)
+                table, hdr = FITS_time.replace_time_table(t)
                 assert len(w) == 1
                 assert str(w[0].message).startswith('Location cannot be written for "a"'
                                                     ' due to incompatability')
@@ -106,15 +106,15 @@ class TestFitsTime(FitsTestCase):
                          'OBSGEO-Y' : t['a'].location.y.value,
                          'OBSGEO-Z' : t['a'].location.z.value,
                          'TCTYP1' : t['a'].scale.upper(),
-                         'TCAPF1' : t['a'].format.upper(),
-                         'TRPOS1' : FitsTime.TIME_SCALE_REF[t['a'].scale],
+                         'HIERARCH ASTROPY TIME FORMAT1' : t['a'].format.upper(),
+                         'TRPOS1' : FITS_time.TIME_SCALE_REF[t['a'].scale],
                          'TCTYP2' : t['b'].scale.upper(),
-                         'TCAPF2' : t['b'].format.upper()}
+                         'HIERARCH ASTROPY TIME FORMAT2' : t['b'].format.upper()}
    
-        table, hdr = FitsTime.replace_time_table(t)
+        table, hdr = FITS_time.replace_time_table(t)
 
         # Check global time keywords
-        for key, value in FitsTime.GLOBAL_TIME_INFO.items():
+        for key, value in FITS_time.GLOBAL_TIME_INFO.items():
              assert hdr[key] == value[0]
              assert hdr.comments[key] == value[1]
              hdr.remove(key)
