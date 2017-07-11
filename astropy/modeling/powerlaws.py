a65d659454ffd2ce56dfd61f1b4bcb0de18500b5
sjhannon <shann004@ucr.edu>
2017-07-11 16:29:22 -0700

    Update ExponentialCutoffPowerLaw1D
    Update description to include Schecter function alias.

--- astropy/modeling/powerlaws.py
+++ astropy/modeling/powerlaws.py
@@ -390,7 +390,8 @@ class SmoothlyBrokenPowerLaw1D(Fittable1DModel):
 
 class ExponentialCutoffPowerLaw1D(Fittable1DModel):
     """
-    One dimensional power law model with an exponential cutoff (also referred to as the Schecter function).
+    One dimensional power law model with an exponential cutoff, also 
+    referred to as the Schecter function.
 
     Parameters
     ----------
