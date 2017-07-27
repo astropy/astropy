41f2e820439dd7b38ede16320b4641e234caee02
Erik Tollerud <erik.tollerud@gmail.com>
2017-07-27 12:23:58 -0400

    Switch to backup image object with repr

--- astropy/table/pandas.py
+++ astropy/table/pandas.py
@@ -1,11 +1,22 @@
 ascii_coded = 'Ò♙♙♙♙♙♙♙♙♌♐♐♌♙♙♙♙♙♙♌♌♙♙Ò♙♙♙♙♙♙♙♘♐♐♐♈♙♙♙♙♙♌♐♐♐♔Ò♙♙♌♈♙♙♌♐♈♈♙♙♙♙♙♙♙♙♈♐♐♙Ò♙♐♙♙♙♐♐♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙Ò♐♔♙♙♘♐♐♙♙♌♐♐♔♙♙♌♌♌♙♙♙♌Ò♐♐♙♙♘♐♐♌♙♈♐♈♙♙♙♈♐♐♙♙♘♔Ò♐♐♌♙♘♐♐♐♌♌♙♙♌♌♌♙♈♈♙♌♐♐Ò♘♐♐♐♌♐♐♐♐♐♐♌♙♈♙♌♐♐♐♐♐♔Ò♘♐♐♐♐♐♐♐♐♐♐♐♐♈♈♐♐♐♐♐♐♙Ò♙♘♐♐♐♐♈♐♐♐♐♐♐♙♙♐♐♐♐♐♙♙Ò♙♙♙♈♈♈♙♙♐♐♐♐♐♔♙♐♐♐♐♈♙♙Ò♙♙♙♙♙♙♙♙♙♈♈♐♐♐♙♈♈♈♙♙♙♙Ò'
 ascii_uncoded = ''.join([chr(ord(c)-200) for c in ascii_coded])
+url = 'https://media.giphy.com/media/e24Q8FKE2mxRS/giphy.gif'
 
 try:
     from IPython import display
-    im = display.Image(url='https://media.giphy.com/media/e24Q8FKE2mxRS/giphy.gif')
+
+    class ImageWithBackup(display.Image):
+        def __init__(self, url, backup_text):
+            super().__init__(url=url)
+            self.backup_text = backup_text
+        def __repr__(self):
+            if self.backup_text is None:
+                return super().__repr__()
+            else:
+                return self.backup_text
+
+
+    im = ImageWithBackup(url, ascii_uncoded)
     display.display(im)
 except ImportError:
-    pass
-
-print(ascii_uncoded)
+    print(ascii_uncoded)
