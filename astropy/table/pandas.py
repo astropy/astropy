85131d8b541bd2a9632dc3063450e27de04aa2ad
Erik Tollerud <erik.tollerud@gmail.com>
2017-07-28 15:30:11 -0400

    fix coding and py 2.x to make sure tests are happy

--- astropy/table/pandas.py
+++ astropy/table/pandas.py
@@ -1,27 +1,37 @@
-ascii_coded = 'Ò♙♙♙♙♙♙♙♙♌♐♐♌♙♙♙♙♙♙♌♌♙♙Ò♙♙♙♙♙♙♙♘♐♐♐♈♙♙♙♙♙♌♐♐♐♔Ò♙♙♌♈♙♙♌♐♈♈♙♙♙♙♙♙♙♙♈♐♐♙Ò♙♐♙♙♙♐♐♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙Ò♐♔♙♙♘♐♐♙♙♌♐♐♔♙♙♌♌♌♙♙♙♌Ò♐♐♙♙♘♐♐♌♙♈♐♈♙♙♙♈♐♐♙♙♘♔Ò♐♐♌♙♘♐♐♐♌♌♙♙♌♌♌♙♈♈♙♌♐♐Ò♘♐♐♐♌♐♐♐♐♐♐♌♙♈♙♌♐♐♐♐♐♔Ò♘♐♐♐♐♐♐♐♐♐♐♐♐♈♈♐♐♐♐♐♐♙Ò♙♘♐♐♐♐♈♐♐♐♐♐♐♙♙♐♐♐♐♐♙♙Ò♙♙♙♈♈♈♙♙♐♐♐♐♐♔♙♐♐♐♐♈♙♙Ò♙♙♙♙♙♙♙♙♙♈♈♐♐♐♙♈♈♈♙♙♙♙Ò'
-ascii_uncoded = ''.join([chr(ord(c)-200) for c in ascii_coded])
-url = 'https://media.giphy.com/media/e24Q8FKE2mxRS/giphy.gif'
-message_coded = 'ĘĩĶĬĩĻ÷ĜĩĪĴĭèıĶļĭĺĩīļıķĶ'
-message_uncoded = ''.join([chr(ord(c)-200) for c in message_coded])
+# -*- coding: utf-8 -*-
 
 try:
-    from IPython import display
+    ascii_coded = 'Ò♙♙♙♙♙♙♙♙♌♐♐♌♙♙♙♙♙♙♌♌♙♙Ò♙♙♙♙♙♙♙♘♐♐♐♈♙♙♙♙♙♌♐♐♐♔Ò♙♙♌♈♙♙♌♐♈♈♙♙♙♙♙♙♙♙♈♐♐♙Ò♙♐♙♙♙♐♐♙♙♙♙♙♙♙♙♙♙♙♙♙♙♙Ò♐♔♙♙♘♐♐♙♙♌♐♐♔♙♙♌♌♌♙♙♙♌Ò♐♐♙♙♘♐♐♌♙♈♐♈♙♙♙♈♐♐♙♙♘♔Ò♐♐♌♙♘♐♐♐♌♌♙♙♌♌♌♙♈♈♙♌♐♐Ò♘♐♐♐♌♐♐♐♐♐♐♌♙♈♙♌♐♐♐♐♐♔Ò♘♐♐♐♐♐♐♐♐♐♐♐♐♈♈♐♐♐♐♐♐♙Ò♙♘♐♐♐♐♈♐♐♐♐♐♐♙♙♐♐♐♐♐♙♙Ò♙♙♙♈♈♈♙♙♐♐♐♐♐♔♙♐♐♐♐♈♙♙Ò♙♙♙♙♙♙♙♙♙♈♈♐♐♐♙♈♈♈♙♙♙♙Ò'
+    ascii_uncoded = ''.join([chr(ord(c)-200) for c in ascii_coded])
+    url = 'https://media.giphy.com/media/e24Q8FKE2mxRS/giphy.gif'
+    message_coded = 'ĘĩĶĬĩĻ÷ĜĩĪĴĭèıĶļĭĺĩīļıķĶ'
+    message_uncoded = ''.join([chr(ord(c)-200) for c in message_coded])
 
-    html = display.Image(url=url)._repr_html_()
-    message = 'Pandas/Table interaction'
+    try:
+        from IPython import display
 
-    class HTMLWithBackup(display.HTML):
-        def __init__(self, data, backup_text):
-            super().__init__(data)
-            self.backup_text = backup_text
-        def __repr__(self):
-            if self.backup_text is None:
-                return super().__repr__()
-            else:
-                return self.backup_text
+        html = display.Image(url=url)._repr_html_()
+        message = 'Pandas/Table interaction'
+
+        class HTMLWithBackup(display.HTML):
+            def __init__(self, data, backup_text):
+                super().__init__(data)
+                self.backup_text = backup_text
+            def __repr__(self):
+                if self.backup_text is None:
+                    return super().__repr__()
+                else:
+                    return self.backup_text
 
 
-    dhtml = HTMLWithBackup(message_uncoded + html, ascii_uncoded)
-    display.display(dhtml)
-except ImportError:
-    print(ascii_uncoded)
+        dhtml = HTMLWithBackup(message_uncoded + html, ascii_uncoded)
+        display.display(dhtml)
+    except ImportError:
+        print(ascii_uncoded)
+except:
+    # suppress all exceptions for py 2.x - can remove this outer try-accept when
+    # Astropy 3.0 is actually released... this code isn't easy to make py 2.x
+    # compliant but it doesn't matter since 3.0 is coming soon.
+    from sys import version_info
+    if version_info.major > 2:
+        raise
