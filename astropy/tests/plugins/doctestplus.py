df745a7547b9a439b19f96a829b39b57b680a1d0
Dan D'Avella <ddavella@stsci.edu>
2017-08-11 09:51:51 -0400

    Update documentation headers for pytest plugins

--- astropy/tests/plugins/doctestplus.py
+++ astropy/tests/plugins/doctestplus.py
@@ -1,3 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+This plugin provides advanced doctest support and enables the testing of .rst
+files.
+"""
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
