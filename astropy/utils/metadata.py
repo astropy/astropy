Fix bug stacking unicode columns: width grows by factor of 4

astropy/utils/metadata.py
@@ -4,6 +4,7 @@ This module contains helper functions and classes for handling metadata.
 """
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
 from ..extern import six
 from ..utils import wraps
 
@@ -15,6 +16,7 @@ from copy import deepcopy
 
 import numpy as np
 from ..utils.exceptions import AstropyWarning
+from ..utils.misc import dtype_bits_or_chars
 
 
 __all__ = ['MergeConflictError', 'MergeConflictWarning', 'MERGE_STRATEGIES',
@@ -65,7 +67,8 @@ def common_dtype(arrs):
     # values or the final arr_common = .. step is unpredictable.
     for i, arr in enumerate(arrs):
         if arr.dtype.kind in ('S', 'U'):
-            arrs[i] = [(u'0' if arr.dtype.kind == 'U' else b'0') * arr.itemsize]
+            arrs[i] = [(u'0' if arr.dtype.kind == 'U' else b'0') *
+                       dtype_bits_or_chars(arr.dtype)]
 
     arr_common = np.array([arr[0] for arr in arrs])
     return arr_common.dtype.str

astropy/utils/tests/test_metadata.py
@@ -3,8 +3,10 @@ import abc
 from collections import OrderedDict
 
 import pytest
+import numpy as np
 
 from ..metadata import MetaData, MergeConflictError, merge, enable_merge_strategies
+from ..metadata import common_dtype
 from ...utils import metadata
 from ...io import fits
 
@@ -83,9 +85,6 @@ def test_metadata_merging_conflict_exception():
     data2.meta['somekey'] = {'x': 1, 'y': 999}
     with pytest.raises(MergeConflictError):
         merge(data1.meta, data2.meta, metadata_conflicts='error')
-
-
-import numpy as np
 
 
 def test_metadata_merging():
@@ -189,3 +188,13 @@ def test_metadata_merging_new_strategy():
     assert not MergeConcatStrings.enabled
 
     metadata.MERGE_STRATEGIES = original_merge_strategies
+
+
+def test_common_dtype_string():
+    u3 = np.array([u'123'])
+    u4 = np.array([u'1234'])
+    b3 = np.array([b'123'])
+    b5 = np.array([b'12345'])
+    assert common_dtype([u3, u4]).endswith('U4')
+    assert common_dtype([b5, u4]).endswith('U5')
+    assert common_dtype([b3, b5]).endswith('S5')