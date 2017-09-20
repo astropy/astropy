cc70f98ff99c7226cf5d3fbfb635f0afa740f21c
Dan D'Avella <drdavella@gmail.com>
2017-09-19 20:40:25 -0400

    Move ASDF implementation of Time to astropy as test case...
    This is the first attempt to move astropy-specific tag implementations
    from ASDF proper to astropy as a proof-of-concept. More to come soon.

---
+++ astropy/io/asdf/tags/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+# TODO: Import entire tree automatically and make these work like "plugins"?
+
+#from . import unit
+from . import time
+#from . import wcs
---
+++ astropy/io/asdf/tags/time/__init__.py
@@ -0,0 +1,4 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from .time import TimeType
