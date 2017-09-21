44e5d2596b249d3ee9038aa568dd6489bd43d6b7
Simon Conseil <contact@saimon.org>
2017-09-22 00:11:22 +0200

    Remove the _funcsigs compat module

--- astropy/utils/compat/funcsigs.py
+++ astropy/utils/compat/funcsigs.py
@@ -1,6 +1,3 @@
-try:
-    from inspect import signature, Parameter, Signature, BoundArguments
-except ImportError:
-    from ._funcsigs import signature, Parameter, Signature, BoundArguments
+from inspect import signature, Parameter, Signature, BoundArguments
 
 __all__ = ['BoundArguments', 'Parameter', 'Signature', 'signature']
