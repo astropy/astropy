23d25dd150942e8b1072cb0612c3a1aa876e7748
Simon Conseil <contact@saimon.org>
2017-09-21 11:49:22 +0200

    Update CircleCI to run on python 3

--- .run_docker_tests.sh
+++ .run_docker_tests.sh
@@ -5,7 +5,7 @@ docker info
 cat << EOF | docker run -i \
                         -v ${PWD}:/astropy_src \
                         -a stdin -a stdout -a stderr \
-                        astropy/astropy-32bit-test-env:1.9 \
+                        astropy/astropy-py35-32bit-test-env:1.9 \
                         bash || exit $?
 
 cd /astropy_src
@@ -14,12 +14,15 @@ echo "Output of uname -m:"
 uname -m
 
 echo "Output of sys.maxsize in Python:"
-python -c 'import sys; print(sys.maxsize)'
+python3 -c 'import sys; print(sys.maxsize)'
 
 # The doctestplus plugin in Astropy doesn't work correctly with pytest>=3.2, so
 # we install an older version here
-easy_install "pytest<3.2" pytest-xdist
+easy_install-3.5 pytest pytest-xdist
 
-python setup.py test --parallel=4
+# Install git
+apt-get install -y git
+
+python3 setup.py test --parallel=4
 
 EOF
