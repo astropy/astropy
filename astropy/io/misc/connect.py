afc5ba6940cd7d5fb25a401ec6301ed2d1fd3e56
Pey Lian Lim <lim@stsci.edu>
2017-09-25 14:44:16 -0400

    Rearranged import order in io.misc.connect

--- astropy/io/misc/connect.py
+++ astropy/io/misc/connect.py
@@ -2,11 +2,10 @@
 # This file connects any readers/writers defined in io.misc to the
 # astropy.table.Table class
 
-
-from .hdf5 import read_table_hdf5, write_table_hdf5, is_hdf5
-
 from .. import registry as io_registry
 from ...table import Table
+
+from .hdf5 import read_table_hdf5, write_table_hdf5, is_hdf5
 
 io_registry.register_reader('hdf5', Table, read_table_hdf5)
 io_registry.register_writer('hdf5', Table, write_table_hdf5)
