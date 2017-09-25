2cd177d59638ed1c8d9859ce3076e41c899767d7
Simon Conseil <contact@saimon.org>
2017-09-25 12:51:13 +0200

    Add deprecation warnings

--- astropy/utils/compat/funcsigs.py
+++ astropy/utils/compat/funcsigs.py
@@ -1,3 +1,9 @@
 from inspect import signature, Parameter, Signature, BoundArguments
 
 __all__ = ['BoundArguments', 'Parameter', 'Signature', 'signature']
+
+import warnings
+from ..exceptions import AstropyDeprecationWarning
+
+warnings.warn("astropy.utils.compat.funcsigs is now deprecated - "
+              "use inspect instead", AstropyDeprecationWarning)
--- astropy/utils/compat/futures/__init__.py
+++ astropy/utils/compat/futures/__init__.py
@@ -1,1 +1,7 @@
 from concurrent.futures import *
+
+import warnings
+from ...exceptions import AstropyDeprecationWarning
+
+warnings.warn("astropy.utils.compat.futures is now deprecated - "
+              "use concurrent.futures instead", AstropyDeprecationWarning)
