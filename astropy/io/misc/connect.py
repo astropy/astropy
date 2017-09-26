145e0ed6abae8fc59a1d8be67d141e82b5c176bd
Pey Lian Lim <lim@stsci.edu>
2017-09-26 16:14:58 -0400

    Fix HDF5 connect import errors

--- astropy/io/misc/connect.py
+++ astropy/io/misc/connect.py
@@ -2,11 +2,6 @@
 # This file connects any readers/writers defined in io.misc to the
 # astropy.table.Table class
 
-from .. import registry as io_registry
-from ...table import Table
+from . import hdf5
 
-from .hdf5 import read_table_hdf5, write_table_hdf5, is_hdf5
-
-io_registry.register_reader('hdf5', Table, read_table_hdf5)
-io_registry.register_writer('hdf5', Table, write_table_hdf5)
-io_registry.register_identifier('hdf5', Table, is_hdf5)
+hdf5.register_hdf5()
