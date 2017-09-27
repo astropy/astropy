fc9e824697b411394cb2ac6792227ead78ab4703
Simon Conseil <contact@saimon.org>
2017-09-27 20:15:29 +0200

    Rework and enhance fitsheader tests

--- astropy/io/fits/tests/test_fitsheader.py
+++ astropy/io/fits/tests/test_fitsheader.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import pytest
-import re
 
 from . import FitsTestCase
 from ..scripts import fitsheader
@@ -11,83 +10,70 @@ class TestFITSheader_script(FitsTestCase):
 
     def test_noargs(self):
         with pytest.raises(SystemExit) as e:
-            fitsheader.main()
-        assert e.value.code == 2
+            fitsheader.main(['-h'])
+        assert e.value.code == 0
 
     def test_file_exists(self, capsys):
-        fitsheader.main([self.data('test0.fits')])
+        fitsheader.main([self.data('arange.fits')])
         out, err = capsys.readouterr()
-        assert not err.startswith('WARNING')
-        assert not err.startswith('ERROR')
+        assert out.splitlines()[1].startswith(
+            'SIMPLE  =                    T / conforms to FITS standard')
+        assert err == ''
 
-    @pytest.mark.parametrize('test_kw,expected', [
-        ('BSCALE', 'BSCALE  =           1.000000E0 / REAL = TAPE*BSCALE + BZERO')
-    ])
-    def test_by_existing_keyword(self, test_kw, expected, capsys):
-        fitsheader.main(['-k', test_kw, self.data('test0.fits')])
+    def test_by_keyword(self, capsys):
+        fitsheader.main(['-k', 'NAXIS', self.data('arange.fits')])
         out, err = capsys.readouterr()
-        assert expected in out
+        assert out.splitlines()[1].startswith(
+            'NAXIS   =                    3 / number of array dimensions')
 
-    # Different test because stderr will not be none in case of testing by
-    # existing keyword fitsheader searches for a keyword in all the HDU's
+        fitsheader.main(['-k', 'NAXIS*', self.data('arange.fits')])
+        out, err = capsys.readouterr()
+        out = out.splitlines()
+        assert len(out) == 5
+        assert out[1].startswith('NAXIS')
+        assert out[2].startswith('NAXIS1')
+        assert out[3].startswith('NAXIS2')
+        assert out[4].startswith('NAXIS3')
 
-    def test_by_non_existing_keyword(self, capsys):
-        fitsheader.main(['-k', 'RANDOMKEY', self.data('test0.fits')])
+        fitsheader.main(['-k', 'RANDOMKEY', self.data('arange.fits')])
         out, err = capsys.readouterr()
         assert err.startswith('WARNING') and 'RANDOMKEY' in err
         assert not err.startswith('ERROR')
 
-    @pytest.mark.parametrize('test_ext', [
-        ('0'),
-        ('PRIMARY')
-    ])
-    def test_by_extension(self, test_ext, capsys):
-        fitsheader.main(['-e', test_ext, self.data('test0.fits')])
+    def test_by_extension(self, capsys):
+        fitsheader.main(['-e', '1', self.data('test0.fits')])
         out, err = capsys.readouterr()
+        assert len(out.splitlines()) == 62
 
-        # Something like -
-        # "# HDU 0 in ./io/fits/tests/data/test0.fits:"
-        # preceds output if extension is correct
-        # Clipping that and checking if there is valid ASCII data below that
-        out = out.split('\n', 1)[1]
-
-        assert re.match('[a-zA-Z0-9_/=.]+', out) is not None
-        assert not err.startswith('WARNING')
-        assert not err.startswith('ERROR')
-
-    @pytest.mark.parametrize('test_ext,test_kw', [
-        ('0', 'BSCALE'),
-        ('SCI', 'EXPNAME')
-    ])
-    def test_by_correct_extension_and_keyword(self, capsys, test_ext, test_kw):
-        fitsheader.main(['-e', test_ext, '-k', test_kw,
-                         self.data('test0.fits')])
+        fitsheader.main(['-e', '3', '-k', 'BACKGRND', self.data('test0.fits')])
         out, err = capsys.readouterr()
-        out = out.split('\n', 1)[1]
+        assert out.splitlines()[1].startswith('BACKGRND=                 312.')
 
-        assert re.match('[a-zA-Z0-9_/=.]+', out) is not None
-        assert not err.startswith('WARNING')
-        assert not err.startswith('ERROR')
-
-    @pytest.mark.parametrize('test_ext,test_kw', [
-        ('0', 'RANDOM'),
-        ('RANDOM_EXT', 'EXPNAME'),
-        ('9', 'EXPNAME')
-    ])
-    def test_by_incorrect_extension_and_keyword(self, capsys, test_ext, test_kw):
-        fitsheader.main(['-e', test_ext, '-k', test_kw,
-                         self.data('test0.fits')])
+        fitsheader.main(['-e', '0', '-k', 'BACKGRND', self.data('test0.fits')])
         out, err = capsys.readouterr()
-
-        # Something like -
-        # "# HDU 0 in ./io/fits/tests/data/test0.fits:"
-        # preceds output if keyword is wrong and extension is correct
-        # Clipping that
-        try:
-            out = out.split('\n', 1)[1]
-        except IndexError:
-            out = ''
-
-        assert re.match('[a-zA-Z0-9_/=.]+', out) is None
         assert err.startswith('WARNING')
-        assert not err.startswith('ERROR')
+
+        fitsheader.main(['-e', '3', '-k', 'FOO', self.data('test0.fits')])
+        out, err = capsys.readouterr()
+        assert err.startswith('WARNING')
+
+    def test_table(self, capsys):
+        fitsheader.main(['-t', '-k', 'BACKGRND', self.data('test0.fits')])
+        out, err = capsys.readouterr()
+        out = out.splitlines()
+        assert len(out) == 5
+        assert out[1].endswith('|   1 | BACKGRND | 316.0 |')
+        assert out[2].endswith('|   2 | BACKGRND | 351.0 |')
+        assert out[3].endswith('|   3 | BACKGRND | 312.0 |')
+        assert out[4].endswith('|   4 | BACKGRND | 323.0 |')
+
+        fitsheader.main(['-t', '-e', '0', '-k', 'NAXIS',
+                         self.data('arange.fits'),
+                         self.data('ascii.fits'),
+                         self.data('blank.fits')])
+        out, err = capsys.readouterr()
+        out = out.splitlines()
+        assert len(out) == 4
+        assert out[1].endswith('|   0 |   NAXIS |     3 |')
+        assert out[2].endswith('|   0 |   NAXIS |     0 |')
+        assert out[3].endswith('|   0 |   NAXIS |     2 |')
