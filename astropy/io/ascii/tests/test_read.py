c13d8f623888a0e05dcc46f6ba461fa6409fe83b
Tom Aldcroft <taldcroft@gmail.com>
2017-09-28 07:14:12 -0400

    Fix another Windows-only test fail

--- astropy/io/ascii/tests/test_read.py
+++ astropy/io/ascii/tests/test_read.py
@@ -1331,7 +1331,7 @@ def test_read_chunks_formats(masked):
     """
     Test different supported formats for chunked reading.
     """
-    t1 = simple_table(size=100, cols=10, kinds='fS', masked=masked)
+    t1 = simple_table(size=102, cols=10, kinds='fS', masked=masked)
     for i, name in enumerate(t1.colnames):
         t1.rename_column(name, 'col{}'.format(i + 1))
 
