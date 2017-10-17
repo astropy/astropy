65feddb79705f7882019b76dc6d295ec5f6fc6dd
Simon Conseil <contact@saimon.org>
2017-10-17 13:53:34 +0200

    Merge pull request #6753 from MSeifert04/size_comparison_file
    Wrong comparison operator

--- astropy/io/fits/file.py
+++ astropy/io/fits/file.py
@@ -251,10 +251,10 @@ class _File(object):
         if size and shape:
             actualsize = np.prod(shape) * dtype.itemsize
 
-            if actualsize < size:
+            if actualsize > size:
                 raise ValueError('size {} is too few bytes for a {} array of '
                                  '{}'.format(size, shape, dtype))
-            if actualsize < size:
+            elif actualsize < size:
                 raise ValueError('size {} is too many bytes for a {} array of '
                                  '{}'.format(size, shape, dtype))
 
