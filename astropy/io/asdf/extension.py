4b94e31fde629d50e9c7ea69ab33fe9133853efd
Dan D'Avella <drdavella@gmail.com>
2017-10-20 17:01:39 -0400

    Move ASDF implementation of models/transforms to astropy

--- astropy/io/asdf/extension.py
+++ astropy/io/asdf/extension.py
@@ -9,6 +9,11 @@ from astropy.io.asdf.tags.fits import FitsType
 from astropy.io.asdf.tags.time import TimeType
 from astropy.io.asdf.tags.unit import UnitType, QuantityType
 from astropy.io.asdf.tags.table import TableType, ColumnType
+from astropy.io.asdf.tags.transform import (
+    TransformType, IdentityType, ConstantType, DomainType, CompoundType,
+    RemapAxesType, ShiftType, ScaleType, PolynomialType, AffineType,
+    Rotate2DType, Rotate3DType, TabularType)
+from astropy.io.asdf.tags.transform.projections import _projection_types
 
 
 SCHEMA_PATH = os.path.abspath(
@@ -18,14 +23,30 @@ SCHEMA_PATH = os.path.abspath(
 class AstropyExtension(object):
     @property
     def types(self):
+        # TODO: This could be simplified by an AstropyType that inherits from
+        # asdf.asdftypes.CustomType and automatically adds each subclass to
+        # a list (much like AsdfType does internally).
         return [
             FitsType,
             TimeType,
             UnitType,
             QuantityType,
             TableType,
-            ColumnType
-        ]
+            ColumnType,
+            TransformType,
+            IdentityType,
+            ConstantType,
+            DomainType,
+            CompoundType,
+            RemapAxesType,
+            ShiftType,
+            ScaleType,
+            PolynomialType,
+            AffineType,
+            Rotate2DType,
+            Rotate3DType,
+            TabularType
+        ] + _projection_types
 
     @property
     def tag_mapping(self):
