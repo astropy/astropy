cfe344d380d27226940c08e021668d3f15725cdf
Dan D'Avella <drdavella@gmail.com>
2017-10-22 14:53:03 -0400

    Remove 'repeat' plugin: use pytest-repeat instead

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 pytest_plugins = [
-    'astropy.tests.plugins.repeat',
     'astropy.tests.plugins.config',
     'astropy.tests.plugins.display',
 ]
