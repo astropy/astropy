eead147ad978efe82a90b738d9b86fb4d94ce438
Craig Jones <crjones@stsci.edu>
2017-11-02 10:18:11 -0400

    2D/3D with and without wcs data zooming

--- astropy/nddata/utils.py
+++ astropy/nddata/utils.py
@@ -496,48 +496,59 @@ def rebin(data, subsample_factor, wcs=None):
 
     Examples
     --------
-    >>> import numpy as np
-    >>> from astropy.nddata.utils import block_replicate
-    >>> data = np.array([[0., 1.], [2., 3.]])
-    >>> block_replicate(data, 2)
-    array([[ 0.  ,  0.  ,  0.25,  0.25],
-           [ 0.  ,  0.  ,  0.25,  0.25],
-           [ 0.5 ,  0.5 ,  0.75,  0.75],
-           [ 0.5 ,  0.5 ,  0.75,  0.75]])
-
-    >>> block_replicate(data, 2, conserve_sum=False)
-    array([[ 0.,  0.,  1.,  1.],
-           [ 0.,  0.,  1.,  1.],
-           [ 2.,  2.,  3.,  3.],
-           [ 2.,  2.,  3.,  3.]])
     """
-    import reproject
-    import scipy.ndimage
-    import scipy.interpolate
-    from astropy import wcs as astropywcs
-
     data = np.asanyarray(data)
 
     if wcs:
-        w = astropywcs.WCS(naxis=2)
-        w.wcs.crpix = wcs.wcs.crpix[:2] / subsample_factor
-        w.wcs.cdelt = wcs.wcs.cdelt[:2] * subsample_factor
-        w.wcs.crval = wcs.wcs.crval[:2]
-        w.wcs.ctype = ['RA---TAN', 'DEC--TAN']
+        # If the WCS is included as a parameter then we will do the work here.
+        import reproject
 
-        output_shape = [int(x/subsample_factor) for x in data.shape]
+        w = wcs.deepcopy()
+        w.wcs.crpix = wcs.wcs.crpix / np.array(subsample_factor)
+        w.wcs.cdelt = wcs.wcs.cdelt * np.array(subsample_factor)
+        
+        if isinstance(subsample_factor, (list, tuple)):
+            # The subsample factor, if a list/tuple, is in the same order
+            # as the w.wcs.ctype, BUT, the data cube is in the opposite
+            # order so the subsample factor must be reversed here.
+            subsample_factor = np.array(subsample_factor)[::-1]
+        else:
+            subsample_factor = np.array(subsample_factor)
 
-        output_data, output_wcs = reproject.reproject_interp((data, wcs), w, shape_out=output_shape)
+        output_shape = [int(x) for x in np.array(data.shape)/subsample_factor]
+
+        output_data, output_footprint = reproject.reproject_interp(
+                    (data, wcs), w, shape_out=output_shape
+                )
         return output_data, w
     else:
-        # Used map_coordinates to match what is done in reproject in order to make it similar to what is above.
+        # If the WCS is NOT included as a parameter then we will use the 
+        # map_coordinates function which is similar to what was done
+        # in reproject.
         from scipy.ndimage.interpolation import map_coordinates as scipy_map_coordinates
 
-        y = np.linspace(0, data.shape[0], data.shape[0]/subsample_factor)
-        x = np.linspace(0, data.shape[1], data.shape[1]/subsample_factor)
-        xx, yy = np.meshgrid(x, y)
-        coords = np.array([yy.flatten(), xx.flatten()])
-        output_data = scipy_map_coordinates(data, coords).reshape(xx.shape)
+        if isinstance(subsample_factor, (list, tuple)):
+            subsample_factor = np.array([float(x) for x in subsample_factor[::-1]])
+        else:
+            subsample_factor = float(subsample_factor)
+
+        step_size = np.array(data.shape) / subsample_factor
+
+        if len(data.shape) == 2:
+            y = np.linspace(0, data.shape[0], step_size[0])
+            x = np.linspace(0, data.shape[1], step_size[1])
+            xx, yy = np.meshgrid(x, y)
+            coords = np.array([yy.flatten(), xx.flatten()])
+            output_data = scipy_map_coordinates(data, coords).reshape(xx.shape)
+        elif len(data.shape) == 3:
+            w = np.linspace(0, data.shape[0], step_size[0])
+            x = np.linspace(0, data.shape[1], step_size[1])
+            y = np.linspace(0, data.shape[2], step_size[2])
+            xx, ww, yy = np.meshgrid(x, w, y)
+            coords = np.array([ww.flatten(), xx.flatten(), yy.flatten()])
+            output_data = scipy_map_coordinates(data, coords).reshape(xx.shape)
+        else:
+            raise ValueError('Data size must be be 2D or 3D.')
 
         return output_data
 
