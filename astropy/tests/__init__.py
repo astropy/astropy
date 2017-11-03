32268da2cc5e3ced0db3b92496639545623a0ace
Daniel D'Avella <ddavella@stsci.edu>
2017-11-03 15:25:04 -0400

    Import disable_internet into `astropy.tests` for backwards compatibility

--- astropy/tests/__init__.py
+++ astropy/tests/__init__.py
@@ -4,3 +4,25 @@ This package contains utilities to run the astropy test suite, tools
 for writing tests, and general tests that are not associated with a
 particular package.
 """
+
+# NOTE: This is retained only for backwards compatibility. Affiliated packages
+# should no longer import `disable_internet` from `astropy.tests`. It is now
+# available from `pytest_remotedata`. However, this is not the recommended
+# mechanism for controlling access to remote data in tests. Instead, packages
+# should make use of decorators provided by the pytest_remotedata plugin:
+# - `@pytest.mark.remote_data` for tests that require remote data access
+# - `@pytest.mark.internet_off` for tests that should only run when remote data
+#       access is disabled.
+# Remote data access for the test suite is controlled by the `--remote-data`
+# command line flag. This is either passed to `pytest` directly or to the
+# `setup.py test` command.
+#
+# TODO: This import should eventually be removed once backwards compatibility
+# is no longer supported.
+
+try:
+    # This should only be necessary during testing, in which case the test
+    # package must be installed anyway.
+    from pytest_remotedata import disable_internet
+except ImportError:
+    pass
