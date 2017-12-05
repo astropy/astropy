dddcaa6de5509fd029372a487a8d57882a033de2
Simon Conseil <contact@saimon.org>
2017-12-05 23:28:22 +0100

    Use splitlines to compare outputs

--- astropy/table/tests/test_showtable.py
+++ astropy/table/tests/test_showtable.py
@@ -20,12 +20,11 @@ def test_missing_file(capsys):
 def test_info(capsys):
     showtable.main([os.path.join(FITS_ROOT, 'data/table.fits'), '--info'])
     out, err = capsys.readouterr()
-    assert out == ('<Table length=3>{0}'
-                   ' name   dtype {0}'
-                   '------ -------{0}'
-                   'target   str20{0}'
-                   ' V_mag float32{0}'
-                   '\n').format(os.linesep)
+    assert out.splitlines() == ['<Table length=3>',
+                                ' name   dtype ',
+                                '------ -------',
+                                'target   str20',
+                                ' V_mag float32']
 
 
 def test_stats(capsys):
@@ -41,12 +40,11 @@ def test_stats(capsys):
 def test_fits(capsys):
     showtable.main([os.path.join(FITS_ROOT, 'data/table.fits')])
     out, err = capsys.readouterr()
-    assert out == (' target V_mag\n'
-                   '             \n'
-                   '------- -----\n'
-                   'NGC1001  11.1\n'
-                   'NGC1002  12.3\n'
-                   'NGC1003  15.2\n')
+    assert out.splitlines() == [' target V_mag',
+                                '------- -----',
+                                'NGC1001  11.1',
+                                'NGC1002  12.3',
+                                'NGC1003  15.2']
 
 
 def test_fits_hdu(capsys):
@@ -64,49 +62,45 @@ def test_fits_hdu(capsys):
 def test_csv(capsys):
     showtable.main([os.path.join(ASCII_ROOT, 't/simple_csv.csv')])
     out, err = capsys.readouterr()
-    assert out == (' a   b   c \n'
-                   '           \n'
-                   '--- --- ---\n'
-                   '  1   2   3\n'
-                   '  4   5   6\n')
+    assert out.splitlines() == [' a   b   c ',
+                                '--- --- ---',
+                                '  1   2   3',
+                                '  4   5   6']
 
 
 def test_ascii_format(capsys):
     showtable.main([os.path.join(ASCII_ROOT, 't/commented_header.dat'),
                     '--format', 'ascii.commented_header'])
     out, err = capsys.readouterr()
-    assert out == (' a   b   c \n'
-                   '           \n'
-                   '--- --- ---\n'
-                   '  1   2   3\n'
-                   '  4   5   6\n')
+    assert out.splitlines() == [' a   b   c ',
+                                '--- --- ---',
+                                '  1   2   3',
+                                '  4   5   6']
 
 
 def test_ascii_delimiter(capsys):
     showtable.main([os.path.join(ASCII_ROOT, 't/simple2.txt'),
                     '--format', 'ascii', '--delimiter', '|'])
     out, err = capsys.readouterr()
-    assert out == (
-        "obsid redshift  X    Y      object   rad \n"
-        "                                         \n"
-        "----- -------- ---- ---- ----------- ----\n"
-        " 3102     0.32 4167 4085 Q1250+568-A  9.0\n"
-        " 3102     0.32 4706 3916 Q1250+568-B 14.0\n"
-        "  877     0.22 4378 3892 'Source 82' 12.5\n"
-    )
+    assert out.splitlines() == [
+        "obsid redshift  X    Y      object   rad ",
+        "----- -------- ---- ---- ----------- ----",
+        " 3102     0.32 4167 4085 Q1250+568-A  9.0",
+        " 3102     0.32 4706 3916 Q1250+568-B 14.0",
+        "  877     0.22 4378 3892 'Source 82' 12.5",
+    ]
 
 
 def test_votable(capsys):
     showtable.main([os.path.join(VOTABLE_ROOT, 'data/regression.xml'),
                     '--table-id', 'main_table', '--max-width', '50'])
     out, err = capsys.readouterr()
-    assert out == (
-        '   string_test    string_test_2 ... bitarray2 [16]\n'
-        '                                ...               \n'
-        '----------------- ------------- ... --------------\n'
-        '    String & test    Fixed stri ...  True .. False\n'
-        'String &amp; test    0123456789 ...       -- .. --\n'
-        '             XXXX          XXXX ...       -- .. --\n'
-        '                                ...       -- .. --\n'
-        '                                ...       -- .. --\n'
-    )
+    assert out.splitlines() == [
+        '   string_test    string_test_2 ... bitarray2 [16]',
+        '----------------- ------------- ... --------------',
+        '    String & test    Fixed stri ...  True .. False',
+        'String &amp; test    0123456789 ...       -- .. --',
+        '             XXXX          XXXX ...       -- .. --',
+        '                                ...       -- .. --',
+        '                                ...       -- .. --',
+    ]
