8840c1eb0146e9295b925d228195724bbba9e551
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-12-09 10:38:21 -0500

    don't add to graph unless its validated

--- astropy/coordinates/transformations.py
+++ astropy/coordinates/transformations.py
@@ -38,6 +38,41 @@ __all__ = ['TransformGraph', 'CoordinateTransform', 'FunctionTransform',
            'FunctionTransformWithFiniteDifference', 'CompositeTransform']
 
 
+def frame_attrs_from_set(frame_set):
+    """
+    A `dict` of all the attributes of all frame classes in this
+    `TransformGraph`.
+
+    Broken out of the class so this can be called on a temporary frame set to
+    validate new additions to the transform graph before actually adding them.
+    """
+    result = {}
+
+    for frame_cls in frame_set:
+        result.update(frame_cls.frame_attributes)
+
+    return result
+
+
+def frame_comps_from_set(frame_set):
+    """
+    A `set` of all component names every defined within any frame class in
+    this `TransformGraph`.
+
+    Broken out of the class so this can be called on a temporary frame set to
+    validate new additions to the transform graph before actually adding them.
+    """
+    result = set()
+
+    for frame_cls in frame_set:
+        rep_info = frame_cls._frame_specific_representation_info
+        for mappings in rep_info.values():
+            for rep_map in mappings:
+                result.update([rep_map.framename])
+
+    return result
+
+
 class TransformGraph:
     """
     A graph representing the paths between coordinate frames.
@@ -64,24 +99,22 @@ class TransformGraph:
         A `set` of all the frame classes present in this `TransformGraph`.
         """
         if self._cached_frame_set is None:
-            self._cached_frame_set = frm_set = set()
+            self._cached_frame_set = set()
             for a in self._graph:
-                frm_set.add(a)
+                self._cached_frame_set.add(a)
                 for b in self._graph[a]:
-                    frm_set.add(b)
+                    self._cached_frame_set.add(b)
 
         return self._cached_frame_set.copy()
 
     @property
     def frame_attributes(self):
         """
-        A `dict` of all the attributes of all frame classes in this `TransformGraph`.
+        A `dict` of all the attributes of all frame classes in this
+        `TransformGraph`.
         """
         if self._cached_frame_attributes is None:
-            result = {}
-            for frame_cls in self.frame_set:
-                result.update(frame_cls.frame_attributes)
-            self._cached_frame_attributes = result
+            self._cached_frame_attributes = frame_attrs_from_set(self.frame_set)
 
         return self._cached_frame_attributes
 
@@ -92,13 +125,7 @@ class TransformGraph:
         this `TransformGraph`.
         """
         if self._cached_component_names is None:
-            self._cached_component_names = set()
-
-            for frame_cls in self.frame_set:
-                rep_info = frame_cls._frame_specific_representation_info
-                for mappings in rep_info.values():
-                    for rep_map in mappings:
-                        self._cached_component_names.update([rep_map.framename])
+            self._cached_component_names = frame_comps_from_set(self.frame_set)
 
         return self._cached_component_names
 
@@ -145,14 +172,15 @@ class TransformGraph:
         if not callable(transform):
             raise TypeError('transform must be callable')
 
-        self._graph[fromsys][tosys] = transform
-        self.invalidate_cache()
+        frame_set = self.frame_set.copy()
+        frame_set.add(fromsys)
+        frame_set.add(tosys)
 
-        # Now we check to see if any attributes were added that override
-        # *any* component names, which we can't have for some of the logic in
-        # the SkyCoord initializer
-        attrs = set(self.frame_attributes.keys())
-        comps = self.frame_component_names
+        # Now we check to see if any attributes on the proposed frames override
+        # *any* component names, which we can't allow for some of the logic in
+        # the SkyCoord initializer to work
+        attrs = set(frame_attrs_from_set(frame_set).keys())
+        comps = frame_comps_from_set(frame_set)
 
         invalid_attrs = attrs.intersection(comps)
         if invalid_attrs:
@@ -164,14 +192,14 @@ class TransformGraph:
                 if attr in tosys.frame_attributes:
                     invalid_frames.update([tosys])
 
-            del self._graph[fromsys][tosys]
-
             raise ValueError("Frame(s) {0} contain invalid attribute names: {1}"
                              "\nFrame attributes can not conflict with *any* of"
-                             " the frame data component names (see "
-                             "`frame_transform_graph.frame_component_names`):\n"
-                             "{2}".format(list(invalid_frames), invalid_attrs,
-                                          comps))
+                             " the frame data component names (see"
+                             " `frame_transform_graph.frame_component_names`)."
+                             .format(list(invalid_frames), invalid_attrs))
+
+        self._graph[fromsys][tosys] = transform
+        self.invalidate_cache()
 
     def remove_transform(self, fromsys, tosys, transform):
         """
