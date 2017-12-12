cfd06cfc4a249ac4f8890fdd7818fa02bcaaab3d
Nadia Dencheva <nadia.dencheva@gmail.com>
2017-12-12 15:15:04 -0500

    use dictionaries for strict and dimensionless units from the start

--- astropy/modeling/rotations.py
+++ astropy/modeling/rotations.py
@@ -96,9 +96,9 @@ class _EulerRotation:
             b.shape = shape
         return a, b
 
-    input_units_strict = True
+    _input_units_strict = True
 
-    input_units_allow_dimensionless = True
+    _input_units_allow_dimensionless = True
 
     @property
     def input_units(self):
@@ -347,9 +347,9 @@ class Rotation2D(Model):
 
     angle = Parameter(default=0.0, getter=_to_orig_unit, setter=_to_radian)
 
-    input_units_strict = True
+    _input_units_strict = True
 
-    input_units_allow_dimensionless = True
+    _input_units_allow_dimensionless = True
 
     @property
     def inverse(self):
