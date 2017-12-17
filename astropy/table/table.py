a2b6a8a9d4b44ada313f87519a0288bf91bf805a
ananyashreyjain <31594632+ananyashreyjain@users.noreply.github.com>
2017-12-17 20:50:19 +0530

    Update table.py

--- astropy/table/table.py
+++ astropy/table/table.py
@@ -3,6 +3,7 @@ from .index import TableIndices, TableLoc, TableILoc
 
 import re
 import sys
+import os
 from collections import OrderedDict, Mapping
 import warnings
 from copy import deepcopy
@@ -2533,6 +2534,9 @@ class Table:
         The arguments and keywords (other than ``format``) provided to this function are
         passed through to the underlying data reader (e.g. `~astropy.io.ascii.write`).
         """
+        if((ext=='.h5'or ext=='.hdf5')and kwargs['overwrite']):
+            for col in self.colnames:
+                self.replace_column(col, np.core.defchararray.encode((self.columns[col])))
         io_registry.write(self, *args, **kwargs)
 
     def copy(self, copy_data=True):
