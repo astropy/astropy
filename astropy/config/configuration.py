6b5dd89de315fd5566f61ec9440e52b4189223ce
Stuart Mumford <stuart@cadair.com>
2017-12-21 12:33:14 +0000

    only need rootname on config item

--- astropy/config/configuration.py
+++ astropy/config/configuration.py
@@ -92,11 +92,6 @@ class ConfigNamespace(metaclass=_ConfigNamespaceMeta):
         conf = Conf()
     """
 
-    rootname = 'astropy'
-    """
-    Rootname sets the base path for all config files.
-    """
-
     def set_temp(self, attr, value):
         """
         Temporarily set a configuration value.
