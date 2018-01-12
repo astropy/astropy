460eed68fc687c33aff21430381baddf1aa19d59
Stuart Mumford <stuart@cadair.com>
2018-01-12 16:47:46 +0000

    Add asdf tag for Representation classes and schemas

--- astropy/io/misc/asdf/tags/coordinates/tests/test_coords.py
+++ astropy/io/misc/asdf/tags/coordinates/tests/test_coords.py
@@ -6,10 +6,20 @@ import pytest
 asdf = pytest.importorskip('asdf')
 from asdf.tests.helpers import assert_roundtrip_tree
 
+import astropy.units as u
 from astropy import units
-from astropy.coordinates import ICRS, Longitude, Latitude, Angle
+from astropy.coordinates import ICRS, HCRS, Longitude, Latitude, Angle
 
 from ....extension import AstropyExtension
+
+
+def test_hcrs_basic(tmpdir):
+    ra = Longitude(25, unit=units.deg)
+    dec = Latitude(45, unit=units.deg)
+
+    tree = {'coord': HCRS(ra=ra, dec=dec)}
+
+    assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
 
 
 def test_icrs_basic(tmpdir):
@@ -22,12 +32,40 @@ def test_icrs_basic(tmpdir):
     assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
 
 
-@pytest.mark.xfail(
-    reason="Compound ICRS coordinates have not been implemented for ASDF yet")
-def test_icrs_compound(tmpdir):
+def test_icrs_nodata(tmpdir):
+    tree = {'coord': ICRS()}
+
+    assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
+
+
+def test_hcrs_compound(tmpdir):
 
     icrs = ICRS(ra=[0, 1, 2]*units.deg, dec=[3, 4, 5]*units.deg)
 
     tree = {'coord': icrs}
 
     assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
+
+
+def test_sunpy(tmpdir):
+
+    scoord = pytest.importorskip('sunpy.coordinates')
+
+    hpc = scoord.Helioprojective(100*units.arcsec, 100*units.arcsec,
+                                 obstime="2011-01-01")
+
+    tree = {'coord': hpc}
+
+    assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
+
+
+def test_sunpy_hgs(tmpdir):
+
+    scoord = pytest.importorskip('sunpy.coordinates')
+
+    hpc = scoord.HeliographicStonyhurst(1*u.deg, 0*u.deg, 0.98*u.AU)
+
+    tree = {'coord': hpc}
+
+    assert_roundtrip_tree(tree, tmpdir, extensions=AstropyExtension())
+
