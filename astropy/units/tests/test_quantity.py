8793de9dd0a4b58e969f083beb5978fe3f805273
ananyashreyjain <31594632+ananyashreyjain@users.noreply.github.com>
2018-01-22 10:43:00 +0530

    pep8 guidelines

--- astropy/units/tests/test_quantity.py
+++ astropy/units/tests/test_quantity.py
@@ -1445,8 +1445,8 @@ def test_unit_class_override():
     assert type(q2) is MyQuantity
 
 def test_human_readable_unit():
-    assert u.utils.human_readable(10000*u.s,unit='prefix').value==((10000*u.s).to(u.hr)).value
-    assert u.utils.human_readable(10000*u.s,unit='None')==str(10000*u.s)
-    assert u.utils.human_readable(10000*u.s,unit='human')==" 2h46m"
-    assert u.utils.human_readable(1234567*u.m,unit='prefix').value==((1234567*u.m).to(u.Mm)).value
-    assert u.utils.human_readable(1234567*u.m,unit='human')== "1.235 Mm"
+    assert u.utils.human_readable(10000*u.s, unit='prefix').value == ((10000*u.s).to(u.hr)).value
+    assert u.utils.human_readable(10000*u.s, unit='None') == str(10000*u.s)
+    assert u.utils.human_readable(10000*u.s, unit='human') == " 2h46m"
+    assert u.utils.human_readable(1234567*u.m, unit='prefix').value == ((1234567*u.m).to(u.Mm)).value
+    assert u.utils.human_readable(1234567*u.m, unit='human') == "1.235 Mm"
