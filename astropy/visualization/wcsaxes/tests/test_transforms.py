ecf8cbd2971216c8cbc44a5fba5338b289158239
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-01-23 21:24:49 +0100

    Merge pull request #6531 from lpsinger/wcs_has_inverse
    Enable subtraction syntax for inverting and composing WCS transforms

--- astropy/visualization/wcsaxes/tests/test_transforms.py
+++ astropy/visualization/wcsaxes/tests/test_transforms.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 
-from matplotlib.transforms import Affine2D
+from matplotlib.transforms import Affine2D, IdentityTransform
 
 from ....wcs import WCS
 
@@ -22,6 +22,29 @@ WCS3D.wcs.cunit = ['km', 'km', 'km']
 WCS3D.wcs.crpix = [614.5, 856.5, 333]
 WCS3D.wcs.cdelt = [6.25, 6.25, 23]
 WCS3D.wcs.crval = [0., 0., 1.]
+
+
+def test_shorthand_inversion():
+    """Test that the Matplotlib subtraction shorthand for composing and
+    inverting transformations works."""
+    w1 = WCS(naxis=2)
+    w1.wcs.ctype = ['RA---TAN', 'DEC--TAN']
+    w1.wcs.crpix = [256.0, 256.0]
+    w1.wcs.cdelt = [-0.05, 0.05]
+    w1.wcs.crval = [120.0, -19.0]
+
+    w2 = WCS(naxis=2)
+    w2.wcs.ctype = ['RA---SIN', 'DEC--SIN']
+    w2.wcs.crpix = [256.0, 256.0]
+    w2.wcs.cdelt = [-0.05, 0.05]
+    w2.wcs.crval = [235.0, +23.7]
+
+    t1 = WCSWorld2PixelTransform(w1)
+    t2 = WCSWorld2PixelTransform(w2)
+
+    assert t1 - t2 == t1 + t2.inverted()
+    assert t1 - t2 != t2.inverted() + t1
+    assert t1 - t1 == IdentityTransform()
 
 
 # We add Affine2D to catch the fact that in Matplotlib, having a Composite
