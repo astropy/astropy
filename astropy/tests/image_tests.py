9385fb06a49794319f0e9f4dce7a11545cfd896a
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-02-01 23:51:33 +0000

    Updated baseline URLs

--- astropy/tests/image_tests.py
+++ astropy/tests/image_tests.py
@@ -1,16 +1,13 @@
-from distutils.version import LooseVersion
-
 import matplotlib
 from matplotlib import pyplot as plt
 
 from ..utils.decorators import wraps
 
-MPL_VERSION = LooseVersion(matplotlib.__version__)
+MPL_VERSION = matplotlib.__version__
 
-ROOT = "http://{server}/testing/astropy/2017-07-12T14:12:26.217559/{mpl_version}/"
+ROOT = "http://{server}/testing/astropy/2018-02-01T23:31:45.013149/{mpl_version}/"
 
-IMAGE_REFERENCE_DIR = (ROOT.format(server='data.astropy.org', mpl_version='1.5.x') + ',' +
-                       ROOT.format(server='www.astropy.org/astropy-data', mpl_version='1.5.x'))
+IMAGE_REFERENCE_DIR = ROOT.format(server='astrofrog.github.io/astropy-data', mpl_version=MPL_VERSION[:3] + '.x')
 
 
 def ignore_matplotlibrc(func):
