187cd78089c8a293c002aa49d7f3e45e378083fb
Daniel D'Avella <ddavella@stsci.edu>
2018-03-09 15:50:38 -0500

    Be explicit about removing xcodeproject files [skip ci]

--- cextern/trim_cfitsio.sh
+++ cextern/trim_cfitsio.sh
@@ -31,7 +31,7 @@ rm -f cfitsio/docs/*.ps
 rm -f cfitsio/docs/*.pdf
 rm -f cfitsio/docs/*.doc
 rm -f cfitsio/docs/*.toc
-rm -rf cfitsio/[^L]*.*
+rm -f cfitsio/[^L]*.*
 cat <<EOF >cfitsio/README.txt
 Note: astropy only requires the CFITSIO library, and hence in this bundled version,
 we removed all other files except the required license (License.txt) and changelog
