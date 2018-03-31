74f35732fa2f08cb6d23707e803ab8b83001943c
Brigitta Sipocz <b.sipocz@gmail.com>
2018-03-31 15:07:55 +0100

    Merge pull request #7342 from pllim/np-dev-utils
    TST: Fix import error from numpy.testing.utils

--- astropy/modeling/tests/test_compound.py
+++ astropy/modeling/tests/test_compound.py
@@ -8,7 +8,7 @@ import pickle
 import pytest
 import numpy as np
 
-from numpy.testing.utils import assert_allclose, assert_array_equal
+from numpy.testing import assert_allclose, assert_array_equal
 
 
 from ..core import Model, ModelDefinitionError
--- astropy/units/tests/test_units.py
+++ astropy/units/tests/test_units.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-
-
 """
 Regression tests for the units package
 """
-
-
 import pickle
 from fractions import Fraction
 
 import pytest
 import numpy as np
-from numpy.testing.utils import assert_allclose
+from numpy.testing import assert_allclose
 
 from ...tests.helper import raises, catch_warnings
 
