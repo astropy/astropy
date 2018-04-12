45966159c8e6cfd5729aa5948bf3c981b037f24a
Stuart Mumford <stuart@cadair.com>
2018-04-12 14:29:00 +0100

    change asdf test version check so the tests run!

--- astropy/io/misc/asdf/tags/transform/tests/test_transform.py
+++ astropy/io/misc/asdf/tags/transform/tests/test_transform.py
@@ -4,7 +4,7 @@
 import pytest
 import numpy as np
 
-asdf = pytest.importorskip('asdf', minversion='2.0.0')
+asdf = pytest.importorskip('asdf', minversion='2.0.0.dev0')
 from asdf import util
 from asdf.tests import helpers
 
