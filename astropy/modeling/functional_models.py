45cba1f5b4a53e1252cc8dd9b5fb95e4da2c8a0b
Nadia Dencheva <nadia.astropy@gmail.com>
2018-04-16 16:11:16 -0400

    Merge pull request #7378 from johnnygreco/sersic-doc-error
    Corrected Sersic1D and Sersic2D amplitude definition in docstrings

--- astropy/modeling/functional_models.py
+++ astropy/modeling/functional_models.py
@@ -592,7 +592,7 @@ class Sersic1D(Fittable1DModel):
     Parameters
     ----------
     amplitude : float
-        Central surface brightness, within r_eff.
+        Surface brightness at r_eff.
     r_eff : float
         Effective (half-light) radius
     n : float
@@ -2347,7 +2347,7 @@ class Sersic2D(Fittable2DModel):
     Parameters
     ----------
     amplitude : float
-        Central surface brightness, within r_eff.
+        Surface brightness at r_eff.
     r_eff : float
         Effective (half-light) radius
     n : float
