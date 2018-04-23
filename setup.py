87283fef45a1aab2ee83cb43e4155aef2541fa04
Brigitta Sipocz <bsipocz@gmail.com>
2018-04-23 20:18:22 +0100

    Back to development: v2.0.7.dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import astropy
 NAME = 'astropy'
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '2.0.6'
+VERSION = '2.0.7.dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
