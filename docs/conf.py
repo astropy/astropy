107f4bb84a12e5f4c2776a4576a2fd8061ea40bb
Brigitta Sipocz <b.sipocz@gmail.com>
2018-05-17 00:48:43 +0100

    Merge pull request #7476 from bsipocz/docs_intersphinx_use_stable
    Use released intersphinxs rather than development version.

--- docs/conf.py
+++ docs/conf.py
@@ -78,11 +78,12 @@ check_sphinx_version("1.2.1")
 del intersphinx_mapping['astropy']
 
 # add any custom intersphinx for astropy
-intersphinx_mapping['pytest'] = ('https://docs.pytest.org/en/latest/', None)
+intersphinx_mapping['pytest'] = ('https://docs.pytest.org/en/stable/', None)
 intersphinx_mapping['ipython'] = ('http://ipython.readthedocs.io/en/stable/', None)
 intersphinx_mapping['pandas'] = ('http://pandas.pydata.org/pandas-docs/stable/', None)
 intersphinx_mapping['sphinx_automodapi'] = ('https://sphinx-automodapi.readthedocs.io/en/stable/', None)
 intersphinx_mapping['packagetemplate'] = ('http://docs.astropy.org/projects/package-template/en/latest/', None)
+intersphinx_mapping['h5py'] = ('http://docs.h5py.org/en/stable/', None)
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
