13ee02b6d4888df82196ea3c2381ff9734219bd2
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-05-28 16:47:32 -0400

    Basic start of StructuredQuantity.

--- astropy/units/structured.py
+++ astropy/units/structured.py
@@ -12,7 +12,7 @@ from .core import Unit, UnitBase
 from .quantity import Quantity
 
 
-__all__ = ['StructuredUnit']
+__all__ = ['StructuredUnit', 'StructuredQuantity']
 
 
 class StructuredUnit(np.void):
@@ -22,6 +22,10 @@ class StructuredUnit(np.void):
             dtype = np.dtype((cls, dtype))
         self = np.array(units, dtype)[()]
         return self
+
+    @property
+    def _quantity_class(self):
+        return StructuredQuantity
 
     def __getitem__(self, item):
         val = super().__getitem__(item)
@@ -33,3 +37,29 @@ class StructuredUnit(np.void):
     # define this to pass through Unit initializer
     def _get_physical_type_id(self):
         return NotImplementedError
+
+
+class StructuredQuantity(Quantity):
+    def __new__(cls, value, unit=None, dtype=None, *args, **kwargs):
+        if not isinstance(unit, StructuredUnit):
+            unit = StructuredUnit(unit, dtype)
+        self = np.array(value, dtype, *args, **kwargs).view(cls)
+        self._unit = unit
+        return self
+
+    def __getitem__(self, item):
+        out = super().__getitem__(item)
+        if out.dtype is not self.dtype:
+            # item caused dtype change -> indexed with string-like.
+            out_unit = self.unit[item]
+            if not out.dtype.fields:
+                out = out.view(getattr(out_unit, '_quantity_cls',
+                                       Quantity))
+            out._unit = out_unit
+
+        return out
+
+    def _set_unit(self, unit):
+        if not isinstance(unit, StructuredUnit):
+            unit = StructuredUnit(unit)
+        self._unit = unit
