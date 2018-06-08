d8b729f6a32a195258ad446e88c1b69cbb09d751
Perry Greenfield <perry@stsci.edu>
2018-06-08 12:31:44 -0400

    make CompoundModel subclass of Model

--- astropy/modeling/tests/test_compound.py
+++ astropy/modeling/tests/test_compound.py
@@ -19,6 +19,7 @@ from astropy.modeling.models import (Const1D, Shift, Scale, Rotation2D, Gaussian
                       Chebyshev2D, Legendre2D, Chebyshev1D, Legendre1D,
                       AffineTransformation2D, Identity, Mapping,
                       Tabular1D)
+from ..core import CompoundModel
 
 
 try:
@@ -29,34 +30,6 @@ except ImportError:
     HAS_SCIPY = False
 
 HAS_SCIPY_14 = HAS_SCIPY and minversion(scipy, "0.14")
-
-
-@pytest.mark.parametrize(('expr', 'result'),
-                         [(lambda x, y: x + y, 5.0),
-                          (lambda x, y: x - y, -1.0),
-                          (lambda x, y: x * y, 6.0),
-                          (lambda x, y: x / y, 2.0 / 3.0),
-                          (lambda x, y: x ** y, 8.0)])
-def test_two_model_class_arithmetic_1d(expr, result):
-    # Const1D is perhaps the simplest model to test basic arithmetic with.
-    # TODO: Should define more tests later on for more complicated
-    # combinations of models
-
-    S = expr(Const1D, Const1D)
-
-    assert issubclass(S, Model)
-    assert S.n_inputs == 1
-    assert S.n_outputs == 1
-
-    # Initialize an instance of the model, providing values for the two
-    # "amplitude" parameters
-    s = S(2, 3)
-
-    # It shouldn't matter what input we evaluate on since this is a constant
-    # function
-    out = s(0)
-    assert out == result
-    assert isinstance(out, float)
 
 
 @pytest.mark.parametrize(('expr', 'result'),
