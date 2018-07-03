48f8b1ca81cf160aa3dae8e0c0cbc7c4e919eb4e
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-07-03 15:06:05 -0400

    Split out utilities from quantity to their own file.

--- astropy/units/quantity/__init__.py
+++ astropy/units/quantity/__init__.py
@@ -5,5 +5,14 @@ associated units. `Quantity` objects support operations like ordinary numbers,
 but will deal with unit conversions internally.
 """
 
-from .quantity import _UNIT_NOT_INITIALISED, _unquantify_allclose_arguments, conf
+from . import quantity, utils
 from .quantity import *
+from .utils import *
+
+# Extra imports for help for those who imported private atttributes
+# from the old quantity.py
+from .quantity import _UNIT_NOT_INITIALISED, conf
+from .utils import _unquantify_allclose_arguments
+
+
+__all__ = quantity.__all__ + utils.__all__
