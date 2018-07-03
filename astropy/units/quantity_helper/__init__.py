309bcaf4ab9153da6f32450795ea113edda972d9
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-07-02 20:05:27 -0400

    Move quantity_helper to its own module.
    This so that it will be possible to split up the numpy, scipy,
    and future erfa parts.

---
+++ astropy/units/quantity_helper/__init__.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""Helper functions for Quantity.
+
+In particular, this implements the logic that determines scaling and result
+units for a given ufunc, given input units.
+"""
+
+UFUNC_HELPERS = {}
+from .helpers import *
