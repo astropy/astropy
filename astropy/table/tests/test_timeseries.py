10e020b7e0723adf10fb06b78763b3f80269ef4f
Brigitta Sipocz <bsipocz@gmail.com>
2018-08-01 17:04:26 +0100

    Addressing review for the tests

--- astropy/table/tests/test_timeseries.py
+++ astropy/table/tests/test_timeseries.py
@@ -8,8 +8,8 @@ from ..timeseries import TimeSeries, TimeSeriesColumn, TimeSeriesRow
 from ...time import Time, TimeDelta
 
 
-input_time = Time(['2016-03-22T12:30:31', '2015-01-21T12:30:32', '2016-03-22T12:30:40'])
-input_data = Table([[1, 2, 11], [3, 4, 1], [1, 1, 1]], names=['a', 'b', 'c'])
+INPUT_TIME = Time(['2016-03-22T12:30:31', '2015-01-21T12:30:32', '2016-03-22T12:30:40'])
+INPUT_DATA = Table([[1, 2, 11], [3, 4, 1], [1, 1, 1]], names=['a', 'b', 'c'])
 
 
 @pytest.mark.xfail
@@ -22,15 +22,23 @@ def test_empty_initialization():
 
 
 @pytest.mark.xfail
-def test_initialization():
-    ts = TimeSeries(time=input_time, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+def test_initialize_only_time():
+    ts = TimeSeries(time=INPUT_TIME)
 
     assert isinstance(ts, TimeSeries)
     assert isinstance(ts, QTable)
 
-    assert all(ts.time == input_time)
 
-    ts = TimeSeries(time=input_time, data=input_data)
+@pytest.mark.xfail
+def test_initialization():
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+
+    assert isinstance(ts, TimeSeries)
+    assert isinstance(ts, QTable)
+
+    assert all(ts.time == INPUT_TIME)
+
+    ts = TimeSeries(time=INPUT_TIME, data=INPUT_DATA)
 
 
 @pytest.mark.xfail
@@ -46,13 +54,28 @@ def test_initialization_with_deltatime():
 
 @pytest.mark.xfail
 def test_initialization_with_time_in_data():
-    data = input_data.copy()
-    data['time'] = input_time
+    data = INPUT_DATA.copy()
+    data['time'] = INPUT_TIME
 
     ts = TimeSeries(data=data)
 
-    # This should probably raise an error rather than deal with it
-    ts = TimeSeries(data=data, time=input_time)
+    assert isinstance(ts, TimeSeries)
+    assert isinstance(ts, QTable)
+    assert set(ts.colnames) == set(['a', 'b', 'c', 'time'])
+    assert all(ts.time == INPUT_TIME)
+
+    # TODO: add more valid cases when a time is passed in data & names
+    ts2 = TimeSeries(data=[[10, 2, 3], INPUT_TIME], names=['a', 'time'])
+
+
+    with pytest.raises.ValueError:
+        # Don't allow ambiguous cases of passing multiple 'time' columns
+        TimeSeries(data=data, time=INPUT_TIME)
+
+    with pytest.raises.ValueError:
+        # 'time' is a protected name, don't allow ambiguous cases
+        TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], INPUT_TIME], names=['a', 'time'])
+
 
 
 # TODO remove redundancy for input table initialization, or just simply structure better
@@ -60,7 +83,7 @@ def test_initialization_with_time_in_data():
 
 @pytest.mark.xfail
 def test_adding_more_columns():
-    ts = TimeSeries(time=input_time, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
     ts['c'] = Column([5, 4, 3])
 
     ts.add_column(Column([6, 5, 4], name='d'))
@@ -68,23 +91,23 @@ def test_adding_more_columns():
 
 @pytest.mark.xfail
 def test_access_column():
-    ts = TimeSeries(time=input_time, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
     assert isinstance(ts['a'], TimeSeriesColumn)
-    assert all(ts['a'].time == input_time)
+    # assert all(ts['a'].time == INPUT_TIME)  # not sure whether we need this
 
     assert ts['a'].name == 'a'
 
 
 @pytest.mark.xfail
 def test_access_time():
-    ts = TimeSeries(time=input_time, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
 
-    assert all(ts['time'] == input_time)
+    assert all(ts['time'] == INPUT_TIME)
 
 
 @pytest.mark.xfail
 def test_access_row():
-    ts = TimeSeries(time=input_time, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
 
     assert isinstance(ts[0], TimeSeriesRow)
     # TODO more content checking
@@ -92,16 +115,16 @@ def test_access_row():
 
 @pytest.mark.xfail
 def test_access_col_value():
-    ts = TimeSeries(time=input_time, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
     # TODO: we need to get the sorting by time to get this right
     assert ts['a'][0] == 20
 
 
 @pytest.mark.xfail
 def test_access_time_value():
-    ts = TimeSeries(time=input_time, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 20, 3], [4, 5, 6]], names=['a', 'b'])
 
-    assert ts['a'].time[0] == ts['time'][0]
+    # assert ts['a'].time[0] == ts['time'][0]  # not sure whether we need this
 
     # TODO: we need to get the sorting by time to get this right
     assert ts['time'][0] == Time('2015-01-21T12:30:32')
@@ -109,17 +132,17 @@ def test_access_time_value():
 
 @pytest.mark.xfail
 def test_normal_Columns():
-    ts = TimeSeries(time=input_time, data=input_data)
+    ts = TimeSeries(time=INPUT_TIME, data=INPUT_DATA)
 
-    assert all(ts.columns['a'] == input_data['a'])
+    assert all(ts.columns['a'] == INPUT_DATA['a'])
 
-    assert all(ts['a'][()] == input_data['a'])
+    assert all(ts['a'][()] == INPUT_DATA['a'])
 
 
 @pytest.mark.xfail
 def test_operations():
     # TODO: hstack, vstack, join
-    ts = TimeSeries(time=input_time, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
     new_column = Column([5, 4, 3], name='c')
 
     hstack([ts, new_column])
@@ -129,9 +152,9 @@ def test_operations():
 def test_sorting():
     # TODO: decide which sorting we would like to allow
 
-    sorted_input_time = input_time.sort()
+    sorted_input_time = INPUT_TIME.sort()
     input_data = Table([[10, 2, 3], [4, 5, 6]], names=['a', 'b'])
-    ts = TimeSeries(time=input_time, data=input_data)
+    ts = TimeSeries(time=INPUT_TIME, data=input_data)
     ts_sort = TimeSeries(time=sorted_input_time, data=input_data)
 
     # There is automated sorting by time
@@ -148,7 +171,7 @@ def test_adding_index_column():
     # TODO: decide which sorting we would like to allow, if it's only by time,
     # the indices should already been set 'time'
 
-    ts = TimeSeries(time=input_time, data=input_data)
+    ts = TimeSeries(time=INPUT_TIME, data=INPUT_DATA)
     ts.add_index('a')
     ts.add_index('time')
 
@@ -157,8 +180,8 @@ def test_adding_index_column():
 
 @pytest.mark.xfail
 def test_access_multiple_columns():
-    ts = TimeSeries(time=input_time, data=[[1, 20, 3], [4, 5, 6], [4, 3, 2]], names=['a', 'b', 'c'])
-    ts_out = TimeSeries(time=input_time, data=[[1, 20, 3], [4, 5, 6]], names=['a', 'b'])
+    ts = TimeSeries(time=INPUT_TIME, data=[[1, 20, 3], [4, 5, 6], [4, 3, 2]], names=['a', 'b', 'c'])
+    ts_out = TimeSeries(time=INPUT_TIME, data=[[1, 20, 3], [4, 5, 6]], names=['a', 'b'])
 
     # TODO: For this we need to be able to do TimeSeries([TimeSeries, TimeSeries, Time])
     # type initialization, or override TimeSeries.__getitem__
