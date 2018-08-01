5aeeac76b5bca7818cb6c14ba0cb3f3d471f9ac9
Brigitta Sipocz <bsipocz@gmail.com>
2018-08-01 17:14:52 +0100

    Moving timeseries into a new submodule

---
+++ astropy/timeseries/__init__.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+This subpackage contains classes and functions for work with time series
+data sets.
+
+"""
+
+from .timeseries import *
