a5c53280648eb49b5f818b3119633302604bf474
anaposses <anaposses@gmail.com>
2018-08-09 19:38:13 -0300

    Add test to CDData.read PL#7613

--- astropy/nddata/tests/test_ccddata.py
+++ astropy/nddata/tests/test_ccddata.py
@@ -883,7 +883,6 @@ def test_stddevuncertainty_compat_descriptor_no_weakref():
     assert uncert.parent_nddata is ccd
     uncert._parent_nddata = None
 
-    
 def test_CCDataread_returns_image(ccd_data, tmpdir):
     # Test if CCData.read returns a image when reading a fits file containing 
     #a table and image, in that order.
