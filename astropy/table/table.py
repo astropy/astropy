032fba7b9dc54ecc0944afd70a70269d2f24cc3e
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2018-08-21 12:22:20 -0400

    Address review comments

--- astropy/table/table.py
+++ astropy/table/table.py
@@ -2724,6 +2724,9 @@ class Table:
             if isinstance(column, MaskedColumn) and np.any(column.mask):
                 if column.dtype.kind in ['i', 'u']:
                     out[name] = column.astype(float).filled(np.nan)
+                    warnings.warn(
+                        "converted column '{}' from integer to float".format(
+                            name), TableReplaceWarning, stacklevel=3)
                 elif column.dtype.kind in ['f', 'c']:
                     out[name] = column.filled(np.nan)
                 else:
