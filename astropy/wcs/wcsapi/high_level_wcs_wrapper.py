24460d04bf60f5a8969f9344466db53fa9acbe63
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-09-17 21:12:59 +0100

    Split high-level API into the actual API definition, a mix-in class with default implementations, and a wrapper class for low-level WCS objects.

---
+++ astropy/wcs/wcsapi/high_level_wcs_wrapper.py
@@ -0,0 +1,61 @@
+from .base_high_level_api import HighLevelWCSMixin
+
+__all__ = ['HighLevelWCSWrapper']
+
+
+class HighLevelWCSWrapper(HighLevelWCSMixin):
+
+    def __init__(self, low_level_wcs):
+        self._low_level_wcs = low_level_wcs
+
+    def low_level_wcs(self):
+        return self._low_level_wcs
+
+    @property
+    def pixel_n_dim(self):
+        """
+        See `~BaseLowLevelWCS.world_n_dim`
+        """
+        return self.low_level_wcs.pixel_n_dim
+
+    @property
+    def world_n_dim(self):
+        """
+        See `~BaseLowLevelWCS.world_n_dim`
+        """
+        return self.low_level_wcs.world_n_dim
+
+    @property
+    def world_axis_physical_types(self):
+        """
+        See `~BaseLowLevelWCS.world_axis_physical_types`
+        """
+        return self.low_level_wcs.world_axis_physical_types
+
+    @property
+    def world_axis_units(self):
+        """
+        See `~BaseLowLevelWCS.world_axis_units`
+        """
+        return self.low_level_wcs.world_axis_units
+
+    @property
+    def array_shape(self):
+        """
+        See `~BaseLowLevelWCS.array_shape`
+        """
+        return self.low_level_wcs.array_shape
+
+    @property
+    def pixel_bounds(self):
+        """
+        See `~BaseLowLevelWCS.pixel_bounds`
+        """
+        return self.low_level_wcs.pixel_bounds
+
+    @property
+    def axis_correlation_matrix(self):
+        """
+        See `~BaseLowLevelWCS.axis_correlation_matrix`
+        """
+        return self.low_level_wcs.pixel_bounds
