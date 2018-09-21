4c3460c07d21e55ce2283f1540916bf568ff97d3
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-09-21 11:22:34 +0100

    Make set_xlabel and set_ylabel API consistent with Matplotlib 3.x but compatible with Matplotlib 2.x

--- astropy/visualization/wcsaxes/core.py
+++ astropy/visualization/wcsaxes/core.py
@@ -451,20 +451,24 @@ class WCSAxes(Axes):
 
         self._drawn = True
 
-    # The label=None, xlabel=None (and similarly for ylabel) is to ensure
+    # MATPLOTLIB_LT_30: The ``kwargs.pop('label', None)`` is to ensure
     # compatibility with Matplotlib 2.x (which has label) and 3.x (which has
     # xlabel). While these are meant to be a single positional argument,
     # Matplotlib internally sometimes specifies e.g. set_xlabel(xlabel=...).
 
-    def set_xlabel(self, label=None, xlabel=None, labelpad=1, **kwargs):
-        if label is None and xlabel is not None:
-            label = xlabel
-        self.coords[self._x_index].set_axislabel(label, minpad=labelpad, **kwargs)
+    def set_xlabel(self, xlabel=None, labelpad=1, **kwargs):
+        if xlabel is None:
+            xlabel = kwargs.pop('label', None)
+            if xlabel is None:
+                raise ValueError('xlabel should be set')
+        self.coords[self._x_index].set_axislabel(xlabel, minpad=labelpad, **kwargs)
 
-    def set_ylabel(self, label=None, ylabel=None, labelpad=1, **kwargs):
-        if label is None and ylabel is not None:
-            label = ylabel
-        self.coords[self._y_index].set_axislabel(label, minpad=labelpad, **kwargs)
+    def set_ylabel(self, ylabel=None, labelpad=1, **kwargs):
+        if ylabel is None:
+            ylabel = kwargs.pop('label', None)
+            if ylabel is None:
+                raise ValueError('ylabel should be set')
+        self.coords[self._y_index].set_axislabel(ylabel, minpad=labelpad, **kwargs)
 
     def get_xlabel(self):
         return self.coords[self._x_index].get_axislabel()
