12beed210334b2d2c41891d0ffe3bb0a3f52bb5a
James Noss <jnoss@stsci.edu>
2018-10-01 16:24:44 -0400

    move decs to header
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- astropy/convolution/src/boundary_padded.c
+++ astropy/convolution/src/boundary_padded.c
@@ -18,53 +18,6 @@
 #include <omp.h>
 #endif
 
-#include "numpy/ndarrayobject.h"
-#define DTYPE npy_float64
-
-LIB_CONVOLVE_EXPORT void convolveNd_padded_boundary_c(DTYPE * const result,
-        const DTYPE * const f,
-        const unsigned n_dim,
-        const size_t * const image_shape,
-        const DTYPE * const g,
-        const size_t * const kernel_shape,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-
-// 1D
-void convolve1d_padded_boundary_c(DTYPE * const result,
-        const DTYPE * const f, const size_t nx,
-        const DTYPE * const g, const size_t nkx,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-FORCE_INLINE void convolve1d_padded_boundary(DTYPE * const result,
-        const DTYPE * const f, const size_t nx,
-        const DTYPE * const g, const size_t nkx,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-
-// 2D
-void convolve2d_padded_boundary_c(DTYPE * const result,
-        const DTYPE * const f, const size_t nx, const size_t ny,
-        const DTYPE * const g, const size_t nkx, const size_t nky,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-FORCE_INLINE void convolve2d_padded_boundary(DTYPE * const result,
-        const DTYPE * const f, const size_t nx, const size_t ny,
-        const DTYPE * const g, const size_t nkx, const size_t nky,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-
-// 3D
-void convolve3d_padded_boundary_c(DTYPE * const result,
-        const DTYPE * const f, const size_t nx, const size_t ny, const size_t nz,
-        const DTYPE * const g, const size_t nkx, const size_t nky, const size_t nkz,
-        const bool nan_interpolate,
-        const unsigned n_threads);
-FORCE_INLINE void convolve3d_padded_boundary(DTYPE * const result,
-        const DTYPE * const f, const size_t nx, const size_t ny, const size_t nz,
-        const DTYPE * const g, const size_t nkx, const size_t nky, const size_t nkz,
-        const bool nan_interpolate,
-        const unsigned n_threads);
 
 void convolveNd_padded_boundary_c(DTYPE * const result,
         const DTYPE * const f,
