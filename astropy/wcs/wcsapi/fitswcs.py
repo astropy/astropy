f72c437981d911f6932cef09ecfa28f241a3f55d
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-10-03 18:25:05 +0100

    Make rounding consistent with the gwcs package

--- astropy/wcs/wcsapi/fitswcs.py
+++ astropy/wcs/wcsapi/fitswcs.py
@@ -138,7 +138,12 @@ class FITSWCSAPIMixin(BaseLowLevelWCS, HighLevelWCSMixin):
         return self.all_world2pix(*world_arrays, 0)
 
     def world_to_array_index_values(self, *world_arrays):
-        return np.round(self.all_world2pix(*world_arrays, 0)[::-1]).astype(int)
+        pixel_arrays = self.all_world2pix(*world_arrays, 0)[::-1]
+        array_indices = tuple(np.asarray(np.floor(pixel + 0.5), dtype=np.int) for pixel in pixel_arrays)
+        if len(array_indices) == 1:
+            return array_indices[0]
+        else:
+            return array_indices
 
     @property
     def world_axis_object_components(self):
