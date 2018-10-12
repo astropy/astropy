41742715442e6d494506be82f92f0e384cdfa1a9
Daniel D'Avella <ddavella@stsci.edu>
2018-10-12 11:47:24 -0400

    Workaround for tests suite with newer versions of pytest

--- astropy/tests/pytest_plugins.py
+++ astropy/tests/pytest_plugins.py
@@ -14,6 +14,11 @@ from ..utils.exceptions import AstropyDeprecationWarning
 from .helper import enable_deprecations_as_exceptions
 from .plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 
+# This makes sure that this module is not collected when running the test
+# suite. This is necessary in order to get the test suite to run without errors
+# using pytest>=3.7
+import pytest
+pytest.skip()
 
 _warning_message = "The module `astropy.tests.pytest_plugins has been " \
     "deprecated. The variables `PYTEST_HEADER_MODULES` and `TESTED_VERSIONS`" \
