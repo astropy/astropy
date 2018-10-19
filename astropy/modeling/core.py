629d6cfa6c0ebe06e6f267befb58ccbb3b0d7044
Perry Greenfield <perry@stsci.edu>
2018-10-19 11:30:50 -0400

    fix issue with order of access of _param_metrics for compound models, test fix 2

--- astropy/modeling/core.py
+++ astropy/modeling/core.py
@@ -2074,7 +2074,7 @@ class Model(metaclass=_ModelMeta):
         entirely in the near future.
         """
 
-        param_metrics = self._param_metrics
+        #param_metrics = self._param_metrics
         values = []
         shapes = []
         for name in self.param_names:
@@ -2085,7 +2085,7 @@ class Model(metaclass=_ModelMeta):
             else:
                 value = param.value
 
-            broadcast_shape = param_metrics[name].get('broadcast_shape')
+            broadcast_shape = self._param_metrics[name].get('broadcast_shape')
             if broadcast_shape is not None:
                 value = value.reshape(broadcast_shape)
 
@@ -2800,8 +2800,8 @@ class CompoundModel(Model):
         # the assumption here is reliable:
         if self.param_names is None:
             raise RuntimeError("Compound model parameter interface is not "
-                               "supported\n"
-                               "until the .map_parameters() method is called.")
+                              "supported\n"
+                              "until the .map_parameters() method is called.")
 
         self._parameters_to_array()
         start = self._param_metrics[self.param_names[0]]['slice'].start
