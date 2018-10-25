b151cae0b4b531d4451905b8dcf756649a3ff5dd
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-25 17:18:04 -0400

    Added license header back in

--- astropy/units/function/magnitude_zero_points.py
+++ astropy/units/function/magnitude_zero_points.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
 """
 This module has been deprecated and moved to astropy.units.photometric.  The
 names remain here for backwards compatibility.
