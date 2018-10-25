e88419cb4ae97f88bb14a1e218874a05d97bfea6
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-25 12:25:50 -0400

    add little-h units

--- astropy/units/astrophys.py
+++ astropy/units/astrophys.py
@@ -160,6 +160,15 @@ def_unit(['adu'], namespace=_ns, prefixes=True)
 def_unit(['beam'], namespace=_ns, prefixes=True)
 def_unit(['electron'], doc="Number of electrons", namespace=_ns,
          format={'latex': r'e^{-}', 'unicode': 'e⁻'})
+# This is not formally a unit, but is used in that way in many contexts, and
+# an appropriate equivalency is only possible if it's treated as a unit (see
+# https://arxiv.org/pdf/1308.4150.pdf for more)
+def_unit(['h100', 'littleh'], namespace=_ns, prefixes=False,
+         doc="Reduced/\"dimensionless\" Hubble constant",
+         format={'latex': r'h_{100}'})
+def_unit(['h70'], 0.7 * h100, namespace=_ns, prefixes=False,
+         doc="Reduced/\"dimensionless\" Hubble constant - 70 km/s/Mpc",
+         format={'latex': r'h_{70}'})
 
 
 ###########################################################################
