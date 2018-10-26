bf505bf055eebfebe8e54b43d9c48dd00a2b9080
Mihai Cara <mihail.cara@gmail.com>
2018-10-26 03:26:43 -0400

    Add narrative help and thorough tests

--- astropy/nddata/bitmask.py
+++ astropy/nddata/bitmask.py
@@ -1,25 +1,25 @@
 """
-A module that provides functions for manipulating bitmasks and data quality
+A module that provides functions for manipulating bit masks and data quality
 (DQ) arrays.
 
 """
 
 import sys
 import warnings
+import numbers
 import numpy as np
 
 
-__all__ = ['bitfield_to_boolean_mask', 'interpret_bit_flags', 'is_bit_flag']
+__all__ = ['bitfield_to_boolean_mask', 'interpret_bit_flags']
 
 
-INT_TYPE = (int, long,) if sys.version_info < (3,) else (int,)
-MAX_UINT_TYPE = np.maximum_sctype(np.uint)
-SUPPORTED_FLAGS = int(np.bitwise_not(
-    0, dtype=MAX_UINT_TYPE, casting='unsafe'
+_MAX_UINT_TYPE = np.maximum_sctype(np.uint)
+_SUPPORTED_FLAGS = int(np.bitwise_not(
+    0, dtype=_MAX_UINT_TYPE, casting='unsafe'
 ))
 
 
-def is_bit_flag(n):
+def _is_bit_flag(n):
     """
     Verifies if the input number is a bit flag (i.e., an integer number that is
     an integer power of 2).
@@ -44,47 +44,47 @@ def is_bit_flag(n):
 
 def _is_int(n):
     return (
-        (isinstance(n, INT_TYPE) and not isinstance(n, bool)) or
+        (isinstance(n, numbers.Integral) and not isinstance(n, bool)) or
         (isinstance(n, np.generic) and np.issubdtype(n, np.integer))
     )
 
 
 def interpret_bit_flags(bit_flags, flip_bits=None):
     """
-    Converts input bit flags to a single integer value (bitmask) or `None`.
+    Converts input bit flags to a single integer value (bit mask) or `None`.
 
     When input is a list of flags (either a Python list of integer flags or a
-    sting of comma- or '+'-separated list of flags), the returned bitmask
+    sting of comma- or '+'-separated list of flags), the returned bit mask
     is obtained by summing input flags.
 
     .. note::
-        In order to flip the bits of the returned bitmask,
+        In order to flip the bits of the returned bit mask,
         for input of `str` type, prepend '~' to the input string. '~' must
         be prepended to the *entire string* and not to each bit flag! For
-        input that is already a bitmask or a Python list of bit flags, set
+        input that is already a bit mask or a Python list of bit flags, set
         ``flip_bits`` for `True` in order to flip the bits of the returned
-        bitmask.
+        bit mask.
 
     Parameters
     ----------
     bit_flags : int, str, list, None
-        An integer bitmask or flag, `None`, a string of comma- or
+        An integer bit mask or flag, `None`, a string of comma- or
         '+'-separated list of integer bit flags, or a Python list of integer
         bit flags. If ``bit_flags`` is a `str` and if it is prepended with '~',
-        then the output bitmask will have its bits flipped (compared to simple
-        sum of input flags). For input ``bit_flags`` that is already a bitmask
+        then the output bit mask will have its bits flipped (compared to simple
+        sum of input flags). For input ``bit_flags`` that is already a bit mask
         or a Python list of bit flags, bit-flipping can be controlled through
         ``flip_bits`` parameter.
 
     flip_bits : bool, None
-        Indicates whether or not to flip the bits of the returned bitmask
+        Indicates whether or not to flip the bits of the returned bit mask
         obtained from input bit flags. This parameter must be set to `None`
         when input ``bit_flags`` is either `None` or a Python list of flags.
 
     Returns
     -------
     bitmask : int or None
-        Returns and integer bit mask formed from the input bit value or `None`
+        Returns an integer bit mask formed from the input bit value or `None`
         if input ``bit_flags`` parameter is `None` or an empty string.
         If input string value was prepended with '~' (or ``flip_bits`` was set
         to `True`), then returned value will have its bits flipped
@@ -194,9 +194,9 @@ def interpret_bit_flags(bit_flags, flip_bits=None):
 
     bitmask = 0
     for v in bitset:
-        if not is_bit_flag(v) and not allow_non_flags:
+        if not _is_bit_flag(v) and not allow_non_flags:
             raise ValueError("Input list contains invalid (not powers of two) "
-                             "bit flags")
+                             "bit flag: {:d}".format(v))
         bitmask += v
 
     if flip_bits:
@@ -211,7 +211,7 @@ def bitfield_to_boolean_mask(bitfield, ignore_flags=0, flip_bits=None,
     bitfield_to_boolean_mask(bitfield, ignore_flags=None, flip_bits=None, \
 good_mask_value=False, dtype=numpy.bool_)
     Converts an array of bit fields to a boolean (or integer) mask array
-    according to a bitmask constructed from the supplied bit flags (see
+    according to a bit mask constructed from the supplied bit flags (see
     ``ignore_flags`` parameter).
 
     This function is particularly useful to convert data quality arrays to
@@ -226,7 +226,7 @@ good_mask_value=False, dtype=numpy.bool_)
         selectively ignore some bits in the ``bitfield`` array data.
 
     ignore_flags : int, str, list, None (Default = 0)
-        An integer bitmask, a Python list of bit flags, a comma- or
+        An integer bit mask, a Python list of bit flags, a comma- or
         '+'-separated string list of integer bit flags that indicate what
         bits in the input ``bitfield`` should be *ignored* (i.e., zeroed), or
         `None`.
@@ -235,13 +235,13 @@ good_mask_value=False, dtype=numpy.bool_)
           `bitfield_to_boolean_mask` interpret all ``bitfield`` elements
           as "good" regardless of their value.
 
-        | When ``ignore_flags`` argument is an integer bitmask, it will be
+        | When ``ignore_flags`` argument is an integer bit mask, it will be
           combined using bitwise-NOT and bitwise-AND with each element of the
           input ``bitfield`` array (``~ignore_flags & bitfield``). If the
           resultant bitfield element is non-zero, that element will be
           interpreted as a "bad" in the output boolean mask and it will be
           interpreted as "good" otherwise. ``flip_bits`` parameter may be used
-          to flip the bits (``bitwise-NOT``) of the bitmask thus effectively
+          to flip the bits (``bitwise-NOT``) of the bit mask thus effectively
           changing the meaning of the ``ignore_flags`` parameter from "ignore"
           to "use only" these flags.
 
@@ -252,14 +252,14 @@ good_mask_value=False, dtype=numpy.bool_)
             interpreted as "bad".
 
         | When ``ignore_flags`` argument is a Python list of integer bit
-          flags, these flags are added together to create an integer bitmask.
+          flags, these flags are added together to create an integer bit mask.
           Each item in the list must be a flag, i.e., an integer that is an
           integer power of 2. In order to flip the bits of the resultant
-          bitmask, use ``flip_bits`` parameter.
+          bit mask, use ``flip_bits`` parameter.
 
         | Alternatively, ``ignore_flags`` may be a string of comma- or
           '+'-separated list of integer bit flags that should be added together
-          to create an integer bitmask. For example, both ``'4,8'`` and
+          to create an integer bit mask. For example, both ``'4,8'`` and
           ``'4+8'`` are equivalent and indicate that bit flags 4 and 8 in
           the input ``bitfield`` array should be ignored when generating
           boolean mask.
@@ -275,7 +275,7 @@ good_mask_value=False, dtype=numpy.bool_)
             Each item in the list must be a flag, i.e., an integer that is an
             integer power of 2. In addition, for convenience, an arbitrary
             **single** integer is allowed and it will be interpretted as an
-            integer bitmask. For example, instead of ``'4,8'`` one could
+            integer bit mask. For example, instead of ``'4,8'`` one could
             simply provide string ``'12'``.
 
         .. note::
@@ -295,14 +295,14 @@ good_mask_value=False, dtype=numpy.bool_)
             with string ``ignore_flags`` and it must be set to `None`.
 
     flip_bits : bool, None (Default = None)
-        Specifies whether or not to invert the bits of the bitmask either
+        Specifies whether or not to invert the bits of the bit mask either
         supplied directly through ``ignore_flags`` parameter or built from the
         bit flags passed through ``ignore_flags`` (only when bit flags are
         passed as Python lists of integer bit flags). Occasionally, it may be
         useful to *consider only specific bit flags* in the ``bitfield``
         array when creating a boolean mask as opposite to *ignoring* specific
         bit flags as ``ignore_flags`` behaves by default. This can be achieved
-        by inverting/flipping the bits of the bitmask created from
+        by inverting/flipping the bits of the bit mask created from
         ``ignore_flags`` flags which effectively changes the meaning of the
         ``ignore_flags`` parameter from "ignore" to "use only" these flags.
         Setting ``flip_bits`` to `None` means that no bit flipping will be
@@ -312,7 +312,7 @@ good_mask_value=False, dtype=numpy.bool_)
 
         .. warning::
             This parameter can be set to either `True` or `False` **ONLY** when
-            ``ignore_flags`` is either an integer bitmask or a Python
+            ``ignore_flags`` is either an integer bit mask or a Python
             list of integer bit flags. When ``ignore_flags`` is either
             `None` or a string list of flags, ``flip_bits`` **MUST** be set
             to `None`.
@@ -341,8 +341,8 @@ good_mask_value=False, dtype=numpy.bool_)
     mask : numpy.ndarray
         Returns an array of the same dimensionality as the input ``bitfield``
         array whose elements can have two possible values,
-        e.g., ``numpy.True_`` or ``numpy.False_`` (or 1 or 0 for integer ``dtype``)
-        according to values of to the input ``bitfield`` elements,
+        e.g., ``numpy.True_`` or ``numpy.False_`` (or 1 or 0 for integer
+        ``dtype``) according to values of to the input ``bitfield`` elements,
         ``ignore_flags`` parameter, and the ``good_mask_value`` parameter.
 
     Examples
@@ -396,7 +396,7 @@ good_mask_value=False, dtype=numpy.bool_)
         return mask
 
     # filter out bits beyond the maximum supported by the data type:
-    ignore_mask = ignore_mask & SUPPORTED_FLAGS
+    ignore_mask = ignore_mask & _SUPPORTED_FLAGS
 
     # invert the "ignore" mask:
     ignore_mask = np.bitwise_not(ignore_mask, dtype=bitfield.dtype,
--- astropy/nddata/tests/test_bitmask.py
+++ astropy/nddata/tests/test_bitmask.py
@@ -1,81 +1,182 @@
-# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+A module containing unit tests for the `bitmask` modue.
 
+Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+import warnings
 import numpy as np
 import pytest
 
-from ...tests.helper import catch_warnings
-from ..bitmask import bitfield_to_boolean_mask
+from .. import bitmask
 
 
-def test_bitfield_not_integer():
+MAX_INT_TYPE = np.maximum_sctype(np.int)
+MAX_UINT_TYPE = np.maximum_sctype(np.uint)
+MAX_UINT_FLAG = np.left_shift(
+    MAX_UINT_TYPE(1),
+    MAX_UINT_TYPE(np.iinfo(MAX_UINT_TYPE).bits - 1)
+)
+MAX_INT_FLAG = np.left_shift(
+    MAX_INT_TYPE(1),
+    MAX_INT_TYPE(np.iinfo(MAX_INT_TYPE).bits - 2)
+)
+SUPER_LARGE_FLAG = 1 << np.iinfo(MAX_UINT_TYPE).bits
+EXTREME_TEST_DATA = np.array([
+        0, 1, 1 + 1 << 2, MAX_INT_FLAG, ~0, MAX_INT_TYPE(MAX_UINT_FLAG),
+        1 + MAX_INT_TYPE(MAX_UINT_FLAG)
+], dtype=MAX_INT_TYPE)
+
+
+@pytest.mark.parametrize('flag', [0, -1])
+def test_nonpositive_not_a_bit_flag(flag):
+    assert not bitmask._is_bit_flag(n=flag)
+
+
+@pytest.mark.parametrize('flag', [
+    1, MAX_UINT_FLAG, int(MAX_UINT_FLAG), SUPER_LARGE_FLAG
+])
+def test_is_bit_flag(flag):
+    assert bitmask._is_bit_flag(n=flag)
+
+
+@pytest.mark.parametrize('number', [0, 1, MAX_UINT_FLAG, SUPER_LARGE_FLAG])
+def test_is_int(number):
+    assert bitmask._is_int(number)
+
+
+@pytest.mark.parametrize('number', ['1', True, 1.0])
+def test_nonint_is_not_an_int(number):
+    assert not bitmask._is_int(number)
+
+
+@pytest.mark.parametrize('flag,flip,expected', [
+    (3, None, 3),
+    (3, True, -4),
+    (3, False, 3),
+    ([1, 2], False, 3),
+    ([1, 2], True, -4)
+])
+def test_interpret_valid_int_bit_flags(flag, flip, expected):
+    assert(
+        bitmask.interpret_bit_flags(bit_flags=flag, flip_bits=flip) == expected
+    )
+
+
+@pytest.mark.parametrize('flag', [None, ' ', 'None', 'Indef'])
+def test_interpret_none_bit_flags_as_None(flag):
+    assert bitmask.interpret_bit_flags(bit_flags=flag) is None
+
+
+@pytest.mark.parametrize('flag,expected', [
+    ('1', 1),
+    ('~-1', ~(-1)),
+    ('~1', ~1),
+    ('1,2', 3),
+    ('1+2', 3),
+    ('(1,2)', 3),
+    ('(1+2)', 3),
+    ('~1,2', ~3),
+    ('~1+2', ~3),
+    ('~(1,2)', ~3),
+    ('~(1+2)', ~3)
+])
+def test_interpret_valid_str_bit_flags(flag, expected):
+    assert(
+        bitmask.interpret_bit_flags(bit_flags=flag) == expected
+    )
+
+
+@pytest.mark.parametrize('flag,flip', [
+    (None, True),
+    (' ', True),
+    ('None', True),
+    ('Indef', True),
+    (None, False),
+    (' ', False),
+    ('None', False),
+    ('Indef', False),
+    ('1', True),
+    ('1', False)
+])
+def test_interpret_None_or_str_and_flip_incompatibility(flag, flip):
     with pytest.raises(TypeError):
-        bitfield_to_boolean_mask(np.random.random((10, 10)))
+        bitmask.interpret_bit_flags(bit_flags=flag, flip_bits=flip)
 
 
-def test_bitfield_negative_flags():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, [-1])
-
-
-def test_bitfield_non_poweroftwo_flags():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, [3])
-
-
-def test_bitfield_flipbits_when_no_bits():
-    bm = np.random.randint(0, 10, (10, 10))
+@pytest.mark.parametrize('flag', [True, 1.0, [1.0], object])
+def test_interpret_wrong_flag_type(flag):
     with pytest.raises(TypeError):
-        bitfield_to_boolean_mask(bm, None, flip_bits=1)
+        bitmask.interpret_bit_flags(bit_flags=flag)
 
 
-def test_bitfield_flipbits_when_stringbits():
-    bm = np.random.randint(0, 10, (10, 10))
+@pytest.mark.parametrize('flag', ['SOMETHING', '1.0,2,3'])
+def test_interpret_wrong_string_int_format(flag):
+    with pytest.raises(ValueError):
+        bitmask.interpret_bit_flags(bit_flags=flag)
+
+
+def test_interpret_duplicate_flag_warning():
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        assert bitmask.interpret_bit_flags([2, 4, 4]) == 6
+        assert len(w)
+        assert issubclass(w[-1].category, UserWarning)
+        assert "Duplicate" in str(w[-1].message)
+
+
+@pytest.mark.parametrize('flag', [[1, 2, 3], '1, 2, 3'])
+def test_interpret_non_flag(flag):
+    with pytest.raises(ValueError):
+        bitmask.interpret_bit_flags(bit_flags=flag)
+
+
+def test_interpret_allow_single_value_str_nonflags():
+    assert bitmask.interpret_bit_flags(bit_flags=str(3)) == 3
+
+
+@pytest.mark.parametrize('flag', [
+    '~',
+    '( )',
+    '(~1,2)',
+    '~(1,2',
+    '1,~2',
+    '1,(2,4)',
+    '1,2+4',
+    '1+4,2'
+])
+def test_interpret_bad_str_syntax(flag):
+    with pytest.raises(ValueError):
+        bitmask.interpret_bit_flags(bit_flags=flag)
+
+
+def test_bitfield_must_be_integer_check():
     with pytest.raises(TypeError):
-        bitfield_to_boolean_mask(bm, '3', flip_bits=1)
+        bitmask.bitfield_to_boolean_mask(1.0, 1)
 
 
-def test_bitfield_string_flag_flip_not_start_of_string():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, '1, ~4')
+@pytest.mark.parametrize('data,flags,flip,goodval,dtype,ref', [
+    (EXTREME_TEST_DATA, None, None, True, np.bool_,
+     EXTREME_TEST_DATA.size * [1]),
+    (EXTREME_TEST_DATA, None, None, False, np.bool_,
+     EXTREME_TEST_DATA.size * [0]),
+    (EXTREME_TEST_DATA, [1, MAX_UINT_FLAG], False, True, np.bool_,
+     [1, 1, 0, 0, 0, 1, 1]),
+    (EXTREME_TEST_DATA, None, None, True, np.bool_,
+     EXTREME_TEST_DATA.size * [1]),
+    (EXTREME_TEST_DATA, [1, MAX_UINT_FLAG], False, False, np.bool_,
+     [0, 0, 1, 1, 1, 0, 0]),
+    (EXTREME_TEST_DATA, [1, MAX_UINT_FLAG], True, True, np.int8,
+     [1, 0, 1, 1, 0, 0, 0])
+])
+def test_bitfield_to_boolean_mask(data, flags, flip, goodval, dtype, ref):
+    mask = bitmask.bitfield_to_boolean_mask(
+        bitfield=data,
+        ignore_flags=flags,
+        flip_bits=flip,
+        good_mask_value=goodval,
+        dtype=dtype
+    )
 
-
-def test_bitfield_string_flag_unbalanced_parens():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, '(1, 4))')
-
-
-def test_bitfield_string_flag_wrong_positioned_parens():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, '((1, )4)')
-
-
-def test_bitfield_string_flag_empty():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(ValueError):
-        bitfield_to_boolean_mask(bm, '~')
-
-
-def test_bitfield_flag_non_integer():
-    bm = np.random.randint(0, 10, (10, 10))
-    with pytest.raises(TypeError):
-        bitfield_to_boolean_mask(bm, [1.3])
-
-
-def test_bitfield_duplicate_flag_throws_warning():
-    bm = np.random.randint(0, 10, (10, 10))
-    with catch_warnings(UserWarning) as w:
-        bitfield_to_boolean_mask(bm, [1, 1])
-    assert len(w)
-
-
-def test_bitfield_none_identical_to_strNone():
-    bm = np.random.randint(0, 10, (10, 10))
-    m1 = bitfield_to_boolean_mask(bm, None)
-    m2 = bitfield_to_boolean_mask(bm, 'None')
-    np.testing.assert_array_equal(m1, m2)
-
+    assert(mask.dtype == dtype)
+    assert np.all(mask == ref)
