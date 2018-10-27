b5cce79901ca314fc2d5fef00c838fed9fd5de88
Erik Tollerud <erik.tollerud@gmail.com>
2018-10-26 21:13:22 -0400

    preserve magnitude_zero_points context manager behavior

--- astropy/units/function/magnitude_zero_points.py
+++ astropy/units/function/magnitude_zero_points.py
@@ -8,10 +8,16 @@ names remain here for backwards compatibility.
 from warnings import warn
 
 from ..photometric import AB, ST
+from ...utils import deprecated
 
 _ns = globals()
 
 
+@deprecated(since='3.1', alternative='astropy.units.photometric',
+            message='The magnitude_zero_points module has been deprecated, and'
+                    ' moved to astropy.units.photometric and are enabled by '
+                    'default. magnitude_zero_points is retained as aliases to '
+                    'the new units.')
 def enable():
     """
     Enable magnitude zero point units so they appear in results of
@@ -21,6 +27,12 @@ def enable():
     This may be used with the ``with`` statement to enable these
     units only temporarily.
     """
-    warn('The magnitude_zero_points module has been deprecated, and moved to '
-         'astropy.units.photometric and are enabled by default. '
-         'magnitude_zero_points is retained as aliases to the new units.')
+    # While it may seem like the below can be removed, in fact it needs to
+    # remain as long as this function is around so that enable acts as a context
+    # manager
+
+    # Local import to avoid cyclical import
+    from ..core import add_enabled_units
+    # Local import to avoid polluting namespace
+    import inspect
+    return add_enabled_units(inspect.getmodule(enable))
