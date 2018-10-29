e0eba254970887bbca9bb7aa771cd65483d475b8
Larry Bradley <larry.bradley@gmail.com>
2018-10-29 16:12:58 -0400

    Merge pull request #8007 from astrofrog/fix-more-warnings
    TST: Fix warnings in visualization

--- astropy/visualization/wcsaxes/formatter_locator.py
+++ astropy/visualization/wcsaxes/formatter_locator.py
@@ -56,7 +56,7 @@ class BaseFormatterLocator:
 
     def __init__(self, values=None, number=None, spacing=None, format=None):
 
-        if (values, number, spacing).count(None) < 2:
+        if len([x for x in (values, number, spacing) if x is None]) < 2:
             raise ValueError("At most one of values/number/spacing can be specifed")
 
         if values is not None:
