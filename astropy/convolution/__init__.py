cc74f3b2d45f8e45b7774231fafcce9ad7dcc113
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-10-30 08:46:15 +0000

    Reduce flake8 warnings

--- astropy/convolution/__init__.py
+++ astropy/convolution/__init__.py
@@ -1,13 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 
-from .core import *
-from .kernels import *
-from .utils import discretize_model
+from .core import *  # noqa
+from .kernels import *  # noqa
+from .utils import discretize_model  # noqa
 
 try:
     # Not guaranteed available at setup time
-    from .convolve import convolve, convolve_fft, interpolate_replace_nans, convolve_models
+    from .convolve import convolve, convolve_fft, interpolate_replace_nans, convolve_models  # noqa
 except ImportError:
     if not _ASTROPY_SETUP_:
         raise
