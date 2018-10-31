66d113c460182dc5ea4348866831cdfe1507b6c0
Daniel D'Avella <ddavella@stsci.edu>
2018-10-31 09:38:28 -0400

    Remove deprecated usage of AsdfFile.open

--- astropy/io/misc/asdf/tags/unit/tests/test_unit.py
+++ astropy/io/misc/asdf/tags/unit/tests/test_unit.py
@@ -18,12 +18,12 @@ unit: !unit/unit-1.0.0 "2.1798721  10-18kg m2 s-2"
     """
 
     buff = helpers.yaml_to_asdf(yaml)
-    with asdf.AsdfFile.open(buff) as ff:
+    with asdf.open(buff) as ff:
         assert ff.tree['unit'].is_equivalent(u.Ry)
 
         buff2 = io.BytesIO()
         ff.write_to(buff2)
 
     buff2.seek(0)
-    with asdf.AsdfFile.open(buff2) as ff:
+    with asdf.open(buff2) as ff:
         assert ff.tree['unit'].is_equivalent(u.Ry)
