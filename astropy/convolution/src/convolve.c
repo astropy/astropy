b92892c65ae653dd9b6d3be32f0d904da74956f7
James Noss <jnoss@stsci.edu>
2018-11-01 16:12:55 -0400

    Transparent kernel access
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- astropy/convolution/src/convolve.c
+++ astropy/convolution/src/convolve.c
@@ -217,13 +217,14 @@ FORCE_INLINE void convolve1d(DTYPE * const result,
     // when threaded. Without these, compile time constant conditionals may
     // not be optimized away.
     const size_t nx = _nx;
+    const size_t nkx = _nkx;
+    const size_t nkx_minus_1 = nkx - 1;
     const bool nan_interpolate = _nan_interpolate;
     const bool padded = _padded;
 
     // Thread locals
     const size_t wkx = _wkx;
     size_t wkx_plus_i;
-    size_t ker_i;
     const omp_iter_var nx_minus_wkx = nx - wkx;
     size_t i_minus_wkx;
     omp_iter_var i_plus_wkx_plus_1;
@@ -245,11 +246,10 @@ FORCE_INLINE void convolve1d(DTYPE * const result,
         if (nan_interpolate) // compile time constant
             bot = 0.;
         {omp_iter_var ii;
-        for (ii = i_minus_wkx; ii < i_plus_wkx_plus_1; ++ii)
+        for (ii = 0; ii < nkx; ++ii)
         {
-            ker_i = wkx_plus_i - ii; // nkx - 1 - (wkx + ii - i)
-            val = f[ii];
-            ker = g[ker_i];
+            val = f[i_minus_wkx + ii];
+            ker = g[nkx_minus_1 - ii];
             if (nan_interpolate) // compile time constant
             {
                 if (!isnan(val))
@@ -319,7 +319,8 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
     // when threaded. Without these, compile time constant conditionals may
     // not be optimized away.
     const size_t nx = _nx, ny = _ny;
-    const size_t nky = _nky;
+    const size_t nkx = _nkx, nky = _nky;
+    const size_t nkx_minus_1 = nkx - 1, nky_minus_1 = nky - 1;
     const bool nan_interpolate = _nan_interpolate;
     const bool padded = _padded;
 
@@ -327,7 +328,6 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
     const size_t wkx = _wkx;
     const size_t wky = _wky;
     size_t wkx_plus_i, wky_plus_j;
-    size_t ker_i, ker_j;
     const omp_iter_var nx_minus_wkx = nx - wkx;
     const omp_iter_var ny_minus_wky = ny - wky;
     const size_t ny_minus_2wky = ny - 2 * wky;
@@ -358,15 +358,13 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
             if (nan_interpolate) // compile time constant
                 bot = 0.;
             {omp_iter_var ii;
-            for (ii = i_minus_wkx; ii < i_plus_wkx_plus_1; ++ii)
+            for (ii = 0; ii < nkx; ++ii)
             {
-                ker_i = wkx_plus_i - ii; // nkx - 1 - (wkx + ii - i)
                 {omp_iter_var jj;
-                for (jj = j_minus_wky; jj < j_plus_wky_plus_1; ++jj)
+                for (jj = 0; jj < nky; ++jj)
                 {
-                    ker_j = wky_plus_j - jj; // nky - 1 - (wky + jj - j)
-                    val = f[ii*ny + jj]; //[ii, jj];
-                    ker = g[ker_i*nky + ker_j]; // [ker_i, ker_j];
+                    val = f[(i_minus_wkx+ii)*ny + (j_minus_wky+jj)];
+                    ker = g[(nkx_minus_1 - ii)*nky + (nky_minus_1 - jj)];
                     if (nan_interpolate) // compile time constant
                     {
                         if (!isnan(val))
@@ -440,7 +438,8 @@ FORCE_INLINE void convolve3d(DTYPE * const result,
     // when threaded. Without these, compile time constant conditionals may
     // not be optimized away.
     const size_t nx = _nx, ny = _ny, nz = _nz;
-    const size_t nky = _nky, nkz = _nkz;
+    const size_t nkx = _nkx, nky = _nky, nkz = _nkz;
+    const size_t nkx_minus_1 = nkx - 1, nky_minus_1 = nky - 1, nkz_minus_1 = nkz - 1;
     const bool nan_interpolate = _nan_interpolate;
     const bool padded = _padded;
 
@@ -449,7 +448,6 @@ FORCE_INLINE void convolve3d(DTYPE * const result,
     const size_t wky = _wky;
     const size_t wkz = _wkz;
     size_t wkx_plus_i, wky_plus_j, wkz_plus_k;
-    size_t ker_i, ker_j, ker_k;
     const size_t nx_minus_wkx = nx - wkx;
     const omp_iter_var ny_minus_wky = ny - wky;
     const omp_iter_var nz_minus_wkz = nz - wkz;
@@ -490,20 +488,16 @@ FORCE_INLINE void convolve3d(DTYPE * const result,
                 if (nan_interpolate) // compile time constant
                     bot = 0.;
                 {omp_iter_var ii;
-                for (ii = i_minus_wkx; ii < i_plus_wkx_plus_1; ++ii)
+                for (ii = 0; ii < nkx; ++ii)
                 {
-                    ker_i = wkx_plus_i - ii; // nkx - 1 - (wkx + ii - i)
                     {omp_iter_var jj;
-                    for (jj = j_minus_wky; jj < j_plus_wky_plus_1; ++jj)
+                    for (jj = 0; jj < nky; ++jj)
                     {
-                        ker_j = wky_plus_j - jj; // nky - 1 - (wky + jj - j)
                         {omp_iter_var kk;
-                        for (kk = k_minus_wkz; kk < k_plus_wkz_plus_1; ++kk)
+                        for (kk = 0; kk < nkz; ++kk)
                         {
-                            ker_k = wkz_plus_k - kk; // nkz - 1 - (wkz + kk - k)
-
-                            val = f[(ii*ny + jj)*nz + kk]; //[ii, jj, kk];
-                            ker = g[(ker_i*nky + ker_j)*nkz + ker_k]; // [ker_i, ker_j, ker_k];
+                            val = f[((i_minus_wkx + ii)*ny + (j_minus_wky + jj))*nz + (k_minus_wkz + kk)];
+                            ker = g[((nkx_minus_1 - ii)*nky + (nky_minus_1 - jj))*nkz + (nkz_minus_1 - kk)];
                             if (nan_interpolate) // compile time constant
                             {
                                 if (!isnan(val))
