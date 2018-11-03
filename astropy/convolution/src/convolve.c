5eef12ef2654a95627040ad9dd846d677a4b7aa9
James Noss <jnoss@stsci.edu>
2018-11-02 21:02:22 -0400

    padded -> assume_results_array_padded_by_half_kernel_size
    Signed-off-by: James Noss <jnoss@stsci.edu>

--- astropy/convolution/src/convolve.c
+++ astropy/convolution/src/convolve.c
@@ -20,7 +20,7 @@
  * index when accessing the results array - for example in the 1D case we shift
  * the index in the results array compared to the input array by half the kernel
  * size. This is done via the 'result_index' variable, and this behavior is
- * triggered by the 'padded' setting.
+ * triggered by the 'assume_results_array_padded_by_half_kernel_size' setting.
  *
  */
 
@@ -54,7 +54,7 @@ void convolveNd_c(DTYPE * const result,
         const DTYPE * const g,
         const size_t * const kernel_shape,
         const bool nan_interpolate,
-        const bool padded,
+        const bool assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -72,17 +72,23 @@ void convolveNd_c(DTYPE * const result,
         convolve1d_c(result, f,
                 image_shape[0],
                 g, kernel_shape[0],
-                nan_interpolate, padded, n_threads);
+                nan_interpolate,
+                assume_results_array_padded_by_half_kernel_size,
+                n_threads);
     else if (n_dim == 2)
         convolve2d_c(result, f,
                 image_shape[0], image_shape[1],
                 g, kernel_shape[0], kernel_shape[1],
-                nan_interpolate, padded, n_threads);
+                nan_interpolate,
+                assume_results_array_padded_by_half_kernel_size,
+                n_threads);
     else if (n_dim == 3)
         convolve3d_c(result, f,
                         image_shape[0], image_shape[1], image_shape[2],
                         g, kernel_shape[0], kernel_shape[1], kernel_shape[2],
-                        nan_interpolate, padded, n_threads);
+                        nan_interpolate,
+                        assume_results_array_padded_by_half_kernel_size,
+                        n_threads);
     else
         assert(0); // Unimplemented: n_dim > 3
 }
@@ -101,7 +107,8 @@ void convolveNd_c(DTYPE * const result,
 void convolve1d_c(DTYPE * const result,
         const DTYPE * const f, const size_t nx,
         const DTYPE * const g, const size_t nkx,
-        const bool nan_interpolate, const bool padded,
+        const bool nan_interpolate,
+        const bool assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -114,12 +121,12 @@ void convolve1d_c(DTYPE * const result,
 #endif
 
     if (nan_interpolate) {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve1d(result, f, nx, g, nkx, true, true, n_threads);
       else
         convolve1d(result, f, nx, g, nkx, true, false, n_threads);
     } else {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve1d(result, f, nx, g, nkx, false, true, n_threads);
       else
         convolve1d(result, f, nx, g, nkx, false, false, n_threads);
@@ -129,7 +136,8 @@ void convolve1d_c(DTYPE * const result,
 void convolve2d_c(DTYPE * const result,
         const DTYPE * const f, const size_t nx, const size_t ny,
         const DTYPE * const g, const size_t nkx, const size_t nky,
-        const bool nan_interpolate, const bool padded,
+        const bool nan_interpolate,
+        const bool assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -142,12 +150,12 @@ void convolve2d_c(DTYPE * const result,
 #endif
 
     if (nan_interpolate) {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve2d(result, f, nx, ny, g, nkx, nky, true, true, n_threads);
       else
         convolve2d(result, f, nx, ny, g, nkx, nky, true, false, n_threads);
     } else {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve2d(result, f, nx, ny, g, nkx, nky, false, true, n_threads);
       else
         convolve2d(result, f, nx, ny, g, nkx, nky, false, false, n_threads);
@@ -157,7 +165,8 @@ void convolve2d_c(DTYPE * const result,
 void convolve3d_c(DTYPE * const result,
         const DTYPE * const f, const size_t nx, const size_t ny, const size_t nz,
         const DTYPE * const g, const size_t nkx, const size_t nky, const size_t nkz,
-        const bool nan_interpolate, const bool padded,
+        const bool nan_interpolate,
+        const bool assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -170,12 +179,12 @@ void convolve3d_c(DTYPE * const result,
 #endif
 
     if (nan_interpolate) {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve3d(result, f, nx, ny, nz, g, nkx, nky, nkz, true, true, n_threads);
       else
         convolve3d(result, f, nx, ny, nz, g, nkx, nky, nkz, true, false, n_threads);
     } else {
-      if (padded)
+      if (assume_results_array_padded_by_half_kernel_size)
         convolve3d(result, f, nx, ny, nz, g, nkx, nky, nkz, false, true, n_threads);
       else
         convolve3d(result, f, nx, ny, nz, g, nkx, nky, nkz, false, false, n_threads);
@@ -186,7 +195,8 @@ void convolve3d_c(DTYPE * const result,
 FORCE_INLINE void convolve1d(DTYPE * const result,
         const DTYPE * const f, const size_t _nx,
         const DTYPE * const g, const size_t _nkx,
-        const bool _nan_interpolate, const bool _padded,
+        const bool _nan_interpolate,
+        const bool _assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -220,7 +230,7 @@ FORCE_INLINE void convolve1d(DTYPE * const result,
     const size_t nkx = _nkx;
     const size_t nkx_minus_1 = nkx - 1;
     const bool nan_interpolate = _nan_interpolate;
-    const bool padded = _padded;
+    const bool assume_results_array_padded_by_half_kernel_size = _assume_results_array_padded_by_half_kernel_size;
 
     // Thread locals
     const size_t wkx = _wkx;
@@ -258,10 +268,10 @@ FORCE_INLINE void convolve1d(DTYPE * const result,
                 top += val * ker;
         }}
 
-        if (padded) { // compile time constant
-          result_index = i_minus_wkx;
+        if (assume_results_array_padded_by_half_kernel_size) { // compile time constant
+            result_index = i;
         } else {
-          result_index = i;
+            result_index = i_minus_wkx;
         }
 
         if (nan_interpolate) // compile time constant
@@ -283,7 +293,8 @@ FORCE_INLINE void convolve1d(DTYPE * const result,
 FORCE_INLINE void convolve2d(DTYPE * const result,
         const DTYPE * const f, const size_t _nx, const size_t _ny,
         const DTYPE * const g, const size_t _nkx, const size_t _nky,
-        const bool _nan_interpolate, const bool _padded,
+        const bool _nan_interpolate,
+        const bool _assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -318,7 +329,7 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
     const size_t nkx = _nkx, nky = _nky;
     const size_t nkx_minus_1 = nkx - 1, nky_minus_1 = nky - 1;
     const bool nan_interpolate = _nan_interpolate;
-    const bool padded = _padded;
+    const bool assume_results_array_padded_by_half_kernel_size = _assume_results_array_padded_by_half_kernel_size;
 
     // Thread locals
     const size_t wkx = _wkx;
@@ -374,10 +385,10 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
                 }}
             }}
 
-            if (padded) { // compile time constant
-              result_index = i_minus_wkx * ny_minus_2wky + j_minus_wky;
+            if (assume_results_array_padded_by_half_kernel_size) { // compile time constant
+                result_index = result_cursor + j;
             } else {
-              result_index = result_cursor + j;
+                result_index = i_minus_wkx * ny_minus_2wky + j_minus_wky;
             }
 
             if (nan_interpolate) // compile time constant
@@ -400,7 +411,8 @@ FORCE_INLINE void convolve2d(DTYPE * const result,
 FORCE_INLINE void convolve3d(DTYPE * const result,
         const DTYPE * const f, const size_t _nx, const size_t _ny, const size_t _nz,
         const DTYPE * const g, const size_t _nkx, const size_t _nky, const size_t _nkz,
-        const bool _nan_interpolate, const bool _padded,
+        const bool _nan_interpolate,
+        const bool _assume_results_array_padded_by_half_kernel_size,
         const unsigned n_threads)
 {
 #ifdef NDEBUG
@@ -437,7 +449,7 @@ FORCE_INLINE void convolve3d(DTYPE * const result,
     const size_t nkx = _nkx, nky = _nky, nkz = _nkz;
     const size_t nkx_minus_1 = nkx - 1, nky_minus_1 = nky - 1, nkz_minus_1 = nkz - 1;
     const bool nan_interpolate = _nan_interpolate;
-    const bool padded = _padded;
+    const bool assume_results_array_padded_by_half_kernel_size = _assume_results_array_padded_by_half_kernel_size;
 
     // Thread locals
     const size_t wkx = _wkx;
@@ -509,10 +521,10 @@ FORCE_INLINE void convolve3d(DTYPE * const result,
                     }}
                 }}
 
-                if (padded) { // compile time constant
-                    result_index = (i_minus_wkx*ny_minus_2wky + j_minus_wky)*nz_minus_2wkz + k_minus_wkz;
-                } else {
+                if (assume_results_array_padded_by_half_kernel_size) { // compile time constant
                     result_index = array_cursor + k;
+                } else {
+                    result_index = (i_minus_wkx*ny_minus_2wky + j_minus_wky)*nz_minus_2wkz + k_minus_wkz;
                 }
 
                 if (nan_interpolate) // compile time constant
