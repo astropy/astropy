01a3dcec64c0f3dfc150511b6a978ae3c95a4b48
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-11-03 21:59:35 +0000

    Fix warnings about docs not being the latest ones (which had an incorrect link to the latest one) to include the correct link to the stable docs. This was done by changing some of the CSS class names (used by the JS code to figure out where to insert the warning) and then inserting the warning manually.

--- docs/expand_archive.py
+++ docs/expand_archive.py
@@ -19,6 +19,17 @@ def setup(app):
 
 META_NOINDEX = '<meta name="robots" content="noindex, nofollow">'
 
+LATEST_WARNING = """
+<div class="admonition warning">
+  <p class="first admonition-title">Note</p>
+  <p class="last">
+    This is an old version of the documentation. See
+    <a href="/en/stable/index.html">http://docs.astropy.org/en/stable</a>
+    for the latest version.
+  </p>
+</div>
+"""
+
 
 def expand_archive(app, exc):
 
@@ -34,7 +45,12 @@ def expand_archive(app, exc):
         tar_file.extractall(app.builder.outdir)
 
     # Go through all html files in the built output and add the meta tag
-    # to prevent search engines from crawling the pages
+    # to prevent search engines from crawling the pages. In addition, we also
+    # rename some of the CSS classes otherwise the RTD javascript inserts
+    # a message about this being an old version of the docs with an incorrect
+    # link. We rename the classes (body -> old_body and document -> old_document)
+    # to prevent this (since the RTD js uses these tags) and then insert the
+    # warning in manually afterwards with the correct link.
 
     logger.info(bold('adding {0} tag to pages...').format(META_NOINDEX))
 
@@ -43,11 +59,46 @@ def expand_archive(app, exc):
         with open(filename, 'r') as f:
             content = f.read()
 
+        # Insert meta noindex tag
+
         if META_NOINDEX not in content:
             if '<head>' in content:
                 content = content.replace('<head>', '<head>{0}'.format(META_NOINDEX))
             else:
                 raise Exception("Could not determine start of <head> section in {0}".format(filename))
+
+        # Rename CSS classes to prevent automated warning
+
+        content = content.replace('class="document"', 'class="old_document"')
+        content = content.replace('class="body"', 'class="old_body"')
+
+        # Insert manual warning about this being an old version of the docs
+
+        pos = content.index('<div class="old_body"')
+        pos = content.index('>', pos)
+
+        content = content[:pos + 1] + LATEST_WARNING + content[pos + 1:]
+
+        # Write file back out
+
+        with open(filename, 'w') as f:
+            f.write(content)
+
+    # Finally we update the actual CSS files to change the body and document
+    # class names.
+
+    logger.info(bold('updating CSS files...'))
+
+    for filename in glob.glob(os.path.join(app.builder.outdir, '**',
+                                           'bootstrap-astropy.css'), recursive=True):
+
+        with open(filename, 'r') as f:
+            content = f.read()
+
+        # Note that the spaces after the strings are required to avoid matching
+        # e.g. div.documentwrapper.
+        content = content.replace('div.body ', 'div.old_body ')
+        content = content.replace('div.document ', 'div.old_document ')
 
         with open(filename, 'w') as f:
             f.write(content)
