62bdd855edc2a628c895bc86200d1683a5057ee9
Simon Conseil <contact@saimon.org>
2018-11-05 14:51:44 +0100

    Merge pull request #7856 from drdavella/fits-append-mode
    Handle valid modes correctly for file objects passed to fits.append

--- astropy/io/fits/tests/test_convenience.py
+++ astropy/io/fits/tests/test_convenience.py
@@ -162,3 +162,10 @@ class TestConvenience(FitsTestCase):
         # test with datafile
         fits.tabledump(temp_filename, datafile=self.temp('test_tb.txt'))
         assert os.path.isfile(self.temp('test_tb.txt'))
+
+    @pytest.mark.parametrize('mode', ['wb', 'wb+', 'ab', 'ab+'])
+    def test_append_filehandle(self, tmpdir, mode):
+
+        append_file = tmpdir.join('append.fits')
+        with append_file.open(mode) as handle:
+            fits.append(filename=handle, data=np.ones((4, 4)))
