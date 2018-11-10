Fix ``Moffat1D`` and ``Moffat2D`` derivatives. [#8094]

astropy/modeling/functional_models.py
@@ -2269,12 +2269,12 @@ class Moffat1D(Fittable1DModel):
     def fit_deriv(x, amplitude, x_0, gamma, alpha):
         """One dimensional Moffat model derivative with respect to parameters"""
 
-        d_A = (1 + (x - x_0) ** 2 / gamma ** 2) ** (-alpha)
-        d_x_0 = (-amplitude * alpha * d_A * (-2 * x + 2 * x_0) /
-                 (gamma ** 2 * d_A ** alpha))
-        d_gamma = (2 * amplitude * alpha * d_A * (x - x_0) ** 2 /
-                   (gamma ** 3 * d_A ** alpha))
-        d_alpha = -amplitude * d_A * np.log(1 + (x - x_0) ** 2 / gamma ** 2)
+        fac = (1 + (x - x_0) ** 2 / gamma ** 2)
+        d_A = fac ** (-alpha)
+        d_x_0 = (2 * amplitude * alpha * (x - x_0) * d_A / (fac * gamma ** 2))
+        d_gamma = (2 * amplitude * alpha * (x - x_0) ** 2 * d_A /
+                   (fac * gamma ** 3))
+        d_alpha = -amplitude * d_A * np.log(fac)
         return [d_A, d_x_0, d_gamma, d_alpha]
 
     @property
@@ -2349,12 +2349,13 @@ class Moffat2D(Fittable2DModel):
 
         rr_gg = ((x - x_0) ** 2 + (y - y_0) ** 2) / gamma ** 2
         d_A = (1 + rr_gg) ** (-alpha)
-        d_x_0 = (-amplitude * alpha * d_A * (-2 * x + 2 * x_0) /
+        d_x_0 = (2 * amplitude * alpha * d_A * (x - x_0) /
                  (gamma ** 2 * (1 + rr_gg)))
-        d_y_0 = (-amplitude * alpha * d_A * (-2 * y + 2 * y_0) /
+        d_y_0 = (2 * amplitude * alpha * d_A * (y - y_0) /
                  (gamma ** 2 * (1 + rr_gg)))
         d_alpha = -amplitude * d_A * np.log(1 + rr_gg)
-        d_gamma = 2 * amplitude * alpha * d_A * (rr_gg / (gamma * (1 + rr_gg)))
+        d_gamma = (2 * amplitude * alpha * d_A * rr_gg /
+                   (gamma ** 3 * (1 + rr_gg)))
         return [d_A, d_x_0, d_y_0, d_gamma, d_alpha]
 
     @property