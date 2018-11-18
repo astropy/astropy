025ce1321f7c2b32b828b1e16c0dff042f17c1ae
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-11-17 22:35:53 -0500

    Initial reductions and sum

--- astropy/utils/masked/tests/test_masked.py
+++ astropy/utils/masked/tests/test_masked.py
@@ -4,7 +4,7 @@ from numpy.testing import assert_array_equal
 
 from .... import units as u
 from ..core import Masked
-from ....tests.helper import assert_quantity_allclose, pytest
+from ....tests.helper import pytest
 
 
 class TestMaskedArrayInitialization:
@@ -24,6 +24,19 @@ class TestMaskedArrayInitialization:
 class TestMaskedQuantityInitialization(TestMaskedArrayInitialization):
     def setup(self):
         self.a = np.array([1., 2.]) * u.m
+
+
+class TestFilled:
+    def setup(self):
+        self.a = np.arange(6.).reshape(2, 3)
+        self.ma = Masked(self.a, mask=np.array([[True, False, False],
+                                                [False, True, False]]))
+
+    @pytest.mark.parametrize('fill_value', (0, 1))
+    def test_filled(self, fill_value):
+        expected = np.where(self.ma.mask, fill_value, self.a)
+        result = self.ma.filled(fill_value)
+        assert_array_equal(expected, result)
 
 
 class TestMaskedArrayUfuncs:
@@ -47,6 +60,25 @@ class TestMaskedArrayUfuncs:
         ma_mb = ufunc(self.ma, self.mb)
         assert_array_equal(ma_mb.data, ufunc(self.a, self.b))
         assert_array_equal(ma_mb.mask, (self.ma.mask | self.mb.mask))
+
+    @pytest.mark.parametrize('ufunc', (np.add, np.subtract))
+    @pytest.mark.parametrize('axis', (0, 1))
+    def test_reduce_filled_zero(self, ufunc, axis):
+        reduction = getattr(ufunc, 'reduce')
+        ma_reduce = reduction(self.ma, axis=axis)
+        expected_data = reduction(self.a * (1 - self.ma.mask),
+                                  axis=axis)
+        expected_mask = np.logical_and.reduce(self.ma.mask, axis=axis)
+        assert isinstance(ma_reduce, type(self.ma))
+        assert_array_equal(ma_reduce.data, expected_data)
+        assert_array_equal(ma_reduce.mask, expected_mask)
+
+    @pytest.mark.parametrize('axis', (0, 1))
+    def test_sum(self, axis):
+        ma_sum = self.ma.sum(axis)
+        masked0 = self.ma.data * (1. - self.ma.mask)
+        assert_array_equal(ma_sum.data, masked0.sum(axis))
+        assert not np.any(ma_sum.mask)
 
 
 class TestMaskedQuantityUfuncs(TestMaskedArrayUfuncs):
