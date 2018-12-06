6078edb772a065a0116873e80c87acfae4f5e5f7
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-12-06 14:05:47 -0700

    Move a large chunk of the setup.py configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@
 
 import sys
 
+from setuptools.config import read_configuration
+conf = read_configuration('setup.cfg')
+
 # This is the same check as astropy/__init__.py but this one has to
 # happen before importing ah_bootstrap
-__minimum_python_version__ = '3.5'
-if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
+minimum_python_version = conf['minimum_python_version']
+if sys.version_info < tuple((int(val) for val in minimum_python_version.split('.'))):
     sys.stderr.write("ERROR: Astropy requires Python {} or later\n".format(
-        __minimum_python_version__))
+        minimum_python_version))
     sys.exit(1)
 
 import os
@@ -25,10 +28,8 @@ from astropy_helpers.version_helpers import generate_version_py
 
 import astropy
 
-NAME = 'astropy'
-
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '3.2.dev'
+VERSION = conf['metadata']['version']
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
@@ -53,34 +54,19 @@ package_info = get_package_info()
 # Add the project-global data
 package_info['package_data'].setdefault('astropy', []).append('data/*')
 
-# Add any necessary entry points
-entry_points = {}
-# Command-line scripts
-entry_points['console_scripts'] = [
-    'fits2bitmap = astropy.visualization.scripts.fits2bitmap:main',
-    'fitscheck = astropy.io.fits.scripts.fitscheck:main',
-    'fitsdiff = astropy.io.fits.scripts.fitsdiff:main',
-    'fitsheader = astropy.io.fits.scripts.fitsheader:main',
-    'fitsinfo = astropy.io.fits.scripts.fitsinfo:main',
-    'samp_hub = astropy.samp.hub_script:hub_script',
-    'showtable = astropy.table.scripts.showtable:main',
-    'volint = astropy.io.votable.volint:main',
-    'wcslint = astropy.wcs.wcslint:main',
-]
-# Register ASDF extensions
-entry_points['asdf_extensions'] = [
-    'astropy = astropy.io.misc.asdf.extension:AstropyExtension',
-    'astropy-asdf = astropy.io.misc.asdf.extension:AstropyAsdfExtension',
-]
-
 min_numpy_version = 'numpy>=' + astropy.__minimum_numpy_version__
 
-setup_requires = [min_numpy_version]
+if is_distutils_display_option():
+    # Avoid installing setup_requires dependencies if the user just
+    # queries for information
+    setup_requires = []
+else:
+    setup_requires = [min_numpy_version]
 
-# Make sure to have the packages needed for building astropy, but do not require them
-# when installing from an sdist as the c files are included there.
-if not os.path.exists(os.path.join(os.path.dirname(__file__), 'PKG-INFO')):
-    setup_requires.extend(['cython>=0.21', 'jinja2>=2.7'])
+    # Make sure we have the packages needed for building astropy, but do not require them
+    # when installing from an sdist as the c files are included there.
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), 'PKG-INFO')):
+        setup_requires.extend(['cython>=0.21', 'jinja2>=2.7'])
 
 install_requires = [min_numpy_version]
 
@@ -89,37 +75,10 @@ install_requires = [min_numpy_version]
 if is_distutils_display_option():
     setup_requires = []
 
-
-setup(name=NAME,
-      version=VERSION,
-      description='Community-developed python astronomy tools',
-      requires=['numpy'],
+setup(version=VERSION,
       setup_requires=setup_requires,
       install_requires=install_requires,
-      provides=[NAME],
-      author='The Astropy Developers',
-      author_email='astropy.team@gmail.com',
-      license='BSD',
-      url='http://astropy.org',
       long_description=astropy.__doc__,
-      keywords=['astronomy', 'astrophysics', 'cosmology', 'space', 'science',
-                'units', 'table', 'wcs', 'samp', 'coordinate', 'fits',
-                'modeling', 'models', 'fitting', 'ascii'],
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: C',
-          'Programming Language :: Cython',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Topic :: Scientific/Engineering :: Astronomy',
-          'Topic :: Scientific/Engineering :: Physics'
-      ],
       cmdclass=cmdclassd,
-      zip_safe=False,
-      entry_points=entry_points,
-      python_requires='>=' + __minimum_python_version__,
-      tests_require=['pytest-astropy'],
       **package_info
 )
