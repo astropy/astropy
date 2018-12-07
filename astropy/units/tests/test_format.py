8f82e014f92522c5d8a4904449e3078767741932
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-12-07 13:31:52 -0700

    add tests for signless powers
    update changelog
    
    added parser directives to fix tests
    
    add change log entry to 2.0.13

--- astropy/units/tests/test_format.py
+++ astropy/units/tests/test_format.py
@@ -434,39 +434,41 @@ def test_vounit_implicit_custom():
     assert x.bases[0]._represents.bases[0].name == 'urlong'
 
 
-def test_fits_scale_factor():
+@pytest.mark.parametrize('scale, number, string',
+                         [('10+2', 100, '10**2'),
+                          ('10(+2)', 100, '10**2'),
+                          ('10**+2', 100, '10**2'),
+                          ('10**(+2)', 100, '10**2'),
+                          ('10^+2', 100, '10**2'),
+                          ('10^(+2)', 100, '10**2'),
+                          ('10**2', 100, '10**2'),
+                          ('10**(2)', 100, '10**2'),
+                          ('10^2', 100, '10**2'),
+                          ('10^(2)', 100, '10**2'),
+                          ('10-20', 10**(-20), '10**-20'),
+                          ('10(-20)', 10**(-20), '10**-20'),
+                          ('10**-20', 10**(-20), '10**-20'),
+                          ('10**(-20)', 10**(-20), '10**-20'),
+                          ('10^-20', 10**(-20), '10**-20'),
+                          ('10^(-20)', 10**(-20), '10**-20'),
+                          ])
+def test_fits_scale_factor(scale, number, string):
+
+    x = u.Unit(scale + ' erg/s/cm**2/Angstrom', format='fits')
+    assert x == number * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
+    assert x.to_string(format='fits') == string + ' Angstrom-1 cm-2 erg s-1'
+
+    x = u.Unit(scale + '*erg/s/cm**2/Angstrom', format='fits')
+    assert x == number * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
+    assert x.to_string(format='fits') == string + ' Angstrom-1 cm-2 erg s-1'
+
+
+def test_fits_scale_factor_errors():
     with pytest.raises(ValueError):
         x = u.Unit('1000 erg/s/cm**2/Angstrom', format='fits')
 
     with pytest.raises(ValueError):
         x = u.Unit('12 erg/s/cm**2/Angstrom', format='fits')
-
-    x = u.Unit('10+2 erg/s/cm**2/Angstrom', format='fits')
-    assert x == 100 * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**2 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10**(-20) erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**-20 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10**-20 erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**-20 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10^(-20) erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**-20 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10^-20 erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**-20 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10-20 erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
-    assert x.to_string(format='fits') == '10**-20 Angstrom-1 cm-2 erg s-1'
-
-    x = u.Unit('10**(-20)*erg/s/cm**2/Angstrom', format='fits')
-    assert x == 10**(-20) * (u.erg / u.s / u.cm ** 2 / u.Angstrom)
 
     x = u.Unit(1.2 * u.erg)
     with pytest.raises(ValueError):
