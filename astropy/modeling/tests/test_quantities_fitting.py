fix issues with fitting compound models with units

astropy/modeling/tests/test_quantities_fitting.py
@@ -3,17 +3,17 @@
 """
 Tests that relate to fitting models with quantity parameters
 """
-
-
 import numpy as np
 import pytest
 
-from astropy.modeling.models import Gaussian1D
+
+from astropy.modeling import models
 from astropy import units as u
 from astropy.units import UnitsError
 from astropy.tests.helper import assert_quantity_allclose
 from astropy.utils import NumpyRNGContext
 from astropy.modeling import fitting
+
 
 try:
     from scipy import optimize
@@ -42,13 +42,19 @@ def _fake_gaussian_data():
     return x, y
 
 
+compound_models_no_units = [models.Linear1D() + models.Gaussian1D() | models.Scale(),
+                            models.Linear1D() + models.Gaussian1D() + models.Gaussian1D(),
+                            models.Linear1D() + models.Gaussian1D() | models.Shift(),
+                           ]
+
+
 @pytest.mark.skipif('not HAS_SCIPY')
 def test_fitting_simple():
 
     x, y = _fake_gaussian_data()
 
     # Fit the data using a Gaussian with units
-    g_init = Gaussian1D()
+    g_init = models.Gaussian1D()
     fit_g = fitting.LevMarLSQFitter()
     g = fit_g(g_init, x, y)
 
@@ -65,7 +71,9 @@ def test_fitting_with_initial_values():
     x, y = _fake_gaussian_data()
 
     # Fit the data using a Gaussian with units
-    g_init = Gaussian1D(amplitude=1. * u.mJy, mean=3 * u.cm, stddev=2 * u.mm)
+    g_init = models.Gaussian1D(amplitude=1. * u.mJy,
+                               mean=3 * u.cm,
+                               stddev=2 * u.mm)
     fit_g = fitting.LevMarLSQFitter()
     g = fit_g(g_init, x, y)
 
@@ -81,7 +89,9 @@ def test_fitting_missing_data_units():
     """
     Raise an error if the model has units but the data doesn't
     """
-    g_init = Gaussian1D(amplitude=1. * u.mJy, mean=3 * u.cm, stddev=2 * u.mm)
+    g_init = models.Gaussian1D(amplitude=1. * u.mJy,
+                               mean=3 * u.cm,
+                               stddev=2 * u.mm)
     fit_g = fitting.LevMarLSQFitter()
 
     with pytest.raises(UnitsError) as exc:
@@ -103,7 +113,7 @@ def test_fitting_missing_model_units():
 
     x, y = _fake_gaussian_data()
 
-    g_init = Gaussian1D(amplitude=1., mean=3, stddev=2)
+    g_init = models.Gaussian1D(amplitude=1., mean=3, stddev=2)
     fit_g = fitting.LevMarLSQFitter()
     g = fit_g(g_init, x, y)
 
@@ -111,7 +121,7 @@ def test_fitting_missing_model_units():
     assert_quantity_allclose(g.mean, 1.3 * u.m, rtol=0.05)
     assert_quantity_allclose(g.stddev, 0.8 * u.m, rtol=0.05)
 
-    g_init = Gaussian1D(amplitude=1., mean=3 * u.m, stddev=2 * u.m)
+    g_init = models.Gaussian1D(amplitude=1., mean=3 * u.m, stddev=2 * u.m)
     fit_g = fitting.LevMarLSQFitter()
     g = fit_g(g_init, x, y)
 
@@ -126,9 +136,54 @@ def test_fitting_incompatible_units():
     Raise an error if the data and model have incompatible units
     """
 
-    g_init = Gaussian1D(amplitude=1. * u.Jy, mean=3 * u.m, stddev=2 * u.cm)
+    g_init = models.Gaussian1D(amplitude=1. * u.Jy,
+                               mean=3 * u.m,
+                               stddev=2 * u.cm)
     fit_g = fitting.LevMarLSQFitter()
 
     with pytest.raises(UnitsError) as exc:
         fit_g(g_init, [1, 2, 3] * u.Hz, [4, 5, 6] * u.Jy)
     assert exc.value.args[0] == ("'Hz' (frequency) and 'm' (length) are not convertible")
+
+
+@pytest.mark.skipif('not HAS_SCIPY')
+@pytest.mark.parametrize('model', compound_models_no_units)
+def test_compound_without_units(model):
+    x = np.linspace(-5, 5, 10) * u.Angstrom
+    with NumpyRNGContext(12345):
+        y = np.random.sample(10)
+
+    fitter = fitting.LevMarLSQFitter()
+
+    res_fit = fitter(model, x, y * u.Hz)
+    assert all([res_fit[i]._has_units for i in range(3)])
+    z = res_fit(x)
+    assert isinstance(z, u.Quantity)
+
+    res_fit = fitter(model, np.arange(10) * u.Unit('Angstrom'), y)
+    assert all([res_fit[i]._has_units for i in range(3)])
+    z = res_fit(x)
+    assert isinstance(z, np.ndarray)
+
+
+@pytest.mark.skipif('not HAS_SCIPY')
+def test_compound_fitting_with_units():
+    x = np.linspace(-5, 5, 15) * u.Angstrom
+    y = np.linspace(-5, 5, 15) * u.Angstrom
+
+    fitter = fitting.LevMarLSQFitter()
+    m = models.Gaussian2D(10*u.Hz,
+                          3*u.Angstrom, 4*u.Angstrom,
+                          1*u.Angstrom, 2*u.Angstrom)
+    p = models.Planar2D(3*u.Hz/u.Angstrom, 4*u.Hz/u.Angstrom, 1*u.Hz)
+    model = m + p
+
+    z = model(x, y)
+    res = fitter(model, x, y, z)
+    assert isinstance(res(x, y), np.ndarray)
+    assert all([res[i]._has_units for i in range(2)])
+
+    model = models.Gaussian2D() + models.Planar2D()
+    res = fitter(model, x, y, z)
+    assert isinstance(res(x, y), np.ndarray)
+    assert all([res[i]._has_units for i in range(2)])