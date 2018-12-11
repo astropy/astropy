0e1493d67f90858b9cdd992fcfaed61e9f0c7bbf
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2018-12-11 14:59:00 -0500

    Fix bug with raising to a negative power.
    This is a regression introduced by gh-7649 and reported in gh-8260.
    Before this fix:
    ```
    v2 = 1*u.m**2/u.s**2
    (v2 ** (-1/2)).to(u.s/u.m)
    ```
    leads to a unit conversion error because the bases are in the
    wrong order.

--- astropy/units/tests/test_units.py
+++ astropy/units/tests/test_units.py
@@ -807,3 +807,15 @@ def test_unit_summary_prefixes():
             assert prefixes == 'No'
         elif unit.name == 'vox':
             assert prefixes == 'Yes'
+
+
+def test_raise_to_negative_power():
+    """Test that order of bases is changed when raising to negative power.
+
+    Regression test for https://github.com/astropy/astropy/issues/8260
+    """
+    m2s2 = u.m ** 2 / u.s **2
+    spm = m2s2 ** (-1 / 2)
+    assert spm.bases == [u.s, u.m]
+    assert spm.powers == [1, -1]
+    assert spm == u.s / u.m
