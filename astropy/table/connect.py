3d6752c25a12fe983cc7ed1ee0105903ddc44a8c
Tom Aldcroft <taldcroft@gmail.com>
2018-12-15 10:01:16 -0500

    Add table connect.py module [skip ci]

---
+++ astropy/table/connect.py
@@ -0,0 +1,106 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+from astropy.io import registry
+
+from .info import serialize_method_as
+
+
+class TableRead(registry.UnifiedReadWrite):
+    """
+    Read and parse a data table and return as a Table.
+
+    This function provides the Table interface to the astropy unified I/O
+    layer.  This allows easily reading a file in many supported data formats
+    using syntax such as::
+
+      >>> from astropy.table import Table
+      >>> dat = Table.read('table.dat', format='ascii')
+      >>> events = Table.read('events.fits', format='fits')
+
+    See also: http://docs.astropy.org/en/stable/io/unified.html
+
+    Parameters
+    ----------
+    format : str
+        File format specifier.
+    *args : tuple, optional
+        Positional arguments passed through to data reader. If supplied the
+        first argument is the input filename.
+    **kwargs : dict, optional
+        Keyword arguments passed through to data reader.
+
+    Returns
+    -------
+    out : `Table`
+        Table corresponding to file contents
+
+    Notes
+    -----
+    """
+    def __init__(self, cls):
+        super().__init__(cls, 'read')
+
+    def __call__(self, *args, **kwargs):
+        cls = self._cls
+        out = registry.read(cls, *args, **kwargs)
+
+        # For some readers (e.g., ascii.ecsv), the returned `out` class is not
+        # guaranteed to be the same as the desired output `cls`.  If so,
+        # try coercing to desired class without copying (io.registry.read
+        # would normally do a copy).  The normal case here is swapping
+        # Table <=> QTable.
+        if cls is not out.__class__:
+            try:
+                out = cls(out, copy=False)
+            except Exception:
+                raise TypeError('could not convert reader output to {0} '
+                                'class.'.format(cls.__name__))
+        return out
+
+
+class TableWrite(registry.UnifiedReadWrite):
+    """
+    Write this Table object out in the specified format.
+
+    This function provides the Table interface to the astropy unified I/O
+    layer.  This allows easily writing a file in many supported data formats
+    using syntax such as::
+
+      >>> from astropy.table import Table
+      >>> dat = Table([[1, 2], [3, 4]], names=('a', 'b'))
+      >>> dat.write('table.dat', format='ascii')
+
+    See also: http://docs.astropy.org/en/stable/io/unified.html
+
+    Parameters
+    ----------
+    format : str
+        File format specifier.
+    serialize_method : str, dict, optional
+        Serialization method specifier for columns.
+    *args : tuple, optional
+        Positional arguments passed through to data writer. If supplied the
+        first argument is the output filename.
+    **kwargs : dict, optional
+        Keyword arguments passed through to data writer.
+
+    Notes
+    -----
+    """
+    def __init__(self, cls):
+        super().__init__(cls, 'write')
+
+    def __call__(self, *args, **kwargs):
+        serialize_method = kwargs.pop('serialize_method', None)
+        with serialize_method_as(self, serialize_method):
+            registry.write(self, *args, **kwargs)
+
+
+class TableReadMethod:
+    def __get__(self, instance, owner_cls):
+        return TableRead(owner_cls)
+
+
+class TableWriteMethod:
+    def __get__(self, instance, owner_cls):
+        return TableWrite(owner_cls)
