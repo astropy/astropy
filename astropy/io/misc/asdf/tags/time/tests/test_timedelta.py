abceb223b98abad64a594862fb974d82edb73b83
Daniel D'Avella <ddavella@stsci.edu>
2019-01-02 13:27:14 -0500

    Add ASDF test for TimeDelta scales and fix schema

--- astropy/io/misc/asdf/tags/time/tests/test_timedelta.py
+++ astropy/io/misc/asdf/tags/time/tests/test_timedelta.py
@@ -6,7 +6,7 @@ import pytest
 asdf = pytest.importorskip('asdf')
 from asdf.tests.helpers import assert_roundtrip_tree
 
-from astropy.time import Time
+from astropy.time import Time, TimeDelta
 
 
 @pytest.mark.parametrize('fmt', Time.FORMATS.keys())
@@ -17,4 +17,11 @@ def test_timedelta(fmt, tmpdir):
 
     td = t2 - t1
     tree = dict(timedelta=td)
+    assert_roundtrip_tree(tree, tmpdir)
+
+
+@pytest.mark.parametrize('scale', list(TimeDelta.SCALES) + [None])
+def test_timedetal_scales(scale, tmpdir):
+
+    tree = dict(timedelta=TimeDelta(0.125, scale=scale))
     assert_roundtrip_tree(tree, tmpdir)
