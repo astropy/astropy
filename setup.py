ea22f7ff7ac3a9cfc4e17632dacd11ec2f9b83ab
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-02-07 10:47:37 +0000

    Define default setup_requires and install_requires in setup.cfg

--- setup.py
+++ setup.py
@@ -13,22 +13,17 @@ import ah_bootstrap  # noqa
 from astropy_helpers.distutils_helpers import is_distutils_display_option
 from astropy_helpers.setup_helpers import setup
 
-min_numpy_version = 'numpy>=1.13'
+from setuptools.config import read_configuration
 
 if is_distutils_display_option():
     # Avoid installing setup_requires dependencies if the user just
     # queries for information
     setup_requires = []
 else:
-    setup_requires = [min_numpy_version]
-
+    setup_requires = read_configuration('setup.cfg')['options']['setup_requires']
     # Make sure we have the packages needed for building astropy, but do not
-    # require them when installing from an sdist as the c files are included
-    # there.
+    # require them when installing from an sdist as the c files are included.
     if not os.path.exists(os.path.join(os.path.dirname(__file__), 'PKG-INFO')):
         setup_requires.extend(['cython>=0.21', 'jinja2>=2.7'])
 
-install_requires = [min_numpy_version]
-
-setup(setup_requires=setup_requires,
-      install_requires=install_requires)
+setup(setup_requires=setup_requires)
