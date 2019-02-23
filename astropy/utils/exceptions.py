87523dcc9ca23dad036ca0444c27db19f8af42bc
Erik Tollerud <erik.tollerud@gmail.com>
2019-02-22 23:27:57 -0500

    Merge pull request #8441 from bsipocz/utils_add_erfa_exceptions
    Expose ERFA exceptions through utils.exceptions

--- astropy/utils/exceptions.py
+++ astropy/utils/exceptions.py
@@ -1,8 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 This module contains errors/exceptions and warnings of general use for
-astropy. Exceptions that are specific to a given subpackage should *not*
-be here, but rather in the particular subpackage.
+astropy. Exceptions that are specific to a given subpackage should *not* be
+here, but rather in the particular subpackage. Exception is the _erfa module
+as we rather have the users import those exceptions from here.
 """
 
 
@@ -42,6 +43,29 @@ class AstropyBackwardsIncompatibleChangeWarning(AstropyWarning):
     The suggested procedure is to issue this warning for the version in
     which the change occurs, and remove it for all following versions.
     """
+
+
+class ErfaError(ValueError):
+    """
+    A class for errors triggered by ERFA functions (status codes < 0)
+
+    Note: this class should *not* be referenced by fully-qualified name, because
+    it may move to ERFA in a future version.  In a future such move it will
+    still be imported here as an alias, but the true namespace of the class may
+    change.
+    """
+
+
+class ErfaWarning(AstropyUserWarning):
+    """
+    A class for warnings triggered by ERFA functions (status codes > 0)
+
+    Note: this class should *not* be referenced by fully-qualified name, because
+    it may move to ERFA in a future version.  In a future such move it will
+    still be imported here as an alias, but the true namespace of the class may
+    change.
+    """
+
 
 class _NoValue:
     """Special keyword value.
