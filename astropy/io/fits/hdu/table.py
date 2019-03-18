ce36563adc7fd23f648f3d6f830c64d9c1f0baa9
Simon Conseil <contact@saimon.org>
2019-03-18 23:43:35 +0100

    Revert cosmetic changes

--- astropy/io/fits/hdu/table.py
+++ astropy/io/fits/hdu/table.py
@@ -18,11 +18,11 @@ from .base import DELAYED, _ValidHDU, ExtensionHDU
 # This module may have many dependencies on astropy.io.fits.column, but
 # astropy.io.fits.column has fewer dependencies overall, so it's easier to
 # keep table/column-related utilities in astropy.io.fits.column
-from astropy.io.fits.column import (
-    FITS2NUMPY, KEYWORD_NAMES, KEYWORD_TO_ATTRIBUTE, ATTRIBUTE_TO_KEYWORD,
-    TDEF_RE, Column, ColDefs, _AsciiColDefs, _FormatP, _FormatQ, _makep,
-    _parse_tformat, _scalar_to_format, _convert_format, _cmp_recformats
-)
+from astropy.io.fits.column import (FITS2NUMPY, KEYWORD_NAMES, KEYWORD_TO_ATTRIBUTE,
+                      ATTRIBUTE_TO_KEYWORD, TDEF_RE, Column, ColDefs,
+                      _AsciiColDefs, _FormatP, _FormatQ, _makep,
+                      _parse_tformat, _scalar_to_format, _convert_format,
+                      _cmp_recformats)
 from astropy.io.fits.fitsrec import FITS_rec, _get_recarray_field, _has_unicode_fields
 from astropy.io.fits.header import Header, _pad_length
 from astropy.io.fits.util import _is_int, _str_to_num
