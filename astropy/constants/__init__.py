c87be3f1c741b189a8bbb69b78007ec0e4879e02
David Shupe <shupe@ipac.caltech.edu>
2019-03-21 09:44:28 -0700

    add configuration for constants versions

--- astropy/constants/__init__.py
+++ astropy/constants/__init__.py
@@ -13,6 +13,7 @@ A typical use case might be::
     <Quantity 0.510998927603161 MeV>
 
 """
+import warnings
 from contextlib import contextmanager
 
 from astropy.utils import find_current_module
@@ -24,11 +25,46 @@ try:
 except ImportError:
     pass
 
-from .constant import Constant, EMConstant  # noqa
-from . import si  # noqa
-from . import cgs  # noqa
-from . import codata2014, iau2015  # noqa
+import astropy.config as _config
+
 from . import utils as _utils
+
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `astropy.constants`.
+    """
+    # NOTE: Update this when default changes.
+    physical_constants = _config.ConfigItem(
+        'codata2014',
+        'Version of physical constants to use.')
+    astronomical_constants = _config.ConfigItem(
+        'iau2015',
+        'Version of astronomical constants to use.')
+
+
+conf = Conf()
+
+if ((conf.physical_constants == 'codata2014') or
+        (conf.physical_constants == 'astropyconst20')):
+    from . import codata2014 as codata
+elif ((conf.physical_constants == 'codata2010') or
+        (conf.physical_constants == 'astropyconst13')):
+    from .astropyconst13 import codata2010 as codata
+else:
+    raise ValueError('Invalid physical constants version: {}'
+                     .format(conf.astronomical_constants))
+
+if ((conf.astronomical_constants == 'iau2015') or
+        (conf.astronomical_constants == 'astropyconst20')):
+    from . import iau2015 as iaudata
+elif ((conf.astronomical_constants == 'iau2012') or
+        (conf.astronomical_constants == 'astropyconst13')):
+    from .astropyconst13 import iau2012 as iaudata
+else:
+    raise ValueError('Invalid astronomical constants version: {}'
+                     .format(conf.astronomical_constants))
+
 
 # for updating the constants module docstring
 _lines = [
@@ -38,9 +74,11 @@ _lines = [
     '========== ============== ================ =========================',
 ]
 
-# NOTE: Update this when default changes.
-_utils._set_c(codata2014, iau2015, find_current_module(),
-              not_in_module_only=True, doclines=_lines, set_class=True)
+# Catch warnings about "already has a definition in the None system"
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    _utils._set_c(codata, iaudata, inspect.getmodule(inspect.currentframe()),
+                  not_in_module_only=False, doclines=_lines, set_class=True)
 
 _lines.append(_lines[1])
 
@@ -67,12 +105,13 @@ def set_enabled_constants(modname):
     # Re-import here because these were deleted from namespace on init.
     import warnings
     from astropy.utils import find_current_module
+
     from . import utils as _utils
 
-    # NOTE: Update this when default changes.
+    # NOTE: Update this whenever versions are added.
     if modname == 'astropyconst13':
-        from .astropyconst13 import codata2010 as codata
-        from .astropyconst13 import iau2012 as iaudata
+        from .astropyconst13 import codata2010 as codata_context
+        from .astropyconst13 import iau2012 as iaudata_context
     else:
         raise ValueError(
             'Context manager does not currently handle {}'.format(modname))
@@ -82,7 +121,7 @@ def set_enabled_constants(modname):
     # Ignore warnings about "Constant xxx already has a definition..."
     with warnings.catch_warnings():
         warnings.simplefilter('ignore')
-        _utils._set_c(codata, iaudata, module,
+        _utils._set_c(codata_context, iaudata_context, module,
                       not_in_module_only=False, set_class=True)
 
     try:
@@ -90,9 +129,15 @@ def set_enabled_constants(modname):
     finally:
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            # NOTE: Update this when default changes.
-            _utils._set_c(codata2014, iau2015, module,
+            _utils._set_c(codata, iaudata, module,
                           not_in_module_only=False, set_class=True)
+            del codata_context, iaudata_context
+
+
+# These imports are used by other astropy modules
+from .constant import Constant, EMConstant  # noqa
+from . import si  # noqa
+from . import cgs  # noqa
 
 
 # Clean up namespace
