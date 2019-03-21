1ed5392c937e11b003634c5c2b052d0d2926b6d6
David Shupe <shupe@ipac.caltech.edu>
2019-03-21 16:44:27 -0700

    use constants specified in config in si

--- astropy/constants/si.py
+++ astropy/constants/si.py
@@ -9,10 +9,10 @@ for a complete listing of constants defined in Astropy.
 import itertools
 
 from .constant import Constant
-from . import codata2014, iau2015
+from . import codata, iaudata
 
-for _nm, _c in itertools.chain(sorted(vars(codata2014).items()),
-                               sorted(vars(iau2015).items())):
+for _nm, _c in itertools.chain(sorted(vars(codata).items()),
+                               sorted(vars(iaudata).items())):
     if (isinstance(_c, Constant) and _c.abbrev not in locals()
          and _c.system == 'si'):
         locals()[_c.abbrev] = _c
