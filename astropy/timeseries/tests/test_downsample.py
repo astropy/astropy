5e99278a03c8df5bd7024db2813d659be55790db
Nabil Freij <nabil.freij@gmail.com>
2019-03-26 21:39:09 +0000

    update reduceat

--- astropy/timeseries/tests/test_downsample.py
+++ astropy/timeseries/tests/test_downsample.py
@@ -15,17 +15,17 @@ ts_units = TimeSeries(time=INPUT_TIME, data=[[1, 2, 3, 4] * u.count], names=['a'
 
 
 def test_reduceat():
+    add_output = np.add.reduceat(np.arange(8),[0, 4, 1, 5, 2, 6, 3, 7])
     # Similar to np.add for an array input.
-    output = reduceat(np.arange(8), [0, 4, 1, 5, 2, 6, 3, 7], np.sum)
-    # The result from np.add.reduceat is array([ 6,  4, 10,  5, 14,  6, 18,  7])
-    # A big difference.
-    assert_equal(output, np.array([6, 0, 10, 0, 14, 0, 18, 7]))
+    sum_output = reduceat(np.arange(8), [0, 4, 1, 5, 2, 6, 3, 7], np.sum)
+    assert_equal(sum_output, add_output)
 
-    output = reduceat(np.arange(8), np.arange(8)[::2], np.mean)
-    assert_equal(output, np.array([0.5, 2.5, 4.5, 6.5]))
-    output = reduceat(np.arange(8), [0, 4, 1, 5, 2, 6, 3, 7], np.nanmean)
-    # TODO: Nan outputs are normal?
-    assert_equal(output, np.array([1.5, np.nan, 2.5, np.nan, 3.5, np.nan, 4.5, 7. ]))
+    mean_output = reduceat(np.arange(8), np.arange(8)[::2], np.mean)
+    assert_equal(mean_output, np.array([0.5, 2.5, 4.5, 6.5]))
+    nanmean_output = reduceat(np.arange(8), [0, 4, 1, 5, 2, 6, 3, 7], np.mean)
+    assert_equal(nanmean_output, np.array([1.5, 4, 2.5, 5, 3.5, 6, 4.5, 7.]))
+    assert_equal(reduceat(np.arange(8), np.arange(8)[::2], np.mean),
+                 reduceat(np.arange(8), np.arange(8)[::2], np.nanmean))
 
 
 def test_timeseries_invalid():
