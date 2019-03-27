dcb69b9ef0f91f4617870594af3a6c8976812bc7
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-03-27 14:20:20 -0400

    Add tests for np.prod and Quantity filling.
    Also refactor tests a little.

--- astropy/utils/masked/tests/test_masked.py
+++ astropy/utils/masked/tests/test_masked.py
@@ -2,78 +2,128 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from .... import units as u
+from astropy import units as u
+from astropy.units import Quantity
+from astropy.coordinates import Longitude
 from ..core import Masked
 from ....tests.helper import pytest
 
 
-class TestMaskedArrayInitialization:
+class ArraySetup:
+    def setup_arrays(self):
+        self.a = np.arange(6.).reshape(2, 3)
+        self.mask_a = np.array([[True, False, False],
+                                [False, True, False]])
+        self.b = np.array([-3., -2., -1.])
+        self.mask_b = np.array([False, True, False])
+        self.c = np.array([[0.25], [0.5]])
+        self.mask_c = np.array([[False], [True]])
+
     def setup(self):
-        self.a = np.array([1., 2.])
+        self.setup_arrays()
+
+
+class QuantitySetup(ArraySetup):
+    def setup_arrays(self):
+        super().setup_arrays()
+        self.a = Quantity(self.a, u.m)
+        self.b = Quantity(self.b, u.cm)
+        self.c = Quantity(self.c, u.s)
+
+
+class LongitudeSetup(ArraySetup):
+    def setup_arrays(self):
+        super().setup_arrays()
+        self.a = Longitude(self.a, u.deg)
+        self.b = Longitude(self.b, u.deg)
+        self.c = Longitude(self.c, u.deg)
+
+
+class TestMaskedArrayInitialization(ArraySetup):
+    def setup_arrays(self):
+        self.a = np.arange(6.).reshape(2, 3)
+        self.mask_a = np.array([[True, False, False],
+                                [False, True, False]])
+
+    def setup(self):
+        self.setup_arrays()
 
     def test_simple(self):
-        m = np.array([True, False])
-        ma = Masked(self.a, mask=m)
+        ma = Masked(self.a, mask=self.mask_a)
         assert isinstance(ma, np.ndarray)
         assert isinstance(ma, type(self.a))
         assert isinstance(ma, Masked)
         assert_array_equal(ma.data, self.a)
-        assert_array_equal(ma.mask, m)
+        assert_array_equal(ma.mask, self.mask_a)
 
 
 class TestMaskedQuantityInitialization(TestMaskedArrayInitialization):
+    def setup_arrays(self):
+        super().setup_arrays()
+        self.a = Quantity(self.a, u.m)
+
+
+# Following are tests where we trust the initializer works.
+
+
+class MaskedArraySetup(ArraySetup):
     def setup(self):
-        self.a = np.array([1., 2.]) * u.m
+        self.setup_arrays()
+        self.ma = Masked(self.a, mask=self.mask_a)
+        self.mb = Masked(self.b, mask=self.mask_b)
+        self.mc = Masked(self.c, mask=self.mask_c)
 
 
-class TestFilled:
-    def setup(self):
-        self.a = np.arange(6.).reshape(2, 3)
-        self.ma = Masked(self.a, mask=np.array([[True, False, False],
-                                                [False, True, False]]))
-
+class TestMaskedArrayFilled(MaskedArraySetup):
     @pytest.mark.parametrize('fill_value', (0, 1))
     def test_filled(self, fill_value):
-        expected = np.where(self.ma.mask, fill_value, self.a)
+        fill_value = fill_value * getattr(self.a, 'unit', 1)
+        expected = self.a.copy()
+        expected[self.ma.mask] = fill_value
         result = self.ma.filled(fill_value)
         assert_array_equal(expected, result)
 
 
-class TestMaskedArrayUfuncs:
-    def setup(self):
-        self.a = np.arange(6.).reshape(2, 3)
-        self.b = np.array([-3., -2., -1.])
-        self.c = np.array([[0.25], [0.5]])
-        self.ma = Masked(self.a, mask=np.array([[True, False, False],
-                                                [False, True, False]]))
-        self.mb = Masked(self.b, mask=np.array([False, True, False]))
-        self.mc = Masked(self.c, mask=np.array([[False], [True]]))
+class TestMaskedQuantityFilled(TestMaskedArrayFilled, QuantitySetup):
+    pass
 
+
+class TestMaskedLongitudeFilled(TestMaskedArrayFilled, LongitudeSetup):
+    pass
+
+
+class MaskedUfuncTests(MaskedArraySetup):
     def test_add(self):
         mapmb = self.ma + self.mb
-        assert_array_equal(mapmb.data, self.a + self.b)
-        assert_array_equal(mapmb.mask, (self.ma.mask | self.mb.mask))
+        expected_data = self.a + self.b
+        expected_mask = (self.ma.mask | self.mb.mask)
+        assert_array_equal(mapmb.data, expected_data)
+        assert_array_equal(mapmb.mask, expected_mask)
 
     @pytest.mark.parametrize('ufunc', (np.add, np.subtract, np.divide,
                                        np.arctan2))
     def test_2op_ufunc(self, ufunc):
         ma_mb = ufunc(self.ma, self.mb)
-        assert_array_equal(ma_mb.data, ufunc(self.a, self.b))
-        assert_array_equal(ma_mb.mask, (self.ma.mask | self.mb.mask))
+        expected_data = ufunc(self.a, self.b)
+        expected_mask = (self.ma.mask | self.mb.mask)
+        # Note: assert_array_equal also checks type, i.e., that, e.g.,
+        # Longitude decays into an Angle.
+        assert_array_equal(ma_mb.data, expected_data)
+        assert_array_equal(ma_mb.mask, expected_mask)
 
     @pytest.mark.parametrize('ufunc', (np.add, np.subtract))
     @pytest.mark.parametrize('axis', (0, 1))
     def test_reduce_filled_zero(self, ufunc, axis):
+        # axis=None for np.add tested indirectly by test_sum below.
         reduction = getattr(ufunc, 'reduce')
         ma_reduce = reduction(self.ma, axis=axis)
         expected_data = reduction(self.a * (1 - self.ma.mask),
                                   axis=axis)
         expected_mask = np.logical_and.reduce(self.ma.mask, axis=axis)
-        assert isinstance(ma_reduce, type(self.ma))
         assert_array_equal(ma_reduce.data, expected_data)
         assert_array_equal(ma_reduce.mask, expected_mask)
 
-    @pytest.mark.parametrize('axis', (0, 1))
+    @pytest.mark.parametrize('axis', (0, 1, None))
     def test_sum(self, axis):
         ma_sum = self.ma.sum(axis)
         masked0 = self.ma.data * (1. - self.ma.mask)
@@ -81,12 +131,19 @@ class TestMaskedArrayUfuncs:
         assert not np.any(ma_sum.mask)
 
 
-class TestMaskedQuantityUfuncs(TestMaskedArrayUfuncs):
-    def setup(self):
-        self.a = np.arange(6.).reshape(2, 3) * u.m
-        self.b = np.array([-3., -2., -1.]) * u.m
-        self.c = np.array([[0.25], [0.5]]) * u.s
-        self.ma = Masked(self.a, mask=np.array([[True, False, False],
-                                                [False, True, False]]))
-        self.mb = Masked(self.b, mask=np.array([False, True, False]))
-        self.mc = Masked(self.c, mask=np.array([[False], [True]]))
+class TestMaskedArrayUfuncs(MaskedUfuncTests, ArraySetup):
+    @pytest.mark.parametrize('axis', (0, 1, None))
+    def test_reduce_filled_one(self, axis):
+        ma_reduce = np.prod(self.ma, axis=axis)
+        expected_data = np.prod(self.ma.filled(1), axis=axis)
+        expected_mask = np.logical_and.reduce(self.ma.mask, axis=axis)
+        assert_array_equal(ma_reduce.data, expected_data)
+        assert_array_equal(ma_reduce.mask, expected_mask)
+
+
+class TestMaskedQuantityUfuncs(MaskedUfuncTests, QuantitySetup):
+    pass
+
+
+class TestMaskedLongitude(MaskedUfuncTests, LongitudeSetup):
+    pass
