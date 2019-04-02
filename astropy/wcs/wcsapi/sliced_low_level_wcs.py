6d2121a13c61fb5ce7cf31b32677e87f1e366271
Stuart Mumford <stuart@cadair.com>
2019-04-02 14:33:38 +0100

    first pass at a low level wcs slicer
    Co-authored-by: Thomas Robitaille <thomas.robitaille@gmail.com>

---
+++ astropy/wcs/wcsapi/sliced_low_level_wcs.py
@@ -0,0 +1,148 @@
+import numbers
+import numpy as np
+from astropy.wcs.wcsapi import BaseLowLevelWCS
+
+
+def sanitize_slices(slices, ndim):
+    """
+    Given a set of input 
+    """
+
+    if not isinstance(slices, (tuple, list)):  # We just have a single int
+        slices = (slices,)
+
+    slices = list(slices)
+
+    if Ellipsis in slices:
+        if slices.count(Ellipsis) > 1:
+            raise IndexError("an index can only have a single ellipsis ('...')")
+
+        # Replace the Ellipsis with the correct number of slice(None)s
+        e_ind = slices.index(Ellipsis)
+        slices.remove(Ellipsis)
+        n_e = ndim - len(slices)
+        for i in range(n_e):
+            ind = e_ind + i
+            slices.insert(ind, slice(None))
+
+    for i in range(ndim):
+        if i < len(slices):
+            slc = slices[i]
+            if isinstance(slc, slice):
+                if slc.step and slc.step != 1:
+                    raise ValueError("Slicing WCS with a step is not supported.")
+            elif not isinstance(slc, numbers.Integral):
+                raise ValueError("Only integer or range slices are accepted.")
+        else:
+            slices.append(slice(None)) 
+
+    return slices
+
+
+class SlicedLowLevelWCS(BaseLowLevelWCS):
+
+    def __init__(self, wcs, slices):
+ 
+        self._wcs = wcs
+        self._slices = sanitize_slices(slices, self._wcs.pixel_n_dim)
+
+        # figure out which pixel dimensions have been kept, then use axis correlation
+        # matrix to figure out which world dims are kept
+        self._pixel_keep = np.nonzero([not isinstance(self._slices[ip], numbers.Integral)
+                                       for ip in range(self._wcs.pixel_n_dim)])[0]
+
+        # axis_correlation_matrix[world, pixel]
+        self._world_keep = np.nonzero(
+            self._wcs.axis_correlation_matrix[:, self._pixel_keep].any(axis=1))[0]
+
+    @property
+    def pixel_n_dim(self):
+        return len(self._pixel_keep)
+
+    @property
+    def world_n_dim(self):
+        return len(self._world_keep)
+
+    @property
+    def world_axis_physical_types(self):
+        return [self._wcs.world_axis_physical_types[i] for i in self._world_keep]
+      
+    @property
+    def world_axis_units(self):
+        return [self._wcs.world_axis_units[i] for i in self._world_keep]
+
+    def pixel_to_world_values(self, *pixel_arrays):
+        pixel_arrays_new = []
+        ipix_curr = -1
+        for ipix in range(self._wcs.pixel_n_dim):
+            if self._slices[ipix] is slice(None):
+                ipix_curr += 1
+                pixel_arrays_new.append(pixel_arrays[ipix_curr])
+            elif isinstance(self._slices[ipix], int):
+                pixel_arrays_new.append(self._slices[ipix])
+            else:
+                ipix_curr += 1
+                if self._slices[ipix].start is not None:
+                    pixel_arrays_new.append(pixel_arrays[ipix_curr] + self._slices[ipix].start)
+                else:
+                    pixel_arrays_new.append(pixel_arrays[ipix_curr])
+
+        world_arrays = self._wcs.pixel_to_world_values(*pixel_arrays_new)
+        return [world_arrays[iw] for iw in self._world_keep]
+
+    def array_index_to_world_values(self, *index_arrays):
+        return self.pixel_to_world_values(*index_arrays[::-1])
+ 
+    def world_to_pixel_values(self, *world_arrays):
+        world_arrays_new = []
+        iworld_curr = -1
+        for iworld in range(self._wcs.world_n_dim):
+            if iworld in self._world_keep:
+                iworld_curr += 1
+                world_arrays_new.append(world_arrays[iworld_curr])
+            else:
+                world_arrays_new.append(1.)
+
+        pixel_arrays = self._wcs.world_to_pixel_values(*world_arrays_new)
+
+        for ipixel in range(self._wcs.pixel_n_dim):
+            if isinstance(self._slices[ipixel], slice) and self._slices[ipixel].start is not None:
+                pixel_arrays[ipixel] -= self._slices[ipixel].start
+
+        return [pixel_arrays[ip] for ip in self._pixel_keep]
+
+    def world_to_array_index_values(self, *world_arrays):
+        pixel_arrays = self.world_to_pixel_values(*world_arrays, 0)[::-1]
+        array_indices = tuple(np.asarray(np.floor(pixel + 0.5), dtype=np.int) for pixel in pixel_arrays)
+        return array_indices
+
+    @property
+    def world_axis_object_components(self):
+        return [self._wcs.world_axis_object_components[idx] for idx in self._world_keep]
+
+    @property
+    def world_axis_object_classes(self):
+        keys_keep = [item[0] for item in self.world_axis_object_components]
+        return dict([item for item in self._wcs.world_axis_object_classes.items() if item[0] in keys_keep])
+
+    @property
+    def array_shape(self):
+        return np.broadcast_to(0, self._wcs.array_shape)[self._slices].shape
+        # return tuple([self._wcs.array_shape[idx] for idx in self._pixel_keep])
+
+    @property
+    def pixel_shape(self):
+        return self.array_shape[::-1]
+
+    @property
+    def pixel_bounds(self):
+        # FIXME: incorect when slices are present
+        return [self._wcs.pixel_bounds[::-1][idx] for idx in self._pixel_keep][::-1]
+   
+    @property
+    def axis_correlation_matrix(self):
+        return self._wcs.axis_correlation_matrix[self._world_keep][:,self._pixel_keep]
+
+
+
+
---
+++ astropy/wcs/wcsapi/tests/test_sliced_low_level_wcs.py
@@ -0,0 +1,149 @@
+import pytest
+
+import numpy as np
+
+from numpy.testing import assert_equal, assert_allclose
+from astropy.wcs import WCS
+from astropy.io.fits import Header
+from astropy.coordinates import SkyCoord, Galactic
+from astropy.units import Quantity
+from astropy.wcs.wcsapi.sliced_low_level_wcs import SlicedLowLevelWCS, sanitize_slices
+import astropy.units as u
+
+# To test the slicing we start off from standard FITS WCS
+# objects since those implement the low-level API. We create
+# a WCS for a spectral cube with axes in non-standard order
+# and with correlated celestial axes and an uncorrelated
+# spectral axis.
+
+HEADER_SPECTRAL_CUBE = """
+NAXIS   = 3
+NAXIS1  = 10
+NAXIS2  = 20
+NAXIS3  = 30
+CTYPE1  = GLAT-CAR
+CTYPE2  = FREQ
+CTYPE3  = GLON-CAR
+CRVAL1  = 10
+CRVAL2  = 20
+CRVAL3  = 25
+CRPIX1  = 30
+CRPIX2  = 40
+CRPIX3  = 45
+CDELT1  = -0.1
+CDELT2  =  0.5
+CDELT3  =  0.1
+CUNIT1  = deg
+CUNIT2  = Hz
+CUNIT3  = deg
+"""
+
+WCS_SPECTRAL_CUBE = WCS(Header.fromstring(HEADER_SPECTRAL_CUBE, sep='\n'))
+
+
+@pytest.mark.parametrize("item, ndim, expected", (
+    ([Ellipsis, 10], 4, [slice(None)] * 3 + [10]),
+    ([10, slice(20, 30)], 5, [10, slice(20, 30)] + [slice(None)] * 3),
+    ([10, Ellipsis, 8], 10, [10] + [slice(None)] * 8 + [8])
+))
+def test_sanitize_slice(item, ndim, expected):
+    new_item = sanitize_slices(item, ndim)
+    # FIXME: do we still need the first two since the third assert
+    # should cover it all?
+    assert len(new_item) == ndim
+    assert all(isinstance(i, (slice, int)) for i in new_item)
+    assert new_item == expected
+
+
+def test_ellipsis():
+
+    wcs = SlicedLowLevelWCS(WCS_SPECTRAL_CUBE, Ellipsis)
+
+    assert wcs.pixel_n_dim == 3
+    assert wcs.world_n_dim == 3
+    assert wcs.array_shape == (30, 20, 10)
+    assert wcs.pixel_shape == (10, 20, 30)
+    assert wcs.world_axis_physical_types == ['pos.galactic.lat', 'em.freq', 'pos.galactic.lon']
+    assert wcs.world_axis_units == ['deg', 'Hz', 'deg']
+
+    assert_equal(wcs.axis_correlation_matrix, [[True, False, True], [False, True, False], [True, False, True]])
+
+    assert wcs.world_axis_object_components == [('celestial', 1, 'spherical.lat.degree'),
+                                                  ('freq', 0, 'value'),
+                                                  ('celestial', 0, 'spherical.lon.degree')]
+
+    assert wcs.world_axis_object_classes['celestial'][0] is SkyCoord
+    assert wcs.world_axis_object_classes['celestial'][1] == ()
+    assert isinstance(wcs.world_axis_object_classes['celestial'][2]['frame'], Galactic)
+    assert wcs.world_axis_object_classes['celestial'][2]['unit'] is u.deg
+
+    assert wcs.world_axis_object_classes['freq'][0] is Quantity
+    assert wcs.world_axis_object_classes['freq'][1] == ()
+    assert wcs.world_axis_object_classes['freq'][2] == {'unit': 'Hz'}
+
+    assert_allclose(wcs.pixel_to_world_values(29, 39, 44), (10, 20, 25))
+    assert_allclose(wcs.array_index_to_world_values(44, 39, 29), (10, 20, 25))
+
+    assert_allclose(wcs.world_to_pixel_values(10, 20, 25), (29., 39., 44.))
+    assert_equal(wcs.world_to_array_index_values(10, 20, 25), (44, 39, 29))
+
+
+def test_spectral_slice():
+
+    wcs = SlicedLowLevelWCS(WCS_SPECTRAL_CUBE, [slice(None), 10])
+
+    assert wcs.pixel_n_dim == 2
+    assert wcs.world_n_dim == 2
+    assert wcs.array_shape == (30, 10)
+    assert wcs.pixel_shape == (10, 30)
+    assert wcs.world_axis_physical_types == ['pos.galactic.lat', 'pos.galactic.lon']
+    assert wcs.world_axis_units == ['deg', 'deg']
+
+    assert_equal(wcs.axis_correlation_matrix, [[True, True], [True, True]])
+
+    assert wcs.world_axis_object_components == [('celestial', 1, 'spherical.lat.degree'),
+                                                ('celestial', 0, 'spherical.lon.degree')]
+
+    assert wcs.world_axis_object_classes['celestial'][0] is SkyCoord
+    assert wcs.world_axis_object_classes['celestial'][1] == ()
+    assert isinstance(wcs.world_axis_object_classes['celestial'][2]['frame'], Galactic)
+    assert wcs.world_axis_object_classes['celestial'][2]['unit'] is u.deg
+
+    assert_allclose(wcs.pixel_to_world_values(29, 44), (10, 25))
+    assert_allclose(wcs.array_index_to_world_values(44, 29), (10, 25))
+
+    assert_allclose(wcs.world_to_pixel_values(10, 25), (29., 44.))
+    assert_equal(wcs.world_to_array_index_values(10, 25), (44, 29))
+
+
+def test_spectral_range():
+    
+    wcs = SlicedLowLevelWCS(WCS_SPECTRAL_CUBE, [slice(None), slice(4, 10)])
+
+    assert wcs.pixel_n_dim == 3
+    assert wcs.world_n_dim == 3
+    assert wcs.array_shape == (30, 6, 10)
+    assert wcs.pixel_shape == (10, 6, 30)
+    assert wcs.world_axis_physical_types == ['pos.galactic.lat', 'em.freq', 'pos.galactic.lon']
+    assert wcs.world_axis_units == ['deg', 'Hz', 'deg']
+
+    assert_equal(wcs.axis_correlation_matrix, [[True, False, True], [False, True, False], [True, False, True]])
+
+    assert wcs.world_axis_object_components == [('celestial', 1, 'spherical.lat.degree'),
+                                                  ('freq', 0, 'value'),
+                                                  ('celestial', 0, 'spherical.lon.degree')]
+
+    assert wcs.world_axis_object_classes['celestial'][0] is SkyCoord
+    assert wcs.world_axis_object_classes['celestial'][1] == ()
+    assert isinstance(wcs.world_axis_object_classes['celestial'][2]['frame'], Galactic)
+    assert wcs.world_axis_object_classes['celestial'][2]['unit'] is u.deg
+
+    assert wcs.world_axis_object_classes['freq'][0] is Quantity
+    assert wcs.world_axis_object_classes['freq'][1] == ()
+    assert wcs.world_axis_object_classes['freq'][2] == {'unit': 'Hz'}
+
+    assert_allclose(wcs.pixel_to_world_values(29, 35, 44), (10, 20, 25))
+    assert_allclose(wcs.array_index_to_world_values(44, 35, 29), (10, 20, 25))
+
+    assert_allclose(wcs.world_to_pixel_values(10, 20, 25), (29., 35., 44.))
+    assert_equal(wcs.world_to_array_index_values(10, 20, 25), (44, 35, 29))
