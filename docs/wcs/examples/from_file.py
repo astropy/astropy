a50ed7d0c7227c0fbc1449e411b830b208723eef
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-04-17 15:58:46 -0400

    Apply review comments

--- docs/wcs/examples/from_file.py
+++ docs/wcs/examples/from_file.py
@@ -21,41 +21,36 @@ def load_wcs_from_file(filename):
     w.wcs.print_contents()
 
     # Three pixel coordinates of interest.
-    # Note we've silently assumed a NAXIS=2 image here.
-    # Note also that the pixel coordinates are pairs of [X, Y],
-    # and since WCS built from FITS header automatically has
-    # origin set to 1, [0, 0] is actually not inside the image.
+    # Note we've silently assumed an NAXIS=2 image here.
+    # The pixel coordinates are pairs of [X, Y].
+    # The "origin" argument indicates whether the input coordinates
+    # are 0-based (as in Numpy arrays) or
+    # 1-based (as in the FITS convention, for example coordinates
+    # coming from DS9).
     pixcrd = np.array([[0, 0], [24, 38], [45, 98]], dtype=np.float64)
 
     # Convert pixel coordinates to world coordinates
     # The second argument is "origin" -- in this case we're declaring we
-    # have 1-based (Fortran-like) coordinates.
-    world = w.wcs_pix2world(pixcrd, 1)
+    # have 0-based (Numpy-like) coordinates.
+    world = w.wcs_pix2world(pixcrd, 0)
     print(world)
 
     # Convert the same coordinates back to pixel coordinates.
-    pixcrd2 = w.wcs_world2pix(world, 1)
+    pixcrd2 = w.wcs_world2pix(world, 0)
     print(pixcrd2)
 
     # These should be the same as the original pixel coordinates, modulo
     # some floating-point error.
     assert np.max(np.abs(pixcrd - pixcrd2)) < 1e-6
 
-    # As illustrated below, origin defines whether the origin pixel position
-    # is 0- or 1-indexed.
-    x_origin = 0
-    y_origin = 0
+    # The example below illustrates the use of "origin" to convert between
+    # 0- and 1- based coordinates when executing the forward and backward
+    # WCS transform.
+    x = 0
+    y = 0
     origin = 0
-    assert (w.wcs_pix2world(x_origin, y_origin, origin) ==
-            w.wcs_pix2world(x_origin + 1, y_origin + 1, origin + 1))
-
-    # Following the origin logic from above, X=0 and Y=0 are actually
-    # not within the image when origin=1.
-    # This example passes in X=0 and Y=0 for origin=1, and converts the result
-    # back to pixels for origin=0. The final result of (-1, -1) shows that
-    # pixels are out-of-bounds.
-    print(w.wcs_world2pix(
-        *w.wcs_pix2world(x_origin, y_origin, origin + 1), origin))
+    assert (w.wcs_pix2world(x, y, origin) ==
+            w.wcs_pix2world(x + 1, y + 1, origin + 1))
 
 
 if __name__ == '__main__':
