a7a15ab20f4190316bfdf443335de3d26c49c2a9
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-04-18 10:46:41 +0100

    Added backward-compatibility imports with deprecation warnings

---
+++ astropy/stats/lombscargle/__init__.py
@@ -0,0 +1,31 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# The BoxLeastSquares periodogram functionality has been moved to
+# astropy.timeseries.bls. The purpose of this file is to provide backward-
+# compatibility during a transition phase. We can't emit a deprecation warning
+# simply on import of this module, since the classes are imported into the
+# top-level astropy.stats, so instead we wrap the main class and emit a
+# warning during initialization.
+
+import warnings
+
+from astropy.timeseries.lombscargle import LombScargle as OriginalLombScargle
+from astropy.utils.exceptions import AstropyDeprecationWarning
+
+__all__ = ['LombScargle']
+
+
+class LombScargle(OriginalLombScargle):
+    """
+    Compute the Lomb-Scargle Periodogram.
+
+    This class has been deprecated and will be removed in a future version.
+    Use `~astropy.timeseries.LombScargle` instead.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn('Importing LombScargle from astropy.stats has been '
+                      'deprecated and will no longer be supported in future. '
+                      'Please import this class from the astropy.timeseries '
+                      'module instead', AstropyDeprecationWarning)
+        super().__init__(*args, **kwargs)
