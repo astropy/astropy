1900e54e4d7c013db9d711aaed093d9634a927a4
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-04-20 11:00:17 +0100

    Make sure BasePeriodogram is in API docs

--- astropy/timeseries/periodograms/__init__.py
+++ astropy/timeseries/periodograms/__init__.py
@@ -1,2 +1,3 @@
+from astropy.timeseries.periodograms.base import *  # noqa
 from astropy.timeseries.periodograms.lombscargle import *  # noqa
 from astropy.timeseries.periodograms.bls import *  # noqa
