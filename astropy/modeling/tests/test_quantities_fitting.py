fixed handling of no_unit compound model fitting when provided inputs with units

astropy/modeling/tests/test_quantities_fitting.py
@@ -42,10 +42,14 @@ def _fake_gaussian_data():
     return x, y
 
 
-compound_models_no_units = [models.Linear1D() + models.Gaussian1D() | models.Scale(),
-#                            models.Linear1D() + models.Gaussian1D() + models.Gaussian1D(),
-#                           models.Linear1D() + models.Gaussian1D() | models.Shift(),
-                       ]
+bad_compound_models_no_units = [
+    models.Linear1D() + models.Gaussian1D() | models.Scale(),
+    models.Linear1D() + models.Gaussian1D() | models.Shift()
+]
+
+compound_models_no_units = [
+    models.Linear1D() + models.Gaussian1D() + models.Gaussian1D()
+]
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -161,10 +165,10 @@ def test_compound_without_units(model):
     z = res_fit(x)
     assert isinstance(z, u.Quantity)
 
-    # res_fit = fitter(model, np.arange(10) * u.Unit('Angstrom'), y)
-    # assert all([res_fit[i]._has_units for i in range(3)])
-    # z = res_fit(x)
-    # assert isinstance(z, np.ndarray)
+    res_fit = fitter(model, np.arange(10) * u.Unit('Angstrom'), y)
+    assert all([res_fit[i]._has_units for i in range(3)])
+    z = res_fit(x)
+    assert isinstance(z, np.ndarray)
 
 
 @pytest.mark.skipif('not HAS_SCIPY')
@@ -179,12 +183,23 @@ def test_compound_fitting_with_units():
     p = models.Planar2D(3*u.Hz/u.Angstrom, 4*u.Hz/u.Angstrom, 1*u.Hz)
     model = m + p
 
-    # z = model(x, y)
-    # res = fitter(model, x, y, z)
-    # assert isinstance(res(x, y), np.ndarray)
-    # assert all([res[i]._has_units for i in range(2)])
+    z = model(x, y)
+    res = fitter(model, x, y, z)
+    assert isinstance(res(x, y), np.ndarray)
+    assert all([res[i]._has_units for i in range(2)])
 
-    # model = models.Gaussian2D() + models.Planar2D()
-    # res = fitter(model, x, y, z)
-    # assert isinstance(res(x, y), np.ndarray)
-    # assert all([res[i]._has_units for i in range(2)])
+    model = models.Gaussian2D() + models.Planar2D()
+    res = fitter(model, x, y, z)
+    assert isinstance(res(x, y), np.ndarray)
+    assert all([res[i]._has_units for i in range(2)])
+
+@pytest.mark.skipif('not HAS_SCIPY')
+@pytest.mark.parametrize('model', bad_compound_models_no_units)
+def test_bad_compound_without_units(model):
+    with pytest.raises(ValueError):
+        x = np.linspace(-5, 5, 10) * u.Angstrom
+        with NumpyRNGContext(12345):
+            y = np.random.sample(10)
+
+        fitter = fitting.LevMarLSQFitter()
+        res_fit = fitter(model, x, y * u.Hz)