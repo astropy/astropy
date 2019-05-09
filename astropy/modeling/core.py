d11148c292aa875a84ba252ed0fd165bd656c279
perrygreenfield <perry@stsci.edu>
2019-05-09 14:20:13 -0400

    fixed inverse deletion

--- astropy/modeling/core.py
+++ astropy/modeling/core.py
@@ -2818,6 +2818,12 @@ class CompoundModel(Model):
     def inverse(self, value):
         self._inverse = value
 
+    @inverse.deleter
+    def inverse(self):
+        self._has_inverse = False
+        self._user_inverse = None
+        # del self._inverse
+
     @property
     def fittable(self):
         if self._fittable is None:
