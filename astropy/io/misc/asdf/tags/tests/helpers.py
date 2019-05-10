111d0bd356c18d0c028c73cd8c84c9d3e3ae591c
James Davies <jdavies@stsci.edu>
2019-05-10 14:49:54 -0400

    Fix ASDF tag test helper to load schemas correctly

--- astropy/io/misc/asdf/tags/tests/helpers.py
+++ astropy/io/misc/asdf/tags/tests/helpers.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import urllib.parse
+import urllib.request
 
 import yaml
 
@@ -13,15 +14,15 @@ def run_schema_example_test(organization, standard, name, version, check_func=No
     import asdf
     from asdf.tests import helpers
     from asdf.types import format_tag
-    from asdf.resolver import default_resolver
+    from asdf.resolver import default_tag_to_url_mapping
+    from asdf.schema import load_schema
 
     tag = format_tag(organization, standard, version, name)
-    schema_path = urllib.parse.urlparse(default_resolver(tag)).path
-
-    with open(schema_path, 'rb') as ff:
-        schema = yaml.load(ff)
+    uri = asdf.resolver.default_tag_to_url_mapping(tag)
+    r = asdf.AsdfFile().resolver
 
     examples = []
+    schema = load_schema(uri, resolver=r)
     for node in asdf.treeutil.iter_tree(schema):
         if (isinstance(node, dict) and
             'examples' in node and
@@ -31,7 +32,7 @@ def run_schema_example_test(organization, standard, name, version, check_func=No
 
     for example in examples:
         buff = helpers.yaml_to_asdf('example: ' + example.strip())
-        ff = asdf.AsdfFile(uri=schema_path)
+        ff = asdf.AsdfFile(uri=uri)
         # Add some dummy blocks so that the ndarray examples work
         for i in range(3):
             b = asdf.block.Block(np.zeros((1024*1024*8), dtype=np.uint8))
