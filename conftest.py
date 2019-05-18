324736e020ec1a8c942e9d1da3c75e21018b8658
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-05-18 21:29:15 +0100

    Set temporary directories for config/cache in root-level conftest.py since this is needed when running pytest directly, in particular if running ``pytest docs/`` (which therefore ignores any conftest.py inside astropy/``

--- conftest.py
+++ conftest.py
@@ -1,5 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+# This file is the main file used when running tests with pytest directly,
+# in particular if running e.g. ``pytest docs/``.
+
+import os
+import tempfile
+
 pytest_plugins = [
     'astropy.tests.plugins.display',
 ]
+
+# Make sure we use temporary directories for the config and cache
+# so that the tests are insensitive to local configuration.
+
+os.environ['XDG_CONFIG_HOME'] = tempfile.mkdtemp('astropy_config')
+os.environ['XDG_CACHE_HOME'] = tempfile.mkdtemp('astropy_cache')
+
+os.mkdir(os.path.join(os.environ['XDG_CONFIG_HOME'], 'astropy'))
+os.mkdir(os.path.join(os.environ['XDG_CACHE_HOME'], 'astropy'))
