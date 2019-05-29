5e62fb58e280fdee8df8fab646604e822151c52c
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2019-05-29 16:09:22 -0400

    Fix TestTimeUT1 to use remote_data properly

--- astropy/time/tests/test_ut1.py
+++ astropy/time/tests/test_ut1.py
@@ -5,6 +5,7 @@ import pytest
 import numpy as np
 
 from astropy.time import Time
+from astropy.utils.iers import conf as iers_conf
 from astropy.utils.iers import iers  # used in testing
 
 allclose_jd = functools.partial(np.allclose, rtol=0, atol=1e-9)
@@ -19,10 +20,10 @@ else:
     HAS_IERS_A = True
 
 
-@pytest.mark.remote_data
 class TestTimeUT1:
     """Test Time.ut1 using IERS tables"""
 
+    @pytest.mark.remote_data
     def test_utc_to_ut1(self):
         "Test conversion of UTC to UT1, making sure to include a leap second"""
         t = Time(['2012-06-30 12:00:00', '2012-06-30 23:59:59',
@@ -45,28 +46,30 @@ class TestTimeUT1:
     def test_ut1_to_utc(self):
         """Also test the reverse, around the leap second
         (round-trip test closes #2077)"""
-        t = Time(['2012-06-30 12:00:00', '2012-06-30 23:59:59',
-                  '2012-07-01 00:00:00', '2012-07-01 00:00:01',
-                  '2012-07-01 12:00:00'], scale='ut1')
-        t_utc_jd = t.utc.jd
-        t_comp = np.array([2456109.0000010049,
-                           2456109.4999836441,
-                           2456109.4999952177,
-                           2456109.5000067917,
-                           2456109.9999952167])
-        assert allclose_jd(t_utc_jd, t_comp)
-        t_back = t.utc.ut1
-        assert allclose_jd(t.jd, t_back.jd)
+        with iers_conf.set_temp('auto_download', False):
+            t = Time(['2012-06-30 12:00:00', '2012-06-30 23:59:59',
+                      '2012-07-01 00:00:00', '2012-07-01 00:00:01',
+                      '2012-07-01 12:00:00'], scale='ut1')
+            t_utc_jd = t.utc.jd
+            t_comp = np.array([2456109.0000010049,
+                               2456109.4999836441,
+                               2456109.4999952177,
+                               2456109.5000067917,
+                               2456109.9999952167])
+            assert allclose_jd(t_utc_jd, t_comp)
+            t_back = t.utc.ut1
+            assert allclose_jd(t.jd, t_back.jd)
 
     def test_delta_ut1_utc(self):
         """Accessing delta_ut1_utc should try to get it from IERS
         (closes #1924 partially)"""
-        t = Time('2012-06-30 12:00:00', scale='utc')
-        assert not hasattr(t, '_delta_ut1_utc')
-        # accessing delta_ut1_utc calculates it
-        assert allclose_sec(t.delta_ut1_utc, -0.58682110003124965)
-        # and keeps it around
-        assert allclose_sec(t._delta_ut1_utc, -0.58682110003124965)
+        with iers_conf.set_temp('auto_download', False):
+            t = Time('2012-06-30 12:00:00', scale='utc')
+            assert not hasattr(t, '_delta_ut1_utc')
+            # accessing delta_ut1_utc calculates it
+            assert allclose_sec(t.delta_ut1_utc, -0.58682110003124965)
+            # and keeps it around
+            assert allclose_sec(t._delta_ut1_utc, -0.58682110003124965)
 
 
 @pytest.mark.skipif('not HAS_IERS_A')
