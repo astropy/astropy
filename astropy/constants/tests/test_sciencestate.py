0dec10b915236f870308244bfc698adb5320e1d3
David Shupe <shupe@ipac.caltech.edu>
2019-05-30 15:15:05 -0700

    move imports to top

--- astropy/constants/tests/test_sciencestate.py
+++ astropy/constants/tests/test_sciencestate.py
@@ -1,10 +1,11 @@
 import pytest
 
+from astropy import physical_constants, astronomical_constants
+import astropy.constants as const
+
 
 def test_version_match():
-    from astropy import physical_constants, astronomical_constants
     pversion = physical_constants.get()
-    import astropy.constants as const
     refpversion = const.h.__class__.__name__.lower()
     assert pversion == refpversion
     aversion = astronomical_constants.get()
@@ -13,10 +14,6 @@ def test_version_match():
 
 
 def test_previously_imported():
-    from astropy import physical_constants, astronomical_constants
-    import astropy.units
-    import astropy.constants
-
     with pytest.raises(RuntimeError):
         physical_constants.set('codata2018')
 
