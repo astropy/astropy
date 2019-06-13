a47bd234e802f5b89eb1ed187b91ce36f0cc35e2
P. L. Lim <2090236+pllim@users.noreply.github.com>
2019-06-13 13:27:37 -0400

    Merge pull request #8837 from jdavies-st/conftest-pytest-plugins
    Move asdf schema_tester pytest plugin to top-level conftest.py

--- conftest.py
+++ conftest.py
@@ -3,12 +3,27 @@
 # This file is the main file used when running tests with pytest directly,
 # in particular if running e.g. ``pytest docs/``.
 
+from importlib.util import find_spec
 import os
+import pkg_resources
 import tempfile
+
+from astropy.tests.plugins.display import PYTEST_HEADER_MODULES
+import astropy
 
 pytest_plugins = [
     'astropy.tests.plugins.display',
 ]
+
+if find_spec('asdf') is not None:
+    from asdf import __version__ as asdf_version
+    if asdf_version >= astropy.__minimum_asdf_version__:
+        entry_points = []
+        for entry_point in pkg_resources.iter_entry_points('pytest11'):
+            entry_points.append(entry_point.name)
+        if "asdf_schema_tester" not in entry_points:
+            pytest_plugins += ['asdf.tests.schema_tester']
+        PYTEST_HEADER_MODULES['Asdf'] = 'asdf'
 
 # Make sure we use temporary directories for the config and cache
 # so that the tests are insensitive to local configuration.
