10f11ec13834033b343cf9d6fcbd0cf05f43b6f2
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-06-14 14:47:50 +0100

    Remove unused imports

--- astropy/constants/astropyconst13.py
+++ astropy/constants/astropyconst13.py
@@ -4,7 +4,6 @@ Astronomical and physics constants for Astropy v1.3 and earlier.
 See :mod:`astropy.constants` for a complete listing of constants
 defined in Astropy.
 """
-import inspect
 from astropy.utils import find_current_module
 from . import utils as _utils
 from . import codata2010, iau2012
@@ -12,5 +11,5 @@ from . import codata2010, iau2012
 _utils._set_c(codata2010, iau2012, find_current_module())
 
 # Clean up namespace
-del inspect
+del find_current_module
 del _utils
--- astropy/constants/astropyconst20.py
+++ astropy/constants/astropyconst20.py
@@ -3,7 +3,6 @@
 Astronomical and physics constants for Astropy v2.0.  See :mod:`astropy.constants`
 for a complete listing of constants defined in Astropy.
 """
-import inspect
 from astropy.utils import find_current_module
 from . import utils as _utils
 from . import codata2014, iau2015
@@ -11,5 +10,5 @@ from . import codata2014, iau2015
 _utils._set_c(codata2014, iau2015, find_current_module())
 
 # Clean up namespace
-del inspect
+del find_current_module
 del _utils
