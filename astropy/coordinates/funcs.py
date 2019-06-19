Fix concatenation of representations when units are not the same.

astropy/coordinates/funcs.py
@@ -270,19 +270,15 @@ def _concatenate_components(reps_difs, names):
     """
     values = []
     for name in names:
-        data_vals = []
-        for x in reps_difs:
-            data_val = getattr(x, name)
-            data_vals.append(data_val.reshape(1, ) if x.isscalar else data_val)
-        concat_vals = np.concatenate(data_vals)
-
-        # Hack because np.concatenate doesn't fully work with Quantity
-        if isinstance(concat_vals, u.Quantity):
-            concat_vals._unit = data_val.unit
-
+        unit0 = getattr(reps_difs[0], name).unit
+        # Go via to_value because np.concatenate doesn't work with Quantity
+        data_vals = [getattr(x, name).to_value(unit0) for x in reps_difs]
+        concat_vals = np.concatenate(np.atleast_1d(*data_vals))
+        concat_vals = concat_vals << unit0
         values.append(concat_vals)
 
     return values
+
 
 def concatenate_representations(reps):
     """