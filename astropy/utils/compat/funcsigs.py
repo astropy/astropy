07fc368c68e2d032b04b683090f5210f4af11ff5
Brigitta Sipocz <bsipocz@gmail.com>
2019-06-23 16:19:33 -0700

    Removed deprecated funcsigs and futures

--- astropy/utils/compat/funcsigs.py
+++
@@ -1,9 +0,0 @@
-from inspect import signature, Parameter, Signature, BoundArguments
-
-__all__ = ['BoundArguments', 'Parameter', 'Signature', 'signature']
-
-import warnings
-from astropy.utils.exceptions import AstropyDeprecationWarning
-
-warnings.warn("astropy.utils.compat.funcsigs is now deprecated - "
-              "use inspect instead", AstropyDeprecationWarning)
