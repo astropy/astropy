67f5a45daf82ba2bcb3c150d8ea4bca37f5dbe4a
Perry Greenfield <perry@stsci.edu>
2019-07-10 12:41:39 -0400

    added fixes from #8799, and whitespace cleanup, removed print statement

--- astropy/modeling/tests/test_core.py
+++ astropy/modeling/tests/test_core.py
@@ -8,7 +8,15 @@ from numpy.testing import assert_allclose
 from astropy.modeling.core import Model, custom_model
 from astropy.modeling.parameters import Parameter
 from astropy.modeling import models
+import astropy.units as u
+from astropy.tests.helper import assert_quantity_allclose
 
+try:
+    import scipy  # pylint: disable=W0611
+except ImportError:
+    HAS_SCIPY = False
+else:
+    HAS_SCIPY = True
 
 class NonFittableModel(Model):
     """An example class directly subclassing Model for testing."""
@@ -380,3 +388,15 @@ def test_compound_deepcopy():
     assert id(model[0]) != id(new_model[0])
     assert id(model[1]) != id(new_model[1])
     assert id(model[2]) != id(new_model[2])
+
+
+@pytest.mark.skipif('not HAS_SCIPY')
+def test_units_with_bounding_box():
+    points = np.arange(10, 20)
+    table = np.arange(10) * u.Angstrom
+    t = models.Tabular1D(points, lookup_table=table)
+
+    assert isinstance(t(10), u.Quantity)
+    assert isinstance(t(10, with_bounding_box=True), u.Quantity)
+    
+    assert_quantity_allclose(t(10), t(10, with_bounding_box=True))
