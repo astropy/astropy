7699ec6c3f8e4af376d64553bbc8c76bf0b35617
perrygreenfield <perry@stsci.edu>
2019-07-11 14:57:51 -0400

    added text regarding restriction on slicing compound models to docs

--- astropy/modeling/core.py
+++ astropy/modeling/core.py
@@ -2918,6 +2918,7 @@ class CompoundModel(Model):
         self._param_map_inverse = dict((v, k) for k, v in param_map.items())
         self._initialize_slices()
         self._initialize_constraints()
+        self._param_names = tuple(self._param_names)
 
     def _initialize_slices(self):
         # TODO eliminate redundant code with core here and next two methods
