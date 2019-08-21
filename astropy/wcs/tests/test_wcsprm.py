1059f523610c484faf925f6fc3f88ac235d62d55
MSeifert04 <michaelseifert04@yahoo.de>
2019-08-21 17:40:55 +0200

    Make sure the getter of cunit and ctype also raises an IndexError for out-of-bounds accesses.

--- astropy/wcs/tests/test_wcsprm.py
+++ astropy/wcs/tests/test_wcsprm.py
@@ -265,10 +265,11 @@ def test_ctype_repr():
 def test_ctype_index_error():
     w = _wcs.Wcsprm()
     assert list(w.ctype) == ['', '']
-    with pytest.raises(IndexError):
-        w.ctype[2] = 'FOO'
-    with pytest.raises(IndexError):
-        w.ctype[-3] = 'FOO'
+    for idx in (2, -3):
+        with pytest.raises(IndexError):
+            w.ctype[idx]
+        with pytest.raises(IndexError):
+            w.ctype[idx] = 'FOO'
 
 
 def test_ctype_invalid_error():
@@ -337,10 +338,11 @@ def test_unit2():
 
 def test_unit3():
     w = wcs.WCS()
-    with pytest.raises(IndexError):
-        w.wcs.cunit[2] = u.m
-    with pytest.raises(IndexError):
-        w.wcs.cunit[-3] = u.m
+    for idx in (2, -3):
+        with pytest.raises(IndexError):
+            w.wcs.cunit[idx]
+        with pytest.raises(IndexError):
+            w.wcs.cunit[idx] = u.m
     with pytest.raises(ValueError):
         w.wcs.cunit = [u.m, u.m, u.m]
 
