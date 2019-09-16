329b3eb9ee1f98635e3a3e2e933c6085ad14ec03
Brigitta Sipocz <bsipocz@gmail.com>
2019-09-16 09:29:06 -0700

    Merge pull request #9234 from MSeifert04/doc-fix-render-issue-hdu-verify
    Fix rendering of verify method in documentation.

--- astropy/io/fits/verify.py
+++ astropy/io/fits/verify.py
@@ -61,7 +61,7 @@ class _Verify(object):
             Output verification option.  Must be one of ``"fix"``,
             ``"silentfix"``, ``"ignore"``, ``"warn"``, or
             ``"exception"``.  May also be any combination of ``"fix"`` or
-            ``"silentfix"`` with ``"+ignore"``, ``+warn``, or ``+exception"
+            ``"silentfix"`` with ``"+ignore"``, ``"+warn"``, or ``"+exception"``
             (e.g. ``"fix+warn"``).  See :ref:`verify` for more info.
         """
 
