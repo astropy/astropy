27dcb92be99b3579e445e97c7a8e30416d835447
Larry Bradley <larry.bradley@gmail.com>
2019-10-02 10:37:04 -0400

    Add biweight tuple axis tests

--- astropy/stats/tests/test_biweight.py
+++ astropy/stats/tests/test_biweight.py
@@ -2,11 +2,13 @@
 
 import pytest
 import numpy as np
-from numpy.testing import assert_allclose, assert_array_almost_equal_nulp
+from numpy.testing import (assert_allclose, assert_array_almost_equal_nulp,
+                           assert_equal)
 
 from astropy.stats.biweight import (biweight_location, biweight_scale,
-                        biweight_midvariance, biweight_midcovariance,
-                        biweight_midcorrelation)
+                                    biweight_midvariance,
+                                    biweight_midcovariance,
+                                    biweight_midcorrelation)
 from astropy.tests.helper import catch_warnings
 from astropy.utils.misc import NumpyRNGContext
 
@@ -98,6 +100,26 @@ def test_biweight_location_axis_3d():
         assert_allclose(bw[y], bwi)
 
 
+def test_biweight_location_axis_tuple():
+    """Test a 3D array with a tuple axis keyword."""
+
+    data = np.arange(24).reshape(2, 3, 4)
+    data[0, 0] = 100.
+
+    assert_equal(biweight_location(data, axis=0),
+                 biweight_location(data, axis=(0,)))
+    assert_equal(biweight_location(data, axis=-1),
+                 biweight_location(data, axis=(2,)))
+    assert_equal(biweight_location(data, axis=(0, 1)),
+                 biweight_location(data, axis=(0, 1)))
+    assert_equal(biweight_location(data, axis=(0, 2)),
+                 biweight_location(data, axis=(0, -1)))
+    assert_equal(biweight_location(data, axis=(0, 1, 2)),
+                 biweight_location(data, axis=(2, 0, 1)))
+    assert_equal(biweight_location(data, axis=(0, 1, 2)),
+                 biweight_location(data, axis=None))
+
+
 def test_biweight_scale():
     # NOTE:  biweight_scale is covered by biweight_midvariance tests
     data = [1, 3, 5, 500, 2]
@@ -169,6 +191,28 @@ def test_biweight_midvariance_axis_3d():
             bwi.append(biweight_midvariance(data[:, y, i]))
         bwi = np.array(bwi)
         assert_allclose(bw[y], bwi)
+
+
+def test_biweight_scale_axis_tuple():
+    """Test a 3D array with a tuple axis keyword."""
+
+    data = np.arange(24).reshape(2, 3, 4)
+    data[0, 0] = 100.
+
+    assert_equal(biweight_scale(data, axis=0),
+                 biweight_scale(data, axis=(0,)))
+    assert_equal(biweight_scale(data, axis=-1),
+                 biweight_scale(data, axis=(2,)))
+    assert_equal(biweight_scale(data, axis=(0, 1)),
+                 biweight_scale(data, axis=(0, 1)))
+    assert_equal(biweight_scale(data, axis=(0, 2)),
+                 biweight_scale(data, axis=(0, -1)))
+    assert_equal(biweight_scale(data, axis=(0, 1, 2)),
+                 biweight_scale(data, axis=(2, 0, 1)))
+    assert_equal(biweight_scale(data, axis=(0, 1, 2)),
+                 biweight_scale(data, axis=None))
+    assert_equal(biweight_scale(data, axis=(0, 2), modify_sample_size=True),
+                 biweight_scale(data, axis=(0, -1), modify_sample_size=True))
 
 
 def test_biweight_midvariance_constant_axis():
