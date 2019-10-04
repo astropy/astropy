50444993d941d1c4e78842aa7a471f1bb0dc6269
Erik Tollerud <erik.tollerud@gmail.com>
2019-10-04 17:51:46 -0400

    add back in hacks for incomplete generator machinery

--- cextern/erfa/erfaversion.c
+++ cextern/erfa/erfaversion.c
@@ -6,6 +6,20 @@
 ** This file is NOT derived from SOFA sources
 */
 
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.6.0"
+
+/* Define to the major version of this package. */
+#define PACKAGE_VERSION_MAJOR 1
+
+/* Define to the micro version of this package. */
+#define PACKAGE_VERSION_MICRO 0
+
+/* Define to the minor version of this package. */
+#define PACKAGE_VERSION_MINOR 6
+
+/* Define to the version of SOFA */
+#define SOFA_VERSION "20190722"
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
