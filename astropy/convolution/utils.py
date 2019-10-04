d23c4bfb1fb7372387601f18eb12359052ed35d7
Brigitta Sipocz <bsipocz@gmail.com>
2019-10-04 16:12:00 -0700

    Merge pull request #9293 from r-xue/fix-discretize_model-oversample
    fix a bug within discretize_oversample_1D/2D()

--- astropy/convolution/utils.py
+++ astropy/convolution/utils.py
@@ -244,14 +244,15 @@ def discretize_oversample_1D(model, x_range, factor=10):
     Discretize model by taking the average on an oversampled grid.
     """
     # Evaluate model on oversampled grid
-    x = np.arange(x_range[0] - 0.5 * (1 - 1 / factor),
-                  x_range[1] + 0.5 * (1 + 1 / factor), 1. / factor)
+    x = np.linspace(x_range[0] - 0.5 * (1 - 1 / factor),
+                    x_range[1] - 0.5 * (1 + 1 / factor),
+                    num = (x_range[1] - x_range[0]) * factor)
 
     values = model(x)
 
     # Reshape and compute mean
     values = np.reshape(values, (x.size // factor, factor))
-    return values.mean(axis=1)[:-1]
+    return values.mean(axis=1)
 
 
 def discretize_oversample_2D(model, x_range, y_range, factor=10):
@@ -259,18 +260,20 @@ def discretize_oversample_2D(model, x_range, y_range, factor=10):
     Discretize model by taking the average on an oversampled grid.
     """
     # Evaluate model on oversampled grid
-    x = np.arange(x_range[0] - 0.5 * (1 - 1 / factor),
-                  x_range[1] + 0.5 * (1 + 1 / factor), 1. / factor)
+    x = np.linspace(x_range[0] - 0.5 * (1 - 1 / factor),
+                    x_range[1] - 0.5 * (1 + 1 / factor),
+                    num = (x_range[1] - x_range[0]) * factor)
+    y = np.linspace(y_range[0] - 0.5 * (1 - 1 / factor),
+                    y_range[1] - 0.5 * (1 + 1 / factor),
+                    num = (y_range[1] - y_range[0]) * factor)
 
-    y = np.arange(y_range[0] - 0.5 * (1 - 1 / factor),
-                  y_range[1] + 0.5 * (1 + 1 / factor), 1. / factor)
     x_grid, y_grid = np.meshgrid(x, y)
     values = model(x_grid, y_grid)
 
     # Reshape and compute mean
     shape = (y.size // factor, factor, x.size // factor, factor)
     values = np.reshape(values, shape)
-    return values.mean(axis=3).mean(axis=1)[:-1, :-1]
+    return values.mean(axis=3).mean(axis=1)
 
 
 def discretize_integrate_1D(model, x_range):
