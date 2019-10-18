11661231ff2107b3e44402a4e71c5e3b3211c34b
Nadia Dencheva <nadia.dencheva@gmail.com>
2019-10-18 10:29:38 -0400

    add numpy ufuncs as models

---
+++ astropy/modeling/tests/test_math_func.py
@@ -0,0 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+import numpy as np
+from numpy.testing import assert_allclose#, assert_quantity_allclose
+
+from .. import math_functions
+
+x = np.linspace(-20, 360, 100)
+y = np.linspace(-20, 360, 100)
+
+def test_math():
+    for name in math_functions.__all__:
+        model = getattr(math_functions, name)()
+        print('name', name)
+        if model.n_inputs == 1:
+            assert_allclose(model(x), model.func(x))
+        elif model.n_inputs == 2:
+            assert_allclose(model(x, y), model.func(x, y))
