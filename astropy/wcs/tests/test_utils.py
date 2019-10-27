fix off-by-one error

astropy/wcs/tests/test_utils.py
@@ -1032,48 +1032,48 @@ def test_pixel_to_pixel_1d():
 def test_fit_wcs_from_points():
 
     header_str =  """
-XTENSION= 'IMAGE   '           / Image extension                                
-BITPIX  =                  -32 / array data type                                
-NAXIS   =                    2 / number of array dimensions                     
-NAXIS1  =                   50                                                  
-NAXIS2  =                   50                                                  
-PCOUNT  =                    0 / number of parameters                           
-GCOUNT  =                    1 / number of groups                               
-RADESYS = 'ICRS    '                                                            
-EQUINOX =               2000.0                                                  
-WCSAXES =                    2                                                  
-CTYPE1  = 'RA---TAN-SIP'                                                        
-CTYPE2  = 'DEC--TAN-SIP'                                                        
-CRVAL1  =    250.3497414839765                                                  
-CRVAL2  =    2.280925599609063                                                  
-CRPIX1  =               1045.0                                                  
-CRPIX2  =               1001.0                                                  
-CD1_1   =   -0.005564478186178                                                  
-CD1_2   =   -0.001042099258152                                                  
-CD2_1   =     0.00118144146585                                                  
-CD2_2   =   -0.005590816683583                                                  
-A_ORDER =                    2                                                  
-B_ORDER =                    2                                                  
-A_2_0   =    2.02451189234E-05                                                  
-A_0_2   =   3.317603337918E-06                                                  
-A_1_1   = 1.73456334971071E-05                                                  
-B_2_0   =   3.331330003472E-06                                                  
-B_0_2   = 2.04247482482589E-05                                                  
-B_1_1   = 1.71476710804143E-05                                                  
-AP_ORDER=                    2                                                  
-BP_ORDER=                    2                                                  
-AP_1_0  = 0.000904700296389636                                                  
-AP_0_1  = 0.000627660715584716                                                  
-AP_2_0  =  -2.023482905861E-05                                                  
-AP_0_2  =  -3.332285841011E-06                                                  
-AP_1_1  =  -1.731636633824E-05                                                  
-BP_1_0  = 0.000627960882053211                                                  
-BP_0_1  = 0.000911222886084808                                                  
-BP_2_0  =  -3.343918167224E-06                                                  
-BP_0_2  =  -2.041598249021E-05                                                  
-BP_1_1  =  -1.711876336719E-05                                                  
-A_DMAX  =    44.72893589844534                                                  
-B_DMAX  =    44.62692873032506       
+XTENSION= 'IMAGE   '           / Image extension
+BITPIX  =                  -32 / array data type
+NAXIS   =                    2 / number of array dimensions
+NAXIS1  =                   50
+NAXIS2  =                   50
+PCOUNT  =                    0 / number of parameters
+GCOUNT  =                    1 / number of groups
+RADESYS = 'ICRS    '
+EQUINOX =               2000.0
+WCSAXES =                    2
+CTYPE1  = 'RA---TAN-SIP'
+CTYPE2  = 'DEC--TAN-SIP'
+CRVAL1  =    250.3497414839765
+CRVAL2  =    2.280925599609063
+CRPIX1  =               1045.0
+CRPIX2  =               1001.0
+CD1_1   =   -0.005564478186178
+CD1_2   =   -0.001042099258152
+CD2_1   =     0.00118144146585
+CD2_2   =   -0.005590816683583
+A_ORDER =                    2
+B_ORDER =                    2
+A_2_0   =    2.02451189234E-05
+A_0_2   =   3.317603337918E-06
+A_1_1   = 1.73456334971071E-05
+B_2_0   =   3.331330003472E-06
+B_0_2   = 2.04247482482589E-05
+B_1_1   = 1.71476710804143E-05
+AP_ORDER=                    2
+BP_ORDER=                    2
+AP_1_0  = 0.000904700296389636
+AP_0_1  = 0.000627660715584716
+AP_2_0  =  -2.023482905861E-05
+AP_0_2  =  -3.332285841011E-06
+AP_1_1  =  -1.731636633824E-05
+BP_1_0  = 0.000627960882053211
+BP_0_1  = 0.000911222886084808
+BP_2_0  =  -3.343918167224E-06
+BP_0_2  =  -2.041598249021E-05
+BP_1_1  =  -1.711876336719E-05
+A_DMAX  =    44.72893589844534
+B_DMAX  =    44.62692873032506
 """
 
     header = fits.Header.fromstring(header_str, sep='\n')
@@ -1095,8 +1095,7 @@ B_DMAX  =    44.62692873032506
     world_pix_new = linear_wcs.pixel_to_world(x, y)
 
     # Checking the result
-    dists = world_pix.separation(world_pix_new).to('deg')
-    sigma = np.sqrt(sum(dists.value**2))
+    dists = world_pix.separation(world_pix_new)
 
-    assert dists.max().value < 7e-5
-    assert sigma < 2.5e-5
+    assert dists.max() < 7e-5*u.deg
+    assert np.std(dists) < 2.5e-5*u.deg