92b58e66935664cd99c0c1e4bef7a9cda793ed2e
Erik Tollerud <erik.tollerud@gmail.com>
2019-10-27 01:03:36 -0400

    swap default IERS A server

--- astropy/utils/iers/iers.py
+++ astropy/utils/iers/iers.py
@@ -45,8 +45,8 @@ __all__ = ['Conf', 'conf', 'earth_orientation_table',
 
 # IERS-A default file name, URL, and ReadMe with content description
 IERS_A_FILE = 'finals2000A.all'
-IERS_A_URL = 'https://datacenter.iers.org/data/9/finals2000A.all'
-IERS_A_URL_MIRROR = 'ftp://cddis.gsfc.nasa.gov/pub/products/iers/finals2000A.all'
+IERS_A_URL = 'ftp://cddis.gsfc.nasa.gov/pub/products/iers/finals2000A.all'
+IERS_A_URL_MIRROR = 'https://datacenter.iers.org/data/9/finals2000A.all'
 IERS_A_README = get_pkg_data_filename('data/ReadMe.finals2000A')
 
 # IERS-B default file name, URL, and ReadMe with content description
