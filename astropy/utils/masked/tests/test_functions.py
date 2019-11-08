5252e86fcfd345703e24f53c7d0bdd1a4eb79008
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-11-08 12:57:29 +0100

    Support broadcasting.

--- astropy/utils/masked/tests/test_functions.py
+++ astropy/utils/masked/tests/test_functions.py
@@ -2,8 +2,10 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
+from ..core import Masked
 
-from .test_masked import MaskedArraySetup, QuantitySetup, LongitudeSetup
+from .test_masked import (MaskedArraySetup, QuantitySetup, LongitudeSetup,
+                          assert_masked_equal)
 
 
 class TestMaskedArrayConcatenation(MaskedArraySetup):
@@ -24,4 +26,39 @@ class TestMaskedQuantityConcatenation(TestMaskedArrayConcatenation,
 
 class TestMaskedLongitudeConcatenation(TestMaskedArrayConcatenation,
                                        LongitudeSetup):
+    pass
+
+
+class TestMaskedArrayBroadcast(MaskedArraySetup):
+    def test_broadcast_to(self):
+        shape = self.ma.shape
+        ba = np.broadcast_to(self.mb, shape, subok=True)
+        assert ba.shape == shape
+        assert ba.mask.shape == shape
+        expected = Masked(np.broadcast_to(self.mb.unmasked, shape, subok=True),
+                          np.broadcast_to(self.mb.mask, shape, subok=True))
+        assert_masked_equal(ba, expected)
+
+    def test_broadcast_arrays(self):
+        mb = np.broadcast_arrays(self.ma, self.mb, self.mc, subok=True)
+        b = np.broadcast_arrays(self.a, self.b, self.c, subok=True)
+        bm = np.broadcast_arrays(self.mask_a, self.mask_b, self.mask_c)
+        for mb_, b_, bm_ in zip(mb, b, bm):
+            assert_array_equal(mb_.unmasked, b_)
+            assert_array_equal(mb_.mask, bm_)
+
+    def test_broadcast_arrays_not_all_masked(self):
+        mb = np.broadcast_arrays(self.a, self.mb, self.c, subok=True)
+        assert_array_equal(mb[0], self.a)
+        expected1 = np.broadcast_to(self.mb, self.a.shape, subok=True)
+        assert_masked_equal(mb[1], expected1)
+        expected2 = np.broadcast_to(self.c, self.a.shape, subok=True)
+        assert_array_equal(mb[2], expected2)
+
+
+class TestMaskedQuantityBroadcast(TestMaskedArrayBroadcast, QuantitySetup):
+    pass
+
+
+class TestMaskedLongitudeBroadcast(TestMaskedArrayBroadcast, LongitudeSetup):
     pass
