6513f308baba5b594f51215fb1f7eb817c2d0fc2
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-11-08 11:51:06 +0100

    Split off tests for numpy functions.

---
+++ astropy/utils/masked/tests/test_functions.py
@@ -0,0 +1,27 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+import numpy as np
+from numpy.testing import assert_array_equal
+
+
+from .test_masked import MaskedArraySetup, QuantitySetup, LongitudeSetup
+
+
+class TestMaskedArrayConcatenation(MaskedArraySetup):
+    def test_concatenate(self):
+        mb = self.mb[np.newaxis]
+        concat_a_b = np.concatenate((self.ma, mb), axis=0)
+        expected_data = np.concatenate((self.a, self.b[np.newaxis]), axis=0)
+        expected_mask = np.concatenate((self.mask_a, self.mask_b[np.newaxis]),
+                                       axis=0)
+        assert_array_equal(concat_a_b.unmasked, expected_data)
+        assert_array_equal(concat_a_b.mask, expected_mask)
+
+
+class TestMaskedQuantityConcatenation(TestMaskedArrayConcatenation,
+                                      QuantitySetup):
+    pass
+
+
+class TestMaskedLongitudeConcatenation(TestMaskedArrayConcatenation,
+                                       LongitudeSetup):
+    pass
