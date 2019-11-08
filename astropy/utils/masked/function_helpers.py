3237c7c1e11e8a1ec08771503fd1e5f3e1a795de
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2019-11-08 11:46:35 +0100

    Support sorting, plus minor cleanup.

--- astropy/utils/masked/function_helpers.py
+++ astropy/utils/masked/function_helpers.py
@@ -10,23 +10,33 @@ APPLY_TO_BOTH_FUNCTIONS = {}
 apply_to_both = FunctionAssigner(APPLY_TO_BOTH_FUNCTIONS)
 
 
-def _data_mask(*args):
+def _data_mask(array):
     from .core import Masked
 
+    if isinstance(array, Masked):
+        return array.unmasked, array.mask
+    else:
+        return array, np.zeros(np.shape(array), bool)
+
+
+def _data_masks(*args):
     data = []
     masks = []
     for arg in args:
-        if isinstance(arg, Masked):
-            data.append(arg.unmasked)
-            masks.append(arg.mask)
-        else:
-            data.append(arg)
-            masks.append(np.zeros(np.shape(arg), bool))
+        datum, mask = _data_mask(arg)
+        data.append(datum)
+        masks.append(mask)
 
     return data, masks
 
 
 @apply_to_both
 def concatenate(arrays, axis=0, out=None):
-    data, masks = _data_mask(*arrays)
+    data, masks = _data_masks(*arrays)
     return data, masks, (axis,), {}, out
+
+
+@apply_to_both
+def take_along_axis(arr, indices, axis):
+    data, mask = _data_mask(arr)
+    return data, mask, (indices, axis), {}, None
