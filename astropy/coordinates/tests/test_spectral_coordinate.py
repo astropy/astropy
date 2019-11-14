31bfa1a089337ce8e379bc15e50cd04ea7de1a37
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-14 16:27:18 +0000

    Refactored/tidied up test for initialization with observer and target

--- astropy/coordinates/tests/test_spectral_coordinate.py
+++ astropy/coordinates/tests/test_spectral_coordinate.py
@@ -3,21 +3,13 @@ import pytest
 from astropy.tests.helper import assert_quantity_allclose
 from astropy.coordinates import (SkyCoord, EarthLocation, ICRS, GCRS, Galactic,
                                  CartesianDifferential, SphericalRepresentation,
-                                 RadialDifferential, get_body_barycentric_posvel)
+                                 RadialDifferential, get_body_barycentric_posvel,
+                                 FK5)
 from astropy import time
 import numpy as np
 import astropy.units as u
 
 from ..spectra.spectral_coordinate import SpectralCoord
-
-# Local Standard of Rest
-LSRD = Galactic(u=0 * u.km, v=0 * u.km, w=0 * u.km,
-                U=9 * u.km / u.s, V=12 * u.km / u.s, W=7 * u.km / u.s,
-                representation_type='cartesian', differential_type='cartesian')
-
-# Target located in direction of motion of LSRD
-LSRD_DIR_STATIONARY = Galactic(u=9 * u.km, v=12 * u.km, w=7 * u.km,
-                               representation_type='cartesian')
 
 
 def assert_frame_allclose(frame1, frame2,
@@ -32,60 +24,10 @@ def assert_frame_allclose(frame1, frame2,
     assert frame1 is frame2
 
 
-# GENERAL TESTS
+def test_create_spectral_coord_orig():
 
+    # TODO: decide whether this test is still needed once the rest is implemented
 
-class TestCreateSpectralCoord:
-
-    def setup_class(self):
-        self.wav = [100, 200, 300] * u.nm
-
-    def test_simple(self):
-        # Initialize a SpectralCoord without any observer or target
-        coord = SpectralCoord(self.wav)
-        assert coord.observer is None
-        assert coord.target is None
-        assert coord.rest is None
-        assert coord.velocity_convention is None
-        assert coord.radial_velocity is None
-        assert coord.redshift is None
-
-    @pytest.mark.parametrize('observer_skycoord', [False, True])
-    def test_with_frame_observer(self, observer_skycoord):
-        # Ensure we can initialize a SpectralCoord with an observer
-        observer = LSRD
-        if observer_skycoord:
-            observer = SkyCoord(observer)
-        coord = SpectralCoord(self.wav, observer=LSRD)
-        assert coord.observer is LSRD
-        assert coord.target is None
-        assert coord.rest is None
-        assert coord.velocity_convention is None
-        assert coord.radial_velocity is None
-        assert coord.redshift is None
-
-    @pytest.mark.parametrize('observer_skycoord', [False, True])
-    @pytest.mark.parametrize('target_skycoord', [False, True])
-    def test_with_skycoord_observer_skycoord_target(self, observer_skycoord, target_skycoord):
-        # Ensure we can initialize a SpectralCoord with an observer and a target
-        observer = LSRD
-        if observer_skycoord:
-            observer = SkyCoord(observer)
-        target = Galactic(u=9 * u.km, v=12 * u.km, w=7 * u.km,
-                          representation_type='cartesian')
-        if target_skycoord:
-            target = SkyCoord(target)
-        coord = SpectralCoord(self.wav, observer=observer, target=target)
-        assert coord.observer is skycoord
-        assert coord.target is target
-        assert coord.rest is None
-        assert coord.velocity_convention is None
-        assert_quantity_allclose(coord.radial_velocity,
-                                 (9 * 9 + 12 * 12 + 7 * 7) ** 0.5 * u.km / u.s)
-        assert coord.redshift is None
-
-
-def test_create_spectral_coord():
     keck = EarthLocation.of_site('keck')
     obstime = time.Time('2018-12-13 9:00')
 
@@ -98,6 +40,67 @@ def test_create_spectral_coord():
     assert len(spec_coord) == 1000
 
     spec_coord.rest = 1215 * u.AA
+
+
+# GENERAL TESTS
+
+# We first run through a series of cases to test different ways of initializing
+# the observer and target for SpectralCoord, including for example frames,
+# SkyCoords, and making sure that SpectralCoord is not sensitive to the actual
+# frame or representation class.
+
+# Local Standard of Rest
+LSRD = Galactic(u=0 * u.km, v=0 * u.km, w=0 * u.km,
+                U=9 * u.km / u.s, V=12 * u.km / u.s, W=7 * u.km / u.s,
+                representation_type='cartesian', differential_type='cartesian')
+
+LSRD_EQUIV = [LSRD, SkyCoord(LSRD), LSRD.transform_to(ICRS), LSRD.represent_as('spherical')]
+
+
+@pytest.fixture(params=[None] + LSRD_EQUIV)
+def observer(request):
+    return request.param
+
+
+# Target located in direction of motion of LSRD with no velocities
+LSRD_DIR_STATIONARY = Galactic(u=9 * u.km, v=12 * u.km, w=7 * u.km,
+                               representation_type='cartesian')
+
+LSRD_DIR_STATIONARY_EQUIV = [LSRD_DIR_STATIONARY, SkyCoord(LSRD_DIR_STATIONARY),
+                             LSRD_DIR_STATIONARY.transform_to(FK5()), LSRD_DIR_STATIONARY.represent_as('cylindrical')]
+
+
+@pytest.fixture(params=[None] + LSRD_DIR_STATIONARY_EQUIV)
+def target(request):
+    return request.param
+
+
+def test_create_spectral_coord_observer_target(observer, target):
+
+    coord = SpectralCoord([100, 200, 300] * u.nm, observer=observer, target=target)
+
+    if observer is None:
+        assert coord.observer is None
+    else:
+        assert_frame_allclose(observer, coord.observer)
+
+    if target is None:
+        assert coord.target is None
+    else:
+        assert_frame_allclose(target, coord.target)
+
+    assert coord.rest is None
+    assert coord.velocity_convention is None
+    assert coord.radial_velocity is None
+
+    if observer is None or target is None:
+        assert coord.redshift is None
+        assert coord.radial_velocity is None
+    elif observer in LSRD_EQUIV and target in LSRD_DIR_STATIONARY_EQUIV:
+        assert_quantity_allclose(coord.redshift, ((1 + 274 / 299792.458**2) / (1 - 274 / 299792.458**2)) ** 0.5 - 1)
+        assert_quantity_allclose(coord.radial_velocity, 274 ** 0.5 * u.km / u.s)
+    else:
+        raise NotImplementedError()
 
 
 # SCIENCE USE CASE TESTS
