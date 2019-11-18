8f8fd43e112a63c087e57e65c0b42026589544a5
Erik Tollerud <erik.tollerud@gmail.com>
2019-11-18 18:03:24 -0500

    add shifting tests

--- astropy/coordinates/tests/test_spectral_coordinate.py
+++ astropy/coordinates/tests/test_spectral_coordinate.py
@@ -8,6 +8,7 @@ from astropy.coordinates import (SkyCoord, EarthLocation, ICRS, GCRS, Galactic,
 from astropy import time
 import numpy as np
 import astropy.units as u
+from astropy.constants import c
 
 from ..spectra.spectral_coordinate import SpectralCoord
 
@@ -167,3 +168,74 @@ def test_spectral_coord_m31():
     # maximum possible speed should be M31 + earth + earth's surface.  Then
     # transform to an approximate redshift bound
     assert np.abs(spc.redshift) < 0.00112
+
+
+def test_shift_to_rest_galaxy():
+    """
+    This tests storing a spectral coordinate with a specific redshift, and then
+    doing basic rest-to-observed-and-back transformations
+    """
+    z = 5
+    rest_line_wls = [5007, 6563]*u.angstrom
+
+    observed_spc = SpectralCoord(rest_line_wls*(z+1), redshift=z)
+    rest_spc = observed_spc.to_rest()
+    # alternatively:
+    # rest_spc = observed_spc.with_observer(observed_spec.target)
+    # although then it would have to be clearly documented, or the `to_rest` implemented in Spectrum1D?
+
+    assert_quantity_allclose(rest_spc, rest_line_wls)
+    assert_frame_allclose(rest_spc.observer, rest_spc.target)
+
+    with pytest.raises:
+        # *any* observer shift should fail, since we didn't specify one at the
+        # outset
+        rest_spc.with_observer(ICRS())
+
+
+    # note: it may be an acceptable fallback for the next part to onle work on
+    # spectrum1D but not SpectralCoord - the thinking being that the shift to
+    # rest might require dropping the "observer" information from spectralcoord.
+    # but maybe not? So including the test here.
+
+    roundtrip_obs_spc = rest_spc.to_observed()
+    assert_quantity_allclose(roundtrip_obs_spc, rest_line_wls*(z+1))
+
+
+
+def test_shift_to_rest_star_withobserver():
+    rv = -18*u.km/u.s
+    rest_line_wls = [5007, 6563]*u.angstrom
+
+    obstime = time.Time('2018-12-13 9:00')
+    eloc = EarthLocation.of_site('greenwich')
+    obs = eloc.get_gcrs(obstime)  # always built-in, no download required
+    acen = SkyCoord(ra=219.90085*u.deg, dec=-60.83562*u.deg, frame='icrs',
+                    distance=4.37*u.lightyear)
+    # Note that above the rv is missing from the SkyCoord.
+    # That's intended, as it will instead be set in the `SpectralCoord`.  But
+    # the SpectralCoord machinery should yield something comparable to test_spectral_coord_alphacen
+
+    observed_spc = SpectralCoord(rest_line_wls*(rv/c + 1),
+                                 observer=obs, target=acen, radial_velocity=rv)
+
+    rest_spc = observed_spc.to_rest()
+    assert_quantity_allclose(rest_spc, rest_line_wls)
+
+    barycentric_spc = observed_spc.with_observer(ICRS())
+    baryrest_spc = observed_spc.to_rest()
+    assert not u.quantity_allclose(baryrest_spc, rest_line_wls)
+
+    # now make sure the change the barycentric shift did is comparable to the
+    # offset rv_correction produces
+    barytarg = SkyCoord(barycentric_spc.target)
+    vcorr = barytarg.rv_correction(kind='barycentric', obstime=obstime,
+                                   location=eloc)
+
+    drv = baryrest_spc.radial_velocity - observed_spc.radial_velocity
+
+    # note this probably will not work on the first try, but it's ok if this is
+    # "good enough", where good enough is estimated below.  But that could be
+    # adjusted if we think that's too agressive of a precision target for what
+    # the machinery can handle
+    assert_quantity_allclose(vcorr, drv, atol=10*u.m/u.s)
