20ac429549c00c6d56242d95ad15ba17c42e8cc4
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-12-05 14:30:44 +0000

    Remove safeguard against importing astropy during setup since it should no longer be a risk

--- setup.py
+++ setup.py
@@ -2,13 +2,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
-import builtins
 from setuptools import setup
-
-# We set up the following variable because we then use this in astropy/__init__.py
-# to make sure that we aren't importing astropy during the setup process (we used
-# to do this)
-builtins._ASTROPY_CORE_SETUP_ = True
 
 # NOTE: The configuration for the package, including the name, version, and
 # other information are set in the setup.cfg file. Here we mainly set up
