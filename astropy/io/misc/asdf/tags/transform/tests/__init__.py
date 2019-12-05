ac224da7e827987f95d058effe2b7f75a0355dc6
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-12-05 17:27:04 +0000

    Skip asdf tests if entry points are not installed
    This allows tests to be run without issues by building extensions in-place then running pytest

---
+++ astropy/io/misc/asdf/tags/transform/tests/__init__.py
@@ -0,0 +1,6 @@
+import pytest
+from astropy.io.misc.asdf.tests import ASDF_ENTRY_INSTALLED
+
+if not ASDF_ENTRY_INSTALLED:
+    pytest.skip('The astropy asdf entry points are not installed',
+                allow_module_level=True)
--- astropy/io/misc/asdf/tests/__init__.py
+++ astropy/io/misc/asdf/tests/__init__.py
@@ -1,2 +1,17 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
+
+# Define a constant to know if the entry points are installed, since this impacts
+# whether we can run the tests.
+
+import pytest
+from pkg_resources import iter_entry_points
+
+entry_points = [entry.name for entry in iter_entry_points('asdf_extensions')]
+ASDF_ENTRY_INSTALLED = 'astropy' in entry_points and 'astropy-asdf' in entry_points
+
+del entry_points, iter_entry_points
+
+if not ASDF_ENTRY_INSTALLED:
+    pytest.skip('The astropy asdf entry points are not installed',
+                allow_module_level=True)
