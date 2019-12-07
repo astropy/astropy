e4bf3d66319712ddede2ce2168a02e8833a5d6de
Brigitta Sipőcz <bsipocz@gmail.com>
2019-12-07 10:23:24 -0800

    Merge pull request #9734 from mhvk/erfa-update-to-1.7
    Update bundled erfa sources to 1.7

--- cextern/erfa/erfaextra.h
+++ cextern/erfa/erfaextra.h
@@ -1,9 +1,20 @@
 /*
-** Copyright (C) 2016-2017, NumFOCUS Foundation.
+** Copyright (C) 2016-2019, NumFOCUS Foundation.
 **
 ** Licensed under a 3-clause BSD style license - see LICENSE
 **
-** This file is NOT derived from SOFA sources
+** This file is NOT derived from SOFA sources.
+**
+** The functions here provide an interface to ERFA and SOFA version
+** information, and for updating the leap second table.
+**
+** The eraGetLeapSeconds and eraSetLeapSeconds functions are used as an
+** experimental interface for getting and setting the leap second table in
+** astropy 4.0.  They will be supported as long as astropy 4.0 is supported
+** (until 2021), but not necessarily beyond.  Comments and ideas about the
+** best way to keep the leap second tables up to date for all users of erfa
+** are welcome (https://github.com/liberfa/erfa).
+**
 */
 
 
@@ -51,9 +62,8 @@ int eraVersionMicro(void);
 const char* eraSofaVersion(void);
 
 
-
 /*
-** Get and set leap seconds (not supported by SOFA)
+** Get and set leap seconds (not supported by SOFA; EXPERIMENTAL)
 */
 int eraGetLeapSeconds(eraLEAPSECOND **leapseconds);
 void eraSetLeapSeconds(eraLEAPSECOND *leapseconds, int count);
