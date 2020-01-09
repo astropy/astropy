a46772f87751bc7822bcf5fc4838fadd3766d72f
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-09 15:03:48 +0000

    Fixed tests with latest developer version of Matplotlib

--- astropy/tests/image_tests.py
+++ astropy/tests/image_tests.py
@@ -5,10 +5,10 @@ from astropy.utils.decorators import wraps
 
 MPL_VERSION = matplotlib.__version__
 
-# The developer versions of the form 3.1.x+... contain changes that will only
-# be included in the 3.2.x release, so we update this here.
-if MPL_VERSION[:3] == '3.1' and '+' in MPL_VERSION:
-    MPL_VERSION = '3.2'
+# The developer versions of the form 3.2.x+... contain changes that will only
+# be included in the 3.3.x release, so we update this here.
+if MPL_VERSION[:3] == '3.2' and '+' in MPL_VERSION:
+    MPL_VERSION = '3.3'
 
 ROOT = "http://{server}/testing/astropy/2019-08-02T11:38:58.288466/{mpl_version}/"
 IMAGE_REFERENCE_DIR = (ROOT.format(server='data.astropy.org', mpl_version=MPL_VERSION[:3] + '.x') + ',' +
