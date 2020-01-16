653fa512220a2abdac83c51dabb040d7afac2d3e
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-01-16 15:17:24 +0000

    Use setuptools_scm version template to automatically pick up changes in version if setuptools_scm is installed

--- setup.py
+++ setup.py
@@ -63,5 +63,17 @@ if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
     print(DOCS_HELP)
     sys.exit(1)
 
-setup(use_scm_version={'write_to': os.path.join('astropy', 'version.py')},
+VERSION_TEMPLATE = """
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+try:
+    from setuptools_scm import get_version
+    version = get_version(root='..', relative_to=__file__)
+except Exception:
+    version = '{version}'
+""".lstrip()
+
+setup(use_scm_version={'write_to': os.path.join('astropy', 'version.py'),
+                       'write_to_template': VERSION_TEMPLATE},
       ext_modules=get_extensions())
