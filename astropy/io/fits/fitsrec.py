a549a0e1b54565e159402010469407db39325149
Derek Homeier <dhomeie@gwdg.de>
2020-01-29 19:39:20 +0100

    BUG: set FITS ASCII table int dtype as required by colwidth

--- astropy/io/fits/fitsrec.py
+++ astropy/io/fits/fitsrec.py
@@ -843,7 +843,10 @@ class FITS_rec(np.recarray):
         """
 
         format = column.format
-        recformat = ASCII2NUMPY[format[0]]
+        if hasattr(column, 'dtype'):
+            recformat = column.dtype
+        else:
+            recformat = ASCII2NUMPY[format[0]]
         # if the string = TNULL, return ASCIITNULL
         nullval = str(column.null).strip().encode('ascii')
         if len(nullval) > format.width:
