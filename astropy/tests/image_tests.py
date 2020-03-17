57a4bf6016a61ff0fdabd7d07d3c44a29ee5018b
Leo Singer <leo.singer@ligo.org>
2020-03-17 11:38:34 -0400

    TST: Make ignore_matplotlibrc a pytest fixture
    This allows it to be composed with pytest marks (e.g.
    `pytest.mark.skipif`). See #9991.

--- astropy/tests/image_tests.py
+++ astropy/tests/image_tests.py
@@ -13,13 +13,3 @@ if MPL_VERSION[:3] == '3.2' and '+' in MPL_VERSION:
 ROOT = "http://{server}/testing/astropy/2019-08-02T11:38:58.288466/{mpl_version}/"
 IMAGE_REFERENCE_DIR = (ROOT.format(server='data.astropy.org', mpl_version=MPL_VERSION[:3] + '.x') + ',' +
                        ROOT.format(server='www.astropy.org/astropy-data', mpl_version=MPL_VERSION[:3] + '.x'))
-
-
-def ignore_matplotlibrc(func):
-    # This is a decorator for tests that use matplotlib but not pytest-mpl
-    # (which already handles rcParams)
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with plt.style.context({}, after_reset=True):
-            return func(*args, **kwargs)
-    return wrapper
