67b51fd4bf5bb5f123364f219c02843a0fb4d707
nmearl <contact@nicholasearl.me>
2020-03-25 11:12:36 -0400

    Include new tags and types for spectral coordinate

---
+++ astropy/io/misc/asdf/tags/coordinates/spectralcoord.py
@@ -0,0 +1,41 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from asdf.tags.core import NDArrayType
+from asdf.yamlutil import custom_tree_to_tagged_tree
+
+from astropy.coordinates.spectral_coord import SpectralCoord
+from astropy.io.misc.asdf.types import AstropyType
+from astropy.io.misc.asdf.tags.unit.unit import UnitType
+
+
+__all__ = ['SpectralCoordType']
+
+
+class SpectralCoordType(AstropyType):
+    """
+    ASDF tag implementation used to serialize/derialize SpectralCoord objects
+    """
+    name = 'coordinates/spectral_coord'
+    types = [SpectralCoord]
+    version = '1.0.0'
+
+    @classmethod
+    def to_tree(cls, spec_coord, ctx):
+        node = {}
+        if isinstance(spec_coord, SpectralCoord):
+            node['value'] = custom_tree_to_tagged_tree(spec_coord.value, ctx)
+            node['unit'] = custom_tree_to_tagged_tree(spec_coord.unit, ctx)
+            return node
+        raise TypeError(f"'{spec_coord}' is not a valid SpectralCoord")
+
+    @classmethod
+    def from_tree(cls, node, ctx):
+        if isinstance(node, SpectralCoord):
+            return node
+
+        unit = UnitType.from_tree(node['unit'], ctx)
+        value = node['value']
+        if isinstance(value, NDArrayType):
+            value = value._make_array()
+        return SpectralCoord(value, unit=unit)
