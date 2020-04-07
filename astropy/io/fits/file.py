e207c8a22728c8dad7554a122e1f5f8f0fb6aa8a
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-04-07 10:57:10 +0100

    Change ostream from meaning 'wb' to 'wb+' to allow writing of dask arrays to memory map.

--- astropy/io/fits/file.py
+++ astropy/io/fits/file.py
@@ -41,7 +41,7 @@ IO_FITS_MODES = {
     'copyonwrite': 'rb',
     'update': 'rb+',
     'append': 'ab+',
-    'ostream': 'wb',
+    'ostream': 'wb+',
     'denywrite': 'rb'}
 
 # Maps OS-level file modes to the appropriate astropy.io.fits specific mode
