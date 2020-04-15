061f944e90eaa0e9f1d07f9acc61d5035dc8fb1b
Tom Aldcroft <taldcroft@gmail.com>
2020-04-15 10:02:27 -0400

    Make tests compatible with numpy 1.16

--- astropy/coordinates/tests/test_sky_coord.py
+++ astropy/coordinates/tests/test_sky_coord.py
@@ -392,25 +392,25 @@ def test_setitem_no_velocity(frame):
 
     sc1 = sc0.copy()
     sc1[1] = sc2[0]
-    assert np.allclose(sc1.ra, [1, 10] * u.deg)
-    assert np.allclose(sc1.dec, [3, 30] * u.deg)
+    assert np.allclose(sc1.ra.to_value(u.deg), [1, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [3, 30])
     assert sc1.obstime == Time('B1955')
     assert sc1.frame.name == frame
 
     sc1 = sc0.copy()
     sc1[:] = sc2[0]
-    assert np.allclose(sc1.ra, [10, 10] * u.deg)
-    assert np.allclose(sc1.dec, [30, 30] * u.deg)
+    assert np.allclose(sc1.ra.to_value(u.deg), [10, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [30, 30])
 
     sc1 = sc0.copy()
     sc1[:] = sc2[:]
-    assert np.allclose(sc1.ra, [10, 20] * u.deg)
-    assert np.allclose(sc1.dec, [30, 40] * u.deg)
+    assert np.allclose(sc1.ra.to_value(u.deg), [10, 20])
+    assert np.allclose(sc1.dec.to_value(u.deg), [30, 40])
 
     sc1 = sc0.copy()
     sc1[[1, 0]] = sc2[:]
-    assert np.allclose(sc1.ra, [20, 10] * u.deg)
-    assert np.allclose(sc1.dec, [40, 30] * u.deg)
+    assert np.allclose(sc1.ra.to_value(u.deg), [20, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [40, 30])
 
 
 def test_setitem_velocities():
@@ -423,29 +423,29 @@ def test_setitem_velocities():
 
     sc1 = sc0.copy()
     sc1[1] = sc2[0]
-    assert np.allclose(sc1.ra, [1, 10] * u.deg)
-    assert np.allclose(sc1.dec, [3, 30] * u.deg)
-    assert np.allclose(sc1.radial_velocity, [1, 10] * u.km / u.s)
+    assert np.allclose(sc1.ra.to_value(u.deg), [1, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [3, 30])
+    assert np.allclose(sc1.radial_velocity.to_value(u.km / u.s), [1, 10])
     assert sc1.obstime == Time('B1950')
     assert sc1.frame.name == 'fk4'
 
     sc1 = sc0.copy()
     sc1[:] = sc2[0]
-    assert np.allclose(sc1.ra, [10, 10] * u.deg)
-    assert np.allclose(sc1.dec, [30, 30] * u.deg)
-    assert np.allclose(sc1.radial_velocity, [10, 10] * u.km / u.s)
+    assert np.allclose(sc1.ra.to_value(u.deg), [10, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [30, 30])
+    assert np.allclose(sc1.radial_velocity.to_value(u.km / u.s), [10, 10])
 
     sc1 = sc0.copy()
     sc1[:] = sc2[:]
-    assert np.allclose(sc1.ra, [10, 20] * u.deg)
-    assert np.allclose(sc1.dec, [30, 40] * u.deg)
-    assert np.allclose(sc1.radial_velocity, [10, 20] * u.km / u.s)
+    assert np.allclose(sc1.ra.to_value(u.deg), [10, 20])
+    assert np.allclose(sc1.dec.to_value(u.deg), [30, 40])
+    assert np.allclose(sc1.radial_velocity.to_value(u.km / u.s), [10, 20])
 
     sc1 = sc0.copy()
     sc1[[1, 0]] = sc2[:]
-    assert np.allclose(sc1.ra, [20, 10] * u.deg)
-    assert np.allclose(sc1.dec, [40, 30] * u.deg)
-    assert np.allclose(sc1.radial_velocity, [20, 10] * u.km / u.s)
+    assert np.allclose(sc1.ra.to_value(u.deg), [20, 10])
+    assert np.allclose(sc1.dec.to_value(u.deg), [40, 30])
+    assert np.allclose(sc1.radial_velocity.to_value(u.km / u.s), [20, 10])
 
 
 def test_setitem_exceptions():
