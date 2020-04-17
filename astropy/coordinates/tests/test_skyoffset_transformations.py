1e5e2b274571857e567bedc51f46aff9b8d967f4
Jerry Ma <jerry.ma.nk@gmail.com>
2020-04-17 17:07:56 -0400

    Fix SkyOffsetFrame wrap angle for data stored as non-spherical representation type.

--- astropy/coordinates/tests/test_skyoffset_transformations.py
+++ astropy/coordinates/tests/test_skyoffset_transformations.py
@@ -290,6 +290,15 @@ def test_skyoffset_lonwrap():
     sc = SkyCoord(190*u.deg, -45*u.deg, frame=SkyOffsetFrame(origin=origin))
     assert sc.lon < 180 * u.deg
 
+    sc2 = SkyCoord(-10*u.deg, -45*u.deg, frame=SkyOffsetFrame(origin=origin))
+    assert sc2.lon < 180 * u.deg
+
+    sc3 = sc.realize_frame(sc.represent_as('cartesian'))
+    assert sc3.lon < 180 * u.deg
+
+    sc4 = sc2.realize_frame(sc2.represent_as('cartesian'))
+    assert sc4.lon < 180 * u.deg
+
 
 def test_skyoffset_velocity():
     c = ICRS(ra=170.9*u.deg, dec=-78.4*u.deg,
