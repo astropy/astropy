23fee5b217031e026f14e88c869c01a66e8dc6f7
Thomas Robitaille <thomas.robitaille@gmail.com>
2020-04-30 17:52:38 +0100

    Improve behavior with ufuncs and arithmetic to only return SpectralQuantity/SpectralCoord in a few specific cases

--- astropy/coordinates/tests/test_spectral_quantity.py
+++ astropy/coordinates/tests/test_spectral_quantity.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from numpy.testing import assert_allclose
 
@@ -36,6 +37,8 @@ class TestSpectralQuantity:
         sq1 = SpectralQuantity(1 * unit1)
         sq2 = sq1.to(unit2)
         sq3 = sq2.to(unit1)
+        assert isinstance(sq2, SpectralQuantity)
+        assert isinstance(sq3, SpectralQuantity)
         assert_quantity_allclose(sq1, sq3)
 
     def test_doppler_conversion(self):
@@ -127,21 +130,59 @@ class TestSpectralQuantity:
         # Checks for arithmetic - some operations should return SpectralQuantity,
         # while some should just return plain Quantity
 
+        # First, operations that should return SpectralQuantity
+
         sq1 = SpectralQuantity(10 * u.AA)
         sq2 = sq1 * 2
         assert isinstance(sq2, SpectralQuantity)
         assert sq2.value == 20
-        assert sq2.unit is u.AA
+        assert sq2.unit == u.AA
 
-        sq1 = SpectralQuantity(10 * u.AA)
-        sq3 = sq1 / u.s
-        # FIXME: for now, this doesn't work because A/s is a valid velocity unit
-        # assert isinstance(sq3, u.Quantity) and not isinstance(sq3, SpectralQuantity)
-        assert sq3.value == 10
-        assert sq3.unit.is_equivalent(u.AA / u.s)
+        sq2 = sq1 / 2
+        assert isinstance(sq2, SpectralQuantity)
+        assert sq2.value == 5
+        assert sq2.unit == u.AA
 
-        sq1 = SpectralQuantity(10 * u.AA)
-        sq4 = sq1 / u.kg
-        assert isinstance(sq4, u.Quantity) and not isinstance(sq4, SpectralQuantity)
-        assert sq4.value == 10
-        assert sq4.unit.is_equivalent(u.AA / u.kg)
+        # Next, operations that should return Quantity
+
+        q3 = sq1 / u.s
+        assert isinstance(q3, u.Quantity) and not isinstance(q3, SpectralQuantity)
+        assert q3.value == 10
+        assert q3.unit.is_equivalent(u.AA / u.s)
+
+        q4 = sq1 / u.kg
+        assert isinstance(q4, u.Quantity) and not isinstance(q4, SpectralQuantity)
+        assert q4.value == 10
+        assert q4.unit.is_equivalent(u.AA / u.kg)
+
+        q5 = sq1 + 10 * u.AA
+        assert isinstance(q5, u.Quantity) and not isinstance(q5, SpectralQuantity)
+        assert q5.value == 20
+        assert q5.unit == u.AA
+
+        q6 = sq1 / SpectralQuantity(5 * u.AA)
+        assert isinstance(q6, u.Quantity) and not isinstance(q6, SpectralQuantity)
+        assert q6.value == 2
+        assert q6.unit == u.one
+
+    def test_ufuncs(self):
+
+        # Checks for ufuncs - some operations should return SpectralQuantity,
+        # while some should just return plain Quantity
+
+        # First, operations that should return SpectralQuantity
+
+        sq1 = SpectralQuantity([10, 20, 30] * u.AA)
+        for ufunc in (np.max, np.nanmax, np.min, np.nanmin):
+            sq2 = ufunc(sq1)
+            assert isinstance(sq2, SpectralQuantity)
+            assert sq2.value == ufunc(sq1.value)
+            assert sq2.unit == u.AA
+
+        # Next, operations that should return Quantity
+
+        for ufunc in (np.std, np.sum):
+            q3 = ufunc(sq1)
+            assert isinstance(q3, u.Quantity) and not isinstance(q3, SpectralQuantity)
+            assert q3.value == ufunc(sq1.value)
+            assert q3.unit == u.AA
