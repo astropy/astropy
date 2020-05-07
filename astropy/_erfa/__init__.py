9fd16137c1bf567927563cb586e90eaa4eb7bc8c
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-05-07 19:50:39 -0400

    Replace _erfa with imports from pyerfa

--- astropy/_erfa/__init__.py
+++ astropy/_erfa/__init__.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-from .core import *
-from .ufunc import (dt_eraASTROM, dt_eraLDBODY, dt_eraLEAPSECOND, dt_pv,
-                    dt_sign, dt_type, dt_ymdf, dt_hmsf, dt_dmsf)
-from .helpers import leap_seconds
+from erfa import core, ufunc, helpers
+from erfa.core import *
+from erfa.ufunc import (dt_eraASTROM, dt_eraLDBODY, dt_eraLEAPSECOND, dt_pv,
+                        dt_sign, dt_type, dt_ymdf, dt_hmsf, dt_dmsf)
+from erfa.helpers import leap_seconds
