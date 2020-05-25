407b2d89ae9a3b9e8aa87aa9993cb2cc86a100c6
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-05-25 08:54:15 -0400

    Merge pull request #10373 from Zac-HD/property-tests
    Set up Hypothesis and add property-based tests for `Time` precision

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ import os
 import pkg_resources
 import tempfile
 
+import hypothesis
 
 try:
     from pytest_astropy_header.display import PYTEST_HEADER_MODULES
@@ -25,6 +26,19 @@ if find_spec('asdf') is not None:
         if "asdf_schema_tester" not in entry_points:
             pytest_plugins += ['asdf.tests.schema_tester']
         PYTEST_HEADER_MODULES['Asdf'] = 'asdf'
+
+# Tell Hypothesis that we might be running slow tests, to print the seed blob
+# so we can easily reproduce failures from CI, and derive a fuzzing profile
+# to try many more inputs.  Select profiles with the HYPOTHESIS_PROFILE
+# environment variable, defaulting to automatic CI detection.
+
+hypothesis.settings.register_profile('normal', deadline=None)
+hypothesis.settings.register_profile('ci', deadline=None, print_blob=True)
+hypothesis.settings.register_profile(
+    'fuzzing', parent=hypothesis.settings.get_profile('ci'), max_examples=10**4
+)
+default = 'ci' if os.environ.get('CI') == 'true' else 'normal'
+hypothesis.settings.load_profile(os.environ.get('HYPOTHESIS_PROFILE', default))
 
 # Make sure we use temporary directories for the config and cache
 # so that the tests are insensitive to local configuration.
