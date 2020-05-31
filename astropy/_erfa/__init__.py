c8a559ed9e85795f67f26bdb7f27f1d62f6e92e6
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-05-30 21:33:01 -0400

    Deprecate _erfa and the associated error and warning

--- astropy/_erfa/__init__.py
+++ astropy/_erfa/__init__.py
@@ -1,7 +1,16 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+import warnings
 
-from erfa import core, ufunc, helpers
-from erfa.core import *
-from erfa.ufunc import (dt_eraASTROM, dt_eraLDBODY, dt_eraLEAPSECOND, dt_pv,
+from erfa import core, ufunc, helpers  # noqa
+from erfa.core import *  # noqa
+from erfa.ufunc import (dt_eraASTROM, dt_eraLDBODY, dt_eraLEAPSECOND, dt_pv,  # noqa
                         dt_sign, dt_type, dt_ymdf, dt_hmsf, dt_dmsf)
-from erfa.helpers import leap_seconds
+from erfa.helpers import leap_seconds  # noqa
+
+from astropy.utils.exceptions import AstropyDeprecationWarning
+
+
+warnings.warn('The private astropy._erfa module has been made into its '
+              'own package, pyerfa, which is a dependency of '
+              'astropy and can be imported directly using "import erfa"',
+              AstropyDeprecationWarning)
