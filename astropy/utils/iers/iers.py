e7f347b2d04896e96f3f24629ebb0cc2eea03dcc
Anne Archibald <peridot.faceted@gmail.com>
2020-07-31 18:34:44 +0100

    Remove 'ftp_tls' from IERS files

--- astropy/utils/iers/iers.py
+++ astropy/utils/iers/iers.py
@@ -87,7 +87,6 @@ def download_file(*args, **kwargs):
     """
     kwargs.setdefault('http_headers', {'User-Agent': 'astropy/iers',
                                        'Accept': '*/*'})
-    kwargs.setdefault('ftp_tls', True)
 
     with utils.data.conf.set_temp('remote_timeout', conf.remote_timeout):
         return utils.data.download_file(*args, **kwargs)
