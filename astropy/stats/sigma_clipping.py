88fac1e4ba8a5471dcd7a371e95913d6d1551799
James E.H. Turner <jturner@gemini.edu>
2020-08-03 18:09:09 -0400

    Use double rather than single back-ticks around axis keyword in hope of solving readthedocs failure.
    Co-authored-by: Simon Conseil <contact@saimon.org>

--- astropy/stats/sigma_clipping.py
+++ astropy/stats/sigma_clipping.py
@@ -167,7 +167,7 @@ class SigmaClip:
 
     grow : float or `False`, optional
         Radius within which to mask the neighbouring pixels of those that
-        fall outwith the clipping limits (only applied along `axis`, if
+        fall outwith the clipping limits (only applied along ``axis``, if
         specified). A value of 1 will mask the nearest pixels in a cross
         pattern around each deviant pixel, while 1.5 will also reject the
         nearest diagonal neighbours and so on.
@@ -628,7 +628,7 @@ def sigma_clip(data, sigma=3, sigma_lower=None, sigma_upper=None, maxiters=5,
 
     grow : float or `False`, optional
         Radius within which to mask the neighbouring pixels of those that
-        fall outwith the clipping limits (only applied along `axis`, if
+        fall outwith the clipping limits (only applied along ``axis``, if
         specified). A value of 1 will mask the nearest pixels in a cross
         pattern around each deviant pixel, while 1.5 will also reject the
         nearest diagonal neighbours and so on.
@@ -786,7 +786,7 @@ def sigma_clipped_stats(data, mask=None, mask_value=None, sigma=3.0,
 
     grow : float or `False`, optional
         Radius within which to mask the neighbouring pixels of those that
-        fall outwith the clipping limits (only applied along `axis`, if
+        fall outwith the clipping limits (only applied along ``axis``, if
         specified). A value of 1 will mask the nearest pixels in a cross
         pattern around each deviant pixel, while 1.5 will also reject the
         nearest diagonal neighbours and so on.
