44131f303c085fc575a11a83e05d999aacb7500c
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-08-17 15:09:45 -0400

    Merge pull request #10659 from mhvk/numpy-dev-financial-removal
    Account for removal of financial functions in numpy 1.20

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -6,7 +6,8 @@ earlier versions of Numpy.
 from astropy.utils import minversion
 
 
-__all__ = ['NUMPY_LT_1_17', 'NUMPY_LT_1_18', 'NUMPY_LT_1_19']
+__all__ = ['NUMPY_LT_1_17', 'NUMPY_LT_1_18', 'NUMPY_LT_1_19',
+           'NUMPY_LT_1_20']
 
 # TODO: It might also be nice to have aliases to these named for specific
 # features/bugs we're checking for (ex:
@@ -14,3 +15,4 @@ __all__ = ['NUMPY_LT_1_17', 'NUMPY_LT_1_18', 'NUMPY_LT_1_19']
 NUMPY_LT_1_17 = not minversion('numpy', '1.17')
 NUMPY_LT_1_18 = not minversion('numpy', '1.18')
 NUMPY_LT_1_19 = not minversion('numpy', '1.19')
+NUMPY_LT_1_20 = not minversion('numpy', '1.20')
