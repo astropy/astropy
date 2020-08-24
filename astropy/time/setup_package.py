c2a00306c970ec4a8849f5452f2cd99ab462a883
Tom Aldcroft <taldcroft@gmail.com>
2020-08-24 09:56:10 -0400

    Add setup_package.py remove Makefile [skip ci]

---
+++ astropy/time/setup_package.py
@@ -0,0 +1,29 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# Copied from astropy/convolution/setup_package.py
+
+import os
+import sys
+from setuptools import Extension
+
+import numpy
+
+C_TIME_PKGDIR = os.path.relpath(os.path.dirname(__file__))
+
+SRC_FILES = [os.path.join(C_TIME_PKGDIR, filename)
+             for filename in ['src/parse_times.c']]
+
+extra_compile_args = ['-UNDEBUG']
+if not sys.platform.startswith('win'):
+    extra_compile_args.append('-fPIC')
+
+
+def get_extensions():
+    # Add '-Rpass-missed=.*' to ``extra_compile_args`` when compiling with clang
+    # to report missed optimizations
+    _time_ext = Extension(name='astropy.time._parse_times', sources=SRC_FILES,
+                          extra_compile_args=extra_compile_args,
+                          include_dirs=[numpy.get_include()],
+                          language='c')
+
+    return [_time_ext]
