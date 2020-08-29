0d45961831fa3915e18727e13f581def4f1db54b
Tom Aldcroft <taldcroft@gmail.com>
2020-08-29 14:33:15 -0400

    Add config option and factor out set_jds_fast

--- astropy/time/__init__.py
+++ astropy/time/__init__.py
@@ -1,3 +1,20 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
+from astropy import config as _config
+
+class Conf(_config.ConfigNamespace):  # noqa
+    """
+    Configuration parameters for `astropy.table`.
+    """
+
+    use_fast_parser = _config.ConfigItem(
+        ['True', 'False', 'force'],
+        "Use fast C parser for supported time strings formats, including ISO, "
+        "ISOT, and YearDayTime. Allowed values are the 'False' (use Python parser),"
+        "'True' (use C parser and fall through to Python parser if fails), and "
+        "'force' (use C parser and raise exception if it fails). Note that the"
+        "options are all strings.")
+
+conf = Conf()  # noqa
+
 from .formats import *  # noqa
 from .core import *  # noqa
