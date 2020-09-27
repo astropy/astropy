a02961107d4b3163a98f1e4501756e9fd41ea8e3
E. Madison Bray <erik.bray@lri.fr>
2020-09-27 18:13:39 +0200

    [#10760] Never use setuptools_scm except when importing astropy from within a checkout of the git repository
    This adds a special package astropy._dev which is never installed, and only
    exists when developing Astropy from within the source repo.  It is ommitted
    from distributions such as tarballs and wheels.
    
    astropy/version.py becomes a file checked into the repo, and the file
    generated by setuptools_scm is now astropy/_version.py, and is greatly
    simplified.
    
    astropy/version.py will use the version from astropy._dev._scm_version if
    it exists (generated at runtime by setuptools_scm).  Otherwise it will use
    the static version from astropy/_version.py.

---
+++ astropy/_dev/_scm_version.py
@@ -0,0 +1,8 @@
+# Try to use setuptools_scm to get the current version; this is only used
+# in development installations from the git repository.
+import os.path as pth
+try:
+    from setuptools_scm import get_version
+    version = get_version(root=pth.join('..', '..'), relative_to=__file__)
+except Exception:
+    raise ImportError('setuptools_scm broken or not installed')
