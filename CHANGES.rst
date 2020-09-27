2d99a81b23a3203e3b0ab50b1f52d67553607b73
Simon Conseil <contact@saimon.org>
2020-09-27 16:15:27 -0300

    Merge pull request #10772 from saimn/fix-osx-cfitsio
    Fix compilation of cfitsio with Xcode 12

--- CHANGES.rst
+++ CHANGES.rst
@@ -832,6 +832,8 @@ astropy.io.fits
 - Fix repr for commentary cards and strip spaces for commentary keywords.
   [#10640]
 
+- Fix compilation of cfitsio with Xcode 12. [#10772]
+
 astropy.io.misc
 ^^^^^^^^^^^^^^^
 
--- astropy/io/fits/setup_package.py
+++ astropy/io/fits/setup_package.py
@@ -33,6 +33,8 @@ def _get_compression_extension():
                 '-Wno-declaration-after-statement'
             ])
 
+            cfg['define_macros'].append(('HAVE_UNISTD_H', None))
+
             if not get_distutils_build_option('debug'):
                 # these switches are to silence warnings from compiling CFITSIO
                 # For full silencing, some are added that only are used in
