4a302f14742677e692e8f48c1fe5b43288eafe83
addddd123 <adshe886@gmail.com>
2020-10-01 19:53:44 +0530

    moved units to misc.py #fix-10787

--- astropy/units/astrophys.py
+++ astropy/units/astrophys.py
@@ -45,23 +45,8 @@ def_unit(['lyr', 'lightyear'], (_si.c * si.yr).to(si.m),
          namespace=_ns, prefixes=True, doc="Light year")
 
 
-###########################################################################
-# AREAS
-
-def_unit(['barn', 'barn'], 10 ** -28 * si.m ** 2, namespace=_ns, prefixes=True,
-         doc="barn: unit of area used in HEP")
 
 
-###########################################################################
-# ANGULAR MEASUREMENTS
-
-def_unit(['cycle', 'cy'], 2.0 * _numpy.pi * si.rad,
-         namespace=_ns, prefixes=False,
-         doc="cycle: angular measurement, a full turn or rotation")
-
-def_unit(['spat', 'sp'], 4.0 * _numpy.pi * si.sr,
-         namespace=_ns, prefixes=False,
-         doc="spat: the solid angle of the sphere, 4pi sr")
 
 ###########################################################################
 # MASS
@@ -81,10 +66,6 @@ def_unit(['M_p'], _si.m_p, namespace=_ns, doc="Proton mass",
          format={'latex': r'M_{p}', 'unicode': 'Mₚ'})
 def_unit(['M_e'], _si.m_e, namespace=_ns, doc="Electron mass",
          format={'latex': r'M_{e}', 'unicode': 'Mₑ'})
-# Unified atomic mass unit
-def_unit(['u', 'Da', 'Dalton'], _si.u, namespace=_ns,
-         prefixes=True, exclude_prefixes=['a', 'da'],
-         doc="Unified atomic mass unit")
 
 ##########################################################################
 # ENERGY
@@ -100,12 +81,6 @@ def_unit(['Ry', 'rydberg'],
          "constant",
          format={'latex': r'R_{\infty}', 'unicode': 'R∞'})
 
-##########################################################################
-# PRESSURE
-
-def_unit(['bar'], 1e5 * si.Pa, namespace=_ns,
-         prefixes=[(['m'], ['milli'], 1.e-3)],
-         doc="bar: pressure")
 
 ###########################################################################
 # ILLUMINATION
@@ -160,12 +135,7 @@ def_unit(['bin'], namespace=_ns, prefixes=True)
 def_unit((['vox', 'voxel'], ['voxel']),
          format={'fits': 'voxel', 'ogip': 'voxel', 'vounit': 'voxel'},
          namespace=_ns, prefixes=True)
-def_unit((['bit', 'b'], ['bit']), namespace=_ns,
-         prefixes=si_prefixes + binary_prefixes)
-def_unit((['byte', 'B'], ['byte']), 8 * bit, namespace=_ns,
-         format={'vounit': 'byte'},
-         prefixes=si_prefixes + binary_prefixes,
-         exclude_prefixes=['d'])
+
 def_unit(['adu'], namespace=_ns, prefixes=True)
 def_unit(['beam'], namespace=_ns, prefixes=True)
 def_unit(['electron'], doc="Number of electrons", namespace=_ns,
@@ -179,14 +149,6 @@ def_unit(['littleh'], namespace=_ns, prefixes=False,
          doc="Reduced/\"dimensionless\" Hubble constant",
          format={'latex': r'h_{100}'})
 
-# The torr is almost the same as mmHg but not quite.
-# See https://en.wikipedia.org/wiki/Torr
-# Define the unit here despite it not being an astrophysical unit.
-# It may be moved if more similar units are created later.
-def_unit(['Torr', 'torr'], _si.atm.value/760. * si.Pa, namespace=_ns,
-         prefixes=[(['m'], ['milli'], 1.e-3)],
-         doc="Unit of pressure based on an absolute scale, now defined as "
-             "exactly 1/760 of a standard atmosphere")
 
 ###########################################################################
 # CLEANUP
@@ -204,3 +166,5 @@ del si
 from .utils import generate_unit_summary as _generate_unit_summary
 if __doc__ is not None:
     __doc__ += _generate_unit_summary(globals())
+
+
--- astropy/utils/misc.py
+++ astropy/utils/misc.py
@@ -944,3 +944,57 @@ def coffee(is_adam=False, is_brigitta=False):  # pragma: no cover
     else:
         option = 'coffee'
     _hungry_for(option)
+
+
+
+
+###########################################################################
+#UNITS MOVED FROM ASTROPHY.PY TO THIS FILE
+
+###########################################################################
+# AREAS
+
+def_unit(['barn', 'barn'], 10 ** -28 * si.m ** 2, namespace=_ns, prefixes=True,
+         doc="barn: unit of area used in HEP")
+
+##########################################################################
+# PRESSURE
+
+def_unit(['bar'], 1e5 * si.Pa, namespace=_ns,
+         prefixes=[(['m'], ['milli'], 1.e-3)],
+         doc="bar: pressure")
+###
+
+def_unit((['bit', 'b'], ['bit']), namespace=_ns,
+         prefixes=si_prefixes + binary_prefixes)
+def_unit((['byte', 'B'], ['byte']), 8 * bit, namespace=_ns,
+         format={'vounit': 'byte'},
+         prefixes=si_prefixes + binary_prefixes,
+         exclude_prefixes=['d'])
+
+
+###########################################################################
+# ANGULAR MEASUREMENTS
+
+def_unit(['cycle', 'cy'], 2.0 * _numpy.pi * si.rad,
+         namespace=_ns, prefixes=False,
+         doc="cycle: angular measurement, a full turn or rotation")
+
+def_unit(['spat', 'sp'], 4.0 * _numpy.pi * si.sr,
+         namespace=_ns, prefixes=False,
+         doc="spat: the solid angle of the sphere, 4pi sr")
+
+
+###########################################################################
+# The torr is almost the same as mmHg but not quite.
+# See https://en.wikipedia.org/wiki/Torr
+# It may be moved if more similar units are created later.
+def_unit(['Torr', 'torr'], _si.atm.value/760. * si.Pa, namespace=_ns,
+         prefixes=[(['m'], ['milli'], 1.e-3)],
+         doc="Unit of pressure based on an absolute scale, now defined as "
+             "exactly 1/760 of a standard atmosphere")
+
+# Unified atomic mass unit
+def_unit(['u', 'Da', 'Dalton'], _si.u, namespace=_ns,
+         prefixes=True, exclude_prefixes=['a', 'da'],
+         doc="Unified atomic mass unit")
