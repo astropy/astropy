3a9b0d2db072dba34e7e0decc188ff9446fc35cc
Brigitta Sipocz <bsipocz@gmail.com>
2020-10-26 16:40:49 -0700

    Going back to previous workaround to approach cycler

--- astropy/visualization/mpl_style.py
+++ astropy/visualization/mpl_style.py
@@ -4,9 +4,12 @@
 # plotting style. It is no longer documented/recommended as of Astropy v3.0
 # but is kept here for backward-compatibility.
 
-# This is a required dependency of matplotlib, so should be present if
-# matplotlib is installed.
-from cycler import cycler
+from astropy import __minimum_matplotlib_version__
+from astropy.utils import minversion
+
+# This returns False if matplotlib cannot be imported
+MATPLOTLIB = minversion('matplotlib', __minimum_matplotlib_version__)
+
 
 __all__ = ['astropy_mpl_style_1', 'astropy_mpl_style']
 
@@ -74,7 +77,13 @@ color_cycle = ['#348ABD',   # blue
                '#188487',   # turquoise
                '#E24A33']   # orange
 
-astropy_mpl_style_1['axes.prop_cycle'] = cycler('color', color_cycle)
+if MATPLOTLIB:
+    # This is a dependency of matplotlib, so should be present.
+    from cycler import cycler
+    astropy_mpl_style_1['axes.prop_cycle'] = cycler('color', color_cycle)
+else:
+    astropy_mpl_style_1['axes.color_cycle'] = color_cycle
+
 
 astropy_mpl_style = astropy_mpl_style_1
 """The most recent version of the astropy plotting style."""
