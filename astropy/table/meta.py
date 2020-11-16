1a8731e697be021828d2040776e6767964c87882
Tom Aldcroft <taldcroft@gmail.com>
2020-11-16 05:33:08 -0500

    Remove unneeded code since dict is ordered

--- astropy/table/meta.py
+++ astropy/table/meta.py
@@ -1,59 +1,6 @@
 import textwrap
-import copy
 
 __all__ = ['get_header_from_yaml', 'get_yaml_from_header', 'get_yaml_from_table']
-
-
-class ColumnOrderList(list):
-    """
-    List of tuples that sorts in a specific order that makes sense for
-    astropy table column attributes.
-    """
-
-    def sort(self, *args, **kwargs):
-        super().sort()
-
-        column_keys = ['name', 'unit', 'datatype', 'format', 'description', 'meta']
-        in_dict = dict(self)
-        out_list = []
-
-        for key in column_keys:
-            if key in in_dict:
-                out_list.append((key, in_dict[key]))
-        for key, val in self:
-            if key not in column_keys:
-                out_list.append((key, val))
-
-        # Clear list in-place
-        del self[:]
-
-        self.extend(out_list)
-
-
-class ColumnDict(dict):
-    """
-    Specialized dict subclass to represent attributes of a Column
-    and return items() in a preferred order.  This is only for use
-    in generating a YAML map representation that has a fixed order.
-    """
-
-    def items(self):
-        """
-        Return items as a ColumnOrderList, which sorts in the preferred
-        way for column attributes.
-        """
-        return ColumnOrderList(super().items())
-
-
-def _repr_column_dict(dumper, data):
-    """
-    Represent ColumnDict in yaml dump.
-
-    This is the same as an ordinary mapping except that the keys
-    are written in a fixed order that makes sense for astropy table
-    columns.
-    """
-    return dumper.represent_mapping('tag:yaml.org,2002:map', data)
 
 
 def _get_col_attributes(col):
@@ -61,23 +8,29 @@ def _get_col_attributes(col):
     Extract information from a column (apart from the values) that is required
     to fully serialize the column.
     """
-    attrs = ColumnDict()
-    attrs['name'] = col.info.name
+    attrs = {}
 
-    type_name = col.info.dtype.type.__name__
-    if type_name.startswith(('bytes', 'str')):
-        type_name = 'string'
-    if type_name.endswith('_'):
-        type_name = type_name[:-1]  # string_ and bool_ lose the final _ for ECSV
-    attrs['datatype'] = type_name
+    def get_datatype(dtype):
+        datatype = dtype.type.__name__
+        if datatype.startswith(('bytes', 'str')):
+            datatype = 'string'
+        if datatype.endswith('_'):
+            datatype = datatype[:-1]  # string_ and bool_ lose the final _ for ECSV
+        return datatype
 
     # Set the output attributes
-    for attr, nontrivial, xform in (('unit', lambda x: x is not None, str),
+    for attr, nontrivial, xform in (('name', lambda x: True, None),
+                                    ('unit', lambda x: x is not None, str),
+                                    ('dtype', lambda x: True, get_datatype),
                                     ('format', lambda x: x is not None, None),
                                     ('description', lambda x: x is not None, None),
                                     ('meta', lambda x: x, None)):
         col_attr = getattr(col.info, attr)
         if nontrivial(col_attr):
+            if attr == 'dtype':
+                # ECSV uses `datatype` not `dtype` for similarity with ASDF and
+                # numpy agnosticism.
+                attr = 'datatype'
             attrs[attr] = xform(col_attr) if xform else col_attr
 
     return attrs
@@ -135,58 +88,12 @@ def get_yaml_from_header(header):
 
     from astropy.io.misc.yaml import AstropyDumper
 
-    class TableDumper(AstropyDumper):
-        """
-        Custom Dumper that represents dict (insertion-ordered) as an !!omap object.
-        """
-
-        def represent_mapping(self, tag, mapping, flow_style=None):
-            """
-            This is a combination of the Python 2 and 3 versions of this method
-            in the PyYAML library to allow the required key ordering via the
-            ColumnOrderList object.  The Python 3 version insists on turning the
-            items() mapping into a list object and sorting, which results in
-            alphabetical order for the column keys.
-            """
-            value = []
-            node = yaml.MappingNode(tag, value, flow_style=flow_style)
-            if self.alias_key is not None:
-                self.represented_objects[self.alias_key] = node
-            best_style = True
-            if hasattr(mapping, 'items'):
-                mapping = mapping.items()
-                if hasattr(mapping, 'sort'):
-                    mapping.sort()
-                else:
-                    mapping = list(mapping)
-                    try:
-                        mapping = sorted(mapping)
-                    except TypeError:
-                        pass
-
-            for item_key, item_value in mapping:
-                node_key = self.represent_data(item_key)
-                node_value = self.represent_data(item_value)
-                if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
-                    best_style = False
-                if not (isinstance(node_value, yaml.ScalarNode) and not node_value.style):
-                    best_style = False
-                value.append((node_key, node_value))
-            if flow_style is None:
-                if self.default_flow_style is not None:
-                    node.flow_style = self.default_flow_style
-                else:
-                    node.flow_style = best_style
-            return node
-
-    TableDumper.add_representer(ColumnDict, _repr_column_dict)
-
-    header = copy.copy(header)  # Don't overwrite original
     header['datatype'] = [_get_col_attributes(col) for col in header['cols']]
+    header = {key: header[key] for key in sorted(header)}
     del header['cols']
 
-    lines = yaml.dump(header, default_flow_style=None,
-                      Dumper=TableDumper, width=130).splitlines()
+    lines = yaml.dump(header, default_flow_style=None, sort_keys=False,
+                      Dumper=AstropyDumper, width=130).splitlines()
     return lines
 
 
