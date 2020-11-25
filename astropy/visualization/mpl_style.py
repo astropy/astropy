48ee52109c191cc37304525831832b183eee2671
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2020-11-25 16:02:03 -0500

    Remove minversion checks in asdf tests and mpl style

--- astropy/visualization/mpl_style.py
+++ astropy/visualization/mpl_style.py
@@ -4,18 +4,6 @@
 # plotting style. It is no longer documented/recommended as of Astropy v3.0
 # but is kept here for backward-compatibility.
 
-from pkg_resources import get_distribution
-
-from astropy.utils import minversion
-
-for cur_req in get_distribution('astropy').requires(extras=('all', )):
-    if cur_req.name == 'matplotlib':
-        __minimum_matplotlib_version__ = cur_req.specs[0][1]
-
-# This returns False if matplotlib cannot be imported
-MATPLOTLIB = minversion('matplotlib', __minimum_matplotlib_version__)
-
-
 __all__ = ['astropy_mpl_style_1', 'astropy_mpl_style']
 
 # Version 1 astropy plotting style for matplotlib
@@ -82,11 +70,12 @@ color_cycle = ['#348ABD',   # blue
                '#188487',   # turquoise
                '#E24A33']   # orange
 
-if MATPLOTLIB:
-    # This is a dependency of matplotlib, so should be present.
+try:
+    # This is a dependency of matplotlib, so should be present if matplotlib
+    # is installed.
     from cycler import cycler
     astropy_mpl_style_1['axes.prop_cycle'] = cycler('color', color_cycle)
-else:
+except ImportError:
     astropy_mpl_style_1['axes.color_cycle'] = color_cycle
 
 
