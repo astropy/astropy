17b0cbdd55e5288a52becf109f4cf3b529f4753c
Derek Homeier <dhomeie@gwdg.de>
2020-11-25 22:52:27 +0100

    +IGNORE_WARNINGS for doctest [skip ci]

--- astropy/cosmology/funcs.py
+++ astropy/cosmology/funcs.py
@@ -146,10 +146,10 @@ def z_at_value(func, fval, zmin=1e-8, zmax=1000, ztol=1e-8, maxfun=500,
     generally return a solution on the same side.
 
     >>> z_at_value(Planck13.angular_diameter_distance,
-    ...            1500 * u.Mpc, bracket=(1.0, 1.2))  # doctest: +FLOAT_CMP
+    ...            1500 * u.Mpc, bracket=(1.0, 1.2))  # doctest: +FLOAT_CMP +IGNORE_WARNINGS
     0.6812769577
     >>> z_at_value(Planck13.angular_diameter_distance,
-    ...            1500 * u.Mpc, bracket=(2.0, 2.5))  # doctest: +FLOAT_CMP
+    ...            1500 * u.Mpc, bracket=(2.0, 2.5))  # doctest: +FLOAT_CMP +IGNORE_WARNINGS
     3.7914913242
 
     Also note that the luminosity distance and distance modulus (two
