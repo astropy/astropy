9fd1faddbb7f50772a8ee67baaed8e39157a38f1
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2020-12-09 21:40:45 -0500

    Get gufunc to work generally independent of dimensions.
    With this, basic transformations of SkyCoord propagate masks
    correctly.

--- astropy/utils/masked/tests/test_containers.py
+++ astropy/utils/masked/tests/test_containers.py
@@ -6,13 +6,13 @@ from numpy.testing import assert_array_equal
 import pytest
 
 from astropy import units as u
-from astropy.coordinates import representation as r
+from astropy.coordinates import SkyCoord, representation as r
 
 from .. import Masked
 
 
 class TestRepresentations:
-    def setup(self):
+    def setup_class(self):
         self.x = np.array([3., 5., 0.]) << u.m
         self.y = np.array([4., 12., 1.]) << u.m
         self.z = np.array([0., 0., 1.]) << u.m
@@ -55,3 +55,28 @@ class TestRepresentations:
             c = getattr(cr2, comp)
             assert_array_equal(mc.unmasked, c)
             assert_array_equal(mc.mask, self.mask)
+
+
+class TestSkyCoord:
+    def setup_class(self):
+        self.ra = np.array([3., 5., 0.]) << u.hourangle
+        self.dec = np.array([4., 12., 1.]) << u.deg
+        self.sc = SkyCoord(self.ra, self.dec)
+        self.mask = np.array([False, False, True])
+        self.mra = Masked(self.ra, self.mask)
+        self.mdec = Masked(self.dec, self.mask)
+        self.msc = SkyCoord(self.mra, self.mdec)
+
+    def test_initialization(self):
+        check = self.msc.dec == self.mdec
+        assert_array_equal(check.unmasked, np.ones(3, bool))
+        assert_array_equal(check.mask, self.mask)
+        assert_array_equal(self.msc.data.lon, self.mra)
+        assert_array_equal(self.msc.data.lat, self.mdec)
+
+    def test_transformation(self):
+        gcrs = self.sc.gcrs
+        mgcrs = self.msc.gcrs
+        assert_array_equal(mgcrs.data.lon.mask, self.msc.data.lon.mask)
+        assert_array_equal(mgcrs.data.lon.unmasked, gcrs.data.lon)
+        assert_array_equal(mgcrs.data.lat.unmasked, gcrs.data.lat)
