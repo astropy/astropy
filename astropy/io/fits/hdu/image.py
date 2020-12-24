151af06440dfc0b99c7a3d7172b78599ca3375b4
P. L. Lim <2090236+pllim@users.noreply.github.com>
2020-12-24 16:00:46 -0500

    Merge pull request #11192 from timgates42/bugfix_typo_explicitly
    docs: fix simple typo, explcitly -> explicitly

--- astropy/io/fits/hdu/image.py
+++ astropy/io/fits/hdu/image.py
@@ -510,7 +510,7 @@ class _ImageBaseHDU(_ValidHDU):
         # Do the scaling
         if _zero != 0:
             # 0.9.6.3 to avoid out of range error for BZERO = +32768
-            # We have to explcitly cast _zero to prevent numpy from raising an
+            # We have to explicitly cast _zero to prevent numpy from raising an
             # error when doing self.data -= zero, and we do this instead of
             # self.data = self.data - zero to avoid doubling memory usage.
             np.add(self.data, -_zero, out=self.data, casting='unsafe')
