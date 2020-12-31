41d7c1cca27dcf646eb892f28314c4c965d50f24
Nick Murphy <namurphy@cfa.harvard.edu>
2020-12-31 18:13:35 -0500

    Parametrize existing tests for unit physical types

--- astropy/units/tests/test_physical.py
+++ astropy/units/tests/test_physical.py
@@ -11,56 +11,18 @@ from astropy.constants import hbar
 from astropy.tests.helper import raises
 
 
-def test_simple():
-    assert u.m.physical_type == 'length'
-
-
-def test_power():
-    assert (u.cm ** 3).physical_type == 'volume'
-
-
-def test_speed():
-    assert (u.km / u.h).physical_type == 'speed'
-
-
-def test_unknown():
-    assert (u.m * u.s).physical_type == 'unknown'
-
-
-def test_dimensionless():
-    assert (u.m / u.m).physical_type == 'dimensionless'
-
-
-def test_angular_momentum():
-    assert hbar.unit.physical_type == 'angular momentum'
-
-
-def test_flam():
-    flam = u.erg / (u.cm**2 * u.s * u.AA)
-    assert flam.physical_type == 'spectral flux density wav'
-
-
-def test_photlam():
-    photlam = u.photon / (u.cm ** 2 * u.s * u.AA)
-    assert photlam.physical_type == 'photon flux density wav'
-
-
-def test_photnu():
-    photnu = u.photon / (u.cm ** 2 * u.s * u.Hz)
-    assert photnu.physical_type == 'photon flux density'
-
-
-@raises(ValueError)
-def test_redundant_physical_type():
-    physical.def_physical_type(u.m, 'utter craziness')
-
-
-def test_data_quantity():
-    assert u.byte.physical_type == 'data quantity'
-    assert u.bit.physical_type == 'data quantity'
-
-
 unit_physical_type_pairs = [
+    (u.m, "length"),
+    (u.cm ** -3, "volume"),
+    (u.km / u.h, "speed"),
+    (u.m * u.s, "unknown"),
+    (u.m / u.m, "dimensionless"),
+    (hbar.unit, "angular momentum"),
+    (u.erg / (u.cm**2 * u.s * u.AA), "spectral flux density wav"),  # flam
+    (u.photon / (u.cm ** 2 * u.s * u.AA), "photon flux density wav"),  # photlam
+    (u.photon / (u.cm ** 2 * u.s * u.Hz), "photon flux density"),  # photnu
+    (u.byte, "data quantity"),
+    (u.bit, "data quantity"),
     (u.J * u.m ** -2 * u.s ** -1, "energy flux"),
     (u.cm ** -3 * u.hr ** -1, "volumetric rate"),
 ]
@@ -78,3 +40,8 @@ def test_physical_types(unit, physical_type):
             f"{repr(physical_type)}, but instead returned "
             f"{unit.physical_type}."
         )
+
+
+@raises(ValueError)
+def test_redundant_physical_type():
+    physical.def_physical_type(u.m, 'kessel run')
