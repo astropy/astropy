fc704a544c4c1178930b71212f79b0719ad8b84e
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2021-01-08 12:38:39 -0500

    Test roundtrip using multiple serialize methods.
    Clarify that serialize_method does not work for subclasses.
    
    This as it is not clear how to pass on that information to
    the underlying MaskedNDArrayInfo.

--- astropy/utils/masked/tests/test_table.py
+++ astropy/utils/masked/tests/test_table.py
@@ -9,24 +9,17 @@ from astropy import units as u
 from astropy.table import QTable
 
 from astropy.utils.masked import Masked
+from astropy.utils.compat.optional_deps import HAS_YAML, HAS_H5PY
 
-try:
-    import yaml  # noqa
-except ImportError:
-    HAS_YAML = False
-else:
-    HAS_YAML = True
+from .test_masked import assert_masked_equal
 
 
 FILE_FORMATS = ['ecsv', 'fits']
-try:
-    import h5py  # noqa
+if HAS_H5PY:
     FILE_FORMATS.append('h5')
-except ImportError:
-    pass
 
 
-class TestMaskedArrayTable:
+class MaskedArrayTableSetup:
     @classmethod
     def setup_arrays(self):
         self.a = np.array([3., 5., 0.])
@@ -39,6 +32,8 @@ class TestMaskedArrayTable:
         self.ma.info.format = '.1f'
         self.t = QTable([self.ma], names=['ma'])
 
+
+class TestMaskedArrayTable(MaskedArrayTableSetup):
     def test_table_initialization(self):
         assert_array_equal(self.t['ma'].unmasked, self.a)
         assert_array_equal(self.t['ma'].mask, self.mask_a)
@@ -51,14 +46,53 @@ class TestMaskedArrayTable:
     @pytest.mark.parametrize('file_format', FILE_FORMATS)
     def test_table_write(self, file_format, tmpdir):
         name = str(tmpdir.join(f"a.{file_format}"))
+        kwargs = {}
         if file_format == 'h5':
-            self.t.write(name, path='trial', serialize_meta=True)
-        else:
-            self.t.write(name)
+            kwargs['path'] = 'trial'
+            kwargs['serialize_meta'] = True
+
+        self.t.write(name, **kwargs)
         t2 = QTable.read(name)
         assert isinstance(t2['ma'], self.ma.__class__)
         assert np.all(t2['ma'] == self.ma)
         assert np.all(t2['ma'].mask == self.mask_a)
+        if file_format == 'fits' and type(self.a) is np.ndarray:
+            # Imperfect roundtrip through FITS native format description.
+            assert self.t['ma'].info.format in t2['ma'].info.format
+        else:
+            assert t2['ma'].info.format == self.t['ma'].info.format
+
+
+@pytest.mark.skipif(not HAS_YAML, reason='serialization needs yaml')
+class TestSerializationMethods(MaskedArrayTableSetup):
+    # TODO: ensure this works for MaskedQuantity, etc., as well.
+    # Needs to somehow pass on serialize_method; see MaskedArraySubclassInfo.
+    @pytest.mark.parametrize('serialize_method', ['data_mask', 'null_value'])
+    def test_table_write(self, serialize_method, tmpdir):
+        name = str(tmpdir.join("test.ecsv"))
+        self.t.write(name, serialize_method=serialize_method)
+        with open(name) as fh:
+            lines = fh.readlines()
+
+        t2 = QTable.read(name)
+        assert isinstance(t2['ma'], self.ma.__class__)
+
+        if serialize_method == 'data_mask':
+            # Will data_mask, we have data and mask columns and should
+            # exactly round-trip.
+            assert len(lines[-1].split()) == 2
+            assert_masked_equal(t2['ma'], self.ma)
+        else:
+            # With null_value we have just a data column with null values
+            # marked, so we lost information on the data below the mask.
+            assert len(lines[-1].split()) == 1
+            assert np.all(t2['ma'] == self.ma)
+            assert np.all(t2['ma'].mask == self.mask_a)
+
+    def test_non_existing_serialize_method(self, tmpdir):
+        name = str(tmpdir.join('bad.ecsv'))
+        with pytest.raises(ValueError, match='serialize method must be'):
+            self.t.write(name, serialize_method='bad_serialize_method')
 
 
 class TestMaskedQuantityTable(TestMaskedArrayTable):
