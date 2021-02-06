e7d129ce8708d7f2f5eb076dd29757588fdb694d
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2021-02-06 18:25:38 -0500

    Fix code style in setup_package.py

--- astropy/stats/setup_package.py
+++ astropy/stats/setup_package.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import os
-import sys
 from setuptools import Extension
 
 import numpy
@@ -11,6 +10,7 @@ SRCDIR = os.path.join(os.path.relpath(os.path.dirname(__file__)), 'src')
 SRCFILES = ['wirth_select.c', 'compute_bounds.c', 'fast_sigma_clip.c']
 
 SRCFILES = [os.path.join(SRCDIR, srcfile) for srcfile in SRCFILES]
+
 
 def get_extensions():
     _sigma_clip_ext = Extension(name='astropy.stats._fast_sigma_clip', sources=SRCFILES,
