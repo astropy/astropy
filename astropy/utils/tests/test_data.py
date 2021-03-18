d17add28355c70fbdec686165aa7a35ddc96196a
P. L. Lim <2090236+pllim@users.noreply.github.com>
2021-03-18 19:49:02 -0400

    Manual backport of #10757

--- astropy/utils/tests/test_data.py
+++ astropy/utils/tests/test_data.py
@@ -2100,7 +2100,7 @@ def test_clear_download_cache_variants(temp_cache, valid_urls):
     assert not is_url_in_cache(u)
 
 
-@pytest.mark.xfail('CI')
+@pytest.mark.skipif("CI", reason="Flaky on CI")
 @pytest.mark.remote_data
 def test_ftp_tls_auto(temp_cache):
     url = "ftp://anonymous:mail%40astropy.org@gdc.cddis.eosdis.nasa.gov/pub/products/iers/finals2000A.all"
