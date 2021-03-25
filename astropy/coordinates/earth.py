c5166fdbb18ed5a29cb45fcc5f84562f98eb88be
P. L. Lim <2090236+pllim@users.noreply.github.com>
2021-03-24 21:37:07 -0400

    Merge pull request #11422 from skulumani/earthlocation-typo
    Correct typo in doc string

--- astropy/coordinates/earth.py
+++ astropy/coordinates/earth.py
@@ -604,7 +604,7 @@ class EarthLocation(u.Quantity):
 
     @property
     def lat(self):
-        """Longitude of the location, for the default ellipsoid."""
+        """Latitude of the location, for the default ellipsoid."""
         return self.geodetic[1]
 
     @property
