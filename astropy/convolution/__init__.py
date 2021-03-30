8b36caaa03a68343bae106af9b70440cda8864cc
William Jamieson <wjamieson@stsci.edu>
2021-03-30 10:13:44 -0400

    Initial fixes for issue #11310
    Added initial Convolution model wrapper.
    
    Force fft_pading for convolve_models_fft to be on by default
    
    [ci skip] Added preliminary multi dimensional support. Note that > 1d is
    very slow right now.
    
    Faster possible interpolator.
    
    Added support for "mesh grid" array list and single point input. Currenlty cannot support fixing coordinates.
    
    Fixed inputs so that we can accept all types of inputs to the model.
    
    Fixed a typo
    
    Refactored convolution to support cache invalidation
    
    Added huge option to convolve_models_fft
    
    Added convolution tests
    
    Fixed coding style issues.
    
    Added test skipping for scipyless tests
    
    Moved scipy dependency inside function
    
    Apply suggestions from code review
    
    Co-authored-by: Nadia Dencheva <nadia.astropy@gmail.com>
    Co-authored-by: Adam Ginsburg <keflavich@gmail.com>
    
    Addressing further review comments by @keflavich and @nden
    
    Updated change log for `towncrier`

--- astropy/convolution/__init__.py
+++ astropy/convolution/__init__.py
@@ -5,7 +5,7 @@ from .core import *  # noqa
 from .kernels import *  # noqa
 from .utils import discretize_model  # noqa
 
-from .convolve import convolve, convolve_fft, interpolate_replace_nans, convolve_models  # noqa
+from .convolve import convolve, convolve_fft, interpolate_replace_nans, convolve_models, convolve_models_fft  # noqa
 
 # Deprecated kernels that are not defined in __all__
 from .kernels import MexicanHat1DKernel, MexicanHat2DKernel
