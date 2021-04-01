b414fb8ea73bdd14229c8430e8b099c89aa9dee5
Pey Lian Lim <2090236+pllim@users.noreply.github.com>
2021-04-01 12:48:01 -0400

    MNT: Sync conftest files

--- astropy/conftest.py
+++ astropy/conftest.py
@@ -1,8 +1,11 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-"""
-This file contains pytest configuration settings that are astropy-specific
+"""This file contains pytest configuration settings that are astropy-specific
 (i.e.  those that would not necessarily be shared by affiliated packages
 making use of astropy's test runner).
+
+This is the file that ``astropy.test()`` sees and is an exact copy of the one
+in the root directory, except for the fixtures and pytest_terminal_summary,
+which are only in this file.
 """
 import os
 import builtins
@@ -10,16 +13,17 @@ import sys
 import tempfile
 import warnings
 
+import hypothesis
+import pytest
+
+from astropy import __version__
+from astropy.tests.helper import enable_deprecations_as_exceptions
+
 try:
     from pytest_astropy_header.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 except ImportError:
     PYTEST_HEADER_MODULES = {}
     TESTED_VERSIONS = {}
-
-import pytest
-
-from astropy import __version__
-from astropy.tests.helper import enable_deprecations_as_exceptions
 
 try:
     # This is needed to silence a warning from matplotlib caused by
@@ -130,6 +134,12 @@ def pytest_unconfigure(config):
         os.environ['XDG_CACHE_HOME'] = builtins._xdg_cache_home_orig
 
 
+def pytest_report_header(config):
+    # This gets added after the pytest-astropy-header output.
+    return (f'ARCH_ON_CI: {os.environ.get("ARCH_ON_CI", "undefined")}\n'
+            f'IS_CRON: {os.environ.get("IS_CRON", "undefined")}\n')
+
+
 def pytest_terminal_summary(terminalreporter):
     """Output a warning to IPython users in case any tests failed."""
 
@@ -151,3 +161,33 @@ def pytest_terminal_summary(terminalreporter):
         'https://docs.astropy.org/en/stable/known_issues.html#failing-logging-'
         'tests-when-running-the-tests-in-ipython for more information.',
         yellow=True, bold=True)
+
+
+# Tell Hypothesis that we might be running slow tests, to print the seed blob
+# so we can easily reproduce failures from CI, and derive a fuzzing profile
+# to try many more inputs when we detect a scheduled build or when specifically
+# requested using the HYPOTHESIS_PROFILE=fuzz environment variable or
+# `pytest --hypothesis-profile=fuzz ...` argument.
+
+hypothesis.settings.register_profile(
+    'ci', deadline=None, print_blob=True, derandomize=True
+)
+hypothesis.settings.register_profile(
+    'fuzzing', deadline=None, print_blob=True, max_examples=1000
+)
+default = 'fuzzing' if (os.environ.get('IS_CRON') == 'true' and os.environ.get('ARCH_ON_CI') not in ('aarch64', 'ppc64le')) else 'ci'  # noqa: E501
+hypothesis.settings.load_profile(os.environ.get('HYPOTHESIS_PROFILE', default))
+
+# Make sure we use temporary directories for the config and cache
+# so that the tests are insensitive to local configuration.
+
+os.environ['XDG_CONFIG_HOME'] = tempfile.mkdtemp('astropy_config')
+os.environ['XDG_CACHE_HOME'] = tempfile.mkdtemp('astropy_cache')
+
+os.mkdir(os.path.join(os.environ['XDG_CONFIG_HOME'], 'astropy'))
+os.mkdir(os.path.join(os.environ['XDG_CACHE_HOME'], 'astropy'))
+
+# Note that we don't need to change the environment variables back or remove
+# them after testing, because they are only changed for the duration of the
+# Python process, and this configuration only matters if running pytest
+# directly, not from e.g. an IPython session.
