0c2eaadd09d77941d38257d221b5d7d83ed2c12d
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-04-06 21:06:27 -0400

    add support for subpackages too

--- astropy/utils/compat/optional_deps.py
+++ astropy/utils/compat/optional_deps.py
@@ -4,15 +4,20 @@
 """
 import importlib
 
-# TODO: This is a duplicate of the dependencies in setup.cfg "all", but some of
-# the package names are different from the pip-install name (e.g.,
+# First, the top-level packages:
+# TODO: This list is a duplicate of the dependencies in setup.cfg "all", but
+# some of the package names are different from the pip-install name (e.g.,
 # beautifulsoup4 -> bs4).
 _optional_deps = ['bleach', 'bottleneck', 'bs4', 'bz2', 'h5py', 'html5lib',
                   'IPython', 'jplephem', 'lxml', 'matplotlib', 'mpmath',
                   'pandas', 'PIL', 'pkg_resources', 'pytz', 'scipy', 'skyfield',
                   'sortedcontainers', 'lzma', 'yaml']
+_deps = {k: k for k in _optional_deps}
 
-__all__ = [f"HAS_{pkg}" for pkg in _optional_deps]
+# Any subpackages that have different import behavior:
+_deps['plt'] = 'matplotlib.pyplot'
+
+__all__ = [f"HAS_{pkg}" for pkg in _deps]
 
 
 def __getattr__(name):
@@ -20,7 +25,7 @@ def __getattr__(name):
         module_name = name[4:]
 
         try:
-            importlib.import_module(module_name)
+            importlib.import_module(_deps[module_name])
         except (ImportError, ModuleNotFoundError):
             return False
         return True
