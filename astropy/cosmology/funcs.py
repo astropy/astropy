890e69e62a768e7cfe1d8799dd435a4bd1856639
Derek Homeier <dhomeie@gwdg.de>
2021-04-27 01:10:12 +0200

    DOC: give doctest-pyversion a chance

--- astropy/cosmology/funcs.py
+++ astropy/cosmology/funcs.py
@@ -151,16 +151,21 @@ def z_at_value(func, fval, zmin=1e-8, zmax=1000, ztol=1e-8, maxfun=500,
     generally return a solution on the same side.
 
     >>> z_at_value(Planck18.angular_diameter_distance,
-    ...            1500 * u.Mpc, bracket=(1.0, 1.2))       # doctest: +SKIP
-    0.68044452                                             # doctest: +SKIP
+    ...            1500 * u.Mpc, bracket=(1.0, 1.2))       # doctest: +FLOAT_CMP +IGNORE_WARNINGS
+    0.68044452
+
+    ... doctest:
+        :pyversion: > 3.7
     >>> z_at_value(Planck18.angular_diameter_distance,
-    ...            1500 * u.Mpc, bracket=(2.0, 2.5))       # doctest: +SKIP
-    3.7823268                                              # doctest: +SKIP
+    ...            1500 * u.Mpc, bracket=(2.0, 2.5))       # doctest: +FLOAT_CMP +IGNORE_WARNINGS
+    3.7823268
 
     Be aware though that this does not guarantee the intended result if
     the bracket is chosen too wide and/or too close to the turning point.
     In such cases the 3-parameter variant can be more reliable.
 
+    ... doctest:
+        :pyversion: > 3.7
     >>> z_at_value(Planck18.angular_diameter_distance,
     ...            1500 * u.Mpc, bracket=(0.1, 1.5))       # doctest: +FLOAT_CMP
     3.7823268
