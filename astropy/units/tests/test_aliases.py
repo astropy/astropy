f5754486be81a5d760e335f7851d958f50abaa9c
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2021-04-26 17:06:57 -0400

    Also test the alias attribute of the registry.

--- astropy/units/tests/test_aliases.py
+++ astropy/units/tests/test_aliases.py
@@ -17,11 +17,40 @@ class TestAliases:
     def teardown_method(self):
         u.set_enabled_aliases({})
 
+    def teardown_class(self):
+        assert u.get_current_unit_registry().aliases == {}
+
+    @pytest.mark.parametrize('format_', [None, 'fits', 'ogip', 'vounit', 'cds'])
+    @pytest.mark.parametrize('aliases,bad,unit', trials)
+    def test_set_enabled_aliases_context_manager(self, aliases, bad, unit, format_):
+        if format_ == 'cds':
+            bad = bad.replace(' ', '.').replace('**', '')
+
+        with u.set_enabled_aliases(aliases):
+            assert u.get_current_unit_registry().aliases == aliases
+            assert u.Unit(bad) == unit
+
+        assert u.get_current_unit_registry().aliases == {}
+        with pytest.raises(ValueError):
+            u.Unit(bad)
+
+    @pytest.mark.parametrize('aliases,bad,unit', trials)
+    def test_add_enabled_aliases_context_manager(self, aliases, bad, unit):
+        with u.add_enabled_aliases(aliases):
+            assert u.get_current_unit_registry().aliases == aliases
+            assert u.Unit(bad) == unit
+
+        assert u.get_current_unit_registry().aliases == {}
+        with pytest.raises(ValueError):
+            u.Unit(bad)
+
     def test_set_enabled_aliases(self):
         for i, (aliases, bad, unit) in enumerate(trials):
             u.set_enabled_aliases(aliases)
-            bad_unit = u.Unit(bad)
-            assert bad_unit == unit
+
+            assert u.get_current_unit_registry().aliases == aliases
+
+            assert u.Unit(bad) == unit
 
             for _, bad2, unit2 in trials:
                 if bad2 == bad or bad2 in aliases:
@@ -31,8 +60,13 @@ class TestAliases:
                         u.Unit(bad2)
 
     def test_add_enabled_aliases(self):
+        expected_aliases = {}
         for i, (aliases, bad, unit) in enumerate(trials):
             u.add_enabled_aliases(aliases)
+
+            expected_aliases.update(aliases)
+            assert u.get_current_unit_registry().aliases == expected_aliases
+
             assert u.Unit(bad) == unit
 
             for j, (_, bad2, unit2) in enumerate(trials):
@@ -41,25 +75,6 @@ class TestAliases:
                 else:
                     with pytest.raises(ValueError):
                         u.Unit(bad2)
-
-    @pytest.mark.parametrize('format_', [None, 'fits', 'ogip', 'vounit', 'cds'])
-    @pytest.mark.parametrize('aliases,bad,unit', trials)
-    def test_set_enabled_aliases_context_manager(self, aliases, bad, unit, format_):
-        if format_ == 'cds':
-            bad = bad.replace(' ', '.').replace('**', '')
-        with u.set_enabled_aliases(aliases):
-            assert u.Unit(bad) == unit
-
-        with pytest.raises(ValueError):
-            u.Unit(bad)
-
-    @pytest.mark.parametrize('aliases,bad,unit', trials)
-    def test_add_enabled_aliases_context_manager(self, aliases, bad, unit):
-        with u.add_enabled_aliases(aliases):
-            assert u.Unit(bad) == unit
-
-        with pytest.raises(ValueError):
-            u.Unit(bad)
 
     def test_cannot_alias_existing_unit(self):
         with pytest.raises(ValueError, match='already means'):
