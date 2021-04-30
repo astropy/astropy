45e588135176f9afe05472c035806669c548e46d
Michael Brewer <brewer@astro.umass.edu>
2021-04-30 12:33:33 -0400

    Implement framework for observed hour angle-declination transforms.

--- astropy/coordinates/builtin_frames/icrs_observed_transforms.py
+++ astropy/coordinates/builtin_frames/icrs_observed_transforms.py
@@ -2,7 +2,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """
 Contains the transformation functions for getting to "observed" systems from ICRS.
-Currently that just means AltAz.
 """
 import erfa
 
@@ -16,17 +15,17 @@ from astropy.coordinates.representation import (SphericalRepresentation,
 
 from .icrs import ICRS
 from .altaz import AltAz
+from .hadec import HADec
 from .utils import PIOVER2
 from ..erfa_astrom import erfa_astrom
 
 
-@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, ICRS, AltAz)
-def icrs_to_altaz(icrs_coo, altaz_frame):
+def icrs_to_observed(icrs_coo, observed_frame, coord_type):
     # if the data are UnitSphericalRepresentation, we can skip the distance calculations
     is_unitspherical = (isinstance(icrs_coo.data, UnitSphericalRepresentation) or
                         icrs_coo.cartesian.x.unit == u.one)
-    # first set up the astrometry context for ICRS<->AltAz
-    astrom = erfa_astrom.get().apco(altaz_frame)
+    # first set up the astrometry context for ICRS<->observed
+    astrom = erfa_astrom.get().apco(observed_frame)
 
     # correct for parallax to find BCRS direction from observer (as in erfa.pmpx)
     if is_unitspherical:
@@ -39,36 +38,43 @@ def icrs_to_altaz(icrs_coo, altaz_frame):
     # convert to topocentric CIRS
     cirs_ra, cirs_dec = atciqz(srepr, astrom)
 
-    # now perform AltAz conversion
-    az, zen, ha, odec, ora = erfa.atioq(cirs_ra, cirs_dec, astrom)
-    alt = PIOVER2-zen
-    if is_unitspherical:
-        aa_srepr = UnitSphericalRepresentation(az << u.radian, alt << u.radian, copy=False)
+    # now perform observed conversion
+    if coord_type == 'H':
+        _, _, lon, lat, _ = erfa.atioq(cirs_ra, cirs_dec, astrom)
     else:
-        aa_srepr = SphericalRepresentation(az << u.radian, alt << u.radian, srepr.distance, copy=False)
-    return altaz_frame.realize_frame(aa_srepr)
+        lon, zen, _, _, _ = erfa.atioq(cirs_ra, cirs_dec, astrom)
+        lat = PIOVER2 - zen
+
+    if is_unitspherical:
+        obs_srepr = UnitSphericalRepresentation(lon << u.radian, lat << u.radian, copy=False)
+    else:
+        obs_srepr = SphericalRepresentation(lon << u.radian, lat << u.radian, srepr.distance, copy=False)
+    return observed_frame.realize_frame(obs_srepr)
 
 
-@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, AltAz, ICRS)
-def altaz_to_icrs(altaz_coo, icrs_frame):
+def observed_to_icrs(observed_coo, icrs_frame, coord_type):
     # if the data are UnitSphericalRepresentation, we can skip the distance calculations
-    is_unitspherical = (isinstance(altaz_coo.data, UnitSphericalRepresentation) or
-                        altaz_coo.cartesian.x.unit == u.one)
+    is_unitspherical = (isinstance(observed_coo.data, UnitSphericalRepresentation) or
+                        observed_coo.cartesian.x.unit == u.one)
 
-    usrepr = altaz_coo.represent_as(UnitSphericalRepresentation)
-    az = usrepr.lon.to_value(u.radian)
-    zen = PIOVER2 - usrepr.lat.to_value(u.radian)
+    usrepr = observed_coo.represent_as(UnitSphericalRepresentation)
+    lon = usrepr.lon.to_value(u.radian)
+    lat = usrepr.lat.to_value(u.radian)
 
-    # first set up the astrometry context for ICRS<->CIRS at the altaz_coo time
-    astrom = erfa_astrom.get().apco(altaz_coo)
+    if coord_type == 'A':
+        # the 'A' indicates zen/az inputs
+        lat = PIOVER2 - lat
+
+    # first set up the astrometry context for ICRS<->CIRS at the observed_coo time
+    astrom = erfa_astrom.get().apco(observed_coo)
 
     # Topocentric CIRS
-    cirs_ra, cirs_dec = erfa.atoiq('A', az, zen, astrom) << u.radian
+    cirs_ra, cirs_dec = erfa.atoiq(coord_type, lon, lat, astrom) << u.radian
     if is_unitspherical:
         srepr = SphericalRepresentation(cirs_ra, cirs_dec, 1, copy=False)
     else:
         srepr = SphericalRepresentation(lon=cirs_ra, lat=cirs_dec,
-                                        distance=altaz_coo.distance, copy=False)
+                                        distance=observed_coo.distance, copy=False)
 
     # BCRS (Astrometric) direction to source
     bcrs_ra, bcrs_dec = aticq(srepr, astrom) << u.radian
@@ -78,9 +84,29 @@ def altaz_to_icrs(altaz_coo, icrs_frame):
         icrs_srepr = UnitSphericalRepresentation(bcrs_ra, bcrs_dec, copy=False)
     else:
         icrs_srepr = SphericalRepresentation(lon=bcrs_ra, lat=bcrs_dec,
-                                             distance=altaz_coo.distance, copy=False)
+                                             distance=observed_coo.distance, copy=False)
         observer_icrs = CartesianRepresentation(astrom['eb'], unit=u.au, xyz_axis=-1, copy=False)
         newrepr = icrs_srepr.to_cartesian() + observer_icrs
         icrs_srepr = newrepr.represent_as(SphericalRepresentation)
 
     return icrs_frame.realize_frame(icrs_srepr)
+
+
+@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, ICRS, AltAz)
+def icrs_to_altaz(cirs_coo, altaz_frame):
+    return icrs_to_observed(cirs_coo, altaz_frame, 'A')
+
+
+@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, AltAz, ICRS)
+def altaz_to_icrs(altaz_coo, cirs_frame):
+    return observed_to_icrs(altaz_coo, cirs_frame, 'A')
+
+
+@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, ICRS, HADec)
+def icrs_to_hadec(cirs_coo, hadec_frame):
+    return icrs_to_observed(cirs_coo, hadec_frame, 'H')
+
+
+@frame_transform_graph.transform(FunctionTransformWithFiniteDifference, HADec, ICRS)
+def hadec_to_icrs(hadec_coo, cirs_frame):
+    return observed_to_icrs(hadec_coo, cirs_frame, 'H')
