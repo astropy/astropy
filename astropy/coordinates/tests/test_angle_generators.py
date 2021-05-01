c5d536b9736ed2fe1fde57aa51eecf391e4424ad
Erik Tollerud <erik.tollerud@gmail.com>
2021-05-01 12:03:08 -0400

    address @lpsinger's suggestion

--- astropy/coordinates/tests/test_angle_generators.py
+++ astropy/coordinates/tests/test_angle_generators.py
@@ -4,6 +4,7 @@ import numpy as np
 import pytest
 
 import astropy.units as u
+from astropy.utils import NumpyRNGContext
 from astropy.coordinates.angle_utilities import (
     golden_spiral_grid,
     uniform_spherical_random_surface,
@@ -18,18 +19,19 @@ def test_golden_spiral_grid_input():
 
 @pytest.mark.parametrize("func", [uniform_spherical_random_surface,
                                   uniform_spherical_random_volume])
-@pytest.mark.parametrize("rng", [None, np.random.default_rng(42)])
-def test_uniform_spherical_random_input(func, rng):
-    sph = func(size=100, rng=rng)
-    assert len(sph) == 100
+def test_uniform_spherical_random_input(func):
+    with NumpyRNGContext(42):
+        sph = func(size=100)
+        assert len(sph) == 100
 
 
 def test_uniform_spherical_random_volume_input():
-    sph = uniform_spherical_random_volume(size=100, max_radius=1)
-    assert len(sph) == 100
-    assert sph.distance.unit == u.dimensionless_unscaled
-    assert sph.distance.max() <= 1.
+    with NumpyRNGContext(42):
+        sph = uniform_spherical_random_volume(size=100, max_radius=1)
+        assert len(sph) == 100
+        assert sph.distance.unit == u.dimensionless_unscaled
+        assert sph.distance.max() <= 1.
 
-    sph = uniform_spherical_random_volume(size=100, max_radius=4*u.pc)
-    assert len(sph) == 100
-    assert sph.distance.max() <= 4*u.pc
+        sph = uniform_spherical_random_volume(size=100, max_radius=4*u.pc)
+        assert len(sph) == 100
+        assert sph.distance.max() <= 4*u.pc
