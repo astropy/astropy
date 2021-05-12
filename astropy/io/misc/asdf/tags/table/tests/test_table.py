1dcf9f3382882001d8c6d8d32c32406a5733bed4
Ed Slavich <eslavich@stsci.edu>
2021-05-12 14:21:28 -0400

    Compare asdf version

--- astropy/io/misc/asdf/tags/table/tests/test_table.py
+++ astropy/io/misc/asdf/tags/table/tests/test_table.py
@@ -6,6 +6,7 @@ import pytest
 asdf = pytest.importorskip('asdf')
 
 import numpy as np
+from packaging.version import Version
 
 import astropy.units as u
 from astropy import table
@@ -98,13 +99,12 @@ def test_table_inline(tmpdir):
     def check(ff):
         assert len(list(ff.blocks.internal_blocks)) == 0
 
-    if hasattr(asdf, 'config_context'):
-        # asdf 2.8
+    if Version(asdf.__version__) >= Version('2.8.0'):
+        # The auto_inline argument is deprecated as of asdf 2.8.0.
         with asdf.config_context() as config:
             config.array_inline_threshold = 64
             helpers.assert_roundtrip_tree({'table': t}, tmpdir, asdf_check_func=check)
     else:
-        # asdf 2.7
         helpers.assert_roundtrip_tree({'table': t}, tmpdir, asdf_check_func=check,
                                       write_options={'auto_inline': 64})
 
