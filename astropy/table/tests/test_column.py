12eedfd2eae35a52cf6a13d1cfb9b36669cbd403
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2021-07-09 13:50:43 -0400

    Directly set .info everywhere, not a specific attribute.
    Also set info in yet another place, relevant for slicing.

--- astropy/table/tests/test_column.py
+++ astropy/table/tests/test_column.py
@@ -922,7 +922,7 @@ def test_column_value_access():
 
 
 def test_masked_column_serialize_method_propagation():
-    mc = table.MaskedColumn([1., 2.], mask=[True, False])
+    mc = table.MaskedColumn([1., 2., 3.], mask=[True, False, True])
     assert mc.info.serialize_method['ecsv'] == 'null_value'
     mc.info.serialize_method['ecsv'] = 'data_mask'
     assert mc.info.serialize_method['ecsv'] == 'data_mask'
@@ -932,3 +932,5 @@ def test_masked_column_serialize_method_propagation():
     assert mc3.info.serialize_method['ecsv'] == 'data_mask'
     mc4 = mc.view(table.MaskedColumn)
     assert mc4.info.serialize_method['ecsv'] == 'data_mask'
+    mc5 = mc[1:]
+    assert mc5.info.serialize_method['ecsv'] == 'data_mask'
