6e7a2a684aaca285d3ae20831cae6df2c8829a15
Stuart Mumford <stuart@cadair.com>
2021-07-28 17:19:40 +0100

    add a test

--- astropy/nddata/nddata.py
+++ astropy/nddata/nddata.py
@@ -227,6 +227,7 @@ class NDData(NDDataBase):
         # Store the attributes
         self._data = data
         self.mask = mask
+        self._wcs = None
         self.wcs = wcs
         self.meta = meta  # TODO: Make this call the setter sometime
         self._unit = unit
