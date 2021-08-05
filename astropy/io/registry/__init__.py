9347e1764fc86dab1e502831ef8a5f80331af437
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2021-08-05 14:09:44 -0400

    rename default to compat
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/io/registry/__init__.py
+++ astropy/io/registry/__init__.py
@@ -3,9 +3,8 @@
 Unified I/O Registry.
 """
 
-from . import core, default
+from . import compat, core
+from .compat import *
 from .core import *
-from .default import *
-from .default import _identifiers, _readers, _writers
 
-__all__ = core.__all__ + default.__all__
+__all__ = core.__all__ + compat.__all__
