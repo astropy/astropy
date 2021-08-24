7e75499efe90c8beb0f7f9dd12814d7d41c1edd4
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2021-08-24 14:42:39 -0400

    move definitions to cosmology
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/cosmology/units.py
+++ astropy/cosmology/units.py
@@ -2,23 +2,56 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """Cosmological units and equivalencies.
-"""  # (needed for unit summary)
+"""  # (newline needed for unit summary)
+
+import astropy.units as u
+from astropy.units.utils import generate_unit_summary as _generate_unit_summary
 
 __all__ = ["littleh", "with_H0"]
 
-from astropy.units.utils import generate_unit_summary as _generate_unit_summary
+_ns = globals()
 
 ###############################################################################
 # Cosmological Units
-# isort: split
 
-from astropy.units.astrophys import littleh
+# This is not formally a unit, but is used in that way in many contexts, and
+# an appropriate equivalency is only possible if it's treated as a unit (see
+# https://arxiv.org/pdf/1308.4150.pdf for more)
+# Also note that h or h100 or h_100 would be a better name, but they either
+# conflict or have numbers in them, which is disallowed
+littleh = u.def_unit(['littleh'], namespace=_ns, prefixes=False,
+                     doc='Reduced/"dimensionless" Hubble constant',
+                     format={'latex': r'h_{100}'})
+
 
 ###############################################################################
 # Equivalencies
-# isort: split
 
-from astropy.units.equivalencies import with_H0
+def with_H0(H0=None):
+    """
+    Convert between quantities with little-h and the equivalent physical units.
+
+    Parameters
+    ----------
+    H0 : None or `~astropy.units.Quantity` ['frequency']
+        The value of the Hubble constant to assume. If a
+        `~astropy.units.Quantity`, will assume the quantity *is* ``H0``. If
+        `None` (default), use the ``H0`` attribute from
+        :mod:`~astropy.cosmology.default_cosmology`.
+
+    References
+    ----------
+    For an illuminating discussion on why you may or may not want to use
+    little-h at all, see https://arxiv.org/pdf/1308.4150.pdf
+    """
+    if H0 is None:
+        from .realizations import default_cosmology
+        H0 = default_cosmology.get().H0
+
+    h100_val_unit = u.Unit(100/(H0.to_value((u.km/u.s)/u.Mpc)) * littleh)
+
+    return u.Equivalency([(h100_val_unit, None)], "with_H0", kwargs={"H0": H0})
+
 
 # =============================================================================
 # DOCSTRING
@@ -26,4 +59,4 @@ from astropy.units.equivalencies import with_H0
 # This generates a docstring for this module that describes all of the
 # standard units defined here.
 if __doc__ is not None:
-    __doc__ += _generate_unit_summary(globals())
+    __doc__ += _generate_unit_summary(_ns)
--- docs/units/equivalencies.rst
+++ docs/units/equivalencies.rst
@@ -461,57 +461,6 @@ point of 3631.1 Jy::
 
 .. EXAMPLE END
 
-.. _H0-equivalency:
-
-Reduced Hubble Constant and "little-h" Equivalency
---------------------------------------------------
-
-The dimensionless version of the Hubble constant — often known as "little h" —
-is a frequently used quantity in extragalactic astrophysics. It is also widely
-known as the bane of beginners' existence in such fields (See e.g., the title
-of `this paper <https://doi.org/10.1017/pasa.2013.31>`__, which also provides
-valuable advice on the use of little h). ``astropy`` provides the
-:func:`~astropy.units.equivalencies.with_H0` equivalency that helps keep this
-straight in at least some of these cases, by providing a way to convert to/from
-physical to "little h" units.
-
-Examples
-^^^^^^^^
-
-.. EXAMPLE START: Using the "little h" Equivalency
-
-To convert to or from physical to "little h" units:
-
-    >>> H0_70 = 70 * u.km/u.s / u.Mpc
-    >>> distance = 70 * (u.Mpc/u.littleh)
-    >>> distance.to(u.Mpc, u.with_H0(H0_70))  # doctest: +FLOAT_CMP
-    <Quantity 100.0 Mpc>
-    >>> luminosity = 0.49 * u.Lsun * u.littleh**-2
-    >>> luminosity.to(u.Lsun, u.with_H0(H0_70))  # doctest: +FLOAT_CMP
-    <Quantity 1.0 solLum>
-
-Note the unit name ``littleh``: while this unit is usually expressed in the
-literature as just ``h``, here it is ``littleh`` to avoid confusion with
-"hours."
-
-If no argument is given (or the argument is `None`), this equivalency assumes
-the ``H0`` from the current default :ref:`cosmology <astropy-cosmology>`::
-
-    >>> distance = 100 * (u.Mpc/u.littleh)
-    >>> distance.to(u.Mpc, u.with_H0())  # doctest: +FLOAT_CMP
-    <Quantity 147.79781259 Mpc>
-
-This equivalency also allows a common magnitude formulation of little h
-scaling:
-
-    >>> mag_quantity = 12 * (u.mag - u.MagUnit(u.littleh**2))
-    >>> mag_quantity  # doctest: +FLOAT_CMP
-    <Magnitude 12. mag(1 / littleh2)>
-    >>> mag_quantity.to(u.mag, u.with_H0(H0_70))  # doctest: +FLOAT_CMP
-    <Quantity 11.2254902 mag>
-
-.. EXAMPLE END
-
 Temperature Equivalency
 -----------------------
 
