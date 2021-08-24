62ec3df36cb59f37dc98627743747799983b3035
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2021-08-24 16:15:50 -0400

    rename module to io
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/cosmology/io/__init__.py
+++ astropy/cosmology/io/__init__.py
@@ -6,4 +6,4 @@ Read/Write/Interchange methods for `astropy.cosmology`. **NOT public API**.
 """
 
 # Import the interchange to register them into the io registry.
-from astropy.cosmology._io import mapping  # noqa: F403
+from . import mapping  # noqa: F403
