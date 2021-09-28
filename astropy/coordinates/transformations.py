27390ad8d0a0a253c92544274f9a82b9df0229b5
Albert Y. Shih <ayshih@gmail.com>
2021-09-27 22:30:41 -0400

    Add a private function to CompositeTransform to return an encapsulated version

--- astropy/coordinates/transformations.py
+++ astropy/coordinates/transformations.py
@@ -1408,7 +1408,7 @@ class CompositeTransform(CoordinateTransform):
         curr_coord = fromcoord
         for t in self.transforms:
             # build an intermediate frame with attributes taken from either
-            # `fromframe`, or if not there, `toframe`, or if not there, use
+            # `toframe`, or if not there, `fromcoord`, or if not there, use
             # the defaults
             # TODO: caching this information when creating the transform may
             # speed things up a lot
@@ -1427,6 +1427,84 @@ class CompositeTransform(CoordinateTransform):
         # this is safe even in the case where self.transforms is empty, because
         # coordinate objects are immutible, so copying is not needed
         return curr_coord
+
+    def _as_single_transform(self):
+        """
+        Return an encapsulated version of the composite transform so that it appears to
+        be a single transform.
+
+        The returned transform internally calls the constituent transforms.  If all of
+        the transforms are affine, the merged transform is
+        `~astropy.coordinates.transformations.DynamicMatrixTransform` (if there are no
+        origin shifts) or `~astropy.coordinates.transformations.AffineTransform`
+        (otherwise).  If at least one of the transforms is not affine, the merged
+        transform is
+        `~astropy.coordinates.transformations.FunctionTransformWithFiniteDifference`.
+        """
+        # Create a list of the transforms including flattening any constituent CompositeTransform
+        transforms = [t if not isinstance(t, CompositeTransform) else t._as_single_transform()
+                      for t in self.transforms]
+
+        if all([isinstance(t, BaseAffineTransform) for t in transforms]):
+            # Check if there may be an origin shift
+            fixed_origin = all([isinstance(t, (StaticMatrixTransform, DynamicMatrixTransform))
+                                for t in transforms])
+
+            # Dynamically define the transformation function
+            def single_transform(from_coo, to_frame):
+                if from_coo.is_equivalent_frame(to_frame):  # loopback to the same frame
+                    return None if fixed_origin else (None, None)
+
+                # Create a merged attribute dictionary for any intermediate frames
+                # For any attributes shared by the "from"/"to" frames, the "to" frame takes
+                #   precedence because this is the same choice implemented in __call__()
+                merged_attr = {name: getattr(from_coo, name)
+                               for name in from_coo.frame_attributes}
+                merged_attr.update({name: getattr(to_frame, name)
+                                    for name in to_frame.frame_attributes})
+
+                affine_params = (None, None)
+                # Step through each transform step (frame A -> frame B)
+                for i, t in enumerate(transforms):
+                    # Extract the relevant attributes for frame A
+                    if i == 0:
+                        # If frame A is actually the initial frame, preserve its attributes
+                        a_attr = {name: getattr(from_coo, name)
+                                  for name in from_coo.frame_attributes}
+                    else:
+                        a_attr = {k: v for k, v in merged_attr.items()
+                                  if k in t.fromsys.frame_attributes}
+
+                    # Extract the relevant attributes for frame B
+                    b_attr = {k: v for k, v in merged_attr.items()
+                              if k in t.tosys.frame_attributes}
+
+                    # Obtain the affine parameters for the transform
+                    # Note that we insert some dummy data into frame A because the transformation
+                    #   machinery requires there to be data present.  Removing that limitation
+                    #   is a possible TODO, but some care would need to be taken because some affine
+                    #   transforms have branching code depending on the presence of differentials.
+                    next_affine_params = t._affine_params(t.fromsys(from_coo.data, **a_attr),
+                                                          t.tosys(**b_attr))
+
+                    # Combine the affine parameters with the running set
+                    affine_params = _combine_affine_params(affine_params, next_affine_params)
+
+                # If there is no origin shift, return only the matrix
+                return affine_params[0] if fixed_origin else affine_params
+
+            # The return type depends on whether there is any origin shift
+            transform_type = DynamicMatrixTransform if fixed_origin else AffineTransform
+        else:
+            # Dynamically define the transformation function
+            def single_transform(from_coo, to_frame):
+                if from_coo.is_equivalent_frame(to_frame):  # loopback to the same frame
+                    return to_frame.realize_frame(from_coo.data)
+                return self(from_coo, to_frame)
+
+            transform_type = FunctionTransformWithFiniteDifference
+
+        return transform_type(single_transform, self.fromsys, self.tosys, priority=self.priority)
 
 
 def _combine_affine_params(params, next_params):
