cfa72ef7724c813635cbeb44a7d94f52ba665f19
Eli Rykoff <erykoff@stanford.edu>
2021-09-29 11:38:04 -0700

    Add parquet serialization settings.

--- astropy/table/column.py
+++ astropy/table/column.py
@@ -1260,6 +1260,7 @@ class MaskedColumnInfo(ColumnInfo):
             self.serialize_method = {'fits': 'null_value',
                                      'ecsv': 'null_value',
                                      'hdf5': 'data_mask',
+                                     'parquet': 'data_mask',
                                      None: 'null_value'}
 
     def _represent_as_dict(self):
