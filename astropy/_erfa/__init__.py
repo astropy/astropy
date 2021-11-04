817127acd7a7cb6664f46fc6ba663b5f32ba9653
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2021-11-04 01:00:42 -0400

    isort top-level astropy
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/_erfa/__init__.py
+++ astropy/_erfa/__init__.py
@@ -1,14 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import warnings
 
-from erfa import core, ufunc, helpers  # noqa
+from erfa import core, helpers, ufunc  # noqa
 from erfa.core import *  # noqa
-from erfa.ufunc import (dt_eraASTROM, dt_eraLDBODY, dt_eraLEAPSECOND, dt_pv,  # noqa
-                        dt_sign, dt_type, dt_ymdf, dt_hmsf, dt_dmsf)
 from erfa.helpers import leap_seconds  # noqa
+from erfa.ufunc import (dt_dmsf, dt_eraASTROM, dt_eraLDBODY,  # noqa
+                        dt_eraLEAPSECOND, dt_hmsf, dt_pv, dt_sign, dt_type, dt_ymdf)
 
 from astropy.utils.exceptions import AstropyDeprecationWarning
-
 
 warnings.warn('The private astropy._erfa module has been made into its '
               'own package, pyerfa, which is a dependency of '
