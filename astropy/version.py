b68481fad02534f8d5c5ca29b2992c6b9120d9d0
Stuart Mumford <stuart@cadair.com>
2021-11-19 14:47:50 +0000

    remove use of distutils from version.py

--- astropy/version.py
+++ astropy/version.py
@@ -16,24 +16,8 @@ except Exception:
     version = '0.0.0'
 
 
-# We use LooseVersion to define major, minor, micro, but ignore any suffixes.
-def split_version(version):
-    pieces = [0, 0, 0]
+from packaging.version import parse as _parse
 
-    try:
-        from distutils.version import LooseVersion
-
-        for j, piece in enumerate(LooseVersion(version).version[:3]):
-            pieces[j] = int(piece)
-
-    except Exception:
-        pass
-
-    return pieces
-
-
-major, minor, bugfix = split_version(version)
-
-del split_version  # clean up namespace.
-
-release = 'dev' not in version
+_version = _parse(version)
+major, minor, bugfix = [*_version.release, 0][:3]
+release = not _version.is_devrelease
