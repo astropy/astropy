ddff8565972351be954a2635e850239095cd3e56
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2022-01-28 12:07:55 -0500

    Add comments to Cosmology JSON
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/cosmology/io/json.py
+++ astropy/cosmology/io/json.py
@@ -9,20 +9,20 @@ from astropy.io.misc.json import _json_base_encode, JSONExtendedEncoder, JSONExt
 
 
 def encode_cosmology(cosmo):
-    code = _json_base_encode(cosmo)
+    code = _json_base_encode(cosmo)  # get type code for decoding
 
-    map = cosmo.to_format("mapping")
-    map.pop("cosmology")
-    meta = map.pop("meta")
+    map = cosmo.to_format("mapping")  # dictionary representation
+    map.pop("cosmology")  # extraneous given `_json_base_encode`
+    meta = map.pop("meta")  # going to move to sub-dict of `code`
 
-    code["value"] = map
-    code["meta"] = meta
+    code["value"] = map  # parameters + name
+    code["meta"] = meta  # everything else
 
     return code
 
 
 def decode_cosmology(cosmo_cls, parameters, meta):
-    map = {**parameters, "meta": meta}
+    map = {**parameters, "meta": meta}  # mix back to one dict
     return Cosmology.from_format(map, format="mapping", cosmology=cosmo_cls)
 
 
