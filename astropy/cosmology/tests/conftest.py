efd35363f8fef1ab79d294825c058ffc9d37996c
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2022-03-18 14:07:01 -0400

    refactor conftest as tests helper
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

---
+++ astropy/cosmology/tests/conftest.py
@@ -0,0 +1,6 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""Configure the tests for :mod:`astropy.cosmology`."""
+
+from astropy.cosmology.tests.helper import clean_registry
+from astropy.tests.helper import pickle_protocol
