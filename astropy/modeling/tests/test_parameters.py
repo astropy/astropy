86f02294b515defd228dddfd61b692b8afb87e93
William Jamieson <wjamieson@stsci.edu>
2022-04-26 10:47:01 -0400

    Address comments from pllim

--- astropy/modeling/tests/test_parameters.py
+++ astropy/modeling/tests/test_parameters.py
@@ -460,11 +460,10 @@ class TestParameters:
         assert param.unit == u.m
 
         # Force magnitude unit (mag=False)
-        with pytest.raises(ValueError) as err:
+        with pytest.raises(ValueError,
+                           match=r"This parameter does not support the magnitude units such as .*"):
             param._set_unit(u.ABmag, True)
-        assert str(err.value) == (
-            "This parameter does not support the magnitude units such as mag(AB)"
-        )
+
         # Force magnitude unit (mag=True)
         param._mag = True
         param._set_unit(u.ABmag, True)
