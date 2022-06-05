8e8d60ab0fe89a50ed9778b3f96aebd644171e41
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2022-06-05 11:11:09 -0400

    Test stacking of tables with structured columns.

--- astropy/table/tests/test_operations.py
+++ astropy/table/tests/test_operations.py
@@ -789,6 +789,21 @@ class TestJoin():
         with pytest.raises(ValueError, match=msg):
             table.join(t1, t2, keys_left=['a'], keys_right=['a'], join_funcs={})
 
+    def test_join_structured_column(self):
+        """Regression tests for gh-13271."""
+        # Two tables with matching names, including a structured column.
+        t1 = Table([np.array([(1., 1), (2., 2)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['one', 'two']], names=['structured', 'string'])
+        t2 = Table([np.array([(2., 2), (4., 4)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['three', 'four']], names=['structured', 'string'])
+        t12 = table.join(t1, t2, ['structured'], join_type='outer')
+        assert t12.pformat() == [
+            'structured [f, i] string_1 string_2',
+            '----------------- -------- --------',
+            '          (1., 1)      one       --',
+            '          (2., 2)      two    three',
+            '          (4., 4)       --     four']
+
 
 class TestSetdiff():
 
@@ -1260,6 +1275,33 @@ class TestVStack():
         with pytest.raises(ValueError, match='representations are inconsistent'):
             table.vstack([t1, t3])
 
+    def test_vstack_structured_column(self):
+        """Regression tests for gh-13271."""
+        # Two tables with matching names, including a structured column.
+        t1 = Table([np.array([(1., 1), (2., 2)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['one', 'two']], names=['structured', 'string'])
+        t2 = Table([np.array([(3., 3), (4., 4)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['three', 'four']], names=['structured', 'string'])
+        t12 = table.vstack([t1, t2])
+        assert t12.pformat() == [
+            'structured [f, i] string',
+            '----------------- ------',
+            '          (1., 1)    one',
+            '          (2., 2)    two',
+            '          (3., 3)  three',
+            '          (4., 4)   four']
+
+        # One table without the structured column.
+        t3 = t2[('string',)]
+        t13 = table.vstack([t1, t3])
+        assert t13.pformat() == [
+            'structured [f, i] string',
+            '----------------- ------',
+            '         (1.0, 1)    one',
+            '         (2.0, 2)    two',
+            '               --  three',
+            '               --   four']
+
 
 class TestDStack():
 
@@ -1399,6 +1441,29 @@ class TestDStack():
         t12 = table.dstack([t1, t2])
         assert skycoord_equal(sc1, t12['col0'][:, 0])
         assert skycoord_equal(sc2, t12['col0'][:, 1])
+
+    def test_dstack_structured_column(self):
+        """Regression tests for gh-13271."""
+        # Two tables with matching names, including a structured column.
+        t1 = Table([np.array([(1., 1), (2., 2)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['one', 'two']], names=['structured', 'string'])
+        t2 = Table([np.array([(3., 3), (4., 4)], dtype=[('f', 'f8'), ('i', 'i8')]),
+                    ['three', 'four']], names=['structured', 'string'])
+        t12 = table.dstack([t1, t2])
+        assert t12.pformat() == [
+            'structured [f, i]     string   ',
+            '------------------ ------------',
+            '(1., 1) .. (3., 3) one .. three',
+            '(2., 2) .. (4., 4)  two .. four']
+
+        # One table without the structured column.
+        t3 = t2[('string',)]
+        t13 = table.dstack([t1, t3])
+        assert t13.pformat() == [
+            'structured [f, i]    string   ',
+            '----------------- ------------',
+            '   (1.0, 1) .. -- one .. three',
+            '   (2.0, 2) .. --  two .. four']
 
 
 class TestHStack():
