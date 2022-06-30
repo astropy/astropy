b2ee06b8cb2057902ceda3c8754cc13be2b7f299
Chiara Marmo <marmochiaskl@gmail.com>
2022-06-30 10:19:36 -1000

    Read multi-dimensional variable length arrays. Add test.
    Add changelog.

--- astropy/io/fits/fitsrec.py
+++ astropy/io/fits/fitsrec.py
@@ -814,6 +814,8 @@ class FITS_rec(np.recarray):
         to a VLA column with the array data returned from the heap.
         """
 
+        if column.dim:
+            vla_shape = tuple(map(int, column.dim.strip("()").split(",")))
         dummy = _VLF([None] * len(self), dtype=recformat.dtype)
         raw_data = self._get_raw_data()
 
@@ -837,6 +839,10 @@ class FITS_rec(np.recarray):
                 dt = np.dtype(recformat.dtype)
                 arr_len = count * dt.itemsize
                 dummy[idx] = raw_data[offset : offset + arr_len].view(dt)
+                if column.dim and len(vla_shape) > 1:
+                    vla_dimx = vla_shape[0]
+                    vla_dimy = int(len(dummy[idx]) / vla_shape[0])
+                    dummy[idx] = dummy[idx].reshape(vla_dimx, vla_dimy)
                 dummy[idx].dtype = dummy[idx].dtype.newbyteorder(">")
                 # Each array in the field may now require additional
                 # scaling depending on the other scaling parameters
@@ -952,7 +958,7 @@ class FITS_rec(np.recarray):
                     actual_nitems = 1
                 else:
                     actual_nitems = field.shape[1]
-                if nitems > actual_nitems:
+                if nitems > actual_nitems and not isinstance(recformat, _FormatP):
                     warnings.warn(
                         "TDIM{} value {:d} does not fit with the size of "
                         "the array items ({:d}).  TDIM{:d} will be ignored.".format(
@@ -1021,7 +1027,7 @@ class FITS_rec(np.recarray):
                 with suppress(UnicodeDecodeError):
                     field = decode_ascii(field)
 
-        if dim:
+        if dim and not isinstance(recformat, _FormatP):
             # Apply the new field item dimensions
             nitems = reduce(operator.mul, dim)
             if field.ndim > 1:
