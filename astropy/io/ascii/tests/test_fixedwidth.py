c91a3915b2e91b668a02d85016b86260c63df033
Tom Aldcroft <taldcroft@gmail.com>
2022-09-21 08:53:53 -0400

    Add tests

--- astropy/io/ascii/tests/test_fixedwidth.py
+++ astropy/io/ascii/tests/test_fixedwidth.py
@@ -498,3 +498,46 @@ AAA y z
     assert np.all(dat['a'] == [1, 4])
     assert np.all(dat['b'] == [2, 5])
     assert np.all(dat['c'] == [3, 6])
+
+
+def test_fixed_width_header_rows():
+    tbl = [
+        '| int16 | float32 |      <U3 | int64 |',
+        '|     a |       b |        c |     d |',
+        '|     m |         |          | m / s |',
+        '|       |     .2f |          |       |',
+        '|       |         | C column |       |',
+        '|     1 |    1.00 |        c |     4 |',
+        '|     2 |    2.00 |        d |     5 |',
+        '|     3 |    3.00 |        e |     6 |'
+    ]
+    header_rows = ["dtype", "name", "unit", "format", "description"]
+    dat = ascii.read(tbl, format='fixed_width', delimiter='|', header_rows=header_rows)
+    out = StringIO()
+    ascii.write(dat, out, format='fixed_width', delimiter='|', header_rows=header_rows)
+    assert out.getvalue().splitlines() == tbl
+
+
+def test_fixed_width_two_line_header_rows():
+    tbl = [
+        'int32 float32      <U2 int64',
+        '    m                  m / s',
+        '          .2f               ',
+        '              C column      ',
+        '    a       b        c     d',
+        '----- ------- -------- -----',
+        '    1    1.00        c     4',
+        '    2    2.00        d     5',
+        '    3    3.00        e     6'
+    ]
+    header_rows = ["dtype", "unit", "format", "description", "name"]
+    dat = ascii.read(tbl, format='fixed_width_two_line', header_rows=header_rows)
+    out = StringIO()
+    ascii.write(dat, out, format='fixed_width_two_line', header_rows=header_rows)
+    assert out.getvalue().splitlines() == tbl
+
+
+def test_fixed_width_no_header_header_rows():
+    tbl = ['    1    1.00        c     4']
+    with pytest.raises(TypeError, match=r"unexpected keyword argument 'header_rows'"):
+        ascii.read(tbl, format='fixed_width_no_header', header_rows=["unit"])
