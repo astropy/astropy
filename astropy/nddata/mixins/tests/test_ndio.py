b8771e831cbbcd0b995e1b8e2a6cf56b2a15ff80
Jero Bado <programmer@jerobado.com>
2022-10-07 20:46:27 +0800

    Replace `tmpdir` with `tmp_path` in `nddata` tests

--- astropy/nddata/mixins/tests/test_ndio.py
+++ astropy/nddata/mixins/tests/test_ndio.py
@@ -4,7 +4,7 @@ from astropy.nddata import NDData, NDDataRef, NDIOMixin
 NDDataIO = NDDataRef
 
 
-def test_simple_write_read(tmpdir):
+def test_simple_write_read():
     ndd = NDDataIO([1, 2, 3])
     assert hasattr(ndd, 'read')
     assert hasattr(ndd, 'write')
