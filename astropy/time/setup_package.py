647d730dbfd7c45df2b92d6de518385976c6e71f
William Jamieson <william.b.jamieson@nasa.gov>
2022-11-09 11:28:30 -0700

    [black] Allow `black` to run on `astropy.time`
    [black] Changes made by black
    
    [black] Apply black --preview
    
    [black] Normalize strings

--- astropy/time/setup_package.py
+++ astropy/time/setup_package.py
@@ -9,16 +9,19 @@ from setuptools import Extension
 
 C_TIME_PKGDIR = os.path.relpath(os.path.dirname(__file__))
 
-SRC_FILES = [os.path.join(C_TIME_PKGDIR, filename)
-             for filename in ['src/parse_times.c']]
+SRC_FILES = [
+    os.path.join(C_TIME_PKGDIR, filename) for filename in ["src/parse_times.c"]
+]
 
 
 def get_extensions():
     # Add '-Rpass-missed=.*' to ``extra_compile_args`` when compiling with clang
     # to report missed optimizations
-    _time_ext = Extension(name='astropy.time._parse_times',
-                          sources=SRC_FILES,
-                          include_dirs=[numpy.get_include()],
-                          language='c')
+    _time_ext = Extension(
+        name="astropy.time._parse_times",
+        sources=SRC_FILES,
+        include_dirs=[numpy.get_include()],
+        language="c",
+    )
 
     return [_time_ext]
--- astropy/time/tests/test_pickle.py
+++ astropy/time/tests/test_pickle.py
@@ -11,16 +11,15 @@ class TestPickle:
     """Basic pickle test of time"""
 
     def test_pickle(self):
-
-        times = ['1999-01-01 00:00:00.123456789', '2010-01-01 00:00:00']
-        t1 = Time(times, scale='utc')
+        times = ["1999-01-01 00:00:00.123456789", "2010-01-01 00:00:00"]
+        t1 = Time(times, scale="utc")
 
         for prot in range(pickle.HIGHEST_PROTOCOL):
             t1d = pickle.dumps(t1, prot)
             t1l = pickle.loads(t1d)
             assert np.all(t1l == t1)
 
-        t2 = Time('2012-06-30 12:00:00', scale='utc')
+        t2 = Time("2012-06-30 12:00:00", scale="utc")
 
         for prot in range(pickle.HIGHEST_PROTOCOL):
             t2d = pickle.dumps(t2, prot)
