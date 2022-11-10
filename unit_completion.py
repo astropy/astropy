df266fe9cf14185741fdf9ca23410daf1a2acb5f
Meng Dan <17638547327@163.com>
2022-11-10 18:39:33 +0800

    add newline

--- unit_completion.py
+++ unit_completion.py
@@ -2,7 +2,7 @@ from astropy import units as u
 
 with open("astropy/units/autocompletion.py", "w") as f:
     f.write("from .core import UnitBase\n")
-    f.write("from .function import FunctionUnitBase")
+    f.write("from .function import FunctionUnitBase\n")
     for attr in dir(u):
         value = getattr(u, attr)
         if isinstance(value, u.UnitBase):
