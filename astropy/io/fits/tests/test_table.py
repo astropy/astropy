dfe3d2a4703a55bda5f90f499687f726e9804243
Simon Conseil <contact@saimon.org>
2022-12-16 19:29:16 +0100

    Backport PR #13417: Fix reading of MBFITS tables with variable length array

--- astropy/io/fits/tests/test_table.py
+++ astropy/io/fits/tests/test_table.py
@@ -3251,6 +3251,72 @@ class TestVLATables(FitsTestCase):
                 [np.array([], dtype=np.int32), np.array([], dtype=np.int32)],
             )
 
+    def test_multidim_VLA_tables(self):
+        """
+        Check if multidimensional VLF are correctly write and read.
+        See https://github.com/astropy/astropy/issues/12860
+        and https://github.com/astropy/astropy/issues/7810
+        """
+        a = np.arange(5).reshape((5, 1))
+        b = np.arange(7).reshape((7, 1))
+        array = np.array([a, b], dtype=object)
+        col = fits.Column(name="test", format="PD(7)", dim="(1,7)", array=array)
+        fits.BinTableHDU.from_columns([col]).writeto(self.temp("test.fits"))
+
+        with fits.open(self.temp("test.fits")) as hdus:
+            assert hdus[1].columns.formats == ["PD(7)"]
+            np.array_equal(
+                hdus[1].data["test"],
+                [
+                    np.array([[0.0, 1.0, 2.0, 3.0, 4.0]]),
+                    np.array([[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0]]),
+                ],
+            )
+
+        a = np.arange(10).reshape((5, 2))
+        b = np.arange(14).reshape((7, 2))
+        array = np.array([a, b], dtype=object)
+        col = fits.Column(name="test", format="PD(14)", dim="(2,7)", array=array)
+        fits.BinTableHDU.from_columns([col]).writeto(self.temp("test2.fits"))
+
+        with fits.open(self.temp("test2.fits")) as hdus:
+            assert hdus[1].columns.formats == ["PD(14)"]
+            np.array_equal(
+                hdus[1].data["test"],
+                [
+                    np.array(
+                        [[0.0, 1.0], [2.0, 3.0], [4.0, 5.0], [6.0, 7.0], [8.0, 9.0]]
+                    ),
+                    np.array(
+                        [
+                            [0.0, 1.0],
+                            [2.0, 3.0],
+                            [4.0, 5.0],
+                            [6.0, 7.0],
+                            [8.0, 9.0],
+                            [10.0, 11.0],
+                            [12.0, 13.0],
+                        ]
+                    ),
+                ],
+            )
+
+        a = np.arange(3).reshape((1, 3))
+        b = np.arange(6).reshape((2, 3))
+        array = np.array([a, b], dtype=object)
+        col = fits.Column(name="test", format="PD(6)", dim="(3,2)", array=array)
+        fits.BinTableHDU.from_columns([col]).writeto(self.temp("test3.fits"))
+
+        with fits.open(self.temp("test3.fits")) as hdus:
+            assert hdus[1].columns.formats == ["PD(6)"]
+            np.array_equal(
+                hdus[1].data["test"],
+                [
+                    np.array([[0.0, 1.0, 2.0]]),
+                    np.array([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0]]),
+                ],
+            )
+
 
 # These are tests that solely test the Column and ColDefs interfaces and
 # related functionality without directly involving full tables; currently there
