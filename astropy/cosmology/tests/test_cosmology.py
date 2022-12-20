e89e167aa494ab4601fd8d5371f397cf49dbd910
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2022-12-20 16:37:55 -0500

    move test_age_in_special_cosmologies
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/cosmology/tests/test_cosmology.py
+++ astropy/cosmology/tests/test_cosmology.py
@@ -195,25 +195,6 @@ def test_efunc_vs_invefunc_flrw():
 
 
 @pytest.mark.skipif(not HAS_SCIPY, reason="test requires scipy")
-def test_age_in_special_cosmologies():
-    """Check that age in de Sitter and Einstein-de Sitter Universes work.
-
-    Some analytic solutions fail at these critical points.
-    """
-    c_dS = flrw.FlatLambdaCDM(100, 0, Tcmb0=0)
-    assert allclose(c_dS.age(z=0), np.inf * u.Gyr)
-    assert allclose(c_dS.age(z=1), np.inf * u.Gyr)
-    assert allclose(c_dS.lookback_time(z=0), 0 * u.Gyr)
-    assert allclose(c_dS.lookback_time(z=1), 6.777539216261741 * u.Gyr)
-
-    c_EdS = flrw.FlatLambdaCDM(100, 1, Tcmb0=0)
-    assert allclose(c_EdS.age(z=0), 6.518614811154189 * u.Gyr)
-    assert allclose(c_EdS.age(z=1), 2.3046783684542738 * u.Gyr)
-    assert allclose(c_EdS.lookback_time(z=0), 0 * u.Gyr)
-    assert allclose(c_EdS.lookback_time(z=1), 4.213936442699092 * u.Gyr)
-
-
-@pytest.mark.skipif(not HAS_SCIPY, reason="test requires scipy")
 def test_distance_in_special_cosmologies():
     """Check that de Sitter and Einstein-de Sitter Universes both work.
 
