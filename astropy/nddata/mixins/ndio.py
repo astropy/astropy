03aa40067a487bebc9fe75550a5a71ea133f35e6
Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>
2023-01-18 16:31:21 -0500

    fix D406 - NewLineAfterSectionName
    `ruff` will guard against violations from now on.
    
    This commit includes manual corrections for non-sections that looked like section names.
    
    Signed-off-by: Nathaniel Starkman (@nstarman) <nstarkman@protonmail.com>

--- astropy/nddata/mixins/ndio.py
+++ astropy/nddata/mixins/ndio.py
@@ -25,7 +25,7 @@ class NDDataRead(registry.UnifiedReadWrite):
       >>> CCDData.read.help('fits')  # Get detailed help on CCDData FITS reader
       >>> CCDData.read.list_formats()  # Print list of available formats
 
-    See Also:
+    For more information see:
 
     - https://docs.astropy.org/en/stable/nddata
     - https://docs.astropy.org/en/stable/io/unified.html
@@ -76,7 +76,7 @@ class NDDataWrite(registry.UnifiedReadWrite):
       >>> CCDData.write.help('fits')  # Get detailed help on CCDData FITS writer
       >>> CCDData.write.list_formats()  # Print list of available formats
 
-    See Also:
+    See Also
 
     - https://docs.astropy.org/en/stable/nddata
     - https://docs.astropy.org/en/stable/io/unified.html
