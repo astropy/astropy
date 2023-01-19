98caeaa56d3b61deff0391c5d29c090d81a10d1f
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-01-19 14:52:51 +0000

    Adjusted setup_package.py to recognize ASTROPY_USE_SYSTEM_*

--- astropy/io/fits/_tiled_compression/setup_package.py
+++ astropy/io/fits/_tiled_compression/setup_package.py
@@ -1,25 +1,37 @@
 # Licensed under a 3-clause BSD style license
 
 import os
+from collections import defaultdict
 
 from setuptools import Extension
+
+from extension_helpers import pkg_config
 
 SRC_DIR = os.path.join(os.path.dirname(__file__), "src")
 
 
 def get_extensions():
-    return [
-        Extension(
-            "astropy.io.fits._tiled_compression._compression",
-            sources=[
-                os.path.join(SRC_DIR, "compression.c"),
+
+    cfg = defaultdict(list)
+    cfg["sources"].append(os.path.join(SRC_DIR, "compression.c"))
+
+    if int(os.environ.get("ASTROPY_USE_SYSTEM_CFITSIO", 0)) or int(
+        os.environ.get("ASTROPY_USE_SYSTEM_ALL", 0)
+    ):
+        for k, v in pkg_config(["cfitsio"], ["cfitsio"]).items():
+            cfg[k].extend(v)
+    else:
+        cfg["sources"].extend(
+            [
                 os.path.join("cextern", "cfitsio", "lib", "pliocomp.c"),
                 os.path.join("cextern", "cfitsio", "lib", "ricecomp.c"),
                 os.path.join("cextern", "cfitsio", "lib", "fits_hcompress.c"),
                 os.path.join("cextern", "cfitsio", "lib", "fits_hdecompress.c"),
                 os.path.join("cextern", "cfitsio", "lib", "quantize.c"),
                 os.path.join("cextern", "cfitsio", "lib", "imcompress.c"),
-            ],
-            include_dirs=[SRC_DIR],
+            ]
         )
-    ]
+
+    cfg["include_dirs"].append(SRC_DIR)
+
+    return [Extension("astropy.io.fits._tiled_compression._compression", **cfg)]
