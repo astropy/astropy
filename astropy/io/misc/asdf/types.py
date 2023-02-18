4e84f010bc23deafcdbb814c2db6c270aed36074
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-02-18 11:52:16 -0500

    Add missing import

--- astropy/io/misc/asdf/types.py
+++ astropy/io/misc/asdf/types.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 # -*- coding: utf-8 -*-
+import warnings
 
 from asdf.exceptions import AsdfDeprecationWarning
 from asdf.types import CustomType, ExtensionTypeMeta
