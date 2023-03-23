8a834ac81774d1eebbf810befe6999d6ceea63b9
Marten van Kerkwijk <mhvk@astro.utoronto.ca>
2023-03-23 16:46:14 -0400

    Support masked where in ufuncs

--- astropy/utils/masked/tests/test_functions.py
+++ astropy/utils/masked/tests/test_functions.py
@@ -11,6 +11,7 @@ from numpy.testing import assert_array_equal
 
 from astropy import units as u
 from astropy.units import Quantity
+from astropy.utils.compat.numpycompat import NUMPY_LT_1_25
 from astropy.utils.masked.core import Masked
 
 from .test_masked import (
@@ -44,6 +45,57 @@ class MaskedUfuncTests(MaskedArraySetup):
         assert result is out
         assert_masked_equal(result, ma_mb)
 
+    @pytest.mark.parametrize("base_mask", [True, False])
+    def test_ufunc_inplace_where(self, base_mask):
+        # Construct base filled with -9 and base_mask (copying to get unit/class).
+        base = self.ma.copy()
+        base.unmasked.view(np.ndarray)[...] = -9.0
+        base._mask[...] = base_mask
+        out = base.copy()
+        where = np.array([[True, False, False], [False, True, False]])
+        result = np.add(self.ma, self.mb, out=out, where=where)
+        # Direct checks.
+        assert np.all(result.unmasked[~where] == base.unmasked[0, 0])
+        assert np.all(result.unmasked[where] == (self.a + self.b)[where])
+        # Full comparison.
+        expected = base.unmasked.copy()
+        np.add(self.a, self.b, out=expected, where=where)
+        expected_mask = base.mask.copy()
+        np.logical_or(self.mask_a, self.mask_b, out=expected_mask, where=where)
+        assert_array_equal(result.unmasked, expected)
+        assert_array_equal(result.mask, expected_mask)
+
+    @pytest.mark.parametrize("base_mask", [True, False])
+    def test_ufunc_inplace_masked_where(self, base_mask):
+        base = self.ma.copy()
+        base.unmasked.view(np.ndarray)[...] = -9.0
+        base._mask[...] = base_mask
+        out = base.copy()
+        where = Masked(
+            [[True, False, True], [False, False, True]],
+            mask=[[True, False, False], [True, False, True]],
+        )
+        result = np.add(self.ma, self.mb, out=out, where=where)
+        # Direct checks.
+        assert np.all(result.unmasked[~where.unmasked] == base.unmasked[0, 0])
+        assert np.all(
+            result.unmasked[where.unmasked] == (self.a + self.b)[where.unmasked]
+        )
+        assert np.all(result.mask[where.mask])
+        assert np.all(result.mask[~where.mask & ~where.unmasked] == base.mask[0, 0])
+        assert np.all(
+            result.mask[~where.mask & where.unmasked]
+            == (self.mask_a | self.mask_b)[~where.mask & where.unmasked]
+        )
+        # Full comparison.
+        expected = base.unmasked.copy()
+        np.add(self.a, self.b, out=expected, where=where.unmasked)
+        expected_mask = base.mask.copy()
+        np.logical_or(self.mask_a, self.mask_b, out=expected_mask, where=where.unmasked)
+        expected_mask |= where.mask
+        assert_array_equal(result.unmasked, expected)
+        assert_array_equal(result.mask, expected_mask)
+
     def test_ufunc_inplace_no_masked_input(self):
         a_b = np.add(self.a, self.b)
         out = Masked(np.zeros_like(a_b))
@@ -53,9 +105,18 @@ class MaskedUfuncTests(MaskedArraySetup):
         assert_array_equal(result.mask, np.zeros(a_b.shape, bool))
 
     def test_ufunc_inplace_error(self):
+        # Output is not masked.
         out = np.zeros(self.ma.shape)
         with pytest.raises(TypeError):
             np.add(self.ma, self.mb, out=out)
+
+    @pytest.mark.xfail(NUMPY_LT_1_25, reason="masked where not supported in numpy<1.25")
+    def test_ufunc_inplace_error_masked_where(self):
+        # Input and output are not masked, but where is.
+        # Note: prior to numpy 1.25, we cannot control this.
+        out = self.a.copy()
+        with pytest.raises(TypeError):
+            np.add(self.a, self.b, out=out, where=Masked(True, mask=True))
 
     @pytest.mark.parametrize("ufunc", (np.add.outer, np.minimum.outer))
     def test_2op_ufunc_outer(self, ufunc):
