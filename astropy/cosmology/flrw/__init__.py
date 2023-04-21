544c68082eb118766b734cffcf706e56135ae543
P. L. Lim <2090236+pllim@users.noreply.github.com>
2023-04-21 16:54:50 -0400

    MNT: Remove deprecated imports from flrw
    [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci

--- astropy/cosmology/flrw/__init__.py
+++ astropy/cosmology/flrw/__init__.py
@@ -17,38 +17,3 @@ __all__ = (
     + wpwazpcdm.__all__
     + w0wzcdm.__all__
 )
-
-
-def __getattr__(attr):
-    """Lazy import deprecated private API."""
-    base_attrs = (
-        "H0units_to_invs",
-        "a_B_c2",
-        "critdens_const",
-        "kB_evK",
-        "radian_in_arcmin",
-        "radian_in_arcsec",
-        "sec_to_Gyr",
-    )
-
-    if attr in base_attrs + ("quad",) + ("ellipkinc", "hyp2f1"):
-        import warnings
-
-        from astropy.utils.exceptions import AstropyDeprecationWarning
-
-        from . import base, lambdacdm
-
-        msg = (
-            f"`astropy.cosmology.flrw.{attr}` is a private variable (since "
-            "v5.1) and in future will raise an exception."
-        )
-        warnings.warn(msg, AstropyDeprecationWarning)
-
-        if attr in base_attrs:
-            return getattr(base, "_" + attr)
-        elif attr == "quad":
-            return getattr(base, attr)
-        elif attr in ("ellipkinc", "hyp2f1"):
-            return getattr(lambdacdm, attr)
-
-    raise AttributeError(f"module {__name__!r} has no attribute {attr!r}.")
