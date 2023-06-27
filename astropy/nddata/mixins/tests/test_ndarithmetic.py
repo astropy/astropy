fdb7c2593ff02243c8ac85419803e8c751dc0d64
Brett M. Morris <bmmorris@stsci.edu>
2023-06-27 15:33:50 -0400

    bugfix for bitmasks passed to nddata

--- astropy/nddata/mixins/tests/test_ndarithmetic.py
+++ astropy/nddata/mixins/tests/test_ndarithmetic.py
@@ -1310,3 +1310,42 @@ def test_raise_method_not_supported():
     # raise error for unsupported propagation operations:
     with pytest.raises(ValueError):
         ndd1.uncertainty.propagate(np.mod, ndd2, result, correlation)
+
+
+def test_nddata_bitmask_arithmetic():
+    # NDData.mask is usually assumed to be boolean, but could be
+    # a bitmask. Ensure bitmask works:
+    array = np.array([[0, 1, 0], [1, 0, 1], [0, 1, 0]])
+    mask = np.array([[0, 1, 64], [8, 0, 1], [2, 1, 0]])
+
+    nref_nomask = NDDataRef(array)
+    nref_masked = NDDataRef(array, mask=mask)
+
+    # multiply no mask by constant (no mask * no mask)
+    assert nref_nomask.multiply(1.0, handle_mask=np.bitwise_or).mask is None
+
+    # multiply no mask by itself (no mask * no mask)
+    assert nref_nomask.multiply(nref_nomask, handle_mask=np.bitwise_or).mask is None
+
+    # multiply masked by constant (mask * no mask)
+    np.testing.assert_equal(
+        nref_masked.multiply(1.0, handle_mask=np.bitwise_or).mask, mask
+    )
+
+    # multiply masked by itself (mask * mask)
+    np.testing.assert_equal(
+        nref_masked.multiply(nref_masked, handle_mask=np.bitwise_or).mask, mask
+    )
+
+    # multiply masked by no mask (mask * no mask)
+    np.testing.assert_equal(
+        nref_masked.multiply(nref_nomask, handle_mask=np.bitwise_or).mask, mask
+    )
+
+    # check bitwise logic still works
+    other_mask = np.array([[64, 1, 0], [2, 1, 0], [8, 0, 2]])
+    nref_mask_other = NDDataRef(array, mask=other_mask)
+    np.testing.assert_equal(
+        nref_mask_other.multiply(nref_masked, handle_mask=np.bitwise_or).mask,
+        np.bitwise_or(mask, other_mask),
+    )
