9b47e50a0a94524a41ddd90e20462d534e3e704d
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-07-10 12:10:50 +0100

    Fix code style

--- .github/workflows/update_astropy_iers_data_pin.py
+++ .github/workflows/update_astropy_iers_data_pin.py
@@ -1,27 +1,30 @@
 import sys
+
 import requests
 
-metadata = requests.get('https://pypi.org/pypi/astropy-iers-data/json').json()
+metadata = requests.get(
+    "https://pypi.org/pypi/astropy-iers-data/json", timeout=10
+).json()
 
-last_version = metadata['info']['version']
+last_version = metadata["info"]["version"]
 
-with open('setup.cfg') as f:
+with open("setup.cfg") as f:
     lines = f.readlines()
 
 changed_lines = 0
 
 for iline in range(len(lines)):
-    if 'astropy-iers-data' in lines[iline]:
+    if "astropy-iers-data" in lines[iline]:
         changed_lines += 1
-        lines[iline] = f'    astropy-iers-data>={last_version}\n'
+        lines[iline] = f"    astropy-iers-data>={last_version}\n"
 
 if changed_lines == 0:
-    print('No line found containing astropy-iers-data in setup.cfg')
+    print("No line found containing astropy-iers-data in setup.cfg")
     sys.exit(1)
 elif changed_lines > 1:
-    print('More than one line found containing astropy-iers-data in setup.cfg')
+    print("More than one line found containing astropy-iers-data in setup.cfg")
     sys.exit(1)
 
 
-with open('setup.cfg', 'w') as f:
+with open("setup.cfg", "w") as f:
     f.writelines(lines)
