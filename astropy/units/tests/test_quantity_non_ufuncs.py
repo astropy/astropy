Small flake8/ruff fixes for units

astropy/units/tests/test_quantity_non_ufuncs.py
@@ -2039,12 +2039,8 @@ class TestFFT(InvariantUnitTestSetup):
 class TestLinAlg(metaclass=CoverageMeta):
     def setup_method(self):
         self.q = (
-            np.array(
-                [[ 1.0, -1.0,  2.0],
-                 [ 0.0,  3.0, -1.0],
-                 [-1.0, -1.0,  1.0]]
-            ) << u.m
-        )  # fmt: skip
+            np.array([[1.0, -1.0, 2.0], [0.0, 3.0, -1.0], [-1.0, -1.0, 1.0]]) << u.m
+        )
 
     def test_cond(self):
         c = np.linalg.cond(self.q)
@@ -2353,11 +2349,10 @@ class TestRecFunctions(metaclass=CoverageMeta):
         assert np.array_equal(arr, self.q_pv)
         assert arr.unit == (u.km, u.km / u.s)
 
-    @pytest.mark.xfail
     @pytest.mark.parametrize("flatten", [True, False])
     def test_merge_arrays_nonquantities(self, flatten):
         # Fails because cannot create quantity from structured array.
-        arr = rfn.merge_arrays((q_pv["p"], q_pv.value), flatten=flatten)
+        arr = rfn.merge_arrays((self.q_pv["p"], self.q_pv.value), flatten=flatten)
 
     def test_merge_array_nested_structure(self):
         # Merge 2-element tuples without flattening.