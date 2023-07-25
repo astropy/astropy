dcce78af4073f8e356fcddc848a66ff166126d96
nstarman <nstarman@users.noreply.github.com>
2023-07-25 10:37:21 -0400

    reformat cosmology i/o docs
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/cosmology/_io/table.py
+++ astropy/cosmology/_io/table.py
@@ -1,5 +1,160 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+"""|Cosmology| <-> |Table| I/O, using |Cosmology.to_format| and |Cosmology.from_format|.
+
+This module provides functions to transform a |Cosmology| object to and from a |Table|
+object. The functions are registered with ``convert_registry`` under the format name
+"astropy.table". |Table| itself has an abundance of I/O methods, making this conversion
+useful for further interoperability with other formats.
+
+A Cosmology as a `~astropy.table.QTable` will have the cosmology's name and parameters
+as columns.
+
+    >>> from astropy.cosmology import Planck18
+    >>> ct = Planck18.to_format("astropy.table")
+    >>> ct
+    <QTable length=1>
+        name        H0        Om0    Tcmb0    Neff      m_nu      Ob0
+                km / (Mpc s)            K                 eV
+        str8     float64    float64 float64 float64  float64[3] float64
+    -------- ------------ ------- ------- ------- ----------- -------
+    Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
+
+The cosmological class and other metadata, e.g. a paper reference, are in the Table's
+metadata.
+
+    >>> ct.meta
+    OrderedDict([..., ('cosmology', 'FlatLambdaCDM')])
+
+
+Cosmology supports the astropy Table-like protocol (see :ref:`Table-like Objects`) to
+the same effect:
+
+.. code-block::
+
+    >>> from astropy.table import QTable
+    >>> QTable(Planck18)
+    <QTable length=1>
+      name        H0        Om0    Tcmb0    Neff      m_nu      Ob0
+             km / (Mpc s)            K                 eV
+      str8     float64    float64 float64 float64  float64[3] float64
+    -------- ------------ ------- ------- ------- ----------- -------
+    Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
+
+
+To move the cosmology class from the metadata to a Table row, set the
+``cosmology_in_meta`` argument to `False`:
+
+    >>> Planck18.to_format("astropy.table", cosmology_in_meta=False)
+    <QTable length=1>
+        cosmology     name        H0        Om0    Tcmb0    Neff      m_nu      Ob0
+                            km / (Mpc s)            K                 eV
+        str13       str8     float64    float64 float64 float64  float64[3] float64
+    ------------- -------- ------------ ------- ------- ------- ----------- -------
+    FlatLambdaCDM Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
+
+Astropy recommends `~astropy.table.QTable` for tables with `~astropy.units.Quantity`
+columns. However the returned type may be overridden using the ``cls`` argument:
+
+    >>> from astropy.table import Table
+    >>> Planck18.to_format("astropy.table", cls=Table)
+    <Table length=1>
+    ...
+
+Fields of the cosmology may be renamed using the ``rename`` argument.
+
+    >>> Planck18.to_format("astropy.table", rename={"H0": "Hubble"})
+    <QTable length=1>
+        name      Hubble      Om0    Tcmb0    Neff      m_nu      Ob0
+                km / (Mpc s)            K                 eV
+        str8     float64    float64 float64 float64  float64[3] float64
+    -------- ------------ ------- ------- ------- ----------- -------
+    Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
+
+Appropriately formatted tables can be converted to |Cosmology| instances. Since the
+|Table| can hold arbitrary metadata, we can faithfully round-trip a |Cosmology| through
+|Table|, e.g. to construct a ``Planck18`` cosmology identical to the instance from which
+it was generated.
+
+    >>> ct = Planck18.to_format("astropy.table")
+
+    >>> cosmo = Cosmology.from_format(ct, format="astropy.table")
+    >>> cosmo
+    FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                    Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
+    >>> cosmo == Planck18
+    True
+
+The ``cosmology`` information (row or metadata) may be omitted if the cosmology class
+(or its string name) is passed as the ``cosmology`` keyword argument to
+|Cosmology.from_format|.
+
+    >>> del ct.meta["cosmology"]  # remove cosmology from metadata
+    >>> Cosmology.from_format(ct, cosmology="FlatLambdaCDM")
+    FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                    Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
+
+Alternatively, specific cosmology classes can be used to parse the data.
+
+    >>> from astropy.cosmology import FlatLambdaCDM
+    >>> FlatLambdaCDM.from_format(ct)
+    FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                    Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
+
+When using a specific cosmology class, the class' default parameter values are used to
+fill in any missing information.
+
+    >>> del ct["Tcmb0"]  # show FlatLambdaCDM provides default
+    >>> FlatLambdaCDM.from_format(ct)
+    FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                    Tcmb0=0.0 K, Neff=3.046, m_nu=None, Ob0=0.04897)
+
+For tables with multiple rows of cosmological parameters, the ``index`` argument is
+needed to select the correct row. The index can be an integer for the row number or, if
+the table is indexed by a column, the value of that column. If the table is not indexed
+and ``index`` is a string, the "name" column is used as the indexing column.
+
+Here is an example where ``index`` is needed and can be either an integer (for the row
+number) or the name of one of the cosmologies, e.g. 'Planck15'.
+
+    >>> from astropy.cosmology import Planck13, Planck15, Planck18
+    >>> from astropy.table import vstack
+    >>> cts = vstack([c.to_format("astropy.table")
+    ...               for c in (Planck13, Planck15, Planck18)],
+    ...              metadata_conflicts='silent')
+    >>> cts
+    <QTable length=3>
+        name        H0        Om0    Tcmb0    Neff      m_nu      Ob0
+                km / (Mpc s)            K                 eV
+        str8     float64    float64 float64 float64  float64[3]  float64
+    -------- ------------ ------- ------- ------- ----------- --------
+    Planck13        67.77 0.30712  2.7255   3.046 0.0 .. 0.06 0.048252
+    Planck15        67.74  0.3075  2.7255   3.046 0.0 .. 0.06   0.0486
+    Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06  0.04897
+
+    >>> cosmo = Cosmology.from_format(cts, index=1, format="astropy.table")
+    >>> cosmo == Planck15
+    True
+
+Fields in the table can be renamed to match the `~astropy.cosmology.Cosmology` class'
+signature using the ``rename`` argument. This is useful when the table's column names do
+not match the class' parameter names.
+
+    >>> renamed_table = Planck18.to_format("astropy.table", rename={"H0": "Hubble"})
+    >>> renamed_table
+    <QTable length=1>
+        name      Hubble      Om0    Tcmb0    Neff      m_nu      Ob0
+                km / (Mpc s)            K                 eV
+        str8     float64    float64 float64 float64  float64[3] float64
+    -------- ------------ ------- ------- ------- ----------- -------
+    Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
+
+    >>> cosmo = Cosmology.from_format(renamed_table, format="astropy.table",
+    ...                               rename={"Hubble": "H0"})
+    >>> cosmo == Planck18
+    True
+"""
+
 import numpy as np
 
 from astropy.cosmology.connect import convert_registry
@@ -19,27 +174,27 @@ def from_table(table, index=None, *, move_to_meta=False, cosmology=None, rename=
     table : `~astropy.table.Table`
         The object to parse into a |Cosmology|.
     index : int, str, or None, optional
-        Needed to select the row in tables with multiple rows. ``index`` can be
-        an integer for the row number or, if the table is indexed by a column,
-        the value of that column. If the table is not indexed and ``index``
-        is a string, the "name" column is used as the indexing column.
+        Needed to select the row in tables with multiple rows. ``index`` can be an
+        integer for the row number or, if the table is indexed by a column, the value of
+        that column. If the table is not indexed and ``index`` is a string, the "name"
+        column is used as the indexing column.
 
     move_to_meta : bool (optional, keyword-only)
-        Whether to move keyword arguments that are not in the Cosmology class'
-        signature to the Cosmology's metadata. This will only be applied if the
-        Cosmology does NOT have a keyword-only argument (e.g. ``**kwargs``).
-        Arguments moved to the metadata will be merged with existing metadata,
-        preferring specified metadata in the case of a merge conflict
-        (e.g. for ``Cosmology(meta={'key':10}, key=42)``, the ``Cosmology.meta``
-        will be ``{'key': 10}``).
+        Whether to move keyword arguments that are not in the Cosmology class' signature
+        to the Cosmology's metadata. This will only be applied if the Cosmology does NOT
+        have a keyword-only argument (e.g. ``**kwargs``). Arguments moved to the
+        metadata will be merged with existing metadata, preferring specified metadata in
+        the case of a merge conflict (e.g. for ``Cosmology(meta={'key':10}, key=42)``,
+        the ``Cosmology.meta`` will be ``{'key': 10}``).
 
     cosmology : str or type or None (optional, keyword-only)
-        The cosmology class (or string name thereof) to use when constructing
-        the cosmology instance. The class also provides default parameter values,
-        filling in any non-mandatory arguments missing in 'table'.
+        The cosmology class (or string name thereof) to use when constructing the
+        cosmology instance. The class also provides default parameter values, filling in
+        any non-mandatory arguments missing in 'table'.
 
     rename : dict or None (optional, keyword-only)
-        A dictionary mapping columns in 'table' to fields of the `~astropy.cosmology.Cosmology` class.
+        A dictionary mapping columns in 'table' to fields of the
+        `~astropy.cosmology.Cosmology` class.
 
     Returns
     -------
@@ -47,9 +202,8 @@ def from_table(table, index=None, *, move_to_meta=False, cosmology=None, rename=
 
     Examples
     --------
-    To see loading a `~astropy.cosmology.Cosmology` from a Table with
-    ``from_table``, we will first make a |QTable| using
-    :func:`~astropy.cosmology.Cosmology.to_format`.
+    To see loading a `~astropy.cosmology.Cosmology` from a Table with ``from_table``, we
+    will first make a |QTable| using :func:`~astropy.cosmology.Cosmology.to_format`.
 
         >>> from astropy.cosmology import Cosmology, Planck18
         >>> ct = Planck18.to_format("astropy.table")
@@ -61,32 +215,45 @@ def from_table(table, index=None, *, move_to_meta=False, cosmology=None, rename=
         -------- ------------ ------- ------- ------- ----------- -------
         Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06 0.04897
 
-    Now this table can be used to load a new cosmological instance identical
-    to the ``Planck18`` cosmology from which it was generated.
+    Now this table can be used to load a new cosmological instance identical to the
+    ``Planck18`` cosmology from which it was generated.
 
         >>> cosmo = Cosmology.from_format(ct, format="astropy.table")
         >>> cosmo
         FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
                       Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
 
-    Specific cosmology classes can be used to parse the data. The class'
-    default parameter values are used to fill in any information missing in the
-    data.
+    The ``cosmology`` information (column or metadata) may be omitted if the cosmology
+    class (or its string name) is passed as the ``cosmology`` keyword argument to
+    |Cosmology.from_format|.
+
+        >>> del ct.meta["cosmology"]  # remove cosmology from metadata
+        >>> Cosmology.from_format(ct, cosmology="FlatLambdaCDM")
+        FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                      Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
+
+    Alternatively, specific cosmology classes can be used to parse the data.
 
         >>> from astropy.cosmology import FlatLambdaCDM
+        >>> FlatLambdaCDM.from_format(ct)
+        FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
+                      Tcmb0=2.7255 K, Neff=3.046, m_nu=[0. 0. 0.06] eV, Ob0=0.04897)
+
+    When using a specific cosmology class, the class' default parameter values are used
+    to fill in any missing information.
+
         >>> del ct["Tcmb0"]  # show FlatLambdaCDM provides default
         >>> FlatLambdaCDM.from_format(ct)
         FlatLambdaCDM(name="Planck18", H0=67.66 km / (Mpc s), Om0=0.30966,
                       Tcmb0=0.0 K, Neff=3.046, m_nu=None, Ob0=0.04897)
 
-    For tables with multiple rows of cosmological parameters, the ``index``
-    argument is needed to select the correct row. The index can be an integer
-    for the row number or, if the table is indexed by a column, the value of
-    that column. If the table is not indexed and ``index`` is a string, the
-    "name" column is used as the indexing column.
+    For tables with multiple rows of cosmological parameters, the ``index`` argument is
+    needed to select the correct row. The index can be an integer for the row number or,
+    if the table is indexed by a column, the value of that column. If the table is not
+    indexed and ``index`` is a string, the "name" column is used as the indexing column.
 
-    Here is an example where ``index`` is needed and can be either an integer
-    (for the row number) or the name of one of the cosmologies, e.g. 'Planck15'.
+    Here is an example where ``index`` is needed and can be either an integer (for the
+    row number) or the name of one of the cosmologies, e.g. 'Planck15'.
 
         >>> from astropy.cosmology import Planck13, Planck15, Planck18
         >>> from astropy.table import vstack
@@ -103,13 +270,13 @@ def from_table(table, index=None, *, move_to_meta=False, cosmology=None, rename=
         Planck15        67.74  0.3075  2.7255   3.046 0.0 .. 0.06   0.0486
         Planck18        67.66 0.30966  2.7255   3.046 0.0 .. 0.06  0.04897
 
-        >>> cosmo = Cosmology.from_format(cts, index=1, format="astropy.table")
+        >>> cosmo = Cosmology.from_format(cts, index="Planck15", format="astropy.table")
         >>> cosmo == Planck15
         True
 
     Fields in the table can be renamed to match the `~astropy.cosmology.Cosmology`
-    class' signature using the ``rename`` argument. This is useful when the
-    table's column names do not match the class' parameter names.
+    class' signature using the ``rename`` argument. This is useful when the table's
+    column names do not match the class' parameter names.
 
         >>> renamed_table = Planck18.to_format("astropy.table", rename={"H0": "Hubble"})
         >>> renamed_table
@@ -215,7 +382,7 @@ def to_table(cosmology, *args, cls=QTable, cosmology_in_meta=True, rename=None):
         >>> ct.meta
         OrderedDict([..., ('cosmology', 'FlatLambdaCDM')])
 
-    To move the cosmology class from the metadata to a Table row, set the
+    To move the cosmology class from the metadata to a Table column, set the
     ``cosmology_in_meta`` argument to `False`:
 
         >>> Planck18.to_format("astropy.table", cosmology_in_meta=False)
