076bf80ab8f46bee79b5161ed0f6339874219a35
nstarman <nstarman@users.noreply.github.com>
2023-08-06 21:13:17 -0400

    ruff isort units
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/units/physical.py
+++ astropy/units/physical.py
@@ -7,8 +7,15 @@ import warnings
 
 from astropy.utils.exceptions import AstropyDeprecationWarning
 
-from . import imperial  # for bkwd compat #11975 and #11977  # noqa: F401
-from . import astrophys, cgs, core, misc, quantity, si
+from . import (
+    astrophys,
+    cgs,
+    core,
+    imperial,  # for bkwd compat #11975 and #11977  # noqa: F401
+    misc,
+    quantity,
+    si,
+)
 
 __all__ = ["def_physical_type", "get_physical_type", "PhysicalType"]
 
