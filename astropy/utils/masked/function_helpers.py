4695e123ff62d8fe56fdd653833af7c256975a8d
Clément Robert <cr52@protonmail.com>
2023-08-30 17:49:14 +0200

    Update function_helpers.py
    Co-authored-by: Derek Homeier <709020+dhomeier@users.noreply.github.com>

--- astropy/utils/masked/function_helpers.py
+++ astropy/utils/masked/function_helpers.py
@@ -136,8 +136,9 @@ IGNORED_FUNCTIONS |= {
 if NUMPY_LT_2_0:
     NP_LIB_ARRAYSETOP_FUNCS = np.lib.arraysetops.__all__
 else:
-    # np.lib.arraysetops is private in numpy 2.0 and raises AttributeError
-    # substitute a hard-coded copy for now
+    # Public set operations have been moved to the top-level namespace in numpy 2.0
+    # (numpy/numpy#24507), raising an AttributeError when accessed through np.lib.arraysetops.
+    # Listing them explicitly here.
     NP_LIB_ARRAYSETOP_FUNCS = [
         'ediff1d', 'intersect1d', 'setxor1d', 'union1d', 'setdiff1d', 'unique',
         'in1d', 'isin'
