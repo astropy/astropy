76d416b13ae0f8df228e71705c985055eaff68e4
Derek Homeier <709020+dhomeier@users.noreply.github.com>
2023-08-30 17:52:42 +0200

    Fix fast reader tests and warnings for "win32" (#15240)
    * TST: Fix fast reader tests and warnings for "win32"
    
    * TST: use `nullcontext()` to cover 0-warning cases
    
    ---------
    
    Co-authored-by: Derek Homeier <dhomeier@users.noreply.github.com>

--- astropy/io/ascii/tests/test_c_reader.py
+++ astropy/io/ascii/tests/test_c_reader.py
@@ -1244,7 +1244,7 @@ def test_data_out_of_range(parallel, fast_reader, guess):
         fast_reader["parallel"] = parallel
         if fast_reader.get("use_fast_converter"):
             rtol = 1.0e-15
-        elif (sys.maxsize < 2**32) or (sys.platform == "win32"):
+        elif sys.maxsize < 2**32:
             # On 32bit the standard C parser (strtod) returns strings for these
             pytest.xfail("C parser cannot handle float64 on 32bit systems")
 
@@ -1300,7 +1300,10 @@ def test_data_out_of_range(parallel, fast_reader, guess):
             fast_reader=fast_reader,
         )
     if test_for_warnings:  # Assert precision warnings for cols 4-6
-        assert len(w) == 3
+        if sys.platform == "win32" and not fast_reader.get("use_fast_converter"):
+            assert len(w) == 2
+        else:
+            assert len(w) == 3
         for i in range(len(w)):
             assert f"OverflowError converting to FloatType in column col{i+4}" in str(
                 w[i].message
@@ -1331,7 +1334,12 @@ def test_data_out_of_range(parallel, fast_reader, guess):
             fast_reader=fast_reader,
         )
     if test_for_warnings:
-        assert len(w) == 3
+        # CI Windows identifies as "win32" but has 64 bit compiler;
+        # its `strtod` not emitting certain warnings.
+        if sys.platform == "win32" and not fast_reader.get("use_fast_converter"):
+            assert len(w) == 2
+        else:
+            assert len(w) == 3
     read_values = np.array([col[0] for col in t.itercols()])
     assert_almost_equal(read_values, values, rtol=rtol, atol=1.0e-324)
 
@@ -1354,12 +1362,20 @@ def test_data_at_range_limit(parallel, fast_reader, guess):
     # Python reader and strtod() are expected to return precise results
     rtol = 1.0e-30
 
+    # CI "win32" 64 bit compiler with `strtod` not emitting certain warnings.
+    if sys.platform == "win32":
+        ctx = nullcontext()
+    else:
+        ctx = pytest.warns()
+
     # Update fast_reader dict; adapt relative precision for fast_converter
     if fast_reader:
         fast_reader["parallel"] = parallel
+        # `xstrtod` behaves the same on win32
         if fast_reader.get("use_fast_converter"):
             rtol = 1.0e-15
-        elif (sys.maxsize < 2**32) or (sys.platform == "win32"):
+            ctx = pytest.warns()
+        elif sys.maxsize < 2**32:
             # On 32bit the standard C parser (strtod) returns strings for these
             pytest.xfail("C parser cannot handle float64 on 32bit systems")
 
@@ -1399,7 +1415,7 @@ def test_data_at_range_limit(parallel, fast_reader, guess):
         pytest.skip("Catching warnings broken in parallel mode")
     elif not fast_reader:
         pytest.skip("Python/numpy reader does not raise on Overflow")
-    with pytest.warns() as warning_lines:
+    with ctx as w:
         t = ascii.read(
             StringIO("0." + 314 * "0" + "1"),
             format="no_header",
@@ -1407,12 +1423,11 @@ def test_data_at_range_limit(parallel, fast_reader, guess):
             fast_reader=fast_reader,
         )
 
-    n_warns = len(warning_lines)
-    assert n_warns in (0, 1), f"Expected 0 or 1 warning, found {n_warns}"
-    if n_warns == 1:
+    if not isinstance(ctx, nullcontext):
+        assert len(w) == 1, f"Expected 1 warning, found {len(w)}"
         assert (
             "OverflowError converting to FloatType in column col1, possibly "
-            "resulting in degraded precision" in str(warning_lines[0].message)
+            "resulting in degraded precision" in str(w[0].message)
         )
 
     assert_almost_equal(t["col1"][0], 1.0e-315, rtol=1.0e-10, atol=1.0e-324)
