bfec2a6833e35728260e183cfd204a8a6bb3834b
Hélvio Peixoto <hfcpeixoto@gmail.com>
2023-09-01 11:11:48 -0300

    Address ruff violations to rule PIE810 (#15261)
    * Fix PIE810 ruff violations
    
    * Update astropy/io/votable/tests/test_vo.py
    
    ---------
    
    Co-authored-by: Eero Vaher <eero.vaher@gmail.com>

--- astropy/io/votable/tests/test_vo.py
+++ astropy/io/votable/tests/test_vo.py
@@ -728,11 +728,8 @@ def table_from_scratch():
 @np.errstate(over="ignore")
 def test_open_files():
     for filename in get_pkg_data_filenames("data", pattern="*.xml"):
-        if filename.endswith("custom_datatype.xml") or filename.endswith(
-            "timesys_errors.xml"
-        ):
-            continue
-        parse(filename)
+        if not filename.endswith(("custom_datatype.xml", "timesys_errors.xml")):
+            parse(filename)
 
 
 def test_too_many_columns():
