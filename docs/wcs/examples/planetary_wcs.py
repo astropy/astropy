870c659dce7e62b66fbb579112a6b39af5877572
Chiara Marmo <marmochia.oudeis@proton.me>
2023-09-25 19:29:54 +0200

    Fixes

--- docs/wcs/examples/planetary_wcs.py
+++ docs/wcs/examples/planetary_wcs.py
@@ -5,15 +5,15 @@ from astropy.coordinates import BaseBodycentricRepresentation, BaseCoordinateFra
 from astropy.wcs.utils import celestial_frame_to_wcs
 
 
-class IAUMARS2000BodycentricRepresentation(BaseBodycentricRepresentation):
-    _equatorial_radius = 3396190.0 * u.m
-    _flattening = 0.5886007555512007 * u.percent
+class MARSCustomBodycentricRepresentation(BaseBodycentricRepresentation):
+    _equatorial_radius = 3399190.0 * u.m
+    _flattening = 0.5886* u.percent
 
-class IAUMARS2000BodyFrame(BaseCoordinateFrame):
+class MARSCustomBodyFrame(BaseCoordinateFrame):
     name = "Mars"
 
-frame = IAUMARS2000BodyFrame()
-frame.representation_type = IAUMARS2000BodycentricRepresentation
+frame = MARSCustomBodyFrame()
+frame.representation_type = MARSCustomBodycentricRepresentation
 mywcs = celestial_frame_to_wcs(frame, projection="CAR")
 print(mywcs.wcs.ctype)
 print(mywcs.wcs.name)
