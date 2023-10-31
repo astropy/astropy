3b95b2cce834c92f113812d407ed2bc4deb59375
Sedona Price <sedona.price@gmail.com>
2023-10-31 11:34:29 -0400

    Alphabetize module imports
    Edit imports in log_linear_rgb.py, lupton_rgb.py to directly reference module files

--- astropy/visualization/lupton_rgb.py
+++ astropy/visualization/lupton_rgb.py
@@ -10,7 +10,9 @@ For details, see : https://ui.adsabs.harvard.edu/abs/2004PASP..116..133L
 
 import numpy as np
 
-from astropy.visualization import BaseStretch, ZScaleInterval
+
+from astropy.visualization.interval import ZScaleInterval
+from astropy.visualization.stretch import BaseStretch
 from astropy.visualization.stretch import _prepare as _stretch_prepare
 
 from .log_linear_rgb import RGBImageMapping
