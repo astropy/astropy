a481094dc0a6058fd3024b3330c3c2211e2de39d
Clément Robert <cr52@protonmail.com>
2023-12-04 12:26:08 +0100

    DOC: edit out a reference to pylab (prefer matplotlib.pyplot)

--- astropy/visualization/hist.py
+++ astropy/visualization/hist.py
@@ -11,7 +11,7 @@ def hist(x, bins=10, ax=None, max_bins=1e5, **kwargs):
     This is a histogram function that enables the use of more sophisticated
     algorithms for determining bins.  Aside from the ``bins`` argument allowing
     a string specified how bins are computed, the parameters are the same
-    as pylab.hist().
+    as matplotlib.pyplot.hist().
 
     This function was ported from astroML: https://www.astroml.org/
 
