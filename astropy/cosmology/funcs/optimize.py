c95adcc7a96e981a432f26169398435747c6e686
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-01-10 22:56:20 +0000

    typealias import

--- astropy/cosmology/funcs/optimize.py
+++ astropy/cosmology/funcs/optimize.py
@@ -4,7 +4,7 @@
 from __future__ import annotations
 
 import warnings
-from typing import TYPE_CHECKING, Any, Callable, Protocol, TypeAlias, TypedDict
+from typing import TYPE_CHECKING, Any, Callable, Protocol, TypedDict
 
 import numpy as np
 import numpy.typing as npt
@@ -16,7 +16,7 @@ from astropy.utils.exceptions import AstropyUserWarning
 
 if TYPE_CHECKING:
     from scipy.optimize import OptimizeResult
-    from typing_extensions import NotRequired
+    from typing_extensions import NotRequired, TypeAlias
 
 __all__ = ["z_at_value"]
 
