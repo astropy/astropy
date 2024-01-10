274faeeea57e57b3e3df52cb0e08b9a044b06103
nstarman <nstarman@users.noreply.github.com>
2024-01-10 18:28:34 -0500

    Create `units.typing` module with `QuantityLike`
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ astropy/units/typing.py
@@ -0,0 +1,62 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""Typing module for :mod:`~astropy.units`."""
+
+__all__ = ["QuantityLike"]
+
+
+import numpy.typing as npt
+from typing_extensions import TypeAlias
+
+from astropy.units import Quantity
+
+# Note: Quantity is technically covered by npt.ArrayLike, but we want to
+# explicitly include it here so that it is clear that we are also including
+# Quantity objects in the definition of QuantityLike.
+QuantityLike: TypeAlias = Quantity | npt.ArrayLike
+"""Type alias for a quantity-like object.
+
+This is an object that can be converted to a :class:`~astropy.units.Quantity` object
+using the :func:`~astropy.units.Quantity` constructor.
+
+Examples
+--------
+We assume the following imports:
+
+    >>> from astropy import units as u
+
+This is a non-exhaustive list of examples of quantity-like objects:
+
+Integers and floats:
+
+    >>> u.Quantity(1, u.meter)
+    <Quantity 1.0 m>
+
+    >>> u.Quantity(1.0, u.meter)
+    <Quantity 1.0 m>
+
+Tuples and lists:
+
+    >>> u.Quantity((1.0, 2.0), u.meter)
+    <Quantity [1., 2.] m>
+
+    >>> u.Quantity([1.0, 2.0], u.meter)
+    <Quantity [1., 2.] m>
+
+Numpy arrays:
+
+    >>> u.Quantity(np.array([1.0, 2.0]), u.meter)
+
+:class:`~astropy.units.Quantity` objects:
+
+    >>> u.Quantity(u.Quantity(1.0, u.meter))
+    <Quantity 1.0 m>
+
+Strings:
+
+    >>> u.Quantity('1.0 m')
+    <Quantity 1.0 m>
+
+For more examples see the :mod:`numpy.typing` definition of
+:obj:`numpy.typing.ArrayLike`.
+"""
