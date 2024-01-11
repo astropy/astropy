3c829e89437ff6067fce6cb440f92fe2c6c8c900
nstarman <nstarman@users.noreply.github.com>
2024-01-10 23:45:06 -0500

    cleanup imports
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/units/__init__.py
+++ astropy/units/__init__.py
@@ -10,43 +10,31 @@ Pontzen, who has granted the Astropy project permission to use the
 code under a BSD license.
 """
 
-# Import order matters here -- circular dependencies abound!
-# Lots of things to import - go from more basic to advanced, so that
-# whatever advanced ones need generally has been imported already;
-# this also makes it easier to understand where most time is spent
-# (e.g., using python -X importtime).
-
 from . import astrophys, cgs, misc, photometric, si
+from .astrophys import *
+from .cgs import *
 from .core import *
 from .core import set_enabled_units
-from .quantity import *
-
-# isort: off
-from .function import units as function_units
-
-from .si import *
-from .astrophys import *
-from .photometric import *
-from .cgs import *
-from .physical import *
-from .function.units import *
-from .misc import *
-
-from .equivalencies import *
-
-from .function.core import *
-from .function.logarithmic import *
-
 from .decorators import *
+from .misc import *
+from .photometric import *
+from .physical import *
+from .quantity import *
+from .si import *
 from .structured import *
 
-# isort: on
+# Import order matters here -- circular dependencies abound!
+# isort: split
+from .equivalencies import *
+from .function import units as function_units
+from .function.core import *
+from .function.logarithmic import *
+from .function.units import *
 
 del bases
 
 # Enable the set of default units.  This notably does *not* include
 # Imperial units.
-
 set_enabled_units([si, cgs, astrophys, function_units, misc, photometric])
 
 
