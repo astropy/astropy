cdcaa0dbb73b13f69c32e3b580b2a663308b7657
nstarman <nstarman@users.noreply.github.com>
2024-01-11 16:26:36 -0500

    Define `__all__`
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/units/__init__.py
+++ astropy/units/__init__.py
@@ -10,7 +10,18 @@ Pontzen, who has granted the Astropy project permission to use the
 code under a BSD license.
 """
 
-from . import astrophys, cgs, misc, photometric, si
+from . import (
+    astrophys,
+    cgs,
+    core,
+    decorators,
+    misc,
+    photometric,
+    physical,
+    quantity,
+    si,
+    structured,
+)
 from .astrophys import *
 from .cgs import *
 from .core import *
@@ -25,17 +36,28 @@ from .structured import *
 
 # Import order matters here -- circular dependencies abound!
 # isort: split
+from . import equivalencies, function
 from .equivalencies import *
-from .function import units as function_units
-from .function.core import *
-from .function.logarithmic import *
-from .function.units import *
+from .function import *
 
-del bases
+__all__ = []
+__all__ += core.__all__
+__all__ += quantity.__all__
+__all__ += decorators.__all__
+__all__ += structured.__all__
+__all__ += si.__all__
+__all__ += cgs.__all__
+__all__ += astrophys.__all__
+__all__ += physical.__all__
+__all__ += misc.__all__
+__all__ += photometric.__all__
+__all__ += structured.__all__
+__all__ += equivalencies.__all__
+__all__ += function.__all__
 
 # Enable the set of default units.  This notably does *not* include
 # Imperial units.
-set_enabled_units([si, cgs, astrophys, function_units, misc, photometric])
+set_enabled_units([si, cgs, astrophys, function.units, misc, photometric])
 
 
 # -------------------------------------------------------------------------
