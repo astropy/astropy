0369a22e9b15410b1eac71004666e04cd98a8ef2
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-01-16 16:59:36 -0500

    Update astropy/units/__init__.py

--- astropy/units/__init__.py
+++ astropy/units/__init__.py
@@ -56,7 +56,7 @@ __all__ += equivalencies.__all__
 __all__ += function.__all__
 # We sort `__all__` for the docs. We use `globals` to avoid confusing any static
 # analysis tools.
-globals()["__all__"] = sorted(__all__)
+globals()["__all__"].sort()
 
 # Enable the set of default units.  This notably does *not* include
 # Imperial units.
