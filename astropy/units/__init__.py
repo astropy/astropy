ac222343ee7685a223b74fed7a6e121e0634586a
Nathaniel Starkman <nstarman@users.noreply.github.com>
2024-01-17 10:34:48 -0500

    Alphabetize units docs, by ensuring the recently introduced __all__ is sorted
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/units/__init__.py
+++ astropy/units/__init__.py
@@ -54,6 +54,9 @@ __all__ += photometric.__all__
 __all__ += structured.__all__
 __all__ += equivalencies.__all__
 __all__ += function.__all__
+# We sort `__all__` for the docs. We use `globals` to avoid confusing any static
+# analysis tools.
+globals()["__all__"].sort()
 
 # Enable the set of default units.  This notably does *not* include
 # Imperial units.
