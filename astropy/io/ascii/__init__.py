fb6712d89d7b47164fd52f0f158a8a2ea72e18cd
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-02-12 11:32:08 -0500

    first working draft

--- astropy/io/ascii/__init__.py
+++ astropy/io/ascii/__init__.py
@@ -57,6 +57,7 @@ from .fixedwidth import (
     FixedWidthTwoLine,
 )
 from .html import HTML
+from .tdat import Tdat
 from .ipac import Ipac
 from .latex import AASTex, Latex, latexdicts
 from .mrt import Mrt
