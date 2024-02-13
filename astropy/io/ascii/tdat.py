0f85567de41d87479696e6b02514ab0f9a39fa92
Abdu Zoghbi <a.zoghbi@nasa.gov>
2024-02-13 11:56:14 -0500

    add more header parsing

--- astropy/io/ascii/tdat.py
+++ astropy/io/ascii/tdat.py
@@ -6,6 +6,7 @@ not meant to be used directly, but instead are available as readers/writers in
 :Author: Abdu Zoghbi
 """
 import re
+from collections import OrderedDict
 
 from . import core
 
@@ -14,26 +15,76 @@ class TdatHeader(core.BaseHeader):
     """Header Reader for TDAT format"""
 
     start_line = 0
-    comment = r"\s*(#|//).*$"
+    comment = r"\s*(#|//)(.*)$"
     write_comment = "# "
 
-    def process_lines(self, lines):
-        """Select lines between <HEADER> and <DATA>"""
+    def update_meta(self, lines, meta):
+        """Extract meta information: comments and key/values in the header"""
+        comments = []
+        keywords = OrderedDict()
+        col_lines = []
+
+        key_parser = re.compile(r'(?P<key>\w+)\s*=\s*([\'"`])?(?P<value>.*?)(?(2)\2|\s*$)')
+
+        desc_comments = ['Table Parameters', 'Virtual Parameters',
+                         'Relationship Definitions', 'Data Format Specification']
+        
         is_header = False
         re_comment = re.compile(self.comment)
         for line in lines:
-            # skip comments
-            if re_comment.match(line):
-                continue
             if '<header>' in line.lower():
                 is_header = True
                 # we don't want <header> itself
                 continue
             if '<data>' in line.lower():
                 # end of header
-                return
+                is_header = False
+            
             if is_header:
+                # get comments
+                match = re_comment.match(line)
+                kmatch = key_parser.match(line)
+                if match:
+                    add = True
+                    for cc in desc_comments:
+                        if cc in match.group(2):
+                            add = False
+                    if add and match.group(2) != '':
+                        comments.append(match.group(2))
+                elif kmatch:
+                    keywords[kmatch.group('key')] = kmatch.group('value')
+                else:
+                    if 'field' in line:
+                        col_lines.append(line)
+        
+        meta['table']['comments'] = comments
+        meta['table']['keywords'] = keywords
+        self.col_lines = col_lines
+        
+
+    def process_lines(self, lines):
+        """Select lines between <HEADER> and <DATA>"""
+
+        # if already processed, yield the lines
+        if hasattr(self, 'col_lines'):
+            for line in self.col_lines:
                 yield line
+        else:
+            is_header = False
+            re_comment = re.compile(self.comment)
+            for line in lines:
+                # skip comments
+                if re_comment.match(line):
+                    continue
+                if '<header>' in line.lower():
+                    is_header = True
+                    # we don't want <header> itself
+                    continue
+                if '<data>' in line.lower():
+                    # end of header
+                    return
+                if is_header:
+                    yield line
 
     def get_cols(self, lines):
         """Identify the columns and table description"""
@@ -55,22 +106,26 @@ class TdatHeader(core.BaseHeader):
             (?:[//|#]+\s*(?P<comment>[^/#]*))?
             \s*
             """, re.VERBOSE)
+        
         cols = []
         for line in header_lines:
-            match = col_parser.match(line)
-            if match:
-                col = core.Column(name=match.group('name'))
-                col.ctype = match.group('ctype')
-                col.fmt = match.group('fmt')
-                col.unit = match.group('unit')
-                col.ucd = match.group('ucd')
-                col.idx = match.group('idx')
-                col.desc = match.group('desc')
-                col.comment = match.group('comment')
+            
+            # look for field[..]= ... column definitions
+            cmatch = col_parser.match(line)
+            if cmatch:
+                col = core.Column(name=cmatch.group('name'))
+                col.ctype = cmatch.group('ctype')
+                col.fmt = cmatch.group('fmt')
+                col.unit = cmatch.group('unit')
+                col.ucd = cmatch.group('ucd')
+                col.idx = cmatch.group('idx')
+                col.description = cmatch.group('desc')
+                col.comment = cmatch.group('comment')
                 cols.append(col)
 
         self.names = [col.name for col in cols]
         self.cols = cols
+
                 
 class TdatDataSplitter(core.BaseSplitter):
     """Splitter for tdat data."""
