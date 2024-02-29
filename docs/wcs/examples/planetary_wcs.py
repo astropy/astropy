73df5922e36931e54ed66a52abc15806c846febc
Mridul Seth <git@mriduls.com>
2024-02-29 20:30:44 +0530

    MAINT: Let ruff format run on docs and examples (#16132)
    * MAINT: Let ruff format run on docs and examples

--- docs/wcs/examples/planetary_wcs.py
+++ docs/wcs/examples/planetary_wcs.py
@@ -7,10 +7,12 @@ from astropy.wcs.utils import celestial_frame_to_wcs
 
 class MARSCustomBodycentricRepresentation(BaseBodycentricRepresentation):
     _equatorial_radius = 3399190.0 * u.m
-    _flattening = 0.5886* u.percent
+    _flattening = 0.5886 * u.percent
+
 
 class MARSCustomBodyFrame(BaseCoordinateFrame):
     name = "Mars"
+
 
 frame = MARSCustomBodyFrame()
 frame.representation_type = MARSCustomBodycentricRepresentation
