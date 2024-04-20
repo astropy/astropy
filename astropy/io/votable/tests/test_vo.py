64442f2e816b42b79231c6fc4d9aa06b0679a71a
Clément Robert <cr52@protonmail.com>
2024-04-20 10:55:02 +0200

    CLN: cleanup unused test function

--- astropy/io/votable/tests/test_vo.py
+++ astropy/io/votable/tests/test_vo.py
@@ -845,42 +845,6 @@ def test_select_columns_binary(format_):
     assert table.colnames == ["string_test", "string_test_2", "unicode_test"]
 
 
-def table_from_scratch():
-    from astropy.io.votable.tree import Field, Resource, TableElement, VOTableFile
-
-    # Create a new VOTable file...
-    votable = VOTableFile()
-
-    # ...with one resource...
-    resource = Resource()
-    votable.resources.append(resource)
-
-    # ... with one table
-    table = TableElement(votable)
-    resource.tables.append(table)
-
-    # Define some fields
-    table.fields.extend(
-        [
-            Field(votable, ID="filename", datatype="char"),
-            Field(votable, ID="matrix", datatype="double", arraysize="2x2"),
-        ]
-    )
-
-    # Now, use those field definitions to create the numpy record arrays, with
-    # the given number of rows
-    table.create_arrays(2)
-
-    # Now table.array can be filled with data
-    table.array[0] = ("test1.xml", [[1, 0], [0, 1]])
-    table.array[1] = ("test2.xml", [[0.5, 0.3], [0.2, 0.1]])
-
-    # Now write the whole thing to a file.
-    # Note, we have to use the top-level votable file object
-    out = io.StringIO()
-    votable.to_xml(out)
-
-
 def test_direct_fields_mutation():
     from astropy.io.votable.tree import Field, Resource, TableElement, VOTableFile
 
