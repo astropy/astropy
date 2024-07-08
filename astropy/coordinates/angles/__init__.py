11777112e1f216a7ba3fee6d93160a0a8fafd8ac
Eero Vaher <eero.vaher@fysik.lu.se>
2024-07-08 18:59:59 +0200

    Shorten `astropy/coordinates/angles/__init__.py`

--- astropy/coordinates/angles/__init__.py
+++ astropy/coordinates/angles/__init__.py
@@ -9,13 +9,3 @@ between coordinate systems.
 from .core import *
 from .errors import *
 from .utils import *
-
-# isort: split
-from . import core as _core
-from . import errors as _errors
-from . import utils as _utils
-
-__all__ = []
-__all__ += _core.__all__
-__all__ += _errors.__all__
-__all__ += _utils.__all__
