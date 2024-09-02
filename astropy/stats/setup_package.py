dd66cc414f065224f99ad22b4305bf6d4c893c6d
Clément Robert <cr52@protonmail.com>
2024-09-02 17:03:16 +0200

    MNT: Run PTH flake test in prep for supporting pathlib (stats)
    Co-authored-by: Mridul Seth <git@mriduls.com>

--- astropy/stats/setup_package.py
+++ astropy/stats/setup_package.py
@@ -2,14 +2,14 @@
 
 from __future__ import annotations
 
-import os
+from pathlib import Path
 
 from numpy import get_include as get_numpy_include
 from setuptools import Extension
 
-ROOT = os.path.relpath(os.path.dirname(__file__))
+ROOT = Path(__file__).parent.resolve().relative_to(Path.cwd())
 SRCFILES = ["wirth_select.c", "compute_bounds.c", "fast_sigma_clip.c"]
-SRCFILES = [os.path.join(ROOT, "src", srcfile) for srcfile in SRCFILES]
+SRCFILES = [str(ROOT / "src" / srcfile) for srcfile in SRCFILES]
 
 
 def get_extensions() -> list[Extension, Extension]:
@@ -23,7 +23,7 @@ def get_extensions() -> list[Extension, Extension]:
     _stats_ext = Extension(
         name="astropy.stats._stats",
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
-        sources=[os.path.join(ROOT, "_stats.pyx")],
+        sources=[str(ROOT / "_stats.pyx")],
         include_dirs=[get_numpy_include()],
     )
 
