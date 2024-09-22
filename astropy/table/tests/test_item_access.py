a0d57e30764e735d4d388c224568e7d992cca953
Clément Robert <cr52@protonmail.com>
2024-09-22 18:25:54 +0200

    TST: refactor a couple tests to avoid the use of random numbers

--- astropy/table/tests/test_item_access.py
+++ astropy/table/tests/test_item_access.py
@@ -8,6 +8,8 @@ https://github.com/astropy/astropy/wiki/Table-item-access-definition
 import numpy as np
 import pytest
 
+from astropy.table import Row
+
 
 @pytest.mark.usefixtures("table_data")
 class BaseTestItems:
@@ -248,8 +250,8 @@ class TestTableItems(BaseTestItems):
         py 3.3 failure mode
         """
         t = table_data.Table(table_data.COLS)
-        idxs = np.random.randint(len(t), size=2)
-        t[idxs[1]]
+        row = t[np.int64(0)]
+        assert type(t[np.int64(0)]) is Row
 
     def test_select_bad_column(self, table_data):
         """Select column name that does not exist"""
