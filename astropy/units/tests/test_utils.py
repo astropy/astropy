bd634d8168afc58373f2a6a2e81ed61d3603bbf5
Eero Vaher <eero.vaher@fysik.lu.se>
2024-09-26 19:11:05 +0200

    Rewrite a test to avoid explicit use of private API

--- astropy/units/tests/test_utils.py
+++ astropy/units/tests/test_utils.py
@@ -4,12 +4,9 @@ Test utilities for `astropy.units`.
 """
 
 import numpy as np
-from numpy import finfo
 
 from astropy.units.quantity import Quantity
-from astropy.units.utils import quantity_asanyarray, sanitize_scale
-
-_float_finfo = finfo(float)
+from astropy.units.utils import quantity_asanyarray
 
 
 def test_quantity_asanyarray():
@@ -25,8 +22,3 @@ def test_quantity_asanyarray():
 
     np_array = quantity_asanyarray(array_of_integers, dtype=np.inexact)
     assert np.issubdtype(np_array.dtype, np.inexact)
-
-
-def test_sanitize_scale():
-    assert sanitize_scale(complex(2, _float_finfo.eps)) == 2
-    assert sanitize_scale(complex(_float_finfo.eps, 2)) == 2j
