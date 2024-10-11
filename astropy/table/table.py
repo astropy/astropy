8a36d3c691837e7a9d6950ce803c92b14240d1a0
P. L. Lim <2090236+pllim@users.noreply.github.com>
2024-10-11 16:44:14 -0400

    Revive Table.show_in_notebook method but with ipydatagrid.
    Co-authored-by: Tom Aldcroft <taldcroft@gmail.com>

--- astropy/table/table.py
+++ astropy/table/table.py
@@ -1778,89 +1778,23 @@ class Table:
         else:
             return self
 
-    @deprecated(
-        "6.1",
-        message="""show_in_notebook() is deprecated as of 6.1 and to create
-         interactive tables it is recommended to use dedicated tools like:
-         - https://github.com/bloomberg/ipydatagrid
-         - https://docs.bokeh.org/en/latest/docs/user_guide/interaction/widgets.html#datatable
-         - https://dash.plotly.com/datatable""",
-    )
-    def show_in_notebook(
-        self,
-        tableid=None,
-        css=None,
-        display_length=50,
-        table_class="astropy-default",
-        show_row_index="idx",
-    ):
+    def show_in_notebook(self, **datagrid_kwargs):
         """Render the table in HTML and show it in the IPython notebook.
+
+        .. note::
+
+            This method now requires an optional dependency, ``ipydatagrid``,
+            and has a different API.
 
         Parameters
         ----------
-        tableid : str or None
-            An html ID tag for the table.  Default is ``table{id}-XXX``, where
-            id is the unique integer id of the table object, id(self), and XXX
-            is a random number to avoid conflicts when printing the same table
-            multiple times.
-        table_class : str or None
-            A string with a list of HTML classes used to style the table.
-            The special default string ('astropy-default') means that the string
-            will be retrieved from the configuration item
-            ``astropy.table.default_notebook_table_class``. Note that these
-            table classes may make use of bootstrap, as this is loaded with the
-            notebook.  See `this page <https://getbootstrap.com/css/#tables>`_
-            for the list of classes.
-        css : str
-            A valid CSS string declaring the formatting for the table. Defaults
-            to ``astropy.table.jsviewer.DEFAULT_CSS_NB``.
-        display_length : int, optional
-            Number or rows to show. Defaults to 50.
-        show_row_index : str or False
-            If this does not evaluate to False, a column with the given name
-            will be added to the version of the table that gets displayed.
-            This new column shows the index of the row in the table itself,
-            even when the displayed table is re-sorted by another column. Note
-            that if a column with this name already exists, this option will be
-            ignored. Defaults to "idx".
+        **datagrid_kwargs : dict, optional
+            Keyword arguments as accepted by ``ipydatagrid.DataGrid``.
 
-        Notes
-        -----
-        Currently, unlike `show_in_browser` (with ``jsviewer=True``), this
-        method needs to access online javascript code repositories.  This is due
-        to modern browsers' limitations on accessing local files.  Hence, if you
-        call this method while offline (and don't have a cached version of
-        jquery and jquery.dataTables), you will not get the jsviewer features.
         """
-        from IPython.display import HTML
+        from ipydatagrid import DataGrid
 
-        from .jsviewer import JSViewer
-
-        if tableid is None:
-            tableid = f"table{id(self)}-{np.random.randint(1, 1e6)}"
-
-        jsv = JSViewer(display_length=display_length)
-        if show_row_index:
-            display_table = self._make_index_row_display_table(show_row_index)
-        else:
-            display_table = self
-        if table_class == "astropy-default":
-            table_class = conf.default_notebook_table_class
-        html = display_table._base_repr_(
-            html=True,
-            max_width=-1,
-            tableid=tableid,
-            max_lines=-1,
-            show_dtype=False,
-            tableclass=table_class,
-        )
-
-        columns = display_table.columns.values()
-        sortable_columns = [
-            i for i, col in enumerate(columns) if col.info.dtype.kind in "iufc"
-        ]
-        html += jsv.ipynb(tableid, css=css, sort_columns=sortable_columns)
-        return HTML(html)
+        return DataGrid(self.to_pandas(), **datagrid_kwargs)
 
     @deprecated(
         "6.1",
