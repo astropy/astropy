45ae8c00f45a4fa0e2b79ab21b6e9a2b8fdfec15
P. L. Lim <2090236+pllim@users.noreply.github.com>
2024-10-28 11:22:44 -0400

    Backport PR #16615: Add an announcement banner

--- docs/conf.py
+++ docs/conf.py
@@ -247,6 +247,7 @@ html_theme_options.update(
         },
         # https://github.com/pydata/pydata-sphinx-theme/issues/1492
         "navigation_with_keys": False,
+        "announcement": "https://www.astropy.org/annoucement_banner.html",
     }
 )
 
