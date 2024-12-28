0cf09dee0ec205237a3cc2305b510cc7c0035d88
Chiara Marmo <marmochiaskl@gmail.com>
2024-12-28 18:46:24 +0100

    Add bzero bscale when do_not_scale_image_data is true, add test

--- astropy/io/fits/tests/test_hdulist.py
+++ astropy/io/fits/tests/test_hdulist.py
@@ -178,6 +178,16 @@ class TestHDUListFunctions(FitsTestCase):
         with pytest.raises(ValueError):
             hdul.append(hdu)
 
+    def test_append_scaled_image_with_do_not_scale_image_data(self):
+        """Tests appending a scaled ImageHDU to a HDUList."""
+
+        with fits.open(self.data("scale.fits"), do_not_scale_image_data=True) as source:
+            with fits.open(self.temp("dest.fits"), mode="append") as dest:
+                dest.append(source[0])
+                dest.append(source[0])
+                assert dest[-1].header["BSCALE"] == source[0].header["BSCALE"]
+                assert dest[-1].header["BZERO"] == source[0].header["BZERO"]
+
     def test_insert_primary_to_empty_list(self):
         """Tests inserting a Simple PrimaryHDU to an empty HDUList."""
         hdul = fits.HDUList()
