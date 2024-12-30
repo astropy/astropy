07c160b6bdd6403b55c8685a1e5069e9f1c61d83
aarsh1a <139792275+aarsh1a@users.noreply.github.com>
2024-12-30 23:14:57 +0530

    Update formats.py

--- astropy/time/formats.py
+++ astropy/time/formats.py
@@ -653,14 +653,15 @@ class TimeDecimalYear(TimeNumeric):
     the precise number of days in the year (365 or 366).
 
     Example:
-    >>> from astropy.time import Time
-    >>> tm = Time("2024-04-05T12:34:00")
-    >>> tm0 = Time("2024-01-01T00:00:00")
-    >>> tm1 = Time("2025-01-01T00:00:00")
-    >>> 2024 + (tm.jd - tm0.jd) / (tm1.jd - tm0.jd)
-    2024.2609934729812
-    >>> float(tm.decimalyear)
-    2024.2609934729812
+        >>> from astropy.time import Time
+        >>> tm = Time("2024-04-05T12:34:00")
+        >>> tm0 = Time("2024-01-01T00:00:00")
+        >>> tm1 = Time("2025-01-01T00:00:00")
+        >>> year_fraction = 2024 + (tm.jd - tm0.jd) / (tm1.jd - tm0.jd)
+        >>> year_fraction
+        2024.2609934729812
+        >>> tm.decimalyear
+        2024.2609934729812
 
     When to use:
     Since for this format the length of the year varies between 365 and 366 days, it is
@@ -2090,7 +2091,7 @@ class TimeJulianEpoch(TimeEpochDate):
     exactly 86400 SI seconds.
     The Julian epoch year is defined such that 2000.0 corresponds to 12:00 TT on January 1, 2000.
     Using Astropy, this can be expressed as:
-    Example:
+
         >>> from astropy.time import Time
         >>> import astropy.units as u
         >>> j2000_epoch = Time("2000-01-01T12:00:00", scale="tt")
@@ -2103,7 +2104,6 @@ class TimeJulianEpoch(TimeEpochDate):
     catalog or the time of an observation. The Julian epoch year is sometimes written as
     a string like "J2001.5" with a preceding "J". You can initialize a ``Time`` object with such a string:
 
-    Example:
         >>> Time("J2001.5").jyear
         2001.5
 
