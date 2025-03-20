49d21665f21a058c2242074bcc292cc2b0d7d5c2
Eero Vaher <eero.vaher@fysik.lu.se>
2025-03-20 23:30:57 +0100

    Enforce Ruff rule I001 in `io/ascii/__init__.py`
    27abe26a86ed6a5a1b48ef2f1620e19335b2c5e3 added an exception of the Ruff
    rule I001 (unsorted-imports) for `astropy/io/ascii/__init__.py`, but
    enforcing the rule is simpler.

--- astropy/io/ascii/__init__.py
+++ astropy/io/ascii/__init__.py
@@ -1,6 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """An extensible ASCII table reader and writer."""
 
+from astropy import config as _config
+
 from . import connect
 from .basic import (
     Basic,
@@ -62,9 +64,6 @@ from .qdp import QDP
 from .rst import RST
 from .sextractor import SExtractor
 from .ui import get_read_trace, get_reader, get_writer, read, set_guess, write
-
-
-from astropy import config as _config
 
 
 class Conf(_config.ConfigNamespace):
