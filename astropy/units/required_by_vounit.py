343d1a3f9c2be0eb80361deb10d5afb6e9abfd7a
Eero Vaher <eero.vaher@fysik.lu.se>
2025-03-21 17:10:52 +0100

    Define `__all__` in all unit definition files
    Two unit definition files carefully avoided adding public-looking names
    to their module namespaces, but it is much simpler to define `__all__`
    explicitly.

--- astropy/units/required_by_vounit.py
+++ astropy/units/required_by_vounit.py
@@ -10,32 +10,18 @@ but that are rarely used in practice and liable to lead to confusion (such as
 possible for the non-prefixed unit, ``astropy.units.solMass``.
 """
 
-_ns = globals()
+from . import astrophys
+from .core import UnitBase, _add_prefixes
+from .utils import generate_prefixonly_unit_summary, generate_unit_summary
 
+_add_prefixes(astrophys.solMass, namespace=globals(), prefixes=True)
+_add_prefixes(astrophys.solRad, namespace=globals(), prefixes=True)
+_add_prefixes(astrophys.solLum, namespace=globals(), prefixes=True)
 
-def _initialize_module():
-    # Local imports to avoid polluting top-level namespace
-    from . import astrophys
-    from .core import _add_prefixes
-
-    _add_prefixes(astrophys.solMass, namespace=_ns, prefixes=True)
-    _add_prefixes(astrophys.solRad, namespace=_ns, prefixes=True)
-    _add_prefixes(astrophys.solLum, namespace=_ns, prefixes=True)
-
-
-_initialize_module()
-
-
-###########################################################################
-# DOCSTRING
+__all__ = [name for (name, member) in globals().items() if isinstance(member, UnitBase)]
 
 if __doc__ is not None:
     # This generates a docstring for this module that describes all of the
     # standard units defined here.
-    from .utils import (
-        generate_prefixonly_unit_summary as _generate_prefixonly_unit_summary,
-    )
-    from .utils import generate_unit_summary as _generate_unit_summary
-
-    __doc__ += _generate_unit_summary(globals())
-    __doc__ += _generate_prefixonly_unit_summary(globals())
+    __doc__ += generate_unit_summary(globals())
+    __doc__ += generate_prefixonly_unit_summary(globals())
