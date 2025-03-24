6403f06c153ecde308285ffb51f07da953d4dbc5
Eero Vaher <eero.vaher@fysik.lu.se>
2025-03-24 19:22:47 +0100

    Rewrite `required_by_vounit` tests
    The single test for `required_by_vounit` is replaced with multiple
    parametrized tests, which are better because now a failing `assert` does
    not prevent running the other checks. Equally importantly the tests are
    moved out from `test_deprecated.py` because nothing in
    `required_by_vounit` is nor should be deprecated.

--- astropy/units/tests/test_deprecated.py
+++ astropy/units/tests/test_deprecated.py
@@ -1,13 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 
-"""Regression tests for deprecated units or those that are "soft" deprecated
-because they are required for VOUnit support but are not in common use."""
-
 import pytest
 
 from astropy import units as u
-from astropy.units import deprecated, required_by_vounit
+from astropy.units import deprecated
 
 
 def test_emu():
@@ -35,26 +32,3 @@ def test_emu():
                 assert getattr(deprecated, unitname).represents.bases[0] == getattr(
                     u, namewoprefix
                 )
-
-
-def test_required_by_vounit():
-    # The tests below could be replicated with all the various prefixes, but it
-    # seems unnecessary because they all come as a set.  So we only use nano for
-    # the purposes of this test.
-
-    with pytest.raises(AttributeError):
-        # nano-solar mass/rad/lum shouldn't be in the base unit namespace
-        u.nsolMass
-        u.nsolRad
-        u.nsolLum
-
-    # but they should be enabled by default via required_by_vounit, to allow
-    # the Unit constructor to accept them
-    assert u.Unit("nsolMass") == required_by_vounit.nsolMass
-    assert u.Unit("nsolRad") == required_by_vounit.nsolRad
-    assert u.Unit("nsolLum") == required_by_vounit.nsolLum
-
-    # but because they are prefixes, they shouldn't be in find_equivalent_units
-    assert required_by_vounit.nsolMass not in u.solMass.find_equivalent_units()
-    assert required_by_vounit.nsolRad not in u.solRad.find_equivalent_units()
-    assert required_by_vounit.nsolLum not in u.solLum.find_equivalent_units()
