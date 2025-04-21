5d0a6274cf52896fc0b7b4323f0b479fb98a5845
Larry Bradley <larry.bradley@gmail.com>
2025-04-21 16:53:04 -0400

    Fix interval classes for MaskedArray and MaskedNDArray (#17927)
    * Fix interval classes for MaskedArray input
    
    * Add interval tests for MaskedArray input
    
    * Import assert_allclose from np.testing
    
    * Add changelog entry
    
    * Support Masked as well, by using get_data_and_mask
    
    ---------
    
    Co-authored-by: Marten H. van Kerkwijk <mhvk@astro.utoronto.ca>

--- astropy/visualization/interval.py
+++ astropy/visualization/interval.py
@@ -9,6 +9,8 @@ import abc
 
 import numpy as np
 
+from astropy.utils.masked import get_data_and_mask
+
 from .transform import BaseTransform
 
 __all__ = [
@@ -45,6 +47,31 @@ class BaseInterval(BaseTransform):
             The mininium and maximum image value in the interval.
         """
         raise NotImplementedError("Needs to be implemented in a subclass.")
+
+    @staticmethod
+    def _process_values(values):
+        """
+        Process the input values.
+
+        This function filters out masked and/or invalid values (inf,
+        nan) and returns a flattened 1D array.
+
+        Parameters
+        ----------
+        values : array-like
+            The input values.
+
+        Returns
+        -------
+        result : 1D ndarray
+            The processed values.
+        """
+        data, mask = get_data_and_mask(np.asanyarray(values))
+        ok = np.isfinite(data)
+        if mask is not None:
+            ok &= ~mask
+
+        return data[ok]
 
     def __call__(self, values, clip=True, out=None):
         """
@@ -123,12 +150,7 @@ class ManualInterval(BaseInterval):
         if self.vmin is not None and self.vmax is not None:
             return self.vmin, self.vmax
 
-        # Make sure values is a Numpy array
-        values = np.asarray(values).ravel()
-
-        # Filter out invalid values (inf, nan)
-        values = values[np.isfinite(values)]
-
+        values = self._process_values(values)
         vmin = np.min(values) if self.vmin is None else self.vmin
         vmax = np.max(values) if self.vmax is None else self.vmax
 
@@ -141,11 +163,7 @@ class MinMaxInterval(BaseInterval):
     """
 
     def get_limits(self, values):
-        # Make sure values is a Numpy array
-        values = np.asarray(values).ravel()
-
-        # Filter out invalid values (inf, nan)
-        values = values[np.isfinite(values)]
+        values = self._process_values(values)
 
         return np.min(values), np.max(values)
 
@@ -179,16 +197,12 @@ class AsymmetricPercentileInterval(BaseInterval):
         self.n_samples = n_samples
 
     def get_limits(self, values):
-        # Make sure values is a Numpy array
-        values = np.asarray(values).ravel()
+        values = self._process_values(values)
 
         # If needed, limit the number of samples. We sample with replacement
         # since this is much faster.
         if self.n_samples is not None and values.size > self.n_samples:
             values = np.random.choice(values, self.n_samples)
-
-        # Filter out invalid values (inf, nan)
-        values = values[np.isfinite(values)]
 
         # Determine values at percentiles
         vmin, vmax = np.percentile(
@@ -274,9 +288,9 @@ class ZScaleInterval(BaseInterval):
         self.max_iterations = max_iterations
 
     def get_limits(self, values):
+        values = self._process_values(values)
+
         # Sample the image
-        values = np.asarray(values)
-        values = values[np.isfinite(values)]
         stride = int(max(1.0, values.size / self.n_samples))
         samples = values[::stride][: self.n_samples]
         samples.sort()
--- astropy/visualization/tests/test_interval.py
+++ astropy/visualization/tests/test_interval.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 import pytest
+from numpy.testing import assert_allclose
 
 from astropy.utils import NumpyRNGContext
+from astropy.utils.masked import Masked
 from astropy.visualization.interval import (
     AsymmetricPercentileInterval,
     ManualInterval,
@@ -19,60 +21,60 @@ class TestInterval:
     def test_manual(self):
         interval = ManualInterval(-10.0, +15.0)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -10.0)
-        np.testing.assert_allclose(vmax, +15.0)
+        assert_allclose(vmin, -10.0)
+        assert_allclose(vmax, +15.0)
 
     def test_manual_defaults(self):
         interval = ManualInterval(vmin=-10.0)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -10.0)
-        np.testing.assert_allclose(vmax, np.max(self.data))
+        assert_allclose(vmin, -10.0)
+        assert_allclose(vmax, np.max(self.data))
 
         interval = ManualInterval(vmax=15.0)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, np.min(self.data))
-        np.testing.assert_allclose(vmax, 15.0)
+        assert_allclose(vmin, np.min(self.data))
+        assert_allclose(vmax, 15.0)
 
     def test_manual_zero_limit(self):
         # Regression test for a bug that caused ManualInterval to compute the
         # limit (min or max) if it was set to zero.
         interval = ManualInterval(vmin=0, vmax=0)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, 0)
-        np.testing.assert_allclose(vmax, 0)
+        assert_allclose(vmin, 0)
+        assert_allclose(vmax, 0)
 
     def test_manual_defaults_with_nan(self):
         interval = ManualInterval()
         data = np.copy(self.data)
         data[0] = np.nan
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -20)
-        np.testing.assert_allclose(vmax, +60)
+        assert_allclose(vmin, -20)
+        assert_allclose(vmax, +60)
 
     def test_minmax(self):
         interval = MinMaxInterval()
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -20.0)
-        np.testing.assert_allclose(vmax, +60.0)
+        assert_allclose(vmin, -20.0)
+        assert_allclose(vmax, +60.0)
 
     def test_percentile(self):
         interval = PercentileInterval(62.2)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -4.88)
-        np.testing.assert_allclose(vmax, 44.88)
+        assert_allclose(vmin, -4.88)
+        assert_allclose(vmax, 44.88)
 
     def test_asymmetric_percentile(self):
         interval = AsymmetricPercentileInterval(10.5, 70.5)
         vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -11.6)
-        np.testing.assert_allclose(vmax, 36.4)
+        assert_allclose(vmin, -11.6)
+        assert_allclose(vmax, 36.4)
 
     def test_asymmetric_percentile_nsamples(self):
         with NumpyRNGContext(12345):
             interval = AsymmetricPercentileInterval(10.5, 70.5, n_samples=20)
             vmin, vmax = interval.get_limits(self.data)
-        np.testing.assert_allclose(vmin, -14.367676767676768)
-        np.testing.assert_allclose(vmax, 40.266666666666666)
+        assert_allclose(vmin, -14.367676767676768)
+        assert_allclose(vmax, 40.266666666666666)
 
 
 class TestIntervalList(TestInterval):
@@ -85,25 +87,37 @@ class TestInterval2D(TestInterval):
     data = np.linspace(-20.0, 60.0, 100).reshape(100, 1)
 
 
+class TestIntervalMaskedArray(TestInterval):
+    # Make sure intervals work with MaskedArray
+    data = np.concatenate((np.linspace(-20.0, 60.0, 100), np.full(100, 1e6)))
+    data = np.ma.MaskedArray(data, data > 1000)
+
+
+class TestIntervalMaskedNDArray(TestInterval):
+    # Make sure intervals work with MaskedArray
+    data = np.concatenate((np.linspace(-20.0, 60.0, 100), np.full(100, 1e6)))
+    data = Masked(data, data > 1000)
+
+
 def test_zscale():
     np.random.seed(42)
     data = np.random.randn(100, 100) * 5 + 10
     interval = ZScaleInterval()
     vmin, vmax = interval.get_limits(data)
-    np.testing.assert_allclose(vmin, -9.6, atol=0.1)
-    np.testing.assert_allclose(vmax, 25.4, atol=0.1)
+    assert_allclose(vmin, -9.6, atol=0.1)
+    assert_allclose(vmax, 25.4, atol=0.1)
 
     data = list(range(1000)) + [np.nan]
     interval = ZScaleInterval()
     vmin, vmax = interval.get_limits(data)
-    np.testing.assert_allclose(vmin, 0, atol=0.1)
-    np.testing.assert_allclose(vmax, 999, atol=0.1)
+    assert_allclose(vmin, 0, atol=0.1)
+    assert_allclose(vmax, 999, atol=0.1)
 
     data = list(range(100))
     interval = ZScaleInterval()
     vmin, vmax = interval.get_limits(data)
-    np.testing.assert_allclose(vmin, 0, atol=0.1)
-    np.testing.assert_allclose(vmax, 99, atol=0.1)
+    assert_allclose(vmin, 0, atol=0.1)
+    assert_allclose(vmax, 99, atol=0.1)
 
 
 def test_zscale_npoints():
@@ -124,7 +138,7 @@ def test_integers():
     # Need to make sure integers get cast to float
     interval = MinMaxInterval()
     values = interval([1, 3, 4, 5, 6])
-    np.testing.assert_allclose(values, [0.0, 0.4, 0.6, 0.8, 1.0])
+    assert_allclose(values, [0.0, 0.4, 0.6, 0.8, 1.0])
 
     # Don't accept integer array in output
     out = np.zeros(5, dtype=int)
@@ -136,7 +150,7 @@ def test_integers():
     # But integer input and floating point output is fine
     out = np.zeros(5, dtype=float)
     interval([1, 3, 4, 5, 6], out=out)
-    np.testing.assert_allclose(out, [0.0, 0.4, 0.6, 0.8, 1.0])
+    assert_allclose(out, [0.0, 0.4, 0.6, 0.8, 1.0])
 
 
 def test_constant_data():
@@ -146,5 +160,5 @@ def test_constant_data():
     interval = MinMaxInterval()
     limits = interval.get_limits(data)
     values = interval(data)
-    np.testing.assert_allclose(limits, (1.0, 1.0))
-    np.testing.assert_allclose(values, np.zeros(shape))
+    assert_allclose(limits, (1.0, 1.0))
+    assert_allclose(values, np.zeros(shape))
