d35b5b1ae731c475f2118cd30dbc0b847886696a
Tom Aldcroft <taldcroft@gmail.com>
2025-05-09 14:05:15 -0400

    Still more refactoring

--- astropy/io/misc/pyarrow/ecsv.py
+++ astropy/io/misc/pyarrow/ecsv.py
@@ -13,6 +13,8 @@ import numpy.typing as npt
 
 from astropy.table import Table, meta, serialize
 
+__all__ = ["read_ecsv", "register_pyarrow_ecsv_table", "write_ecsv"]
+
 
 def is_numpy_dtype(dtype: str) -> bool:
     """Check if the given dtype is a valid numpy dtype."""
@@ -106,95 +108,101 @@ class ColumnAttrs:
     @functools.cached_property
     def parsetype(self) -> str:
         """Type used to parse the CSV file data"""
-        return self.get_parsetype_dtype_shape[0]
+        return self.parsetype_dtype_shape[0]
 
     @functools.cached_property
     def dtype(self) -> str:
         """Numpy dtype in the final column data"""
-        return self.get_parsetype_dtype_shape[1]
+        return self.parsetype_dtype_shape[1]
 
     @functools.cached_property
     def shape(self) -> tuple[int, ...]:
         """Shape of the column data"""
-        return self.get_parsetype_dtype_shape[2]
+        return self.parsetype_dtype_shape[2]
 
     @functools.cached_property
-    def get_parsetype_dtype_shape(self) -> tuple[str, str, tuple[int, ...]]:
-        """Get the parsetype, dtype, and shape of the column from ECSV header.
+    def parsetype_dtype_shape(self) -> tuple[str, str, tuple[int, ...]]:
+        """Get the parsetype, dtype, and shape of the column from ECSV header."""
+        return get_parsetype_dtype_shape(self.datatype, self.subtype, self.name)
 
-        This property implements most of the complexity of the ECSV data type
-        handling. It converts the ECSV ``datatype`` and ``subtype`` to:
-        - ``parsetype``: the type used to parse the CSV file data
-        - ``dtype``: the numpy dtype in the final column data
-        - ``shape``: the shape of the column data
 
-        The ECSV standard allows for a wide variety of data types and subtypes, and
-        we also need to handle some legacy cases and be liberal in what we accept.
-        """
-        from astropy.io.ascii.core import InconsistentTableError
-        from astropy.io.ascii.ecsv import ECSV_DATATYPES, InvalidEcsvDatatypeWarning
+def get_parsetype_dtype_shape(
+    datatype, subtype, name
+) -> tuple[str, str, tuple[int, ...]]:
+    """Get the parsetype, dtype, and shape of the column from datatype and subtype.
 
-        parsetype = "str" if self.datatype == "string" else self.datatype
-        dtype = None
-        shape = ()
+    This property implements most of the complexity of the ECSV data type
+    handling. It converts the ECSV ``datatype`` and ``subtype`` to the following:
+    - ``parsetype``: the type used to parse the CSV file data
+    - ``dtype``: the numpy dtype in the final column data
+    - ``shape``: the shape of the column data
 
-        if self.datatype not in ECSV_DATATYPES:
-            msg = (
-                f"unexpected datatype {self.datatype!r} of column {self.name!r} "
-                f"is not in allowed ECSV datatypes {ECSV_DATATYPES}."
+    The ECSV standard allows for a wide variety of data types and subtypes, and
+    we also need to handle some legacy cases and be liberal in what we accept.
+    """
+    from astropy.io.ascii.core import InconsistentTableError
+    from astropy.io.ascii.ecsv import ECSV_DATATYPES, InvalidEcsvDatatypeWarning
+
+    parsetype = "str" if datatype == "string" else datatype
+    dtype = None
+    shape = ()
+
+    if datatype not in ECSV_DATATYPES:
+        msg = (
+            f"unexpected datatype {datatype!r} of column {name!r} "
+            f"is not in allowed ECSV datatypes {ECSV_DATATYPES}."
+        )
+        # Try being liberal on input if the `parsetype` (derived from ECSV
+        # `datatype`) looks like a numpy dtype. In this case, parse the column as
+        # string and then cast as `parsetype`. This allows for back-compatibility
+        # with early versions of io.ascii.ecsv that wrote and read e.g.
+        # datatype=datetime64.
+        if is_numpy_dtype(parsetype):
+            dtype = parsetype
+            parsetype = "str"
+            warnings.warn(msg, InvalidEcsvDatatypeWarning)
+        else:
+            # No joy, this is an exception
+            raise InconsistentTableError(msg)
+
+    if subtype and parsetype != "str":
+        # Note: the "column .. failed to convert" bit is odd here but it is to match
+        # the io.ascii.ecsv behavior.
+        raise ValueError(
+            f"column {name!r} failed to convert: "
+            f'datatype of column {name!r} must be "string"'
+        )
+
+    # If `dtype` is not yet defined then default to casting output as `parsetype`.
+    if dtype is None:
+        dtype = parsetype
+
+    if subtype:
+        # Subtype can be written like "int64[2,null]" and we want to split this
+        # out to "int64" and [2, None].
+        if "[" in subtype:
+            idx = subtype.index("[")
+            dtype = subtype[:idx]
+            shape = tuple(json.loads(subtype[idx:]))
+        else:
+            dtype = subtype
+
+        # Map ECSV types to numpy dtypes
+        dtype = {"json": "object", "string": "str"}.get(dtype, dtype)
+
+        # Check if the subtype corresponds to a valid numpy dtype. This is required by
+        # the astropy implementation, but not by the ECSV standard. The standard states
+        # that an unknown subtype can be ignored, so that is what we do here (but with
+        # a warning).
+        if not is_numpy_dtype(dtype):
+            warnings.warn(
+                f"unexpected subtype {subtype!r} set for column "
+                f"{name!r}, using dtype={parsetype!r} instead.",
+                category=InvalidEcsvDatatypeWarning,
             )
-            # Try being liberal on input if the `parsetype` (derived from ECSV
-            # `datatype`) looks like a numpy dtype. In this case, parse the column as
-            # string and then cast as `parsetype`. This allows for back-compatibility
-            # with early versions of io.ascii.ecsv that wrote and read e.g.
-            # datatype=datetime64.
-            if is_numpy_dtype(parsetype):
-                dtype = parsetype
-                parsetype = "str"
-                warnings.warn(msg, InvalidEcsvDatatypeWarning)
-            else:
-                # No joy, this is an exception
-                raise InconsistentTableError(msg)
-
-        if self.subtype and parsetype != "str":
-            # Note: the "column .. failed to convert" bit is odd here but it is to match
-            # the io.ascii.ecsv behavior.
-            raise ValueError(
-                f"column {self.name!r} failed to convert: "
-                f'datatype of column {self.name!r} must be "string"'
-            )
-
-        # If `dtype` is not yet defined then default to casting output as `parsetype`.
-        if dtype is None:
             dtype = parsetype
 
-        subtype = self.subtype
-        if subtype:
-            # Subtype can be written like "int64[2,null]" and we want to split this
-            # out to "int64" and [2, None].
-            if "[" in subtype:
-                idx = subtype.index("[")
-                dtype = subtype[:idx]
-                shape = tuple(json.loads(subtype[idx:]))
-            else:
-                dtype = subtype
-
-            # Map ECSV types to numpy dtypes
-            dtype = {"json": "object", "string": "str"}.get(dtype, dtype)
-
-            # Check if the subtype corresponds to a valid numpy dtype. This is required by
-            # the astropy implementation, but not by the ECSV standard. The standard states
-            # that an unknown subtype can be ignored, so that is what we do here (but with
-            # a warning).
-            if not is_numpy_dtype(dtype):
-                warnings.warn(
-                    f"unexpected subtype {self.subtype!r} set for column "
-                    f"{self.name!r}, using dtype={parsetype!r} instead.",
-                    category=InvalidEcsvDatatypeWarning,
-                )
-                dtype = parsetype
-
-        return parsetype, dtype, shape
+    return parsetype, dtype, shape
 
 
 def read_header(
@@ -272,7 +280,8 @@ def read_data(
     return data
 
 
-def _get_str_vals(col, data):
+def get_str_vals(data):
+    """Convert the data to a list of str and get the mask if available."""
     # For masked we need a list because for multidim the data under the mask is set
     # to a compatible value.
     if hasattr(data, "mask"):
@@ -293,7 +302,7 @@ def convert_column(col: ColumnAttrs, data_in: "npt.NDArray") -> "npt.NDArray":
             # Handle three distinct column types where each row element is serialized
             # to JSON. First convert the input data array or masked array to a list of
             # str and get the mask where available.
-            str_vals, mask = _get_str_vals(col, data_in)
+            str_vals, mask = get_str_vals(data_in)
 
             if col.dtype == "object":
                 # Any Python objects serializable to JSON
