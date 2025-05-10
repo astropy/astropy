fe02f428bd6d53dfb56c70d06d742626cb9d6bdb
Varun Nikam <varunshailendra8@gmail.com>
2025-05-10 23:10:44 +0530

    Fix ruff rule RET504 violations in astropy/io

--- astropy/io/misc/parquet.py
+++ astropy/io/misc/parquet.py
@@ -304,9 +304,7 @@ def read_table_parquet(
 
     # Convert all compound columns to astropy objects
     # (e.g. time.jd1, time.jd2 into a single time column)
-    table = serialize._construct_mixins_from_columns(table)
-
-    return table
+    return serialize._construct_mixins_from_columns(table)
 
 
 def write_table_parquet(table, output, overwrite=False):
@@ -674,11 +672,7 @@ def read_parquet_votable(filename):
     data_table_with_no_metadata = Table.read(filename, format="parquet")
 
     # Stitch the two tables together to create final table
-    complete_table = vstack(
-        [empty_table_with_columns_and_metadata, data_table_with_no_metadata]
-    )
-
-    return complete_table
+    return vstack([empty_table_with_columns_and_metadata, data_table_with_no_metadata])
 
 
 def register_parquet_votable():
