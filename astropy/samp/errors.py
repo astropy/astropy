40ba555f6a92734a473e477b760725863c0132a5
Matthieu Bec <mdcb808@gmail.com>
2025-05-15 10:39:57 -0700

    SAMPHubServer._call_and_wait raises a new SAMPProxyTimeoutError (derived from SAMPProxyError) exception on timeout. This allows client code to more easily distinguish timeouts from other kind of exceptions.

--- astropy/samp/errors.py
+++ astropy/samp/errors.py
@@ -32,3 +32,9 @@ class SAMPProxyError(xmlrpc.Fault):
     """
     SAMP Proxy Hub exception.
     """
+
+
+class SAMPProxyTimeoutError(SAMPProxyError):
+    """
+    SAMP Proxy Hub timeout exception.
+    """
