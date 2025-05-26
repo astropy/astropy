96f491a8933f17ae81457420a37b139898797a2e
Eero Vaher <eero.vaher@fysik.lu.se>
2025-05-26 15:26:14 +0200

    Drop support for `numpy` < 1.24

--- astropy/units/structured.py
+++ astropy/units/structured.py
@@ -12,8 +12,6 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 
-from astropy.utils.compat.numpycompat import NUMPY_LT_1_24
-
 from .core import UNITY, Unit, UnitBase
 
 if TYPE_CHECKING:
@@ -246,11 +244,7 @@ class StructuredUnit:
             If given, should be a subclass of `~numpy.void`. By default,
             will return a new `~astropy.units.StructuredUnit` instance.
         """
-        applied = tuple(func(part) for part in self.values())
-        if NUMPY_LT_1_24:
-            results = np.array(applied, self._units.dtype)[()]
-        else:
-            results = np.void(applied, self._units.dtype)
+        results = np.void(tuple(map(func, self.values())), self._units.dtype)
         if cls is not None:
             return results.view((cls, results.dtype))
 
