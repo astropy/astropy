0575cb2be026872157501316893adeaf947c0dda
Preshanth Jagannathan <pjaganna@nrao.edu>
2025-06-19 17:28:42 -0600

    Fixing to remove Om function from base to the trait class

--- astropy/cosmology/_src/flrw/base.py
+++ astropy/cosmology/_src/flrw/base.py
@@ -428,33 +428,6 @@ class FLRW(
         return self.Om(z) + self.Ogamma(z) + self.Onu(z) + self.Ode(z) + self.Ok(z)
 
     @deprecated_keywords("z", since="7.0")
-    def Om(self, z):
-        """Return the density parameter for non-relativistic matter at redshift ``z``.
-
-        Parameters
-        ----------
-        z : Quantity-like ['redshift'], array-like
-            Input redshift.
-
-            .. versionchanged:: 7.0
-                Passing z as a keyword argument is deprecated.
-
-        Returns
-        -------
-        Om : ndarray or float
-            The density of non-relativistic matter relative to the critical
-            density at each redshift.
-            Returns `float` if the input is scalar.
-
-        Notes
-        -----
-        This does not include neutrinos, even if non-relativistic at the
-        redshift of interest; see `Onu`.
-        """
-        z = aszarr(z)
-        return self.Om0 * (z + 1.0) ** 3 * self.inv_efunc(z) ** 2
-
-    @deprecated_keywords("z", since="7.0")
     def Odm(self, z):
         """Return the density parameter for dark matter at redshift ``z``.
 
--- astropy/cosmology/_src/traits/_matter_density.py
+++ astropy/cosmology/_src/traits/_matter_density.py
@@ -1,24 +1,36 @@
-from numpy.typing import ArrayLike
-
+from astropy.cosmology._src.utils import aszarr, deprecated_keywords
 from astropy.units import Quantity
+
+__all__ = ["_MatterDensity"]
 
 
 class _MatterDensity:
     Om0: Quantity
     """Omega matter; matter density/critical density at z=0."""
 
-    def Om(self, z: Quantity | ArrayLike) -> Quantity:
-        """
-        Calculate the matter density at redshift `z`.
+    @deprecated_keywords("z", since="7.0")
+    def Om(self, z):
+        """Return the density parameter for non-relativistic matter at redshift ``z``.
 
         Parameters
         ----------
-        z : Quantity or array-like
-            Redshift.
+        z : Quantity-like ['redshift'], array-like
+            Input redshift.
+
+            .. versionchanged:: 7.0
+                Passing z as a keyword argument is deprecated.
 
         Returns
         -------
-        Quantity
-            Matter density at redshift `z`.
+        Om : ndarray or float
+            The density of non-relativistic matter relative to the critical
+            density at each redshift.
+            Returns `float` if the input is scalar.
+
+        Notes
+        -----
+        This does not include neutrinos, even if non-relativistic at the
+        redshift of interest; see `Onu`.
         """
-        return self.Om0 * (1 + z) ** 3
+        z = aszarr(z)
+        return self.Om0 * (z + 1.0) ** 3 * self.inv_efunc(z) ** 2
