4a8278db8bd5772d08e8717a91e682e1c44a5dc5
TheSkyentist <raphael.hviding@gmail.com>
2025-07-10 08:55:12 +0200

    Fixed bug where I removed sphinx_automodapi

--- docs/conf.py
+++ docs/conf.py
@@ -118,6 +118,10 @@ intersphinx_mapping.update(
         "pytest": ("https://docs.pytest.org/en/stable/", None),
         "ipython": ("https://ipython.readthedocs.io/en/stable/", None),
         "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+        "sphinx_automodapi": (
+            "https://sphinx-automodapi.readthedocs.io/en/stable/",
+            None,
+        ),
         "asdf-astropy": ("https://asdf-astropy.readthedocs.io/en/latest/", None),
         "fsspec": ("https://filesystem-spec.readthedocs.io/en/latest/", None),
         "cycler": ("https://matplotlib.org/cycler/", None),
