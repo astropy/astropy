d99942c6fba805b1e893bae94f283a0316634f17
nstarman <nstarman@users.noreply.github.com>
2025-08-23 20:20:28 -0400

    🏷️: FArray type alias
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/cosmology/_src/typing.py
+++ astropy/cosmology/_src/typing.py
@@ -1,12 +1,17 @@
 """Static typing for :mod:`astropy.cosmology`. PRIVATE API."""
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-__all__ = ["_CosmoT"]
+__all__ = ["FArray", "_CosmoT"]
 
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, TypeAlias, TypeVar
+
+import numpy as np
+from numpy.typing import NDArray
 
 if TYPE_CHECKING:
     import astropy.cosmology
 
 _CosmoT = TypeVar("_CosmoT", bound="astropy.cosmology.Cosmology")
 """Type variable for :class:`~astropy.cosmology.Cosmology` and subclasses."""
+
+FArray: TypeAlias = NDArray[np.floating]
