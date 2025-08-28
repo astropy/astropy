66116d7a1d8351d630dc381e4dc435496100aac5
Eero Vaher <eero.vaher@fysik.lu.se>
2025-08-28 23:47:21 +0200

    Show that unit modules insert bad names to __all__
    A new test reveals that unit definition modules can insert names to
    `__all__` that are not NFKC normalized. Such names are invalid because
    Python automatically applies NFKC normalization to all identifiers.

--- astropy/units/tests/test_units.py
+++ astropy/units/tests/test_units.py
@@ -746,6 +746,14 @@ def test_duplicate_define(name, message):
         u.def_unit(name, u.hourangle, namespace=namespace)
 
 
+@pytest.mark.xfail(reason="regression test to reveal a bug")
+def test_unit_module_dunder_all_nfkc_normalization():
+    # Python applies NFKC normalization to identifiers, so inserting a name to __all__
+    # that changes with NFKC normalization can only cause trouble.
+    assert "ℓ" not in u.__all__
+    assert u.ℓ is u.liter
+
+
 def test_all_units():
     from astropy.units.core import get_current_unit_registry
 
