aae94995cd89cee0d780c7cb402a8ca4fef01214
Pieter Eendebak <pieter.eendebak@gmail.com>
2025-09-07 22:55:15 +0200

    Merge pull request #18599 from eendebakpt/numpy_dtype_deprecations_v0
    Replace deprecated setting of dtype on numpy arrays in io.fits

--- astropy/io/fits/hdu/groups.py
+++ astropy/io/fits/hdu/groups.py
@@ -537,12 +537,12 @@ class GroupsHDU(PrimaryHDU, _TableLikeHDU):
                 if self.data.flags.writeable:
                     byteswapped = True
                     d = self.data.byteswap(True)
-                    d.dtype = d.dtype.newbyteorder(">")
+                    d = d.view(d.dtype.newbyteorder(">"))
                 else:
                     # If the data is not writeable, we just make a byteswapped
                     # copy and don't bother changing it back after
                     d = self.data.byteswap(False)
-                    d.dtype = d.dtype.newbyteorder(">")
+                    d = d.view(d.dtype.newbyteorder(">"))
                     byteswapped = False
             else:
                 byteswapped = False
@@ -555,8 +555,7 @@ class GroupsHDU(PrimaryHDU, _TableLikeHDU):
             # If the data was byteswapped in this method then return it to
             # its original little-endian order.
             if byteswapped:
-                d.byteswap(True)
-                d.dtype = d.dtype.newbyteorder("<")
+                self.data.byteswap(True)
 
             return cs
         else:
