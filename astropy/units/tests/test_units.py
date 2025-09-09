aff2599e0a7911bcf96df26433a326e48939f1e0
Eero Vaher <eero.vaher@fysik.lu.se>
2025-09-09 23:29:28 +0200

    Reveal invalid identifiers in unit module __all__
    The new test reveals that `astropy.units.cds.__all__` contains `"°"`
    even though that is not a valid identifier and therefore does not belong
    in `__all__`.

--- astropy/units/tests/test_units.py
+++ astropy/units/tests/test_units.py
@@ -13,7 +13,7 @@ from numpy.testing import assert_allclose
 
 from astropy import constants as c
 from astropy import units as u
-from astropy.units import utils
+from astropy.units import cds, utils
 from astropy.units.required_by_vounit import GsolLum, ksolMass, nsolRad
 from astropy.utils.compat.optional_deps import HAS_ARRAY_API_STRICT, HAS_DASK
 from astropy.utils.exceptions import AstropyDeprecationWarning
@@ -751,6 +751,11 @@ def test_unit_module_dunder_all_nfkc_normalization():
     # that changes with NFKC normalization can only cause trouble.
     assert "ℓ" not in u.__all__
     assert u.ℓ is u.liter
+
+
+@pytest.mark.xfail(reason="regression test to reveal a bug")
+def test_unit_module_dunder_all_only_indentifiers():
+    assert "°" not in cds.__all__
 
 
 def test_all_units():
