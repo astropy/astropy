Fix checksum calculation when there is a heap gap

astropy/io/fits/hdu/table.py
@@ -453,7 +453,7 @@ class _TableBaseHDU(ExtensionHDU, _TableLikeHDU):
         else:
             return len(self.data)
 
-    @lazyproperty
+    @property
     def _theap(self):
         size = self._header["NAXIS1"] * self._header["NAXIS2"]
         return self._header.get("THEAP", size)
@@ -878,7 +878,7 @@ class BinTableHDU(_TableBaseHDU):
             # by adding a block starting with zeros, and then the first few
             # bytes of the heap.
             heap_data = data._get_heap_data()
-            if extra := len(base_as_bytes) % 4:
+            if extra := self._theap % 4:
                 first_part = np.zeros(4, "u1")
                 first_part[extra:] = heap_data[: 4 - extra]
                 csum = self._compute_checksum(first_part, csum)

astropy/io/fits/tests/test_checksum.py
@@ -223,8 +223,11 @@ class TestChecksumFunctions(BaseChecksumTests):
     def test_variable_length_table_data3(self):
         """regression test for #14396"""
         # This is testing specifically a scenario where the start of the heap
-        # is not aligned with 4-byte blocks
+        # is not aligned with 4-byte blocks (32 bit integers)
 
+        # By default, the heap starts immediately after the table, which is at
+        # NAXIS1 x NAXIS2, or byte 17 in this case. This is not aligned with
+        # the 4-byte blocks
         testfile = self.temp("tmp.fits")
         col1 = fits.Column(name="a", format="1A", array=["a"])
         col2 = fits.Column(name="b", format="QD", array=[[1]])
@@ -234,6 +237,30 @@ class TestChecksumFunctions(BaseChecksumTests):
         with fits.open(testfile, checksum=True) as hdul:
             assert hdul[1].header["DATASUM"] == "1648357376"
             assert hdul[1].header["CHECKSUM"] == "2CoL4BnL2BnL2BnL"
+
+        # Here we force the heap to be aligned with the 4-byte blocks by using
+        # the THEAP keyword. This shows that we cannot always calculate DATASUM
+        # by simple concatenating the table data with the heap data.
+        testfile = self.temp("tmp2.fits")
+        col1 = fits.Column(name="a", format="1A", array=["a"])
+        col2 = fits.Column(name="b", format="QD", array=[[1]])
+        tab = fits.BinTableHDU.from_columns(name="test", columns=[col1, col2])
+        tab.header["THEAP"] = 20
+        tab.writeto(testfile, checksum=True)
+        with fits.open(testfile, checksum=True) as hdul:
+            assert hdul[1].header["DATASUM"] == "2716860416"
+            assert hdul[1].header["CHECKSUM"] == "jIAFjI19jI8CjI89"
+
+        # Here we take the previous table and just update the THEAP value to 17.
+        # This should put the heap in the same position as the first case and
+        # thus the DATASUM should be the same. However, the CHECKSUM should be
+        # different, as the header is different (it now has the THEAP keyword).
+        testfile = self.temp("tmp3.fits")
+        tab.header["THEAP"] = 17
+        tab.writeto(testfile, checksum=True)
+        with fits.open(testfile, checksum=True) as hdul:
+            assert hdul[1].header["DATASUM"] == "1648357376"
+            assert hdul[1].header["CHECKSUM"] == "jcdDjZZBjabBjYZB"
 
     def test_ascii_table_data(self):
         a1 = np.array(["abc", "def"])