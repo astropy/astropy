435f4d6e511bd4b3044ea774b54befba14daceed
Clément Robert <cr52@protonmail.com>
2025-10-14 17:13:19 +0200

    TYP: annotate IndexEngine protocol and implementations (#18698)
    * TYP: annotate IndexEngine protocol and implementations
    
    * adjust return type on IndexEngine.find
    
    * adjust return type on IndexEngine.range
    
    * adjust return type on IndexEngine.items
    
    * make protocol more lenient
    
    * resolve nameerrors
    
    * TYP: adjust annotations again

--- astropy/table/bst.py
+++ astropy/table/bst.py
@@ -1,5 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import operator
+from collections.abc import Hashable, Mapping, Sequence
+from numbers import Integral
 
 __all__ = ["BST"]
 
@@ -172,11 +174,12 @@ class BST:
         for key, row in zip(data, row_index):
             self.add(tuple(key), row)
 
-    def add(self, key, data=None):
+    def add(self, key: tuple, data: int | None = None) -> None:
         """
         Add a key, data pair.
         """
         if data is None:
+            # nothing about this branch conforms to the IndexEngine protocol
             data = key
 
         self.size += 1
@@ -203,7 +206,7 @@ class BST:
                 curr_node.data = sorted(curr_node.data)
                 return
 
-    def find(self, key):
+    def find(self, key: tuple) -> Sequence[Integral]:
         """
         Return all data values corresponding to a given key.
 
@@ -228,14 +231,14 @@ class BST:
             return (None, None)
         return self._find_recursive(key, self.root, None)
 
-    def shift_left(self, row):
+    def shift_left(self, row: int) -> None:
         """
         Decrement all rows larger than the given row.
         """
         for node in self.traverse():
             node.data = [x - 1 if x > row else x for x in node.data]
 
-    def shift_right(self, row):
+    def shift_right(self, row: int) -> None:
         """
         Increment all rows greater than or equal to the given row.
         """
@@ -278,13 +281,13 @@ class BST:
             return self._postorder(self.root, [])
         raise ValueError(f'Invalid traversal method: "{order}"')
 
-    def items(self):
+    def items(self) -> list[tuple[Hashable, list[Integral]]]:
         """
         Return BST items in order as (key, data) pairs.
         """
         return [(x.key, x.data) for x in self.traverse()]
 
-    def sort(self):
+    def sort(self) -> None:
         """
         Make row order align with key order.
         """
@@ -294,7 +297,7 @@ class BST:
             node.data = list(range(i, i + num_rows))
             i += num_rows
 
-    def sorted_data(self):
+    def sorted_data(self) -> None:
         """
         Return BST rows sorted by key values.
         """
@@ -330,7 +333,7 @@ class BST:
         else:
             parent.replace(node, new_node)
 
-    def remove(self, key, data=None):
+    def remove(self, key: tuple, data: int | None = None) -> bool:
         """
         Remove data corresponding to the given key.
 
@@ -374,13 +377,13 @@ class BST:
         self.size -= 1
         return True
 
-    def is_valid(self):
+    def is_valid(self) -> bool:
         """
         Returns whether this is a valid BST.
         """
         return self._is_valid(self.root)
 
-    def _is_valid(self, node):
+    def _is_valid(self, node) -> bool:
         if node is None:
             return True
         return (
@@ -390,7 +393,12 @@ class BST:
             and self._is_valid(node.right)
         )
 
-    def range(self, lower, upper, bounds=(True, True)):
+    def range(
+        self,
+        lower: tuple[Hashable, ...],
+        upper: tuple[Hashable, ...],
+        bounds: tuple[bool, bool] = (True, True),
+    ) -> list[int]:
         """
         Return all nodes with keys in the given range.
 
@@ -472,7 +480,7 @@ class BST:
             return -1
         return max(self._height(node.left), self._height(node.right)) + 1
 
-    def replace_rows(self, row_map):
+    def replace_rows(self, row_map: "Mapping[int, int]") -> None:
         """
         Replace all rows with the values they map to in the
         given dictionary. Any rows not present as keys in
