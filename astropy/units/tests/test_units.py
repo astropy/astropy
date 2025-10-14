d6b8ea4fedf1f9a77121f51d87cb90451baf157f
Eero Vaher <eero.vaher@fysik.lu.se>
2025-10-14 23:19:41 +0200

    Add tests for parsing `"as"` as a unit
    The symbol for the attosecond is "as", which needs to be handled with
    care because it happens to be a Python keyword. The new tests reveal
    that only the `"cds"` unit formatter can parse the string.

--- astropy/units/tests/test_units.py
+++ astropy/units/tests/test_units.py
@@ -1290,3 +1290,27 @@ def test_required_by_vounit_parsing(unit):
 @required_by_vounit_parametrization
 def test_required_by_vounit_not_in_find_equivalent_units(unit):
     assert unit not in unit.represents.bases[0].find_equivalent_units()
+
+
+@pytest.mark.parametrize(
+    "format_",
+    [
+        "cds",
+        pytest.param(
+            "fits", marks=pytest.mark.xfail(reason="regression test to reveal a bug")
+        ),
+        pytest.param(
+            "generic", marks=pytest.mark.xfail(reason="regression test to reveal a bug")
+        ),
+        pytest.param(
+            "ogip", marks=pytest.mark.xfail(reason="regression test to reveal a bug")
+        ),
+        pytest.param(
+            "vounit", marks=pytest.mark.xfail(reason="regression test to reveal a bug")
+        ),
+    ],
+)
+def test_parsing_as(format_):
+    # The symbol for the attosecond is "as", which can be problematic because it
+    # happens to be a Python keyword.
+    assert u.Unit("as", format=format_) == u.attosecond
