f80aaff08eb7f67e667c5d95978295596eacee99
Adrien Thob <adrien.thob@gmail.com>
2025-10-23 00:50:39 -0400

    Quick ruff formatting fix

--- astropy/visualization/wcsaxes/coordinate_helpers.py
+++ astropy/visualization/wcsaxes/coordinate_helpers.py
@@ -15,7 +15,7 @@ from matplotlib.transforms import Affine2D, ScaledTranslation
 
 from astropy import units as u
 from astropy.utils.decorators import deprecated_renamed_argument
-from astropy.utils.exceptions import AstropyDeprecationWarning, AstropyUserWarning 
+from astropy.utils.exceptions import AstropyDeprecationWarning, AstropyUserWarning
 
 from .axislabels import AxisLabels
 from .formatter_locator import AngleFormatterLocator, ScalarFormatterLocator
@@ -770,7 +770,7 @@ class CoordinateHelper:
                 f"({protected_kw}) of the 'loc' keyword argument will be "
                 "overwritten during the rendering. Use the 'loc' keyword "
                 "argument instead.",
-                AstropyUserWarning
+                AstropyUserWarning,
             )
 
         self._axislabel_set = True
