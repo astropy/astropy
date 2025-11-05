590b28304c3369ca9fbd4f3f1f343690adbdea38
Clément Robert <cr52@protonmail.com>
2025-11-05 11:39:20 +0100

    BUG: fix newly detected signature mismatches with numpy 2.4

--- astropy/units/quantity_helper/function_helpers.py
+++ astropy/units/quantity_helper/function_helpers.py
@@ -345,8 +345,7 @@ def full_like(a, fill_value, *args, **kwargs):
     return (a.view(np.ndarray), a._to_own_unit(fill_value)) + args, kwargs, unit, None
 
 
-@function_helper
-def putmask(a, mask, values):
+def putmask_impl(a, /, mask, values):
     from astropy.units import Quantity
 
     if isinstance(a, Quantity):
@@ -355,6 +354,18 @@ def putmask(a, mask, values):
         return (a, mask, values.to_value(dimensionless_unscaled)), {}, None, None
     else:
         raise NotImplementedError
+
+
+if not NUMPY_LT_2_4:
+
+    @function_helper
+    def putmask(a, /, mask, values):
+        return putmask_impl(a, mask=mask, values=values)
+else:
+
+    @function_helper
+    def putmask(a, mask, values):
+        return putmask_impl(a, mask=mask, values=values)
 
 
 @function_helper
@@ -468,12 +479,26 @@ def _iterable_helper(*args, out=None, **kwargs):
     return arrays, kwargs, unit, out
 
 
-@function_helper
-def concatenate(arrays, axis=0, out=None, **kwargs):
-    # TODO: make this smarter by creating an appropriately shaped
-    # empty output array and just filling it.
-    arrays, kwargs, unit, out = _iterable_helper(*arrays, out=out, axis=axis, **kwargs)
-    return (arrays,), kwargs, unit, out
+if NUMPY_LT_2_4:
+
+    @function_helper
+    def concatenate(arrays, axis=0, out=None, **kwargs):
+        # TODO: make this smarter by creating an appropriately shaped
+        # empty output array and just filling it.
+        arrays, kwargs, unit, out = _iterable_helper(
+            *arrays, out=out, axis=axis, **kwargs
+        )
+        return (arrays,), kwargs, unit, out
+else:
+
+    @function_helper
+    def concatenate(arrays, /, axis=0, out=None, **kwargs):
+        # TODO: make this smarter by creating an appropriately shaped
+        # empty output array and just filling it.
+        arrays, kwargs, unit, out = _iterable_helper(
+            *arrays, out=out, axis=axis, **kwargs
+        )
+        return (arrays,), kwargs, unit, out
 
 
 def _block(arrays, max_depth, result_ndim, depth=0):
@@ -777,7 +802,7 @@ def pad(array, pad_width, mode="constant", **kwargs):
 
 
 @function_helper
-def where(condition, *args):
+def where(condition, /, *args):
     from astropy.units import Quantity
 
     if isinstance(condition, Quantity) or len(args) != 2:
@@ -880,14 +905,14 @@ def cross_like_a_b(a, b, *args, **kwargs):
     return (a.view(np.ndarray), b.view(np.ndarray)) + args, kwargs, unit, None
 
 
-@function_helper(
-    helps={
-        np.inner,
-        np.vdot,
-        np.correlate,
-        np.convolve,
-    }
-)
+@function_helper(helps={np.inner, np.vdot})
+def cross_like_a_b_posonly(a, b, /):
+    a, b = _as_quantities(a, b)
+    unit = a.unit * b.unit
+    return (a.view(np.ndarray), b.view(np.ndarray)), {}, unit, None
+
+
+@function_helper(helps={np.correlate, np.convolve})
 def cross_like_a_v(a, v, *args, **kwargs):
     a, v = _as_quantities(a, v)
     unit = a.unit * v.unit
@@ -911,7 +936,7 @@ def einsum(*operands, out=None, **kwargs):
 
 
 @function_helper
-def bincount(x, weights=None, minlength=0):
+def bincount(x, /, weights=None, minlength=0):
     from astropy.units import Quantity
 
     if isinstance(x, Quantity):
