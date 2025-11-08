822e56dbcae1045ff31a1a2ff83d0d55892fecfb
nstarman <nstarman@users.noreply.github.com>
2025-11-08 14:28:11 -0600

    feat: no more cosmology.connect
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

--- astropy/cosmology/_src/tests/test_deprecated_modules.py
+++ astropy/cosmology/_src/tests/test_deprecated_modules.py
@@ -6,25 +6,6 @@ import warnings
 import pytest
 
 
-def test_connect():
-    """Test `astropy.cosmology.connect`."""
-    from astropy.cosmology import connect
-
-    try:
-        del connect.CosmologyFromFormat
-    except Exception:
-        pass
-
-    with (
-        warnings.catch_warnings(),  # Always raise warning so 2x test sees it too.
-        pytest.deprecated_call(
-            match=re.escape("The module `astropy.cosmology.connect` is deprecated"),
-        ),
-    ):
-        warnings.simplefilter("always")
-        from astropy.cosmology.connect import CosmologyFromFormat  # noqa: F401
-
-
 def test_core():
     """Test `astropy.cosmology.core`."""
     from astropy.cosmology import core
