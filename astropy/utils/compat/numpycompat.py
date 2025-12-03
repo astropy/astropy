0dcf273ed0b5110515141150e4fcc46e55697dfc
Marten H. van Kerkwijk <mhvk@astro.utoronto.ca>
2025-12-03 12:49:52 -0500

    MAINT: deprecate, not remove COPY_IF_NEEDED

--- astropy/utils/compat/numpycompat.py
+++ astropy/utils/compat/numpycompat.py
@@ -4,9 +4,12 @@ This is a collection of monkey patches and workarounds for bugs in
 earlier versions of Numpy.
 """
 
+import warnings
+
 import numpy as np
 
 from astropy.utils import minversion
+from astropy.utils.exceptions import AstropyPendingDeprecationWarning
 
 __all__ = [
     "NUMPY_LT_2_1",
@@ -22,3 +25,16 @@ NUMPY_LT_2_1 = not minversion(np, "2.1.0.dev")
 NUMPY_LT_2_2 = not minversion(np, "2.2.0.dev0")
 NUMPY_LT_2_3 = not minversion(np, "2.3.0.dev0")
 NUMPY_LT_2_4 = not minversion(np, "2.4.0.dev0")
+
+
+def __getattr__(attr):
+    # MHvK: Added in 8.0. Regular deprecation in 9.0, remove in 10.0?
+    if attr == "COPY_IF_NEEDED":
+        warnings.warn(
+            "COPY_IF_NEEDED is no longer needed now that astropy only "
+            "supports numpy >= 2. It can be safely replaced with 'None'. ",
+            AstropyPendingDeprecationWarning,
+        )
+        return None
+
+    raise AttributeError(f"module {__name__!r} has no attribute {attr!r}.")
