6d48e0058dd5d2d027d8ec4eb6a4e9b135bf2f86
Roy Smart <roytsmart@gmail.com>
2025-12-09 11:09:35 -0700

    coverage

--- astropy/units/tests/test_quantity_overload.py
+++ astropy/units/tests/test_quantity_overload.py
@@ -3,16 +3,9 @@ import pytest
 
 import astropy.units as u
 
-_m = [
-    1,
-    1 * u.one,
-    np.array([-1, 0, 1]),
-]
-
 _wavelength = [
     500 * u.nm,
-    [300, 400, 500] * u.AA,
-    2 * u.eV,
+    [300, 400, 500] * u.eV,
 ]
 
 _d = [
@@ -20,13 +13,19 @@ _d = [
     [0.01, 0.02, 0.03] * u.mm,
 ]
 
+_m = [
+    1,
+    [-1, 0, 1] * u.one,
+]
+
 _equivalencies = u.spectral()
 
 
 def grating_equation(
-    m: float | np.ndarray | u.Quantity[u.one],
     wavelength: u.Quantity[u.um],
     d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray | u.Quantity[u.one] = 1,
 ) -> u.Quantity[u.rad]:
     """Simple version of the grating equation which uses quantities."""
     return np.arcsin(m * wavelength / d)
@@ -34,9 +33,10 @@ def grating_equation(
 
 @u.quantity_overload
 def grating_equation_overload(
-    m: float | np.ndarray | u.Quantity[u.one],
     wavelength: u.Quantity[u.um],
     d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray = 1,
 ) -> u.Quantity[u.rad]:
     """
     A version of the grating equation which uses np.emath.arcsin(),
@@ -45,44 +45,45 @@ def grating_equation_overload(
     return np.emath.arcsin(m * wavelength / d)
 
 
-@pytest.mark.parametrize("m", _m)
 @pytest.mark.parametrize("wavelength", _wavelength)
 @pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
 def test_grating_equation(
-    m: u.Quantity,
     wavelength: u.Quantity,
     d: u.Quantity,
+    m: u.Quantity,
 ):
 
     try:
-        result_expected = grating_equation(m, wavelength, d)
+        result_expected = grating_equation(wavelength, d, m=m)
     except u.UnitTypeError:
         with pytest.raises(u.UnitConversionError):
-            grating_equation_overload(m, wavelength, d)
+            grating_equation_overload(wavelength, d, m=m)
         return
 
-    result = grating_equation_overload(m, wavelength, d)
+    result = grating_equation_overload(wavelength, d, m=m)
 
     assert np.allclose(result, result_expected)
 
 
-@pytest.mark.parametrize("m", _m)
 @pytest.mark.parametrize("wavelength", _wavelength)
 @pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
 def test_grating_equation_ndarray_inputs(
-    m: u.Quantity,
     wavelength: u.Quantity,
     d: u.Quantity,
+    m: u.Quantity,
 ):
     with pytest.raises(u.UnitConversionError):
-        grating_equation_overload(m, wavelength.value, d)
+        grating_equation_overload(wavelength.value, d, m=m)
 
 
 @u.quantity_overload(equivalencies=u.spectral())
 def grating_equation_overload_equiv(
-    m: float | np.ndarray | u.Quantity[u.one],
     wavelength: u.Quantity[u.um],
     d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray = 1,
 ) -> u.Quantity[u.rad]:
     """
     A version of the grating equation which uses np.emath.arcsin(),
@@ -91,28 +92,29 @@ def grating_equation_overload_equiv(
     return np.emath.arcsin(m * wavelength / d)
 
 
-@pytest.mark.parametrize("m", _m)
 @pytest.mark.parametrize("wavelength", _wavelength)
 @pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
 def test_grating_equation_equivalencies(
-    m: u.Quantity,
     wavelength: u.Quantity,
     d: u.Quantity,
+    m: u.Quantity,
 ):
 
     wavelength_ = wavelength.to(u.nm, equivalencies=_equivalencies)
 
-    result_expected = grating_equation(m, wavelength_, d)
-    result = grating_equation_overload_equiv(m, wavelength, d)
+    result_expected = grating_equation(wavelength_, d, m=m)
+    result = grating_equation_overload_equiv(wavelength, d, m=m)
 
     assert np.allclose(result, result_expected)
 
 
 @u.quantity_overload
-def grating_equation_overload_equiv_missing_unit(
-    m: float | np.ndarray | u.Quantity[u.one],
+def grating_equation_overload_missing_unit(
     wavelength: u.Quantity,
     d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray = 1,
 ) -> u.Quantity[u.rad]:
     """
     A version of the grating equation which has a missing type annotation.
@@ -120,13 +122,102 @@ def grating_equation_overload_equiv_missing_unit(
     return np.emath.arcsin(m * wavelength / d)
 
 
-@pytest.mark.parametrize("m", _m)
 @pytest.mark.parametrize("wavelength", _wavelength)
 @pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
 def test_grating_equation_missing_unit(
-    m: u.Quantity,
     wavelength: u.Quantity,
     d: u.Quantity,
+    m: u.Quantity,
 ):
     with pytest.raises(u.UnitConversionError):
-        grating_equation_overload_equiv_missing_unit(m, wavelength, d)
+        grating_equation_overload_missing_unit(wavelength, d, m=m)
+
+
+@u.quantity_overload
+def grating_equation_overload_multiple_units(
+    wavelength: u.Quantity[u.um] | u.Quantity[u.m],
+    d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray = 1,
+) -> u.Quantity[u.rad]:
+    """
+    A version of the grating equation which has multiple units specified.
+    """
+    return np.emath.arcsin(m * wavelength / d)
+
+
+@pytest.mark.parametrize("wavelength", _wavelength)
+@pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
+def test_grating_equation_missing_unit(
+    wavelength: u.Quantity,
+    d: u.Quantity,
+    m: u.Quantity,
+):
+    with pytest.raises(
+        expected_exception=TypeError,
+        match="Only one unit specification allowed, got .*",
+    ):
+        grating_equation_overload_multiple_units(wavelength, d, m=m)
+
+
+@u.quantity_overload(equivalencies=_equivalencies)
+def grating_equation_overload_variational(
+    wavelength: u.Quantity[u.um],
+    d: u.Quantity[u.um],
+    *args,
+    m: int | np.ndarray = 1,
+    **kwargs,
+) -> u.Quantity[u.rad]:
+    """
+    A version of the grating equation which has variational arguments.
+    """
+    return np.emath.arcsin(m * wavelength / d)
+
+
+@pytest.mark.parametrize("wavelength", _wavelength)
+@pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
+def test_grating_equation_variational(
+    wavelength: u.Quantity,
+    d: u.Quantity,
+    m: u.Quantity,
+):
+    wavelength_ = wavelength.to(u.nm, equivalencies=_equivalencies)
+
+    result_expected = grating_equation(wavelength_, d, m=m)
+    result = grating_equation_overload_variational(wavelength, d, None, m=m, foo=None)
+
+    assert np.allclose(result, result_expected)
+
+
+@u.quantity_overload(equivalencies=_equivalencies)
+def grating_equation_overload_multiple_return(
+    wavelength: u.Quantity[u.um],
+    d: u.Quantity[u.um],
+    *,
+    m: int | np.ndarray = 1,
+) -> tuple[u.Quantity[u.rad], float | np.ndarray]:
+    """
+    A version of the grating equation which uses np.emath.arcsin(),
+    which is not implemented by astropy.
+    """
+    return np.emath.arcsin(m * wavelength / d), 0
+
+
+@pytest.mark.parametrize("wavelength", _wavelength)
+@pytest.mark.parametrize("d", _d)
+@pytest.mark.parametrize("m", _m)
+def test_grating_equation_multiple_return(
+    wavelength: u.Quantity,
+    d: u.Quantity,
+    m: u.Quantity,
+):
+    wavelength_ = wavelength.to(u.nm, equivalencies=_equivalencies)
+
+    result_expected = grating_equation(wavelength_, d, m=m)
+    result_1, result_2 = grating_equation_overload_multiple_return(wavelength, d, m=m)
+
+    assert np.allclose(result_1, result_expected)
+    assert np.allclose(result_2, 0)
