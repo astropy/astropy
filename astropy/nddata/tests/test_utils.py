deb38025e815f69d35b110fb5efb51ccbf0e05fd
Fazeel Usmani <fazeelusmani18@gmail.com>
2025-12-12 16:24:35 +0530

    Remove unused import of Version from test_utils.py

--- astropy/nddata/tests/test_utils.py
+++ astropy/nddata/tests/test_utils.py
@@ -3,7 +3,6 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose, assert_array_equal
-from packaging.version import Version
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
