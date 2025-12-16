57c38168393fe44b0776dd7c23d7d3e648f5b1cc
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-12-16 10:02:38 -0500

    Merge pull request #19050 from pllim/wcslib-8.5
    MNT: Upgrade to WCSLIB 8.5

--- cextern/wcslib/C/wcsfix.h
+++ cextern/wcslib/C/wcsfix.h
@@ -1,6 +1,6 @@
 /*============================================================================
-  WCSLIB 8.4 - an implementation of the FITS WCS standard.
-  Copyright (C) 1995-2024, Mark Calabretta
+  WCSLIB 8.5 - an implementation of the FITS WCS standard.
+  Copyright (C) 1995-2025, Mark Calabretta
 
   This file is part of WCSLIB.
 
@@ -19,10 +19,10 @@
 
   Author: Mark Calabretta, Australia Telescope National Facility, CSIRO.
   http://www.atnf.csiro.au/people/Mark.Calabretta
-  $Id: wcsfix.h,v 8.4 2024/10/28 13:56:16 mcalabre Exp $
+  $Id: wcsfix.h,v 8.5 2025/12/06 13:47:41 mcalabre Exp $
 *=============================================================================
 *
-* WCSLIB 8.4 - C routines that implement the FITS World Coordinate System
+* WCSLIB 8.5 - C routines that implement the FITS World Coordinate System
 * (WCS) standard.  Refer to the README file provided with WCSLIB for an
 * overview of the library.
 *
