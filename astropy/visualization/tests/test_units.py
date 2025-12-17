3a031a183221056fc86603b2fe38c2384619e212
DavidAG <davidarnalgar@gmail.com>
2025-12-17 03:50:15 +0100

    Add test for astropy_support function to fix coverage

--- astropy/visualization/tests/test_units.py
+++ astropy/visualization/tests/test_units.py
@@ -14,7 +14,12 @@ import numpy as np
 
 from astropy import units as u
 from astropy.coordinates import Angle
-from astropy.visualization.units import MplQuantityConverter, quantity_support
+from astropy.time import Time
+from astropy.visualization.units import (
+    MplQuantityConverter,
+    astropy_support,
+    quantity_support,
+)
 
 
 @pytest.fixture
@@ -230,3 +235,25 @@ def test_convert(converter_class, val, expected):
 def test_default_units(converter_class, x, expected):
     result = converter_class.default_units(x, None)
     assert result == expected
+
+
+@pytest.mark.skipif(not HAS_PLT, reason="requires matplotlib")
+def test_astropy_support():
+    """Test that astropy_support enables both quantity and time support."""
+    fig = Figure()
+    ax = fig.add_subplot()
+
+    with astropy_support():
+        # Test that Quantity support works
+        ax.plot([1, 2, 3] * u.m, [3, 4, 5] * u.kg)
+        assert ax.xaxis.get_units() == u.m
+        assert ax.yaxis.get_units() == u.kg
+
+    # Test with Time on x-axis and Quantity on y-axis
+    fig2 = Figure()
+    ax2 = fig2.add_subplot()
+
+    with astropy_support():
+        times = Time(["2020-01-01", "2020-01-02", "2020-01-03"])
+        ax2.plot(times, [1, 2, 3] * u.m)
+        assert ax2.yaxis.get_units() == u.m
