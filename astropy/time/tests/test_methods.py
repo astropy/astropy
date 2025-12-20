19138e76c1a4eada55f6a89685d59f535181647a
Szymon Prajs <szymon.overton@gmail.com>
2025-12-20 14:21:15 +0000

    MNT: replace itertools.product in tests
    Use stacked parametrize in time tests and explicit dtype list in units tests.

--- astropy/time/tests/test_methods.py
+++ astropy/time/tests/test_methods.py
@@ -1,7 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 import copy
-import itertools
 import warnings
 
 import numpy as np
@@ -557,7 +556,8 @@ class TestArithmetic:
         self.t2 = self.__class__.t2[use_mask]
         self.jd = self.__class__.jd[use_mask]
 
-    @pytest.mark.parametrize("kw, func", list(itertools.product(kwargs, functions)))
+    @pytest.mark.parametrize("func", functions)
+    @pytest.mark.parametrize("kw", kwargs)
     def test_argfuncs(self, kw, func, use_mask):
         """
         Test that ``np.argfunc(jd, **kw)`` is the same as ``t0.argfunc(**kw)``
@@ -581,7 +581,8 @@ class TestArithmetic:
         assert t0v.shape == jdv.shape
         assert t1v.shape == jdv.shape
 
-    @pytest.mark.parametrize("kw, func", list(itertools.product(kwargs, functions)))
+    @pytest.mark.parametrize("func", functions)
+    @pytest.mark.parametrize("kw", kwargs)
     def test_funcs(self, kw, func, use_mask):
         """
         Test that ``np.func(jd, **kw)`` is the same as ``t1.func(**kw)`` where
