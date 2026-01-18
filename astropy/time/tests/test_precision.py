b4d606dd8d7c80951d3cdd6b871eed876349f65e
Vishwas Bajaj <vishwasbajaj.global@gmail.com>
2026-01-18 12:18:06 +0530

    TST: constrain Hypothesis datetime/timedelta strategies to reduce filtering

--- astropy/time/tests/test_precision.py
+++ astropy/time/tests/test_precision.py
@@ -766,7 +766,17 @@ def test_datetime_difference_agrees_with_timedelta_no_hypothesis():
 
 
 # datetimes have microsecond resolution
-@given(datetimes(), timedeltas())
+@given(
+    datetimes(
+        min_value=datetime(1900,1,1),
+        max_value=datetime(2100,1,1),
+    
+    ),
+    timedeltas(
+        min_value=timedelta(days=-1000),
+        max_value=timedelta(days=1000),
+    ),
+)
 @example(dt=datetime(2000, 1, 1, 0, 0), td=timedelta(days=-397683, microseconds=2))
 @example(dt=datetime(2179, 1, 1, 0, 0), td=timedelta(days=-795365, microseconds=53))
 @example(dt=datetime(2000, 1, 1, 0, 0), td=timedelta(days=1590729, microseconds=10))
