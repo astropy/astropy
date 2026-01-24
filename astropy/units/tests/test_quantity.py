d0285b106ab6e6d8d476035f0cbd8d7a9085903e
KartavayV <vermakartavay25@gmail.com>
2026-01-24 14:19:33 +0530

    Extend Quantity parsing to support vector-like string input

--- astropy/units/tests/test_quantity.py
+++ astropy/units/tests/test_quantity.py
@@ -1746,6 +1746,9 @@ def test_quantity_initialisation_from_string():
     q = u.Quantity("1")
     assert q.unit == u.dimensionless_unscaled
     assert q.value == 1.0
+    q = u.Quantity("[7 5 8]")
+    assert q.unit == u.dimensionless_unscaled
+    assert (q.value == np.array([7., 5., 8.])).all()
     q = u.Quantity("1.5 m/s")
     assert q.unit == u.m / u.s
     assert q.value == 1.5
@@ -1767,6 +1770,8 @@ def test_quantity_initialisation_from_string():
     assert q.unit == u.m
     q = u.Quantity("5Em")
     assert q == u.Quantity(5.0, u.Em)
+    q = u.Quantity("[4 5 6]eV")
+    assert (q == u.Quantity("[4 5 6]", u.eV)).all()
 
     with pytest.raises(TypeError):
         u.Quantity("")
@@ -1784,6 +1789,8 @@ def test_quantity_initialisation_from_string():
         u.Quantity(["5"])
     with pytest.raises(TypeError):
         u.Quantity(np.array(["5"]))
+    with pytest.raises(TypeError):
+        u.Quantity("['1' '5' '8']")
     with pytest.raises(ValueError):
         u.Quantity("5E")
     with pytest.raises(ValueError):
