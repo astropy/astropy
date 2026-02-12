e9249b2888e4f4312ad9209c69586b4dd09e15a6
coder-jayp <jayp222001@gmail.com>
2026-02-12 15:02:00 +0530

    Apply pre-commit auto-fixes: trailing whitespace, sorted imports, final newline

--- astropy/modeling/tests/test_tabular.py
+++ astropy/modeling/tests/test_tabular.py
@@ -2,8 +2,13 @@
 
 import numpy as np
 import pytest
+
 from astropy import units as u
 from astropy.modeling import models
+from astropy.utils.compat.optional_deps import HAS_SCIPY
+
+pytestmark = pytest.mark.skipif(not HAS_SCIPY, reason="Tabular models require scipy")
+
 
 def test_tabular_uses_quantity():
     """
@@ -16,7 +21,7 @@ def test_tabular_uses_quantity():
     # Unitless lookup_table
     m_unitless = models.Tabular1D(x, y)
     assert not m_unitless.uses_quantity
-    assert m_unitless(5.0) == 2.0   
+    assert m_unitless(5.0) == 2.0
     assert m_unitless(0.0) == 0.0
     assert m_unitless(10.0) == 4.0
 
@@ -31,12 +36,13 @@ def test_tabular_uses_quantity():
     z = np.outer(y, y)
     m2d_unitless = models.Tabular2D((x, x), z)
     assert not m2d_unitless.uses_quantity
-    assert m2d_unitless(5.0, 5.0) == 4.0   
+    assert m2d_unitless(5.0, 5.0) == 4.0
 
     # Tabular2D with units
     m2d_qty = models.Tabular2D((x, x), z * u.s)
     assert m2d_qty.uses_quantity
     assert m2d_qty(5.0, 5.0).unit == u.s
+
 
 @pytest.mark.parametrize("method", ["linear", "nearest"])
 def test_tabular_evaluation_permissive(method):
