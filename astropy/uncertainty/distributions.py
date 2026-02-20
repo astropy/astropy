BUG: Fix broadcasting bug in uniform() for ndim >= 2 inputs (#19308)
* BUG: Fix broadcasting bug in uniform() for ndim >= 2 inputs and add regression test

* add changelog

* Simplify code and shorten changelog per review

astropy/uncertainty/distributions.py
@@ -199,11 +199,8 @@ def uniform(
         )
 
     newshape = lower.shape + (n_samples,)
-    if lower.shape == () and upper.shape == ():
-        width = upper - lower  # scalar
-    else:
-        width = (upper - lower)[:, np.newaxis]
-        lower = lower[:, np.newaxis]
+    width = (upper - lower)[..., np.newaxis]
+    lower = lower[..., np.newaxis]
     samples = lower + width * np.random.uniform(size=newshape)
 
     return cls(samples, **kwargs)