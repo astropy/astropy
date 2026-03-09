d0251818b29be397ee1fd1dc991b14caea4ed0dc
Omar Mohammed <omar80747326@gmail.com>
2026-03-09 13:47:23 +0200

    fixed sigmaclip pickling with bottleneck bug

--- astropy/stats/nanfunctions.py
+++ astropy/stats/nanfunctions.py
@@ -101,21 +101,24 @@ if HAS_BOTTLENECK:
         nanvar=np.nanvar,
     )
 
-    def _dtype_dispatch(func_name):
-        # dispatch to bottleneck or numpy depending on the input array dtype
-        # this is done to workaround known accuracy bugs in bottleneck
-        # affecting float32 calculations
-        # see https://github.com/pydata/bottleneck/issues/379
-        # see https://github.com/pydata/bottleneck/issues/462
-        # see https://github.com/astropy/astropy/issues/17185
-        # see https://github.com/astropy/astropy/issues/11492
-        def wrapped(*args, **kwargs):
-            if args[0].dtype.str[1:] == "f8":
-                return bn_funcs[func_name](*args, **kwargs)
-            else:
-                return np_funcs[func_name](*args, **kwargs)
+    class _DtypeDispatcher:
+        """Dispatches to bottleneck or numpy based on input dtype."""
 
-        return wrapped
+        def __init__(self, func_name):
+            self.func_name = func_name
+
+        def __call__(self, *args, **kwargs):
+            if args[0].dtype.str[1:] == "f8":
+                return bn_funcs[self.func_name](*args, **kwargs)
+            else:
+                return np_funcs[self.func_name](*args, **kwargs)
+
+        def __reduce__(self):
+            # Enable pickling
+            return (_DtypeDispatcher, (self.func_name,))
+
+    def _dtype_dispatch(func_name):
+        return _DtypeDispatcher(func_name)
 
     nansum = _dtype_dispatch("nansum")
     nanmin = _dtype_dispatch("nanmin")
