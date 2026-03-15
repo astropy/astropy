06bd45597fa91d99828850031040f10a00b7c074
P. L. Lim <2090236+pllim@users.noreply.github.com>
2026-03-15 13:58:18 -0400

    Bad panda

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,7 @@ intersphinx_mapping.update(
         "pyerfa": ("https://pyerfa.readthedocs.io/en/stable/", None),
         "pytest": ("https://docs.pytest.org/en/stable/", None),
         "ipython": ("https://ipython.readthedocs.io/en/stable/", None),
-        "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+        "pandas": ("https://pandas.pydata.org/docs/", None),
         "sphinx_automodapi": (
             "https://sphinx-automodapi.readthedocs.io/en/stable/",
             None,
