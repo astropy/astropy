7d46efb5b88a90d6f427b21a5bfc3d54e633facb
Simon Conseil <contact@saimon.org>
2026-03-16 09:14:45 +0100

    Backport PR #19413: Remove unused nditer fallback code in io/fits

--- astropy/io/fits/util.py
+++ astropy/io/fits/util.py
@@ -624,23 +624,9 @@ def _array_to_file_like(arr, fileobj):
         else:
             return
 
-    if hasattr(np, "nditer"):
-        # nditer version for non-contiguous arrays
-        for item in np.nditer(arr, order="C"):
-            fileobj.write(item.tobytes())
-    else:
-        # Slower version for Numpy versions without nditer;
-        # The problem with flatiter is it doesn't preserve the original
-        # byteorder
-        byteorder = arr.dtype.byteorder
-        if (sys.byteorder == "little" and byteorder == ">") or (
-            sys.byteorder == "big" and byteorder == "<"
-        ):
-            for item in arr.flat:
-                fileobj.write(item.byteswap().tobytes())
-        else:
-            for item in arr.flat:
-                fileobj.write(item.tobytes())
+    # nditer version for non-contiguous arrays
+    for item in np.nditer(arr, order="C"):
+        fileobj.write(item.tobytes())
 
 
 def _write_string(f, s):
