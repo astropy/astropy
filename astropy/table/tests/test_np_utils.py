14b0a4521f361f161e06d4e8c99d94871bb7f166
Reem Hamraz <reemhamraz.2901@gmail.com>
2026-03-18 15:27:46 +0000

    TST: refactor test_common_dtype to use pytest.mark.parametrize

--- astropy/table/tests/test_np_utils.py
+++ astropy/table/tests/test_np_utils.py
@@ -1,13 +1,11 @@
+import itertools
+
 import numpy as np
+import pytest
 
 from astropy.table import np_utils
 
-
-def test_common_dtype():
-    """
-    Test that allowed combinations are those expected.
-    """
-    dtype = [
+DTYPES = [
         ("int", int),
         ("uint8", np.uint8),
         ("float32", np.float32),
@@ -17,86 +15,41 @@ def test_common_dtype():
         ("bool", bool),
         ("object", np.object_),
     ]
-    arr = np.empty(1, dtype=dtype)
-    fail = set()
-    succeed = set()
-    for name1, _ in dtype:
-        for name2, _ in dtype:
-            try:
-                np_utils.common_dtype([arr[name1], arr[name2]])
-                succeed.add(f"{name1} {name2}")
-            except np_utils.TableMergeError:
-                fail.add(f"{name1} {name2}")
 
-    # known bad combinations
-    bad = {
-        "str int",
-        "str bool",
-        "uint8 bool",
-        "uint8 str",
-        "object float32",
-        "bool object",
-        "uni uint8",
-        "int str",
-        "bool str",
-        "bool float64",
-        "bool uni",
-        "str float32",
-        "uni float64",
-        "uni object",
-        "bool uint8",
-        "object float64",
-        "float32 bool",
-        "str uint8",
-        "uni bool",
-        "float64 bool",
-        "float64 object",
-        "int bool",
-        "uni int",
-        "uint8 object",
-        "int uni",
-        "uint8 uni",
-        "float32 uni",
-        "object uni",
-        "bool float32",
-        "uni float32",
-        "object str",
-        "int object",
-        "str float64",
-        "object int",
-        "float64 uni",
-        "bool int",
-        "object bool",
-        "object uint8",
-        "float32 object",
-        "str object",
-        "float64 str",
-        "float32 str",
-    }
-    assert fail == bad
+# Known bad combinations that must raise TableMergeError
+BAD = {
+    "str int", "str bool", "uint8 bool", "uint8 str",
+    "object float32", "bool object", "uni uint8", "int str",
+    "bool str", "bool float64", "bool uni", "str float32",
+    "uni float64", "uni object", "bool uint8", "object float64",
+    "float32 bool", "str uint8", "uni bool", "float64 bool",
+    "float64 object", "int bool", "uni int", "uint8 object",
+    "int uni", "uint8 uni", "float32 uni", "object uni",
+    "bool float32", "uni float32", "object str", "int object",
+    "str float64", "object int", "float64 uni", "bool int",
+    "object bool", "object uint8", "float32 object", "str object",
+    "float64 str", "float32 str",
+}
 
-    good = {
-        "float64 int",
-        "int int",
-        "uint8 float64",
-        "uint8 int",
-        "str uni",
-        "float32 float32",
-        "float64 float64",
-        "float64 uint8",
-        "float64 float32",
-        "int uint8",
-        "int float32",
-        "uni str",
-        "int float64",
-        "uint8 float32",
-        "float32 int",
-        "float32 uint8",
-        "bool bool",
-        "uint8 uint8",
-        "str str",
-        "float32 float64",
-        "object object",
-        "uni uni",
-    }
-    assert succeed == good
+
+@pytest.mark.parametrize(
+    "name1, name2",
+    [
+        (name1, name2)
+        for (name1, _), (name2, _) in itertools.product(DTYPES, DTYPES)
+    ],
+)
+def test_common_dtype(name1, name2):
+    """
+    Test common_dtype() for every combination of supported dtypes.
+    Known-bad combinations must raise TableMergeError;
+    all others must succeed.
+    """
+    arr = np.empty(1, dtype=DTYPES)
+    pair = f"{name1} {name2}"
+    if pair in BAD:
+        with pytest.raises(np_utils.TableMergeError):
+            np_utils.common_dtype([arr[name1], arr[name2]])
+    else:
+        # Should not raise
+        np_utils.common_dtype([arr[name1], arr[name2]])
