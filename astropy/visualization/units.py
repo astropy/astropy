7fc923ef7dc901317f3349e14772490f8da35e77
Julian Harbeck <harbeck@tu-berlin.de>
2026-04-26 14:07:56 +0200

    Move `MplQuantityConverter` into `HAS_MATPLOTLIB` check

--- astropy/visualization/units.py
+++ astropy/visualization/units.py
@@ -1,8 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-__all__ = ["MplQuantityConverter", "quantity_support"]
-
-import types
 from contextlib import ContextDecorator
 
 import numpy as np
@@ -10,28 +7,99 @@ import numpy as np
 from astropy import units as u
 from astropy.utils.compat.optional_deps import HAS_MATPLOTLIB
 
-if HAS_MATPLOTLIB:
-    from matplotlib.ticker import (
-        AutoLocator,
-        FormatStrFormatter,
-        FuncFormatter,
-        MultipleLocator,
-    )
-    from matplotlib.units import AxisInfo, ConversionInterface, registry
-else:  # Create mock-up classes to avoid import errors when matplotlib is not available.
+__all__ = ["MplQuantityConverter", "quantity_support"]
 
-    class MplMockUp:
+if HAS_MATPLOTLIB:
+    from matplotlib import ticker, units
+
+    class MplQuantityConverter(units.ConversionInterface, ContextDecorator):
+        """Matplotlib converter for ``astropy.units.Quantity``.
+
+        Registers itself to matplotlib as the converter for
+        ``astropy.units.Quantity`` when initialized. If used as a context manager,
+        it will restore the original converter upon exit. Also see
+        :meth:`quantity_support` for a convenient way to use this converter
+        with an optional format for the ``axisinfo``.
+        """
+
+        def __init__(self):
+            # Keep track of original converter in case the context manager is
+            # used in a nested way.
+            self._original_converter = {u.Quantity: units.registry.get(u.Quantity)}
+            units.registry[u.Quantity] = self
+
+        @staticmethod
+        def axisinfo(unit, axis, format=None):
+            if format is None:
+                format = _default_format
+            if unit == u.radian:
+
+                def rad_fn(x, pos=None):
+                    n = int((x / np.pi) * 2.0 + 0.25)
+                    if n < 3:
+                        return ("0", "π/2", "π")[n]
+                    elif n % 2 == 0:
+                        return f"{n // 2}π"
+                    else:
+                        return f"{n}π/2"
+
+                return units.AxisInfo(
+                    majloc=ticker.MultipleLocator(base=np.pi / 2),
+                    majfmt=ticker.FuncFormatter(rad_fn),
+                    label=unit.to_string(),
+                )
+            elif unit == u.degree:
+                return units.AxisInfo(
+                    majloc=ticker.AutoLocator(),
+                    majfmt=ticker.FormatStrFormatter("%g°"),
+                    label=unit.to_string(),
+                )
+            elif unit is not None:
+                return units.AxisInfo(label=unit.to_string(format))
+            return None
+
+        @staticmethod
+        def convert(val, unit, axis):
+            if isinstance(val, u.Quantity):
+                return val.to_value(unit)
+            elif (
+                all(
+                    hasattr(val, attr)
+                    for attr in ["__getitem__", "__iter__", "__len__"]
+                )
+                and len(val) > 0
+                and isinstance(val[0], u.Quantity)
+            ):
+                return [v.to_value(unit) for v in val]
+            else:
+                return val
+
+        @staticmethod
+        def default_units(x, axis):
+            if hasattr(x, "unit"):
+                return x.unit
+            elif (
+                all(hasattr(x, attr) for attr in ["__getitem__", "__iter__", "__len__"])
+                and len(x) > 0
+                and hasattr(x[0], "unit")
+            ):
+                return x[0].unit
+            return None
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, type, value, tb):
+            if self._original_converter[u.Quantity] is None:
+                del units.registry[u.Quantity]
+            else:
+                units.registry[u.Quantity] = self._original_converter[u.Quantity]
+
+else:
+    # Create mock-up classes to avoid import errors when matplotlib is not available.
+    class MplQuantityConverter:
         def __init__(self, *args, **kwargs):
             raise ImportError("matplotlib is required in order to use this class.")
-
-    ConversionInterface = types.new_class("ConversionInterface", (MplMockUp,))
-    AxisInfo = types.new_class("AxisInfo", (MplMockUp,))
-    MultipleLocator = types.new_class("MultipleLocator", (MplMockUp,))
-    FuncFormatter = types.new_class("FuncFormatter", (MplMockUp,))
-    AutoLocator = types.new_class("AutoLocator", (MplMockUp,))
-    FormatStrFormatter = types.new_class("FormatStrFormatter", (MplMockUp,))
-
-    registry = {}
 
 
 __doctest_skip__ = ["quantity_support"]
@@ -78,84 +146,3 @@ def quantity_support(format=None):
             return MplQuantityConverter.axisinfo(unit, axis, format)
 
     return MplQuantityConverterFormatted()
-
-
-class MplQuantityConverter(ConversionInterface, ContextDecorator):
-    """Matplotlib converter for ``astropy.units.Quantity``.
-
-    Registers itself to matplotlib as the converter for
-    ``astropy.units.Quantity`` when initialized. If used as a context manager,
-    it will restore the original converter upon exit. Also see
-    :meth:`quantity_support` for a convenient way to use this converter
-    with an optional format for the ``axisinfo``.
-    """
-
-    def __init__(self):
-        # Keep track of original converter in case the context manager is
-        # used in a nested way.
-        self._original_converter = {u.Quantity: registry.get(u.Quantity)}
-        registry[u.Quantity] = self
-
-    @staticmethod
-    def axisinfo(unit, axis, format=None):
-        if format is None:
-            format = _default_format
-        if unit == u.radian:
-
-            def rad_fn(x, pos=None):
-                n = int((x / np.pi) * 2.0 + 0.25)
-                if n < 3:
-                    return ("0", "π/2", "π")[n]
-                elif n % 2 == 0:
-                    return f"{n // 2}π"
-                else:
-                    return f"{n}π/2"
-
-            return AxisInfo(
-                majloc=MultipleLocator(base=np.pi / 2),
-                majfmt=FuncFormatter(rad_fn),
-                label=unit.to_string(),
-            )
-        elif unit == u.degree:
-            return AxisInfo(
-                majloc=AutoLocator(),
-                majfmt=FormatStrFormatter("%g°"),
-                label=unit.to_string(),
-            )
-        elif unit is not None:
-            return AxisInfo(label=unit.to_string(format))
-        return None
-
-    @staticmethod
-    def convert(val, unit, axis):
-        if isinstance(val, u.Quantity):
-            return val.to_value(unit)
-        elif (
-            all(hasattr(val, attr) for attr in ["__getitem__", "__iter__", "__len__"])
-            and len(val) > 0
-            and isinstance(val[0], u.Quantity)
-        ):
-            return [v.to_value(unit) for v in val]
-        else:
-            return val
-
-    @staticmethod
-    def default_units(x, axis):
-        if hasattr(x, "unit"):
-            return x.unit
-        elif (
-            all(hasattr(x, attr) for attr in ["__getitem__", "__iter__", "__len__"])
-            and len(x) > 0
-            and hasattr(x[0], "unit")
-        ):
-            return x[0].unit
-        return None
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, tb):
-        if self._original_converter[u.Quantity] is None:
-            del registry[u.Quantity]
-        else:
-            registry[u.Quantity] = self._original_converter[u.Quantity]
