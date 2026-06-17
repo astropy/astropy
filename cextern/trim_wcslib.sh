59b21ecfcd6662b274f021f2181d6813da3338c5
Thomas Robitaille <thomas.robitaille@gmail.com>
2026-06-17 22:05:10 +0100

    Remove build-related files for WCSLIB since we use setup_package.py instead

--- cextern/trim_wcslib.sh
+++ cextern/trim_wcslib.sh
@@ -16,3 +16,20 @@ rm -rf wcslib/utils
 rm wcslib/*.pdf
 rm wcslib/.clang-tidy
 rm wcslib/clang-tidy
+
+# Autotools/build infrastructure -- astropy builds wcslib via
+# astropy/wcs/setup_package.py and writes its own wcsconfig.h, so none of
+# the configure/make machinery is used.
+rm -rf wcslib/config
+rm wcslib/configure
+rm wcslib/configure.ac
+rm wcslib/GNUmakefile
+rm wcslib/C/GNUmakefile
+rm wcslib/makedefs.in
+rm wcslib/flavours
+rm wcslib/wcsconfig.h.in
+rm wcslib/wcsconfig_f77.h.in
+rm wcslib/wcsconfig_tests.h.in
+rm wcslib/wcsconfig_utils.h.in
+rm wcslib/wcslib.pc.in
+rm wcslib/INSTALL
