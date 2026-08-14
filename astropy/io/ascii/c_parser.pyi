8386645b53cd0857ef955cd2870de0b7c34c73fb
Rishi Jha <126956744+Jha-2022@users.noreply.github.com>
2026-08-14 15:59:06 +0530

    Update c_parser.pyi

--- astropy/io/ascii/c_parser.pyi
+++ astropy/io/ascii/c_parser.pyi
@@ -7,16 +7,16 @@ class CParser:
         table: Any,
         strip_whitespace_lines: bool,
         strip_whitespace_fields: bool,
-        delimiter: str = ...,
-        header_start: int | None = ...,
-        data_start: int | None = ...,
-        comment: str = ...,
-        quotechar: str = ...,
-        escapechar: str = ...,
-        fill_extra_cols: bool = ...,
+        delimiter: str | Ellipsis = ...,
+        header_start: int | None | Ellipsis = ...,
+        data_start: int | None | Ellipsis = ...,
+        comment: str | Ellipsis = ...,
+        quotechar: str | Ellipsis = ...,
+        escapechar: str | Ellipsis = ...,
+        fill_extra_cols: bool | Ellipsis = ...,
         **kwargs: Any,
     ) -> None: ...
 
-    def read(self, nrows: int | None = ...) -> list[tuple[Any, ...]]: ...
+    def read(self, nrows: int | None | Ellipsis = ...) -> list[tuple[Any, ...]]: ...
 
     def setup_tokenizer(self) -> None: ...
