68b2ac4485da5a17ec1afbd50d1b412d47856d11
Rishi Jha <126956744+Jha-2022@users.noreply.github.com>
2026-08-20 12:43:26 +0530

    Update c_parser.pyi

--- astropy/io/ascii/c_parser.pyi
+++ astropy/io/ascii/c_parser.pyi
@@ -3,17 +3,17 @@ from typing import Any
 class CParser:
     def __init__(
         self,
-        table: Any,
+        table: str | bytes | list[str],
         strip_whitespace_lines: bool,
         strip_whitespace_fields: bool,
-        delimiter: str = ...,
+        delimiter: str = ",",
         header_start: int | None = None,
         data_start: int | None = None,
-        comment: str = ...,
-        quotechar: str = ...,
-        escapechar: str = ...,
-        fill_extra_cols: bool = ...,
-        **kwargs: Any,
+        comment: str = "#",
+        quotechar: str = '"',
+        escapechar: str = "\\",
+        fill_extra_cols: bool = False,
+        **kwargs: object,
     ) -> None: ...
 
     def read(self, nrows: int | None = None) -> list[tuple[str | int | float | bool, ...]]: ...
