a37dd1a33edca1fbcdfe19236d2416da90569ee1
Rishi Jha <126956744+Jha-2022@users.noreply.github.com>
2026-08-20 11:45:03 +0530

    Refactor CParser constructor and read method types

--- astropy/io/ascii/c_parser.pyi
+++ astropy/io/ascii/c_parser.pyi
@@ -1,22 +1,19 @@
 from typing import Any
 
-
 class CParser:
     def __init__(
         self,
         table: Any,
         strip_whitespace_lines: bool,
         strip_whitespace_fields: bool,
-        delimiter: str | Ellipsis = ...,
-        header_start: int | None | Ellipsis = ...,
-        data_start: int | None | Ellipsis = ...,
-        comment: str | Ellipsis = ...,
-        quotechar: str | Ellipsis = ...,
-        escapechar: str | Ellipsis = ...,
-        fill_extra_cols: bool | Ellipsis = ...,
+        delimiter: str = ...,
+        header_start: int | None = None,
+        data_start: int | None = None,
+        comment: str = ...,
+        quotechar: str = ...,
+        escapechar: str = ...,
+        fill_extra_cols: bool = ...,
         **kwargs: Any,
     ) -> None: ...
 
-    def read(self, nrows: int | None | Ellipsis = ...) -> list[tuple[Any, ...]]: ...
-
-    def setup_tokenizer(self) -> None: ...
+    def read(self, nrows: int | None = None) -> list[tuple[str | int | float | bool, ...]]: ...
