6946e1fce48f3bc1c3eb4e93d2bd97950080373e
Stuart Mumford <stuart@cadair.com>
2026-09-21 17:09:37 +0100

    Don't collapse navigation on "prod" RTD builds

--- docs/conf.py
+++ docs/conf.py
@@ -270,6 +270,19 @@ html_theme_options = {
     "collapse_navigation": True,
 }
 
+# If we are on RTD, and it's not a PR build we don't want to collapse
+# the navigation as it's nice to have the full nav tree on our actual
+# published docs, but it's a waste of time everywhere else.
+if rtd_version := os.environ.get("READTHEDOCS_VERSION"):
+    is_pr = False
+    try:
+        int(rtd_version)
+        is_pr = True
+    except Exception:
+        pass
+    if not is_pr:
+        html_theme_options["collapse_navigation"] = False
+
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 html_title = f"{project} v{release}"
